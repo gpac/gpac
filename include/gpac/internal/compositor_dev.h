c68fd75b450234c439d6b482d105f9f014a3ba2b
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2007-10-16 12:30:40 +0000

    no message
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@703 63c20433-aa62-49bd-875c-5a186b69a8fb

--- include/gpac/internal/compositor_dev.h
+++ include/gpac/internal/compositor_dev.h
@@ -810,7 +810,6 @@ void gf_sc_audio_unregister(GF_AudioInput *ai);
 
 
 #ifndef GPAC_DISABLE_SVG
-Bool gf_term_check_iri_change(GF_Terminal *term, MFURL *url, XMLRI *iri);
 GF_Err gf_term_set_mfurl_from_uri(GF_Terminal *sr, MFURL *mfurl, XMLRI *iri);
 Fixed gf_sc_svg_convert_length_to_display(GF_Compositor *sr, SVG_Length *length);
 
