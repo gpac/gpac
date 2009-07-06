34dfabd947c2fa85468ca1c11cf31643ffe86239
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2009-07-06 14:52:48 +0000

    no message
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@1311 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/scene_manager/loader_bt.c
+++ src/scene_manager/loader_bt.c
@@ -1496,7 +1496,6 @@ GF_Node *gf_bt_peek_node(GF_BTParser *parser, char *defID)
 		if ( (!prev_is_insert && !strcmp(str, "AT")) || !strcmp(str, "PROTO") ) {
 			/*only check in current command (but be aware of conditionals..)*/
 			if (!the_node && gf_list_find(parser->bifs_au->commands, parser->cur_com)) {
-				gf_bt_report(parser, GF_BAD_PARAM, "Cannot find node %s\n", nName);
 				break;
 			}
 			continue;
@@ -3257,7 +3256,9 @@ GF_Err gf_bt_loader_run_intern(GF_BTParser *parser, GF_Command *init_com, Bool i
 		/*BIFS commands*/
 		else if (!strcmp(str, "REPLACE") || !strcmp(str, "INSERT") || !strcmp(str, "APPEND") || !strcmp(str, "DELETE")
 			/*BIFS extended commands*/
-			|| !strcmp(str, "GLOBALQP") || !strcmp(str, "MULTIPLEREPLACE") || !strcmp(str, "MULTIPLEINDREPLACE") || !strcmp(str, "XDELETE") || !strcmp(str, "DELETEPROTO") || !strcmp(str, "INSERTPROTO") ) {
+			|| !strcmp(str, "GLOBALQP") || !strcmp(str, "MULTIPLEREPLACE") || !strcmp(str, "MULTIPLEINDREPLACE") || !strcmp(str, "XDELETE") || !strcmp(str, "DELETEPROTO") || !strcmp(str, "INSERTPROTO") 
+			|| !strcmp(str, "XREPLACE") 
+			) {
 			Bool is_base_stream = parser->stream_id ? 0 : 1;
 
 			if (!parser->stream_id) parser->stream_id = parser->base_bifs_id;
