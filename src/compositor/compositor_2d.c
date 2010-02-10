dc7f171ac61ac9dd1173a1df8de4d1420927b603
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2010-02-10 16:09:37 +0000

    Fixed bug in video mode selection (system vs video memory)
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@1621 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/compositor/compositor_2d.c
+++ src/compositor/compositor_2d.c
@@ -470,11 +470,8 @@ static Bool compositor_2d_draw_bitmap_ex(GF_VisualManager *visual, GF_TextureHan
 		if (e) {
 			GF_LOG(GF_LOG_ERROR, GF_LOG_COMPOSE, ("[Compositor2D] Error during hardware blit - trying with soft one\n"));
 			use_soft_stretch = 1;
-			if (!visual->compositor->video_memory) {
-				GF_LOG(GF_LOG_INFO, GF_LOG_COMPOSE, ("[Compositor2D] Reconfiguring video output to use video memory\n"));
-				visual->compositor->video_memory = 1;
-				visual->compositor->root_visual_setup = 0;
-			}
+			visual->compositor->video_memory = visual->compositor->video_memory ? 2 : 1;
+			visual->compositor->root_visual_setup = 0;
 		}
 	}
 	if (use_soft_stretch) {
@@ -485,6 +482,11 @@ static Bool compositor_2d_draw_bitmap_ex(GF_VisualManager *visual, GF_TextureHan
 			e = visual->compositor->video_out->LockBackBuffer(visual->compositor->video_out, &backbuffer, 0);
 		} else {
 			GF_LOG(GF_LOG_ERROR, GF_LOG_COMPOSE, ("[Compositor2D] Cannot lock back buffer - Error %s\n", gf_error_to_string(e) ));
+		}
+		if (!visual->compositor->video_memory) {
+			GF_LOG(GF_LOG_INFO, GF_LOG_COMPOSE, ("[Compositor2D] Reconfiguring video output to use video memory\n"));
+			visual->compositor->video_memory = 1;
+			visual->compositor->root_visual_setup = 0;
 		}
 	}
 	visual->has_modif = 1;
@@ -673,7 +675,8 @@ GF_Err compositor_2d_set_aspect_ratio(GF_Compositor *compositor)
 	evt.setup.width = compositor->vp_width;
 	evt.setup.height = compositor->vp_height;
 	evt.setup.opengl_mode = 0;
-	evt.setup.system_memory = compositor->video_memory ? 0 : 1;
+	evt.setup.system_memory = (compositor->video_memory==1) ? 0 : 1;
+
 #ifdef OPENGL_RASTER
 	if (compositor->opengl_raster) {
 		evt.setup.opengl_mode = 1;
