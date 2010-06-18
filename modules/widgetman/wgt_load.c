f665221f89353f3b81f8a34d5fb956449d88f558
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2010-06-18 09:32:38 +0000

    Initial commit of W3C/MPEG-U widget manager module
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@1845 63c20433-aa62-49bd-875c-5a186b69a8fb

---
+++ modules/widgetman/wgt_load.c
@@ -0,0 +1,290 @@
+//This software module was originally developed by TelecomParisTech in the
+//course of the development of MPEG-U Widgets (ISO/IEC 23007-1) standard.
+//
+//This software module is an implementation of a part of one or 
+//more MPEG-U Widgets (ISO/IEC 23007-1) tools as specified by the MPEG-U Widgets
+//(ISO/IEC 23007-1) standard. ISO/IEC gives users of the MPEG-U Widgets
+//(ISO/IEC 23007-1) free license to this software module or modifications
+//thereof for use in hardware or software products claiming conformance to
+//the MPEG-U Widgets (ISO/IEC 23007-1). Those intending to use this software
+//module in hardware or software products are advised that its use may
+//infringe existing patents.
+//The original developer of this software module and his/her company, the
+//subsequent editors and their companies, and ISO/IEC have no liability 
+//for use of this software module or modifications thereof in an implementation. 
+//Copyright is not released for non MPEG-U Widgets (ISO/IEC 23007-1) conforming 
+//products. 
+//Telecom ParisTech retains full right to use the code for his/her own purpose, 
+//assign or donate the code to a third party and to inhibit third parties from 
+//using the code for non MPEG-U Widgets (ISO/IEC 23007-1) conforming products. 
+//
+//This copyright notice must be included in all copies or derivative works.
+//
+//Copyright (c) 2009.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////
+//
+//	Authors:	
+//					Cyril Concolato, Telecom ParisTech
+//					Jean Le Feuvre, Telecom ParisTech
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+
+#include <gpac/internal/terminal_dev.h>
+#include <gpac/internal/scenegraph_dev.h>
+#include <gpac/nodes_svg.h>
+#include <gpac/constants.h>
+
+#ifdef GPAC_HAS_SPIDERMONKEY
+
+typedef struct
+{
+	GF_Scene *scene;
+	u8 oti;
+	char *file_name;
+	u32 file_size;
+	Bool loaded;
+} WgtLoad;
+
+static GF_Err WGT_ProcessData(GF_SceneDecoder *plug, char *inBuffer, u32 inBufferLength, 
+								u16 ES_ID, u32 stream_time, u32 mmlevel)
+{
+	GF_Err e = GF_OK;
+	WgtLoad *wgtload = (WgtLoad *)plug->privateStack;
+
+	if (stream_time==(u32)-1) {
+		gf_sg_reset(wgtload->scene->graph);
+		return GF_OK;
+	}
+
+	switch (wgtload->oti) {
+	case GPAC_OTI_PRIVATE_SCENE_WGT:
+		if (wgtload->file_name && !wgtload->loaded) { 
+			const char *path, *wmpath;
+			char *tmp;
+			GF_Node *n, *root;
+			GF_FieldInfo info;
+			FILE *jsfile;
+			GF_ChildNodeItem *last;
+			
+			wgtload->loaded = 1;
+
+			gf_sg_add_namespace(wgtload->scene->graph, "http://www.w3.org/2000/svg", NULL); 
+			gf_sg_add_namespace(wgtload->scene->graph, "http://www.w3.org/1999/xlink", "xlink"); 
+			gf_sg_add_namespace(wgtload->scene->graph, "http://www.w3.org/2001/xml-events", "ev"); 		
+			gf_sg_set_scene_size_info(wgtload->scene->graph, 800, 600, 1);
+
+			/* modify the scene with an Inline/Animation pointing to the widget start file URL */			
+			n = root = gf_node_new(wgtload->scene->graph, TAG_SVG_svg);			
+			gf_node_register(root, NULL);			
+			gf_sg_set_root_node(wgtload->scene->graph, root);
+			gf_node_get_attribute_by_tag(n, TAG_SVG_ATT_viewBox, 1, 0, &info);
+			gf_svg_parse_attribute(n, &info, "0 0 320 240", 0);
+			gf_node_get_attribute_by_name(n, "xmlns", 0, 1, 0, &info);
+			gf_svg_parse_attribute(n, &info, "http://www.w3.org/2000/svg", 0);
+/*
+			gf_sg_set_scene_size_info(wgtload->scene->graph, 800, 600, 1);
+			gf_node_get_attribute_by_tag(n, TAG_SVG_ATT_width, 1, 0, &info);
+			gf_svg_parse_attribute(n, &info, "800", 0);
+			gf_node_get_attribute_by_tag(n, TAG_SVG_ATT_height, 1, 0, &info);
+			gf_svg_parse_attribute(n, &info, "600", 0);
+*/
+			gf_node_init(n);
+
+			n = gf_node_new(wgtload->scene->graph, TAG_SVG_animation);			
+			gf_node_set_id(n, 1, "w_anim");
+			gf_node_register(n, root);
+			gf_node_list_add_child_last(&((GF_ParentNode *)root)->children, n, &last);
+			gf_node_get_attribute_by_tag(n, TAG_SVG_ATT_width, 1, 0, &info);
+			gf_svg_parse_attribute(n, &info, "320", 0);
+			gf_node_get_attribute_by_tag(n, TAG_SVG_ATT_height, 1, 0, &info);
+			gf_svg_parse_attribute(n, &info, "240", 0);
+			gf_node_init(n);
+			
+			tmp = wgtload->file_name;
+			while (tmp = strchr(tmp, '\\')) {
+				tmp[0] = '/';
+				tmp++;
+			}
+
+			n = gf_node_new(wgtload->scene->graph, TAG_SVG_script);
+			gf_node_register(n, root);
+			gf_node_list_add_child_last(&((GF_ParentNode *)root)->children, n, &last);
+			path = gf_modules_get_option((GF_BaseInterface *)plug, "Widgets", "WidgetLoadScript");
+			jsfile = path ? fopen(path, "rt") : NULL;
+			if (jsfile) {
+				fclose(jsfile);
+				gf_node_get_attribute_by_tag(n, TAG_XLINK_ATT_href, 1, 0, &info);
+				gf_svg_parse_attribute(n, &info, (char *) path, 0);
+			} else {
+				const char *load_fun = "function load_widget(wid_url) {\n"
+										"	var wid = WidgetManager.load(wid_url);\n"
+										"	var anim = document.getElementById('w_anim');\n"
+										"	if (wid != null) {\n"
+										"		wid.activate(anim);"
+										"		anim.setAttributeNS('http://www.w3.org/1999/xlink', 'href', wid.main);\n"
+										"	} else {\n" 
+										"		alert('Widget ' + wid_url + ' is not valid');\n" 
+										"	}\n"
+										"}\n";
+
+				gf_dom_add_text_node(n, gf_strdup(load_fun) );
+			}
+			gf_node_init(n);
+
+
+			wmpath = gf_modules_get_option((GF_BaseInterface *)plug, "Widgets", "WidgetManagerScript");
+			jsfile = wmpath ? fopen(wmpath, "rt") : NULL;
+			if (jsfile) {
+				fclose(jsfile);
+				n = gf_node_new(wgtload->scene->graph, TAG_SVG_script);
+				gf_node_register(n, root);
+				gf_node_list_add_child_last(&((GF_ParentNode *)root)->children, n, &last);
+				gf_node_get_attribute_by_tag(n, TAG_XLINK_ATT_href, 1, 0, &info);
+				gf_svg_parse_attribute(n, &info, (char *) wmpath, 0);
+				gf_node_init(n);
+
+				n = gf_node_new(wgtload->scene->graph, TAG_SVG_script);
+				gf_node_register(n, root);
+				gf_node_list_add_child_last(&((GF_ParentNode *)root)->children, n, &last);
+				gf_dom_add_text_node(n, gf_strdup("widget_manager_init();") );
+				gf_node_init(n);
+			}
+
+			tmp = gf_malloc(sizeof(char) * (strlen(wgtload->file_name)+50) );
+			sprintf(tmp, "load_widget(\"%s\");\n", wgtload->file_name);
+
+			n = gf_node_new(wgtload->scene->graph, TAG_SVG_script);
+			gf_node_register(n, root);
+			gf_node_list_add_child_last(&((GF_ParentNode *)root)->children, n, &last);
+			gf_dom_add_text_node(n, gf_strdup(tmp) );
+			gf_free(tmp);
+
+			gf_node_init(n);
+
+			if ((wgtload->scene->graph_attached!=1) && (gf_sg_get_root_node(wgtload->scene->graph)!=NULL) ) {
+				gf_scene_attach_to_compositor(wgtload->scene);
+				e = GF_EOS;
+			}
+		}
+		break;
+	
+	default: 
+		return GF_BAD_PARAM;
+	}
+	return e;
+}
+
+static GF_Err WGT_AttachScene(GF_SceneDecoder *plug, GF_Scene *scene, Bool is_scene_decoder)
+{
+	WgtLoad *wgtload = (WgtLoad *)plug->privateStack;
+	wgtload->scene = scene;
+	return GF_OK;
+}
+
+static GF_Err WGT_ReleaseScene(GF_SceneDecoder *plug)
+{
+	WgtLoad *wgtload = (WgtLoad *)plug->privateStack;
+	wgtload->scene = NULL;
+	return GF_OK;
+}
+
+static GF_Err WGT_AttachStream(GF_BaseDecoder *plug, GF_ESD *esd)
+{
+	GF_BitStream *bs;
+	WgtLoad *wgtload = (WgtLoad *)plug->privateStack;
+	if (esd->decoderConfig->upstream) return GF_NOT_SUPPORTED;
+
+	/* decSpecInfo is not null only when reading from an WGT file (local or distant, cached or not) */
+	switch (esd->decoderConfig->objectTypeIndication) {
+	case GPAC_OTI_PRIVATE_SCENE_WGT:
+	default:
+		if (!esd->decoderConfig->decoderSpecificInfo) return GF_NON_COMPLIANT_BITSTREAM;
+		bs = gf_bs_new(esd->decoderConfig->decoderSpecificInfo->data, esd->decoderConfig->decoderSpecificInfo->dataLength, GF_BITSTREAM_READ);
+		wgtload->file_size = gf_bs_read_u32(bs);
+		gf_bs_del(bs);
+		wgtload->file_name =  (char *) gf_malloc(sizeof(char)*(1 + esd->decoderConfig->decoderSpecificInfo->dataLength - sizeof(u32)) );
+		memcpy(wgtload->file_name, esd->decoderConfig->decoderSpecificInfo->data + sizeof(u32), esd->decoderConfig->decoderSpecificInfo->dataLength - sizeof(u32) );
+		wgtload->file_name[esd->decoderConfig->decoderSpecificInfo->dataLength - sizeof(u32) ] = 0;
+		break;
+	}
+	wgtload->oti = esd->decoderConfig->objectTypeIndication;
+	return GF_OK;
+}
+
+static GF_Err WGT_DetachStream(GF_BaseDecoder *plug, u16 ES_ID)
+{
+	WgtLoad *wgtload = (WgtLoad *)plug->privateStack;
+	if (wgtload->file_name) gf_free(wgtload->file_name);
+	wgtload->file_name = NULL;
+	return GF_OK;
+}
+
+const char *WGT_GetName(struct _basedecoder *plug)
+{
+	return "GPAC W3C Widget Loader";
+}
+
+Bool WGT_CanHandleStream(GF_BaseDecoder *ifce, u32 StreamType, u32 ObjectType, char *decSpecInfo, u32 decSpecInfoSize, u32 PL)
+{
+	if (StreamType==GF_STREAM_PRIVATE_SCENE) {
+		if (ObjectType==GPAC_OTI_PRIVATE_SCENE_WGT) return 1;
+		return 0;
+	} 
+	return 0;
+}
+
+static GF_Err WGT_GetCapabilities(GF_BaseDecoder *plug, GF_CodecCapability *cap)
+{
+	cap->cap.valueInt = 0;
+	if (cap->CapCode==GF_CODEC_PADDING_BYTES) {
+		/* Adding one byte of padding for \r\n problems*/
+		cap->cap.valueInt = 1;
+		return GF_OK;
+	}
+	return GF_NOT_SUPPORTED;
+}
+
+static GF_Err WGT_SetCapabilities(GF_BaseDecoder *plug, const GF_CodecCapability capability)
+{
+	return GF_OK;
+}
+
+/*interface create*/
+GF_BaseInterface *LoadWidgetReader()
+{
+	WgtLoad *wgtload;
+	GF_SceneDecoder *sdec;
+	
+	GF_SAFEALLOC(sdec, GF_SceneDecoder)
+	GF_REGISTER_MODULE_INTERFACE(sdec, GF_SCENE_DECODER_INTERFACE, "GPAC W3C Widget Loader", "gpac distribution");
+
+	GF_SAFEALLOC(wgtload, WgtLoad);
+	sdec->privateStack = wgtload;
+	sdec->AttachStream = WGT_AttachStream;
+	sdec->CanHandleStream = WGT_CanHandleStream;
+	sdec->DetachStream = WGT_DetachStream;
+	sdec->AttachScene = WGT_AttachScene;
+	sdec->ReleaseScene = WGT_ReleaseScene;
+	sdec->ProcessData = WGT_ProcessData;
+	sdec->GetName = WGT_GetName;
+	sdec->SetCapabilities = WGT_SetCapabilities;
+	sdec->GetCapabilities = WGT_GetCapabilities;
+	return (GF_BaseInterface *)sdec;
+}
+
+
+/*interface destroy*/
+void ShutdownWidgetReader(GF_BaseInterface *ifce)
+{
+	GF_SceneDecoder *sdec = (GF_SceneDecoder *)ifce;
+	WgtLoad *wgtload = (WgtLoad *) sdec->privateStack;
+
+	gf_free(wgtload);
+	gf_free(sdec);
+}
+
+#endif	/*GPAC_HAS_SPIDERMONKEY*/
---
+++ modules/widgetman/widget.c
@@ -0,0 +1,412 @@
+//This software module was originally developed by TelecomParisTech in the
+//course of the development of MPEG-U Widgets (ISO/IEC 23007-1) standard.
+//
+//This software module is an implementation of a part of one or 
+//more MPEG-U Widgets (ISO/IEC 23007-1) tools as specified by the MPEG-U Widgets
+//(ISO/IEC 23007-1) standard. ISO/IEC gives users of the MPEG-U Widgets
+//(ISO/IEC 23007-1) free license to this software module or modifications
+//thereof for use in hardware or software products claiming conformance to
+//the MPEG-U Widgets (ISO/IEC 23007-1). Those intending to use this software
+//module in hardware or software products are advised that its use may
+//infringe existing patents.
+//The original developer of this software module and his/her company, the
+//subsequent editors and their companies, and ISO/IEC have no liability 
+//for use of this software module or modifications thereof in an implementation. 
+//Copyright is not released for non MPEG-U Widgets (ISO/IEC 23007-1) conforming 
+//products. 
+//Telecom ParisTech retains full right to use the code for his/her own purpose, 
+//assign or donate the code to a third party and to inhibit third parties from 
+//using the code for non MPEG-U Widgets (ISO/IEC 23007-1) conforming products. 
+//
+//This copyright notice must be included in all copies or derivative works.
+//
+//Copyright (c) 2009.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////
+//
+//	Authors:	
+//					Jean Le Feuvre, Telecom ParisTech
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+
+#include "widgetman.h"
+
+#ifdef GPAC_HAS_SPIDERMONKEY
+
+JSBool widget_has_feature(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	const char *feat_name;
+	GF_WidgetInstance *wid = (GF_WidgetInstance *)JS_GetPrivate(c, obj);
+	if (!wid) return JS_FALSE;
+
+	if (!argc || !JSVAL_IS_STRING(argv[0])) return JS_TRUE;
+
+	feat_name = JS_GetStringBytes(JSVAL_TO_STRING( argv[0] ));
+	*rval = BOOLEAN_TO_JSVAL( JS_FALSE );
+
+	if (!strcmp(feat_name, "urn:mpeg:systems:mpeg-u:2009")) *rval = BOOLEAN_TO_JSVAL( JS_TRUE );
+
+	return JS_TRUE;
+}
+
+JSBool widget_open_url(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	GF_Event evt;
+	GF_WidgetInstance *wid = (GF_WidgetInstance *)JS_GetPrivate(c, obj);
+	if (!wid || !argc || !JSVAL_IS_STRING(argv[0])) return JS_TRUE;
+
+	memset(&evt, 0, sizeof(GF_Event));
+	evt.type = GF_EVENT_NAVIGATE;
+	evt.navigate.to_url = JS_GetStringBytes(JSVAL_TO_STRING( argv[0] ));
+	return gf_term_send_event(wid->widget->wm->term, &evt);
+	return JS_TRUE;
+}
+
+JSBool widget_get_attention(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	jsval fval;
+	GF_WidgetInstance *wid = (GF_WidgetInstance *)JS_GetPrivate(c, obj);
+	if (!wid) return JS_TRUE;
+
+	if ((JS_LookupProperty(c, wid->widget->wm->obj, "getAttention", &fval)==JS_TRUE) && JSVAL_IS_OBJECT(fval)) {
+		jsval args[1];
+		args[0] = OBJECT_TO_JSVAL(wid->obj);
+		JS_CallFunctionValue(c, wid->widget->wm->obj, fval, 1, args, rval);
+	}
+	return JS_TRUE;
+}
+
+JSBool widget_show_notification(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	jsval fval;
+	GF_WidgetInstance *wid = (GF_WidgetInstance *)JS_GetPrivate(c, obj);
+	if (!wid) return JS_TRUE;
+
+	if ((JS_LookupProperty(c, wid->widget->wm->obj, "showNotification", &fval)==JS_TRUE) && JSVAL_IS_OBJECT(fval)) {
+		jsval *vars;
+		u32 i;
+		vars = gf_malloc(sizeof(jsval)*(argc+1));
+		vars[0] = OBJECT_TO_JSVAL(wid->obj);
+		for (i=0; i<argc; i++) 
+			vars[i+1] = argv[i];
+
+		JS_CallFunctionValue(c, wid->widget->wm->obj, fval, argc+1, vars, rval);
+	}
+	return JS_TRUE;
+}
+
+
+static JSBool widget_call_message_reply_callback(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	JSObject *list;
+	jsval *vals, fval;
+	u32 i, count;
+	GF_WidgetMessage *msg = JS_GetPrivate(c, obj);
+	if (!msg || !argc || !JSVAL_IS_OBJECT(argv[0]) ) return JS_FALSE;
+
+	if ((JS_LookupProperty(c, obj, "replyCallback", &fval)==JS_TRUE) && JSVAL_IS_OBJECT(fval)) {
+		list = JSVAL_TO_OBJECT(argv[0]);
+		JS_GetArrayLength(c, list, (jsuint*) &count);
+		vals = gf_malloc(sizeof(jsval)*(count+1));
+		vals[0] = OBJECT_TO_JSVAL(obj);
+		for (i=0; i<count; i++) {
+			JS_GetElement(c, list, (jsint) i, &vals[i+1]);
+		}
+		JS_CallFunctionValue(c, obj, fval, count, vals, rval);
+		gf_free(vals);
+	}
+	return JS_TRUE;
+}
+
+static JSBool widget_message_handler_factory(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	char *msg_name;
+	u32 i, count;
+	GF_WidgetInstance *wid = NULL;
+	GF_WidgetInterfaceInstance *bifce = (GF_WidgetInterfaceInstance *)JS_GetPrivate(c, obj);
+	if (!bifce) return JS_FALSE;
+
+	if (!argc) return JS_FALSE;
+	if (!JSVAL_IS_STRING(argv[0])) return JS_FALSE;
+
+	msg_name = JS_GetStringBytes(JSVAL_TO_STRING(argv[0]));
+	if (!msg_name ) return JS_FALSE;
+
+	*rval = JSVAL_NULL;
+	count = gf_list_count(bifce->ifce->messages);
+	for (i=0; i<count; i++) {
+		GF_WidgetMessage *msg = gf_list_get(bifce->ifce->messages, i);
+		if (!strcmp(msg->name, msg_name)) {
+			JSObject *an_obj = JS_NewObject(c, &bifce->wid->widget->wm->widgetAnyClass, 0, 0);
+			JS_SetPrivate(c, an_obj, msg);
+			JS_DefineProperty(c, an_obj, "msgName", STRING_TO_JSVAL( JS_NewStringCopyZ(c, msg->name) ), 0, 0, JSPROP_READONLY | JSPROP_PERMANENT);
+			JS_DefineProperty(c, an_obj, "interfaceHandler", OBJECT_TO_JSVAL( obj ), 0, 0, JSPROP_READONLY | JSPROP_PERMANENT);
+			JS_DefineFunction(c, an_obj, "onInvokeReply", widget_call_message_reply_callback, 1, 0);
+
+			if ((argc==2) && JSVAL_IS_OBJECT(argv[1]) && !JSVAL_IS_NULL(argv[1]))
+				JS_DefineProperty(c, an_obj, "replyCallback", argv[1], 0, 0, JSPROP_READONLY | JSPROP_PERMANENT);
+			
+			*rval = OBJECT_TO_JSVAL(an_obj);
+		}
+	}
+	return JS_TRUE;
+}
+static JSBool widget_invoke_message(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	jsval oval;
+	GF_WidgetMessage *msg = NULL;
+	GF_WidgetInterfaceInstance *bifce = (GF_WidgetInterfaceInstance *)JS_GetPrivate(c, obj);
+	if (!bifce) return JS_FALSE;
+
+	*rval = JSVAL_NULL;
+
+	if (!JSVAL_IS_OBJECT(argv[0])) return JS_FALSE;
+	if (JSVAL_IS_NULL(argv[0])) return JS_FALSE;
+	msg = (GF_WidgetMessage *)JS_GetPrivate(c, JSVAL_TO_OBJECT(argv[0]) );
+	if (!msg) return JS_FALSE;
+
+	/*look for JS Callback "invoke" in the widget manager script*/
+	if (JS_LookupProperty(c, bifce->ifce->obj, "invoke", &oval)==JS_TRUE) {
+		if (JSVAL_IS_OBJECT(oval)) {
+			JS_CallFunctionValue(bifce->wid->widget->wm->ctx, bifce->ifce->obj, oval, argc, argv, rval);
+		}
+	}
+
+	return JS_TRUE;
+}
+
+static JSBool widget_invoke_message_reply(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	jsval oval;
+	GF_WidgetMessage *msg = NULL;
+	GF_WidgetInterfaceInstance *bifce = (GF_WidgetInterfaceInstance *)JS_GetPrivate(c, obj);
+	if (!bifce) return JS_FALSE;
+
+	*rval = JSVAL_NULL;
+
+	if (!JSVAL_IS_OBJECT(argv[0])) return JS_FALSE;
+	if (JSVAL_IS_NULL(argv[0])) return JS_FALSE;
+	msg = (GF_WidgetMessage *)JS_GetPrivate(c, JSVAL_TO_OBJECT(argv[0]) );
+	if (!msg) return JS_FALSE;
+
+	/*look for JS Callback "invokeReply" in the widget manager script*/
+	if (JS_LookupProperty(c, bifce->ifce->obj, "invokeReply", &oval)==JS_TRUE) {
+		if (JSVAL_IS_OBJECT(oval)) {
+			JS_CallFunctionValue(bifce->wid->widget->wm->ctx, bifce->ifce->obj, oval, argc, argv, rval);
+		}
+	}
+	return JS_TRUE;
+}
+
+static void widget_interface_js_bind(JSContext *c, GF_WidgetInterfaceInstance *ifce)
+{
+	if (!ifce->obj) {
+		ifce->obj = JS_NewObject(c, &ifce->wid->widget->wm->widgetAnyClass, 0, 0);
+		JS_SetPrivate(c, ifce->obj, ifce);
+		JS_AddRoot(c, &ifce->obj);
+		JS_DefineProperty(c, ifce->obj, "type", STRING_TO_JSVAL( JS_NewStringCopyZ(c, ifce->ifce->type) ), 0, 0, JSPROP_READONLY | JSPROP_PERMANENT);
+		JS_DefineProperty(c, ifce->obj, "bound", STRING_TO_JSVAL( JS_NewStringCopyZ(c, ifce->hostname) ), 0, 0, JSPROP_READONLY | JSPROP_PERMANENT);
+		JS_DefineFunction(c, ifce->obj, "invoke", widget_invoke_message, 1, 0);
+		JS_DefineFunction(c, ifce->obj, "msgHandlerFactory", widget_message_handler_factory, 1, 0);
+		JS_DefineFunction(c, ifce->obj, "invokeReply", widget_invoke_message_reply, 1, 0);
+	}
+}
+
+static JSBool widget_get_interfaces(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	u32 i, count;
+	char *ifce_name;
+	JSObject *list;
+	jsuint idx;
+	jsval v;
+	GF_WidgetInstance *wid = (GF_WidgetInstance *)JS_GetPrivate(c, obj);
+	if (!wid) return JS_FALSE;
+
+	if (!argc || !JSVAL_IS_STRING(argv[0])) return JS_TRUE;
+	ifce_name = JS_GetStringBytes(JSVAL_TO_STRING(argv[0]));
+
+	list = JS_NewArrayObject(c, 0, 0);
+
+	count = gf_list_count(wid->bound_ifces);
+	for (i=0; i<count; i++) {
+		GF_WidgetInterfaceInstance *ifce = gf_list_get(wid->bound_ifces, i);
+		if (strcmp(ifce->ifce->type, ifce_name)) continue;
+
+		widget_interface_js_bind(c, ifce);
+
+		JS_GetArrayLength(c, list, &idx);
+		v = OBJECT_TO_JSVAL(ifce->obj);
+		JS_SetElement(c, list, idx, &v);
+	}
+	*rval = OBJECT_TO_JSVAL(list);
+	return JS_TRUE;
+}
+
+static JSBool widget_activate_component(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval, Bool is_deactivate)
+{
+	u32 i, count;
+	char *comp_id;
+	GF_WidgetInstance *wid = (GF_WidgetInstance *)JS_GetPrivate(c, obj);
+	if (!wid) return JS_FALSE;
+
+	if (!argc || !JSVAL_IS_STRING(argv[0])) return JS_TRUE;
+	comp_id = JS_GetStringBytes(JSVAL_TO_STRING(argv[0]));
+
+	count = gf_list_count(wid->widget->main->components);
+	for (i=0; i<count; i++) {
+		GF_WidgetComponent *comp = gf_list_get(wid->widget->main->components, i);
+		if (!comp->id  || strcmp(comp->id, comp_id)) continue;
+		
+		wm_activate_component(c, wid, comp, is_deactivate, NULL);
+		return JS_TRUE;
+	}
+	return JS_TRUE;
+}
+
+static JSBool widget_activate_widget(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	return widget_activate_component(c, obj, argc, argv, rval, 0);
+}
+
+static JSBool widget_deactivate_widget(JSContext *c, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+{
+	return widget_activate_component(c, obj, argc, argv, rval, 1);
+}
+
+void widget_on_interface_bind(GF_WidgetInterfaceInstance *ifce, Bool unbind)
+{
+	jsval funval, rval, argv[1];
+
+	const char *fun_name = unbind ? "onInterfaceUnbind" : "onInterfaceBind";
+	if (!ifce || !ifce->wid || !ifce->wid->scene_context) return;
+
+	/*look for JS Callback "invoke" in the widget manager script*/
+	if (JS_LookupProperty(ifce->wid->scene_context, ifce->wid->scene_obj, fun_name, &funval)!=JS_TRUE) 
+		return;
+	if (!JSVAL_IS_OBJECT(funval)) return;
+
+	widget_interface_js_bind(ifce->wid->widget->wm->ctx, ifce);
+	argv[0] = OBJECT_TO_JSVAL(ifce->obj);
+	JS_CallFunctionValue(ifce->wid->widget->wm->ctx, ifce->ifce->obj, funval, 1, argv, &rval);
+
+}
+
+JSBool widget_getProperty(JSContext *c, JSObject *obj, jsval id, jsval *rval)
+{
+	const char *opt;
+	char *prop_name;
+	GF_WidgetInstance *wid = (GF_WidgetInstance *)JS_GetPrivate(c, obj);
+	if (!wid) return JS_FALSE;
+
+	if (!JSVAL_IS_STRING(id)) return JS_TRUE;
+	prop_name = JS_GetStringBytes(JSVAL_TO_STRING(id));
+	if (!prop_name) return JS_FALSE;
+
+	if (!strcmp(prop_name, "viewMode")) {
+		*rval = STRING_TO_JSVAL( JS_NewStringCopyZ(c, "floating") );
+	}
+	else if (!strcmp(prop_name, "locale")) {
+		opt = gf_cfg_get_key(wid->widget->wm->term->user->config, "Systems", "Language2CC");
+		if (!opt) opt = "und";
+		*rval = STRING_TO_JSVAL( JS_NewStringCopyZ(c, opt) );
+	}
+	else if (!strcmp(prop_name, "identifier")) {
+		if (wid->widget->identifier) *rval = STRING_TO_JSVAL( JS_NewStringCopyZ(c, wid->widget->identifier) );
+	}
+	else if (!strcmp(prop_name, "authorName")) {
+		if (wid->widget->authorName) *rval = STRING_TO_JSVAL( JS_NewStringCopyZ(c, wid->widget->authorName) );
+	}
+	else if (!strcmp(prop_name, "authorEmail")) {
+		if (wid->widget->authorEmail) *rval = STRING_TO_JSVAL( JS_NewStringCopyZ(c, wid->widget->authorEmail) );
+	}
+	else if (!strcmp(prop_name, "authorHref")) {
+		if (wid->widget->authorHref) *rval = STRING_TO_JSVAL( JS_NewStringCopyZ(c, wid->widget->authorHref) );
+	}
+	else if (!strcmp(prop_name, "name")) {
+		if (wid->widget->name) *rval = STRING_TO_JSVAL( JS_NewStringCopyZ(c, wid->widget->name) );
+	}
+	else if (!strcmp(prop_name, "version")) {
+		if (wid->widget->version) *rval = STRING_TO_JSVAL( JS_NewStringCopyZ(c, wid->widget->version) );
+	}
+	else if (!strcmp(prop_name, "description")) {
+		if (wid->widget->description) *rval = STRING_TO_JSVAL( JS_NewStringCopyZ(c, wid->widget->description) );
+	}
+	else if (!strcmp(prop_name, "width")) {
+		opt = gf_cfg_get_key(wid->widget->wm->term->user->config, wid->secname, "width");
+		*rval = INT_TO_JSVAL( JS_NewDouble(c, opt ? atoi(opt) : 0) );
+	}
+	else if (!strcmp(prop_name, "height")) {
+		opt = gf_cfg_get_key(wid->widget->wm->term->user->config, wid->secname, "height");
+		*rval = INT_TO_JSVAL( JS_NewDouble(c, opt ? atoi(opt) : 0) );
+	}
+	else if (!strcmp(prop_name, "preferences")) {
+	}
+	return JS_TRUE;
+}
+
+JSBool widget_setProperty(JSContext *c, JSObject *obj, jsval id, jsval *vp)
+{
+	return JS_TRUE;
+}
+
+void widget_load(GF_WidgetManager *wm, GF_SceneGraph *scene, JSContext *c, JSObject *global, Bool unload)
+{
+	u32 i, count;
+	GF_WidgetInstance *wi;
+
+	/*Is this scenegraph a widget or not ? To find out, browse all widget instances*/
+	i=0;
+	while ((wi = gf_list_enum(wm->widget_instances, &i))) {
+		if (!wi->scene || (wi->scene != scene)) continue;
+		break;
+	}
+	if (!wi) return;
+
+	/*OK we found our widget*/
+
+	if (unload) {
+		/*detach all bound interfaces from this script*/
+		count = gf_list_count(wi->bound_ifces);
+		for (i=0; i<count; i++) {
+			GF_WidgetInterfaceInstance *ifce = gf_list_get(wi->bound_ifces, i);
+			if (ifce->obj) {
+				JS_SetPrivate(c, ifce->obj, NULL);
+				JS_RemoveRoot(c, &ifce->obj);
+				ifce->obj = NULL;
+			}
+		}
+		return;
+	} else {
+
+		JSPropertySpec widgetClassProps[] = {
+			{0, 0, 0, 0, 0}
+		};
+		JSFunctionSpec widgetClassFuncs[] = {
+			/*W3C*/
+			{"has_feature", widget_has_feature, 1, 0, 0},
+			{"openURL", widget_open_url, 1, 0, 0},
+			{"getAttention", widget_get_attention, 0, 0, 0},
+			{"showNotification", widget_show_notification, 0, 0, 0},
+			/*MPEG*/
+			{"getInterfaceHandlersByType", widget_get_interfaces, 1, 0, 0},
+			{"activateComponentWidget", widget_activate_widget, 1, 0, 0},
+			{"deactivateComponentWidget", widget_deactivate_widget, 1, 0, 0},
+			{0, 0, 0, 0, 0}
+		};
+
+		JS_InitClass(c, global, 0, &wm->widgetClass, 0, 0,widgetClassProps, widgetClassFuncs, 0, 0);
+
+		
+		wi->scene_obj = JS_DefineObject(c, global, "widget", &wm->widgetClass, 0, 0);
+		JS_AliasProperty(c, global, "widget", "MPEGWidget");
+		JS_SetPrivate(c, wi->scene_obj, wi);
+		/*and remember the script*/
+		wi->scene_context = c;
+		wi->scene_global = global;
+	}
+}
+
+#endif
