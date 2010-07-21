ffd13304c679019359a3e526c4e38a62c0f26ce2
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2010-07-21 17:00:25 +0000

    Fixed bug when dumping to AVI a file with Layout
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@1895 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/compositor/mpeg4_layout.c
+++ src/compositor/mpeg4_layout.c
@@ -596,7 +596,7 @@ static void layout_scroll(GF_TraverseState *tr_state, LayoutStack *st, M_Layout 
 	}
 	/*draw next frame*/
 	if (!stop_anim) {
-		gf_sc_invalidate(tr_state->visual->compositor, NULL);
+		tr_state->visual->compositor->force_next_frame_redraw = 1;
 		return;
 	}
 
