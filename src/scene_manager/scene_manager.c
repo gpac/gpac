b486ebccade887c4fd37d307a3d92f4cda21c7c0
Cyril Concolato <cyril.concolato@telecom-paristech.fr>
2010-09-15 20:41:02 +0000

    fix to avoid scanning the whole list of updates when adding a new one
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@2038 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/scene_manager/scene_manager.c
+++ src/scene_manager/scene_manager.c
@@ -172,25 +172,29 @@ GF_AUContext *gf_sm_stream_au_new(GF_StreamContext *stream, u64 timing, Double t
 {
 	u32 i;
 	GF_AUContext *tmp;
+    u64 tmp_timing;
 
-	/*look for existing AU*/
-	i=0;
-	while ((tmp = (GF_AUContext *)gf_list_enum(stream->AUs, &i))) {
-		if (timing && (tmp->timing==timing)) return tmp;
-		else if (time_sec && (tmp->timing_sec == time_sec)) return tmp;
-		else if (!time_sec && !timing && !tmp->timing && !tmp->timing_sec) return tmp;
-		/*insert AU*/
-		else if ((time_sec && time_sec<tmp->timing_sec) || (timing && timing<tmp->timing)) {
-			GF_SAFEALLOC(tmp, GF_AUContext);
-			tmp->commands = gf_list_new();
-			if (isRap) tmp->flags = GF_SM_AU_RAP;
-			tmp->timing = timing;
-			tmp->timing_sec = time_sec;
-			tmp->owner = stream;
-			gf_list_insert(stream->AUs, tmp, i-1);
-			return tmp;
-		}
-	}
+    tmp_timing = timing ? timing : time_sec*1000;
+    if (stream->last_au_time > tmp_timing) {
+	    /*look for existing AU*/
+	    i=0;
+	    while ((tmp = (GF_AUContext *)gf_list_enum(stream->AUs, &i))) {
+		    if (timing && (tmp->timing==timing)) return tmp;
+		    else if (time_sec && (tmp->timing_sec == time_sec)) return tmp;
+		    else if (!time_sec && !timing && !tmp->timing && !tmp->timing_sec) return tmp;
+		    /*insert AU*/
+		    else if ((time_sec && time_sec<tmp->timing_sec) || (timing && timing<tmp->timing)) {
+			    GF_SAFEALLOC(tmp, GF_AUContext);
+			    tmp->commands = gf_list_new();
+			    if (isRap) tmp->flags = GF_SM_AU_RAP;
+			    tmp->timing = timing;
+			    tmp->timing_sec = time_sec;
+			    tmp->owner = stream;
+			    gf_list_insert(stream->AUs, tmp, i-1);
+			    return tmp;
+		    }
+	    }
+    } 
 	GF_SAFEALLOC(tmp, GF_AUContext);
 	tmp->commands = gf_list_new();
 	if (isRap) tmp->flags = GF_SM_AU_RAP;
@@ -199,6 +203,7 @@ GF_AUContext *gf_sm_stream_au_new(GF_StreamContext *stream, u64 timing, Double t
 	tmp->owner = stream;
 	if (stream->disable_aggregation) tmp->flags |= GF_SM_AU_NOT_AGGREGATED;
 	gf_list_add(stream->AUs, tmp);
+    stream->last_au_time = tmp_timing;
 	return tmp;
 }
 
