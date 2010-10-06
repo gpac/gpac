0e347b441eb3798d04cc199d9d2b3182dd69ef74
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2010-10-06 13:51:24 +0000

    Fixed widget packaging to remove .svn directory
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@2097 63c20433-aa62-49bd-875c-5a186b69a8fb

--- applications/mp4box/fileimport.c
+++ applications/mp4box/fileimport.c
@@ -1843,7 +1843,7 @@ static Bool wgt_enum_files(void *cbck, char *file_name, char *file_path)
 }
 static Bool wgt_enum_dir(void *cbck, char *file_name, char *file_path)
 {
-	if (!stricmp(file_name, "cvs") || !stricmp(file_name, "svn")) return 0;
+	if (!stricmp(file_name, "cvs") || !stricmp(file_name, ".svn")) return 0;
 	gf_enum_directory(file_path, 0, wgt_enum_files, cbck, NULL);
 	return gf_enum_directory(file_path, 1, wgt_enum_dir, cbck, NULL);
 }
