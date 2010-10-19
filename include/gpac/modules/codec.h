37ccabb606e31673654574abcbe1a4a954bd8bfd
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2010-10-19 10:54:11 +0000

    Added quality switching capability
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@2140 63c20433-aa62-49bd-875c-5a186b69a8fb

--- include/gpac/modules/codec.h
+++ include/gpac/modules/codec.h
@@ -119,6 +119,9 @@ enum
 	after the last AU). Otherwise the decoder will be stopped and ask to remove any extra scene being displayed*/
 	GF_CODEC_MEDIA_NOT_OVER,
 
+	/*switches up or down media quality for scalable coding*/
+	GF_CODEC_MEDIA_SWITCH_QUALITY,
+
 	/*special cap indicating the codec should abort processing as soon as possible because it is about to be destroyed*/
 	GF_CODEC_ABORT
 };
