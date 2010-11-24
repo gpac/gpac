f066f6294b18126965a4ea53a888724345f10603
Pierre Souchay <pierre@souchay.net>
2010-11-24 22:10:49 +0000

    Removed C99 compilation warnings and missing include on POSIX systems
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@2304 63c20433-aa62-49bd-875c-5a186b69a8fb

--- modules/widgetman/unzip.c
+++ modules/widgetman/unzip.c
@@ -21,6 +21,7 @@
 #else
 # include <unistd.h>
 # include <utime.h>
+# include <sys/stat.h>
 #endif
 
 #endif
@@ -374,11 +375,12 @@ static int unzlocal_GetCurrentFileInfoInternal (file,
 
 
     /* we check the magic */
-    if (err==UNZ_OK)
+    if (err==UNZ_OK){
         if (unzlocal_getLong(&s->z_filefunc, s->filestream,&uMagic) != UNZ_OK)
             err=UNZ_ERRNO;
         else if (uMagic!=0x02014b50)
             err=UNZ_BADZIPFILE;
+    }
 
     if (unzlocal_getShort(&s->z_filefunc, s->filestream,&file_info.version) != UNZ_OK)
         err=UNZ_ERRNO;
@@ -454,11 +456,12 @@ static int unzlocal_GetCurrentFileInfoInternal (file,
         else
             uSizeRead = extraFieldBufferSize;
 
-        if (lSeek!=0)
+        if (lSeek!=0){
             if (ZSEEK(s->z_filefunc, s->filestream,lSeek,ZLIB_FILEFUNC_SEEK_CUR)==0)
                 lSeek=0;
             else
                 err=UNZ_ERRNO;
+	}
         if ((file_info.size_file_extra>0) && (extraFieldBufferSize>0))
             if (ZREAD(s->z_filefunc, s->filestream,extraField,uSizeRead)!=uSizeRead)
                 err=UNZ_ERRNO;
@@ -479,11 +482,12 @@ static int unzlocal_GetCurrentFileInfoInternal (file,
         else
             uSizeRead = commentBufferSize;
 
-        if (lSeek!=0)
+        if (lSeek!=0){
             if (ZSEEK(s->z_filefunc, s->filestream,lSeek,ZLIB_FILEFUNC_SEEK_CUR)==0)
                 lSeek=0;
             else
                 err=UNZ_ERRNO;
+	}
         if ((file_info.size_file_comment>0) && (commentBufferSize>0))
             if (ZREAD(s->z_filefunc, s->filestream,szComment,uSizeRead)!=uSizeRead)
                 err=UNZ_ERRNO;
@@ -910,11 +914,12 @@ int unzlocal_CheckCurrentFileCoherencyHeader (s,piSizeVar,
         return UNZ_ERRNO;
 
 
-    if (err==UNZ_OK)
+    if (err==UNZ_OK){
         if (unzlocal_getLong(&s->z_filefunc, s->filestream,&uMagic) != UNZ_OK)
             err=UNZ_ERRNO;
         else if (uMagic!=0x04034b50)
             err=UNZ_BADZIPFILE;
+    }
 
     if (unzlocal_getShort(&s->z_filefunc, s->filestream,&uData) != UNZ_OK)
         err=UNZ_ERRNO;
@@ -1199,7 +1204,6 @@ int do_extract_currentfile(uf)
     uInt size_buf;
 
     unz_file_info file_info;
-    uLong ratio=0;
     err = unzlocal_GetCurrentFileInfoInternal(uf,&file_info,NULL,filename_inzip,sizeof(filename_inzip),NULL,0,NULL,0);
 
     if (err!=UNZ_OK)
@@ -1311,7 +1315,6 @@ int gf_unzip_archive(const char *zipfilename, const char *dirname)
     uLong i;
     unz_global_info gi;
     int err;
-    FILE* fout=NULL;
 
     unzFile uf=NULL;
 
