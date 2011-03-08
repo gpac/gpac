46cde1ea8e993b74e36b3cc67e85bd63e3a80806
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2011-03-08 10:11:11 +0000

    [bouqueau] update batch installer
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@2758 63c20433-aa62-49bd-875c-5a186b69a8fb

--- generate_installer.bat
+++ generate_installer.bat
@@ -12,7 +12,9 @@ echo Check NSIS is in your PATH
 REM ============================================
 
 set NSIS_EXEC="%PROGRAMFILES%\NSIS\makensis.exe"
-echo   Assuming NSIS at default location %NSIS_EXEC%
+if not exist "%PROGRAMFILES%\NSIS\makensis.exe" echo   NSIS couldn't be found at default location %NSIS_EXEC%
+if not exist "%PROGRAMFILES%\NSIS\makensis.exe" goto Abort
+echo   Found NSIS at default location %NSIS_EXEC%
 
 
 echo:
@@ -20,7 +22,7 @@ REM ============================================
 echo Retrieving version/revision information
 REM ============================================
 if not exist include/gpac/version.h echo   version couldn't be found - check include/gpac/version.h exists
-if not exist include/gpac/version.h goto abort
+if not exist include/gpac/version.h goto Abort
 
 REM execute svnversion and check if the result if found within version.h
 for /f "delims=" %%i in ('svnversion.exe') do Set VarRevisionSVN=%%i
@@ -30,9 +32,11 @@ for /f "delims=" %%i in ('echo %VarRevisionSVN% ^| findstr /i /r S^"') do goto R
 for /f "delims=" %%i in ('echo %VarRevisionSVN% ^| findstr /i /r P^"') do goto RevisionAbort
 for /f "delims=" %%i in ('echo %VarRevisionSVN% ^| findstr /i /r :^"') do goto RevisionAbort
 for /f "delims=" %%i in ('type include\gpac\version.h ^| findstr /i /r "%VarRevisionSVN%"') do Set VarRevisionBuild=%%i
-if !"%VarRevisionBuild%"==!"" echo   local revision and last build revision are not congruent - please consider rebuilding before generating an installer
-if !"%VarRevisionBuild%"==!"" goto abort
-echo   version found: %VarRevisionSVN%
+echo VarRevisionBuild = %VarRevisionBuild%
+echo VarRevisionSVN   = %VarRevisionSVN%
+if !"%VarRevisionBuild%"==!"%VarRevisionSVN%" echo   local revision and last build revision are not congruent - please consider rebuilding before generating an installer
+if !"%VarRevisionBuild%"==!"%VarRevisionSVN%" goto Abort
+REM echo   version found: %VarRevisionSVN%
 
 move bin\win32\release\nsis_install\default.out bin\win32\release\nsis_install\default.out_
 echo Name "GPAC Framework ${GPAC_VERSION} revision %VarRevisionSVN%" > bin\win32\release\nsis_install\default.out
@@ -57,14 +61,16 @@ echo:
 REM ============================================
 echo Windows GPAC installer generated - goodbye!
 REM ============================================
-exit/b
+goto LeaveBatch
 
 :RevisionAbort
 echo   SVN revision "%VarRevisionSVN%" is not a simple number, you may have local modification (please check 'svnrevision' flags meaning or execute the NSIS script manually)
 
-:abort
+:Abort
 echo:
 echo  *** ABORTING: CHECK ERROR MESSAGES ABOVE ***
+
+:LeaveBatch
 set VarRevisionSVN=
 set VarRevisionBuild=
 cd /d %OLDDIR%
