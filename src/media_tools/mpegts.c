3d6d733482c969bb51c9b40dbc76d513bddad967
Cyril Concolato <cyril.concolato@telecom-paristech.fr>
2011-03-21 14:56:17 +0000

    Adding MPEG-2 TS CAT detection in demuxer
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@2796 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/media_tools/mpegts.c
+++ src/media_tools/mpegts.c
@@ -1497,6 +1497,68 @@ static void gf_m2ts_process_pat(GF_M2TS_Demuxer *ts, GF_M2TS_SECTION_ES *ses, GF
 	if (ts->on_event) ts->on_event(ts, evt_type, NULL);
 }
 
+static void gf_m2ts_process_cat(GF_M2TS_Demuxer *ts, GF_M2TS_SECTION_ES *ses, GF_List *sections, u8 table_id, u16 ex_table_id, u8 version_number, u8 last_section_number, u32 status)
+{
+	u32 evt_type;
+/*
+	GF_M2TS_Program *prog;
+	GF_M2TS_SECTION_ES *pmt;
+	u32 i, nb_progs;
+	u32 nb_sections;
+	u32 data_size;
+	unsigned char *data;
+	GF_M2TS_Section *section;
+*/
+
+	/*wait for the last section */
+	if (!(status&GF_M2TS_TABLE_END)) return;
+
+	/*skip if already received*/
+	if (status&GF_M2TS_TABLE_REPEAT) {
+		if (ts->on_event) ts->on_event(ts, GF_M2TS_EVT_CAT_REPEAT, NULL);
+		return;
+	}
+/*
+	nb_sections = gf_list_count(sections);
+	if (nb_sections > 1) {
+		GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("CAT on multiple sections not supported\n"));
+	}
+
+	section = (GF_M2TS_Section *)gf_list_get(sections, 0);
+	data = section->data;
+	data_size = section->data_size;
+
+	nb_progs = data_size / 4;
+
+	for (i=0; i<nb_progs; i++) {
+		u16 number, pid;
+		number = (data[0]<<8) | data[1];
+		pid = (data[2]&0x1f)<<8 | data[3];
+		data += 4;
+		if (number==0) {
+			if (!ts->nit) {
+				ts->nit = gf_m2ts_section_filter_new(gf_m2ts_process_nit, 0);
+			}
+		} else {
+			GF_SAFEALLOC(prog, GF_M2TS_Program);
+			prog->streams = gf_list_new();
+			prog->pmt_pid = pid;
+			prog->number = number;
+			gf_list_add(ts->programs, prog);
+			GF_SAFEALLOC(pmt, GF_M2TS_SECTION_ES);
+			pmt->flags = GF_M2TS_ES_IS_SECTION;
+			gf_list_add(prog->streams, pmt);
+			pmt->pid = prog->pmt_pid;
+			pmt->program = prog;
+			ts->ess[pmt->pid] = (GF_M2TS_ES *)pmt;
+			pmt->sec = gf_m2ts_section_filter_new(gf_m2ts_process_pmt, 0);
+		}
+	}
+*/
+
+	evt_type = (status&GF_M2TS_TABLE_UPDATE) ? GF_M2TS_EVT_CAT_UPDATE : GF_M2TS_EVT_CAT_FOUND;
+	if (ts->on_event) ts->on_event(ts, evt_type, NULL);
+}
 
 static GFINLINE u64 gf_m2ts_get_pts(unsigned char *data)
 {
@@ -1790,6 +1852,9 @@ static void gf_m2ts_process_packet(GF_M2TS_Demuxer *ts, unsigned char *data)
 	if (hdr.pid == GF_M2TS_PID_PAT) {
 		gf_m2ts_gather_section(ts, ts->pat, NULL, &hdr, data, payload_size);
 		return;
+	} else if (hdr.pid == GF_M2TS_PID_CAT) {
+		gf_m2ts_gather_section(ts, ts->cat, NULL, &hdr, data, payload_size);
+		return;
 	} else {
 		es = ts->ess[hdr.pid];
 
@@ -2055,6 +2120,7 @@ GF_M2TS_Demuxer *gf_m2ts_demux_new()
 	ts->SDTs = gf_list_new();
 
 	ts->pat = gf_m2ts_section_filter_new(gf_m2ts_process_pat, 0);
+	ts->cat = gf_m2ts_section_filter_new(gf_m2ts_process_cat, 0);
 	ts->sdt = gf_m2ts_section_filter_new(gf_m2ts_process_sdt, 1);
 	ts->nit = gf_m2ts_section_filter_new(gf_m2ts_process_nit, 0);
 	ts->eit = gf_m2ts_section_filter_new(NULL/*gf_m2ts_process_eit*/, 1);
@@ -2071,6 +2137,7 @@ void gf_m2ts_demux_del(GF_M2TS_Demuxer *ts)
 {
 	u32 i;
 	if (ts->pat) gf_m2ts_section_filter_del(ts->pat);
+	if (ts->cat) gf_m2ts_section_filter_del(ts->cat);
 	if (ts->sdt) gf_m2ts_section_filter_del(ts->sdt);
 	if (ts->nit) gf_m2ts_section_filter_del(ts->nit);
 	if (ts->eit) gf_m2ts_section_filter_del(ts->eit);
