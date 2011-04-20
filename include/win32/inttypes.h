cce8824c62c745f1be034f6d7df81fea92f46a4d
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2011-04-20 18:10:01 +0000

    Fixed compilation with new ffmpeg, added mising files under Win32
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@2978 63c20433-aa62-49bd-875c-5a186b69a8fb

---
+++ include/win32/inttypes.h
@@ -0,0 +1,22 @@
+/* 7.8 Format conversion of integer types <inttypes.h> */
+
+#ifndef _INTTYPES_H_
+#define _INTTYPES_H_
+
+
+typedef signed char  int8_t;
+typedef signed short int16_t;
+typedef signed int   int32_t;
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int   uint32_t;
+typedef signed __int64   int64_t;
+typedef unsigned __int64 uint64_t;
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* ndef _INTTYPES_H */
