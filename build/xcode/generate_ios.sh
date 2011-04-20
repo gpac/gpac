9ace57f8391b83cb684167050bc8fe92e6311aaa
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2011-04-20 11:55:03 +0000

    [bouqueau] accelerate ios build + generate tar.gz for the app
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@2961 63c20433-aa62-49bd-875c-5a186b69a8fb

--- build/xcode/generate_ios.sh
+++ build/xcode/generate_ios.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
 
-echo "*** Compile libgpac for Simulator (i386) ***"
-xcodebuild -target libgpac_dynamic -sdk iphonesimulator -configuration Release -project gpac4ios.xcodeproj
+#echo "*** Compile libgpac for Simulator (i386) ***"
+#xcodebuild -target libgpac_dynamic -sdk iphonesimulator -configuration Release -project gpac4ios.xcodeproj
 
 echo "*** Compile libgpac for iOS (arm) ***"
 xcodebuild -target libgpac_dynamic -sdk iphoneos -configuration Release -project gpac4ios.xcodeproj
 ./script_libgpac.sh Release
 #xcodebuild -target libgpac_dynamic -sdk iphoneos -configuration Release -project gpac4ios.xcodeproj
 
-echo "*** Compile modules and osmo4ios for Simulator (i386) ***"
-xcodebuild -alltargets -sdk iphonesimulator -configuration Release -project gpac4ios.xcodeproj
+#echo "*** Compile modules and osmo4ios for Simulator (i386) ***"
+#xcodebuild -alltargets -parallelizeTargets -sdk iphonesimulator -configuration Release -project gpac4ios.xcodeproj
 
 echo "*** Compile modules for iOS (arm) ***"
-xcodebuild -alltargets -sdk iphoneos -configuration Release -project gpac4ios.xcodeproj
+xcodebuild -alltargets -parallelizeTargets -sdk iphoneos -configuration Release -project gpac4ios.xcodeproj
 ./script_modules.sh Release
-#xcodebuild -alltargets -sdk iphoneos -configuration Release -project gpac4ios.xcodeproj
 
-echo "*** Compile osmo4ios for Simulator (arm) ***"
+echo "*** Compile osmo4ios for iOS (arm) ***"
 #link must occur at debug to avoid optimizing that leads to freezes
 xcodebuild -target osmo4ios -sdk iphoneos -configuration Debug -project gpac4ios.xcodeproj
 
 echo "*** Copy the generated libs (arm only) ***"
-rm -f ../../bin/iOS/osmo4ios.app/*
 cp build/Release-iphoneos/*.dylib ../../bin/iOS/osmo4ios.app/
 cp build/Debug-iphoneos/osmo4ios.app/osmo4ios ../../bin/iOS/osmo4ios.app/
 cp build/Debug-iphoneos/osmo4ios.app/PkgInfo ../../bin/iOS/osmo4ios.app/
@@ -31,7 +29,24 @@ echo "*** Test the presence of target files ***"
 if [ `ls ../../bin/iOS/osmo4ios.app/ | wc -l` -ne 22 ]
 then
 	echo "Error: target files number not correct (expected 22, got `ls ../../bin/iOS/osmo4ios.app/ | wc -l`)"
-else
-	echo "*** Extra Libs generation for iOS completed! ***"
+	exit 1
 fi
 
+echo "*** Generate an archive and clean ***"
+cd ../..
+version=`grep '#define GPAC_VERSION ' include/gpac/tools.h | cut -d '"' -f 2`
+rev=`LANG=en_US svn info | grep Revision | tr -d 'Revision: '`
+if [ "$rev" != "" ]
+then
+	full_version="$version-r$rev"
+else
+        #if no revision can be extracted from SVN, use date
+	full_version="$version-$(date +%Y%m%d)"
+fi
+cd bin/iOS
+rm -rf osmo4ios.app/.svn
+tar -czf "osmo4ios-$full_version.tar.gz" osmo4ios.app/
+rm -rf osmo4ios.app
+cd ../../build/xcode/
+
+echo "*** Extra Libs generation for iOS completed (full_version)! ***"
