3799cf13f1b46efb8c888965fe8e31ae7f9f0601
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2011-04-26 17:24:35 +0000

    Quick fix for OSX CPU and Mem info
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3022 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/utils/os_divers.c
+++ src/utils/os_divers.c
@@ -768,7 +768,9 @@ typedef struct _SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION
 } SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION;
 #endif
 
+
 #else
+	
 static u64 last_cpu_u_k_time = 0;
 static u64 last_cpu_idle_time = 0;
 static u64 mem_at_startup = 0;
@@ -1211,7 +1213,131 @@ Bool gf_sys_get_rti(u32 refresh_time_ms, GF_SystemRTInfo *rti, u32 flags)
 	return 1;
 }
 
+	
+#elif defined(GPAC_CONFIG_DARWIN)
 
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <sys/vmmeter.h>
+#include <mach/mach_init.h>
+#include <mach/mach_host.h>
+#include <mach/mach_port.h>
+#include <mach/mach_traps.h>
+#include <mach/task_info.h>
+#include <mach/thread_info.h>
+#include <mach/thread_act.h>
+#include <mach/vm_region.h>
+#include <mach/vm_map.h>
+#include <mach/task.h>
+#include <mach/shared_memory_server.h>
+	
+static u64 total_physical_memory = 0;
+	
+Bool gf_sys_get_rti(u32 refresh_time_ms, GF_SystemRTInfo *rti, u32 flags)
+{
+	size_t length;
+	u32 entry_time, i, percent;
+    int mib[6];
+    int result;
+    int pagesize;
+	u64 process_u_k_time;
+    mach_msg_type_number_t count = HOST_VM_INFO_COUNT;	
+    vm_statistics_data_t vmstat;
+    task_t task;
+    kern_return_t error;
+    thread_array_t thread_table;
+    thread_basic_info_t thi;
+    thread_basic_info_data_t thi_data;
+    unsigned table_size;
+    struct task_basic_info ti;
+	double utime, stime;
+	
+	entry_time = gf_sys_clock();
+	if (last_update_time && (entry_time - last_update_time < refresh_time_ms)) {
+		memcpy(rti, &the_rti, sizeof(GF_SystemRTInfo));
+		return 0;
+	}
+	
+	mib[0] = CTL_HW;
+    mib[1] = HW_PAGESIZE;
+    length = sizeof(pagesize);
+    if (sysctl(mib, 2, &pagesize, &length, NULL, 0) < 0) {
+		return 0;
+    }
+
+	if (host_statistics(mach_host_self(), HOST_VM_INFO, (host_info_t)&vmstat, &count) != KERN_SUCCESS) {
+		return 0;
+    }
+
+	the_rti.physical_memory = (vmstat.wire_count + vmstat.active_count + vmstat.inactive_count + vmstat.free_count)* pagesize;
+	the_rti.physical_memory_avail = vmstat.free_count * pagesize;
+
+	if (!total_physical_memory) {
+		mib[0] = CTL_HW;
+		mib[1] = HW_PHYSMEM;
+		length = sizeof(result);
+		if (sysctl(mib, 2, &result, &length, NULL, 0) >= 0) {
+			total_physical_memory = result;
+		}
+	}
+	
+	
+    error = task_for_pid(mach_task_self(), the_rti.pid, &task);
+ 	if (error) {
+		return 0;
+	}
+	
+	percent = 0;
+	utime = ti.user_time.seconds + ti.user_time.microseconds * 1e-6;
+	stime = ti.system_time.seconds + ti.system_time.microseconds * 1e-6;
+	error = task_threads(task, &thread_table, &table_size);
+	assert(error == KERN_SUCCESS);
+	thi = &thi_data;
+	for (i = 0; i != table_size; ++i) {
+		count = THREAD_BASIC_INFO_COUNT;
+		error = thread_info(thread_table[i], THREAD_BASIC_INFO, (thread_info_t)thi, &count);
+		assert(error == KERN_SUCCESS);
+		if ((thi->flags & TH_FLAGS_IDLE) == 0) {
+			utime += thi->user_time.seconds + thi->user_time.microseconds * 1e-6;
+			stime += thi->system_time.seconds + thi->system_time.microseconds * 1e-6;
+			percent +=  (u32) (100 * (double)thi->cpu_usage / TH_USAGE_SCALE);
+		}
+	}
+	error = vm_deallocate(mach_task_self(), (vm_offset_t)thread_table, table_size * sizeof(thread_array_t));
+    mach_port_deallocate(mach_task_self(), task);
+	
+	process_u_k_time = utime + stime;
+	
+	the_rti.sampling_instant = last_update_time;
+	
+	if (last_update_time) {
+		the_rti.sampling_period_duration = (entry_time - last_update_time);
+		the_rti.process_cpu_time_diff = (process_u_k_time - last_process_k_u_time) * 10;
+		
+		the_rti.total_cpu_time_diff = the_rti.sampling_period_duration;
+		/*TODO*/
+		the_rti.cpu_idle_time = 0;
+		the_rti.total_cpu_usage = 0;
+		if (!the_rti.process_cpu_time_diff) the_rti.process_cpu_time_diff = the_rti.total_cpu_time_diff;
+
+		the_rti.process_cpu_usage = percent;
+	} else {
+		mem_at_startup = the_rti.physical_memory_avail;
+	}
+	the_rti.process_memory = mem_at_startup - the_rti.physical_memory_avail;
+
+#ifdef GPAC_MEMORY_TRACKING
+	the_rti.gpac_memory = gpac_allocated_memory;
+#endif
+	
+	last_process_k_u_time = process_u_k_time;
+	last_cpu_idle_time = 0;
+	last_update_time = entry_time;
+	memcpy(rti, &the_rti, sizeof(GF_SystemRTInfo));
+	return 1;
+}
+
+//linux
 #else
 
 Bool gf_sys_get_rti(u32 refresh_time_ms, GF_SystemRTInfo *rti, u32 flags)
