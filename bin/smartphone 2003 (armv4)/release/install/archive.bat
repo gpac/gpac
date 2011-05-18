f8bb78f1ec9859930be257fae5802fc3c8eb0fa2
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2011-05-18 16:04:05 +0000

    [bouqueau] execute script in the local path
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3155 63c20433-aa62-49bd-875c-5a186b69a8fb

--- bin/smartphone 2003 (armv4)/release/install/archive.bat
+++ bin/smartphone 2003 (armv4)/release/install/archive.bat
@@ -1,2 +1,5 @@
 for /f "delims=" %%a in ('svnversion') do set gpac_revision=%%a
+set OLDDIR=%CD%
+cd /d %~dp0
 zip "GPAC_0.4.6-r%gpac_revision%_WindowsMobile.zip" ../*.dll ../*.exe ../*.plg
+cd /d %OLDDIR%
