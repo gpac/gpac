e2c0bccaada1b2210e3b3abdafc7a03d1448b84e
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2011-05-19 08:00:12 +0000

    [bouqueau] smartphone2003: execute build_installer.bat at the end of the build (needed to set dependencies on the sln to ensure it's executed after everyone else)
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3159 63c20433-aa62-49bd-875c-5a186b69a8fb

--- bin/smartphone 2003 (armv4)/release/install/archive.bat
+++ bin/smartphone 2003 (armv4)/release/install/archive.bat
@@ -1,5 +1,8 @@
-for /f "delims=" %%a in ('svnversion') do set gpac_revision=%%a
 set OLDDIR=%CD%
 cd /d %~dp0
+
+for /f "delims=" %%a in ('svnversion ') do set gpac_revision=%%a
+
 zip "GPAC_0.4.6-r%gpac_revision%_WindowsMobile.zip" ../*.dll ../*.exe ../*.plg
+
 cd /d %OLDDIR%
