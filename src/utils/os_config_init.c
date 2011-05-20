bb5e41943cd5983c444b39698db42353e803746f
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2011-05-20 17:24:15 +0000

    Fixed text formatting
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3182 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/utils/os_config_init.c
+++ src/utils/os_config_init.c
@@ -11,12 +11,12 @@
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation; either version 2, or (at your option)
  *  any later version.
- *   
+ *
  *  GPAC is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
- *   
+ *
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; see the file COPYING.  If not, write to
  *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
@@ -285,7 +285,6 @@ static GF_Config *create_default_config(char *file_path)
 		gf_delete_file(szPath);
 		return NULL;
 	}
-
 	/*Create the config file*/
 	sprintf(szPath, "%s%c%s", file_path, GF_PATH_SEPARATOR, CFG_FILE_NAME);
 	f = gf_f64_open(szPath, "wt");
