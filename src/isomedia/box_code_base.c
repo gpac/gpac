557e7e18344fb1fc7c580692474f3b35dd6fc88c
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2011-05-25 17:14:32 +0000

    Fixed potential crash with udta
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3215 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -6814,6 +6814,7 @@ GF_UserDataMap *udta_getEntry(GF_UserDataBox *ptr, u32 box_type, bin128 *uuid)
 {
 	u32 i;
 	GF_UserDataMap *map;
+	if (ptr == NULL) return;
 	i=0;
 	while ((map = (GF_UserDataMap *)gf_list_enum(ptr->recordList, &i))) {
 		if (map->boxType == box_type) {
