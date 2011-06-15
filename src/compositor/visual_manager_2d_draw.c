9740a869c23d7f9be65e64e517b235032b6fa270
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2011-06-15 13:07:30 +0000

    Fixed video size update for private media decoder
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3297 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/compositor/visual_manager_2d_draw.c
+++ src/compositor/visual_manager_2d_draw.c
@@ -358,16 +358,25 @@ void visual_2d_texture_path_extended(GF_VisualManager *visual, GF_Path *path, GF
 
 	if (txh->flags & GF_SR_TEXTURE_PRIVATE_MEDIA) {
 		GF_Window src, dst;
-
 		visual_2d_fill_path(visual, ctx, NULL, tr_state);
 
+		/*if texture not ready, update the size before computing output rectangles */
+		if (!txh->width || !txh->height) {
+			gf_mo_get_visual_info(txh->stream, &txh->width, &txh->height, &txh->stride, &txh->pixel_ar, &txh->pixelformat);
+			/*in case the node is an MPEG-4 bitmap, force stack rebuild at next frame */
+  			gf_node_dirty_set(ctx->drawable->node, GF_SG_NODE_DIRTY, 1);
+		}
+		
 		if (compositor_texture_rectangles(visual, txh, &ctx->bi->clip, &ctx->bi->unclip, &src, &dst, NULL, NULL)) {
 			if (txh->stream && gf_mo_set_position(txh->stream, &src, &dst)) {
 				gf_mo_get_visual_info(txh->stream, &txh->width, &txh->height, &txh->stride, &txh->pixel_ar, &txh->pixelformat);
 				/*force dirty flag to get called again*/
-				gf_node_dirty_set(ctx->drawable->node, GF_SG_NODE_DIRTY, 0);
+				gf_node_dirty_set(ctx->drawable->node, GF_SG_NODE_DIRTY, 1);
+				gf_sc_next_frame_state(visual->compositor, GF_SC_DRAW_FRAME);
 			}
 		}
+		
+			
 
 		return;
 	}
