0dc39747f7cf3f631ff92e08318670ffb7d93362
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2011-07-25 13:49:50 +0000

    Changed JPEG stub names to fix crash in virtualbox
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3446 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/media_tools/img.c
+++ src/media_tools/img.c
@@ -201,7 +201,7 @@ exit:
 
 #ifdef GPAC_HAS_JPEG
 
-void _nonfatal_error2(j_common_ptr cinfo, int lev) {}
+void gf_jpeg_nonfatal_error2(j_common_ptr cinfo, int lev) {}
 
 /*JPG context while decoding*/
 typedef struct
@@ -219,26 +219,26 @@ typedef struct
 	struct jpeg_decompress_struct cinfo;
 } JPGCtx;
 
-void _output_message (j_common_ptr cinfo){
+static void gf_jpeg_output_message (j_common_ptr cinfo){
 	char buffer[JMSG_LENGTH_MAX];
 	/* Create the message */
 	(*cinfo->err->format_message) (cinfo, buffer);
 	GF_LOG(GF_LOG_ERROR, GF_LOG_CODEC, ("[JPEG OUTPUT MESSAGE]: %s\n", buffer));
 }
 
-void _fatal_error(j_common_ptr cinfo)
+static void gf_jpeg_fatal_error(j_common_ptr cinfo)
 {
 	JPGErr *err = (JPGErr *) cinfo->err;
-	_output_message(cinfo);
+	gf_jpeg_output_message(cinfo);
 	longjmp(err->jmpbuf, 1);
 }
 
-void stub(j_decompress_ptr cinfo) {}
+void gf_jpeg_stub(j_decompress_ptr cinfo) {}
 
 /*a JPEG is always carried in a complete, single MPEG4 AU so no refill*/
-boolean fill_input_buffer(j_decompress_ptr cinfo) { return 0; }
+static boolean gf_jpeg_fill_input_buffer(j_decompress_ptr cinfo) { return 0; }
 
-void skip_input_data(j_decompress_ptr cinfo, long num_bytes)
+static void gf_jpeg_skip_input_data(j_decompress_ptr cinfo, long num_bytes)
 {
 	JPGCtx *jpx = (JPGCtx *) cinfo->src;
 	if (num_bytes > (long) jpx->src.bytes_in_buffer) {
@@ -265,10 +265,11 @@ GF_Err gf_img_jpeg_dec(char *jpg, u32 jpg_size, u32 *width, u32 *height, u32 *pi
 	JPGErr jper;
 	JPGCtx jpx;
 
+#if 1
 	jpx.cinfo.err = jpeg_std_error(&(jper.pub));
-	jper.pub.error_exit = _fatal_error;
-	jper.pub.output_message = _output_message;
-	jper.pub.emit_message = _nonfatal_error2;
+	jper.pub.error_exit = gf_jpeg_fatal_error;
+	jper.pub.output_message = gf_jpeg_output_message;
+	jper.pub.emit_message = gf_jpeg_nonfatal_error2;
 	if (setjmp(jper.jmpbuf)) {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CODEC, ("[JPEGDecode] : Failed to decode\n"));
 		jpeg_destroy_decompress(&jpx.cinfo);
@@ -279,11 +280,11 @@ GF_Err gf_img_jpeg_dec(char *jpg, u32 jpg_size, u32 *width, u32 *height, u32 *pi
 	jpeg_create_decompress(&jpx.cinfo);
 
 	/*prepare IO*/
-	jpx.src.init_source = stub;
-	jpx.src.fill_input_buffer = fill_input_buffer;
-	jpx.src.skip_input_data = skip_input_data;
+	jpx.src.init_source = gf_jpeg_stub;
+	jpx.src.fill_input_buffer = gf_jpeg_fill_input_buffer;
+	jpx.src.skip_input_data = gf_jpeg_skip_input_data;
 	jpx.src.resync_to_restart = jpeg_resync_to_restart;
-	jpx.src.term_source = stub;
+	jpx.src.term_source = gf_jpeg_stub;
 	jpx.skip = 0;
 	jpx.src.next_input_byte = jpg;
         jpx.src.bytes_in_buffer = jpg_size;
@@ -372,6 +373,7 @@ GF_Err gf_img_jpeg_dec(char *jpg, u32 jpg_size, u32 *width, u32 *height, u32 *pi
 	jpeg_destroy_decompress(&jpx.cinfo);
 
 	gf_free(scan_line);
+#endif
 
 	return GF_OK;
 }
