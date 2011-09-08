316c5a1afb6ed4179bd5fc6399d08e96e02ada0a
Cyril Concolato <cyril.concolato@telecom-paristech.fr>
2011-09-08 19:32:09 +0000

    Fix printing of times in MP4Client (milliseconds on 3 digits)
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3558 63c20433-aa62-49bd-875c-5a186b69a8fb

--- applications/mp4client/main.c
+++ applications/mp4client/main.c
@@ -293,7 +293,7 @@ static void PrintTime(u64 time)
 	m = (u32) (time / 1000 / 60 - h*60);
 	s = (u32) (time / 1000 - h*3600 - m*60);
 	ms = (u32) (time - (h*3600 + m*60 + s) * 1000);
-	fprintf(stdout, "%02d:%02d:%02d.%02d", h, m, s, ms);
+	fprintf(stdout, "%02d:%02d:%02d.%03d", h, m, s, ms);
 }
 
 
