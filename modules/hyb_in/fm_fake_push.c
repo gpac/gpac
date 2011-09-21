2e320d3fea8a20ebe9fdee74e9ce973c63d6dc26
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2011-09-21 09:27:59 +0000

    [bouqueau] thread hybrid content insertion
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3587 63c20433-aa62-49bd-875c-5a186b69a8fb

--- modules/hyb_in/fm_fake_push.c
+++ modules/hyb_in/fm_fake_push.c
@@ -32,6 +32,10 @@
 
 /**********************************************************************************************************************/
 
+#define EXT_MEDIA_LOAD_THREADED
+
+/**********************************************************************************************************************/
+
 #define FM_FAKE_PUSH_AUDIO_FREQ 44100
 #define FM_FAKE_PUSH_CHAN_NUM	2
 #define FM_FAKE_PUSH_BITS		16
@@ -47,7 +51,9 @@ typedef struct s_FM_FAKE_PUSH {
 	unsigned char buffer90[FM_FAKE_PUSH_FRAME_LEN]; /*played 90 percent of time*/
 
 	GF_Thread *th;
-
+#ifdef EXT_MEDIA_LOAD_THREADED
+	GF_Thread *media_th;
+#endif
 } FM_FAKE_PUSH;
 FM_FAKE_PUSH FM_FAKE_PUSH_private_data;
 
@@ -146,62 +152,72 @@ static GF_Err GetData(const GF_HYBMEDIA *self, char **out_data_ptr, u32 *out_dat
 
 /**********************************************************************************************************************/
 
+u32 ext_media_load_th(void *par) {
+	GF_HYBMEDIA *self = (GF_HYBMEDIA*) par;
+	/*declare object to terminal*/
+	GF_ObjectDescriptor *od = (GF_ObjectDescriptor*)gf_odf_desc_new(GF_ODF_OD_TAG);
+	od->URLString = gf_strdup("http://gpac.sourceforge.net/screenshots/lion.jpg");
+	od->objectDescriptorID = 0;
+	gf_sleep(2000); //TODO: remove the sleep
+	gf_term_add_media(self->owner, (GF_Descriptor*)od, 0);
+	return 0;
+}
+
 static u32 audio_gen_th(void *par) 
 {
 	GF_Err e;
 	char *data;
 	u32 data_size, init_time;
-	s32 time_to_wait = 0;
+	volatile s32 time_to_wait = 0;
 	GF_SLHeader slh;
 	GF_HYBMEDIA *self = (GF_HYBMEDIA*) par;
 	FM_FAKE_PUSH *fm_fake_push = (FM_FAKE_PUSH*)self->private_data;
 	memset(&slh, 0, sizeof(GF_SLHeader));
 
+	{
+		/*for hybrid scenarios: add an external media*/
+		if (1) {
+#ifdef EXT_MEDIA_LOAD_THREADED
+			GF_Thread **th = &((FM_FAKE_PUSH*)self->private_data)->media_th;
+			assert(*th == NULL);	//once at a time
+			*th = gf_th_new("HYB-FM fake external media load thread");
+			gf_th_run(*th, ext_media_load_th, par);
+#else
+			ext_media_load_th(par);
+#endif
+		}
+
+		if (1) {
+			time_t now;
+			struct tm *now_tm;
+			time(&now);
+			now_tm = gmtime(&now);
+			{
+				GF_NetworkCommand com;
+				memset(&com, 0, sizeof(com));
+				com.command_type = GF_NET_CHAN_MAP_TIME;
+
+				com.map_time.media_time = now_tm->tm_hour*3600+now_tm->tm_min*60+now_tm->tm_sec;
+				com.map_time.timestamp = slh.compositionTimeStamp;
+				com.map_time.reset_buffers = 0;
+				com.base.on_channel = self->channel;
+				gf_term_on_command(self->owner, &com, GF_OK);
+				GF_LOG(GF_LOG_INFO, GF_LOG_MEDIA, ("[HYB In] Mapping WC  Time %04d/%02d/%02d %02d:%02d:%02d and Hyb time "LLD"\n",
+					(now_tm->tm_year + 1900), (now_tm->tm_mon + 1), now_tm->tm_mday, now_tm->tm_hour, now_tm->tm_min, now_tm->tm_sec,
+					com.map_time.timestamp));
+			}
+		}
+
+		/*initialize clock*/
+		init_time = gf_sys_clock();
+	}
+
 	while (self->state >= 0) /*pause or play*/
 	{
 		if (self->state == HYB_STATE_PAUSE) {
 			gf_sleep(10);
-			init_time = (u32)(gf_sys_clock() - ((u64)slh.compositionTimeStamp*1000)/FM_FAKE_PUSH_AUDIO_FREQ);	/*pause: won't wait at resume*/
+			init_time = (u32)(gf_sys_clock() - ((u64)slh.compositionTimeStamp*1000)/FM_FAKE_PUSH_AUDIO_FREQ - 50/*50ms delay*/);	/*pause: won't wait at resume*/
 			continue;
-		}
-
-		if (!time_to_wait)
-		{
-			/*TODO: remove the sleep*/
-			gf_sleep(1000);
-
-			/*for hybrid scenarios: add an external media*/
-			{
-				/*declare object to terminal*/
-				GF_ObjectDescriptor *od = (GF_ObjectDescriptor*)gf_odf_desc_new(GF_ODF_OD_TAG);
-				od->URLString = "my_url";
-				od->objectDescriptorID = 0;
-				gf_term_add_media(self->owner, (GF_Descriptor*)od, 0);
-			}
-
-			/*for hybrid scenarios: map clocks*/
-			{
-				time_t now;
-				struct tm *now_tm;
-				time(&now);
-				now_tm = gmtime(&now);
-				{
-					GF_NetworkCommand com;
-					memset(&com, 0, sizeof(com));
-					com.command_type = GF_NET_CHAN_MAP_TIME;
-					com.map_time.media_time = now_tm->tm_hour*3600+now_tm->tm_min*60+now_tm->tm_sec;
-					com.map_time.timestamp = slh.compositionTimeStamp;
-					com.map_time.reset_buffers = 0;
-					com.base.on_channel = self->channel;
-					gf_term_on_command(self->owner, &com, GF_OK);
-					GF_LOG(GF_LOG_INFO, GF_LOG_MEDIA, ("[HYB In] Mapping WC  Time %04d/%02d/%02d %02d:%02d:%02d and Hyb time "LLD"\n",
-						(now_tm->tm_year + 1900), (now_tm->tm_mon + 1), now_tm->tm_mday, now_tm->tm_hour, now_tm->tm_min, now_tm->tm_sec,
-						com.map_time.timestamp));
-				}
-			}
-
-			/*initialize clock*/
-			init_time = gf_sys_clock();
 		}
 
 		time_to_wait = (u32)(init_time + ((u64)slh.compositionTimeStamp*1000)/FM_FAKE_PUSH_AUDIO_FREQ - gf_sys_clock());
@@ -274,6 +290,9 @@ static GF_Err FM_FAKE_PUSH_Disconnect(GF_HYBMEDIA *self)
 
 	audio_gen_stop(self);
 	gf_th_del(priv->th);
+#ifdef EXT_MEDIA_LOAD_THREADED
+	gf_th_del(priv->media_th);
+#endif
 	priv->th = NULL;
 
 	self->owner = NULL;
