1d0c96d5bff417667ea5cc331cde75fb122b5f06
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2011-10-21 16:58:44 +0000

    adding dvb support - daud
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3682 63c20433-aa62-49bd-875c-5a186b69a8fb

--- modules/libplayer/libplayer.c
+++ modules/libplayer/libplayer.c
@@ -44,7 +44,24 @@ typedef s32 off_t;
 
 #endif
 
+// variable used to detect the number of libplayer instances created - up to 2 instances  
 static int libplayer_id = 0;
+//! unfortunately, saving data would not be a good solution for Mosaic Mode in ESG Application since the position changes everytime user 
+//! uses the navigation button
+// value for input output windows, when save_data_instance1 equals to 1 <=> the second instance is already created with the correct size, 
+// save these values in in_instance1 and out_instance1 so that we do not need to recalculate every time  
+//~ static video_rect_t in_instance1, out_instance1;
+//~ static save_data_instance1 = 0;
+
+// variable use to detect whether a dvb instance has already started for instance 1
+static int start_dvb = 0;
+
+enum
+{
+	PLAYER_FILE = 0,
+	PLAYER_DVB = 1
+};
+
 
 typedef struct
 {
@@ -53,6 +70,7 @@ typedef struct
 	u32 init;
 	u32 state;
 	u32 player_id;
+	u32 player_type;
 	u32 width;
 	u32 height;
     char *url;
@@ -102,6 +120,8 @@ Bool LIBPLAYER_CanHandleURL(GF_InputService *plug, const char *url)
 		u32 i;
 		Bool ok = 0;
 		char *cgi_par;
+		// case dvb
+		if (!strnicmp(url, "dvb://", 6)) return 1;
 		if (!strnicmp(sExt, ".gz", 3)) sExt = strrchr(sExt, '.');
 		if (!strnicmp(url, "rtsp://", 7)) return 0;
 		sExt++;
@@ -118,6 +138,7 @@ Bool LIBPLAYER_CanHandleURL(GF_InputService *plug, const char *url)
 		if (cgi_par) cgi_par[0] = '?';
 		if (ok) return 1;
 	}
+	
 	return 0;
 }
 
@@ -169,6 +190,8 @@ GF_Err LIBPLAYER_ConnectService(GF_InputService *plug, GF_ClientService *serv, c
 		read->height = 20;
 		read->url = url;
 		read->player_id = libplayer_id;
+		read->player_type = PLAYER_FILE;
+		
 #ifndef TEST_LIBPLAYER
 		read->player = player_init(PLAYER_TYPE_DUMMY, PLAYER_AO_AUTO, PLAYER_VO_AUTO, PLAYER_MSG_INFO, read->player_id, on_libplayer_event);
 		
@@ -184,7 +207,84 @@ GF_Err LIBPLAYER_ConnectService(GF_InputService *plug, GF_ClientService *serv, c
 	
 #ifndef TEST_LIBPLAYER
 	mrl = NULL;
+	
+	// dvb 
 	if (!strnicmp(url, "dvb://", 6)) {
+		read->player_type = PLAYER_DVB;
+		mrl_resource_dvb_args_t *mrl_dvb_args;
+		mrl_dvb_args = calloc(1, sizeof(mrl_resource_dvb_args_t));
+		char *frequency;
+		
+		// fetch frequency
+		if (frequency = strchr(url+6, '@')) {
+			char *enc, *pid;
+			mrl_dvb_args->frequency = atoi(frequency+1);
+
+			/*
+			 * video
+			 */
+			// video codec
+			if (enc = strstr(url+6, "mpeg2")) {
+				mrl_dvb_args->video_enc = PLAYER_VIDEO_MPEG2;
+			}
+			else if (enc = strstr(url+6, "h264")) {
+				mrl_dvb_args->video_enc = PLAYER_VIDEO_H264;
+			}
+			else {
+				GF_LOG(GF_LOG_ERROR, GF_LOG_MODULE, ("[LibPlayerIN] Unknown video encoding\n"));
+				mrl_dvb_args->video_enc = PLAYER_VIDEO_UNKNOWN;
+			}
+			
+			// video PID
+			if (mrl_dvb_args->video_enc != PLAYER_VIDEO_UNKNOWN) {
+				pid = strchr(enc, ':');
+				mrl_dvb_args->video_pid = atoi(pid+1);
+			}
+			
+			/*
+			 * audio 
+			 */
+			// audio codec
+			if (enc = strstr(url+6, "mp2")) {
+				mrl_dvb_args->audio_enc = PLAYER_AUDIO_MP2;
+			}
+			else if (enc = strstr(url+6, "ac3")) {
+				mrl_dvb_args->audio_enc = PLAYER_AUDIO_AC3;
+			}
+			else {
+				GF_LOG(GF_LOG_ERROR, GF_LOG_MODULE, ("[LibPlayerIN] Unknown audio encoding\n"));
+				mrl_dvb_args->audio_enc = PLAYER_AUDIO_UNKNOWN;
+			}
+			
+			// audio PID
+			if (mrl_dvb_args->audio_enc != PLAYER_AUDIO_UNKNOWN) {
+				pid = strchr(enc, ':');
+				mrl_dvb_args->audio_pid = atoi(pid+1);
+			}
+			
+			if (mrl_dvb_args->video_enc == PLAYER_VIDEO_UNKNOWN && mrl_dvb_args->audio_enc == PLAYER_AUDIO_UNKNOWN) {
+				GF_LOG(GF_LOG_ERROR, GF_LOG_MODULE, ("[LibPlayerIN] Unknown video and audio encoding\n"));
+				free(mrl_dvb_args);
+				return GF_BAD_PARAM;
+			}
+		}
+		else {
+			GF_LOG(GF_LOG_ERROR, GF_LOG_MODULE, ("[LibPlayerIN] Unknown frequency\n"));
+			free(mrl_dvb_args);
+			return GF_BAD_PARAM;
+		}
+
+		// player instance 1 has not starter yet in dvb case <=> player instance 1 is not created yet 
+		if (start_dvb == 0) {
+			mrl = mrl_new(read->player, MRL_RESOURCE_DVB, mrl_dvb_args);
+			GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIN] MRL created for DVB\n"));
+		
+		// player has already started, zapping case, make sure the player is not recreated
+		} else {
+			GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIN] Starting DVB PES filtering\n"));
+			player_dvb_pes_filter_start(read->player, mrl_dvb_args->video_enc, mrl_dvb_args->video_pid, mrl_dvb_args->audio_enc, mrl_dvb_args->audio_pid);
+		}
+
 	}
 	else if (!strnicmp(url, "file://", 7) || !strstr(url, "://")) {
 		mrl_resource_local_args_t *mrl_args;
@@ -195,17 +295,19 @@ GF_Err LIBPLAYER_ConnectService(GF_InputService *plug, GF_ClientService *serv, c
 			mrl_args->location = strdup(url);
 		}
 		mrl = mrl_new (read->player, MRL_RESOURCE_FILE, mrl_args);
-	}
-	else {
+		
 	}
 
-	if (!mrl) {
-		GF_LOG(GF_LOG_ERROR, GF_LOG_MODULE, ("[LibPlayerIN] Failed to create MRL for url %s\n", url));
-		gf_term_on_connect(serv, NULL, GF_URL_ERROR);
-		return GF_OK;
+	// only for DVB case to make sure player is only set once
+	if (start_dvb == 0) {
+		if (!mrl) {
+			GF_LOG(GF_LOG_ERROR, GF_LOG_MODULE, ("[LibPlayerIN] Failed to create MRL for url %s\n", url));
+			gf_term_on_connect(serv, NULL, GF_URL_ERROR);
+			return GF_OK;
+		}
+		
+		player_mrl_set(read->player, mrl);
 	}
-	
-    player_mrl_set(read->player, mrl);
 
 #endif
 	read->state = 0;
@@ -242,20 +344,29 @@ GF_Err LIBPLAYER_CloseService(GF_InputService *plug)
 	LibPlayerIn *read = (LibPlayerIn *) plug->priv;
 	
 #ifndef TEST_LIBPLAYER
+	// only disconnect if 
+	if (read->player_type == PLAYER_FILE) {
 		player_playback_stop(read->player);
 		printf("[LibPlayerIN]player_playback_stop for instance %d\n", read->player_id);
 		player_uninit(read->player);
 		printf("[LibPlayerIN]player_uninit for instance %d\n", read->player_id);
 		read->player = NULL;
 		libplayer_id--;
-
-
-#endif
+		
 		read->state = 0;
 
-	gf_term_on_disconnect(read->service, NULL, GF_OK);
-	GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIn] Closing libplayer instance %d\n", read->player_id));
+		gf_term_on_disconnect(read->service, NULL, GF_OK);
+		GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIn] Closing libplayer instance %d\n", read->player_id));
+		
+		
+	
+	// channel zapping dvb case, don't disconnect service
+	} else {
+		GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIn] Don't close service libplayer instance %d, use this instance for channel zapping\n", read->player_id));
+	}
+
 	return GF_OK;
+#endif
 }
 
 /*Dummy input just send a file name, no multitrack to handle so we don't need to check sub_url nor expected type*/
@@ -290,19 +401,34 @@ GF_Err LIBPLAYER_ServiceCommand(GF_InputService *plug, GF_NetworkCommand *com)
 	case GF_NET_CHAN_PLAY:
 		if (read->state==0) {
 #ifndef TEST_LIBPLAYER
-			player_playback_start(read->player);
+			if ((read->player_id == 0) && (read->player_type == PLAYER_DVB) && (start_dvb == 1)) {
+				GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIN] Instance %d has already started, zapping mode\n", read->player_id));
+			
+			} else {
+				player_playback_start(read->player);
+				if ((read->player_id == 0) && (read->player_type == PLAYER_DVB)) {
+					start_dvb = 1;
+				}
+				
+				read->state = 1;
+				GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIN] Starting playback for instance %d\n", read->player_id));
+			}
 #endif
-			read->state = 1;
-			GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIN] Starting playback for instance %d\n", read->player_id));
 		}
+		
 		return GF_OK;
 	case GF_NET_CHAN_STOP:
 		if (read->state==1) {
 #ifndef TEST_LIBPLAYER
-			player_playback_pause(read->player);
+			// channel zapping, don't stop channel
+            if ((read->player_id == 0) && (read->player_type == PLAYER_DVB) && (start_dvb = 1)) {
+				GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIN] Instance %d is in zapping mode, don't stop channel\n", read->player_id));
+			} else {
+				player_playback_stop(read->player);
+				read->state = 0;
+				GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIN] Stopping playback for instance %d\n", read->player_id));
+			}
 #endif
-			read->state = 0;
-			GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerIN] Stopping playback for instance %d\n", read->player_id));
 		}
 		return GF_OK;
 	case GF_NET_CHAN_CONFIG:  return GF_OK;
@@ -341,6 +467,7 @@ GF_Err LIBPLAYER_DisconnectChannel(GF_InputService *plug, LPNETCHANNEL channel)
 Bool LIBPLAYER_CanHandleURLInService(GF_InputService *plug, const char *url)
 {
 	return 0;
+	//return 1;
 }
 
 static GF_Err LIBPLAYER_AttachStream(GF_BaseDecoder *dec, GF_ESD *esd)
@@ -408,11 +535,24 @@ static GF_Err LIBPLAYER_Control(GF_PrivateMediaDecoder *dec, Bool mute, GF_Windo
 	
 	if (!read) return GF_OK;
 	GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerDEC] Control instance %d\n",read->player_id));
-
+	//! unfortunately, saving data would not be a good solution for Mosaic Mode in ESG Application since the position changes everytime user 
+	//! uses the navigation button
+	//~ if (read->player_id == 1 && save_data_instance1 == 1) {
+		//~ printf("in here for save data instance\n");
+		//~ player_video_io_windows_set(read->player, &in_instance1, &out_instance1);
+		//~ 
+		//~ return GF_OK;
+		//~ 
+	//~ } else {
 	width = mrl_get_property(read->player, NULL, MRL_PROPERTY_VIDEO_WIDTH);
 	height = mrl_get_property(read->player, NULL, MRL_PROPERTY_VIDEO_HEIGHT);
+	//~ }
+
+	
 
 	if((width != read->width) || (height != read->height))	{
+		printf("in here for video size changed\t");
+		printf("width %d read->width %d height %d read->height %d\n", width, read->width, height, read->height);
 		GF_LOG(GF_LOG_DEBUG, GF_LOG_MODULE, ("[LibPlayerDEC] video size changed to width %d - height %d\n", width, height));
 		if (width && height) {
 			read->width = width;
@@ -421,15 +561,26 @@ static GF_Err LIBPLAYER_Control(GF_PrivateMediaDecoder *dec, Bool mute, GF_Windo
 		return GF_BUFFER_TOO_SMALL;
 	}
 	
-		in.x = src->x;
-		in.y = src->y;
-		in.w = src->w;
-		in.h = src->h;
-		out.x = dst->x;
-		out.y = dst->y;
-		out.w = dst->w;
-		out.h = dst->h;
-		player_video_io_windows_set(read->player, &in, &out);
+	in.x = src->x;
+	in.y = src->y;
+	in.w = src->w;
+	in.h = src->h;
+	out.x = dst->x;
+	out.y = dst->y;
+	out.w = dst->w;
+	out.h = dst->h;
+	player_video_io_windows_set(read->player, &in, &out);
+	
+	//! unfortunately, saving data would not be a good solution for Mosaic Mode in ESG Application since the position changes everytime user 
+	//! uses the navigation button
+	//~ if (read->player_id == 1) {
+		//~ in_instance1 = in;
+	//~ //	//~ out_instance1.w = out.w;
+	//~ //	//~ out_instance1.h = out.h;
+		//~ out_instance1 = out;
+		//~ save_data_instance1 = 1;
+	//~ }
+	//~ 
 	
 #endif
 	
