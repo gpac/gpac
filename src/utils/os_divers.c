6e9220e3603f9a80b61d96fc69da6eb562ac04c3
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2012-01-12 14:14:34 +0000

    [bouqueau] apply gf_global_resource_lock() patch by Ivica Arsov
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3841 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/utils/os_divers.c
+++ src/utils/os_divers.c
@@ -1835,6 +1835,7 @@ GF_GlobalLock * gf_global_resource_lock(const char * resourceName){
 #ifdef _WIN32_WCE
 	unsigned short sWResourceName[MAX_PATH];
 #endif
+	DWORD lastErr;
 	GF_GlobalLock *lock = gf_malloc(sizeof(GF_GlobalLock));
 	lock->resourceName = gf_strdup(resourceName);
 
@@ -1845,10 +1846,13 @@ GF_GlobalLock * gf_global_resource_lock(const char * resourceName){
 #else
 	lock->hMutex = CreateMutex(NULL, TRUE, resourceName);
 #endif
-	if (!lock->hMutex) {
-		DWORD lastErr = GetLastError();
-		if (lastErr != ERROR_ALREADY_EXISTS)
-			return NULL;
+	lastErr = GetLastError();
+	if (lastErr && lastErr == ERROR_ALREADY_EXISTS)
+		return NULL;
+	if (!lock->hMutex)
+	{
+		GF_LOG(GF_LOG_ERROR, GF_LOG_MUTEX, ("[Mutex] Couldn't create mutex for global lock: %d\n", lastErr));
+		return NULL;
 	}
 
 	/*then lock it*/
