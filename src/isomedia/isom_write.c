94362cc052244501e6d5b8ccf5e51a87306ee588
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2012-01-16 17:00:08 +0000

    fixed s16/u32 bug in roll table reading
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3844 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/isomedia/isom_write.c
+++ src/isomedia/isom_write.c
@@ -4118,9 +4118,10 @@ void *sg_rap_create_entry(void *udta)
 {
 	GF_VisualRandomAccessEntry *entry;
 	u32 *num_leading_samples = (u32 *) udta;
+	assert(udta);
 	GF_SAFEALLOC(entry, GF_VisualRandomAccessEntry);
-	entry->num_leading_samples_known = num_leading_samples ? 1 : 0;
 	entry->num_leading_samples = *num_leading_samples;
+	entry->num_leading_samples_known = entry->num_leading_samples ? 1 : 0;
 	return entry;
 }
 
