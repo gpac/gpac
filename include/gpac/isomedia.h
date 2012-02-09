0b340e90c4a6c77c76471b9315c61b12d78a053e
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2012-02-09 22:38:37 +0000

    Fixed GCC warning
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@3907 63c20433-aa62-49bd-875c-5a186b69a8fb

--- include/gpac/isomedia.h
+++ include/gpac/isomedia.h
@@ -1221,6 +1221,8 @@ GF_Err gf_isom_fragment_add_sample(GF_ISOFile *the_file, u32 TrackID, GF_ISOSamp
 CANNOT be used with OD tracks*/
 GF_Err gf_isom_fragment_append_data(GF_ISOFile *the_file, u32 TrackID, char *data, u32 data_size, u8 PaddingBits);
 
+void gf_isom_reset_fragment_info(GF_ISOFile *movie);
+
 #endif /*GPAC_DISBALE_ISOM_FRAGMENTS*/
 
 
