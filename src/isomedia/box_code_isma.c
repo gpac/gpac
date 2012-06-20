9b187746b28f12dcdd2d7f8c57ad2a2d574f0ff0
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2012-06-20 20:19:36 +0000

    Misc fixes on licence notices
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@4080 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/isomedia/box_code_isma.c
+++ src/isomedia/box_code_isma.c
@@ -7,7 +7,7 @@
  *
  *  This file is part of GPAC / ISO Media File Format sub-project
  *
- *  GPAC is free software you can redistribute it and/or modify
+ *  GPAC is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation either version 2, or (at your option)
  *  any later version.
