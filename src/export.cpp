0c49f26455b9d4c2829d5c6c8e70d97b0828bc79
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2012-06-21 07:59:07 +0000

    [bouqueau] fix license header
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@4082 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/export.cpp
+++ src/export.cpp
@@ -7,8 +7,19 @@
  *
  *  This file is part of GPAC 
  *
- * Proprietary Licence granted to DTS Licensing Limited by Telecom ParisTech
- * Exported revision 4079 on 06-20-2012
+ *  GPAC is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *   
+ *  GPAC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
  *
  */
 
