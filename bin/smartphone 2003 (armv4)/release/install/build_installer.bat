6aecad73d95f6a3c0c8673c1edbfdfe1d2341d48
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2012-07-11 12:57:39 +0000

    Moved version.h to revision.h and cleanup some references to GPAC_VERSION
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@4089 63c20433-aa62-49bd-875c-5a186b69a8fb

--- bin/smartphone 2003 (armv4)/release/install/build_installer.bat
+++ bin/smartphone 2003 (armv4)/release/install/build_installer.bat
@@ -3,8 +3,10 @@ cd /d %~dp0
 
 for /f "delims=" %%a in ('svnversion') do set gpac_revision=%%a
 
+set gpac_version="0.5.0-r%gpac_revision%"
+
 ECHO [Version] > gpaccab.inf
-ECHO Provider = "GPAC 0.5.0-r%gpac_revision%" >> gpaccab.inf
+ECHO Provider = "GPAC %gpac_version%" >> gpaccab.inf
 type gpac.inf >> gpaccab.inf
 
 CabWiz gpaccab.inf
@@ -12,7 +14,7 @@ CabWiz gpaccab.inf
 ECHO off
 
 ECHO [CEAppManager]> gpac.ini
-ECHO Version = 0.5.0-r%gpac_revision%>> gpac.ini
+ECHO Version = %gpac_version%>> gpac.ini
 ECHO Component = GPAC for Windows Mobile>> gpac.ini
 ECHO [GPAC for Windows Mobile]>> gpac.ini
 ECHO Description = GPAC MPEG-4 Player>> gpac.ini
@@ -24,7 +26,7 @@ ECHO CabFiles = gpaccab.cab >> gpac.ini
 ECHO on
 
 ezsetup -l english -i gpac.ini -r readme.txt -e ../../../../COPYING -o gpac.exe
-rename gpac.exe "GPAC_0.5.0-r%gpac_revision%_WindowsMobile.exe"
+rename gpac.exe "GPAC_%gpac_version%_WindowsMobile.exe"
 DEL gpaccab.cab
 DEL gpaccab.inf
 DEL gpac.ini
