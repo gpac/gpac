ec7abda69751c918150b17e7d92666c9063074c0
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2012-09-05 12:28:28 +0000

    Fixed wrong commit
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@4140 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/odf/odf_dump.c
+++ src/odf/odf_dump.c
@@ -7,8 +7,19 @@
  *
  *  This file is part of GPAC / MPEG-4 ObjectDescriptor sub-project
  *
- * Proprietary Licence granted to Expway by Telecom ParisTech
- * Exported revision 4133 on 09-04-2012
+ *  GPAC is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *   
+ *  GPAC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
  *
  */
 
