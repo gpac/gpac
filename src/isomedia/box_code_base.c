065d02b3b63973f3b0bf7d372c471696079a1fb6
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2012-10-25 12:41:32 +0000

    [bouqueau] fix leak
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@4204 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -1285,6 +1285,7 @@ GF_Err ftyp_Size(GF_Box *s)
 void gnrm_del(GF_Box *s)
 {
 	GF_GenericSampleEntryBox *ptr = (GF_GenericSampleEntryBox *)s;
+	gf_isom_sample_entry_predestroy((GF_SampleEntryBox *)ptr);
 	if (ptr->data) gf_free(ptr->data);
 	gf_free(ptr);
 }
