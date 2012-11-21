75f4aaf54b1dd60e03ab74767c9725f83b6c6a08
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2012-11-21 14:50:15 +0000

    Fixed GCC warnings
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@4236 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/media_tools/mpegts.c
+++ src/media_tools/mpegts.c
@@ -3175,13 +3175,13 @@ Bool gf_m2ts_probe_file(const char *fileName)
 static void rewrite_pts_dts(unsigned char *ptr, u64 TS)
 {
 	ptr[0] &= 0xf1;
-	ptr[0] |= (unsigned char)((TS&0x1c0000000)>>29);
-	ptr[1]  = (unsigned char)((TS&0x03fc00000)>>22);
+	ptr[0] |= (unsigned char)((TS&0x1c0000000ULL)>>29);
+	ptr[1]  = (unsigned char)((TS&0x03fc00000ULL)>>22);
 	ptr[2] &= 0x1;
-	ptr[2] |= (unsigned char)((TS&0x0003f8000)>>14);
-	ptr[3]  = (unsigned char)((TS&0x000007f80)>>7);
+	ptr[2] |= (unsigned char)((TS&0x0003f8000ULL)>>14);
+	ptr[3]  = (unsigned char)((TS&0x000007f80ULL)>>7);
 	ptr[4] &= 0x1;
-	ptr[4] |= (unsigned char)((TS&0x00000007f)<<1);
+	ptr[4] |= (unsigned char)((TS&0x00000007fULL)<<1);
 
 	assert(((u64)(ptr[0]&0xe)<<29) + ((u64)ptr[1]<<22) + ((u64)(ptr[2]&0xfe)<<14) + ((u64)ptr[3]<<7) + ((ptr[4]&0xfe)>>1) == TS);
 }
