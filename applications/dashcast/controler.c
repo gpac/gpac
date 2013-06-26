c65ed9250ba10cb4e6999d7fdcfa4bdb49c6375b
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2013-06-26 12:26:52 +0000

    add dash to the msvc2010 solution + minor things
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@4643 63c20433-aa62-49bd-875c-5a186b69a8fb

--- applications/dashcast/controler.c
+++ applications/dashcast/controler.c
@@ -150,7 +150,7 @@ u32 send_frag_event(void * p_params) {
 
 }
 
-u32 mpd_thread(void * p_params) {
+static u32 mpd_thread(void * p_params) {
 
 	u32 i;
 
@@ -543,7 +543,7 @@ Bool dasher_thread(void * p_params) {
 	return 0;
 }
 
-u32 keyboard_thread(void * p_params) {
+static u32 keyboard_thread(void * p_params) {
 
 	ThreadParam * p_th_param = (ThreadParam *) p_params;
 	CmdData * p_in_data = p_th_param->p_in_data;
@@ -1162,7 +1162,7 @@ u32 audio_encoder_thread(void * p_params) {
 int dc_run_controler(CmdData * p_in_data) {
 
 	u32 i, j;
-
+	
 	ThreadParam keyboard_th_params;
 	ThreadParam mpd_th_params;
 	ThreadParam delete_seg_th_params;
@@ -1301,10 +1301,10 @@ int dc_run_controler(CmdData * p_in_data) {
 
 	/* Initialize keyboard controller thread */
 	keyboard_th_params.p_thread = gf_th_new("keyboard_thread");
-
+	
 	/* Create keyboard controller thread */
 	keyboard_th_params.p_in_data = p_in_data;
-	if (gf_th_run(keyboard_th_params.p_thread, keyboard_thread, (void *) &keyboard_th_params) != GF_OK) {
+	if (gf_th_run(keyboard_th_params.p_thread, keyboard_thread, (void *)&keyboard_th_params) != GF_OK) {
 
 		fprintf(stderr,
 				"Error while doing pthread_create for keyboard_thread.\n");
@@ -1324,16 +1324,16 @@ int dc_run_controler(CmdData * p_in_data) {
 		VideoData * p_tmp_vdata = gf_list_get(p_in_data->p_video_lst, i);
 		p_tmp_vdata->i_framerate = p_in_data->vdata.i_framerate;
 	}
-
+	
 	/******** MPD Thread ********/
 
 	/* Initialize MPD generator thread */
 	mpd_th_params.p_thread = gf_th_new("mpd_thread");
-
-	/* Create keyboard controller thread */
+	
+	/* Create MPD generator thread */
 	mpd_th_params.p_in_data = p_in_data;
 	mpd_th_params.p_mq = &mq;
-	if (gf_th_run(mpd_th_params.p_thread, mpd_thread, (void *) &mpd_th_params) != GF_OK) {
+	if (gf_th_run(mpd_th_params.p_thread, mpd_thread, (void *)&mpd_th_params) != GF_OK) {
 
 		fprintf(stderr, "Error while doing pthread_create for mpd_thread.\n");
 	}
