e1dd87c4c0d8bd7c437feb73bce31680cd4ef2d2
Jerome Gorin <gorinje@gmail.com>
2013-08-29 11:54:31 +0000

    Fix compilation error for ios
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@4717 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/media_tools/mpegts.c
+++ src/media_tools/mpegts.c
@@ -846,7 +846,7 @@ Bool gf_m2ts_crc32_check(char *data, u32 len)
 {
     u32 crc = gf_crc_32(data, len);
 	u32 crc_val = GF_4CC((u8) data[len], (u8) data[len+1], (u8) data[len+2], (u8) data[len+3]);
-	return (crc==crc_val) ? 1 : 0;
+	return (crc==crc_val) ? GF_TRUE : GF_FALSE;
 }
 
 
