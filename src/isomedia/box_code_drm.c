a06c5962848ab019be101223fc887aa053621290
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2013-10-23 18:54:13 +0000

    fixed gcc warning
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@4860 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/isomedia/box_code_drm.c
+++ src/isomedia/box_code_drm.c
@@ -1255,6 +1255,7 @@ GF_Err piff_pssh_Write(GF_Box *s, GF_BitStream *bs)
 {
 	GF_PIFFProtectionSystemHeaderBox *ptr = (GF_PIFFProtectionSystemHeaderBox *) s;
 	GF_Err e = gf_isom_full_box_write(s, bs);
+	if (e) return e;
 
 	gf_bs_write_data(bs, (char *) ptr->SystemID, 16);
 	gf_bs_write_u32(bs, ptr->private_data_size);
