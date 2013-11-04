5061e16e9b6b19df318c4c99270deb407746dbfa
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2013-11-04 15:16:14 +0000

    Moved to hevc branch of openHEVC
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@4877 63c20433-aa62-49bd-875c-5a186b69a8fb

--- extra_lib/include/openHevcWrapper.h
+++ extra_lib/include/openHevcWrapper.h
@@ -47,14 +47,20 @@ typedef struct OpenHevc_Frame_cpy
    OpenHevc_FrameInfo frameInfo;
 } OpenHevc_Frame_cpy;
 
-OpenHevc_Handle libOpenHevcInit(int nb_pthreads);
+OpenHevc_Handle libOpenHevcInit(int nb_pthreads, int thread_type);
+int libOpenHevcStartDecoder(OpenHevc_Handle openHevcHandle);
 int  libOpenHevcDecode(OpenHevc_Handle openHevcHandle, const unsigned char *buff, int nal_len, int64_t pts);
 void libOpenHevcGetPictureInfo(OpenHevc_Handle openHevcHandle, OpenHevc_FrameInfo *openHevcFrameInfo);
+void libOpenHevcCopyExtraData(OpenHevc_Handle openHevcHandle, unsigned char *extra_data, int extra_size_alloc);
+
 void libOpenHevcGetPictureSize2(OpenHevc_Handle openHevcHandle, OpenHevc_FrameInfo *openHevcFrameInfo);
 int  libOpenHevcGetOutput(OpenHevc_Handle openHevcHandle, int got_picture, OpenHevc_Frame *openHevcFrame);
 int  libOpenHevcGetOutputCpy(OpenHevc_Handle openHevcHandle, int got_picture, OpenHevc_Frame_cpy *openHevcFrame);
 void libOpenHevcSetCheckMD5(OpenHevc_Handle openHevcHandle, int val);
-void libOpenHevcSetDisableAU(OpenHevc_Handle openHevcHandle, int val);
+void libOpenHevcSetDebugMode(OpenHevc_Handle openHevcHandle, int val);
+void libOpenHevcSetTemporalLayer_id(OpenHevc_Handle openHevcHandle, int val);
+void libOpenHevcSetNoCropping(OpenHevc_Handle openHevcHandle, int val);
+
 void libOpenHevcClose(OpenHevc_Handle openHevcHandle);
 void libOpenHevcFlush(OpenHevc_Handle openHevcHandle);
 const char *libOpenHevcVersion(OpenHevc_Handle openHevcHandle);
