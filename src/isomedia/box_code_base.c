5170f625e1d6342b8994aca87ffdf7539d8118ac
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2014-02-06 17:04:13 +0000

    Added workaround for broken stts files
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@5051 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -5589,6 +5589,16 @@ GF_Err stts_Read(GF_Box *s, GF_BitStream *bs)
 		ptr->w_currentSampleNum += ptr->entries[i].sampleCount;
 		ptr->w_LastDTS += ptr->entries[i].sampleCount * ptr->entries[i].sampleDelta;
 #endif
+	
+		if (!ptr->entries[i].sampleDelta) {
+			if ((i+1<ptr->nb_entries) ) {
+				GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[iso file] Found stss entry with sample_delta=0 - forbidden ! Fixing to 1\n" ));
+				ptr->entries[i].sampleDelta = 1;
+			} else if (ptr->entries[i].sampleCount>1) {
+				GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[iso file] more than one sample at the end of the track with sample_delta=0 - forbidden ! Fixing to 1\n" ));
+				ptr->entries[i].sampleDelta = 1;
+			}
+		}
 	}
 	//remove the last sample delta.
 #ifndef GPAC_DISABLE_ISOM_WRITE
