f0c507234381b29fa948a1b21d266bbb12cf59b9
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2014-03-06 19:57:32 +0000

    fix bug in prev commit
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@5144 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/utils/os_divers.c
+++ src/utils/os_divers.c
@@ -92,7 +92,7 @@ u64 gf_sys_clock_high_res()
 {
 	struct timeval now;
 	gettimeofday(&now, NULL);
-	return ( (now.tv_sec)*1000000 + (now.tv_usec) - sys_start_time_hr;
+	return (now.tv_sec)*1000000 + (now.tv_usec) - sys_start_time_hr;
 }
 
 #endif
