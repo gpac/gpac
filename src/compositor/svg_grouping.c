68454cca97257244345fe9bfd46aec071ed84bbe
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2014-04-14 10:57:21 +0000

    Fixed compilation without 3D support
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@5201 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/compositor/svg_grouping.c
+++ src/compositor/svg_grouping.c
@@ -541,7 +541,11 @@ static void svg_traverse_g(GF_Node *node, void *rs, Bool is_destroy)
 		return;
 	}
 	/*group cache traverse routine*/
-	else if ((tr_state->traversing_mode == TRAVERSE_DRAW_2D) || (tr_state->traversing_mode == TRAVERSE_DRAW_3D) ) {
+	else if ((tr_state->traversing_mode == TRAVERSE_DRAW_2D) 
+#ifndef GPAC_DISABLE_3D
+		|| (tr_state->traversing_mode == TRAVERSE_DRAW_3D) 
+#endif
+	) {
 		SVGgStack *group = gf_node_get_private(node);
 		group_cache_draw(group->cache, tr_state);
 		return;
