846f32873d14132df498400d61545a79a79f877a
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2014-04-15 09:33:58 +0000

    fix batch changing dir + inconsistencies
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@5203 63c20433-aa62-49bd-875c-5a186b69a8fb

--- version.bat
+++ version.bat
@@ -1,10 +1,16 @@
 @echo off
+set OLDDIR=%CD%
 cd /d %~dp0
 for /f "delims=" %%a in ('svnversion') do echo #define GPAC_SVN_REVISION "%%a" > test.h 
 if not exist include\gpac\revision.h goto create 
 ECHO n|COMP test.h include\gpac\revision.h > nul 
 if errorlevel 1 goto create
 DEL test.h
-exit/b
+goto done
+
 :create
 MOVE /Y test.h include\gpac\revision.h
+
+:done
+cd /d %OLDDIR%
+exit/b
