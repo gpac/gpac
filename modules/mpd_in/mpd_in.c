1c4d3671a784246bea08a29b99bdf4983e1afff4
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2014-07-09 11:20:57 +0000

    Fixed bugs in DASH selection based on display resolution + more logs in selection logic
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@5296 63c20433-aa62-49bd-875c-5a186b69a8fb

--- modules/mpd_in/mpd_in.c
+++ modules/mpd_in/mpd_in.c
@@ -845,8 +845,6 @@ GF_Err MPD_ConnectService(GF_InputService *plug, GF_ClientService *serv, const c
 		com.base.command_type = GF_NET_SERVICE_MEDIA_CAP_QUERY;
 		gf_service_command(serv, &com, GF_OK);
 
-		com.mcaps.width = 1920;
-		com.mcaps.height = 1080;
 		if (com.mcaps.width && com.mcaps.height) {
 			gf_dash_set_max_resolution(mpdin->dash, com.mcaps.width, com.mcaps.height, com.mcaps.display_bit_depth);
 		}
