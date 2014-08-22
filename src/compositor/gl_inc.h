52c551ebdf4b7cfb1bd526a99bdd8535ab076b3d
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2014-08-22 14:05:07 +0000

    Fixed android build
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@5364 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/compositor/gl_inc.h
+++ src/compositor/gl_inc.h
@@ -471,11 +471,13 @@ GLDECL(void, glUniformMatrix4x2fv, (GLint location, GLsizei count, GLboolean tra
 GLDECL(void, glUniformMatrix3x4fv, (GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) )
 GLDECL(void, glUniformMatrix4x3fv, (GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) )
 
+#ifndef GPAC_ANDROID
 GLDECL(void, glEnableVertexAttribArray, (GLuint index) )
 GLDECL(void, glDisableVertexAttribArray, (GLuint index) )
 GLDECL(void, glVertexAttribPointer, (GLuint  index,  GLint  size,  GLenum  type,  GLboolean  normalized,  GLsizei  stride,  const GLvoid *  pointer) )
 GLDECL(void, glVertexAttribIPointer, (GLuint  index,  GLint  size,  GLenum  type,  GLsizei  stride,  const GLvoid *  pointer) )
 GLDECL(GLint, glGetAttribLocation, (GLuint prog, const char *name) )
+#endif
 
 
 #endif //GL_VERSION_2_0
