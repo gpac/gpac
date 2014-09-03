bed62789f8bb21c027870e1bdf4141ee06f10cce
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2014-09-03 19:24:17 +0000

    Fixed bug in X11 move window and in GUI (from previous commit)
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@5394 63c20433-aa62-49bd-875c-5a186b69a8fb

--- modules/sdl_out/video.c
+++ modules/sdl_out/video.c
@@ -1567,6 +1567,7 @@ static GF_Err SDLVid_ProcessEvent(GF_VideoOutput *dr, GF_Event *evt)
 	}
 	break;
 	case GF_EVENT_MOVE:
+
 #if !defined(GPAC_IPHONE) && SDL_VERSION_ATLEAST(2,0,0)
         {
             SDLVID();
