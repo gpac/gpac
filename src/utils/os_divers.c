9eed84ccd0c9afd46fc3c2f3b250d4d3e7a19666
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2014-10-27 17:33:07 +0000

    Fixed bug introduced by OSX file assoication patch
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@5478 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/utils/os_divers.c
+++ src/utils/os_divers.c
@@ -1255,7 +1255,11 @@ const char **gpac_argv = NULL;
 GF_EXPORT
 void gf_sys_set_args(s32 argc, const char **argv)
 {
-	if (!gpac_argc && (argc>=0) ) {
+	//for OSX we allow overwrite of argc/argv due to different behavior between console-mode apps and GUI
+#if !defined(__DARWIN__) && !defined(__APPLE__)
+	if (!gpac_argc && (argc>=0) ) 
+#endif
+	{
 		gpac_argc = (u32) argc;
 		gpac_argv = argv;
 	}
