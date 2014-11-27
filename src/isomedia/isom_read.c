f8ada8f1da8c2e1ccd4a0d0f7697081bce0fffca
Cyril Concolato <cyril.concolato@telecom-paristech.fr>
2014-11-27 14:35:45 +0000

    add sample dependency flags to -dts output
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@5531 63c20433-aa62-49bd-875c-5a186b69a8fb

--- src/isomedia/isom_read.c
+++ src/isomedia/isom_read.c
@@ -1284,6 +1284,19 @@ Bool gf_isom_has_sample_dependency(GF_ISOFile *the_file, u32 trackNumber)
 	return 1;
 }
 
+GF_EXPORT
+GF_Err gf_isom_get_sample_flags(GF_ISOFile *the_file, u32 trackNumber, u32 sampleNumber, u32 *dependsOn, u32 *dependedOn, u32 *redundant)
+{
+	GF_TrackBox *trak = NULL;
+	*dependsOn = 0;
+	*dependedOn = 0;
+	*redundant = 0;
+	trak = gf_isom_get_track_from_file(the_file, trackNumber);
+	if (!trak) return GF_BAD_PARAM;
+	if (!trak->Media->information->sampleTable->SampleDep) return GF_BAD_PARAM;
+	return stbl_GetSampleDepType(trak->Media->information->sampleTable->SampleDep, sampleNumber, dependsOn, dependedOn, redundant);
+}
+
 //return a sample give its number, and set the SampleDescIndex of this sample
 //this index allows to retrieve the stream description if needed (2 media in 1 track)
 //return NULL if error
