126969a0321a8c4d4b8b92e6cfd37ceac27e7002
Jean Le Feuvre <jeanlf@users.sourceforge.net>
2014-11-28 14:15:41 +0000

    Now using FFMPEG 2.4.3 for android
    git-svn-id: http://svn.code.sf.net/p/gpac/code/trunk/gpac@5537 63c20433-aa62-49bd-875c-5a186b69a8fb

--- extra_lib/include/ffmpeg_android/libavutil/intfloat_readwrite.h
+++ extra_lib/include/ffmpeg_android/libavutil/intfloat_readwrite.h
@@ -22,19 +22,23 @@
 #define AVUTIL_INTFLOAT_READWRITE_H
 
 #include <stdint.h>
-#include "attributes.h"
 
+#include "attributes.h"
+#include "version.h"
+
+#if FF_API_INTFLOAT
 /* IEEE 80 bits extended float */
 typedef struct AVExtFloat  {
     uint8_t exponent[2];
     uint8_t mantissa[8];
 } AVExtFloat;
 
-double av_int2dbl(int64_t v) av_const;
-float av_int2flt(int32_t v) av_const;
-double av_ext2dbl(const AVExtFloat ext) av_const;
-int64_t av_dbl2int(double d) av_const;
-int32_t av_flt2int(float d) av_const;
-AVExtFloat av_dbl2ext(double d) av_const;
+attribute_deprecated double av_int2dbl(int64_t v) av_const;
+attribute_deprecated float av_int2flt(int32_t v) av_const;
+attribute_deprecated double av_ext2dbl(const AVExtFloat ext) av_const;
+attribute_deprecated int64_t av_dbl2int(double d) av_const;
+attribute_deprecated int32_t av_flt2int(float d) av_const;
+attribute_deprecated AVExtFloat av_dbl2ext(double d) av_const;
+#endif /* FF_API_INTFLOAT */
 
 #endif /* AVUTIL_INTFLOAT_READWRITE_H */
