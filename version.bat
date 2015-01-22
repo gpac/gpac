9f2b7efd3bc0dd13e3f7dd53795d6aec59e479cc
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-01-22 10:53:44 +0100

    fix version generation for windows

--- version.bat
+++ version.bat
@@ -2,7 +2,7 @@
 set OLDDIR=%CD%
 cd /d %~dp0
 for /f "delims=" %%a in ('git describe --tags --long') do @set VERSION=%%a
-for /f "delims=" %%a in ('git describe --tags --abbrev=0') do @set TAG=%%a-
+for /f "delims=" %%a in ('git describe --tags --abbrev^=0') do @set TAG=%%a-
 for /f "delims=" %%a in ('git rev-parse --abbrev-ref HEAD') do @set BRANCH=%%a
 REM remove anotated tag from VERSION
 setlocal enabledelayedexpansion
