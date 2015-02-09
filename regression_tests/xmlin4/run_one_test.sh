52d9dfe9878b70b41fe9d19cdd4dec360bcc4bc8
cconcolato <cyril.concolato@telecom-paristech.fr>
2015-02-09 06:06:23 +0100

    adding test files for storage of text (including xml) streams in mp4

---
+++ regression_tests/xmlin4/run_one_test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo -e "*************** Running basic test for "$1
+
+echo -e "\nTesting import"
+MP4Box -add $1.nhml -new $1.mp4
+
+echo -e "\nTesting info dump"
+MP4Box -info $1.mp4
+
+echo -e "\nTesting entire track extraction"
+MP4Box -raw 1 $1.mp4
+
+echo -e "\nTesting sample by sample extraction"
+MP4Box -raws 1 $1.mp4
