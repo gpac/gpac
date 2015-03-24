52d87f4b028e3ce2df04b611ad90425a997c3f0c
jeanlf <jeanlf@users.sourceforge.net>
2015-03-24 11:26:27 +0100

    Print average GOP size in -info

--- applications/mp4box/filedump.c
+++ applications/mp4box/filedump.c
@@ -2363,6 +2363,18 @@ void DumpTrackInfo(GF_ISOFile *file, u32 trackID, Bool full_dump)
 		}
 	}
 
+	switch (gf_isom_has_sync_points(file, trackNum)) {
+	case 0:
+		fprintf(stderr, "\tAll samples are sync\n");
+		break;
+	case 1:
+		fprintf(stderr, "\tAverage GOP length: %d samples\n", (u32 ) (gf_isom_get_sample_count(file, trackNum) / gf_isom_get_sync_point_count(file, trackNum) ) );
+		break;
+	case 2:
+		fprintf(stderr, "\tNo sync sample found\n");
+		break;
+	}
+
 	if (!full_dump) {
 		fprintf(stderr, "\n");
 		return;
