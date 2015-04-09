6bd184219efb8b5eff82d7bd9ee9a2cb480f5db8
jeanlf <jeanlf@users.sourceforge.net>
2015-04-09 07:47:07 +0200

    Fixed wrong IOD PL code for AVC (#102)

--- applications/mp4box/filedump.c
+++ applications/mp4box/filedump.c
@@ -1648,7 +1648,7 @@ static void print_config_hash(GF_List *xps_array, char *szName)
 	u8 hash[20];
 	for (i=0; i<gf_list_count(xps_array); i++) {
 		slc = gf_list_get(xps_array, i);
-		gf_sha1_csum(slc->data, slc->size, hash);
+		gf_sha1_csum((u8 *) slc->data, slc->size, hash);
 		fprintf(stderr, "\t%s#%d hash: ", szName, i+1);
 		for (j=0; j<20; j++) fprintf(stderr, "%02X", hash[j]);
 		fprintf(stderr, "\n");
