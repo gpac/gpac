gpac/gpac

commit fa5f4f17eb6937457b3f61acef512468c71aa1f8

export alloc functions instead of using a prepro alias

include/gpac/setup.h
@@ -343,11 +343,11 @@ u64 gf_memory_size(); /*gets memory allocated in bytes*/
 
 #else
 
-#define gf_malloc malloc
-#define gf_calloc calloc
-#define gf_realloc realloc
-#define gf_free free
-#define gf_strdup strdup
+void* gf_malloc(size_t size);
+void* gf_calloc(size_t num, size_t size_of);
+void* gf_realloc(void *ptr, size_t size);
+void gf_free(void *ptr);
+char* gf_strdup(const char *str);
 
 #endif
 
@@ -540,7 +540,7 @@ u64 gf_memory_size(); /*gets memory allocated in bytes*/
 # endif
 #endif
 
-#if !defined(GPAC_HAS_SPIDERMONKEY) || defined(GPAC_DISABLE_SVG)
+#ifndef GPAC_HAS_SPIDERMONKEY
 # ifndef GPAC_DISABLE_MSE
 # define GPAC_DISABLE_MSE
 # endif

include/gpac/version.h
@@ -37,11 +37,10 @@
  */
 #define GPAC_VERSION          "0.5.2-DEV"
 #define GPAC_VERSION_MAJOR 4
-#define GPAC_VERSION_MINOR 4
+#define GPAC_VERSION_MINOR 5
 #define GPAC_VERSION_MICRO 0
 
 #include <gpac/revision.h>
 #define GPAC_FULL_VERSION       GPAC_VERSION "-rev" GPAC_GIT_REVISION
 
 #endif //_GF_VERSION_H
-

commit 4c37fddc8424e0e38109f71396e032d7654b7cfc

fix MSE detection broken in fa5f4f17eb6937457b3f61acef512468c71aa1f8

include/gpac/setup.h
@@ -540,7 +540,7 @@ char* gf_strdup(const char *str);
 # endif
 #endif
 
-#ifndef GPAC_HAS_SPIDERMONKEY
+#if !defined(GPAC_HAS_SPIDERMONKEY) || defined(GPAC_DISABLE_SVG)
 # ifndef GPAC_DISABLE_MSE
 # define GPAC_DISABLE_MSE
 # endif