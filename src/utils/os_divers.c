e85563f98803bce6794c8eeaa63deafb17e11487
jeanlf <jeanlf@users.sf.net>
2015-05-27 13:55:32 +0200

    Fixed typo from last commit

--- src/utils/os_divers.c
+++ src/utils/os_divers.c
@@ -1789,7 +1789,7 @@ static time_t gf_mktime_utc(struct tm *tm)
 {
 	static const time_t kTimeMax = ~(1L << (sizeof(time_t) * CHAR_BIT - 1));
 	static const time_t kTimeMin = (1L << (sizeof(time_t) * CHAR_BIT - 1));
-	time64_t result = timegm64(t);
+	time64_t result = timegm64(tm);
 	if (result < kTimeMin || result > kTimeMax)
 		return -1;
 	return result;
