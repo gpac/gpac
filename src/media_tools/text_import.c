dfedc6af1aeb21c392bb8729a5898767206c8fc3
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2015-06-09 22:21:36 +0200

    ebu-ttd: fix wrong removal of <div> when separating samples see #160

--- src/media_tools/text_import.c
+++ src/media_tools/text_import.c
@@ -901,19 +901,25 @@ static void ttml_import_progress(void *cbk, u64 cur_samp, u64 count)
 static void gf_text_import_ebu_ttd_remove_samples(GF_XMLNode *root, GF_XMLNode **sample_list_node)
 {
 	u32 idx = 0, body_num = 0;
-	GF_XMLNode *node = NULL, *body_node = NULL;
+	GF_XMLNode *node = NULL;
 	*sample_list_node = NULL;
 	while ( (node = (GF_XMLNode*)gf_list_enum(root->content, &idx))) {
 		if (!strcmp(node->name, "body")) {
-			*sample_list_node = node;
-			body_num = gf_list_count(node->content);
-			while (body_num--) {
-				body_node = (GF_XMLNode*)gf_list_get(node->content, 0);
-				assert(gf_list_find(node->content, body_node) == 0);
-				gf_list_rem(node->content, 0);
-				gf_xml_dom_node_del(body_node);
+			GF_XMLNode *body_node;
+			u32 body_idx = 0;
+			while ( (body_node = (GF_XMLNode*)gf_list_enum(node->content, &body_idx))) {
+				if (!strcmp(body_node->name, "div")) {
+					*sample_list_node = body_node;
+					body_num = gf_list_count(body_node->content);
+					while (body_num--) {
+						GF_XMLNode *content_node = (GF_XMLNode*)gf_list_get(body_node->content, 0);
+						assert(gf_list_find(body_node->content, content_node) == 0);
+						gf_list_rem(body_node->content, 0);
+						gf_xml_dom_node_del(content_node);
+					}
+					return;
+				}
 			}
-			return;
 		}
 	}
 }
