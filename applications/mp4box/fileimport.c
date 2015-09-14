0ff260488502b54dbd7f33e67341e8bde5e12c68
jeanlf <jeanlf@users.sourceforge.net>
2015-09-14 11:14:30 +0200

    Fixed gcc warnings and iOS project files for IFF

--- applications/mp4box/fileimport.c
+++ applications/mp4box/fileimport.c
@@ -2700,7 +2700,7 @@ GF_ISOFile *package_file(char *file_name, char *fcc, const char *tmpdir, Bool ma
 	if (e) goto exit;
 	/*add self ref*/
 	if (isom_src) {
-		e = gf_isom_add_meta_item(file, 1, 0, 1, NULL, isom_src, 0, NULL, NULL, NULL,  NULL);
+		e = gf_isom_add_meta_item(file, 1, 0, 1, NULL, isom_src, 0, NULL, NULL, NULL,  NULL, NULL);
 		if (e) goto exit;
 	}
 	e = gf_isom_set_meta_xml(file, 1, 0, file_name, !ascii);
@@ -2749,7 +2749,7 @@ GF_ISOFile *package_file(char *file_name, char *fcc, const char *tmpdir, Bool ma
 			encoding = "binary-gzip";
 		}
 
-		e = gf_isom_add_meta_item(file, 1, 0, 0, item, name, 0, mime, NULL, NULL,  NULL);
+		e = gf_isom_add_meta_item(file, 1, 0, 0, item, name, 0, mime, NULL, NULL,  NULL, NULL);
 		gf_free(name);
 		if (e) goto exit;
 	}
--- src/isomedia/box_dump.c
+++ src/isomedia/box_dump.c
@@ -4678,7 +4678,7 @@ GF_Err colr_dump(GF_Box *a, FILE * trace)
 {
 	GF_ColourInformationBox *ptr = (GF_ColourInformationBox *)a;
 	if (!a) return GF_BAD_PARAM;
-	fprintf(trace, "<ColourInformationBox colour_type=\"%s\" colour_primaries=\"%d\" transfer_characteristics=\"%d\" matrix_coefficients=\"%d\" full_range_flag=\"%d\">\n", ptr->colour_type, ptr->colour_primaries, ptr->transfer_characteristics, ptr->matrix_coefficients, ptr->full_range_flag);
+	fprintf(trace, "<ColourInformationBox colour_type=\"%s\" colour_primaries=\"%d\" transfer_characteristics=\"%d\" matrix_coefficients=\"%d\" full_range_flag=\"%d\">\n", gf_4cc_to_str(ptr->colour_type), ptr->colour_primaries, ptr->transfer_characteristics, ptr->matrix_coefficients, ptr->full_range_flag);
 	DumpBox(a, trace);
 	gf_full_box_dump((GF_Box *)a, trace);
 	gf_box_dump_done("ColourInformationBox", a, trace);
