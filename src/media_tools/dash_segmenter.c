a5ba1a8b8c1dc2b37e0e042603b492fbbadbd791
nguyen-viet-thanh-trung <viet-thanh-trung.nguyen@telecom-paristech.fr>
2015-09-14 12:17:08 +0200

    revert commit #0c05b0b: broken dash segmenter when fragment duration is not equal to segment duration

--- src/media_tools/dash_segmenter.c
+++ src/media_tools/dash_segmenter.c
@@ -1773,7 +1773,7 @@ restart_fragmentation_pass:
 		}
 #endif
 
-		if (force_switch_segment || !next || ((SegmentDuration >= MaxSegmentDuration) && (!split_seg_at_rap || next_sample_rap || tf->splitable))) {
+		if (force_switch_segment || ((SegmentDuration >= MaxSegmentDuration) && (!split_seg_at_rap || !next || next_sample_rap || tf->splitable))) {
 			if (!min_seg_dur || (min_seg_dur>SegmentDuration))
 				min_seg_dur = SegmentDuration;
 			if (max_seg_dur < SegmentDuration)
