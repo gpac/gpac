gpac/gpac

commit e10994e8d4a3d3111a744e118c997506140f0306

[DASH] add duration for each media input: - when shorter, samples are removed; - when longer, the last sample duration is lengthen. See related #141

src/media_tools/dash_segmenter.c
@@ -108,6 +108,7 @@ struct _dash_segment_input
 	char *file_name;
 	char representationID[100];
 	char *periodID;
+	Double media_duration; /*forced media duration*/
 	char *xlink;
 	char *role;
 	u32 nb_rep_descs;
@@ -2767,10 +2768,79 @@ retry_track:
 	return e;
 }
 
+static GF_Err dasher_isom_adjust_last_sample(GF_ISOFile *in, const u32 trackNumber, const u32 track_duration, const u32 target_duration_in_timescale, const Double fragment_duration_in_sec) {
+	const u32 fragment_duration_in_media_timescale = (u32)(fragment_duration_in_sec * gf_isom_get_media_timescale(in, trackNumber));
+	const u32 last_sample_duration = gf_isom_get_sample_duration(in, trackNumber, gf_isom_get_sample_count(in, trackNumber));
+	const u32 new_last_sample_duration_in_media_timescale = last_sample_duration + (target_duration_in_timescale - track_duration) * (u64)gf_isom_get_media_timescale(in, trackNumber) / gf_isom_get_timescale(in);
+	if (new_last_sample_duration_in_media_timescale != last_sample_duration) {
+		if (new_last_sample_duration_in_media_timescale > fragment_duration_in_media_timescale) {
+			GF_LOG(GF_LOG_WARNING, GF_LOG_DASH, ("[DASH] Wrong user parameter would lead to set the last sample duration to %u while fragment duration is %u. Aborting.\n", new_last_sample_duration_in_media_timescale, fragment_duration_in_media_timescale));
+			return GF_BAD_PARAM;
+		} else {
+			return gf_isom_set_last_sample_duration(in, trackNumber, new_last_sample_duration_in_media_timescale);
+		}
+	} else {
+		return GF_OK;
+	}
+}
+
+static GF_Err dasher_isom_force_duration(GF_ISOFile *in, const Double duration_in_sec, const Double fragment_duration_in_sec) {
+	GF_Err e = GF_OK;
+
+	u32 trackNumber;
+	for (trackNumber=1; trackNumber<=gf_isom_get_track_count(in); trackNumber++) {
+		const u32 track_duration = (u32)gf_isom_get_track_duration(in, trackNumber);
+		const u32 target_duration_in_timescale = (u32)(duration_in_sec * gf_isom_get_timescale(in));
+
+		if (target_duration_in_timescale < track_duration) {
+			u32 i, j, track_duration2, sample_count = gf_isom_get_sample_count(in, trackNumber);
+			GF_LOG(GF_LOG_DEBUG, GF_LOG_DASH, ("[DASH] Track %u duration shorter than %lfs: removing samples and adjusting last sample duration.\n", trackNumber, duration_in_sec));
+			for (i=1; i <= sample_count; ++i) {
+				u32 di;
+				GF_ISOSample *s = gf_isom_get_sample(in, trackNumber, i, &di);
+				if (s->DTS >= duration_in_sec * gf_isom_get_media_timescale(in, trackNumber)) {
+					track_duration2 = (u32)(s->DTS * gf_isom_get_timescale(in) / gf_isom_get_media_timescale(in, trackNumber));
+					break;
+				}
+			}
+			for (j=i; j <= sample_count; ++j) {
+				u32 di;
+				GF_ISOSample *s = gf_isom_get_sample(in, trackNumber, i, &di);
+				e = gf_isom_remove_sample(in, trackNumber, i);
+				gf_isom_sample_del(&s);
+				assert(e == GF_OK);
+			}
+			e = dasher_isom_adjust_last_sample(in, trackNumber, track_duration2, target_duration_in_timescale, fragment_duration_in_sec);
+		} else if (target_duration_in_timescale > track_duration) {
+			GF_LOG(GF_LOG_DEBUG, GF_LOG_DASH, ("[DASH] Track %u duration longer than %lfs: increase the last sample duration.\n", trackNumber, duration_in_sec));
+			e = dasher_isom_adjust_last_sample(in, trackNumber, track_duration, target_duration_in_timescale, fragment_duration_in_sec);
+		} else {
+			GF_LOG(GF_LOG_DEBUG, GF_LOG_DASH, ("[DASH] Track %u duration already equal to %lfs. Nothing to do.\n", trackNumber, duration_in_sec));
+		}
+
+		if (e)
+			break;
+	}
+	
+	gf_isom_set_final_name(in, "");
+	return e;
+}
+
 static GF_Err dasher_isom_segment_file(GF_DashSegInput *dash_input, const char *szOutName, GF_DASHSegmenterOptions *dash_cfg, Bool first_in_set)
 {
-	GF_ISOFile *in = gf_isom_open(dash_input->file_name, GF_ISOM_OPEN_READ, dash_cfg->tmpdir);
-	GF_Err e = gf_media_isom_segment_file(in, szOutName, dash_cfg->fragment_duration, dash_cfg, dash_input, first_in_set);
+	GF_Err e;
+	GF_ISOFile *in = gf_isom_open(dash_input->file_name, GF_ISOM_OPEN_EDIT, dash_cfg->tmpdir);
+	
+	if (dash_input->media_duration) {
+		GF_LOG(GF_LOG_INFO, GF_LOG_DASH, ("[DASH] Forcing media duration to %lfs.\n", dash_input->media_duration));
+		e = dasher_isom_force_duration(in, dash_input->media_duration, dash_cfg->fragment_duration);
+		if (e) {
+			GF_LOG(GF_LOG_INFO, GF_LOG_DASH, ("[DASH] Media duration couldn't be forced. Aborting.\n"));
+			return e;
+		}
+	}
+
+	e = gf_media_isom_segment_file(in, szOutName, dash_cfg->fragment_duration, dash_cfg, dash_input, first_in_set);
 	gf_isom_close(in);
 	return e;
 }
@@ -3572,6 +3642,10 @@ static GF_Err dasher_mp2t_segment_file(GF_DashSegInput *dash_input, const char *
 	u32 segment_index;
 	/*compute name for indexed segments*/
 	const char *basename = gf_dasher_strip_output_dir(dash_cfg->mpd_name, szOutName);
+
+	if (dash_input->media_duration) {
+		GF_LOG(GF_LOG_WARNING, GF_LOG_DASH, ("[DASH] media duration cannot be forced with MPEG2-TS segmenter. Ignoring.\n"));
+	}
 
 	/*perform indexation of the file, this info will be destroyed at the end of the segment file routine*/
 	e = dasher_get_ts_demux(&ts_seg, dash_input->file_name, 0);
@@ -4953,6 +5027,7 @@ GF_Err gf_dasher_segment_files(const char *mpdfile, GF_DashSegmenterInput *input
 		if (inputs[i].representationID)
             strcpy(dash_inputs[j].representationID, inputs[i].representationID);
 		dash_inputs[j].periodID = inputs[i].periodID;
+		dash_inputs[j].media_duration = inputs[i].media_duration;
 		dash_inputs[j].xlink = inputs[i].xlink;
 		if (dash_inputs[j].xlink) uses_xlink = GF_TRUE;
 		dash_inputs[j].role = inputs[i].role;

applications/mp4box/main.c
@@ -314,11 +314,13 @@ void PrintDASHUsage()
 	        " \"#video\"           only uses the first video track from the source file\n"
 	        " \"#audio\"           only uses the first video track from the source file\n"
 	        " \":id=NAME\"         sets the representation ID to NAME\n"
+	        " \":dur=VALUE\"       processes VALUE seconds from the media\n"
+	        "                       If VALUE is longer than the media duration, the last media duration is lengthen.\n"
 	        " \":period=NAME\"     sets the representation's period to NAME. Multiple periods may be used\n"
 	        "                       period appear in the MPD in the same order as specified with this option\n"
 	        " \":bandwidth=VALUE\" sets the representation's bandwidth to a given value\n"
-			" \":duration=VALUE\"  Increases the duration of this period by the given duration in seconds\n"
-			"                       only used when no input media is specified (remote period insertion), eg :period=X:xlink=Z:duration=Y.\n"
+	        " \":period_duration=VALUE\"  increases the duration of this period by the given duration in seconds\n"
+	        "                       only used when no input media is specified (remote period insertion), eg :period=X:xlink=Z:duration=Y.\n"
 	        " \":xlink=VALUE\"     sets the xlink value for the period containing this element\n"
 	        "                       only the xlink declared on the first rep of a period will be used\n"
 	        " \":role=VALUE\"      sets the role of this representation (cf DASH spec).\n"
@@ -351,9 +353,9 @@ void PrintDASHUsage()
 	        " -cprt string         adds copyright string to MPD\n"
 	        " -dash-ctx FILE       stores/restore DASH timing from FILE.\n"
 	        " -dynamic             uses dynamic MPD type instead of static.\n"
-			" -last-dynamic        same as dynamic but closes the period (insert lmsg brand if needed and update duration).\n"
-			" -mpd-duration DUR    sets the duration in second of a live session (0 by default). If 0, you must use -mpd-refresh.\n"
-			" -mpd-refresh TIME    specifies MPD update time in seconds (double can be used).\n"
+	        " -last-dynamic        same as dynamic but closes the period (insert lmsg brand if needed and update duration).\n"
+	        " -mpd-duration DUR    sets the duration in second of a live session (0 by default). If 0, you must use -mpd-refresh.\n"
+	        " -mpd-refresh TIME    specifies MPD update time in seconds (double can be used).\n"
 	        " -time-shift  TIME    specifies MPD time shift buffer depth in seconds (default 0). Specify -1 to keep all files\n"
 	        " -subdur DUR          specifies maximum duration in ms of the input file to be dashed in LIVE or context mode.\n"
 	        "                       NOTE: This does not change the segment duration: dashing stops once segments produced exceeded the duration.\n"
@@ -380,7 +382,7 @@ void PrintDASHUsage()
 	        " -no-frags-default    disables default flags in fragments\n"
 	        " -single-traf         uses a single track fragment per moof (smooth streaming and derived specs may require this)\n"
 	        " -dash-ts-prog N      program_number to be considered in case of an MPTS input file.\n"
-			" -frag-rt             when using fragments in live mode, flush fragments according to their timing (only supported with a single input).\n"
+	        " -frag-rt             when using fragments in live mode, flush fragments according to their timing (only supported with a single input).\n"
 	        "\n");
 }
 
@@ -1507,12 +1509,13 @@ GF_DashSegmenterInput *set_dash_input(GF_DashSegmenterInput *dash_inputs, char *
 			while (sep) {
 				/* this is a real separator if it is followed by a keyword we are looking for */
 				if (!strnicmp(sep, ":id=", 4) ||
-				        !strnicmp(sep, ":period=", 8) ||
-				        !strnicmp(sep, ":bandwidth=", 11) ||
-				        !strnicmp(sep, ":role=", 6) ||
-				        !strnicmp(sep, ":desc", 5) ||
-				        !strnicmp(sep, ":duration=", 10) ||
-				        !strnicmp(sep, ":xlink=", 7)) {
+				    !strnicmp(sep, ":dur=", 5) ||
+				    !strnicmp(sep, ":period=", 8) ||
+				    !strnicmp(sep, ":bandwidth=", 11) ||
+				    !strnicmp(sep, ":role=", 6) ||
+				    !strnicmp(sep, ":desc", 5) ||
+				    !strnicmp(sep, ":duration=", 10) || /*legacy*/!strnicmp(sep, ":period_duration=", 10) ||
+				    !strnicmp(sep, ":xlink=", 7)) {
 					break;
 				} else {
 					sep = strchr(sep+1, ':');
@@ -1532,7 +1535,8 @@ GF_DashSegmenterInput *set_dash_input(GF_DashSegmenterInput *dash_inputs, char *
 						GF_LOG(GF_LOG_ERROR, GF_LOG_DASH, ("[DASH] Error: Duplicate Representation ID \"%s\" in command line\n", di->representationID));
 					}
 				}
-			} else if (!strnicmp(opts, "period=", 7)) di->periodID = gf_strdup(opts+7);
+			} else if (!strnicmp(opts, "dur=", 4)) di->media_duration = (Double)atof(opts+4);
+			else if (!strnicmp(opts, "period=", 7)) di->periodID = gf_strdup(opts+7);
 			else if (!strnicmp(opts, "bandwidth=", 10)) di->bandwidth = atoi(opts+10);
 			else if (!strnicmp(opts, "role=", 5)) di->role = gf_strdup(opts+5);
 			else if (!strnicmp(opts, "desc", 4)) {
@@ -1569,8 +1573,10 @@ GF_DashSegmenterInput *set_dash_input(GF_DashSegmenterInput *dash_inputs, char *
 
 			}
 			else if (!strnicmp(opts, "xlink=", 6)) di->xlink = gf_strdup(opts+6);
-			else if (!strnicmp(opts, "duration=", 9)) {
-				di->period_duration = (Double) atof(opts+9);
+			else if (!strnicmp(opts, "period_duration=", 16)) {
+				di->period_duration = (Double) atof(opts+16);
+			}	else if (!strnicmp(opts, "duration=", 9)) {
+				di->period_duration = (Double) atof(opts+9); /*legacy: use period_duration instead*/
 			}
 
 			if (!sep) break;

include/gpac/media_tools.h
@@ -295,6 +295,7 @@ typedef struct
 	char *file_name;
 	char *representationID;
 	char *periodID;
+	Double media_duration;
 	char *xlink;
 	char *role;
 	u32 nb_rep_descs;

commit 2c6e897dec99353a452e24ffaa59834b8d4aa30f

Fixed broken isom_close introduced in e10994e

src/media_tools/dash_segmenter.c
@@ -2968,8 +2968,8 @@ static GF_Err dasher_isom_force_duration(GF_ISOFile *in, const Double duration_i
 static GF_Err dasher_isom_segment_file(GF_DashSegInput *dash_input, const char *szOutName, GF_DASHSegmenter *dash_cfg, Bool first_in_set)
 {
 	GF_Err e = GF_OK;
-	GF_ISOFile *in = gf_isom_open(dash_input->file_name, GF_ISOM_OPEN_EDIT, dash_cfg->tmpdir);
-	
+	GF_ISOFile *in = gf_isom_open(dash_input->file_name, dash_input->media_duration ? GF_ISOM_OPEN_EDIT : GF_ISOM_OPEN_READ, dash_cfg->tmpdir);
+
 	if (!gf_isom_get_track_count(in))
 		goto exit;
 
@@ -2985,7 +2985,8 @@ static GF_Err dasher_isom_segment_file(GF_DashSegInput *dash_input, const char *
 	e = gf_media_isom_segment_file(in, szOutName, dash_cfg, dash_input, first_in_set);
 
 exit:
-	gf_isom_close(in);
+	//we don't want to save any modif due to duration adjustments
+	gf_isom_delete(in);
 	return e;
 }
 