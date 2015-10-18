84266a47e7beaa59143bf7e910ecbfed9b9686bd
jeanlf <jeanlf@users.sourceforge.net>
2015-10-18 18:35:33 +0200

    Added regression tests browsing from gui

---
+++ gui/extensions/bifs_tests/init.js
@@ -0,0 +1,16 @@
+
+my_ext = {
+    whoami: "GPAC-GUI-Extension",
+    name: "BIFS Tests",
+    icon: "",
+    author: "JeanLF",
+    description: "GPAC BIFS Regression Tests",
+    url: "http://gpac.io/",
+    execjs: ["bifs_tests.js"],
+    autostart: false,
+    config_id: "",
+    requires_gl: false,
+    version_major: 1,
+    version_minor: 0
+};
+
