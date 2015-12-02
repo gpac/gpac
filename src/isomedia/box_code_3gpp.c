7ed3f5341e58daed255b891536ee081dc49225d8
jeanlf <jeanlf@users.sf.net>
2015-12-02 18:26:21 +0100

    Fixed bug with corrupted ftab boxes - cf #337

--- src/isomedia/box_code_3gpp.c
+++ src/isomedia/box_code_3gpp.c
@@ -264,7 +264,14 @@ GF_Err ftab_Read(GF_Box *s, GF_BitStream *bs)
 	u32 i;
 	GF_FontTableBox *ptr = (GF_FontTableBox *)s;
 	ptr->entry_count = gf_bs_read_u16(bs);
+	ptr->size -= 2;
+	if (ptr->size<ptr->entry_count*3) {
+		GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[iso file] Corrupted ftap box, skipping\n"));
+		ptr->entry_count = 0;
+		return GF_OK;
+	}
 	ptr->fonts = (GF_FontRecord *) gf_malloc(sizeof(GF_FontRecord)*ptr->entry_count);
+	memset(ptr->fonts, 0, sizeof(GF_FontRecord)*ptr->entry_count);
 	for (i=0; i<ptr->entry_count; i++) {
 		u32 len;
 		ptr->fonts[i].fontID = gf_bs_read_u16(bs);
