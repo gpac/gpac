a1ea75bf76f0057ac8c2c4888bacae378a1b1cda
jeanlf <jeanlf@users.sf.net>
2015-12-11 07:42:49 +0100

    Fixed windows compilation for GL element array

--- src/compositor/gl_inc.h
+++ src/compositor/gl_inc.h
@@ -335,6 +335,7 @@ GLDECL(void, glPointParameterfv, (GLenum, const GLfloat *) )
 #endif
 
 #define GL_ARRAY_BUFFER	0x8892
+#define GL_ELEMENT_ARRAY_BUFFER	0x8893
 #define GL_STREAM_DRAW	0x88E0
 #define GL_STATIC_DRAW	0x88E4
 #define GL_DYNAMIC_DRAW 0x88E8
