47a3328d037e3607036790837294fe6b5b36d79c
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2016-01-03 18:51:33 +0100

    improve HE-AAC display with -info #360 CC @jeanlf

--- applications/mp4box/filedump.c
+++ applications/mp4box/filedump.c
@@ -2025,7 +2025,12 @@ void DumpTrackInfo(GF_ISOFile *file, u32 trackID, Bool full_dump)
 					if (full_dump) fprintf(stderr, "\t");
 					if (e) fprintf(stderr, "Corrupted AAC Config\n");
 					else {
-						fprintf(stderr, "%s - %d Channel(s) - SampleRate %d", gf_m4a_object_type_name(a_cfg.base_object_type), a_cfg.nb_chan, a_cfg.base_sr);
+						char *heaac = "";
+						if (!is_mp2 && a_cfg.has_sbr) {
+							if (a_cfg.has_ps) heaac = "(HE-AAC v2) ";
+							else heaac = "(HE-AAC v1) ";
+						}
+						fprintf(stderr, "%s %s- %d Channel(s) - SampleRate %d", gf_m4a_object_type_name(a_cfg.base_object_type), heaac, a_cfg.nb_chan, a_cfg.base_sr);
 						if (is_mp2) fprintf(stderr, " (MPEG-2 Signaling)");
 						if (a_cfg.has_sbr) fprintf(stderr, " - SBR: SampleRate %d Type %s", a_cfg.sbr_sr, gf_m4a_object_type_name(a_cfg.sbr_object_type));
 						if (a_cfg.has_ps) fprintf(stderr, " - PS");
