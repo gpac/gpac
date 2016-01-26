d31463e40d4d053fdeafbabe8191604ef0931f5a
jeanlf <jeanlf@users.sourceforge.net>
2016-01-26 17:16:52 +0100

    Added warning message when moov is not before moof

--- src/isomedia/isom_intern.c
+++ src/isomedia/isom_intern.c
@@ -277,6 +277,9 @@ GF_Err gf_isom_parse_movie_boxes(GF_ISOFile *mov, u64 *bytesMissing, Bool progre
 			break;
 
 		case GF_ISOM_BOX_TYPE_MOOF:
+			if (!mov->moov) {
+				GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[iso file] Movie fragment but no moov (yet) - possibly broken parsing!\n"));
+			}
 			if (mov->single_moof_mode) {
 				mov->single_moof_state++;
 				if (mov->single_moof_state > 1) {
