565ccb50077067bb0faf24a2c4e9a9c2b2baece7
nguyen-viet-thanh-trung <viet-thanh-trung.nguyen@telecom-paristech.fr>
2016-02-22 16:33:36 +0100

    get and dump operating points information

--- src/export.cpp
+++ src/export.cpp
@@ -834,6 +834,7 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_adobe_protection_info) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_sample_group_info) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_tile_info) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_oinf_info) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_udta_count) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_udta_type) )
 
