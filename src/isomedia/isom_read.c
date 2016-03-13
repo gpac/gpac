16907d0f70037afef90549e44288ecbd7efe35e8
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2016-03-13 09:20:06 +0100

    robustify

--- src/isomedia/isom_read.c
+++ src/isomedia/isom_read.c
@@ -395,7 +395,7 @@ u32 gf_isom_get_track_count(GF_ISOFile *movie)
 {
 	if (!movie || !movie->moov) return 0;
 
-	if (! movie->moov->trackList) {
+	if (!movie->moov->trackList) {
 		movie->LastError = GF_ISOM_INVALID_FILE;
 		return 0;
 	}
