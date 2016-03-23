fa63d15f18fdadf45a8d0ea0a05643e18216c9d8
cconcolato <cyril.concolato@telecom-paristech.fr>
2016-03-23 11:12:56 +0100

    fix -dnal on HEVC tile tracks

--- src/isomedia/avc_ext.c
+++ src/isomedia/avc_ext.c
@@ -1480,6 +1480,13 @@ GF_HEVCConfig *gf_isom_hevc_config_get(GF_ISOFile *the_file, u32 trackNumber, u3
 {
 	GF_TrackBox *trak;
 	GF_MPEGVisualSampleEntryBox *entry;
+	if (gf_isom_get_reference_count(the_file, trackNumber, GF_ISOM_REF_TBAS)) {
+		u32 ref_track;
+		GF_Err e = gf_isom_get_reference(the_file, trackNumber, GF_ISOM_REF_TBAS, 1, &ref_track);
+		if (e == GF_OK) {
+			trackNumber = ref_track;
+		}
+	}
 	trak = gf_isom_get_track_from_file(the_file, trackNumber);
 	if (!trak || !trak->Media || !DescriptionIndex) return NULL;
 	if (gf_isom_get_hevc_shvc_type(the_file, trackNumber, DescriptionIndex)==GF_ISOM_HEVCTYPE_NONE)
