797a61a9726327687e271f9be9c5358c8362046c
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2016-03-30 14:09:46 +0200

    add missing parsing for piff_psec and remove a hack for some ultraviolet files tested with http://playready.directtaps.net/smoothstreaming/sintel_trailer.adap.multiaudio.enc.smooth/sintel_trailer.ism/manifest and http://playready.directtaps.net/smoothstreaming/tears_of_steel.EE4.HD.adap.piff.enc/tears_of_steel.ism/manifest and http://playready.directtaps.net/smoothstreaming/SSWSS720H264PR/SuperSpeedway_720.ism/Manifest CC @jeanlf

--- src/isomedia/box_code_drm.c
+++ src/isomedia/box_code_drm.c
@@ -1246,6 +1246,34 @@ GF_Err piff_psec_Read(GF_Box *s, GF_BitStream *bs)
 		}
 		gf_list_add(ptr->samp_aux_info, sai);
 	}
+	if (ptr->IV_size == 0)
+		ptr->IV_size = 8; //default to 8
+
+	sample_count = gf_bs_read_u32(bs);
+	ptr->size -= 4;
+	if (ptr->IV_size != 8 && ptr->IV_size != 16) {
+		GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[iso file] PIFF PSEC box incorrect IV size: %u - shall be 8 or 16\n", ptr->IV_size));
+		return GF_BAD_PARAM;
+	}
+
+	ptr->samp_aux_info = gf_list_new();
+	for (i=0; i<sample_count; ++i) {
+		GF_CENCSampleAuxInfo *sai;
+		GF_SAFEALLOC(sai, GF_CENCSampleAuxInfo);
+		sai->IV_size = ptr->IV_size;
+		gf_bs_read_data(bs, sai->IV, ptr->IV_size);
+		ptr->size -= ptr->IV_size;
+		if (ptr->flags & 2) {
+			sai->subsample_count = gf_bs_read_u16(bs);
+			sai->subsamples = gf_malloc(sai->subsample_count*sizeof(GF_CENCSubSampleEntry));
+			for (j = 0; j < sai->subsample_count; ++j) {
+				sai->subsamples[j].bytes_clear_data = gf_bs_read_u16(bs);
+				sai->subsamples[j].bytes_encrypted_data = gf_bs_read_u32(bs);
+			}
+			ptr->size -= 2+sai->subsample_count*6;
+		}
+		gf_list_add(ptr->samp_aux_info, sai);
+	}
 
 	ptr->bs_offset = gf_bs_get_position(bs);
 	assert(ptr->size == 0);
