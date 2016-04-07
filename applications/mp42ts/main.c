42e25cac58a2ba3600ab20dbca43a2c5ba2300a3
jeanlf <jeanlf@users.sourceforge.net>
2016-04-07 09:47:51 +0200

    Force log level when using -mem-track

--- applications/mp42ts/main.c
+++ applications/mp42ts/main.c
@@ -2818,6 +2818,7 @@ exit:
 
 #ifdef GPAC_MEMORY_TRACKING
 	if (enable_mem_tracker && (gf_memory_size() || gf_file_handles_count() )) {
+        gf_log_set_tool_level(GF_LOG_MEMORY, GF_LOG_INFO);
 		gf_memory_print();
 		return 2;
 	}
