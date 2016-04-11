e77ac4dbe0e0afc6253e959c95b38d1c09488b73
jeanlf <jeanlf@users.sourceforge.net>
2016-04-11 15:36:05 +0200

    Fixed mem leak in BIFS storage node

--- include/gpac/internal/scenegraph_dev.h
+++ include/gpac/internal/scenegraph_dev.h
@@ -376,6 +376,7 @@ void gf_sg_sfstring_del(SFString par);
 void gf_sg_mfscript_del(MFScript sc);
 void gf_sg_sfcommand_del(SFCommandBuffer cb);
 void gf_sg_sfurl_del(SFURL url);
+	void gf_sg_mfattrref_del(MFAttrRef par);
 
 Bool gf_sg_vrml_node_init(GF_Node *node);
 Bool gf_sg_vrml_node_changed(GF_Node *node, GF_FieldInfo *field);
