eedbc08dd1362da2b58a0d871998daeec3350ecf
jeanlf <jeanlf@users.sourceforge.net>
2016-04-27 12:49:25 +0200

    Fixed encoding of anim in laser

--- src/laser/lsr_enc.c
+++ src/laser/lsr_enc.c
@@ -1516,7 +1516,12 @@ static void lsr_write_rare(GF_LASeRCodec *lsr, GF_Node *n)
 		case TAG_SVG_ATT_transform:
 			lsr_write_matrix(lsr, (SVG_Transform*)att->data);
 			break;
-
+		case TAG_SVG_ATT_focusable:
+			GF_LSR_WRITE_INT(lsr, *(SVG_Focusable*)att->data, 2, "focusable");
+			break;
+		default:
+			GF_LOG(GF_LOG_ERROR, GF_LOG_CODING, ("[LASeR Enc] Rare Field declared but not encoded\n"));
+			break;
 		}
 		att = att->next;
 	}
@@ -1763,8 +1768,13 @@ static void lsr_write_an_anim_value(GF_LASeRCodec *lsr, void *val, u32 lsr_type,
 		{
 			SVG_Number  *n = (SVG_Number *) val;
 			if (n->type != SVG_NUMBER_VALUE) {
+				u8 val = 0;
+				if (n->type==SVG_NUMBER_INHERIT) val=1;
+				/*fixe me spec is not clear here regarding what values should be used ...*/
+				
 				GF_LSR_WRITE_INT(lsr, 1, 1, "escapeFlag");
-				GF_LSR_WRITE_INT(lsr, n->type, 2, "escapeEnum");
+				GF_LSR_WRITE_INT(lsr, val, 2, "escapeEnum");
+				return;
 			} else {
 				GF_LSR_WRITE_INT(lsr, 0, 1, "escapeFlag");
 			}
