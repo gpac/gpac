43a1e352582bb4b4edd98163025100d27029e01c
jeanlf <jeanlf@users.sourceforge.net>
2016-04-27 21:19:13 +0200

    avoid sto/start texture when unchanged in svg image

--- src/compositor/svg_media.c
+++ src/compositor/svg_media.c
@@ -28,6 +28,7 @@
 #ifndef GPAC_DISABLE_SVG
 #include "nodes_stacks.h"
 
+#include <gpac/internal/terminal_dev.h>
 
 static void svg_audio_smil_evaluate_ex(SMIL_Timing_RTI *rti, Fixed normalized_scene_time, u32 status, GF_Node *audio, GF_Node *video);
 static void svg_traverse_audio_ex(GF_Node *node, void *rs, Bool is_destroy, SVGPropertiesPointers *props);
@@ -287,18 +288,21 @@ static void svg_traverse_bitmap(GF_Node *node, void *rs, Bool is_destroy)
 		return;
 
 	if (gf_node_dirty_get(node) & GF_SG_SVG_XLINK_HREF_DIRTY) {
-		gf_term_get_mfurl_from_xlink(node, &stack->txurl);
-		stack->txh.width = stack->txh.height = 0;
+		if (!stack->txh.stream || gf_mo_url_changed(stack->txh.stream, &stack->txurl)) {
 
-		/*remove associated audio if any*/
-		if (stack->audio) {
-			svg_audio_smil_evaluate_ex(NULL, 0, SMIL_TIMING_EVAL_REMOVE, stack->audio, stack->txh.owner);
-			gf_node_unregister(stack->audio, NULL);
-			stack->audio = NULL;
+			gf_term_get_mfurl_from_xlink(node, &stack->txurl);
+			stack->txh.width = stack->txh.height = 0;
+
+			/*remove associated audio if any*/
+			if (stack->audio) {
+				svg_audio_smil_evaluate_ex(NULL, 0, SMIL_TIMING_EVAL_REMOVE, stack->audio, stack->txh.owner);
+				gf_node_unregister(stack->audio, NULL);
+				stack->audio = NULL;
+			}
+			stack->audio_dirty = GF_TRUE;
+
+			if (stack->txurl.count) svg_play_texture(stack, &all_atts);
 		}
-		stack->audio_dirty = GF_TRUE;
-
-		if (stack->txurl.count) svg_play_texture(stack, &all_atts);
 		gf_node_dirty_clear(node, GF_SG_SVG_XLINK_HREF_DIRTY);
 	}
 
@@ -408,7 +412,7 @@ static void SVG_Update_image(GF_TextureHandler *txh)
 
 	gf_sc_texture_update_frame(txh, GF_FALSE);
 	/*URL is present but not opened - redraw till fetch*/
-	if (txh->stream && (!txh->tx_io || txh->needs_refresh) ) {
+	if (txh->stream && !txh->stream_finished && (!txh->tx_io || txh->needs_refresh) ) {
 		/*mark all subtrees using this image as dirty*/
 		gf_node_dirty_parents(txh->owner);
 		gf_sc_invalidate(txh->compositor, NULL);
