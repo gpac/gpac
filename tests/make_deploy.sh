84bfaeaefa20c11d6c9e3f3975de32d7a388e1d3
nguyen-viet-thanh-trung <viet-thanh-trung.nguyen@telecom-paristech.fr>
2016-05-03 16:17:01 +0200

    with each platform using a repo for test result

--- tests/make_deploy.sh
+++ tests/make_deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ -z "$GH_TOKEN" ]; then
+    echo 'GH_TOKEN is not defined, do nothing'
+    exit 0
+fi
+
 set -e # exit with nonzero exit code if anything fails
 
 cp -f stylesheet.xsl index.html results
@@ -20,4 +26,20 @@ git commit -q -m "Deploy to GitHub Pages"
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+if grep "platform=\"Linux\"" all_results.xml > /dev/null; then
+    if [ -z "$GH_REF_LINUX" ]; then
+        echo 'GH_REF_LINUX is not defined, do nothing'
+        exit 0
+    fi
+    echo "git push --force --quiet "https://${GH_TOKEN}@${GH_REF_LINUX}" master:gh-pages"
+    #git push --force --quiet "https://${GH_TOKEN}@${GH_REF_LINUX}" master:gh-pages > /dev/null 2>&1
+elif grep "platform=\"Darwin\"" all_results.xml > /dev/null; then
+   if [ -z "$GH_REF_MACOS" ]; then
+        echo 'GH_REF_MACOS is not defined, do nothing'
+        exit 0
+    fi
+    echo "git push --force --quiet "https://${GH_TOKEN}@${GH_REF_MACOS}" master:gh-pages"
+    #git push --force --quiet "https://${GH_TOKEN}@${GH_REF_MACOS}" master:gh-pages > /dev/null 2>&1 
+else
+    echo 'platform unknown, do nothing'
+fi
