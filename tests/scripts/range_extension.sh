07caaa43f1c9319ab886aec1698e92f31d1190d1
jeljeli <ahmedjeljeli@gmail.com>
2016-05-31 12:09:41 +0200

    add hash test

--- tests/scripts/range_extension.sh
+++ tests/scripts/range_extension.sh
@@ -1,47 +1,32 @@
 #!/bin/sh
 
-test_begin "avc-range-extension"
+temp_file=$TEMP_DIR/test.mp4
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_420.h264 -new $TEMP_DIR/counter-bifs-10min-320x180-bipbop_420.mp4" "import-avc-420-8bit"
-do_playback_test "$TEMP_DIR/counter-bifs-10min-320x180-bipbop_420.mp4" "play-avc-420-8bit"
+rext_test() {
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_422.h264 -new $TEMP_DIR/counter-bifs-10min-320x180-bipbop_422.mp4" "import-avc-422-8bit"
-do_playback_test "$TEMP_DIR/counter-bifs-10min-320x180-bipbop_422.mp4" "play-avc-422-8bit"
+test_begin $1 "import" "play"
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_444.h264 -new $TEMP_DIR/counter-bifs-10min-320x180-bipbop_444.mp4" "import-avc-444-8bit"
-do_playback_test "$TEMP_DIR/counter-bifs-10min-320x180-bipbop_444.mp4" "play-avc-444-8bit"
+do_test "$MP4BOX -add $2 -new $temp_file" "import"
+do_hash_test "$temp_file" "import"
 
-
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_420_10.h264 -new $TEMP_DIR/counter-bifs-10min-320x180-bipbop_420_10.mp4" "import-avc-420-10bit"
-do_playback_test "$TEMP_DIR/counter-bifs-10min-320x180-bipbop_420_10.mp4" "play-avc-420-10bit"
-
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_422_10.h264 -new $TEMP_DIR/counter-bifs-10min-320x180-bipbop_422_10.mp4" "import-avc-422-10bit"
-do_playback_test "$TEMP_DIR/counter-bifs-10min-320x180-bipbop_422_10.mp4" "play-avc-422-10bit"
-
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_444_10.h264 -new $TEMP_DIR/counter-bifs-10min-320x180-bipbop_444_10.mp4" "import-avc-444-10bit"
-do_playback_test "$TEMP_DIR/counter-bifs-10min-320x180-bipbop_444_10.mp4" "play-avc-444-10bit"
+do_playback_test "$temp_file" "play"
 
 test_end
+}
 
-test_begin "hevc-range-extension"
+#avc range extension
+rext_test "avc-420-8bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_420.h264"
+rext_test "avc-422-8bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_422.h264"
+rext_test "avc-444-8bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_444.h264"
+rext_test "avc-420-10bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_420_10.h264"
+rext_test "avc-422-10bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_422_10.h264"
+rext_test "avc-444-10bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-10min-320x180-bipbop_444_10.h264"
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_420.hvc:fmt=HEVC -new $TEMP_DIR/counter-bifs-1min-640x360-bipbop_420.mp4" "import-hevc-420-8bit"
-do_playback_test "$TEMP_DIR/counter-bifs-1min-640x360-bipbop_420.mp4" "play-hevc-420-8bit"
+#hevc range extension
+rext_test "hevc-420-8bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_420.hvc"
+rext_test "hevc-422-8bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_422.hvc"
+rext_test "hevc-444-8bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_444.hvc"
+rext_test "hevc-420-10bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_420_10.hvc"
+rext_test "hevc-422-10bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_422_10.hvc"
+rext_test "hevc-444-10bit" "$EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_444_10.hvc"
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_422.hvc:fmt=HEVC -new $TEMP_DIR/counter-bifs-1min-640x360-bipbop_422.mp4" "import-hevc-422-8bit"
-do_playback_test "$TEMP_DIR/counter-bifs-1min-640x360-bipbop_422.mp4" "play-hevc-422-8bit"
-
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_444.hvc:fmt=HEVC -new $TEMP_DIR/counter-bifs-1min-640x360-bipbop_444.mp4" "import-hevc-444-8bit"
-do_playback_test "$TEMP_DIR/counter-bifs-1min-640x360-bipbop_444.mp4" "play-hevc-444-8bit"
-
-
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_420_10.hvc:fmt=HEVC -new $TEMP_DIR/counter-bifs-1min-640x360-bipbop_420_10.mp4" "import-hevc-420-10bit"
-do_playback_test "$TEMP_DIR/counter-bifs-1min-640x360-bipbop_420_10.mp4" "play-hevc-420-10bit"
-
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_422_10.hvc:fmt=HEVC -new $TEMP_DIR/counter-bifs-1min-640x360-bipbop_422_10.mp4" "import-hevc-422-10bit"
-do_playback_test "$TEMP_DIR/counter-bifs-1min-640x360-bipbop_422_10.mp4" "play-hevc-422-10bit"
-
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/rext/counter-bifs-1min-640x360-bipbop_444_10.hvc:fmt=HEVC -new $TEMP_DIR/counter-bifs-1min-640x360-bipbop_444_10.mp4" "import-hevc-444-10bit"
-do_playback_test "$TEMP_DIR/counter-bifs-1min-640x360-bipbop_444_10.mp4" "play-hevc-444-10bit"
-
-test_end
