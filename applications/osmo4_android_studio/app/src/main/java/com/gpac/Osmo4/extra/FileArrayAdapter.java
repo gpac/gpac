83d310e515746ed728b16f4b4e776fc6d3046832
Abdul Rauf <abdul3rauf@gmail.com>
2016-07-19 16:20:25 +0530

    ui changes

--- applications/osmo4_android_studio/app/src/main/java/com/gpac/Osmo4/extra/FileArrayAdapter.java
+++ applications/osmo4_android_studio/app/src/main/java/com/gpac/Osmo4/extra/FileArrayAdapter.java
@@ -1,76 +1,144 @@
-/**
- * $URL$
- *
- * $LastChangedBy$ - $LastChangedDate$
- */
 package com.gpac.Osmo4.extra;
 
-import java.io.File;
-import java.util.List;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.gpac.Osmo4.R;
 
-/**
- * Class that contains the list of file for given directory
- * 
- * @version $Revision$
- * 
- */
-public class FileArrayAdapter extends ArrayAdapter<FileEntry> {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
 
-    private final Context context;
 
-    private final int id;
+public class FileArrayAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private final List<FileEntry> items;
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
 
-    /**
-     * Constructor
-     * 
-     * @param context
-     * @param textViewResourceId
-     * @param objects
-     */
-    public FileArrayAdapter(Context context, int textViewResourceId, List<FileEntry> objects) {
-        super(context, textViewResourceId, objects);
+    private ArrayList<File> files;
+    private OnItemClickListener onItemClickListener;
+    private Context context;
+
+    public FileArrayAdapter(ArrayList<File> files, OnItemClickListener onItemClickListener, Context context) {
+        this.files = files;
+        this.onItemClickListener = onItemClickListener;
         this.context = context;
-        id = textViewResourceId;
-        items = objects;
     }
 
     @Override
-    public FileEntry getItem(int i) {
-        return items.get(i);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ListItemViewHolder(
+                        LayoutInflater
+                                .from(parent.getContext())
+                                .inflate(R.layout.list_item_file_chooser, parent, false));
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View v = convertView;
-        if (v == null) {
-            LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            v = vi.inflate(id, null);
-        }
-        final FileEntry o = items.get(position);
-        if (o != null) {
-            TextView t1 = (TextView) v.findViewById(R.id.TextView01);
-            TextView t2 = (TextView) v.findViewById(R.id.TextView02);
+    public void onBindViewHolder(final RecyclerView.ViewHolder viewHolder, final int position) {
 
-            if (t1 != null)
-                t1.setText(o.getName());
-            if (t2 != null) {
-                File f = o.getFile();
-                if (f.isDirectory())
-                    t2.setText(context.getString(R.string.directory));
-                else
-                    t2.setText(context.getString(R.string.fileSize, f.length()));
+        ListItemViewHolder holder = (ListItemViewHolder) viewHolder;
+        File file = getItem(position);
+        holder.title.setText(file.getName());
+        holder.lastModified.setText(new Date(file.lastModified()).toString());
+        setIcon(file, holder);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onItemClickListener.onItemClick(v, position);
             }
-        }
-        return v;
+        });
     }
 
+    public void setIcon(File file, ListItemViewHolder holder) {
+
+        String extension;
+        Drawable drawable = null;
+
+        try {
+            extension = FileManager.getExtension(file.getAbsolutePath());
+
+            if (file.isFile()) {
+                switch (extension) {
+
+                    case ".c":  case ".cpp":    case ".doc":    case ".docx":   case ".exe":
+                    case ".h":  case ".html":   case ".java":   case ".log":    case ".txt":
+                    case ".pdf":    case ".ppt":    case ".xls":
+                        drawable = ContextCompat.getDrawable(context, R.drawable.ic_file);
+                        break;
+
+                    case ".3ga":    case ".aac":    case ".mp3":    case ".m4a":    case ".ogg":
+                    case ".wav":    case ".wma":
+                        drawable = ContextCompat.getDrawable(context, R.drawable.ic_audio);
+                        break;
+
+                    case ".3gp":    case ".avi":    case ".mpg":    case ".mpeg":   case ".mp4":
+                    case ".mkv":    case ".webm":   case ".wmv":    case ".vob":
+                        drawable = ContextCompat.getDrawable(context, R.drawable.ic_video);
+                        break;
+
+                    case ".ai": case ".bmp":    case ".exif":   case ".gif":    case ".jpg":
+                    case ".jpeg":   case ".png":    case ".svg":
+                        drawable = ContextCompat.getDrawable(context, R.drawable.ic_image);
+                        break;
+
+                    case ".rar":    case ".zip":    case ".ZIP":
+                        drawable = ContextCompat.getDrawable(context, R.drawable.ic_compressed);
+                        break;
+
+                    default:
+                        drawable = ContextCompat.getDrawable(context, R.drawable.ic_error);
+                        break;
+                }
+            } else if (file.isDirectory()) {
+                drawable = ContextCompat.getDrawable(context, R.drawable.ic_folder);
+            } else drawable = ContextCompat.getDrawable(context, R.drawable.ic_error);
+
+        } catch (Exception e) {
+            drawable = ContextCompat.getDrawable(context, R.drawable.ic_error);
+        }
+        drawable = DrawableCompat.wrap(drawable);
+        holder.icon.setImageDrawable(drawable);
+    }
+
+    @Override
+    public int getItemCount() {
+        return files.size();
+    }
+
+    public File getItem(int position) {
+        return files.get(position);
+    }
+
+    static class ListItemViewHolder extends RecyclerView.ViewHolder {
+
+        CardView cardView;
+        TextView title;
+        TextView lastModified;
+        ImageView icon;
+        LinearLayout linearLayout;
+
+        public ListItemViewHolder(View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView.findViewById(R.id.cardView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            icon = (ImageView) itemView.findViewById(R.id.icon);
+            linearLayout = (LinearLayout) itemView.findViewById(R.id.linearLayout);
+            lastModified = (TextView) itemView.findViewById(R.id.lastModified);
+        }
+    }
+
+    public ArrayList<File> getFiles() {
+        return files;
+    }
 }
