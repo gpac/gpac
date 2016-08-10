fe718283478ca0bc42343347744def61c92cedeb
Abdul Rauf <abdul3rauf@gmail.com>
2016-08-10 18:54:18 +0530

    preference page

--- applications/osmo4_android_studio/app/src/main/java/com/gpac/Osmo4/extra/FileChooserActivity.java
+++ applications/osmo4_android_studio/app/src/main/java/com/gpac/Osmo4/extra/FileChooserActivity.java
@@ -86,8 +86,6 @@ public class FileChooserActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
