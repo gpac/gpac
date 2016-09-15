62b398e14f6d8deb2798acc19a934135bf83b682
jeanlf <jeanlf@users.sourceforge.net>
2016-09-15 18:00:19 +0200

    Added VPS extension removal flag in mp4box

--- applications/mp4box/main.c
+++ applications/mp4box/main.c
@@ -518,6 +518,7 @@ void PrintImportUsage()
 	        " \":fmt=FORMAT\"        overrides format detection with given format (cf BT/XMTA doc)\n"
 	        " \":profile=INT\"       overrides AVC profile\n"
 	        " \":level=INT\"         overrides AVC level\n"
+	        " \":novpsext\"          removes VPS extensions from HEVC VPS\n"
 
 	        " \":font=name\"         specifies font name for text import (default \"Serif\")\n"
 	        " \":size=s\"            specifies font size for text import (default 18)\n"
