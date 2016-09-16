2ff1987a697e672d83944f668a4fbff5bca4f2a0
jeanlf <jeanlf@users.sourceforge.net>
2016-09-16 09:17:56 +0200

    Updated HEVC tile tracks to final spec text

--- src/isomedia/isom_read.c
+++ src/isomedia/isom_read.c
@@ -3888,15 +3888,22 @@ void gf_isom_parse_trif_info(const char *data, u32 size, u32 *id, u32 *independe
 	GF_BitStream *bs;
 	bs = gf_bs_new(data, size, GF_BITSTREAM_READ);
 	*id = gf_bs_read_u16(bs);
-	*independent = gf_bs_read_int(bs, 2);
-	*full_picture = (Bool)gf_bs_read_int(bs, 1);
-	gf_bs_read_int(bs, 5);
-	*x = *full_picture ? 0 : gf_bs_read_u16(bs);
-	*y = *full_picture ? 0 : gf_bs_read_u16(bs);
-	*w = gf_bs_read_u16(bs);
-	*h = gf_bs_read_u16(bs);
+	if (! gf_bs_read_int(bs, 1)) {
+		*independent=0;
+		*full_picture=0;
+		*x = *y = *w = *h = 0;
+	} else {
+		*independent = gf_bs_read_int(bs, 2);
+		*full_picture = (Bool)gf_bs_read_int(bs, 1);
+		/*filter_disabled*/ gf_bs_read_int(bs, 1);
+		/*has_dependency_list*/ gf_bs_read_int(bs, 1);
+		gf_bs_read_int(bs, 2);
+		*x = *full_picture ? 0 : gf_bs_read_u16(bs);
+		*y = *full_picture ? 0 : gf_bs_read_u16(bs);
+		*w = gf_bs_read_u16(bs);
+		*h = gf_bs_read_u16(bs);
+	}
 	gf_bs_del(bs);
-
 }
 
 GF_EXPORT
