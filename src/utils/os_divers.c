2143a1dd4b9fbb6b9d4ff0619f720f5fd98784d1
jeanlf <jeanlf@users.sourceforge.net>
2016-11-15 16:40:34 +0100

    Fixed crash in ntp partial query - cf #686

--- src/utils/os_divers.c
+++ src/utils/os_divers.c
@@ -1671,10 +1671,15 @@ void gf_net_get_ntp(u32 *sec, u32 *frac)
 	u64 frac_part;
 	struct timeval now;
 	gettimeofday(&now, NULL);
-	*sec = (u32) (now.tv_sec) + ntp_shift;
-	frac_part = now.tv_usec * 0xFFFFFFFFULL;
-	frac_part /= 1000000;
-	*frac = (u32) ( frac_part );
+	if (sec) {
+		*sec = (u32) (now.tv_sec) + ntp_shift;
+	}
+	
+	if (frac) {
+		frac_part = now.tv_usec * 0xFFFFFFFFULL;
+		frac_part /= 1000000;
+		*frac = (u32) ( frac_part );
+	}
 }
 
 GF_EXPORT
