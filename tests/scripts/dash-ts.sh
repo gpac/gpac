9f0c1de5464613bce532cc08c8007aa02ebefd7f
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2016-11-28 17:27:40 +0100

    Improve tests for dash and dash-ts

---
+++ tests/scripts/dash-ts.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+test_begin "dash-ts"
+
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter-10mn_I25_baseline_1280x720_512kbps.264 -add $EXTERNAL_MEDIA_DIR/counter/counter-10mn_audio.aac -new $TEMP_DIR/file.mp4" "ts-for-dash-input-preparation"
+
+do_test "$MP42TS -src $TEMP_DIR/file.mp4 -dst-file=$TEMP_DIR/file.ts" "ts-for-dash-input-preparation-2"
+
+do_test "$MP4BOX -dash 1000 $TEMP_DIR/file.ts -out $TEMP_DIR/file.mpd" "ts-dash"
+
+do_playback_test "$TEMP_DIR/file.mpd" "play-ts-dash"
+
+test_end
