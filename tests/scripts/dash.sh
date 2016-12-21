369bbc71226efe43a6b495ac76f8bb1e6af801eb
jeanlf <jeanlf@users.sourceforge.net>
2016-12-21 20:33:52 +0100

    Changed counter to shorter version for tests

--- tests/scripts/dash.sh
+++ tests/scripts/dash.sh
@@ -2,7 +2,7 @@
 
 test_begin "dash"
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter-10mn_I25_baseline_1280x720_512kbps.264 -add $EXTERNAL_MEDIA_DIR/counter/counter-10mn_audio.aac -new $TEMP_DIR/file.mp4" "dash-input-preparation"
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter-30s_I25_baseline_1280x720_512kbps.264 -add $EXTERNAL_MEDIA_DIR/counter/counter-30s_audio.aac -new $TEMP_DIR/file.mp4" "dash-input-preparation"
 
 do_test "$MP4BOX -dash 1000 -profile live $TEMP_DIR/file.mp4 -out $TEMP_DIR/file.mpd" "basic-dash"
 
