6da218ab86400ac14c94f88aa5a8a3728cb70539
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-01-03 15:23:27 +0100

    Fix typo in test files name

--- tests/scripts/dash-multiperiod.sh
+++ tests/scripts/dash-multiperiod.sh
@@ -1,14 +1,14 @@
 test_begin "dash-multiperiod"
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter-30s_I25_baseline_320x180_128kbps.264:dur=5 -new $TEMP_DIR/counter_video_180.mp4" "multiperiod-input-preparation_180"
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_320x180_128kbps.264:dur=5 -new $TEMP_DIR/counter_video_180.mp4" "multiperiod-input-preparation_180"
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter-30s_I25_baseline_640x360_192kbps.264:dur=5 -new $TEMP_DIR/counter_video_360.mp4" "multiperiod-input-preparation2_320"
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_640x360_192kbps.264:dur=5 -new $TEMP_DIR/counter_video_360.mp4" "multiperiod-input-preparation2_320"
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter-30s_I25_baseline_1280x720_512kbps.264:dur=5 -new $TEMP_DIR/counter_video_720.mp4" "multiperiod-input-preparation3_720"
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264:dur=5 -new $TEMP_DIR/counter_video_720.mp4" "multiperiod-input-preparation3_720"
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter-30s_I25_baseline_1920x1080_768kbps.264:dur=5 -new $TEMP_DIR/counter_video_1080.mp4" "multiperiod-input-preparation4_1080"
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1920x1080_768kbps.264:dur=5 -new $TEMP_DIR/counter_video_1080.mp4" "multiperiod-input-preparation4_1080"
 
-do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter-30s_audio.aac:dur=5 -new $TEMP_DIR/counter_audio.mp4" "multiperiod-input-preparation_audio"
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_audio.aac:dur=5 -new $TEMP_DIR/counter_audio.mp4" "multiperiod-input-preparation_audio"
 
 do_test "$MP4BOX -bs-switching no -dash 10000 -frag 1000 -dash-profile live \
 -out $TEMP_DIR/file.mpd -segment-name mp4-live-periods-\$RepresentationID\$-\$Number\$ \
