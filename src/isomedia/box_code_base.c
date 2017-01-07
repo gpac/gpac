2ef9218e5809baa05d93ef95f2d22e1df1da6669
cconcolato <cyril.concolato@telecom-paristech.fr>
2017-01-07 19:22:45 +0100

    fix for conformance file problem

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -2674,10 +2674,11 @@ GF_Err name_Read(GF_Box *s, GF_BitStream *bs)
 	GF_NameBox *ptr = (GF_NameBox *)s;
 
 	length = (u32) (ptr->size);
-	ptr->string = (char*)gf_malloc(sizeof(char) * length);
+	ptr->string = (char*)gf_malloc(sizeof(char) * (length+1));
 	if (! ptr->string) return GF_OUT_OF_MEM;
 
 	gf_bs_read_data(bs, ptr->string, length);
+	ptr->string[length] = 0;
 	return GF_OK;
 }
 GF_Box *name_New()
