14fd7641c373be12d86cb927c6b83af3e2437289
jeanlf <jeanlf@users.sourceforge.net>
2017-01-15 18:38:14 +0100

    misc fixes for prev commit

--- src/export.cpp
+++ src/export.cpp
@@ -680,7 +680,7 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_moov_first) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_box_new) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_box_del) )
-#pragma comment (linker, EXPORT_SYMBOL(gf_isom_parse_box) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_isom_box_parse) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_xml_metadata_description) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_dims_description) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_stxt_get_description) )
