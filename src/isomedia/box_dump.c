d12d6505d81b1e6c8037f59706ef230aa1e2af07
cconcolato <cyril.concolato@telecom-paristech.fr>
2017-01-18 21:01:24 +0100

    parse XML box with a null-terminated string

--- src/isomedia/box_dump.c
+++ src/isomedia/box_dump.c
@@ -1853,8 +1853,8 @@ GF_Err xml_dump(GF_Box *a, FILE * trace)
 	gf_full_box_dump(a, trace);
 	fprintf(trace, ">\n");
 	fprintf(trace, "<![CDATA[\n");
-	if (p->xml_length)
-		gf_fwrite(p->xml, p->xml_length, 1, trace);
+	if (p->xml)
+		gf_fwrite(p->xml, strlen(p->xml), 1, trace);
 	fprintf(trace, "]]>\n");
 	gf_isom_box_dump_done("XMLBox", a, trace);
 	return GF_OK;
