4ca841ae30acfc41c138701e1ba18d013341769a
jeanlf <jeanlf@users.sourceforge.net>
2017-01-19 11:42:52 +0100

    Fixed idat dump

--- src/isomedia/box_dump.c
+++ src/isomedia/box_dump.c
@@ -225,11 +225,11 @@ GF_Err free_dump(GF_Box *a, FILE * trace)
 GF_Err mdat_dump(GF_Box *a, FILE * trace)
 {
 	GF_MediaDataBox *p;
-
+	const char *name = (a->type==GF_ISOM_BOX_TYPE_IDAT ? "ItemDataBox" : "MediaDataBox");
 	p = (GF_MediaDataBox *)a;
-	gf_isom_box_dump_start(a, "MediaDataBox", trace);
+	gf_isom_box_dump_start(a, name, trace);
 	fprintf(trace, "dataSize=\""LLD"\">\n", LLD_CAST p->dataSize);
-	gf_isom_box_dump_done("MediaDataBox", a, trace);
+	gf_isom_box_dump_done(name, a, trace);
 	return GF_OK;
 }
 
