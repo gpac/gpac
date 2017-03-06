3a282fc5b90fb51812e5ae1b9534ad78b112bd98
jeljeli <ahmedjeljeli@gmail.com>
2017-03-06 10:16:47 +0100

    Fixed header

--- modules/mediacodec_dec/mediacodec_dec_jni.c
+++ modules/mediacodec_dec/mediacodec_dec_jni.c
@@ -5,7 +5,7 @@
  *			Copyright (c) Telecom ParisTech 2000-2012
  *					All rights reserved
  *
- *  This file is part of GPAC / Scene Compositor sub-project
+ *  This file is part of GPAC / codec pack module
  *
  *  GPAC is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
