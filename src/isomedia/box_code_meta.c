37ed6fb644dbb6ef47d04d86b47de658dc0a70c2
jeanlf <jeanlf@users.sourceforge.net>
2017-03-15 11:39:35 +0100

    Fixed misc compil warnings

--- src/isomedia/box_code_meta.c
+++ src/isomedia/box_code_meta.c
@@ -245,7 +245,7 @@ GF_Err xml_Write(GF_Box *s, GF_BitStream *bs)
 	if (!s) return GF_BAD_PARAM;
 	e = gf_isom_full_box_write(s, bs);
 	if (e) return e;
-	if (ptr->xml) gf_bs_write_data(bs, ptr->xml, strlen(ptr->xml));
+	if (ptr->xml) gf_bs_write_data(bs, ptr->xml, (u32) strlen(ptr->xml));
 	gf_bs_write_u8(bs, 0);
 	return GF_OK;
 }
