05d2034f6af97eef5b74dcf2d3ea71bb6fccd3b1
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-03-15 16:46:43 +0100

    Use non multiplexed representation for dash-if tests

--- tests/scripts/dash-if-ondemand.sh
+++ tests/scripts/dash-if-ondemand.sh
@@ -4,7 +4,7 @@ test_begin "dash-if-ondemand"
 
 do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264 -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_audio.aac -new $TEMP_DIR/file.mp4" "dash-input-preparation"
 
-do_test "$MP4BOX -dash 1000 -profile dashavc264:onDemand $TEMP_DIR/file.mp4 -out $TEMP_DIR/file.mpd" "basic-dash-if-ondemand"
+do_test "$MP4BOX -dash 1000 -profile dashavc264:onDemand $TEMP_DIR/file.mp4#video $TEMP_DIR/file.mp4#audio -out $TEMP_DIR/file.mpd" "basic-dash-if-ondemand"
 
 do_playback_test "$TEMP_DIR/file.mpd" "play-dash-if-ondemand"
 
