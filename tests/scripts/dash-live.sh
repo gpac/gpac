93e9871f0dfae7b3988670dfe0141c60c8f7a540
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-03-15 13:52:08 +0100

    Backporting dash tests script from main branch

---
+++ tests/scripts/dash-live.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+test_begin "dash-live"
+
+do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264:dur=5 -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_audio.aac:dur=5 -new $TEMP_DIR/file.mp4" "dash-input-preparation"
+
+do_test "$MP4BOX -bs-switching single -mpd-refresh 10 -dash-ctx $TEMP_DIR/dash_ctx -dash-run-for 10000 -dynamic -profile live -dash-live 40  $TEMP_DIR/file.mp4 -out $TEMP_DIR/file.mpd" "dash-live" &
+
+sleep 1
+
+do_playback_test "$TEMP_DIR/file.mpd" "play-basic-dash"
+
+test_end
+
