586e23f5ae2ec836958e1627308d754e3969aa36
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2017-03-19 22:28:46 +0100

    Revert "apply patch reslving URL even on EOS"
    This reverts commit 1ac84e19e17f0cf955a60669399dabcc27ded8bc.
    
    # Conflicts:
    #	src/media_tools/mpd.c

--- src/media_tools/mpd.c
+++ src/media_tools/mpd.c
@@ -2648,13 +2648,6 @@ GF_Err gf_mpd_resolve_url(GF_MPD *mpd, GF_MPD_Representation *rep, GF_MPD_Adapta
 
 			/*check total duration*/
 			if (period->duration && ((start_number + item_index) * *segment_duration_in_ms > period->duration)) {
-				second_sep[0] = '$';
-				/*look for next keyword - copy over remaining text if any*/
-				first_sep = strchr(second_sep + 1, '$');
-				if (first_sep) first_sep[0] = 0;
-				if (strlen(second_sep + 1))
-					strcat(solved_template, second_sep + 1);
-				if (first_sep) first_sep[0] = '$';
 				gf_free(url);
 				gf_free(solved_template);
 				return GF_EOS;
