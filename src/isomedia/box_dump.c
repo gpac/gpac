1bc50135931e0907d6828756cd365e130f81d68b
cconcolato <cyril.concolato@telecom-paristech.fr>
2017-03-29 23:26:10 +0200

    fix box dump

--- src/isomedia/box_dump.c
+++ src/isomedia/box_dump.c
@@ -3981,7 +3981,8 @@ GF_Err stsg_dump(GF_Box *a, FILE * trace)
 	gf_isom_box_dump_start(a, "SubTrackSampleGroupBox", trace);
 
 	if (p->grouping_type)
-		fprintf(trace, "grouping_type=\"%s\">\n", gf_4cc_to_str(p->grouping_type) );
+		fprintf(trace, "grouping_type=\"%s\"", gf_4cc_to_str(p->grouping_type) );
+	fprintf(trace, ">\n");
 
 	for (i = 0; i < p->nb_groups; i++) {
 		fprintf(trace, "<SubTrackSampleGroupBoxEntry group_description_index=\"%d\"/>\n", p->group_description_index[i]);
