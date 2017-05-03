b3f36489e090bfa1bc1c95c8d77bb11078bfa8f2
Aurelien David <aurelien.david@telecom-paristech.fr>
2017-05-03 14:24:40 +0200

    tests scripts fixes for mp4box

--- tests/scripts/mp4box-lang.sh
+++ tests/scripts/mp4box-lang.sh
@@ -28,10 +28,6 @@ my_test_with_hash "$MP4BOX -lang all=eng $TEMP_DIR/file.mp4 -out $output" "all-3
 
 rm $output 2> /dev/null
 my_test_with_hash "$MP4BOX -lang all=en-US $TEMP_DIR/file.mp4 -out $output" "all-2-2-char"
-
-rm $output 2> /dev/null
-my_test_with_hash "$MP4BOX -lang all=en-US $TEMP_DIR/file.mp4 -out $output"
-
 my_test_with_hash "$MP4BOX -lang all=fr-FR $TEMP_DIR/file.mp4 -out $output" "all-twice"
 
 rm $output 2> /dev/null
@@ -42,6 +38,8 @@ my_test_with_hash "$MP4BOX -lang eng $TEMP_DIR/file.mp4 -out $output" "3-char"
 
 rm $output 2> /dev/null
 my_test_with_hash "$MP4BOX -lang en-US $TEMP_DIR/file.mp4 -out $output" "2-2-char"
+my_test_with_hash "$MP4BOX -add $output -new $TEMP_DIR/out2.mp4" "copy"
+rm $TEMP_DIR/out2.mp4 2> /dev/null
 
 rm $output 2> /dev/null
 my_test_with_hash "$MP4BOX -lang 1=en $TEMP_DIR/file.mp4 -out $output" "track-2-char"
@@ -51,19 +49,10 @@ my_test_with_hash "$MP4BOX -lang 1=eng $TEMP_DIR/file.mp4 -out $output" "track-3
 
 rm $output 2> /dev/null
 my_test_with_hash "$MP4BOX -lang 1=en-US $TEMP_DIR/file.mp4 -out $output" "track-2-2-char"
-
-rm $output 2> /dev/null
-my_test_with_hash "$MP4BOX -lang 1=en-US $TEMP_DIR/file.mp4 -out $output"
 my_test_with_hash "$MP4BOX -lang 2=en-US $output" "second-track"
 
 rm $output 2> /dev/null
-my_test_with_hash "$MP4BOX -lang en-US $TEMP_DIR/file.mp4 -out $output" 
-my_test_with_hash "$MP4BOX -add $output -new $TEMP_DIR/out2.mp4" "copy"
-rm $TEMP_DIR/out2.mp4 2> /dev/null
-
-rm $output 2> /dev/null
 my_test_with_hash "$MP4BOX -add $TEMP_DIR/file.mp4#1:lang=en -new $output" "param-2-char"
-my_test_with_hash "$MP4BOX -info $output"
 
 rm $output 2> /dev/null
 my_test_with_hash "$MP4BOX -add $TEMP_DIR/file.mp4#1:lang=eng -new $output" "param-3-char"
