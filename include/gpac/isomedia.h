gpac/gpac

commit cd31273545b69ad688e30c9f2deb5656f79852e4

Fixed bugs in independent tiled VR in DASH

modules/isom_in/read.c
@@ -1164,6 +1164,9 @@ GF_Err ISOR_ServiceCommand(GF_InputService *plug, GF_NetworkCommand *com)
 
 		//and check buffer level on play request
 		isor_check_buffer_level(read);
+		if (!read->nb_playing)
+			gf_isom_reset_fragment_info(read->mov, GF_FALSE);
+
 		read->nb_playing++;
 		return GF_OK;
 	case GF_NET_CHAN_STOP:

src/export.cpp
@@ -2092,6 +2092,7 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_dash_group_get_segment_init_url) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_dash_group_get_segment_init_keys) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_dash_group_select) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_dash_group_seek) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_dash_groups_set_language) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_dash_in_last_period) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_dash_get_period_switch_status) )

include/gpac/dash.h
@@ -347,6 +347,9 @@ Bool gf_dash_group_segment_switch_forced(GF_DashClient *dash, u32 idx);
 /*get video info for this group if video*/
 GF_Err gf_dash_group_get_video_info(GF_DashClient *dash, u32 idx, u32 *max_width, u32 *max_height);
 
+/*seek only a given group*/
+void gf_dash_group_seek(GF_DashClient *dash, u32 group_idx, Double seek_to);
+
 /*sets playback speed of the session. Speed is used in adaptation logic*/
 void gf_dash_set_speed(GF_DashClient *dash, Double speed);
 

include/gpac/internal/compositor_dev.h
@@ -708,7 +708,10 @@ GF_Err gf_sc_texture_open(GF_TextureHandler *txh, MFURL *url, Bool lock_scene_ti
 GF_Err gf_sc_texture_play(GF_TextureHandler *txh, MFURL *url);
 GF_Err gf_sc_texture_play_from_to(GF_TextureHandler *txh, MFURL *url, Double start_offset, Double end_offset, Bool can_loop, Bool lock_scene_timeline);
 /*stops associated object*/
+void gf_sc_texture_stop_no_unregister(GF_TextureHandler *txh);
+/*stops associated object and unregister it*/
 void gf_sc_texture_stop(GF_TextureHandler *txh);
+
 /*restarts associated object - DO NOT CALL stop/start*/
 void gf_sc_texture_restart(GF_TextureHandler *txh);
 /*common routine for all video texture: fetches a frame and update the 2D texture object */
@@ -972,6 +975,7 @@ struct _traversing_state
 	GF_Plane clip_planes[MAX_USER_CLIP_PLANES];
 	u32 num_clip_planes;
 
+	Bool camera_was_dirty;
 
 	/*layer traversal state:
 		set to the first traversed layer3D when picking

include/gpac/internal/mesh.h
@@ -132,6 +132,8 @@ enum
 	MESH_IS_SMOOTHED = 1<<5,
 	/*vertex.color is used with alpha channel*/
 	MESH_HAS_ALPHA = 1<<6,
+	/*flag only used by VRgeometry proto*/
+	MESH_WAS_VISIBLE = 1<<7,
 };
 
 /*indexes as used in glDrawElements - note that integer type is not allowed with oglES*/

include/gpac/internal/terminal_dev.h
@@ -210,7 +210,8 @@ struct _scene
 	/*URLs of current video, audio and subs (we can't store objects since they may be destroyed when seeking)*/
 	SFURL visual_url, audio_url, text_url, dims_url;
 
-	Bool is_srd, is_tiled_srd;
+	Bool is_tiled_srd;
+	u32 srd_type;
 	s32 srd_min_x, srd_max_x, srd_min_y, srd_max_y;
 
 
@@ -1003,6 +1004,7 @@ enum
 	GF_ODM_STATE_PLAY,
 	GF_ODM_STATE_IN_SETUP,
 	GF_ODM_STATE_BLOCKED,
+	GF_ODM_STATE_STOP_NO_NET,
 };
 
 enum

include/gpac/mediaobject.h
@@ -208,6 +208,7 @@ typedef struct
 	u32 scene_width;
 	u32 scene_height;
 
+	Bool has_full_coverage;
 	Bool is_tiled_srd;
 } GF_MediaObjectVRInfo;
 

modules/mpd_in/mpd_in.c
@@ -1515,6 +1515,12 @@ GF_Err MPD_ServiceCommand(GF_InputService *plug, GF_NetworkCommand *com)
 			//to remove once we manage to keep the service alive
 			/*don't forward commands if a switch of period is to be scheduled, we are killing the service anyway ...*/
 			if (gf_dash_get_period_switch_status(mpdin->dash)) return GF_OK;
+		} else if (!com->play.initial_broadcast_play) {
+			/*don't forward commands if a switch of period is to be scheduled, we are killing the service anyway ...*/
+			if (gf_dash_get_period_switch_status(mpdin->dash)) return GF_OK;
+
+			//seek on a single group
+			gf_dash_group_seek(mpdin->dash, idx, com->play.start_range);
 		}
 
 		//check if current segment playback should be aborted

commit 00aef548c312525716fdb3c3ef4354717b5ebe9e

fixed bug introduced in cd31273

include/gpac/isomedia.h
@@ -1506,6 +1506,8 @@ GF_Err gf_isom_fragment_append_data(GF_ISOFile *the_file, u32 TrackID, char *dat
 /*reset internal info used with fragments and segment. Should be called when seeking (with keep_sample_count=0) or when loading a media segments with the same timing as the previously loaded segment*/
 void gf_isom_reset_fragment_info(GF_ISOFile *movie, Bool keep_sample_count);
 
+void gf_isom_reset_seq_num(GF_ISOFile *movie);
+
 /*return the duration of the movie+fragments if known, 0 if error*/
 u64 gf_isom_get_fragmented_duration(GF_ISOFile *movie);
 /*returns the number of sidx boxes*/

modules/isom_in/read.c
@@ -1165,7 +1165,7 @@ GF_Err ISOR_ServiceCommand(GF_InputService *plug, GF_NetworkCommand *com)
 		//and check buffer level on play request
 		isor_check_buffer_level(read);
 		if (!read->nb_playing)
-			gf_isom_reset_fragment_info(read->mov, GF_FALSE);
+			gf_isom_reset_seq_num(read->mov);
 
 		read->nb_playing++;
 		return GF_OK;

src/export.cpp
@@ -836,6 +836,7 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_rvc_config) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_sample_rap_roll_info) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_reset_fragment_info) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_isom_reset_seq_num) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_track_original_id) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_set_nalu_extract_mode) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_nalu_extract_mode) )

src/isomedia/isom_read.c
@@ -3593,6 +3593,14 @@ void gf_isom_reset_fragment_info(GF_ISOFile *movie, Bool keep_sample_count)
 }
 
 GF_EXPORT
+void gf_isom_reset_seq_num(GF_ISOFile *movie)
+{
+#ifdef GPAC_DISABLE_ISOM_FRAGMENTS
+	movie->NextMoofNumber = 0;
+#endif
+}
+
+GF_EXPORT
 GF_Err gf_isom_get_sample_rap_roll_info(GF_ISOFile *the_file, u32 trackNumber, u32 sample_number, Bool *is_rap, Bool *has_roll, s32 *roll_distance)
 {
 	GF_TrackBox *trak;