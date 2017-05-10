6b292a485581e0d1824cca9c57ed2716d50c5844
jeanlf <jeanlf@github.com>
2017-05-10 17:11:04 +0200

    disable auto load of h2b2vs ext

--- gui/extensions/H2B2VS/init.js
+++ gui/extensions/H2B2VS/init.js
@@ -7,7 +7,7 @@ my_ext = {
     description: "H2B2VS Preferences",
     url: "http://h2b2vs.epfl.ch/",
     execjs: ["h2b2vs.js"],
-    autostart: true,
+    autostart: false,
     config_id: "H2B2VS",
     requires_gl: false,
     version_major: 1,
