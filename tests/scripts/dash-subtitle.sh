a58e780c90731e197de29caf68d2d7c64f62ce67
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-06-19 12:14:22 +0200

    fix new dash tests

--- tests/scripts/dash-subtitle.sh
+++ tests/scripts/dash-subtitle.sh
@@ -6,12 +6,8 @@ do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/subtitle.srt -new $TEMP_DIR/fil
 
 do_test "$MP4BOX -dash 1500 $TEMP_DIR/file.mp4 -profile live -out $TEMP_DIR/file.mpd" "dash-subt"
 
-do_hash_test $TEMP_DIR/file.mpd "basic-dash-hash-mpd"
+do_hash_test $TEMP_DIR/file.mpd "dash-subt-hash-mpd"
 
-#do_hash_test $TEMP_DIR/file_track1_dashinit.mp4 "basic-dash-hash-init-segment-video"
-
-#do_hash_test $TEMP_DIR/file_track2_dashinit.mp4 "basic-dash-hash-init-segment-audio"
-
-do_playback_test "$TEMP_DIR/file.mpd" "play-basic-dash"
+do_playback_test "$TEMP_DIR/file.mpd" "play-dash-subt"
 
 test_end
