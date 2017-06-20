7f495c3e53e5346b1e21fb7f7d350184591a4567
NicolasDEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2017-06-20 14:39:54 +0200

    fix typo in dash-main-singlefile.sh

--- tests/scripts/dash-main-singlefile.sh
+++ tests/scripts/dash-main-singlefile.sh
@@ -4,9 +4,9 @@ test_begin "dash-mainprofile-singlefile"
 
 do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264  -new $TEMP_DIR/file.mp4" "dash-input-preparation"
 
-do_test "$MP4BOX -dash 1000 $TEMP_DIR/file.mp4#video --single-file -profile main -subdur 1000 -dash-ctx $TEMP_DIR/dash_ctx -segment-name "test-\$Number\$" -out $TEMP_DIR/file.mpd" "dash-mainprofile-singlefile-1"
+do_test "$MP4BOX -dash 1000 $TEMP_DIR/file.mp4#video -single-file -profile main -subdur 1000 -dash-ctx $TEMP_DIR/dash_ctx -segment-name "test-\$Number\$" -out $TEMP_DIR/file.mpd" "dash-mainprofile-singlefile-1"
 
-do_test "$MP4BOX -dash 1000 $TEMP_DIR/file.mp4#video --single-file -profile main -subdur 1000 -dash-ctx $TEMP_DIR/dash_ctx -segment-name "test-\$Number\$" -out $TEMP_DIR/file.mpd" "dash-mainprofile-singlefile-2"
+do_test "$MP4BOX -dash 1000 $TEMP_DIR/file.mp4#video -single-file -profile main -subdur 1000 -dash-ctx $TEMP_DIR/dash_ctx -segment-name "test-\$Number\$" -out $TEMP_DIR/file.mpd" "dash-mainprofile-singlefile-2"
 
 do_hash_test $TEMP_DIR/file.mpd "dash-mainprofile-singlefile-hash-mpd"
 
