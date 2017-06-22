e082e56e066d6fea45ba513d924f36a8f3f621db
cconcolato <cyril.concolato@telecom-paristech.fr>
2017-06-22 11:32:27 +0200

    cleaning BBA-0 and BOLA ABR implementation

--- modules/mpd_in/mpd_in.c
+++ modules/mpd_in/mpd_in.c
@@ -1022,8 +1022,8 @@ GF_Err MPD_ConnectService(GF_InputService *plug, GF_ClientService *serv, const c
 	else if (!strcmp(opt, "BOLA_U")) {
 		mpdin->adaptation_algorithm = GF_DASH_ALGO_BOLA_U;
 	}
-	else if (!strcmp(opt, "test")) {
-		mpdin->adaptation_algorithm = GF_DASH_ALGO_GPAC_TEST;
+	else if (!strcmp(opt, "BOLA_O")) {
+		mpdin->adaptation_algorithm = GF_DASH_ALGO_BOLA_O;
 	}
 
 	opt = gf_modules_get_option((GF_BaseInterface *)plug, "DASH", "StartRepresentation");
