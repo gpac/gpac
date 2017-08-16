d448480afc4db5836b3247a1940e49a8f7acf0b6
Aurelien David <aurelien.david@telecom-paristech.fr>
2017-08-16 15:33:12 +0200

    m3u8-to-mpd: prevent printing version in test mode

--- applications/mp4box/main.c
+++ applications/mp4box/main.c
@@ -3673,7 +3673,7 @@ int mp4boxMain(int argc, char **argv)
 			fprintf(stderr, "[DASH] Error: MPD creation problem %s\n", gf_error_to_string(e));
 			mp4box_cleanup(1);
 		}
-		e = gf_m3u8_to_mpd(remote ? "tmp_main.m3u8" : inName, mpd_base_url ? mpd_base_url : inName, outfile, 0, "video/mp2t", GF_TRUE, use_url_template, NULL, mpd, GF_TRUE, GF_TRUE);
+		e = gf_m3u8_to_mpd(remote ? "tmp_main.m3u8" : inName, mpd_base_url ? mpd_base_url : inName, outfile, 0, "video/mp2t", GF_TRUE, use_url_template, NULL, mpd, GF_TRUE, GF_TRUE, force_test_mode);
 		if (!e)
 			gf_mpd_write_file(mpd, outfile);
 
