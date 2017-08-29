18ae19dc23972da662606a5676efdf48076a22be
jeanlf <jeanlf@users.sourceforge.net>
2017-08-29 16:48:40 +0200

    added addon splicing of main content

--- src/terminal/scene.c
+++ src/terminal/scene.c
@@ -2577,13 +2577,13 @@ restart:
 #endif
 }
 
-void scene_reset_addon(GF_AddonMedia *addon, Bool disconnect)
+void gf_scene_reset_addon(GF_AddonMedia *addon, Bool disconnect)
 {
-	if (disconnect && addon->root_od) {
-		gf_odm_disconnect(addon->root_od, 1);
-	}
 	if (addon->root_od) {
 		addon->root_od->addon = NULL;
+		if (disconnect) {
+			gf_odm_disconnect(addon->root_od, 1);
+		}
 	}
 
 	if (addon->url) gf_free(addon->url);
@@ -2596,7 +2596,7 @@ void gf_scene_reset_addons(GF_Scene *scene)
 		GF_AddonMedia *addon = gf_list_last(scene->declared_addons);
 		gf_list_rem_last(scene->declared_addons);
 
-		scene_reset_addon(addon, 0);
+		gf_scene_reset_addon(addon, 0);
 	}
 }
 
@@ -2640,7 +2640,7 @@ void gf_scene_register_associated_media(GF_Scene *scene, GF_AssociatedContentLoc
 		addon = gf_list_get(scene->declared_addons, i);
 		if ((addon_info->timeline_id>=0) && addon->timeline_id==addon_info->timeline_id) {
 			my_addon = 1;
-		} else if (addon->url && addon_info->external_URL && !strcmp(addon->url, addon_info->external_URL)) {
+		} else if ((addon_info->timeline_id>=0) && addon->url && addon_info->external_URL && !strcmp(addon->url, addon_info->external_URL)) {
 			my_addon = 1;
 			//send message to service handler
 		}
@@ -2669,7 +2669,7 @@ void gf_scene_register_associated_media(GF_Scene *scene, GF_AssociatedContentLoc
 			}
 			//nothing associated, deactivate addon
 			if (!addon_info->external_URL) {
-				scene_reset_addon(addon, 1);
+				gf_scene_reset_addon(addon, 1);
 			} else if (strcmp(addon_info->external_URL, addon->url)) {
 				//reconfigure addon
 				gf_free(addon->url);
@@ -2678,6 +2678,7 @@ void gf_scene_register_associated_media(GF_Scene *scene, GF_AssociatedContentLoc
 			}
 			return;
 		}
+		addon = NULL;
 	}
 
 	if (!addon_info->external_URL) {
@@ -2700,6 +2701,13 @@ void gf_scene_register_associated_media(GF_Scene *scene, GF_AssociatedContentLoc
 	addon->url = gf_strdup(addon_info->external_URL);
 	addon->media_timescale = 1;
 	addon->timeline_ready = (addon_info->timeline_id<0) ? 1 : 0;
+	addon->splice_start = addon_info->splice_start_time;
+	addon->splice_end = addon_info->splice_end_time;
+	if (addon_info->is_splicing) {
+		addon->addon_type = GF_ADDON_TYPE_SPLICED;
+		scene->has_splicing_addons = GF_TRUE;
+		addon->media_pts = addon->splice_start * 90000;
+	}
 
 	if (!new_addon) return;
 
@@ -2746,7 +2754,7 @@ void gf_scene_notify_associated_media_timeline(GF_Scene *scene, GF_AssociatedCon
 		        //this is a splicing point, discard all previsously declared splicing addons
 		        || prev_addon->is_splicing
 		   ) {
-			scene_reset_addon(prev_addon, GF_TRUE);
+			gf_scene_reset_addon(prev_addon, GF_TRUE);
 			gf_list_rem(scene->declared_addons, i);
 			i--;
 			count--;
@@ -2960,8 +2968,15 @@ void gf_scene_select_scalable_addon(GF_Scene *scene, GF_ObjectManager *odm)
 
 	}
 
-	//signal to the base decoder that we will want full quality
 	caps.CapCode = GF_CODEC_MEDIA_SWITCH_QUALITY;
-	caps.cap.valueInt = 2;
+	// splicing, signal to the base decoder that we will want low quality and wait for splice activation
+	if (odm->parentscene->root_od->addon->is_splicing) {
+		caps.cap.valueInt = 0;
+	}
+	//not splicing, signal to the base decoder that we will want full quality right now
+	else {
+		caps.cap.valueInt = 2;
+	}
 	odm_base->codec->decio->SetCapabilities(odm_base->codec->decio, caps);
+
 }
