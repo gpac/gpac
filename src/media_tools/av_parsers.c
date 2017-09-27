ca079528b0787436c0b2d67dee76e87645cbd6bc
jeanlf <jeanlf@users.sourceforge.net>
2017-09-27 23:15:16 +0200

    added ac3 reframer

--- src/media_tools/av_parsers.c
+++ src/media_tools/av_parsers.c
@@ -5257,11 +5257,11 @@ static Bool AC3_FindSyncCodeBS(GF_BitStream *bs)
 {
 	u8 b1;
 	u64 pos = gf_bs_get_position(bs);
-	u64 end = gf_bs_get_size(bs) - 6;
+	u64 end = gf_bs_get_size(bs);
 
 	pos += 1;
 	b1 = gf_bs_read_u8(bs);
-	while (pos <= end) {
+	while (pos + 1 <= end) {
 		u8 b2 = gf_bs_read_u8(bs);
 		if ((b1 == 0x0b) && (b2==0x77)) {
 			gf_bs_seek(bs, pos-1);
