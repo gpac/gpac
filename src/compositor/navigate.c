90120a2b9de53f96613179cea804bd6296fb6edb
Ahmed Rida SEKKAT <rida.iguidre@gmail.com>
2017-11-13 14:37:57 +0100

    Added a function to compute yaw, pitch and roll from the rotation matrix

--- src/compositor/navigate.c
+++ src/compositor/navigate.c
@@ -394,6 +394,14 @@ static Bool compositor_handle_navigation_3d(GF_Compositor *compositor, GF_Event 
 		key_trans*=4;
 	}
 
+	if (!gf_term_get_option(compositor->term, GF_OPT_ORIENTATION_SENSORS_ACTIVE)) {
+		Fixed yaw, pitch, roll;
+		gf_mx_compute_angles(&compositor->visual->camera.modelview, &yaw, &pitch, &roll);
+		compositor->audio_renderer->yaw = yaw;
+		compositor->audio_renderer->pitch = pitch;
+		compositor->audio_renderer->roll = roll;
+	}
+
 	switch (ev->type) {
 	case GF_EVENT_MOUSEDOWN:
 		/*left*/
