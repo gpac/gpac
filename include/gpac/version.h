42d8f954886c6e1266c14983d39e82221d0cf8bb
jeanlf <jeanlf@users.sourceforge.net>
2018-04-12 00:04:03 +0200

    updated version and sonames

--- include/gpac/version.h
+++ include/gpac/version.h
@@ -47,9 +47,9 @@
  * NO SPACE in GPAC_VERSION / GPAC_FULL_VERSION for proper install
  * SONAME versions must be digits (not strings)
  */
-#define GPAC_VERSION          "0.7.2-DEV"
-#define GPAC_VERSION_MAJOR 7
-#define GPAC_VERSION_MINOR 6
+#define GPAC_VERSION          "0.8.0-DEV"
+#define GPAC_VERSION_MAJOR 8
+#define GPAC_VERSION_MINOR 0
 #define GPAC_VERSION_MICRO 0
 
 #include <gpac/revision.h>
