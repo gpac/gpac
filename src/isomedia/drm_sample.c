f2c560fed3b48d519d1f805f3752696fc53327d5
jeanlf <jeanlf@users.sourceforge.net>
2018-04-26 18:05:23 +0200

    fixed bugs in decrypt with cenc

--- src/isomedia/drm_sample.c
+++ src/isomedia/drm_sample.c
@@ -779,7 +779,23 @@ GF_Err gf_isom_remove_samp_enc_box(GF_ISOFile *the_file, u32 trackNumber)
 		gf_list_del(stbl->other_boxes);
 		stbl->other_boxes = NULL;
 	}
-
+	if (trak->sample_encryption) {
+		gf_isom_box_del((GF_Box *)trak->sample_encryption);
+		trak->sample_encryption = NULL;
+	}
+	for (i = 0; i < gf_list_count(trak->other_boxes); i++) {
+		GF_Box *a = (GF_Box *)gf_list_get(trak->other_boxes, i);
+		if ((a->type ==GF_ISOM_BOX_TYPE_UUID) && (((GF_UUIDBox *)a)->internal_4cc == GF_ISOM_BOX_UUID_PSEC)) {
+			gf_isom_box_del(a);
+			gf_list_rem(trak->other_boxes, i);
+			i--;
+		}
+		else if (a->type == GF_ISOM_BOX_TYPE_SENC) {
+			gf_isom_box_del(a);
+			gf_list_rem(trak->other_boxes, i);
+			i--;
+		}
+	}
 	return GF_OK;
 }
 
