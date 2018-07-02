ce60c15f4dbcc180ca602e8dce7d1dbea7f4fc0c
Aurelien David <aurelien.david@telecom-paristech.fr>
2018-07-02 16:18:04 +0200

    don't try to dump null boxes (#1093)

--- src/isomedia/box_funcs.c
+++ src/isomedia/box_funcs.c
@@ -1228,7 +1228,7 @@ GF_Box *gf_isom_box_new(u32 boxType)
 
 void gf_isom_box_add_for_dump_mode(GF_Box *parent, GF_Box *a)
 {
-	if (use_dump_mode && (!parent->other_boxes || (gf_list_find(parent->other_boxes, a)<0) ) )
+	if (use_dump_mode && a && (!parent->other_boxes || (gf_list_find(parent->other_boxes, a)<0) ) )
 		gf_isom_box_add_default(parent, a);
 }
 
