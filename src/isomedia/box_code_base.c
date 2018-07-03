d3e5232a21c87c211e4f9f3eddb4b6c49df782bf
Aurelien David <aurelien.david@telecom-paristech.fr>
2018-07-03 16:06:34 +0200

    fixed extr_Read segfault (#1101)

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -10976,7 +10976,7 @@ GF_Err extr_Read(GF_Box *s, GF_BitStream *bs)
 
 	e = gf_isom_box_parse((GF_Box**) &ptr->feci, bs);
 	if (e) return e;
-	if (ptr->feci->size>ptr->size) return GF_ISOM_INVALID_MEDIA;
+	if (!ptr->feci || ptr->feci->size > ptr->size) return GF_ISOM_INVALID_MEDIA;
 	ptr->data_length = (u32) (ptr->size - ptr->feci->size);
 	ptr->data = gf_malloc(sizeof(char)*ptr->data_length);
 	gf_bs_read_data(bs, ptr->data, ptr->data_length);
