dbce96989e034651d59a1de43f1a5d508d7a3e87
Aurelien David <aurelien.david@telecom-paristech.fr>
2018-07-06 18:48:49 +0200

    fix segfault in mvex_del -> classic double free

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -4425,6 +4425,9 @@ void mvex_del(GF_Box *s)
 	if (ptr->mehd) gf_isom_box_del((GF_Box*)ptr->mehd);
 	gf_isom_box_array_del(ptr->TrackExList);
 	gf_isom_box_array_del(ptr->TrackExPropList);
+	ptr->mehd = NULL;
+	ptr->TrackExList = NULL;
+	ptr->TrackExPropList = NULL;
 	gf_free(ptr);
 }
 
