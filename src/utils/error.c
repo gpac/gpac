gpac/gpac

commit 1c21919f7dcfdb20f247e77e47d09f9cdef29397

fix crash allowed when compilers implements wrongly strchr() compilers de-constify the return value: char* strchr(const char*, int)

src/utils/error.c
@@ -173,17 +173,24 @@ static struct log_tool_info {
 	{ GF_LOG_SCHEDULER, "scheduler", GF_LOG_INFO },
 };
 
+#define GF_LOG_TOOL_MAX_NAME_SIZE (GF_LOG_TOOL_MAX*10)
+
 GF_EXPORT
-GF_Err gf_log_modify_tools_levels(const char *val)
+GF_Err gf_log_modify_tools_levels(const char *val_)
 {
 #ifndef GPAC_DISABLE_LOG
-	u32 level;
-	char *sep, *sep_level;
+	char tmp[GF_LOG_TOOL_MAX_NAME_SIZE];
+	const char *val = tmp;
+	assert(strlen(val_) < GF_LOG_TOOL_MAX_NAME_SIZE);
+	strncpy(tmp, val_, GF_LOG_TOOL_MAX_NAME_SIZE - 1);
+	tmp[GF_LOG_TOOL_MAX_NAME_SIZE - 1] = 0;
+
 	while (val && strlen(val)) {
+		u32 level;
 		const char *next_val = NULL;
 		const char *tools = NULL;
 		/*look for log level*/
-		sep_level = strchr(val, '@');
+		char *sep_level = strchr(val, '@');
 		if (!sep_level) {
 			if (!strcmp(val, "ncl")) {
 				void default_log_callback_no_col(void *cbck, GF_LOG_Level level, GF_LOG_Tool tool, const char *fmt, va_list vlist);
@@ -228,7 +235,7 @@ GF_Err gf_log_modify_tools_levels(const char *val)
 		while (tools) {
 			u32 i;
 
-			sep = strchr(tools, ':');
+			char *sep = strchr(tools, ':');
 			if (sep) sep[0] = 0;
 
 			if (!stricmp(tools, "all")) {

commit 53f4ed20965c578434ed252d71dfbe6f48a09a8b

fixed crash introduced in 1c21919

src/utils/error.c
@@ -181,6 +181,7 @@ GF_Err gf_log_modify_tools_levels(const char *val_)
 #ifndef GPAC_DISABLE_LOG
 	char tmp[GF_LOG_TOOL_MAX_NAME_SIZE];
 	const char *val = tmp;
+	if (!val_) val_ = "";
 	assert(strlen(val_) < GF_LOG_TOOL_MAX_NAME_SIZE);
 	strncpy(tmp, val_, GF_LOG_TOOL_MAX_NAME_SIZE - 1);
 	tmp[GF_LOG_TOOL_MAX_NAME_SIZE - 1] = 0;