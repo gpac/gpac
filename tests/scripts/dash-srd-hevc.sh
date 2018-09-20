6e0c36b88e2634a66a4b8cabfd3768d4545e2d37
jeanlf <jeanlf@users.sourceforge.net>
2018-09-20 17:11:49 +0200

    fixed SRD test

--- tests/scripts/dash-srd-hevc.sh
+++ tests/scripts/dash-srd-hevc.sh
@@ -3,7 +3,7 @@ test_begin "dash-srd-hevc"
 do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_1280_720_I_25_tiled_1mb.hevc:split_tiles -new $TEMP_DIR/file.mp4" "dash-input-preparation"
 do_hash_test $TEMP_DIR/file.mp4 "split-tiles"
 
-do_test "$MP4BOX -dash 1000 $TEMP_DIR/file.mp4 -out $TEMP_DIR/file.mpd" "basic-dash"
+do_test "$MP4BOX -dash 1000 -profile live $TEMP_DIR/file.mp4 -out $TEMP_DIR/file.mpd" "basic-dash"
 
 do_hash_test $TEMP_DIR/file.mpd "mpd"
 do_hash_test $TEMP_DIR/file_set1_init.mp4 "init"
