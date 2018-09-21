96ec5b59643e822c053d2e436e8a74e036978a03
jeanlf <jeanlf@users.sourceforge.net>
2018-09-21 18:04:38 +0200

    fixed relative name in isobmf dump and mpd + added possibility to keep all generated files from test suite

--- tests/scripts/dash-live.sh
+++ tests/scripts/dash-live.sh
@@ -5,6 +5,11 @@ test_begin "dash-live"
 do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264 -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_audio.aac -new $TEMP_DIR/file.mp4" "dash-input-preparation"
 
 do_test "$MP4BOX -dash 1000 -profile live $TEMP_DIR/file.mp4#video $TEMP_DIR/file.mp4#audio -out $TEMP_DIR/file.mpd" "dash-live"
+do_hash_test "$TEMP_DIR/file.mpd" "mpd"
+do_hash_test "$TEMP_DIR/file_dash_track1_init.mp4" "init1"
+do_hash_test "$TEMP_DIR/file_dash_track2_init.mp4" "init2"
+do_hash_test "$TEMP_DIR/file_dash_track1_20.m4s" "seg1"
+do_hash_test "$TEMP_DIR/file_dash_track2_20.m4s" "seg2"
 
 do_playback_test "$TEMP_DIR/file.mpd" "play-dash-live"
 
