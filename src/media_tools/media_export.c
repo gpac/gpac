f09bf42fcd019e243c6aa3b19a9604878a82e719
jeanlf <jeanlf@users.sourceforge.net>
2018-10-10 11:20:38 +0800

    fixed bug in ttml nhml import

--- src/media_tools/media_export.c
+++ src/media_tools/media_export.c
@@ -2236,7 +2236,7 @@ GF_Err gf_media_export_nhml(GF_MediaExporter *dumper, Bool dims_doc)
 	gf_export_message(dumper, GF_OK, "Exporting NHML for track %s", gf_4cc_to_str(mstype) );
 
 	/*write header*/
-	fprintf(nhml, "<%s version=\"1.0\" timeScale=\"%d\" ", szRootName, gf_isom_get_media_timescale(dumper->file, track) );
+	fprintf(nhml, "<%s version=\"1.0\" trackID=\"%d\" timeScale=\"%d\" ", szRootName, gf_isom_get_track_id(dumper->file, track), gf_isom_get_media_timescale(dumper->file, track) );
 	if (esd) {
         u32 mtype;
 
