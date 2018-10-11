0d8386ffc1a53187ba53e60cdd951fade39649f2
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2018-10-11 19:45:54 +0200

    vpcc: fix overflow while reading

--- src/odf/descriptors.c
+++ src/odf/descriptors.c
@@ -1201,7 +1201,7 @@ GF_Err gf_odf_vp_cfg_write(GF_VPConfig *cfg, char **outData, u32 *outSize)
 }
 
 GF_EXPORT
-GF_VPConfig *gf_odf_vp_cfg_read_bs(GF_BitStream *bs)
+GF_VPConfig *gf_odf_vp_cfg_read_bs(GF_BitStream *bs, Bool is_v0)
 {
 	GF_VPConfig *cfg = gf_odf_vp_cfg_new();
 
@@ -1215,6 +1215,9 @@ GF_VPConfig *gf_odf_vp_cfg_read_bs(GF_BitStream *bs)
 	cfg->colour_primaries = gf_bs_read_int(bs, 8);
 	cfg->transfer_characteristics = gf_bs_read_int(bs, 8);
 	cfg->matrix_coefficients = gf_bs_read_int(bs, 8);
+
+	if (is_v0)
+		return cfg;
 
 	cfg->codec_initdata_size = gf_bs_read_int(bs, 16);
 
@@ -1232,7 +1235,7 @@ GF_EXPORT
 GF_VPConfig *gf_odf_vp_cfg_read(char *dsi, u32 dsi_size)
 {
 	GF_BitStream *bs = gf_bs_new(dsi, dsi_size, GF_BITSTREAM_READ);
-	GF_VPConfig *cfg = gf_odf_vp_cfg_read_bs(bs);
+	GF_VPConfig *cfg = gf_odf_vp_cfg_read_bs(bs, GF_FALSE);
 	gf_bs_del(bs);
 	return cfg;
 }
