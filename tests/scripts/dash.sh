bb69b3e3c081886a1c66b21af9777079f4e19e65
jeanlf <jeanlf@users.sourceforge.net>
2018-10-16 16:55:51 +0200

    fixed some dash tests to use -rap

--- tests/scripts/dash.sh
+++ tests/scripts/dash.sh
@@ -10,7 +10,7 @@ if [ -f "$TEMP_DIR/file.mpd" ] ; then
 	rm "$TEMP_DIR/file.mpd"
 fi
 
-do_test "$MP4BOX -dash 1000 $TEMP_DIR/file.mp4#video $TEMP_DIR/file.mp4#audio -out $TEMP_DIR/file.mpd" "mpd"
+do_test "$MP4BOX -dash 1000 -rap $TEMP_DIR/file.mp4#video $TEMP_DIR/file.mp4#audio -out $TEMP_DIR/file.mpd" "mpd"
 
 do_hash_test "$TEMP_DIR/file.mpd" "mpd"
 do_hash_test "$TEMP_DIR/file_dash_track1_init.mp4" "segment-video"
