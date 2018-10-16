e080807115d0ebc16b525f93a8e8b85ba0eb392b
jeanlf <jeanlf@users.sourceforge.net>
2018-10-16 15:33:56 +0200

    misc mpd cosmetics

--- tests/scripts/dash-descriptor.sh
+++ tests/scripts/dash-descriptor.sh
@@ -2,7 +2,7 @@ test_begin "dash-descriptor"
 
 do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264 -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_audio.aac -new $TEMP_DIR/file.mp4" "dash-input-preparation"
 
-do_test "$MP4BOX -dash 1000 $TEMP_DIR/file.mp4#video:dur=10:desc_rep=<foo>bar</foo> $TEMP_DIR/file.mp4#audio:dur=10 -out $TEMP_DIR/file.mpd" "basic-dash"
+do_test "$MP4BOX -dash 1000 -rap $TEMP_DIR/file.mp4#video:dur=10:desc_rep=<foo>bar</foo> $TEMP_DIR/file.mp4#audio:dur=10 -out $TEMP_DIR/file.mpd" "basic-dash"
 
 do_hash_test $TEMP_DIR/file.mpd "mpd"
 
