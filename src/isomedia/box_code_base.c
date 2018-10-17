fcd6863ad8df37a9c983a911cb69d9140de03c1d
jeanlf <jeanlf@users.sourceforge.net>
2018-10-17 09:19:52 +0200

    fixed bugs in audio sample entry v1 QTFF vs ISOBMFF

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -3961,10 +3961,35 @@ GF_Err audio_sample_entry_Read(GF_Box *s, GF_BitStream *bs)
 {
 	GF_MPEGAudioSampleEntryBox *ptr;
 	char *data;
-	u32 i, size;
+	u8 a, b, c, d;
+	u32 i, size, v, nb_alnum;
 	GF_Err e;
-	u64 pos;
+	u64 pos, start;
 
+	ptr = (GF_MPEGAudioSampleEntryBox *)s;
+
+	start = gf_bs_get_position(bs);
+	gf_bs_seek(bs, start + 8);
+	v = gf_bs_read_u16(bs);
+	if (v) ptr->is_qtff = 1;
+
+	//try to disambiguate QTFF v1 and MP4 v1 audio sample entries ...
+	if (v==1) {
+		//go to end of ISOM audio sample entry, skip 4 byte (box size field), read 4 bytes (box type) and check if this looks like a box
+		gf_bs_seek(bs, start + 8 + 20  + 4);
+		a = gf_bs_read_u8(bs);
+		b = gf_bs_read_u8(bs);
+		c = gf_bs_read_u8(bs);
+		d = gf_bs_read_u8(bs);
+		nb_alnum = 0;
+		if (isalnum(a)) nb_alnum++;
+		if (isalnum(b)) nb_alnum++;
+		if (isalnum(c)) nb_alnum++;
+		if (isalnum(d)) nb_alnum++;
+		if (nb_alnum>2) ptr->is_qtff = 0;
+	}
+
+	gf_bs_seek(bs, start);
 	e = gf_isom_audio_sample_entry_read((GF_AudioSampleEntryBox*)s, bs);
 	if (e) return e;
 	pos = gf_bs_get_position(bs);
@@ -3975,7 +4000,6 @@ GF_Err audio_sample_entry_Read(GF_Box *s, GF_BitStream *bs)
 	if (size<8) return GF_ISOM_INVALID_FILE;
 
 	/*hack for some weird files (possibly recorded with live.com tools, needs further investigations)*/
-	ptr = (GF_MPEGAudioSampleEntryBox *)s;
 	gf_bs_seek(bs, pos);
 	data = (char*)gf_malloc(sizeof(char) * size);
 	gf_bs_read_data(bs, data, size);
