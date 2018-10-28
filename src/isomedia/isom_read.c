fe434d5cdbc8c44bbadfa05778505e8a22cf8d9c
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2018-10-28 19:51:04 +0100

    make memory retrieval safer

--- src/isomedia/isom_read.c
+++ src/isomedia/isom_read.c
@@ -438,13 +438,16 @@ GF_ISOFile *gf_isom_open(const char *fileName, u32 OpenMode, const char *tmp_dir
 GF_EXPORT
 GF_Err gf_isom_get_bs(GF_ISOFile *movie, GF_BitStream **out_bs)
 {
-	if (movie->openMode != GF_ISOM_OPEN_WRITE || !movie->editFileMap) //memory mode
+	if (!movie || movie->openMode != GF_ISOM_OPEN_WRITE || !movie->editFileMap) //memory mode
 		return GF_NOT_SUPPORTED;
 
 	if (movie->segment_bs)
 		*out_bs = movie->segment_bs;
 	else
 		*out_bs = movie->editFileMap->bs;
+
+	if (movie->moof)
+		movie->moof->fragment_offset = 0;
 
 	return GF_OK;
 }
