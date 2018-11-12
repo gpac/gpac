b01676fcf552f59056830a1f4dc50c2c5ede802b
jeanlf <jeanlf@users.sourceforge.net>
2018-11-12 11:38:25 +0100

    fixed mem leak

--- src/filters/reframe_adts.c
+++ src/filters/reframe_adts.c
@@ -706,6 +706,7 @@ static const char *adts_dmx_probe_data(const u8 *data, u32 size, GF_FilterProbeS
 		if (!hdr.nb_ch) continue;
 		nb_frames++;
 	}
+	gf_bs_del(bs);
 	if (nb_frames) {
 		*score = GF_FPROBE_SUPPORTED;
 		return "audio/aac";
@@ -741,14 +742,14 @@ static const GF_FilterArgs ADTSDmxArgs[] =
 	{ OFFS(mpeg4), "forces signaling as MPEG-4 AAC", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(ovsbr), "forces oversampling SBR (does not multiply timescales by 2)", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(sbr), "set SBR signaling:\n"\
-				"\tno: no SBR signaling at all"\
-				"\timp: backward-compatible SBR signaling (audio signaled as AAC-LC)"\
+				"\tno: no SBR signaling at all\n"\
+				"\timp: backward-compatible SBR signaling (audio signaled as AAC-LC)\n"\
 				"\texp: explicit SBR signaling (audio signaled as AAC-SBR)"\
 				, GF_PROP_UINT, "no", "no|imp|exp", GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(ps), "set PS signaling:\n"\
-				"\tno: no PS signaling at all"\
-				"\timp: backward-compatible PS signaling (audio signaled as AAC-LC)"\
-				"\texp: explicit PS signaling (audio signaled as AAC-PS)"\
+				"\tno: no PS signaling at all\n"\
+				"\timp: backward-compatible PS signaling (audio signaled as AAC-LC)\n"\
+				"\texp: explicit PS signaling (audio signaled as AAC-PS)\n"\
 				, GF_PROP_UINT, "no", "no|imp|exp", GF_FS_ARG_HINT_ADVANCED},
 	{0}
 };
