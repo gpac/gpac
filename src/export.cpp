ece7ff0d7e4c2a2a9757c1c6df208c085d997ab6
jeanlf <jeanlf@gpac.io>
2019-01-11 15:44:47 +0100

    fixed lsvc exports

--- src/export.cpp
+++ src/export.cpp
@@ -2235,6 +2235,7 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_fs_get_max_resolution_chain_length ) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_fs_get_last_connect_error ) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_fs_get_last_process_error ) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_fs_filter_exists ) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_filter_add_event_listener ) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_filter_remove_event_listener ) )
 
