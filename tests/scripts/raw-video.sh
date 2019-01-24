fcbfd4e0d784d7745a657478d7ab4dd3b5d5c275
jeanlf <jeanlf@users.sourceforge.net>
2019-01-24 18:07:11 +0100

    fixed bug in raw video tests

--- tests/scripts/raw-video.sh
+++ tests/scripts/raw-video.sh
@@ -6,7 +6,7 @@ IFCE="127.0.0.1"
 raw_test ()
 {
 
-test_begin "rawvid-$1"
+test_begin "raw-vid-$1"
 if [ $test_skip  = 1 ] ; then
 return
 fi
@@ -16,7 +16,7 @@ do_test "$GPAC -i $mp4file -o $rawfile -blacklist=vtbdec,nvdec,ohevcdec" "dump"
 do_hash_test "$rawfile" "dump"
 
 
-myinspect="inspect:speed=-1:fmt=@pn@-@cts@-@bo@@lf@"
+myinspect="inspect:fmt=@pn@-@cts@-@bo@@lf@"
 
 insfile=$TEMP_DIR/dump.txt
 do_test "$GPAC -i $rawfile:size=128x128 $myinspect:log=$insfile" "inspect"
@@ -26,13 +26,13 @@ do_hash_test "$insfile" "inspect"
 if [ $1  = "yuv" ] ; then
 
 insfile=$TEMP_DIR/dumpns.txt
-do_test "$GPAC -i $rawfile:size=128x128 $myinspect:log=$insfile" "inspect_reverseplay"
-do_hash_test "$insfile" "inspect_reverseplay"
+do_test "$GPAC -i $rawfile:size=128x128 $myinspect:speed=-1:log=$insfile" "inspect_reverse"
+do_hash_test "$insfile" "inspect_reverse"
 
 fi
 
 #check GPU output on frame 20 (first few frames are fade to black ...
-#test is comented for now (hashes are ok), due to buildbot stability with GPU access
+#test is commented for now (hashes are ok), due to buildbot stability with GPU access
 #gpudump=$TEMP_DIR/gpudump.rgb
 #do_test "$GPAC -i $rawfile:size=128x128 vout:dumpframes=20:out=$gpudump" "gpu_dump"
 #do_hash_test "$gpudump" "gpu_dump"
@@ -54,7 +54,7 @@ test_end
 mp4file="$TEMP_DIR/vid.mp4"
 $MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_video.h264:dur=1 -new $mp4file 2> /dev/null
 
-#complete lists of dormats in gpac - we don't test all of these
+#complete lists of pixel formats extensions in gpac - we don't test all of these
 #pfstr="yuv yuvl yuv2 yp2l yuv4 yp4l uyvy vyuy yuyv yvyu nv12 nv21 nv1l nv2l yuva yuvd grey gral rgb4 rgb5 rgb6 rgba argb rgb bgr xrgb rgbx xbgr bgrx rgbd rgbds rgbs rgbas"
 #the ones we test for now - nv1l is commented (no support in old ffmpeg used on gpac buildbot) and alpha not yet tested
 pfstr="yuv yuvl yuv2 yp2l yuv4 yp4l uyvy vyuy yuyv yvyu nv12 nv21 grey rgb bgr xrgb rgbx xbgr bgrx"
