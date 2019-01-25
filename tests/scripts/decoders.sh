1196772209b58da0d0982a5a47b46506e92e621d
jeanlf <jeanlf@users.sourceforge.net>
2019-01-25 17:35:30 +0100

    added decoder tests

---
+++ tests/scripts/decoders.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+test_decoder()
+{
+
+test_begin "decoder-$1"
+
+if [ $test_skip  = 1 ] ; then
+return
+fi
+
+mp4_file=$TEMP_DIR/in.mp4
+src_file=$2
+dst_file=$TEMP_DIR/$3
+
+#only test for 1 sec
+$MP4BOX -add $src_file:dur=1 -new $mp4_file 2> /dev/null
+
+do_test "$GPAC -i $mp4_file -o $dst_file -graph -stats $4"  "decoder"
+do_hash_test "$dst_file" "decoder"
+
+test_end
+
+}
+
+nvdec=`$GPAC -h filters 2>&1 | grep nvdec`
+vtbdec=`$GPAC -h filters 2>&1 | grep vtbdec`
+xvid=`$GPAC -h filters 2>&1 | grep xvid`
+
+#test png+alpha decode to raw
+test_decoder "png-imgdec" $MEDIA_DIR/auxiliary_files/logo.png "test.rgb" "-blacklist=ffdec"
+test_decoder "png-ffdec" $MEDIA_DIR/auxiliary_files/logo.png "test.rgb" "-blacklist=imgdec"
+
+#test jpg decode to raw
+test_decoder "jpg-imgdec" $MEDIA_DIR/auxiliary_files/logo.jpg "test.rgb" "-blacklist=ffdec"
+test_decoder "jpg-ffdec" $MEDIA_DIR/auxiliary_files/logo.jpg "test.rgb" "-blacklist=imgdec"
+
+#test aac decode to raw
+test_decoder "aac-faad" $MEDIA_DIR/auxiliary_files/enst_audio.aac "test.pcm" "-blacklist=ffdec"
+test_decoder "aac-ffdec" $MEDIA_DIR/auxiliary_files/enst_audio.aac "test.pcm" "-blacklist=faad"
+
+#test mp3 decode to raw
+test_decoder "mp3-maddec" $MEDIA_DIR/auxiliary_files/count_english.mp3 "test.pcm" "-blacklist=ffdec"
+test_decoder "mp3-ffdec" $MEDIA_DIR/auxiliary_files/count_english.mp3 "test.pcm" "-blacklist=maddec"
+
+#test h264 decode to raw using ffmpeg
+test_decoder "avc-ffdec" $MEDIA_DIR/auxiliary_files/enst_video.h264 "test.yuv" "-blacklist=vtbdec,nvdec,ohevcdec"
+
+#test h264 decode to raw using openhevc
+test_decoder "avc-ohevc" $MEDIA_DIR/auxiliary_files/enst_video.h264 "test.yuv" "-blacklist=vtbdec,nvdec,ffdec"
+
+if [ -n "$vtbdec" ] ; then
+test_decoder "avc-vtb" $MEDIA_DIR/auxiliary_files/enst_video.h264 "test.yuv" "-blacklist=ohevcdec,nvdec,ffdec"
+fi
+
+if [ -n "$nvdec" ] ; then
+test_decoder "avc-nvdec" $MEDIA_DIR/auxiliary_files/enst_video.h264 "test.yuv" "-blacklist=ohevcdec,vtbdec,ffdec"
+fi
+
+#test hevc decode to raw using ffmpeg
+test_decoder "hevc-ffdec" $MEDIA_DIR/auxiliary_files/counter.hvc "test.yuv" "-blacklist=vtbdec,nvdec,ohevcdec"
+
+#test hevc decode to raw using openhevc
+test_decoder "hevc-ohevc" $MEDIA_DIR/auxiliary_files/counter.hvc "test.yuv" "-blacklist=vtbdec,nvdec,ffdec"
+
+#latest OSX releases breaks decoding of our counter sequence !! Commented for now until we find a fix
+#if [ -n "$vtbdec" ] ; then
+#test_decoder "hevc-vtb" $MEDIA_DIR/auxiliary_files/counter.hvc "test.yuv" "-blacklist=ohevcdec,nvdec,ffdec"
+#fi
+
+#if [ -n "$nvdec" ] ; then
+#test_decoder "hevc-nvdec" $MEDIA_DIR/auxiliary_files/counter.hvc "test.yuv" "-blacklist=ohevcdec,vtbdec,ffdec"
+#fi
+
+#test av1
+test_decoder "av1" $MEDIA_DIR/auxiliary_files/video.av1 "test.yuv" "" 1
+
+
+ if [ $EXTERNAL_MEDIA_AVAILABLE = 0 ] ; then
+  return
+ fi
+
+test_decoder "amr-ffdec" $EXTERNAL_MEDIA_DIR/import/bear_audio.amr "test.pcm" ""
+
+test_decoder "amrwb-ffdec" $EXTERNAL_MEDIA_DIR/import/obrother_wideband.amr "test.pcm" ""
+
+test_decoder "h263-ffdec" $EXTERNAL_MEDIA_DIR/import/bear_video.263 "test.yuv" "-blacklist=vtbdec"
+if [ -n "$vtbdec" ] ; then
+test_decoder "h263-vtb" $EXTERNAL_MEDIA_DIR/import/bear_video.263 "test.yuv" "-blacklist=ffdec"
+fi
+
+test_decoder "qcp-ffdec" $EXTERNAL_MEDIA_DIR/import/count_english.qcp "test.pcm" ""
+
+test_decoder "m1v-ffdec" $EXTERNAL_MEDIA_DIR/import/dead.m1v "test.yuv" ""
