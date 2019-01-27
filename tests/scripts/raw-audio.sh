b8eb5d4fbeddb164f30a2c0b57e0ff8ac632fab5
jeanlf <jeanlf@users.sourceforge.net>
2019-01-27 19:42:13 +0100

    fixed raw audio tests

--- tests/scripts/raw-audio.sh
+++ tests/scripts/raw-audio.sh
@@ -14,7 +14,7 @@ fi
 rawfile=$TEMP_DIR/raw.$1
 #test dumping to the given format
 do_test "$GPAC -i $mp4file -o $rawfile" "dump-$1"
-do_hash_test "$rawfile" "dump-$1"
+do_hash_test_bin "$rawfile" "dump-$1" 1
 
 myinspect="inspect:fmt=@pn@-@cts@-@bo@@lf@"
 insfile=$TEMP_DIR/dump.txt
@@ -24,7 +24,7 @@ do_hash_test "$insfile" "inspect"
 #test reading from the given format into pcm
 rawfile2=$TEMP_DIR/raw2.pcm
 do_test "$GPAC -i $rawfile -o $rawfile2" "dump-pcm"
-do_hash_test "$rawfile2" "dump-pcm"
+do_hash_test_bin "$rawfile2" "dump-pcm" 1
 
 #only do the reverse tests for pcm (same for the other formats)
 if [ $1  = "pcm" ] ; then
@@ -46,8 +46,9 @@ fi
 test_end
 }
 
+#we do a test with 0.4 seconds. using more results in higher dynamics in the signal which are not rounded the same way on all platforms
 mp4file="$TEMP_DIR/aud.mp4"
-$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_audio.aac:dur=1 -new $mp4file 2> /dev/null
+$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_audio.aac:dur=0.4 -new $mp4file 2> /dev/null
 
 #complete lists of audio formats extensions in gpac
 afstr="pc8 pcm s24 s32 flt dbl pc8p pcmp s24p s32p fltp dblp"
