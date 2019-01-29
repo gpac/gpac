c3b8bf88a7a4b5554b4ad46942ff4dc75d6aed46
jeanlf <jeanlf@users.sourceforge.net>
2019-01-29 12:12:31 +0100

    disable j2k and m1v tests

--- tests/scripts/mp4box-io.sh
+++ tests/scripts/mp4box-io.sh
@@ -44,8 +44,13 @@ mp4_test ()
   do_hint=0 ;;
  *.jpeg )
   do_hint=0 ;;
+ # jp2, mjp2 and m1v disabled
  *.jp2 )
-  do_hint=0 ;;
+  return ;;
+ *.mj2 )
+  return ;;
+ *.m1v )
+  return ;;
  *.av1 )
   do_hint=0 ;;
  *.obu )
@@ -53,8 +58,6 @@ mp4_test ()
  *.ivf )
   do_hint=0 ;;
  *.png )
-  do_hint=0 ;;
- *.mj2 )
   do_hint=0 ;;
  *.qcp )
   do_play=0 ;;
