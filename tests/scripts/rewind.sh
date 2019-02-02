ed20130591513dfb1921a99b773b72e3a06dd0ee
jeanlf <jeanlf@users.sourceforge.net>
2019-02-02 15:20:06 +0100

    added inspect test in rewind

--- tests/scripts/rewind.sh
+++ tests/scripts/rewind.sh
@@ -4,21 +4,28 @@
 
 mp4file="$TEMP_DIR/file.mp4"
 
+myinspect="inspect:testmode:fmt=@pn@-@dts@-@cts@@lf@"
 
 test_video()
 {
-#rewind needs negative speed playback support, build an MP4
-$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_video.h264:dur=1 -new $mp4file 2> /dev/null
-
 test_begin "rewind-video"
 
 if [ $test_skip  = 1 ] ; then
 return
 fi
 
+#rewind needs negative speed playback support, build an MP4
+$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_video.h264:dur=1 -new $mp4file 2> /dev/null
+
+
 dumpfile=$TEMP_DIR/dump.yuv
 do_test "$GPAC -i $mp4file rewind @ -o $dumpfile:speed=-1 -blacklist=vtbdec,ohevcdec,nvdec"  "rewind"
 do_hash_test "$dumpfile" "rewind"
+
+#do a hash on inspect
+insfile=$TEMP_DIR/dump.txt
+do_test "$GPAC -i $dumpfile:size=128x128 $myinspect:log=$insfile" "inspect"
+do_hash_test "$insfile" "inspect"
 
 test_end
 
@@ -28,8 +35,11 @@ test_end
 
 test_audio()
 {
-#rewind needs negative speed playback support, build an MP4
-$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_audio.aac:dur=0.4 -new $mp4file 2> /dev/null
+test_begin "rewind-audio"
+
+if [ $test_skip  = 1 ] ; then
+return
+fi
 
 do_pcm_hashes=1
 #on linux 32 bit we for now disable the hashes, they all differ due to different float/double precision
@@ -43,17 +53,19 @@ do_pcm_hashes=0
 fi
 fi
 
-test_begin "rewind-audio"
-
-if [ $test_skip  = 1 ] ; then
-return
-fi
+#rewind needs negative speed playback support, build an MP4
+$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_audio.aac:dur=0.4 -new $mp4file 2> /dev/null
 
 dumpfile=$TEMP_DIR/dump.pcm
 do_test "$GPAC -i $mp4file rewind @ -o $dumpfile:speed=-1"  "rewind"
 if [ $do_pcm_hashes != 0 ] ; then
 do_hash_test "$dumpfile" "rewind"
 fi
+
+#do a hash on inspect
+insfile=$TEMP_DIR/dump.txt
+do_test "$GPAC -i $dumpfile:sr=48k:ch=2 $myinspect:log=$insfile" "inspect"
+do_hash_test "$insfile" "inspect"
 
 test_end
 }
