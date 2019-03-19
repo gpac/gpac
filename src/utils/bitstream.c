8686b06198925d5470afee5f2b537667efe28b9c
jeanlf <jeanlf@users.sourceforge.net>
2019-03-19 12:43:43 +0100

    fixed bug in EPB remove in bitstream

--- src/utils/bitstream.c
+++ src/utils/bitstream.c
@@ -1104,7 +1104,7 @@ GF_EXPORT
 u32 gf_bs_peek_bits(GF_BitStream *bs, u32 numBits, u64 byte_offset)
 {
 	u64 curPos;
-	u32 curBits, ret, current;
+	u32 curBits, ret, current, nb_zeros;
 
 	if ( (bs->bsmode != GF_BITSTREAM_READ) && (bs->bsmode != GF_BITSTREAM_FILE_READ)) return 0;
 	if (!numBits || (bs->size < bs->position + byte_offset)) return 0;
@@ -1113,6 +1113,7 @@ u32 gf_bs_peek_bits(GF_BitStream *bs, u32 numBits, u64 byte_offset)
 	curPos = bs->position;
 	curBits = bs->nbBits;
 	current = bs->current;
+	nb_zeros = bs->nb_zeros;
 
 	if (byte_offset) {
 		if (bs->remove_emul_prevention_byte) {
@@ -1131,6 +1132,7 @@ u32 gf_bs_peek_bits(GF_BitStream *bs, u32 numBits, u64 byte_offset)
 	/*to avoid re-reading our bits ...*/
 	bs->nbBits = curBits;
 	bs->current = current;
+	bs->nb_zeros = nb_zeros;
 	return ret;
 }
 
