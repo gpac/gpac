0d0e6301c86921389d0eb466dcb3e247192569e6
jeanlf <jeanlf@users.sourceforge.net>
2019-04-15 17:32:47 +0200

    fixes in inspect stream order in non-interleave mode

--- src/filters/inspect.c
+++ src/filters/inspect.c
@@ -515,12 +515,28 @@ static GF_Err inspect_config_input(GF_Filter *filter, GF_FilterPid *pid, Bool is
 	pctx->src_pid = pid;
 	gf_filter_pid_set_udta(pid, pctx);
 	if (! ctx->interleave) {
+		const GF_PropertyValue *p;
 		pctx->tmp = gf_temp_file_new(NULL);
+
+		//in non interleave mode, log audio first and video after - mostly used for tests where we always need th same output
+		p = gf_filter_pid_get_property(pid, GF_PROP_PID_STREAM_TYPE);
+		if (p && (p->value.uint==GF_STREAM_AUDIO)) {
+			u32 i;
+			gf_list_insert(ctx->src_pids, pctx, 0);
+
+			for (i=0; i<gf_list_count(ctx->src_pids); i++) {
+				PidCtx *actx = gf_list_get(ctx->src_pids, i);
+				actx->idx = i+1;
+			}
+		} else {
+			gf_list_add(ctx->src_pids, pctx);
+		}
 	} else {
 		pctx->tmp = ctx->dump;
+		gf_list_add(ctx->src_pids, pctx);
 	}
-	gf_list_add(ctx->src_pids, pctx);
-	pctx->idx = gf_list_count(ctx->src_pids);
+
+	pctx->idx = gf_list_find(ctx->src_pids, pctx) + 1;
 
 	switch (ctx->mode) {
 	case INSPECT_MODE_PCK:
