28c3a49c435b93a2ac0f22d463891d6974411dfb
jeanlf <jeanlf@users.sourceforge.net>
2019-04-16 16:19:50 +0200

    fixes in avi mux test

--- tests/scripts/mux_avi.sh
+++ tests/scripts/mux_avi.sh
@@ -10,11 +10,20 @@ fi
 dstfile="$TEMP_DIR/test.avi"
 
 do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/count_video.cmp -i $MEDIA_DIR/auxiliary_files/count_english.mp3 $2 -o $dstfile" "mux"
+if [ $3 != 0 ] ; then
 do_hash_test $dstfile "mux"
+else
+
+myinspect=$TEMP_DIR/inspect.txt
+do_test "$GPAC -i $dstfile inspect:all:fmt=%cts%-%size%%lf%:interleave=false:log=$myinspect -graph -stats"
+do_hash_test $myinspect "inspect"
+
+fi
 
 test_end
 
 }
 
-test_avimx "compressed" ""
-test_avimx "uncompressed" "-blacklist=vtbdec,nvdec reframer:raw @"
+test_avimx "compressed" "" 1
+#don't hash uncompressed file
+test_avimx "uncompressed" "-blacklist=vtbdec,nvdec reframer:raw @" 0
