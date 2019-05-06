8108f9c75c2f01addf297879f8dd04d3d01c9b90
jeanlf <jeanlf@users.sourceforge.net>
2019-05-06 09:53:01 +0200

    fixed sub-box parsing bug in unknown sample entries read

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -4099,7 +4099,10 @@ void gen_sample_entry_del(GF_Box *s)
 
 GF_Err gen_sample_entry_Read(GF_Box *s, GF_BitStream *bs)
 {
-	return gf_isom_base_sample_entry_read((GF_SampleEntryBox *)s, bs);
+	GF_Err e = gf_isom_base_sample_entry_read((GF_SampleEntryBox *)s, bs);
+	if (e) return e;
+	ISOM_DECREASE_SIZE(s, 8);
+	return gf_isom_box_array_read(s, bs, gf_isom_box_add_default);
 }
 
 GF_Box *gen_sample_entry_New()
