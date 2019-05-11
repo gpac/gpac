6fd0d0b6f1e9ffc709706a39c0973a35ce6d6e04
jeanlf <jeanlf@users.sourceforge.net>
2019-05-11 11:17:13 +0200

    fixes in audelim test

--- tests/scripts/audelim.sh
+++ tests/scripts/audelim.sh
@@ -23,8 +23,8 @@ do_test "$MP4BOX -add $rawfile:xps_inband -new $mp4file" "xpsinband"
 do_hash_test $mp4file "xpsinband"
 
 mp4file="$TEMP_DIR/test-xpsib-aud.mp4"
-do_test "$MP4BOX -add $rawfile:xps_inband:au_delim -new $mp4file" "xpsinband:audelim"
-do_hash_test $mp4file "xpsinband:audelim"
+do_test "$MP4BOX -add $rawfile:xps_inband:au_delim -new $mp4file" "xpsinband_audelim"
+do_hash_test $mp4file "xpsinband_audelim"
 
 test_end
 
