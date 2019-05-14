1ed69544d11764b94b40f53faec7a9bdb691173e
jeanlf <jeanlf@users.sourceforge.net>
2019-05-14 15:48:57 +0200

    added id3 genre test

--- tests/scripts/mp4box-tags.sh
+++ tests/scripts/mp4box-tags.sh
@@ -6,8 +6,10 @@ return
 fi
 
 mp4file="$TEMP_DIR/test.mp4"
-do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_audio.aac -new $mp4file -itags cover=$MEDIA_DIR/auxiliary_files/logo.png" "create"
+do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_audio.aac -new $mp4file -itags cover=$MEDIA_DIR/auxiliary_files/logo.png:genre=Game" "create"
 do_hash_test $mp4file "create"
+
+do_test "$MP4BOX -info $mp4file" "info"
 
 covfile="$TEMP_DIR/test.png"
 do_test "$MP4BOX -dump-cover $mp4file -out $covfile" "dumpcover"
