68d8487558f78cc9265a7528bfe0e91cd40389e3
jeanlf <jeanlf@users.sourceforge.net>
2019-05-14 15:22:41 +0200

    added dump-udta test

--- tests/scripts/mp4box-udta.sh
+++ tests/scripts/mp4box-udta.sh
@@ -6,14 +6,23 @@ return
 fi
 
 mp4file="$TEMP_DIR/udtamoov.mp4"
+binfile="$TEMP_DIR/dumpmoov.srt"
 
-do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/logo.png -udta ID=0:type=GPAC:src=$MEDIA_DIR/auxiliary_files/subtitle.srt $mp4file" "udta-moov"
+do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/logo.png -udta ID=0:type=GPAC:src=$MEDIA_DIR/auxiliary_files/subtitle.srt -new $mp4file" "udta-moov"
 do_hash_test $mp4file "udta-moov"
 
-mp4file="$TEMP_DIR/udtatrack.mp4"
+do_test "$MP4BOX -dump-udta GPAC $mp4file -out $binfile" "udta-moov-dump"
+do_hash_test $binfile "udta-moov-dump"
 
-do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/logo.png -udta ID=1:type=GPAC:src=base64,dGVzdA== $mp4file" "udta-track"
+
+mp4file="$TEMP_DIR/udtatrack.mp4"
+binfile="$TEMP_DIR/dumptrack.bin"
+
+do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/logo.png -udta ID=1:type=GPAC:src=base64,dGVzdA== -new $mp4file" "udta-track"
 do_hash_test $mp4file "udta-track"
+
+do_test "$MP4BOX -dump-udta 1:GPAC $mp4file -out $binfile" "udta-track-dump"
+do_hash_test $binfile "udta-track-dump"
 
 test_end
 
