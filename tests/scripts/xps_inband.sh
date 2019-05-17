9dd29da55adeb0af0b72f8116a4c540ae563f4ea
jeanlf <jeanlf@users.sourceforge.net>
2019-05-17 18:11:15 +0200

    fixes in xps inband and added test

---
+++ tests/scripts/xps_inband.sh
@@ -0,0 +1,42 @@
+
+test_xps_switch()
+{
+ name=$(basename $1)
+ name=${name%.*}
+
+ test_begin "xps-$name"
+
+if [ $test_skip = 1 ] ; then
+ return
+fi
+
+#test xps out of band creation
+mp4="$TEMP_DIR/file_oob.mp4"
+do_test "$GPAC -i $1 -o $mp4" "create-oob"
+do_hash_test $mp4 "create-oob"
+
+#test xps out of band creation
+mp4="$TEMP_DIR/file_ib.mp4"
+do_test "$GPAC -i $1 -o $mp4:xps_inband=all" "create-ib"
+do_hash_test $mp4 "create-ib"
+
+#test inspect to make sure we rebuild properly from inband files
+inspect="$TEMP_DIR/inspect.xml"
+do_test "$GPAC -i $mp4 inspect:deep:log=$inspect" "inspect"
+do_hash_test $inspect "inspect"
+
+#test NULL decoding
+do_test "$GPAC -i $i -o null:fext=yuv" "decode"
+
+test_end
+
+}
+
+
+
+for i in $EXTERNAL_MEDIA_DIR/xps_switch/* ; do
+
+test_xps_switch $i
+
+done
+
