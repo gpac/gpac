0360213aeedceb9a6292449d661d1d8eda3575bb
jeanlf <jeanlf@users.sourceforge.net>
2019-06-04 15:50:34 +0200

    use insect for resampler instead of bin hash

--- tests/scripts/resample.sh
+++ tests/scripts/resample.sh
@@ -18,13 +18,16 @@ fi
 
 dumpfile=$TEMP_DIR/dump.pcm
 do_test "$GPAC -blacklist=ffdec -i $1:index_dur=0 resample$2 @ -o $dumpfile:sstart=1:send=250"  "resample"
-do_hash_test "$dumpfile" "resample"
+
+myinspect=$TEMP_DIR/inspect.txt
+do_test "$GPAC -i $dumpfile$3 inspect:deep:all:fmt=%pn%-%cts%-%size%%lf%:log=$myinspect"  "inspect"
+do_hash_test "$myinspect" "inspect"
 
 test_end
 
 }
 
-test_resample "$EXTERNAL_MEDIA_DIR/import//aac_vbr_51_128k.aac" ":ch=2"
-test_resample "$MEDIA_DIR/auxiliary_files/enst_audio.aac" ":sr=22050"
+test_resample "$EXTERNAL_MEDIA_DIR/import//aac_vbr_51_128k.aac" ":ch=2" ":ch=2:sr=22050"
+test_resample "$MEDIA_DIR/auxiliary_files/enst_audio.aac" ":sr=22050" ":ch=2:sr=22050"
 
 
