1f0671bc6fc4fee7ecedcb802416f496ca09c7cc
jeanlf <jeanlf@users.sourceforge.net>
2019-06-04 16:41:12 +0200

    fixes in aviraw

--- tests/scripts/mux_avi.sh
+++ tests/scripts/mux_avi.sh
@@ -12,6 +12,11 @@ dstfile="$TEMP_DIR/test.avi"
 do_test "$GPAC -i $MEDIA_DIR/auxiliary_files/count_video.cmp -i $MEDIA_DIR/auxiliary_files/count_english.mp3 $2 -o $dstfile" "mux"
 if [ $3 != 0 ] ; then
 do_hash_test $dstfile "mux"
+
+do_test "$MP4BOX -aviraw video $dstfile" "aviraw-video"
+do_hash_test $TEMP_DIR/test_video.cmp "aviraw-video"
+do_test "$MP4BOX -aviraw audio $dstfile" "aviraw-audio"
+do_hash_test $TEMP_DIR/test_audio.mp3 "aviraw-audio"
 fi
 
 myinspect=$TEMP_DIR/inspect.txt
