8ca88460cb294dcf3c83cb6dc3a63314b386a3aa
Aurelien David <aurelien.david@telecom-paristech.fr>
2019-06-04 15:56:54 +0200

    make_tests: fix nullglob option

--- tests/scripts/hevcsplit.sh
+++ tests/scripts/hevcsplit.sh
@@ -7,7 +7,6 @@ test_begin "hevcsplit-$1"
 if [ $test_skip = 1 ] ; then
 return
 fi
-
 do_test "$GPAC -i $EXTERNAL_MEDIA_DIR/counter/counter_1280_720_I_25_tiled_$1.hevc hevcsplit:FID=1 -o $TEMP_DIR/dump_\$CropOrigin\$.hvc:SID=1#CropOrigin=*" "split"
 
 for i in $TEMP_DIR/*.hvc ; do
@@ -20,7 +19,6 @@ do_hash_test "$i" "$name"
 do_play_test "play" "$i -blacklist=nvdec" ""
 
 done
-
 test_end
 }
 
