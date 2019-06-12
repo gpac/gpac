07156ddb22db98cc60bbd8f5de0b6e302cc05183
jeanlf <jeanlf@users.sourceforge.net>
2019-06-12 16:40:34 +0200

    fixed file header

--- src/filters/hevccombine.c
+++ src/filters/hevccombine.c
@@ -1,9 +1,30 @@
 /*
-*
-*
-*
-*/
-
+ *			GPAC - Multimedia Framework C SDK
+ *
+ *			Authors: Jean Le Feuvre
+ *					 Yacine Mathurin Boubacar Aziakou
+ *					 Samir Mustapha
+ *			Copyright (c) Telecom ParisTech 2019
+ *					All rights reserved
+ *
+ *  This file is part of GPAC / HEVC tile split and rewrite filter
+ *
+ *  GPAC is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  GPAC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+ 
 #include <gpac/bitstream.h>
 #include <gpac/filters.h>
 #include <gpac/avparse.h>
