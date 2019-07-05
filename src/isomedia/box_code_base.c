a9540e0ecf81905a15e667d933293481ea07f036
jeanlf <jeanlf@users.sourceforge.net>
2019-07-05 14:05:19 +0200

    fixed potential crash cf #1262

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -8618,25 +8618,10 @@ void txtc_del(GF_Box *s)
 
 GF_Err txtc_Read(GF_Box *s, GF_BitStream *bs)
 {
-	u32 size, i;
-	char *str;
 	GF_TextConfigBox *ptr = (GF_TextConfigBox*)s;
-
-	size = (u32) ptr->size;
-	str = (char *)gf_malloc(sizeof(char)*size);
-
-	i=0;
-
-	while (size) {
-		str[i] = gf_bs_read_u8(bs);
-		size--;
-		if (!str[i])
-			break;
-		i++;
-	}
-	if (i) ptr->config = gf_strdup(str);
-	gf_free(str);
-
+	ptr->config = (char *)gf_malloc(sizeof(char)*((u32) ptr->size+1));
+	gf_bs_read_data(bs, ptr->config, (u32) ptr->size);
+	ptr->config[ptr->size] = 0;
 	return GF_OK;
 }
 
