bf30f38b280d96cf252bfbd61c1d0c12653e57f7
jeanlf <jeanlf@users.sourceforge.net>
2019-07-22 15:48:29 +0200

    fixed android compil

--- applications/osmo4_android_studio/app/src/main/jni/wrapper_jni.hpp
+++ applications/osmo4_android_studio/app/src/main/jni/wrapper_jni.hpp
@@ -24,7 +24,7 @@ extern "C" {
                           return;\
                         }\
                         jlong h = env->GetLongField(obj, fid);\
-                        CNativeWrapper* wr = static_cast<CNativeWrapper*> (h);
+                        CNativeWrapper* wr = reinterpret_cast<CNativeWrapper*> (h);
 //                        __android_log_print(ANDROID_LOG_VERBOSE, jniTAG, "Handle = %p", wr);
 
 /*
