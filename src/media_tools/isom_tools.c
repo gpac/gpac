ac7d1b513841606d40616a9998dd1334454db217
jeanlf <jeanlf@users.sourceforge.net>
2019-07-25 14:02:59 +0200

    moved prores log on missing color from error to warning

--- src/media_tools/isom_tools.c
+++ src/media_tools/isom_tools.c
@@ -922,7 +922,7 @@ GF_Err gf_media_check_qt_prores(GF_ISOFile *mp4)
 	//todo: patch colr
 	e = gf_isom_get_color_info(mp4, video_tk, 1, &colour_type, &colour_primaries, &transfer_characteristics, &matrix_coefficients, &full_range_flag);
 	if (e==GF_NOT_FOUND) {
-		GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("[ProRes] No color info present in visual track, defaulting to BT709\n"));
+		GF_LOG(GF_LOG_WARNING, GF_LOG_AUTHOR, ("[ProRes] No color info present in visual track, defaulting to BT709\n"));
 		gf_isom_set_visual_color_info(mp4, video_tk, 1, GF_4CC('n','c','l','c'), 1, 1, 1, GF_FALSE, NULL, 0);
 	} else if (e) {
 		return e;
