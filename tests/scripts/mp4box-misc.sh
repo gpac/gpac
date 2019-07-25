bdea228fc584f4b70c9c85e145b46c8b0226691c
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2019-07-25 23:08:26 +0200

    mp4box: add test for hdr.xml (mdcv/clli injection)

--- tests/scripts/mp4box-misc.sh
+++ tests/scripts/mp4box-misc.sh
@@ -63,7 +63,7 @@ do_hash_test "$TEMP_DIR/testrvc_info.xml" "add-rvc"
 
 mp4file="$TEMP_DIR/hdr.mp4"
 do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_video.h264 -hdr $MEDIA_DIR/auxiliary_files/hdr.xml -new $mp4file" "hdr"
-do_hash_test $mp4file" "hdr"
+do_hash_test "$mp4file" "hdr"
 
 test_end
 
