e83058e899faa4d9936934c2ee2cb15390806a4b
jeanlf <jeanlf@users.sourceforge.net>
2019-07-26 10:27:23 +0200

    fixed hdr test

--- tests/scripts/mp4box-misc.sh
+++ tests/scripts/mp4box-misc.sh
@@ -8,7 +8,7 @@ fi
 
 mp4file="$TEMP_DIR/hdr.mp4"
 do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_video.h264 -hdr $MEDIA_DIR/auxiliary_files/hdr.xml -new $mp4file" "hdr"
-do_hash_test $mp4file" "hdr"
+do_hash_test "$mp4file" "hdr"
 
 test_end
 
