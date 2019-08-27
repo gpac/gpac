gpac/gpac

commit 6a089b52ee148fd5661e18d5b485917bdd784650

support for single segment encryption - cf #924

src/filters/mux_isom.c
@@ -180,6 +180,8 @@ typedef struct
 	s32 mediats;
 	GF_AudioSampleEntryImportMode ase;
 	char *styp;
+	Bool sseg;
+
 
 	//internal
 	Bool owns_mov;
@@ -688,6 +690,10 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 			e = mp4mx_setup_dash_vod(ctx, tkw);
 			if (e) return e;
 		}
+	}
+	//we consider that when muxing single segments, we are always in DASH, not VoD mode
+	else if (ctx->noinit) {
+		ctx->dash_mode = MP4MX_DASH_ON;
 	}
 
 	if (!ctx->cdur_set) {
@@ -1457,7 +1463,7 @@ sample_entry_setup:
 			tkw->avcc = NULL;
 		}
 		//patch to align old arch with filters
-		if (!ctx->dash_mode && !ctx->make_qt)
+		if (!ctx->dash_mode && !ctx->make_qt && !gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_ISOM_TREX_TEMPLATE) )
 			gf_isom_set_pl_indication(ctx->file, GF_ISOM_PL_VISUAL, 0x7F);
 
 		if (!tkw->has_brands)
@@ -2309,10 +2315,12 @@ static GF_Err mp4_mux_cenc_update(GF_MP4MuxCtx *ctx, TrackWriter *tkw, GF_Filter
 		memcpy(KID, p->value.data.ptr, 16);
 	}
 
-	p = gf_filter_pck_get_property(pck, GF_PROP_PCK_CENC_SAI);
-	if (p) {
-		sai = p->value.data.ptr;
-		sai_size = p->value.data.size;
+	if (pck) {
+		p = gf_filter_pck_get_property(pck, GF_PROP_PCK_CENC_SAI);
+		if (p) {
+			sai = p->value.data.ptr;
+			sai_size = p->value.data.size;
+		}
 	}
 
 
@@ -2359,7 +2367,9 @@ static GF_Err mp4_mux_cenc_update(GF_MP4MuxCtx *ctx, TrackWriter *tkw, GF_Filter
 	}
 	if (act_type==CENC_CONFIG) return GF_OK;
 
-	pck_is_encrypted = gf_filter_pck_get_crypt_flags(pck);
+	pck_is_encrypted = GF_FALSE;
+	if (pck)
+		pck_is_encrypted = gf_filter_pck_get_crypt_flags(pck);
 
 	//!! tkw->nb_samples / tkw->samples_in_frag not yet incremented !!
 	if (act_type == CENC_ADD_FRAG)
@@ -2982,6 +2992,9 @@ static GF_Err mp4_mux_initialize_movie(GF_MP4MuxCtx *ctx)
 	max_dur.den = 1;
 	max_dur.num = 0;
 
+	if (ctx->sseg)
+		ctx->single_file = GF_FALSE;
+
 	if (ctx->idur.num && ctx->idur.den) {
 		max_dur.num = ctx->idur.num;
 		max_dur.den = ctx->idur.den;
@@ -3407,9 +3420,11 @@ static GF_Err mp4_mux_process_fragmented(GF_Filter *filter, GF_MP4MuxCtx *ctx)
 			if (!ctx->fragment_started) {
 				e = mp4_mux_start_fragment(ctx);
 				if (e) return e;
+
 				ctx->nb_frags++;
 				if (ctx->dash_mode)
 					ctx->nb_frags_in_seg++;
+
 			}
 
 			if (ctx->dash_mode) {
@@ -3531,7 +3546,7 @@ static GF_Err mp4_mux_process_fragmented(GF_Filter *filter, GF_MP4MuxCtx *ctx)
 			u64 idx_start_range, idx_end_range, segment_size_in_bytes;
 			idx_start_range = idx_end_range = 0;
 
-			gf_isom_close_segment(ctx->file, (ctx->subs_sidx >= 0) ? ctx->subs_sidx : 0, (ctx->subs_sidx>=0) ? ctx->ref_tkw->track_id : 0, ctx->ref_tkw->first_dts_in_seg, ctx->ref_tkw->ts_delay, next_ref_ts, ctx->chain_sidx, ctx->ssix, is_eos, GF_FALSE, ctx->eos_marker, &idx_start_range, &idx_end_range, &segment_size_in_bytes);
+			gf_isom_close_segment(ctx->file, (ctx->subs_sidx >= 0) ? ctx->subs_sidx : 0, (ctx->subs_sidx>=0) ? ctx->ref_tkw->track_id : 0, ctx->ref_tkw->first_dts_in_seg, ctx->ref_tkw->ts_delay, next_ref_ts, ctx->chain_sidx, ctx->ssix, ctx->sseg ? GF_FALSE : is_eos, GF_FALSE, ctx->eos_marker, &idx_start_range, &idx_end_range, &segment_size_in_bytes);
 
 			GF_LOG(GF_LOG_DEBUG, GF_LOG_CONTAINER, ("[MP4Mux] Done writing segment %d - estimated next fragment times start %g end %g\n", ctx->dash_seg_num, ref_start, ctx->next_frag_start ));
 
@@ -3635,9 +3650,24 @@ static void mp4_mux_config_timing(GF_MP4MuxCtx *ctx)
 		GF_FilterPacket *pck;
 		TrackWriter *tkw = gf_list_get(ctx->tracks, i);
 		if (tkw->fake_track) continue;
-		
+		if (!tkw->track_num) return;
+
 		pck = gf_filter_pid_get_packet(tkw->ipid);
-		if (!tkw->track_num || !pck) return;
+		if (!pck) {
+			if (gf_filter_pid_is_eos(tkw->ipid)) {
+				if (tkw->cenc_state==1)
+					mp4_mux_cenc_update(ctx, tkw, NULL, CENC_CONFIG, 0);
+
+				if (!tkw->nb_samples) {
+					const GF_PropertyValue *p = gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_ISOM_TREX_TEMPLATE);
+					if (p) {
+						gf_isom_setup_track_fragment_template(ctx->file, tkw->track_id, p->value.data.ptr, p->value.data.size, ctx->nofragdef);
+					}
+				}
+				continue;
+			}
+			return;
+		}
 		ts = gf_filter_pck_get_dts(pck);
 		if (ts==GF_FILTER_NO_TS)
 			ts = gf_filter_pck_get_cts(pck);
@@ -3651,6 +3681,9 @@ static void mp4_mux_config_timing(GF_MP4MuxCtx *ctx)
 		}
 		tkw->ts_shift = ts;
 	}
+	if (first_ts_min==(u64)-1)
+		first_ts_min = 0;
+
 	//for all packets with dts greater than min dts, we need to add a pause
 	for (i=0; i<count; i++) {
 		s64 dts_diff;
@@ -3790,7 +3823,6 @@ GF_Err mp4_mux_process(GF_Filter *filter)
 			return GF_OK;
 		}
 	}
-
 
 	//in frag mode, force fetching a sample from each track before processing
 	if (ctx->store>=MP4MX_MODE_FRAG) {
@@ -3948,6 +3980,13 @@ static GF_Err mp4_mux_initialize(GF_Filter *filter)
 	} else {
 		u32 open_mode = GF_ISOM_OPEN_WRITE;
 		ctx->owns_mov = GF_TRUE;
+
+		if (ctx->sseg) {
+			ctx->noinit = GF_TRUE;
+			ctx->cdur = GF_MAX_DOUBLE;
+			ctx->store = MP4MX_MODE_FRAG;
+		}
+
 		switch (ctx->store) {
 		case MP4MX_MODE_INTER:
 		case MP4MX_MODE_TIGHT:
@@ -4376,6 +4415,8 @@ static const GF_FilterArgs MP4MuxArgs[] =
 	{ OFFS(ssix), "create ssix when sidx is present, level 1 mappping I-frames byte ranges, level 0xFF mapping the rest", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(ccst), "insert coding constraint box for video tracks", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(maxchunk), "set max chunk size in bytes for runs (only used in non-fragmented mode). 0 means no constraints", GF_PROP_UINT, "0", NULL, GF_FS_ARG_HINT_ADVANCED},
+
+	{ OFFS(sseg), "set single segment mode for dash", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_HIDE},
 
 	{ OFFS(block_size), "target output block size, 0 for default internal value (10k)", GF_PROP_UINT, "10000", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{0}

src/media_tools/crypt_tools.c
@@ -219,7 +219,7 @@ static void cryptinfo_node_start(void *sax_cbck, const char *node_name, const ch
 				}
 			}
 			else if (!stricmp(att->name, "keyRoll")) {
-				if (!strncmp(att->value, "idx=", 4))
+				if (!strnicmp(att->value, "idx=", 4))
 					tkc->defaultKeyIdx = atoi(att->value+4);
 				else if (!strncmp(att->value, "roll=", 5))
 					tkc->keyRoll = atoi(att->value+5);
@@ -437,10 +437,11 @@ GF_Err gf_decrypt_file(GF_ISOFile *mp4, const char *drm_file, const char *dst_fi
 	return e;
 }
 
-GF_EXPORT
-GF_Err gf_crypt_file(GF_ISOFile *mp4, const char *drm_file, const char *dst_file, Double interleave_time)
+static GF_Err gf_crypt_file_ex(GF_ISOFile *mp4, const char *drm_file, const char *dst_file, Double interleave_time, const char *fragment_name)
 {
-	char szArgs[4096];
+	char *szArgs=NULL;
+	char an_arg[100];
+
 	GF_Filter *src, *dst, *crypt;
 	GF_FilterSession *fsess;
 	GF_Err e = GF_OK;
@@ -451,8 +452,17 @@ GF_Err gf_crypt_file(GF_ISOFile *mp4, const char *drm_file, const char *dst_file
 		return GF_OUT_OF_MEM;
 	}
 
-	sprintf(szArgs, "mp4dmx:mov=%p", mp4);
+	sprintf(an_arg, "mp4dmx:mov=%p", mp4);
+	gf_dynstrcat(&szArgs, an_arg, NULL);
+	if (fragment_name) {
+		gf_dynstrcat(&szArgs, ":catseg=", NULL);
+		gf_dynstrcat(&szArgs, fragment_name, NULL);
+	}
 	src = gf_fs_load_filter(fsess, szArgs);
+
+	gf_free(szArgs);
+	szArgs = NULL;
+
 
 	if (!src) {
 		gf_fs_del(fsess);
@@ -460,17 +470,31 @@ GF_Err gf_crypt_file(GF_ISOFile *mp4, const char *drm_file, const char *dst_file
 		return GF_FILTER_NOT_FOUND;
 	}
 
-	sprintf(szArgs, "cecrypt:FID=1:cfile=%s", drm_file);
+	gf_dynstrcat(&szArgs, "cecrypt:FID=1:cfile=", NULL);
+	gf_dynstrcat(&szArgs, drm_file, NULL);
 	crypt = gf_fs_load_filter(fsess, szArgs);
+
+	gf_free(szArgs);
+	szArgs = NULL;
+
 	if (!crypt) {
 		gf_fs_del(fsess);
 		GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("[Encrypter] Cannot load encryptor\n"));
 		return GF_FILTER_NOT_FOUND;
 	}
 
-	sprintf(szArgs, "SID=1:cdur=%g", interleave_time);
-		
+	gf_dynstrcat(&szArgs, "SID=1", NULL);
+	if (fragment_name)
+		gf_dynstrcat(&szArgs, ":sseg", NULL);
+	else {
+		sprintf(an_arg, ":cdur=%g", interleave_time);
+		gf_dynstrcat(&szArgs, an_arg, NULL);
+	}
+
 	dst = gf_fs_load_destination(fsess, dst_file, szArgs, NULL, &e);
+
+	gf_free(szArgs);
+	szArgs = NULL;
 	if (!dst) {
 		gf_fs_del(fsess);
 		GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("[Encrypter] Cannot load destination muxer\n"));
@@ -486,5 +510,18 @@ GF_Err gf_crypt_file(GF_ISOFile *mp4, const char *drm_file, const char *dst_file
 	return e;
 }
 
+GF_EXPORT
+GF_Err gf_crypt_fragment(GF_ISOFile *mp4, const char *drm_file, const char *dst_file, const char *fragment_name)
+{
+	return gf_crypt_file_ex(mp4, drm_file, dst_file, 0, fragment_name);
+
+}
+
+GF_EXPORT
+GF_Err gf_crypt_file(GF_ISOFile *mp4, const char *drm_file, const char *dst_file, Double interleave_time)
+{
+	return gf_crypt_file_ex(mp4, drm_file, dst_file, interleave_time, NULL);
+
+}
 #endif /* !defined(GPAC_DISABLE_ISOM_WRITE)*/
 

applications/mp4box/filedump.c
@@ -1525,7 +1525,7 @@ void dump_isom_sdp(GF_ISOFile *file, char *inName, Bool is_final_name)
 
 #ifndef GPAC_DISABLE_ISOM_DUMP
 
-GF_Err dump_isom_xml(GF_ISOFile *file, char *inName, Bool is_final_name, Bool do_track_dump, Bool merge_vtt_cues)
+GF_Err dump_isom_xml(GF_ISOFile *file, char *inName, Bool is_final_name, Bool do_track_dump, Bool merge_vtt_cues, Bool skip_init)
 {
 	GF_Err e;
 	FILE *dump = stdout;
@@ -1550,7 +1550,7 @@ GF_Err dump_isom_xml(GF_ISOFile *file, char *inName, Bool is_final_name, Bool do
 	if (do_track_dump) {
 		fprintf(dump, "<ISOBaseMediaFileTrace>\n");
 	}
-	e = gf_isom_dump(file, dump);
+	e = gf_isom_dump(file, dump, skip_init);
 	if (e) {
 		fprintf(stderr, "Error dumping ISO structure\n");
 	}

applications/mp4box/main.c
@@ -2192,7 +2192,7 @@ GF_SceneDumpFormat dump_mode;
 Double mpd_live_duration = 0;
 Bool HintIt, needSave, FullInter, Frag, HintInter, dump_rtp, regular_iod, remove_sys_tracks, remove_hint, force_new, remove_root_od;
 Bool print_sdp, print_info, open_edit, dump_cr, force_ocr, encode, do_log, do_flat, dump_srt, dump_ttxt, do_saf, dump_m2ts, dump_cart, do_hash, verbose, force_cat, align_cat, pack_wgt, single_group, clean_groups, dash_live, no_fragments_defaults, single_traf_per_moof, tfdt_per_traf, dump_nal_crc, hls_clock, do_mpd_rip, merge_vtt_cues;
-char *inName, *outName, *arg, *mediaSource, *tmpdir, *input_ctx, *output_ctx, *drm_file, *avi2raw, *cprt, *chap_file, *pes_dump, *itunes_tags, *pack_file, *raw_cat, *seg_name, *dash_ctx_file, *compress_top_boxes, *high_dynamc_range_filename;
+char *inName, *outName, *arg, *mediaSource, *tmpdir, *input_ctx, *output_ctx, *drm_file, *avi2raw, *cprt, *chap_file, *pes_dump, *itunes_tags, *pack_file, *raw_cat, *seg_name, *dash_ctx_file, *compress_top_boxes, *high_dynamc_range_filename, *use_init_seg;
 u32 track_dump_type, dump_isom, dump_timestamps;
 u32 trackID;
 Bool comp_lzma=GF_FALSE;
@@ -3594,6 +3594,11 @@ Bool mp4box_parse_args(int argc, char **argv)
 			i++;
 		}
 		else if (!stricmp(arg, "-mpd-rip")) do_mpd_rip = GF_TRUE;
+		else if (!strcmp(arg, "-init-seg")) {
+			CHECK_NEXT_ARG
+			use_init_seg = argv[i + 1];
+			i += 1;
+		}
 
 #ifndef GPAC_DISABLE_CORE_TOOLS
 		else if (!stricmp(arg, "-bin")) do_bin_xml = GF_TRUE;
@@ -4111,7 +4116,7 @@ int mp4boxMain(int argc, char **argv)
 	program_number = 0;
 	info_track_id = 0;
 	do_flat = GF_FALSE;
-	inName = outName = mediaSource = input_ctx = output_ctx = drm_file = avi2raw = cprt = chap_file = pack_file = raw_cat = high_dynamc_range_filename = NULL;
+	inName = outName = mediaSource = input_ctx = output_ctx = drm_file = avi2raw = cprt = chap_file = pack_file = raw_cat = high_dynamc_range_filename = use_init_seg = NULL;
 
 #ifndef GPAC_DISABLE_SWF_IMPORT
 	swf_flags = GF_SM_SWF_SPLIT_TIMELINE;
@@ -4851,13 +4856,34 @@ int mp4boxMain(int argc, char **argv)
 	        ) {
 		FILE *st = gf_fopen(inName, "rb");
 		Bool file_exists = 0;
+		u8 omode;
 		if (st) {
 			file_exists = 1;
 			gf_fclose(st);
 		}
 		switch (get_file_type_by_ext(inName)) {
 		case 1:
-			file = gf_isom_open(inName, (u8) (force_new ? GF_ISOM_WRITE_EDIT : (open_edit ? GF_ISOM_OPEN_EDIT : ( ((dump_isom>0) || print_info) ? GF_ISOM_OPEN_READ_DUMP : GF_ISOM_OPEN_READ) ) ), tmpdir);
+			omode =  (u8) (force_new ? GF_ISOM_WRITE_EDIT : (open_edit ? GF_ISOM_OPEN_EDIT : ( ((dump_isom>0) || print_info) ? GF_ISOM_OPEN_READ_DUMP : GF_ISOM_OPEN_READ) ) );
+
+			if (crypt) {
+				omode = GF_ISOM_OPEN_CAT_FRAGMENTS;
+				if (use_init_seg)
+					file = gf_isom_open(use_init_seg, GF_ISOM_OPEN_READ, tmpdir);
+			}
+			if (!crypt && use_init_seg) {
+				file = gf_isom_open(use_init_seg, GF_ISOM_OPEN_READ_DUMP, tmpdir);
+				if (file) {
+					e = gf_isom_open_segment(file, inName, 0, 0, 0);
+					if (e) {
+						fprintf(stderr, "Error opening segment %s: %s\n", inName, gf_error_to_string(e) );
+						gf_isom_delete(file);
+						file = NULL;
+					}
+				}
+			}
+			if (!file)
+				file = gf_isom_open(inName, omode, tmpdir);
+
 			if (!file && (gf_isom_last_error(NULL) == GF_ISOM_INCOMPLETE_FILE) && !open_edit) {
 				u64 missing_bytes;
 				e = gf_isom_open_progressive(inName, 0, 0, &file, &missing_bytes);
@@ -5063,7 +5089,7 @@ int mp4boxMain(int argc, char **argv)
 	}
 #ifndef GPAC_DISABLE_ISOM_DUMP
 	if (dump_isom) {
-		e = dump_isom_xml(file, dump_std ? NULL : (outName ? outName : outfile), outName ? GF_TRUE : GF_FALSE, (dump_isom==2) ? GF_TRUE : GF_FALSE, merge_vtt_cues);
+		e = dump_isom_xml(file, dump_std ? NULL : (outName ? outName : outfile), outName ? GF_TRUE : GF_FALSE, (dump_isom==2) ? GF_TRUE : GF_FALSE, merge_vtt_cues, use_init_seg ? GF_TRUE : GF_FALSE);
 		if (e) goto err_exit;
 	}
 	if (dump_cr) dump_isom_ismacryp(file, dump_std ? NULL : (outName ? outName : outfile), outName ? GF_TRUE : GF_FALSE);
@@ -5842,7 +5868,11 @@ int mp4boxMain(int argc, char **argv)
 			goto err_exit;
 		}
 		if (crypt == 1) {
-			e = gf_crypt_file(file, drm_file, outfile, interleaving_time);
+			if (use_init_seg) {
+				e = gf_crypt_fragment(file, drm_file, outfile, inName);
+			} else {
+				e = gf_crypt_file(file, drm_file, outfile, interleaving_time);
+			}
 		} else if (crypt ==2) {
 			e = gf_decrypt_file(file, drm_file, outfile, interleaving_time);
 		}

applications/mp4box/mp4box.h
@@ -95,7 +95,7 @@ void PrintBuiltInNodes(u32 graph_type, Bool dump_all);
 void PrintBuiltInBoxes(Bool do_cov);
 
 #ifndef GPAC_DISABLE_ISOM_DUMP
-GF_Err dump_isom_xml(GF_ISOFile *file, char *inName, Bool is_final_name, Bool do_track_dump, Bool merge_vtt_cues);
+GF_Err dump_isom_xml(GF_ISOFile *file, char *inName, Bool is_final_name, Bool do_track_dump, Bool merge_vtt_cues, Bool skip_init);
 #endif
 
 

include/gpac/crypt_tools.h
@@ -201,6 +201,15 @@ GF_Err gf_decrypt_file(GF_ISOFile *infile, const char *drm_file, const char *out
 */
 GF_Err gf_crypt_file(GF_ISOFile *infile, const char *drm_file, const char *outname, Double interleave_time);
 
+/*! encrypts a fragment
+\param infile init segment of the MP4 file to encrypt - this SHALL NOT be encrypted
+\param drm_file location of crypt info data
+\param outname location of destination file
+\param frag_name name of fragment to encrypt
+\return error code if any
+*/
+GF_Err gf_crypt_fragment(GF_ISOFile *mp4, const char *drm_file, const char *dst_file, const char *frag_name);
+
 #endif /*!defined(GPAC_DISABLE_CRYPTO) && !defined(GPAC_DISABLE_ISOM_WRITE)*/
 
 /*! @} */

include/gpac/filters.h
@@ -897,6 +897,7 @@ enum
 	GF_PROP_PID_MAX_CTS_OFFSET = GF_4CC('M','C','T','O'),
 	GF_PROP_PID_CONSTANT_DURATION = GF_4CC('S','C','T','D'),
 	GF_PROP_PID_ISOM_TRACK_TEMPLATE = GF_4CC('I','T','K','T'),
+	GF_PROP_PID_ISOM_TREX_TEMPLATE = GF_4CC('I','T','X','T'),
 	GF_PROP_PID_ISOM_STSD_TEMPLATE = GF_4CC('I','S','T','D'),
 	GF_PROP_PID_ISOM_UDTA = GF_4CC('I','M','U','D'),
 	GF_PROP_PID_ISOM_HANDLER = GF_4CC('I','H','D','L'),

include/gpac/internal/isomedia_dev.h
@@ -3591,6 +3591,8 @@ struct __tag_isom {
 	u32 on_block_out_block_size;
 	//in block disptach mode we don't have the full file, keep the position
 	u64 fragmented_file_pos;
+
+	u32 nb_box_init_seg;
 };
 
 /*time function*/

commit 8671ea99241cb421e60f94c70da906b118e5da52

fixed bugs introduced by 6a089b5

src/filters/mux_isom.c
@@ -3125,7 +3125,7 @@ static GF_Err mp4_mux_initialize_movie(GF_MP4MuxCtx *ctx)
 	max_dur.den = 1;
 	max_dur.num = 0;
 
-	if (ctx->sseg)
+	if (ctx->sseg && ctx->noinit)
 		ctx->single_file = GF_FALSE;
 
 	if (ctx->idur.num && ctx->idur.den) {
@@ -4205,12 +4205,6 @@ static GF_Err mp4_mux_initialize(GF_Filter *filter)
 	} else {
 		u32 open_mode = GF_ISOM_OPEN_WRITE;
 		ctx->owns_mov = GF_TRUE;
-
-		if (ctx->sseg) {
-			ctx->noinit = GF_TRUE;
-			ctx->cdur = 1000000000;
-			ctx->store = MP4MX_MODE_FRAG;
-		}
 
 		switch (ctx->store) {
 		case MP4MX_MODE_INTER:

src/media_tools/crypt_tools.c
@@ -534,9 +534,9 @@ static GF_Err gf_crypt_file_ex(GF_ISOFile *mp4, const char *drm_file, const char
 	}
 
 	gf_dynstrcat(&szArgs, "SID=1", NULL);
-	if (fragment_name)
-		gf_dynstrcat(&szArgs, ":sseg", NULL);
-	else {
+	if (fragment_name) {
+		gf_dynstrcat(&szArgs, ":sseg:noinit:store=frag:cdur=1000000000", NULL);
+	} else {
 		if (interleave_time) {
 			sprintf(an_arg, ":cdur=%g", interleave_time);
 			gf_dynstrcat(&szArgs, an_arg, NULL);