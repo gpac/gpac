95e627115ba3b7a892b0426b99bfa4cc2484016b
jeanlf <jeanlf@users.sourceforge.net>
2019-08-28 18:27:56 +0200

    fixed initial period tunein

--- src/media_tools/dash_client.c
+++ src/media_tools/dash_client.c
@@ -731,7 +731,6 @@ static void gf_dash_group_timeline_setup(GF_MPD *mpd, GF_DASH_Group *group, u64 
 				&& (!ap->duration || (seg_end_ms<=start + ap->duration))
 			) {
 				if (i != group->dash->active_period_index) {
-					group->dash->initial_period_tunein = GF_FALSE;
 					group->dash->reinit_period_index = 1+i;
 					group->dash->start_range_period = (Double) seg_start_ms;
 					group->dash->start_range_period -= ap->start;
@@ -929,7 +928,6 @@ static void gf_dash_group_timeline_setup(GF_MPD *mpd, GF_DASH_Group *group, u64 
 					if (ap->start) start = ap->start;
 
 					if ((seg_start_ms>=ap->start) && (!ap->duration || (seg_end_ms<=start + ap->duration))) {
-						group->dash->initial_period_tunein = GF_FALSE;
 						group->dash->reinit_period_index = 1+i;
 						group->dash->start_range_period = (Double) seg_start_ms;
 						group->dash->start_range_period -= ap->start;
