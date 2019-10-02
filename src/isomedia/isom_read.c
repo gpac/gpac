687390afbf588789f9747d383b70ab0824042bc7
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2019-10-02 12:24:27 +0200

    mp4box -info: display visual info (including bit depth)

--- src/isomedia/isom_read.c
+++ src/isomedia/isom_read.c
@@ -3120,6 +3120,33 @@ GF_Err gf_isom_get_visual_info(GF_ISOFile *movie, u32 trackNumber, u32 StreamDes
 }
 
 GF_EXPORT
+GF_Err gf_isom_get_visual_bit_depth(GF_ISOFile* movie, u32 trackNumber, u32 StreamDescriptionIndex, u16* bitDepth)
+{
+	GF_TrackBox *trak;
+	GF_SampleEntryBox *entry;
+	GF_SampleDescriptionBox *stsd;
+
+	trak = gf_isom_get_track_from_file(movie, trackNumber);
+	if (!trak) return GF_BAD_PARAM;
+
+	stsd = trak->Media->information->sampleTable->SampleDescription;
+	if (!stsd) return movie->LastError = GF_ISOM_INVALID_FILE;
+	if (!StreamDescriptionIndex || StreamDescriptionIndex > gf_list_count(stsd->other_boxes)) return movie->LastError = GF_BAD_PARAM;
+
+	entry = (GF_SampleEntryBox *)gf_list_get(stsd->other_boxes, StreamDescriptionIndex - 1);
+	//no support for generic sample entries (eg, no MPEG4 descriptor)
+	if (entry == NULL) return GF_BAD_PARAM;
+
+	//valid for MPEG visual, JPG and 3GPP H263
+	if (entry->internal_type == GF_ISOM_SAMPLE_ENTRY_VIDEO) {
+		*bitDepth = ((GF_VisualSampleEntryBox*)entry)->bit_depth;
+	} else {
+		return GF_BAD_PARAM;
+	}
+	return GF_OK;
+}
+
+GF_EXPORT
 GF_Err gf_isom_get_audio_info(GF_ISOFile *movie, u32 trackNumber, u32 StreamDescriptionIndex, u32 *SampleRate, u32 *Channels, u8 *bitsPerSample)
 {
 	GF_TrackBox *trak;
