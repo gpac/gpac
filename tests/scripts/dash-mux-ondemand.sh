56aa7cec6757f3889b25a862384ac8f651ed3539
jeanlf <jeanlf@users.sourceforge.net>
2019-11-13 17:21:19 +0100

    fixed wrong test

--- tests/scripts/dash-mux-ondemand.sh
+++ tests/scripts/dash-mux-ondemand.sh
@@ -4,7 +4,7 @@ test_begin "dash-mux-ondemand"
 
 do_test "$MP4BOX -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_I25_baseline_1280x720_512kbps.264 -add $EXTERNAL_MEDIA_DIR/counter/counter_30s_audio.aac -new $TEMP_DIR/file.mp4" "dash-input-preparation"
 
-do_test "$MP4BOX -dash 1000 -profile dashavc264:onDemand $TEMP_DIR/file.mp4 -out $TEMP_DIR/file.mpd" "basic-dash-if-ondemand"
+do_test "$MP4BOX -dash 1000 -profile onDemand $TEMP_DIR/file.mp4 -out $TEMP_DIR/file.mpd" "basic-dash-mux-ondemand"
 
 do_hash_test $TEMP_DIR/file.mpd "mpd"
 do_hash_test $TEMP_DIR/file_dashinit.mp4 "seg"
