27a1a1de85722c08fea6eabc22c3ca9701d67be9
jeanlf <jeanlf@users.sourceforge.net>
2019-11-14 13:59:04 +0100

    updated test for gpac/gpac@1d2f16d54

--- tests/scripts/mp4box-timecode.sh
+++ tests/scripts/mp4box-timecode.sh
@@ -9,7 +9,7 @@ test_begin "mp4box-timecode"
 do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_video.h264:rescale=24000/1001:tc=24000/1001,0,10,10,2 -new $TEMP_DIR/file.mp4" "rescale_tmcd"
 do_hash_test "$TEMP_DIR/file.mp4" "rescale_tmcd"
 
-do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_video.h264:rescale=30000/1001:tc=f30000/1001,0,10,10,2 -new $TEMP_DIR/file_f.mp4" "rescale_tmcd_f"
-do_hash_test "$TEMP_DIR/file_f.mp4" "rescale_tmcd_f"
+do_test "$MP4BOX -add $MEDIA_DIR/auxiliary_files/enst_video.h264:rescale=30000/1001:tc=30000/1001,0,10,10,2,30 -new $TEMP_DIR/file_fpt.mp4" "rescale_tmcd_fpt"
+do_hash_test "$TEMP_DIR/file_fpt.mp4" "rescale_tmcd_fpt"
 
 test_end
