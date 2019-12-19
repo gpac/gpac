gpac/gpac

commit 66ceac5352f6eb24c2d7660170997e269ef2b475

fixed compil and warnings for gcc on ubuntu18

src/filters/reframe_nalu.c
@@ -862,7 +862,7 @@ static void naludmx_create_hevc_decoder_config(GF_NALUDmxCtx *ctx, u8 **dsi, u32
 			}
 
 			/*disable frame rate scan, most bitstreams have wrong values there*/
-			if (!ctx->timescale && first && !ctx->fps.num*ctx->fps.den && sps->has_timing_info
+			if (!ctx->timescale && first && (!ctx->fps.num || !ctx->fps.den) && sps->has_timing_info
 				/*if detected FPS is greater than 1000, assume wrong timing info*/
 				&& (sps->time_scale <= 1000*sps->num_units_in_tick)
 			) {
@@ -990,7 +990,7 @@ void naludmx_create_avc_decoder_config(GF_NALUDmxCtx *ctx, u8 **dsi, u32 *dsi_si
 			}
 
 			/*disable frame rate scan, most bitstreams have wrong values there*/
-			if (first && !ctx->fps.num*ctx->fps.den && sps->vui.timing_info_present_flag
+			if (first && (!ctx->fps.num || !ctx->fps.den) && sps->vui.timing_info_present_flag
 				/*if detected FPS is greater than 1000, assume wrong timing info*/
 				&& (sps->vui.time_scale <= 1000*sps->vui.num_units_in_tick)
 			) {
@@ -1103,7 +1103,7 @@ static void naludmx_check_pid(GF_Filter *filter, GF_NALUDmxCtx *ctx)
 
 	if (!ctx->timescale) {
 		ctx->cur_fps = ctx->fps;
-		if (!ctx->cur_fps.num*ctx->cur_fps.den) {
+		if (!ctx->cur_fps.num && !ctx->cur_fps.den) {
 			ctx->cur_fps.num = 25000;
 			ctx->cur_fps.den = 1000;
 		}

src/filters/ff_common.c
@@ -503,7 +503,7 @@ GF_FilterArgs ffmpeg_arg_translate(const struct AVOption *opt)
 			sprintf(szDef, LLD"-"LLD, (s64) opt->min, (s64) opt->max);
 		arg.min_max_enum = gf_strdup(szDef);
 		break;
-#if LIBAVCODEC_VERSION_MAJOR >= 58
+#if LIBAVCODEC_VERSION_MAJOR >= 57
 	case AV_OPT_TYPE_UINT64:
 //	case AV_OPT_TYPE_UINT:
 		arg.arg_type = GF_PROP_LUINT;
@@ -617,8 +617,10 @@ static void ffmpeg_expand_register(GF_FilterSession *session, GF_FilterRegister 
 	AVOutputFormat *ofmt = NULL;
 	AVCodec *codec = NULL;
 #if (LIBAVFILTER_VERSION_MAJOR > 5)
-	void *avf_it = NULL;
 	const AVFilter *avf = NULL;
+#if (LIBAVFILTER_VERSION_MAJOR > 6)
+	void *avf_it = NULL;
+#endif
 #endif
 
 	const char *fname = "";
@@ -728,7 +730,11 @@ static void ffmpeg_expand_register(GF_FilterSession *session, GF_FilterRegister 
 #endif
 		} else if (type==FF_REG_TYPE_AVF) {
 #if (LIBAVFILTER_VERSION_MAJOR > 5)
+#if (LIBAVFILTER_VERSION_MAJOR > 6)
 			avf = av_filter_iterate(&avf_it);
+#else
+			avf = avfilter_next(avf);
+#endif
 			if (!avf) break;
 			av_class = avf->priv_class;
 			subname = avf->name;

src/filters/ff_mx.c
@@ -201,8 +201,8 @@ static GF_Err ffmx_start_seg(GF_Filter *filter, GF_FFMuxCtx *ctx, const char *se
 		ctx->muxer->pb = NULL;
 	}
 
-	av_freep(&ctx->muxer->url);
-	ctx->muxer->url = av_strdup(seg_name);
+//	av_freep(&ctx->muxer->url);
+//	ctx->muxer->url = av_strdup(seg_name);
 	strncpy(ctx->muxer->filename, seg_name, 1023);
 	ctx->offset_at_seg_start = 0;
 
@@ -213,9 +213,9 @@ static GF_Err ffmx_start_seg(GF_Filter *filter, GF_FFMuxCtx *ctx, const char *se
 			gf_fclose(f);
 		}
 
-		res = ctx->muxer->io_open(ctx->muxer, &ctx->muxer->pb, ctx->muxer->url, AVIO_FLAG_WRITE, NULL);
+		res = ctx->muxer->io_open(ctx->muxer, &ctx->muxer->pb, /*ctx->muxer->url*/seg_name, AVIO_FLAG_WRITE, NULL);
 		if (res < 0) {
-			GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[FFMux] Fail to open new segment %s - error %s\n", ctx->muxer->url, av_err2str(res) ));
+			GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[FFMux] Fail to open new segment %s - error %s\n", seg_name, av_err2str(res) ));
 			return GF_IO_ERR;
 		}
 	}
@@ -231,7 +231,7 @@ static GF_Err ffmx_start_seg(GF_Filter *filter, GF_FFMuxCtx *ctx, const char *se
         res = avformat_write_header(ctx->muxer, &options);
         av_dict_free(&options);
         if (res < 0) {
-			GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[FFMux] Fail to configure segment %s - error %s\n", ctx->muxer->url, av_err2str(res) ));
+			GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[FFMux] Fail to configure segment %s - error %s\n", seg_name, av_err2str(res) ));
 			return GF_IO_ERR;
 		}
     }
@@ -254,13 +254,13 @@ static GF_Err ffmx_close_seg(GF_Filter *filter, GF_FFMuxCtx *ctx, Bool send_evt_
 			if (ctx->status==1) {
 				res = av_write_trailer(ctx->muxer);
 			} else {
-				GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[FFMux] Invalid state %d for segment %s close\n", ctx->status, ctx->muxer->url));
+				GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[FFMux] Invalid state %d for segment %s close\n", ctx->status, /*ctx->muxer->url*/ctx->muxer->filename));
 				return GF_SERVICE_ERROR;
 			}
 		}
 
 		if (res<0) {
-			GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[FFMux] Fail to flush segment %s - error %s\n", ctx->muxer->url, av_err2str(res) ));
+			GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[FFMux] Fail to flush segment %s - error %s\n", /*ctx->muxer->url*/ctx->muxer->filename, av_err2str(res) ));
 			return GF_SERVICE_ERROR;
 		}
 		ctx->status = 2;
@@ -396,7 +396,7 @@ static GF_Err ffmx_process(GF_Filter *filter)
 				res = av_write_frame(ctx->muxer, &ffpck);
 			}
 			if (res<0) {
-				GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[FFMux] Fail to write packet to %s - error %s\n", ctx->muxer->url, av_err2str(res) ));
+				GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[FFMux] Fail to write packet to %s - error %s\n", /*ctx->muxer->url*/ctx->muxer->filename, av_err2str(res) ));
 				e = GF_IO_ERR;
 			}
 

src/filters/jsfilter.c
@@ -796,8 +796,8 @@ static JSValue jsf_filter_prop_set(JSContext *ctx, JSValueConst this_val, JSValu
 	u32 ival;
 	GF_FilterSessionCaps caps;
 	GF_JSFilterCtx *jsf = JS_GetOpaque(this_val, jsf_filter_class_id);
-    if (!jsf)
-        return JS_EXCEPTION;
+	if (!jsf)
+		return JS_EXCEPTION;
 
 	if (magic < JSF_EVT_LAST_DEFINED) {
 		if (JS_IsFunction(ctx, value) || JS_IsUndefined(value) || JS_IsNull(value)) {
@@ -867,11 +867,11 @@ static JSValue jsf_filter_prop_get(JSContext *ctx, JSValueConst this_val, int ma
 	GF_FilterSessionCaps caps;
 	JSValue res;
 	GF_JSFilterCtx *jsf = JS_GetOpaque(this_val, jsf_filter_class_id);
-    if (!jsf)
-        return JS_EXCEPTION;
+	if (!jsf)
+		return JS_EXCEPTION;
 
 	if (magic<JSF_EVT_LAST_DEFINED)
-    	return JS_DupValue(jsf->ctx, jsf->funcs[magic]);
+		return JS_DupValue(jsf->ctx, jsf->funcs[magic]);
 
 	switch (magic) {
 	case JSF_FILTER_MAX_PIDS:
@@ -2662,8 +2662,8 @@ static JSValue jsf_event_set_prop(JSContext *ctx, JSValueConst this_val, JSValue
 	const char *str=NULL;
 	GF_FilterEvent *evt = JS_GetOpaque(this_val, jsf_event_class_id);
     if (!evt) return JS_EXCEPTION;
-    if (!jsf_check_evt(evt->base.type, 0, magic))
-    	return JS_EXCEPTION;
+	if (!jsf_check_evt(evt->base.type, 0, magic))
+		return JS_EXCEPTION;
 
 	switch (magic) {
 	case JSF_EVENT_TYPE:
@@ -2760,8 +2760,8 @@ static JSValue jsf_event_get_prop(JSContext *ctx, JSValueConst this_val, int mag
 {
 	GF_FilterEvent *evt = JS_GetOpaque(this_val, jsf_event_class_id);
     if (!evt) return JS_EXCEPTION;
-    if (!jsf_check_evt(evt->base.type, evt->user_event.event.type, magic))
-    	return JS_EXCEPTION;
+	if (!jsf_check_evt(evt->base.type, evt->user_event.event.type, magic))
+		return JS_EXCEPTION;
 	switch (magic) {
 	/*PLAY*/
 	case JSF_EVENT_TYPE: return JS_NewInt32(ctx, evt->base.type);
@@ -2895,8 +2895,8 @@ static JSValue jsf_event_constructor(JSContext *ctx, JSValueConst new_target, in
     s32 type;
 	if (argc!=1)
 		return JS_EXCEPTION;
-    if (JS_ToInt32(ctx, &type, argv[0]))
-    	return JS_EXCEPTION;
+	if (JS_ToInt32(ctx, &type, argv[0]))
+		return JS_EXCEPTION;
 	if (!type)
     	return JS_EXCEPTION;
     obj = JS_NewObjectClass(ctx, jsf_event_class_id);
@@ -3401,8 +3401,8 @@ static JSValue jsf_pck_copy_props(JSContext *ctx, JSValueConst this_val, int arg
 {
 	GF_Err e;
 	GF_JSPckCtx *pck_dst = JS_GetOpaque(this_val, jsf_pck_class_id);
-    if (!pck_dst || !pck_dst->pck || !argc)
-    	return JS_EXCEPTION;
+	if (!pck_dst || !pck_dst->pck || !argc)
+		return JS_EXCEPTION;
 	GF_JSPckCtx *pck_from = JS_GetOpaque(argv[0], jsf_pck_class_id);
     if (!pck_from || !pck_from->pck)
     	return JS_EXCEPTION;

src/filters/reframe_av1.c
@@ -145,7 +145,7 @@ GF_Err av1dmx_check_format(GF_Filter *filter, GF_AV1DmxCtx *ctx, GF_BitStream *b
 	if (ctx->vp_cfg) gf_odf_vp_cfg_del(ctx->vp_cfg);
 	ctx->vp_cfg = NULL;
 	ctx->cur_fps = ctx->fps;
-	if (!ctx->fps.num*ctx->fps.den) {
+	if (!ctx->fps.num || !ctx->fps.den) {
 		ctx->cur_fps.num = 25000;
 		ctx->cur_fps.den = 1000;
 	}
@@ -196,7 +196,7 @@ GF_Err av1dmx_check_format(GF_Filter *filter, GF_AV1DmxCtx *ctx, GF_BitStream *b
 		ctx->state.tb_num = frame_rate; //time_base.numerator
 		ctx->state.tb_den = time_scale; //time_base.denominator
 
-		if (!ctx->fps.num*ctx->fps.den && ctx->state.tb_num && ctx->state.tb_den && ! ( (ctx->state.tb_num<=1) && (ctx->state.tb_den<=1) ) ) {
+		if ((!ctx->fps.num || !ctx->fps.den) && ctx->state.tb_num && ctx->state.tb_den && ! ( (ctx->state.tb_num<=1) && (ctx->state.tb_den<=1) ) ) {
 			ctx->cur_fps.num = ctx->state.tb_num;
 			ctx->cur_fps.den = ctx->state.tb_den;
 			GF_LOG(GF_LOG_DEBUG, GF_LOG_PARSER, ("[AV1Dmx] Detected IVF format FPS %d/%d\n", ctx->cur_fps.num, ctx->cur_fps.den));
@@ -477,7 +477,7 @@ static void av1dmx_check_pid(GF_Filter *filter, GF_AV1DmxCtx *ctx)
 		}
 		gf_odf_av1_cfg_write(ctx->state.config, &dsi, &dsi_size);
 
-		if (!ctx->fps.num*ctx->fps.den && ctx->state.tb_num && ctx->state.tb_den && ! ( (ctx->state.tb_num<=1) && (ctx->state.tb_den<=1) ) ) {
+		if ((!ctx->fps.num || !ctx->fps.den) && ctx->state.tb_num && ctx->state.tb_den && ! ( (ctx->state.tb_num<=1) && (ctx->state.tb_den<=1) ) ) {
 			ctx->cur_fps.num = ctx->state.tb_num;
 			ctx->cur_fps.den = ctx->state.tb_den;
 		}

src/filters/reframe_img.c
@@ -223,7 +223,7 @@ GF_Err img_process(GF_Filter *filter)
 			gf_filter_pid_drop_packet(ctx->ipid);
 			return GF_SERVICE_ERROR;
 		}
-		if (!ctx->fps.num*ctx->fps.den) {
+		if (!ctx->fps.num || !ctx->fps.den) {
 			ctx->fps.num = 1000;
 			ctx->fps.den = 1000;
 		}

src/filters/reframe_mpgvid.c
@@ -104,7 +104,7 @@ GF_Err mpgviddmx_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_rem
 
 	ctx->ipid = pid;
 	ctx->cur_fps = ctx->fps;
-	if (!ctx->fps.num*ctx->fps.den) {
+	if (!ctx->fps.num || !ctx->fps.den) {
 		ctx->cur_fps.num = 25000;
 		ctx->cur_fps.den = 1000;
 	}

src/filters/reframe_prores.c
@@ -314,8 +314,8 @@ static void proresdmx_check_pid(GF_Filter *filter, GF_ProResDmxCtx *ctx, GF_ProR
 	}
 
 	ctx->cur_fps = ctx->fps;
-	if (!ctx->fps.num*ctx->fps.den) {
-		if (fps.num * fps.den) {
+	if (!ctx->fps.num || !ctx->fps.den) {
+		if (fps.num && fps.den) {
 			ctx->cur_fps = fps;
 		} else {
 			ctx->cur_fps.num = 25000;

commit 6be2371e0c87a752f671fa2520bf26b929390616

fixed bug introduced in 66ceac5

src/filters/reframe_nalu.c
@@ -1103,7 +1103,7 @@ static void naludmx_check_pid(GF_Filter *filter, GF_NALUDmxCtx *ctx)
 
 	if (!ctx->timescale) {
 		ctx->cur_fps = ctx->fps;
-		if (!ctx->cur_fps.num && !ctx->cur_fps.den) {
+		if (!ctx->cur_fps.num || !ctx->cur_fps.den) {
 			ctx->cur_fps.num = 25000;
 			ctx->cur_fps.den = 1000;
 		}