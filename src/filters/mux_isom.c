gpac/gpac

commit 59199e89290fb78b33a961d914c1eb2c3daa1748

httpout support in server, push and source modes

src/filters/mux_isom.c
@@ -538,8 +538,11 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 		gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_CODECID, NULL);
 		gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_UNFRAMED, NULL);
 		gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_STREAM_TYPE, &PROP_UINT(GF_STREAM_FILE) );
-		gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_FILE_EXT, &PROP_STRING("*") );
-		gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_MIME, &PROP_STRING("*") );
+		if (gf_filter_pid_get_property(pid, GF_PROP_PID_FILE_EXT)==NULL)
+			gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_FILE_EXT, &PROP_STRING("*") );
+		if (gf_filter_pid_get_property(pid, GF_PROP_PID_MIME)==NULL)
+			gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_MIME, &PROP_STRING("*") );
+		gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_DASH_MODE, NULL);
 
 		switch (ctx->store) {
 		case MP4MX_MODE_FLAT:

build/msvc14/libgpac.vcxproj
@@ -316,6 +316,7 @@
     <ClCompile Include="..\..\src\filters\mux_ts.c" />
     <ClCompile Include="..\..\src\filters\out_audio.c" />
     <ClCompile Include="..\..\src\filters\out_file.c" />
+    <ClCompile Include="..\..\src\filters\out_http.c" />
     <ClCompile Include="..\..\src\filters\out_pipe.c" />
     <ClCompile Include="..\..\src\filters\out_rtp.c" />
     <ClCompile Include="..\..\src\filters\out_rtsp.c" />

build/msvc14/libgpac.vcxproj.filters
@@ -1316,6 +1316,9 @@
     <ClCompile Include="..\..\src\filters\out_file.c">
       <Filter>filters</Filter>
     </ClCompile>
+    <ClCompile Include="..\..\src\filters\out_http.c">
+      <Filter>filters</Filter>
+    </ClCompile>
     <ClCompile Include="..\..\src\filters\out_video.c">
       <Filter>filters</Filter>
     </ClCompile>

build/xcode/gpac.xcodeproj/project.pbxproj
@@ -1965,7 +1965,6 @@
 		92F8D47E1F71643600616F7C /* filters */ = {
 			isa = PBXGroup;
 			children = (
-				92261A86239A6E4600C14199 /* out_http.c */,
 				92BB85581F7BFB61009BC9C8 /* compose.c */,
 				92FE596C2088E52900CF0C5A /* dasher.c */,
 				92BB85821F7C5773009BC9C8 /* dec_ac52.c */,
@@ -2031,6 +2030,7 @@
 				92E164AE20F8EBB60024A9CA /* nvdec */,
 				922E29822051940C00C8DCBB /* out_audio.c */,
 				9289D9F41FDEFFCD00C24F83 /* out_file.c */,
+				92261A86239A6E4600C14199 /* out_http.c */,
 				9206822D20EA69680041169F /* out_pipe.c */,
 				92C0F37D21EC7BB0002961C8 /* out_rtp.c */,
 				92B2A4A42297FD9E002A65A7 /* out_rtsp.c */,

include/gpac/filters.h
@@ -465,7 +465,7 @@ void gf_fs_lock_filters(GF_FilterSession *session, Bool do_lock);
 */
 u32 gf_fs_get_filters_count(GF_FilterSession *session);
 
-/*! Type of multimedia filter*/
+/*! Type of filter*/
 typedef enum
 {
 	/*! Unknown filter type*/
@@ -1773,6 +1773,28 @@ struct __gf_filter_register
 	/*! user data of register loader, not inspected/modified by filter session*/
 	void *udta;
 
+	/*! optional. Used by sink filters offering multiple sink support
+		GPAC instantiates filter chains based on filter capabilities and arguments of source and destination. When a sink/destination filter can handle
+		multiple file inputs of various formats/types, temporary filters called alias are needed to instantiate the proper chain.
+		If the return value is true, an alias filter of the same type as this filter will be created with the proper arguments and initialized (in order to modify the
+		 alias filter capabilities if needed), and finalized when no longer needed. All other callbacks will however be made on the main filter, not the alias.
+
+		A good example is HTTP output of a DASH session:
+		- the first PID will be a FILE stream for the manifest (MPD, M3U8), and the http output capabilities will be set to accept the manifest file extension.mime
+		- the other PIDs will be media PIDs (mp4, ts, mkv ...) and won't match the filter instance, creating a new instance which we don't want
+
+		By creating filter alias, a temprary HTTP output (alias) will be created, holding the arguments for the chain (including destination type). Overloading the alias filter capabilities
+		with the desired file extension or MIME will trigger the proper chain resolution. The alias will be switched to the final filter upon connecting the PIDs
+
+		To access original arguments of the chain, see \ref gf_filter_pid_get_alias_udta
+		To check if an initialize callback targets the main filter or the temporary one, see \ref gf_filter_is_alias
+	\param filter the target filter
+	\param url the url of the target destination, never NULL.
+	\param mime the MIME type of the target destination. Can be NULL if mime not available
+	\return GF_TRUE if this filter instance should be used to handle the given URL, in which case an alias filter will be created
+	*/
+	Bool (*use_alias)(GF_Filter *filter, const char *url, const char *mime);
+
 
 	/*! version of the filter, usually only for external libs*/
 	const char *version;
@@ -2397,6 +2419,12 @@ GF_FilterArgs *gf_filter_get_args(GF_Filter *filter);
 \return the mime type probed, or NULL if not recognized
 */
 const char *gf_filter_probe_data(GF_Filter *filter, u8 *data, u32 size);
+
+/*! checks if the given filter is an alias filter created by a multiple sink filter
+\param filter target filter
+\return GF_TRUE if this is an alias  filter created by a multiple sink filter, GF_FALSE otherwise
+*/
+Bool gf_filter_is_alias(GF_Filter *filter);
 
 /*! @} */
 
@@ -3026,6 +3054,14 @@ If the session is multi-threaded, this parameter has no effect.
 \return GF_TRUE if PID is currently playing, GF_FALSE otherwise
 */
 GF_Err gf_filter_pid_allow_direct_dispatch(GF_FilterPid *PID);
+
+/*! Gets the private stack of the alias filter associated with an input PID, if any
+
+ \note The filter type of the alias is always the type of the filter holding the input PID connection.
+\param PID the target filter PID
+\return the temporary alias filter private stack, NULL otherwise
+*/
+void *gf_filter_pid_get_alias_udta(GF_FilterPid *PID);
 
 /*! @} */
 

share/doc/man/gpac-filters.1
@@ -812,7 +812,7 @@ Note: Unless disabled at session level (see -no-probe ), file extensions are usu
 .br
 src (string):                            location of source content
 .br
-block_size (unsigned int, default: 1000000): block size used to read file
+block_size (unsigned int, default: 100000): block size used to read file
 .br
 cache (enum, default: disk):             set cache mode
 .br
@@ -4153,7 +4153,9 @@ latm (boolean, default: false):          use latm for AAC payload format
 .br
 mounts (string list):                    list of directories to expose in server mode
 .br
-block_size (unsigned int, default: 4096): block size used to read TCP socket
+block_size (unsigned int, default: 10000): block size used to read TCP socket
+.br
+maxc (unsigned int, default: 100):       maximum number of connections
 .br
 user_agent (string, default: $GUA):      user agent string, by default solved from GPAC preferences
 .br
@@ -4172,6 +4174,223 @@ mcast (enum, default: off):              control multicast setup of a session.
 * mirror: clients can create a multicast session. Any later request to the same URL will use that multicast session
 .br
 
+.br
+
+.br
+.SH httpout
+.LP
+.br
+Description: HTTP Server
+.br
+
+.br
+The HTTP output filter can act as:
+.br
+- a simple HTTP server
+.br
+- an HTTP server sink
+.br
+- an HTTP server file sink
+.br
+- an HTTP client sink
+.br
+- an HTTP server source
+.br
+  
+.br
+The server currently handles GET, HEAD, PUT, POST, DELETE methods.
+.br
+Single or multiple byte ranges are supported for both GET and PUT/POST methods.
+.br
+- for GET, the resulting body is a single-part body formed by the concatenated byte ranges as requested (no overlap checking).
+.br
+- for PUT/POST, the received data is pushed to the target file according to the byte ranges specified in the client request.
+.br
+  
+.br
+Listing can be enabled on server using -dlist.
+.br
+When disabled, a GET on a directory will fail.
+.br
+When enabled, a GET on a directory will return a simple HTML listing of the content inspired from Apache.
+.br
+  
+.br
+.SH Simple HTTP server
+.LP
+.br
+In this mode, the filter doesn't need any input connection and exposes all files in the directories given by -rdirs.
+.br
+PUT and POST methods are only supported if a write directory is specified by -wdir option.
+.br
+Example
+.br
+gpac httpout:rdirs=outcoming
+.br
+
+.br
+This sets up a read-only server.
+.br
+  
+.br
+Example
+.br
+gpac httpout:wdir=incoming
+.br
+
+.br
+This sets up a write-only server.
+.br
+  
+.br
+Example
+.br
+gpac httpout:rdirs=outcoming:wdir=incoming:port=8080
+.br
+
+.br
+This sets up a read-write server running on -port 8080.
+.br
+  
+.br
+.SH HTTP server sink
+.LP
+.br
+In this mode, the filter will forward input PIDs to connected clients, trashing the data if no client is connected unless -hold is specified.
+.br
+The filter doesn't use any read directory in this mode.
+.br
+This mode is mostly usefull to setup live HTTP streaming of media sessions such as MP3, MPEG-2 TS or other muxed representations:
+.br
+Example
+.br
+gpac -i MP3_SOURCE -o http://localhost/live.mp3 --hold
+.br
+
+.br
+In this example, the server waits for client requests on /live.mp3 and will then push each input packet to all connected clients.
+.br
+If the source is not real-time, you can inject a reframer filter performing realtime regulation.
+.br
+Example
+.br
+gpac -i MP3_SOURCE reframer:rt=on @ -o http://localhost/live.mp3
+.br
+
+.br
+In this example, the server will push each input packet to all connected clients, or trash the packet if no connected clients.
+.br
+  
+.br
+.SH HTTP server file sink
+.LP
+.br
+In this mode, the filter will write input PIDs to files in the first read directory specified, acting as a file output sink.
+.br
+The filter uses a read directory in this mode, which must be writable.
+.br
+Upon client GET request, the server will check if the requested file matches the name of a file currently being written by the server.
+.br
+If so, the server will keep refreshing the source size until the associated input file is closed.
+.br
+This mode is typically used for origin server in HAS sessions where clients may request files while they are being produced (low latency DASH).
+.br
+Example
+.br
+gpac -i SOURCE -o http://localhost/live.mpd --rdirs=temp
+.br
+
+.br
+  
+.br
+.SH HTTP client sink
+.LP
+.br
+In this mode, the filter will upload input PIDs data to remote server using PUT (or POST if -post is set).
+.br
+This mode must be explicitly activated using -smode.
+.br
+The filter uses no read or write directories in this mode.
+.br
+Example
+.br
+gpac -i SOURCE -o http://targethost:8080/live.mpd:gpac:smode=push
+.br
+
+.br
+In this example, the filter will send PUT methods to the server running on -port 8080 at targethost location (IP address or name).
+.br
+  
+.br
+.SH HTTP server source
+.LP
+.br
+In this mode, the server acts as a source rather than a sink. It declares incoming PUT or POST methods as output PIDs
+.br
+This mode must be explicitly activated using -smode.
+.br
+The filter uses no read or write directories in this mode, and updloaded data is NOT stored by the server.
+.br
+Example
+.br
+gpac httpout:smode=source vout aout
+.br
+
+.br
+In this example, the filter will try to play uploaded files through video and audio output.
+.br
+
+.br
+
+.br
+.SH Options (expert):
+.LP
+.br
+dst (string):                            location of destination file - see filter help 
+.br
+port (unsigned int, default: 80):        server port
+.br
+ifce (string):                           default network inteface to use
+.br
+rdirs (string list):                     list of directories to expose for read - see filter help
+.br
+wdir (string):                           directory to expose for write - see filter help
+.br
+block_size (unsigned int, default: 10000): block size used to read and write TCP socket
+.br
+user_agent (string, default: $GUA):      user agent string, by default solved from GPAC preferences
+.br
+close (boolean, default: false):         close HTTP connection after each request
+.br
+maxc (unsigned int, default: 100):       maximum number of connections, 0 is unlimited
+.br
+maxp (unsigned int, default: 6):         maximum number of connections for one peer, 0 is unlimited
+.br
+cache_control (string):                  specify the Cache-Control string to add; none disable ETag
+.br
+hold (boolean, default: false):          hold packets until one client connects
+.br
+smode (enum, default: default):          filter operation mode, ignored if -wdir is set. See filter help for more details. Mode can be
+.br
+* default: run in server mode (see filter help)
+.br
+* push: run in client mode using PUT or POST (see filter help)
+.br
+* source: use server as source filter on incoming PUT/POST
+.br
+
+.br
+timeout (unsigned int, default: 4):      timeout in seconds for persistent connections; 0 disable timeout
+.br
+ext (string):                            set extension for graph resolution, regardless of file extension
+.br
+mime (string):                           set mime type for graph resolution
+.br
+quit (boolean, default: false):          exit server once all input PIDs are done and client disconnects (for test purposes)
+.br
+post (boolean, default: false):          use POST instead of PUT for uploading files
+.br
+dlist (boolean, default: false):         enable HTML listing for GET requests on directories
 .br
 
 .br
@@ -4717,7 +4936,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 0.9.0-DEV-rev3007-g5c01dcb53-filters
+build: 0.9.0-DEV-rev3010-g9cf182891-httpout
 .br
 Copyright: (c) 2000-2019 Telecom ParisTech distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -3247,7 +3247,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 0.9.0-DEV-rev3007-g5c01dcb53-filters
+build: 0.9.0-DEV-rev3010-g9cf182891-httpout
 .br
 Copyright: (c) 2000-2019 Telecom ParisTech distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2051,7 +2051,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 0.9.0-DEV-rev3007-g5c01dcb53-filters
+build: 0.9.0-DEV-rev3010-g9cf182891-httpout
 .br
 Copyright: (c) 2000-2019 Telecom ParisTech distributed under LGPL v2.1+ - http://gpac.io
 .br

commit 40b1f6caf383b917303b5a09c44785a8c865a8b2

fixed bug introduced by 59199e8

src/filters/mux_isom.c
@@ -36,6 +36,10 @@
 
 #define GF_VENDOR_GPAC		GF_4CC('G','P','A','C')
 
+
+#define ISOM_FILE_EXT "mp4|mpg4|m4a|m4i|3gp|3gpp|3g2|3gp2|iso|m4s|heif|heic|avci|mj2|mov|qt"
+#define ISOM_FILE_MIME "application/x-isomedia|application/mp4|video/mp4|audio/mp4|video/3gpp|audio/3gpp|video/3gp2|audio/3gp2|video/iso.segment|audio/iso.segment|image/heif|image/heic|image/avci|video/quicktime"
+
 enum{
 	NALU_NONE,
 	NALU_AVC,
@@ -539,9 +543,11 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 		gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_CODECID, NULL);
 		gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_UNFRAMED, NULL);
 		gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_STREAM_TYPE, &PROP_UINT(GF_STREAM_FILE) );
-		if (gf_filter_pid_get_property(pid, GF_PROP_PID_FILE_EXT)==NULL)
+		p = gf_filter_pid_get_property(pid, GF_PROP_PID_FILE_EXT);
+		if (!p || !strstr(ISOM_FILE_EXT, p->value.string))
 			gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_FILE_EXT, &PROP_STRING("*") );
-		if (gf_filter_pid_get_property(pid, GF_PROP_PID_MIME)==NULL)
+		p = gf_filter_pid_get_property(pid, GF_PROP_PID_MIME);
+		if (!p || !strstr(ISOM_FILE_MIME, p->value.string))
 			gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_MIME, &PROP_STRING("*") );
 		gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_DASH_MODE, NULL);
 
@@ -4742,8 +4748,8 @@ static const GF_FilterCapability MP4MuxCaps[] =
 	CAP_BOOL(GF_CAPS_INPUT_EXCLUDED, GF_PROP_PID_UNFRAMED, GF_TRUE),
 	//and any codecid
 	CAP_UINT(GF_CAPS_INPUT_EXCLUDED,  GF_PROP_PID_CODECID, GF_CODECID_NONE),
-	CAP_STRING(GF_CAPS_OUTPUT_STATIC,  GF_PROP_PID_FILE_EXT, "mp4|mpg4|m4a|m4i|3gp|3gpp|3g2|3gp2|iso|m4s|heif|heic|avci|mj2|mov|qt"),
-	CAP_STRING(GF_CAPS_OUTPUT_STATIC,  GF_PROP_PID_MIME, "application/x-isomedia|application/mp4|video/mp4|audio/mp4|video/3gpp|audio/3gpp|video/3gp2|audio/3gp2|video/iso.segment|audio/iso.segment|image/heif|image/heic|image/avci|video/quicktime"),
+	CAP_STRING(GF_CAPS_OUTPUT_STATIC,  GF_PROP_PID_FILE_EXT, ISOM_FILE_EXT),
+	CAP_STRING(GF_CAPS_OUTPUT_STATIC,  GF_PROP_PID_MIME, ISOM_FILE_MIME),
 	{0},
 	//for scene / OD / text, we don't want raw codecid (filters modifying a scene graph we don't expose)
 	CAP_UINT(GF_CAPS_INPUT,GF_PROP_PID_STREAM_TYPE, GF_STREAM_SCENE),