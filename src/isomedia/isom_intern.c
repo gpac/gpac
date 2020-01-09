6040a5981a9f51410bd18af8820afbd2748c2d76
Aurelien David <aurelien.david@telecom-paristech.fr>
2020-01-09 16:55:23 +0100

    add nullptr check in fixtrackid (#1346)

--- src/isomedia/isom_intern.c
+++ src/isomedia/isom_intern.c
@@ -130,6 +130,7 @@ static void FixTrackID(GF_ISOFile *mov)
 	if (gf_list_count(mov->moov->trackList) == 1 && gf_list_count(mov->moof->TrackList) == 1) {
 		GF_TrackFragmentBox *traf = (GF_TrackFragmentBox*)gf_list_get(mov->moof->TrackList, 0);
 		GF_TrackBox *trak = (GF_TrackBox*)gf_list_get(mov->moov->trackList, 0);
+		if (!traf->tfhd || !trak->Header) return;
 		if ((traf->tfhd->trackID != trak->Header->trackID)) {
 			GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[iso file] Warning: trackID of MOOF/TRAF(%u) is not the same as MOOV/TRAK(%u). Trying to fix.\n", traf->tfhd->trackID, trak->Header->trackID));
 			traf->tfhd->trackID = trak->Header->trackID;
