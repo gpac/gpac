gpac/gpac

commit 2fda3d69584e5e385b12b48ce3167073f5a1b614

fixed bug introduced by ce8ae870

src/filter_core/filter.c
@@ -1910,6 +1910,7 @@ static void gf_filter_process_task(GF_FSTask *task)
 	assert(task->filter);
 	assert(filter->freg);
 	assert(filter->freg->process);
+	assert(filter->in_process);
 	task->can_swap = GF_TRUE;
 
 	filter->schedule_next_time = 0;
@@ -1998,6 +1999,8 @@ static void gf_filter_process_task(GF_FSTask *task)
 	GF_LOG(GF_LOG_DEBUG, GF_LOG_FILTER, ("Filter %s process\n", filter->name));
 	gf_rmt_begin_hash(filter->name, GF_RMT_AGGREGATE, &filter->rmt_hash);
 
+	filter->in_process_callback = GF_TRUE;
+
 #ifdef GPAC_MEMORY_TRACKING
 	if (filter->session->check_allocs)
 		e = gf_filter_process_check_alloc(filter);
@@ -2005,6 +2008,7 @@ static void gf_filter_process_task(GF_FSTask *task)
 #endif
 		e = filter->freg->process(filter);
 
+	filter->in_process_callback = GF_FALSE;
 	gf_rmt_end();
 
 	//flush all pending pid init requests following the call to init
@@ -2174,7 +2178,9 @@ void gf_filter_post_process_task_internal(GF_Filter *filter, Bool use_direct_dis
 	if (filter->finalized || filter->removed)
 		return;
 
-	if (!use_direct_dispatch && filter->in_process) {
+	//if regular posting (not direct) and our caller is the main process function, no need to lock task mutex, just increase
+	//the next scheduled time
+	if (!use_direct_dispatch && filter->in_process_callback) {
 		filter->schedule_next_time = 1 + gf_sys_clock_high_res();
 		return;
 	}

src/filter_core/filter_session.h
@@ -513,7 +513,8 @@ struct __gf_filter
 	Bool has_out_caps;
 
 	Bool disabled;
-
+	//set to true before calling filter process() callback, and reset to false right after
+	Bool in_process_callback;
 	Bool no_probe;
 	Bool no_inputs;
 	

commit 80791123bebbc8c2fb5c7595bde29c5583322ea7

fixed bug introduced by 2fda3d69

src/filter_core/filter.c
@@ -1910,7 +1910,6 @@ static void gf_filter_process_task(GF_FSTask *task)
 	assert(task->filter);
 	assert(filter->freg);
 	assert(filter->freg->process);
-	assert(filter->in_process);
 	task->can_swap = GF_TRUE;
 
 	filter->schedule_next_time = 0;

src/filter_core/filter_pck.c
@@ -99,7 +99,6 @@ static GF_FilterPacket *gf_filter_pck_new_alloc_internal(GF_FilterPid *pid, u32 
 {
 	GF_FilterPacket *pck=NULL;
 	GF_FilterPacket *closest=NULL;
-	GF_PckQueueEnum pck_enum_state;
 	u32 count, max_reservoir_size;
 
 	if (PID_IS_INPUT(pid)) {
@@ -111,9 +110,9 @@ static GF_FilterPacket *gf_filter_pck_new_alloc_internal(GF_FilterPid *pid, u32 
 
 	count = gf_fq_count(pid->filter->pcks_alloc_reservoir);
 	if (count) {
+		GF_PckQueueEnum pck_enum_state;
+		memset(&pck_enum_state, 0, sizeof(GF_PckQueueEnum));
 		pck_enum_state.data_size = data_size;
-		pck_enum_state.closest = NULL;
-		pck_enum_state.pck = NULL;
 		gf_fq_enum(pid->filter->pcks_alloc_reservoir, pck_queue_enum, &pck_enum_state);
 		pck = pck_enum_state.pck;
 		closest = pck_enum_state.closest;

src/filter_core/filter_session.c
@@ -729,7 +729,7 @@ void gf_fs_post_task_ex(GF_FilterSession *fsess, gf_fs_task_callback task_fun, G
 
 	//only flatten calls if in main thread (we still have some broken filters using threading
 	//that could trigger tasks
-	if ((force_direct_call || fsess->direct_mode) && fsess->tasks_in_process && gf_th_id()==fsess->main_th.th_id) {
+	if ((force_direct_call || fsess->direct_mode) && !filter->in_process && fsess->tasks_in_process && (gf_th_id()==fsess->main_th.th_id)) {
 		GF_FSTask atask;
 		u64 task_time = gf_sys_clock_high_res();
 		memset(&atask, 0, sizeof(GF_FSTask));