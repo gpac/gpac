6e5e9d7b1a88ba5836a48a8f3491e1a8077193e7
jeanlf <jeanlf@github.com>
2020-03-09 08:05:10 +0100

    fixed potential bug in compositor plyaer mode init

--- src/filters/compose.c
+++ src/filters/compose.c
@@ -574,7 +574,10 @@ GF_Err compose_initialize(GF_Filter *filter)
 	ctx->magic_ptr = (void *) ctx;
 	ctx->filter = filter;
 	
-	e = gf_sc_load(ctx);
+    if (ctx->player && (ctx->ogl == GF_SC_GLMODE_AUTO))
+        ctx->ogl = GF_SC_GLMODE_HYBRID;
+	
+    e = gf_sc_load(ctx);
 	if (e) return e;
 
 	gf_filter_get_session_caps(filter, &sess_caps);
