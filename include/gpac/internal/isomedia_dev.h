gpac/gpac

commit ff331f7f9ec40df25d7b21a896edaba1afc4a347

fixed bugs in dash and encryption of mp3, 3GPP audio, h263 and mj2k

src/isomedia/drm_sample.c
@@ -408,6 +408,13 @@ static GF_Err isom_set_protected_entry(GF_ISOFile *the_file, u32 trackNumber, u3
 	if (e) return e;
 
 	original_format = sea->type;
+	if (original_format==GF_ISOM_BOX_TYPE_GNRA) {
+		original_format = ((GF_GenericAudioSampleEntryBox*)sea)->EntryType;
+	} else if (original_format==GF_ISOM_BOX_TYPE_GNRV) {
+		original_format = ((GF_GenericVisualSampleEntryBox*)sea)->EntryType;
+	} else if (original_format==GF_ISOM_BOX_TYPE_GNRM) {
+		original_format = ((GF_GenericSampleEntryBox*)sea)->EntryType;
+	}
 
 	/* Replacing the Media Type */
 	switch (sea->type) {

src/isomedia/media.c
@@ -70,12 +70,12 @@ GF_Err Media_GetSampleDescIndex(GF_MediaBox *mdia, u64 DTS, u32 *sampleDescIndex
 	return stbl_GetSampleInfos(mdia->information->sampleTable, ( sampleNumber ? sampleNumber : prevSampleNumber), &offset, &num, sampleDescIndex, NULL);
 }
 
-static GF_Err gf_isom_get_3gpp_audio_esd(GF_SampleTableBox *stbl, GF_GenericAudioSampleEntryBox *entry, GF_ESD **out_esd)
+static GF_Err gf_isom_get_3gpp_audio_esd(GF_SampleTableBox *stbl, u32 type, GF_GenericAudioSampleEntryBox *entry, GF_ESD **out_esd)
 {
 	(*out_esd) = gf_odf_desc_esd_new(2);
 	(*out_esd)->decoderConfig->streamType = GF_STREAM_AUDIO;
 	/*official mapping to MPEG-4*/
-	switch (entry->type) {
+	switch (type) {
 	case GF_ISOM_SUBTYPE_3GP_EVRC:
 		(*out_esd)->decoderConfig->objectTypeIndication = GF_CODECID_EVRC;
 		return GF_OK;
@@ -143,9 +143,11 @@ static GF_Err gf_isom_get_3gpp_audio_esd(GF_SampleTableBox *stbl, GF_GenericAudi
 
 GF_Err Media_GetESD(GF_MediaBox *mdia, u32 sampleDescIndex, GF_ESD **out_esd, Bool true_desc_only)
 {
+	u32 type;
 	GF_ESD *esd;
 	GF_MPEGSampleEntryBox *entry = NULL;
 	GF_ESDBox *ESDa;
+	GF_ProtectionSchemeInfoBox *sinf;
 	GF_SampleDescriptionBox *stsd = mdia->information->sampleTable->SampleDescription;
 
 	*out_esd = NULL;
@@ -158,9 +160,29 @@ GF_Err Media_GetESD(GF_MediaBox *mdia, u32 sampleDescIndex, GF_ESD **out_esd, Bo
 
 	*out_esd = NULL;
 	ESDa = NULL;
-	switch (entry->type) {
-	case GF_ISOM_BOX_TYPE_MP4V:
+	type = entry->type;
+	switch (type) {
 	case GF_ISOM_BOX_TYPE_ENCV:
+	case GF_ISOM_BOX_TYPE_ENCA:
+	case GF_ISOM_BOX_TYPE_ENCS:
+	case GF_ISOM_BOX_TYPE_ENCF:
+	case GF_ISOM_BOX_TYPE_ENCM:
+	case GF_ISOM_BOX_TYPE_ENCT:
+		sinf = (GF_ProtectionSchemeInfoBox *) gf_isom_box_find_child(entry->child_boxes, GF_ISOM_BOX_TYPE_SINF);
+		if (sinf && sinf->original_format) {
+			type = sinf->original_format->data_format;
+		}
+		break;
+	case GF_ISOM_BOX_TYPE_RESV:
+		sinf = (GF_ProtectionSchemeInfoBox *) gf_isom_box_find_child(entry->child_boxes, GF_ISOM_BOX_TYPE_RINF);
+		if (sinf && sinf->original_format) {
+			type = sinf->original_format->data_format;
+		}
+		break;
+	}
+
+	switch (type) {
+	case GF_ISOM_BOX_TYPE_MP4V:
 	case GF_ISOM_BOX_TYPE_RESV:
 		ESDa = ((GF_MPEGVisualSampleEntryBox*)entry)->esd;
 		if (ESDa) esd = (GF_ESD *) ESDa->desc;
@@ -204,7 +226,6 @@ GF_Err Media_GetESD(GF_MediaBox *mdia, u32 sampleDescIndex, GF_ESD **out_esd, Bo
 		esd = ((GF_MPEGVisualSampleEntryBox*)entry)->emul_esd;
 		break;
 	case GF_ISOM_BOX_TYPE_MP4A:
-	case GF_ISOM_BOX_TYPE_ENCA:
         {
             GF_MPEGAudioSampleEntryBox *ase = (GF_MPEGAudioSampleEntryBox*)entry;
             ESDa = ase->esd;
@@ -237,7 +258,6 @@ GF_Err Media_GetESD(GF_MediaBox *mdia, u32 sampleDescIndex, GF_ESD **out_esd, Bo
         }
 		break;
 	case GF_ISOM_BOX_TYPE_MP4S:
-	case GF_ISOM_BOX_TYPE_ENCS:
 		ESDa = entry->esd;
 		if (ESDa) esd = (GF_ESD *) ESDa->desc;
 		break;
@@ -279,7 +299,7 @@ GF_Err Media_GetESD(GF_MediaBox *mdia, u32 sampleDescIndex, GF_ESD **out_esd, Bo
 	case GF_ISOM_SUBTYPE_3GP_QCELP:
 	case GF_ISOM_SUBTYPE_3GP_SMV:
 		if (!true_desc_only) {
-			GF_Err e = gf_isom_get_3gpp_audio_esd(mdia->information->sampleTable, (GF_GenericAudioSampleEntryBox*)entry, out_esd);
+			GF_Err e = gf_isom_get_3gpp_audio_esd(mdia->information->sampleTable, type, (GF_GenericAudioSampleEntryBox*)entry, out_esd);
 			if (e) return e;
 			break;
 		} else return GF_ISOM_INVALID_MEDIA;

include/gpac/constants.h
@@ -370,10 +370,11 @@ typedef enum
 	GF_CODECID_JPEG = 0x6C,
 	/*! codecid for PNG streams*/
 	GF_CODECID_PNG = 0x6D,
-	/*! codecid for JPEG-2000 streams*/
-	GF_CODECID_J2K = 0x6E,
 
 	GF_CODECID_LAST_MPEG4_MAPPING = 0xFF,
+
+	/*! codecid for JPEG-2000 streams*/
+	GF_CODECID_J2K = GF_4CC('j','p','2','k'),
 
 	/*!H263 visual streams*/
 	GF_CODECID_S263 = GF_4CC('s','2','6','3'),

src/filters/isoffin_load.c
@@ -259,7 +259,10 @@ static void isor_declare_track(ISOMReader *read, ISOMChannel *ch, u32 track, u32
 				audio_fmt = (pcm_size==64) ? GF_AUDIO_FMT_DBL : GF_AUDIO_FMT_FLT;
 			}
 			break;
-
+		case GF_ISOM_SUBTYPE_MP3:
+			codec_id = GF_CODECID_MPEG_AUDIO;
+			break;
+			
 		default:
 			codec_id = gf_codec_id_from_isobmf(m_subtype);
 			if (!codec_id)

src/isomedia/box_code_3gpp.c
@@ -51,15 +51,18 @@ GF_Err gppc_box_read(GF_Box *s, GF_BitStream *bs)
 	if (ptr == NULL) return GF_BAD_PARAM;
 	memset(&ptr->cfg, 0, sizeof(GF_3GPConfig));
 
+	ISOM_DECREASE_SIZE(s, 5)
 	ptr->cfg.vendor = gf_bs_read_u32(bs);
 	ptr->cfg.decoder_version = gf_bs_read_u8(bs);
 
 	switch (ptr->type) {
 	case GF_ISOM_BOX_TYPE_D263:
+		ISOM_DECREASE_SIZE(s, 2)
 		ptr->cfg.H263_level = gf_bs_read_u8(bs);
 		ptr->cfg.H263_profile = gf_bs_read_u8(bs);
 		break;
 	case GF_ISOM_BOX_TYPE_DAMR:
+		ISOM_DECREASE_SIZE(s, 4)
 		ptr->cfg.AMR_mode_set = gf_bs_read_u16(bs);
 		ptr->cfg.AMR_mode_change_period = gf_bs_read_u8(bs);
 		ptr->cfg.frames_per_sample = gf_bs_read_u8(bs);
@@ -67,6 +70,7 @@ GF_Err gppc_box_read(GF_Box *s, GF_BitStream *bs)
 	case GF_ISOM_BOX_TYPE_DEVC:
 	case GF_ISOM_BOX_TYPE_DQCP:
 	case GF_ISOM_BOX_TYPE_DSMV:
+		ISOM_DECREASE_SIZE(s, 1)
 		ptr->cfg.frames_per_sample = gf_bs_read_u8(bs);
 		break;
 	}

src/isomedia/box_code_base.c
@@ -3878,8 +3878,27 @@ GF_Err audio_sample_entry_box_read(GF_Box *s, GF_BitStream *bs)
  	}
 
 	e = gf_isom_box_array_read(s, bs, audio_sample_entry_on_child_box);
-	if (!e) return GF_OK;
+	if (!e) {
+		if (s->type==GF_ISOM_BOX_TYPE_ENCA) {
+			GF_ProtectionSchemeInfoBox *sinf = (GF_ProtectionSchemeInfoBox *) gf_isom_box_find_child(s->child_boxes, GF_ISOM_BOX_TYPE_SINF);
+
+			if (sinf && sinf->original_format) {
+				u32 type = sinf->original_format->data_format;
+				switch (type) {
+				case GF_ISOM_SUBTYPE_3GP_AMR:
+				case GF_ISOM_SUBTYPE_3GP_AMR_WB:
+				case GF_ISOM_SUBTYPE_3GP_EVRC:
+				case GF_ISOM_SUBTYPE_3GP_QCELP:
+				case GF_ISOM_SUBTYPE_3GP_SMV:
+					if (ptr->cfg_3gpp) ptr->cfg_3gpp->cfg.type = type;
+					break;
+				}
+			}
+		}
+		return GF_OK;
+	}
 	if (size<8) return GF_ISOM_INVALID_FILE;
+
 
 	/*hack for some weird files (possibly recorded with live.com tools, needs further investigations)*/
 	gf_bs_seek(bs, pos);
@@ -4170,7 +4189,8 @@ GF_Err video_sample_entry_box_read(GF_Box *s, GF_BitStream *bs)
 	e = gf_isom_box_array_read(s, bs, video_sample_entry_on_child_box);
 	if (e) return e;
 	/*this is an AVC sample desc*/
-	if (mp4v->avc_config || mp4v->svc_config || mp4v->mvc_config) AVC_RewriteESDescriptor(mp4v);
+	if (mp4v->avc_config || mp4v->svc_config || mp4v->mvc_config)
+		AVC_RewriteESDescriptor(mp4v);
 	/*this is an HEVC sample desc*/
 	if (mp4v->hevc_config || mp4v->lhvc_config || (mp4v->type==GF_ISOM_BOX_TYPE_HVT1))
 		HEVC_RewriteESDescriptor(mp4v);
@@ -4180,6 +4200,19 @@ GF_Err video_sample_entry_box_read(GF_Box *s, GF_BitStream *bs)
 	/*this is an AV1 sample desc*/
 	if (mp4v->vp_config)
 		VP9_RewriteESDescriptor(mp4v);
+
+	if (s->type==GF_ISOM_BOX_TYPE_ENCV) {
+		GF_ProtectionSchemeInfoBox *sinf = (GF_ProtectionSchemeInfoBox *) gf_isom_box_find_child(s->child_boxes, GF_ISOM_BOX_TYPE_SINF);
+
+		if (sinf && sinf->original_format) {
+			u32 type = sinf->original_format->data_format;
+			switch (type) {
+			case GF_ISOM_SUBTYPE_3GP_H263:
+				if (mp4v->cfg_3gpp) mp4v->cfg_3gpp->cfg.type = type;
+				break;
+			}
+		}
+	}
 	return GF_OK;
 }
 

src/isomedia/box_funcs.c
@@ -1282,15 +1282,15 @@ static struct box_registry_entry {
 	BOX_DEFINE_S( GF_ISOM_SUBTYPE_3GP_EVRC, audio_sample_entry, "stsd", "3gpp"),
 	BOX_DEFINE_S( GF_ISOM_SUBTYPE_3GP_SMV, audio_sample_entry, "stsd", "3gpp"),
 	BOX_DEFINE_S( GF_ISOM_SUBTYPE_3GP_H263, video_sample_entry, "stsd", "3gpp"),
-	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DAMR, gppc, "samr sawb", "3gpp"),
-	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DEVC, gppc, "sevc", "3gpp"),
-	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DQCP, gppc, "sqcp", "3gpp"),
-	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DSMV, gppc, "ssmv", "3gpp"),
-	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_D263, gppc, "s263", "3gpp"),
+	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DAMR, gppc, "samr sawb enca", "3gpp"),
+	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DEVC, gppc, "sevc enca", "3gpp"),
+	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DQCP, gppc, "sqcp enca", "3gpp"),
+	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DSMV, gppc, "ssmv enca", "3gpp"),
+	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_D263, gppc, "s263 encv", "3gpp"),
 	//3gpp timed text
 	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_TX3G, tx3g, "stsd", "3gpp"),
 	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_TEXT, text, "stsd", "apple"),
-	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_FTAB, ftab, "tx3g text", "3gpp"),
+	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_FTAB, ftab, "tx3g text enct", "3gpp"),
 	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_STYL, styl, "text_sample", "3gpp"),
 	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_HLIT, hlit, "text_sample", "3gpp"),
 	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_HCLR, hclr, "text_sample", "3gpp"),
@@ -1302,7 +1302,7 @@ static struct box_registry_entry {
 	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_TWRP, twrp, "text_sample", "3gpp"),
 	//3GPP dims
 	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DIMS, dims, "stsd", "3gpp"),
-	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DIMC, dimC, "stsd", "3gpp"),
+	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DIMC, dimC, "dims encs", "3gpp"),
 	BOX_DEFINE_S( GF_ISOM_BOX_TYPE_DIST, diST, "stsd", "3gpp"),
 
 
@@ -1444,7 +1444,7 @@ static struct box_registry_entry {
 
 	//J2K boxes
 	BOX_DEFINE_S(GF_ISOM_BOX_TYPE_MJP2, video_sample_entry, "stsd", "j2k"),
-	BOX_DEFINE_S(GF_ISOM_BOX_TYPE_JP2H, jp2h, "mjp2", "j2k"),
+	BOX_DEFINE_S(GF_ISOM_BOX_TYPE_JP2H, jp2h, "mjp2 encv", "j2k"),
 	BOX_DEFINE_S(GF_ISOM_BOX_TYPE_IHDR, ihdr, "jp2h", "j2k"),
 
 	/* Image tracks */
@@ -1455,7 +1455,7 @@ static struct box_registry_entry {
 
 	//Opus in ISOBMFF boxes
 	BOX_DEFINE_S(GF_ISOM_BOX_TYPE_FLAC, audio_sample_entry, "stsd", "Flac"),
-	FBOX_DEFINE_S(GF_ISOM_BOX_TYPE_DFLA, dfla, "fLaC", 0, "Flac"),
+	FBOX_DEFINE_S(GF_ISOM_BOX_TYPE_DFLA, dfla, "fLaC enca", 0, "Flac"),
 
 };
 

src/utils/constants.c
@@ -75,7 +75,7 @@ CodecIDReg CodecRegistry [] = {
 	{GF_CODECID_MPEG1, GF_CODECID_MPEG1, GF_STREAM_VISUAL, "MPEG-1 Video", "m1v", "mp1v", "video/mp1v-es"},
 	{GF_CODECID_JPEG, GF_CODECID_JPEG, GF_STREAM_VISUAL, "JPEG Image", "jpg|jpeg", "jpeg", "image/jpeg"},
 	{GF_CODECID_PNG, GF_CODECID_PNG, GF_STREAM_VISUAL, "PNG Image", "png", "png ", "image/png"},
-	{GF_CODECID_J2K, GF_CODECID_J2K, GF_STREAM_VISUAL, "JPEG200 Image", "jp2|j2k", "mjp2", "image/jp2"},
+	{GF_CODECID_J2K, 0x6E, GF_STREAM_VISUAL, "JPEG200 Image", "jp2|j2k", "mjp2", "image/jp2"},
 	{GF_CODECID_AAC_MPEG4, GF_CODECID_AAC_MPEG4, GF_STREAM_AUDIO, "MPEG-4 AAC Audio", "aac", "mp4a", "audio/aac"},
 	{GF_CODECID_AAC_MPEG2_MP, GF_CODECID_AAC_MPEG2_MP, GF_STREAM_AUDIO, "MPEG-2 AAC Audio Main", "aac|aac2m", "mp4a", "audio/aac", GF_CODECID_AAC_MPEG4},
 	{GF_CODECID_AAC_MPEG2_LCP, GF_CODECID_AAC_MPEG2_LCP, GF_STREAM_AUDIO, "MPEG-2 AAC Audio Low Complexity", "aac|aac2l", "mp4a", "audio/aac", GF_CODECID_AAC_MPEG4},

commit ddba069b5c530d2be8225d38619f3cee365f8063

fixed bugs introduced by ff331f7f

include/gpac/internal/isomedia_dev.h
@@ -486,8 +486,6 @@ enum
 	/* from drm_sample.c */
 	GF_ISOM_BOX_TYPE_264B 	= GF_4CC('2','6','4','b'),
 	GF_ISOM_BOX_TYPE_265B 	= GF_4CC('2','6','5','b'),
-	GF_ISOM_BOX_TYPE_AV1B 	= GF_4CC('a','v','1','b'),
-	GF_ISOM_BOX_TYPE_VP9B   = GF_4CC('v','p','9','b'),
 
 	/*MPEG-H 3D audio*/
 	GF_ISOM_BOX_TYPE_MHA1 	= GF_4CC('m','h','a','1'),

src/filter_core/filter_pid.c
@@ -310,7 +310,8 @@ void gf_filter_pid_inst_delete_task(GF_FSTask *task)
 
 	//we still have packets out there!
 	if (pidinst->pid->nb_shared_packets_out) {
-		if ((pid->num_destinations==1) && (gf_list_find(pid->destinations, pidinst)>=0)) {
+		//special case for disconnect of fanouts, destroy even if shared packets out
+		if (!pid->num_destinations || ((pid->num_destinations>=1) && (gf_list_find(pid->destinations, pidinst)>=0))) {
 			TASK_REQUEUE(task)
 			return;
 		}

src/isomedia/drm_sample.c
@@ -359,8 +359,6 @@ GF_Err gf_isom_remove_track_protection(GF_ISOFile *the_file, u32 trackNumber, u3
 	}
 	if (sea->type == GF_ISOM_BOX_TYPE_264B) sea->type = GF_ISOM_BOX_TYPE_AVC1;
 	if (sea->type == GF_ISOM_BOX_TYPE_265B) sea->type = GF_ISOM_BOX_TYPE_HVC1;
-	if (sea->type == GF_ISOM_BOX_TYPE_AV1B) sea->type = GF_ISOM_BOX_TYPE_AV01;
-	if (sea->type == GF_ISOM_BOX_TYPE_VP9B) sea->type = GF_ISOM_BOX_TYPE_VP09;
 	return GF_OK;
 }
 
@@ -454,13 +452,9 @@ static GF_Err isom_set_protected_entry(GF_ISOFile *the_file, u32 trackNumber, u3
 		sea->type = GF_ISOM_BOX_TYPE_ENCV;
 		break;
 	case GF_ISOM_BOX_TYPE_VP09:
-		if (is_isma)
-			original_format = GF_ISOM_BOX_TYPE_VP9B;
 		sea->type = GF_ISOM_BOX_TYPE_ENCV;
 		break;
 	case GF_ISOM_BOX_TYPE_AV01:
-		if (is_isma)
-			original_format = GF_ISOM_BOX_TYPE_AV1B;
 		sea->type = GF_ISOM_BOX_TYPE_ENCV;
 		break;
 	case GF_ISOM_BOX_TYPE_MP4S:

src/isomedia/media.c
@@ -183,7 +183,6 @@ GF_Err Media_GetESD(GF_MediaBox *mdia, u32 sampleDescIndex, GF_ESD **out_esd, Bo
 
 	switch (type) {
 	case GF_ISOM_BOX_TYPE_MP4V:
-	case GF_ISOM_BOX_TYPE_RESV:
 		ESDa = ((GF_MPEGVisualSampleEntryBox*)entry)->esd;
 		if (ESDa) esd = (GF_ESD *) ESDa->desc;
 		/*avc1 encrypted*/
@@ -198,6 +197,8 @@ GF_Err Media_GetESD(GF_MediaBox *mdia, u32 sampleDescIndex, GF_ESD **out_esd, Bo
 	case GF_ISOM_BOX_TYPE_HVC2:
 	case GF_ISOM_BOX_TYPE_HEV2:
 	case GF_ISOM_BOX_TYPE_HVT1:
+	case GF_ISOM_BOX_TYPE_264B:
+	case GF_ISOM_BOX_TYPE_265B:
 		esd = ((GF_MPEGVisualSampleEntryBox*) entry)->emul_esd;
 		break;
 	case GF_ISOM_BOX_TYPE_SVC1: