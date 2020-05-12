91d1e4b23c70e24ea4e1332a6a270514a3de0d29
jeanlf <jeanlf@users.sourceforge.net>
2020-05-12 10:14:57 +0200

    fixes in bsrw

--- src/filters/bsrw.c
+++ src/filters/bsrw.c
@@ -41,6 +41,8 @@ struct _bsrw_pid_ctx
 	GF_Err (*rewrite_packet)(GF_BSRWCtx *ctx, BSRWPid *pctx, GF_FilterPacket *pck);
 
 	s32 prev_clrp, prev_txchar, prev_mxcoef, prev_sar;
+
+	u32 nalu_size_length;
 };
 
 struct _bsrw_ctx
@@ -48,11 +50,17 @@ struct _bsrw_ctx
 	GF_Fraction sar;
 	s32 m4vpl, prof, lev, pcomp, pidc, pspace, gpcflags;
 	char *clrp, *txchar, *mxcoef;
+	Bool remsei;
 
 
 	GF_List *pids;
 	Bool reconfigure;
 };
+
+static GF_Err none_rewrite_packet(GF_BSRWCtx *ctx, BSRWPid *pctx, GF_FilterPacket *pck)
+{
+	return gf_filter_pck_forward(pck, pctx->opid);
+}
 
 
 static GF_Err m4v_rewrite_pid_config(GF_BSRWCtx *ctx, BSRWPid *pctx)
@@ -89,6 +97,67 @@ static GF_Err m4v_rewrite_pid_config(GF_BSRWCtx *ctx, BSRWPid *pctx)
 
 	return GF_OK;
 }
+
+static GF_Err avc_rewrite_packet(GF_BSRWCtx *ctx, BSRWPid *pctx, GF_FilterPacket *pck)
+{
+	u32 size, pck_size, final_size;
+	GF_FilterPacket *dst;
+	u8 *output;
+	const u8 *data = gf_filter_pck_get_data(pck, &pck_size);
+	if (!data)
+		return gf_filter_pck_forward(pck, pctx->opid);
+
+	final_size = 0;
+	size=0;
+	while (size<pck_size) {
+		u8 nal_type=0;
+		u32 nal_hdr = pctx->nalu_size_length;
+		u32 nal_size = 0;
+		while (nal_hdr) {
+			nal_size |= data[size];
+			size++;
+			nal_hdr--;
+			if (!nal_hdr) break;
+			nal_size<<=8;
+		}
+		nal_type = data[size];
+		nal_type = nal_type & 0x1F;
+		if (nal_type != GF_AVC_NALU_SEI) {
+			final_size += nal_size+pctx->nalu_size_length;
+		}
+		size += nal_size;
+	}
+	if (final_size == pck_size)
+		return gf_filter_pck_forward(pck, pctx->opid);
+
+	dst = gf_filter_pck_new_alloc(pctx->opid, final_size, &output);
+	gf_filter_pck_merge_properties(pck, dst);
+
+	size=0;
+	while (size<pck_size) {
+		u8 nal_type=0;
+		u32 nal_hdr = pctx->nalu_size_length;
+		u32 nal_size = 0;
+		while (nal_hdr) {
+			nal_size |= data[size];
+			size++;
+			nal_hdr--;
+			if (!nal_hdr) break;
+			nal_size<<=8;
+		}
+		nal_type = data[size];
+		nal_type = nal_type & 0x1F;
+		if (nal_type == GF_AVC_NALU_SEI) {
+			size += nal_size;
+			continue;
+		}
+		memcpy(output, &data[size-pctx->nalu_size_length], pctx->nalu_size_length+nal_size);
+		output += pctx->nalu_size_length+nal_size;
+		size += nal_size;
+	}
+	return gf_filter_pck_send(dst);
+}
+
 static GF_Err avc_rewrite_pid_config(GF_BSRWCtx *ctx, BSRWPid *pctx)
 {
 	GF_AVCConfig *avcc;
@@ -107,16 +176,101 @@ static GF_Err avc_rewrite_pid_config(GF_BSRWCtx *ctx, BSRWPid *pctx)
 		if (!e)
 			gf_filter_pid_set_property(pctx->opid, GF_PROP_PID_SAR, &PROP_FRAC(ctx->sar) );
 	}
-	if (ctx->lev>=0) avcc->AVCLevelIndication = ctx->lev;
-	if (ctx->prof>=0) avcc->AVCProfileIndication = ctx->prof;
-	if (ctx->pcomp>=0) avcc->profile_compatibility = ctx->pcomp;
+
+	if ((ctx->lev>=0) || (ctx->prof>=0) || (ctx->pcomp>=0)) {
+		u32 i, count = gf_list_count(avcc->sequenceParameterSets);
+		for (i=0; i<count; i++) {
+			GF_AVCConfigSlot *sps = gf_list_get(avcc->sequenceParameterSets, i);
+			//first byte is nalu header, then profile_idc (8bits), prof_comp (8buts) a,d level_idc (8bits)
+			if (ctx->prof>=0) {
+				sps->data[1] = (u8) ctx->prof;
+			}
+			if (ctx->pcomp>=0) {
+				sps->data[2] = (u8) ctx->pcomp;
+			}
+			if (ctx->lev>=0) {
+				sps->data[3] = (u8) ctx->lev;
+			}
+		}
+		if (ctx->lev>=0) avcc->AVCLevelIndication = ctx->lev;
+		if (ctx->prof>=0) avcc->AVCProfileIndication = ctx->prof;
+		if (ctx->pcomp>=0) avcc->profile_compatibility = ctx->pcomp;
+	}
 
 	gf_odf_avc_cfg_write(avcc, &dsi, &dsi_size);
+	pctx->nalu_size_length = avcc->nal_unit_size;
+
 	gf_odf_avc_cfg_del(avcc);
 	if (e) return e;
 
 	gf_filter_pid_set_property(pctx->opid, GF_PROP_PID_DECODER_CONFIG, &PROP_DATA_NO_COPY(dsi, dsi_size) );
+
+	if (ctx->remsei) {
+		pctx->rewrite_packet = avc_rewrite_packet;
+	} else {
+		pctx->rewrite_packet = none_rewrite_packet;
+	}
 	return GF_OK;
+}
+
+static GF_Err hevc_rewrite_packet(GF_BSRWCtx *ctx, BSRWPid *pctx, GF_FilterPacket *pck)
+{
+	u32 size, pck_size, final_size;
+	GF_FilterPacket *dst;
+	u8 *output;
+	const u8 *data = gf_filter_pck_get_data(pck, &pck_size);
+	if (!data)
+		return gf_filter_pck_forward(pck, pctx->opid);
+
+	final_size = 0;
+	size=0;
+	while (size<pck_size) {
+		u8 nal_type=0;
+		u32 nal_hdr = pctx->nalu_size_length;
+		u32 nal_size = 0;
+		while (nal_hdr) {
+			nal_size |= data[size];
+			size++;
+			nal_hdr--;
+			if (!nal_hdr) break;
+			nal_size<<=8;
+		}
+		nal_type = data[size];
+		nal_type = (nal_type & 0x7E) >> 1;
+		if ((nal_type != GF_HEVC_NALU_SEI_PREFIX) && (nal_type != GF_HEVC_NALU_SEI_SUFFIX) ) {
+			final_size += nal_size+pctx->nalu_size_length;
+		}
+		size += nal_size;
+	}
+	if (final_size == pck_size)
+		return gf_filter_pck_forward(pck, pctx->opid);
+
+	dst = gf_filter_pck_new_alloc(pctx->opid, final_size, &output);
+	gf_filter_pck_merge_properties(pck, dst);
+
+	size=0;
+	while (size<pck_size) {
+		u8 nal_type=0;
+		u32 nal_hdr = pctx->nalu_size_length;
+		u32 nal_size = 0;
+		while (nal_hdr) {
+			nal_size |= data[size];
+			size++;
+			nal_hdr--;
+			if (!nal_hdr) break;
+			nal_size<<=8;
+		}
+		nal_type = data[size];
+		nal_type = (nal_type & 0x7E) >> 1;
+		if ((nal_type == GF_HEVC_NALU_SEI_PREFIX) || (nal_type == GF_HEVC_NALU_SEI_SUFFIX) ) {
+			size += nal_size;
+			continue;
+		}
+		memcpy(output, &data[size-pctx->nalu_size_length], pctx->nalu_size_length+nal_size);
+		output += pctx->nalu_size_length+nal_size;
+		size += nal_size;
+	}
+	return gf_filter_pck_send(dst);
 }
 
 static GF_Err hevc_rewrite_pid_config(GF_BSRWCtx *ctx, BSRWPid *pctx)
@@ -143,10 +297,17 @@ static GF_Err hevc_rewrite_pid_config(GF_BSRWCtx *ctx, BSRWPid *pctx)
 
 
 	gf_odf_hevc_cfg_write(hvcc, &dsi, &dsi_size);
+	pctx->nalu_size_length = hvcc->nal_unit_size;
 	gf_odf_hevc_cfg_del(hvcc);
 	if (e) return e;
 
 	gf_filter_pid_set_property(pctx->opid, GF_PROP_PID_DECODER_CONFIG, &PROP_DATA_NO_COPY(dsi, dsi_size) );
+
+	if (ctx->remsei) {
+		pctx->rewrite_packet = hevc_rewrite_packet;
+	} else {
+		pctx->rewrite_packet = none_rewrite_packet;
+	}
 	return GF_OK;
 }
 
@@ -173,7 +334,7 @@ static GF_Err rewrite_pid_config(GF_BSRWCtx *ctx, BSRWPid *pctx)
 		else {
 			char szCoef[100];
 			pctx->clrp = atoi(ctx->clrp);
-			sprintf(szCoef, "%d", pctx->clrp);
+			sprintf(szCoef, "%u", pctx->clrp);
 			if (stricmp(szCoef, ctx->clrp))
 				pctx->clrp = -1;
 		}
@@ -185,7 +346,7 @@ static GF_Err rewrite_pid_config(GF_BSRWCtx *ctx, BSRWPid *pctx)
 		else {
 			char szTxChar[100];
 			pctx->txchar = atoi(ctx->txchar);
-			sprintf(szTxChar, "%d", pctx->txchar);
+			sprintf(szTxChar, "%u", pctx->txchar);
 			if (stricmp(szTxChar, ctx->txchar))
 				pctx->txchar = -1;
 		}
@@ -197,7 +358,7 @@ static GF_Err rewrite_pid_config(GF_BSRWCtx *ctx, BSRWPid *pctx)
 		else {
 			char szMx[100];
 			pctx->mxcoef = atoi(ctx->mxcoef);
-			sprintf(szMx, "%d", pctx->mxcoef);
+			sprintf(szMx, "%u", pctx->mxcoef);
 			if (stricmp(szMx, ctx->mxcoef))
 				pctx->mxcoef = -1;
 		}
@@ -206,10 +367,6 @@ static GF_Err rewrite_pid_config(GF_BSRWCtx *ctx, BSRWPid *pctx)
 }
 
 
-static GF_Err none_rewrite_packet(GF_BSRWCtx *ctx, BSRWPid *pctx, GF_FilterPacket *pck)
-{
-	return gf_filter_pck_forward(pck, pctx->opid);
-}
 static GF_Err prores_rewrite_packet(GF_BSRWCtx *ctx, BSRWPid *pctx, GF_FilterPacket *pck)
 {
 	u8 *output;
@@ -263,11 +420,11 @@ static GF_Err prores_rewrite_packet(GF_BSRWCtx *ctx, BSRWPid *pctx, GF_FilterPac
 			pctx->prev_txchar = pctx->txchar;
 		}
 	}
-	if (pctx->txchar>=0) {
+	if (pctx->mxcoef>=0) {
 		output[24] = (u8) pctx->mxcoef;
-		if (pctx->prev_txchar != pctx->txchar) {
-			gf_filter_pid_set_property(pctx->opid, GF_PROP_PID_COLR_TRANSFER, &PROP_UINT(pctx->txchar) );
-			pctx->prev_txchar = pctx->txchar;
+		if (pctx->prev_mxcoef != pctx->mxcoef) {
+			gf_filter_pid_set_property(pctx->opid, GF_PROP_PID_COLR_MX, &PROP_UINT(pctx->mxcoef) );
+			pctx->prev_mxcoef = pctx->mxcoef;
 		}
 	}
 	return gf_filter_pck_send(dst_pck);
@@ -277,7 +434,7 @@ static GF_Err prores_rewrite_packet(GF_BSRWCtx *ctx, BSRWPid *pctx, GF_FilterPac
 static GF_Err bsrw_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remove)
 {
 	const GF_PropertyValue *prop;
-//	GF_BSRWCtx *ctx = (GF_BSRWCtx *) gf_filter_get_udta(filter);
+	GF_BSRWCtx *ctx = (GF_BSRWCtx *) gf_filter_get_udta(filter);
 	BSRWPid *pctx = gf_filter_pid_get_udta(pid);
 
 	//disconnect of src pid (not yet supported)
@@ -286,6 +443,7 @@ static GF_Err bsrw_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_r
 			//one in one out, this is simple
 			gf_filter_pid_remove(pctx->opid);
 			gf_filter_pid_set_udta(pid, NULL);
+			gf_list_del_item(ctx->pids, pctx);
  			gf_free(pctx);
 		}
 		return GF_OK;
@@ -300,6 +458,7 @@ static GF_Err bsrw_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_r
 		gf_filter_pid_set_udta(pid, pctx);
 		pctx->rewrite_pid_config = none_rewrite_pid_config;
 		pctx->rewrite_packet = none_rewrite_packet;
+		gf_list_add(ctx->pids, pctx);
 	}
 
 	prop = gf_filter_pid_get_property(pid, GF_PROP_PID_CODECID);
@@ -337,6 +496,7 @@ static GF_Err bsrw_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_r
 	gf_filter_pid_copy_properties(pctx->opid, pctx->ipid);
 	pctx->codec_id = prop->value.uint;
 	pctx->reconfigure = GF_TRUE;
+	gf_filter_pid_set_framing_mode(pid, GF_TRUE);
 	return GF_OK;
 }
 
@@ -380,6 +540,27 @@ static GF_Err bsrw_update_arg(GF_Filter *filter, const char *arg_name, const GF_
 	ctx->reconfigure = GF_TRUE;
 	return GF_OK;
 }
+
+static GF_Err bsrw_initialize(GF_Filter *filter)
+{
+	GF_BSRWCtx *ctx = (GF_BSRWCtx *) gf_filter_get_udta(filter);
+	ctx->pids = gf_list_new();
+
+#ifdef GPAC_ENABLE_COVERAGE
+	bsrw_update_arg(filter, NULL, NULL);
+#endif
+	return GF_OK;
+}
+static void bsrw_finalize(GF_Filter *filter)
+{
+	GF_BSRWCtx *ctx = (GF_BSRWCtx *) gf_filter_get_udta(filter);
+	while (gf_list_count(ctx->pids)) {
+		BSRWPid *pctx = gf_list_pop_back(ctx->pids);
+		gf_free(pctx);
+	}
+	gf_list_del(ctx->pids);
+}
+
 #define OFFS(_n)	#_n, offsetof(GF_BSRWCtx, _n)
 static const GF_FilterArgs BSRWArgs[] =
 {
@@ -395,6 +576,8 @@ static const GF_FilterArgs BSRWArgs[] =
 	{ OFFS(pidc), "profile IDC for HEVC", GF_PROP_SINT, "-1", NULL, GF_FS_ARG_UPDATE},
 	{ OFFS(pspace), "profile space for HEVC", GF_PROP_SINT, "-1", NULL, GF_FS_ARG_UPDATE},
 	{ OFFS(gpcflags), "general compatibility flags for HEVC", GF_PROP_SINT, "-1", NULL, GF_FS_ARG_UPDATE},
+
+	{ OFFS(remsei), "remove SEI messages from bitstream", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_UPDATE},
 
 	{0}
 };
@@ -427,12 +610,11 @@ GF_FilterRegister BSRWRegister = {
 	GF_FS_SET_HELP("This filter rewrites some metadata of various bitstream formats.\n"
 	"The filter can currently modify the following properties in the bitstream:\n"
 	"- MPEG-4 Visual: aspect ratio and profile/level\n"
-	"- AVC|H264: aspect ratio\n"
+	"- AVC|H264: aspect ratio, profile, level, profile compatibility\n"
 	"- HEVC: aspect ratio\n"
 	"- ProRes: aspect ratio, color primaries, transfer characteristics and matrix coefficients\n"
 	"  \n"
 	"The filter can currently modify the following properties in the stream configuration but not in the bitstream:\n"
-	"- AVC|H264: profile, level, profile compatibility\n"
 	"- HEVC: profile IDC, profile space, general compatibility flags\n"
 	)
 	.private_size = sizeof(GF_BSRWCtx),
@@ -440,6 +622,8 @@ GF_FilterRegister BSRWRegister = {
 	.flags = GF_FS_REG_EXPLICIT_ONLY,
 	.args = BSRWArgs,
 	SETCAPS(BSRWCaps),
+	.initialize = bsrw_initialize,
+	.finalize = bsrw_finalize,
 	.configure_pid = bsrw_configure_pid,
 	.process = bsrw_process,
 	.update_arg = bsrw_update_arg
