gpac/gpac

commit c782327b3b670759d3a4afd9c65967dcabf0701e

AV1: state default are not always zero fixes #1504 false warnings

applications/mp4box/filedump.c
@@ -1656,7 +1656,7 @@ static void dump_isom_obu(GF_ISOFile *file, u32 trackID, FILE *dump, Bool dump_c
 
 	track = gf_isom_get_track_by_id(file, trackID);
 
-	memset(&av1, 0, sizeof(AV1State));
+	av1_reset_state(&av1);
 	av1.config = gf_isom_av1_config_get(file, track, 1);
 	if (!av1.config) {
 		fprintf(stderr, "Error: Track #%d is not AV1!\n", trackID);

include/gpac/internal/media_dev.h
@@ -644,6 +644,7 @@ Bool gf_media_aom_probe_annexb(GF_BitStream *bs);
 GF_Err gf_media_aom_av1_parse_obu(GF_BitStream *bs, ObuType *obu_type, u64 *obu_size, u32 *obu_hdr_size, AV1State *state);
 
 Bool av1_is_obu_header(ObuType obu_type);
+void av1_reset_state(AV1State *state);
 void av1_reset_frame_state(AV1StateFrame *frame_state);
 
 typedef struct

src/isomedia/avc_ext.c
@@ -2668,7 +2668,7 @@ GF_Err av1c_Read(GF_Box *s, GF_BitStream *bs) {
 
 	if (ptr->config) gf_odf_av1_cfg_del(ptr->config);
 	GF_SAFEALLOC(ptr->config, GF_AV1Config);
-	memset(&state, 0, sizeof(AV1State));
+	av1_reset_state(&state);
 	state.config = ptr->config;
 
 	ptr->config->marker = gf_bs_read_int(bs, 1);

src/media_tools/dash_segmenter.c
@@ -659,7 +659,7 @@ GF_Err gf_media_get_rfc_6381_codec_name(GF_ISOFile *movie, u32 track, char *szCo
 		GF_Err e = GF_OK;
 		u32 i = 0;
 
-		memset(&av1_state, 0, sizeof(AV1State));
+		av1_reset_state(&av1_state);
 		av1c = gf_isom_av1_config_get(movie, track, 1);
 		if (!av1c) {
 			GF_LOG(GF_LOG_DEBUG, GF_LOG_AUTHOR, ("[ISOM Tools] No config found for AV1 file (\"%s\") when computing RFC6381.\n", gf_4cc_to_str(subtype)));

src/media_tools/media_import.c
@@ -7378,6 +7378,14 @@ void av1_reset_frame_state(AV1StateFrame *frame_state) {
 	frame_state->is_first_frame = GF_TRUE;
 }
 
+void av1_reset_state(AV1State* state)
+{
+	memset(&state, 0, sizeof(AV1State));
+	state->color_primaries = 2; /*unspecified*/
+	state->transfer_characteristics = 2; /*unspecified*/
+	state->matrix_coefficients = 2; /*unspecified*/
+}
+
 static Bool probe_webm_matrovska(GF_BitStream *bs)
 {
 	char probe[64], *found = NULL;
@@ -7437,7 +7445,7 @@ static GF_Err gf_import_aom_av1(GF_MediaImporter *import)
 		return GF_OK;
 	}
 
-	memset(&state, 0, sizeof(AV1State));
+	av1_reset_state(&state);
 	av1_cfg = gf_odf_av1_cfg_new();
 	state.config = av1_cfg;
 	if (import->flags & GF_IMPORT_KEEP_AV1_TEMPORAL_OBU)

commit 6b86e7c36d9ed3ff716f221184130fc75fdfd408

fixed bugs introduced by c782327b

applications/mp4box/filedump.c
@@ -1577,7 +1577,7 @@ static void dump_obu(FILE *dump, u32 idx, AV1State *av1, char *obu, u32 obu_leng
 #define DUMP_OBU_INT(_v) fprintf(dump, #_v"=\"%d\" ", av1->_v);
 #define DUMP_OBU_INT2(_n, _v) fprintf(dump, _n"=\"%d\" ", _v);
 
-	fprintf(dump, "   <OBU number=\"%d\" size=\"%d\" type=\"%s\" header_size=\"%d\" has_size_field=\"%d\" has_ext=\"%d\" temporalID=\"%d\" spatialID=\"%d\" ", idx, (u32) obu_size, av1_get_obu_name(obu_type), hdr_size, av1->obu_has_size_field, av1->obu_extension_flag, av1->temporal_id , av1->spatial_id);
+	fprintf(dump, "   <OBU number=\"%d\" size=\"%d\" type=\"%s\" header_size=\"%d\" has_size_field=\"%d\" has_ext=\"%d\" temporalID=\"%d\" spatialID=\"%d\" ", idx, (u32) obu_size, gf_av1_get_obu_name(obu_type), hdr_size, av1->obu_has_size_field, av1->obu_extension_flag, av1->temporal_id , av1->spatial_id);
 	if (dump_crc) fprintf(dump, "crc=\"%u\" ", gf_crc_32(obu, obu_length) );
 	switch (obu_type) {
 	case OBU_SEQUENCE_HEADER:
@@ -1656,7 +1656,7 @@ static void dump_isom_obu(GF_ISOFile *file, u32 trackID, FILE *dump, Bool dump_c
 
 	track = gf_isom_get_track_by_id(file, trackID);
 
-	av1_reset_state(&av1);
+	gf_av1_reset_state(&av1);
 	av1.config = gf_isom_av1_config_get(file, track, 1);
 	if (!av1.config) {
 		fprintf(stderr, "Error: Track #%d is not AV1!\n", trackID);
@@ -3042,7 +3042,7 @@ void DumpTrackInfo(GF_ISOFile *file, u32 trackID, Bool full_dump, Bool is_track_
 			u32 j;
 			GF_AV1_OBUArrayEntry *obu = gf_list_get(av1c->obu_array, i);
 			gf_sha1_csum((u8*)obu->obu, (u32)obu->obu_length, hash);
-			fprintf(stderr, "\tOBU#%d %s hash: ", i+1, av1_get_obu_name(obu->obu_type) );
+			fprintf(stderr, "\tOBU#%d %s hash: ", i+1, gf_av1_get_obu_name(obu->obu_type) );
 			for (j=0; j<20; j++) fprintf(stderr, "%02X", hash[j]);
 			fprintf(stderr, "\n");
 		}

include/gpac/avparse.h
@@ -363,7 +363,7 @@ typedef enum {
 	OBU_METADATA_TYPE_TIMECODE = 5
 } ObuMetadataType;
 
-const char *av1_get_obu_name(ObuType obu_type);
+const char *gf_av1_get_obu_name(ObuType obu_type);
 
 /*! @} */
 

include/gpac/internal/media_dev.h
@@ -644,7 +644,7 @@ Bool gf_media_aom_probe_annexb(GF_BitStream *bs);
 GF_Err gf_media_aom_av1_parse_obu(GF_BitStream *bs, ObuType *obu_type, u64 *obu_size, u32 *obu_hdr_size, AV1State *state);
 
 Bool av1_is_obu_header(ObuType obu_type);
-void av1_reset_state(AV1State *state);
+void gf_av1_reset_state(AV1State *state);
 void av1_reset_frame_state(AV1StateFrame *frame_state);
 
 typedef struct

src/export.cpp
@@ -2359,7 +2359,9 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_term_service_get_dm) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_dm_set_localcache_provider) )
 
-#pragma comment (linker, EXPORT_SYMBOL(av1_get_obu_name) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_av1_get_obu_name) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_av1_reset_state) )
+
 
 #pragma comment (linker, EXPORT_SYMBOL(gf_get_next_pow2) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_htonl) )

src/isomedia/avc_ext.c
@@ -2668,7 +2668,7 @@ GF_Err av1c_Read(GF_Box *s, GF_BitStream *bs) {
 
 	if (ptr->config) gf_odf_av1_cfg_del(ptr->config);
 	GF_SAFEALLOC(ptr->config, GF_AV1Config);
-	av1_reset_state(&state);
+	gf_av1_reset_state(&state);
 	state.config = ptr->config;
 
 	ptr->config->marker = gf_bs_read_int(bs, 1);

src/isomedia/box_dump.c
@@ -1712,7 +1712,7 @@ GF_Err av1c_dump(GF_Box *a, FILE *trace) {
 
 		for (i=0; i<obu_count; i++) {
 			GF_AV1_OBUArrayEntry *a = gf_list_get(ptr->config->obu_array, i);
-			fprintf(trace, "<OBU type=\"%d\" name=\"%s\" size=\"%d\" content=\"", a->obu_type, av1_get_obu_name(a->obu_type), (u32) a->obu_length);
+			fprintf(trace, "<OBU type=\"%d\" name=\"%s\" size=\"%d\" content=\"", a->obu_type, gf_av1_get_obu_name(a->obu_type), (u32) a->obu_length);
 			dump_data(trace, (char *)a->obu, (u32) a->obu_length);
 			fprintf(trace, "\"/>\n");
 		}

src/media_tools/av_parsers.c
@@ -2347,7 +2347,7 @@ static GF_Err av1_parse_obu_header(GF_BitStream *bs, ObuType *obu_type, Bool *ob
 }
 
 GF_EXPORT
-const char *av1_get_obu_name(ObuType obu_type)
+const char *gf_av1_get_obu_name(ObuType obu_type)
 {
 	switch (obu_type) {
 	case OBU_SEQUENCE_HEADER: return "seq_header";

src/media_tools/dash_segmenter.c
@@ -659,7 +659,7 @@ GF_Err gf_media_get_rfc_6381_codec_name(GF_ISOFile *movie, u32 track, char *szCo
 		GF_Err e = GF_OK;
 		u32 i = 0;
 
-		av1_reset_state(&av1_state);
+		gf_av1_reset_state(&av1_state);
 		av1c = gf_isom_av1_config_get(movie, track, 1);
 		if (!av1c) {
 			GF_LOG(GF_LOG_DEBUG, GF_LOG_AUTHOR, ("[ISOM Tools] No config found for AV1 file (\"%s\") when computing RFC6381.\n", gf_4cc_to_str(subtype)));

src/media_tools/media_import.c
@@ -7378,12 +7378,17 @@ void av1_reset_frame_state(AV1StateFrame *frame_state) {
 	frame_state->is_first_frame = GF_TRUE;
 }
 
-void av1_reset_state(AV1State* state)
+GF_EXPORT
+void gf_av1_reset_state(AV1State* state)
 {
-	memset(&state, 0, sizeof(AV1State));
-	state->color_primaries = 2; /*unspecified*/
-	state->transfer_characteristics = 2; /*unspecified*/
-	state->matrix_coefficients = 2; /*unspecified*/
+	if (state) {
+		GF_AV1Config *av1cfg = state->config;
+		memset(state, 0, sizeof(AV1State));
+		state->config = av1cfg;
+		state->color_primaries = 2; /*unspecified*/
+		state->transfer_characteristics = 2; /*unspecified*/
+		state->matrix_coefficients = 2; /*unspecified*/
+	}
 }
 
 static Bool probe_webm_matrovska(GF_BitStream *bs)
@@ -7445,7 +7450,7 @@ static GF_Err gf_import_aom_av1(GF_MediaImporter *import)
 		return GF_OK;
 	}
 
-	av1_reset_state(&state);
+	gf_av1_reset_state(&state);
 	av1_cfg = gf_odf_av1_cfg_new();
 	state.config = av1_cfg;
 	if (import->flags & GF_IMPORT_KEEP_AV1_TEMPORAL_OBU)