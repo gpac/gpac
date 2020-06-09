gpac/gpac

commit 1847fae7d3002db5ed1b9f51f408f48d0285a9c8

more cppcheck and scan-build fixes

src/filters/dec_j2k.c
@@ -335,7 +335,13 @@ static GF_Err j2kdec_process(GF_Filter *filter)
 
 	if (res) res = opj_read_header(stream, codec, &image);
 	if (res) res = opj_set_decode_area(codec, image, 0, 0, image->x1, image->y1);
-	if (res) res = opj_decode(codec, stream, image);
+	if (res) {
+		res = opj_decode(codec, stream, image);
+		if (res) {
+			opj_image_destroy(image);
+			image = NULL;
+		}
+	}
 
 #else
 

commit 628060eece28d5bdd4a59011f2a9a228a17831c4

fixed bug introduced by 1847fae7

src/filters/dec_j2k.c
@@ -337,7 +337,8 @@ static GF_Err j2kdec_process(GF_Filter *filter)
 	if (res) res = opj_set_decode_area(codec, image, 0, 0, image->x1, image->y1);
 	if (res) {
 		res = opj_decode(codec, stream, image);
-		if (res) {
+		if (!res) {
+			GF_LOG(GF_LOG_ERROR, GF_LOG_CODEC, ("[OpenJPEG] Decoding failed\n"));
 			opj_image_destroy(image);
 			image = NULL;
 		}