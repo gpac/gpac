cf3c7047b6f60152b4bcffd5ebb220daf3763d5d
Aurelien David <aurelien.david@telecom-paristech.fr>
2020-06-17 15:37:21 +0200

    fix typo in yuv bigendian macros (#1511)

--- src/evg/raster_yuv.c
+++ src/evg/raster_yuv.c
@@ -1291,7 +1291,7 @@ GF_Err evg_surface_clear_yuyv(GF_EVGSurface *_surf, GF_IRect rc, GF_Color col)
 #define set_u16_le(_ptr, val) { ((u8 *)_ptr)[0] = (val>>8)&0xFF;  ((u8 *)_ptr)[1] = (val&0xFF); }
 #define set_u16_be(_ptr, val) { *(u16 *) _ptr = (u16) val; }
 
-#define get_u16_le(val, _ptr) { val = ((u32) (*(u8 *) ptr+1)<< 8) | *(u8 *) ptr; }
+#define get_u16_le(val, _ptr) { val = ((u32) (*(u8 *) _ptr+1)<< 8) | *(u8 *) _ptr; }
 #define get_u16_be(val, _ptr) { val = *(u16 *) (_ptr); }
 
 #else
@@ -1300,7 +1300,7 @@ GF_Err evg_surface_clear_yuyv(GF_EVGSurface *_surf, GF_IRect rc, GF_Color col)
 #define set_u16_be(_ptr, val) { ((u8 *)_ptr)[0] = (val>>8)&0xFF;  ((u8 *)_ptr)[1] = (val&0xFF); }
 
 #define get_u16_le(val, _ptr) { val = *(u16 *) (_ptr); }
-#define get_u16_be(val, _ptr) { val = ((u32) (*(u8 *) ptr)<< 8) | *(u8 *) ptr+1; }
+#define get_u16_be(val, _ptr) { val = ((u32) (*(u8 *) _ptr)<< 8) | *(u8 *) _ptr+1; }
 
 #endif
 
