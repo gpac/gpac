da15933400620e92ff0bd24bdbf1b39b2179bafe
jeanlf <jeanlf@gpac.io>
2020-07-08 16:23:52 +0200

    fixed qt brand adjustment in flat mode

--- src/isomedia/isom_intern.c
+++ src/isomedia/isom_intern.c
@@ -1128,14 +1128,20 @@ GF_ISOFile *gf_isom_create_movie(const char *fileName, GF_ISOOpenMode OpenMode, 
 
 	//but we have the edit one
 	if (OpenMode == GF_ISOM_OPEN_WRITE) {
+		const char *ext;
 		//THIS IS NOT A TEMP FILE, WRITE mode is used for "live capture"
 		//this file will be the final file...
 		mov->fileName = fileName ? gf_strdup(fileName) : NULL;
 		e = gf_isom_datamap_new(fileName, NULL, GF_ISOM_DATA_MAP_WRITE, &mov->editFileMap);
 		if (e) goto err_exit;
 
-		/*brand is set to ISOM by default - it may be touched until sample data is added to track*/
-		gf_isom_set_brand_info( (GF_ISOFile *) mov, GF_ISOM_BRAND_ISOM, 1);
+		/*brand is set to ISOM or QT by default - it may be touched until sample data is added to track*/
+		ext = gf_file_ext_start(fileName);
+		if (ext && (!strnicmp(ext, ".mov", 4) || !strnicmp(ext, ".qt", 3))) {
+			gf_isom_set_brand_info((GF_ISOFile *) mov, GF_ISOM_BRAND_QT, 512);
+		} else {
+			gf_isom_set_brand_info((GF_ISOFile *) mov, GF_ISOM_BRAND_ISOM, 1);
+		}
 	} else {
 		//we are in EDIT mode but we are creating the file -> temp file
 		mov->finalName = fileName ? gf_strdup(fileName) : NULL;
