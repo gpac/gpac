gpac/gpac

commit 552315ee3faee809dbd42ccdc6255adaa0d49483

fixes in audio preroll signaling

src/filters/mux_isom.c
@@ -91,7 +91,7 @@ typedef struct
 
 	Bool skip_bitrate_update;
 	Bool has_open_gop;
-	Bool has_gdr;
+	GF_FilterSAPType gdr_type;
 
 	Bool next_is_first_sample;
 
@@ -2630,7 +2630,9 @@ sample_entry_done:
 
 		//if we have an edit list (due to track template) only providing media offset, trash it
 		if (!gf_isom_get_edit_list_type(ctx->file, tkw->track_num, &moffset)) {
-			gf_isom_remove_edits(ctx->file, tkw->track_num);
+			if (!gf_sys_old_arch_compat()) {
+				gf_isom_remove_edits(ctx->file, tkw->track_num);
+			}
 		}
 		p = gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_DELAY);
 		if (p) {
@@ -2980,7 +2982,6 @@ static GF_Err mp4_mux_process_sample(GF_MP4MuxCtx *ctx, TrackWriter *tkw, GF_Fil
 	u32 timescale = 0;
 	const GF_PropertyValue *subs;
 	GF_FilterSAPType sap_type;
-	u8 dep_flags;
 	Bool insert_subsample_dsi = GF_FALSE;
 	u32 first_nal_is_audelim = GF_FALSE;
 	u32 sample_desc_index = tkw->stsd_idx;
@@ -3204,17 +3205,25 @@ static GF_Err mp4_mux_process_sample(GF_MP4MuxCtx *ctx, TrackWriter *tkw, GF_Fil
 		tkw->has_open_gop = GF_TRUE;
 	}
 	if (!ctx->noroll) {
-		if ((sap_type==4) || tkw->has_gdr) {
+		if ((sap_type==GF_FILTER_SAP_4) || (sap_type==GF_FILTER_SAP_4_PROL) || tkw->gdr_type) {
+			GF_ISOSampleRollType roll_type = 0;
 			s16 roll = gf_filter_pck_get_roll_info(pck);
+			if (sap_type==GF_FILTER_SAP_4) roll_type = GF_ISOM_SAMPLE_ROLL;
+			else if (sap_type==GF_FILTER_SAP_4_PROL) roll_type = GF_ISOM_SAMPLE_PREROLL;
+			else if (tkw->gdr_type==GF_FILTER_SAP_4_PROL) {
+				roll_type = GF_ISOM_SAMPLE_PREROLL_NONE;
+			}
+
 			if (for_fragment) {
-				e = gf_isom_fragment_set_sample_roll_group(ctx->file, tkw->track_id, tkw->samples_in_frag, (sap_type==4) ? GF_TRUE : GF_FALSE, roll);
+				e = gf_isom_fragment_set_sample_roll_group(ctx->file, tkw->track_id, tkw->samples_in_frag, roll_type, roll);
 			} else {
-				e = gf_isom_set_sample_roll_group(ctx->file, tkw->track_num, tkw->nb_samples, (sap_type==4) ? GF_TRUE : GF_FALSE, roll);
+				e = gf_isom_set_sample_roll_group(ctx->file, tkw->track_num, tkw->nb_samples, roll_type, roll);
 			}
 			if (e) {
 				GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[MP4Mux] Failed to set sample DTS "LLU" SAP 4 roll %s in roll group: %s\n", tkw->sample.DTS, roll, gf_error_to_string(e) ));
 			}
-			tkw->has_gdr = GF_TRUE;
+			if (sap_type && !tkw->gdr_type)
+				tkw->gdr_type = sap_type;
 		}
 	}
 	
@@ -3260,16 +3269,18 @@ static GF_Err mp4_mux_process_sample(GF_MP4MuxCtx *ctx, TrackWriter *tkw, GF_Fil
 		}
 	}
 
-	dep_flags = gf_filter_pck_get_dependency_flags(pck);
-	if (dep_flags) {
-		u32 is_leading = (dep_flags>>6) & 0x3;
-		u32 depends_on = (dep_flags>>4) & 0x3;
-		u32 depended_on = (dep_flags>>2) & 0x3;
-		u32 redundant = (dep_flags) & 0x3;
-		if (for_fragment) {
-			gf_isom_fragment_set_sample_flags(ctx->file, tkw->track_id, is_leading, depends_on, depended_on, redundant);
-		} else {
-			gf_isom_set_sample_flags(ctx->file, tkw->track_num, tkw->nb_samples, is_leading, depends_on, depended_on, redundant);
+	if (ctx->deps) {
+		u8 dep_flags = gf_filter_pck_get_dependency_flags(pck);
+		if (dep_flags) {
+			u32 is_leading = (dep_flags>>6) & 0x3;
+			u32 depends_on = (dep_flags>>4) & 0x3;
+			u32 depended_on = (dep_flags>>2) & 0x3;
+			u32 redundant = (dep_flags) & 0x3;
+			if (for_fragment) {
+				gf_isom_fragment_set_sample_flags(ctx->file, tkw->track_id, is_leading, depends_on, depended_on, redundant);
+			} else {
+				gf_isom_set_sample_flags(ctx->file, tkw->track_num, tkw->nb_samples, is_leading, depends_on, depended_on, redundant);
+			}
 		}
 	}
 

applications/mp4box/filedump.c
@@ -897,7 +897,8 @@ void dump_isom_timestamps(GF_ISOFile *file, char *inName, Bool is_final_name, u3
 			s64 cts;
 			u64 dts, offset;
 			u32 isLeading, dependsOn, dependedOn, redundant;
-			Bool is_rap, has_roll;
+			Bool is_rap;
+			GF_ISOSampleRollType roll_type;
 			s32 roll_distance;
 			u32 index;
 			GF_ISOSample *samp = gf_isom_get_sample_info(file, i+1, j+1, &index, &offset);
@@ -906,7 +907,7 @@ void dump_isom_timestamps(GF_ISOFile *file, char *inName, Bool is_final_name, u3
 				continue;
 			}
 			gf_isom_get_sample_flags(file, i+1, j+1, &isLeading, &dependsOn, &dependedOn, &redundant);
-			gf_isom_get_sample_rap_roll_info(file, i+1, j+1, &is_rap, &has_roll, &roll_distance);
+			gf_isom_get_sample_rap_roll_info(file, i+1, j+1, &is_rap, &roll_type, &roll_distance);
 			dts = samp->DTS;
 			cts = dts + (s32) samp->CTS_Offset;
 			fprintf(dump, "Sample %d\tDTS "LLU"\tCTS "LLD"\t%d\t%d", j+1, dts, cts, samp->dataLength, samp->IsRAP);
@@ -914,7 +915,7 @@ void dump_isom_timestamps(GF_ISOFile *file, char *inName, Bool is_final_name, u3
 			if (!skip_offset)
 				fprintf(dump, "\t"LLU, offset);
 
-			fprintf(dump, "\t%d\t%d\t%d\t%d\t%d\t%d\t%d", isLeading, dependsOn, dependedOn, redundant, is_rap, has_roll, roll_distance);
+			fprintf(dump, "\t%d\t%d\t%d\t%d\t%d\t%d\t%d", isLeading, dependsOn, dependedOn, redundant, is_rap, roll_type, roll_distance);
 
 			if (cts< (s64) dts) {
 				if (has_cts_offset==2) {
@@ -1469,10 +1470,11 @@ void dump_isom_saps(GF_ISOFile *file, GF_ISOTrackID trackID, u32 dump_saps_mode,
 
 		sap_type = samp->IsRAP;
 		if (!sap_type) {
-			Bool is_rap, has_roll;
+			Bool is_rap;
+			GF_ISOSampleRollType roll_type;
 			s32 roll_dist;
-			gf_isom_get_sample_rap_roll_info(file, track, i+1, &is_rap, &has_roll, &roll_dist);
-			if (has_roll) sap_type = SAP_TYPE_4;
+			gf_isom_get_sample_rap_roll_info(file, track, i+1, &is_rap, &roll_type, &roll_dist);
+			if (roll_type) sap_type = SAP_TYPE_4;
 			else if (is_rap)  sap_type = SAP_TYPE_3;
 		}
 

include/gpac/filters.h
@@ -3500,8 +3500,10 @@ typedef enum
 	GF_FILTER_SAP_2,
 	/*! open gop */
 	GF_FILTER_SAP_3,
-	/*! GDR */
+	/*! roll period (GDR or audio roll) - roll distance must be indicated in packet */
 	GF_FILTER_SAP_4,
+	/*! Audio preroll period - roll distance must be indicated in packet */
+	GF_FILTER_SAP_4_PROL
 } GF_FilterSAPType;
 
 /*! Sets packet SAP type

include/gpac/isomedia.h
@@ -453,6 +453,19 @@ enum
 
 };
 
+/*! sample roll information type*/
+typedef enum
+{
+	/*! no roll info associated*/
+	GF_ISOM_SAMPLE_ROLL_NONE=0,
+	/*! roll info describes a roll operation*/
+	GF_ISOM_SAMPLE_ROLL,
+	/*! roll info describes an audio preroll*/
+	GF_ISOM_SAMPLE_PREROLL,
+	/*! roll info describes audio preroll but is not set for this sample*/
+	GF_ISOM_SAMPLE_PREROLL_NONE
+} GF_ISOSampleRollType;
+
 #ifndef GPAC_DISABLE_ISOM
 
 #include <gpac/mpeg4_odf.h>
@@ -4171,15 +4184,17 @@ GF_Err gf_isom_fragment_set_cenc_sai(GF_ISOFile *isom_file, GF_ISOTrackID trackI
 \return error if any
 */
 GF_Err gf_isom_clone_pssh(GF_ISOFile *dst_file, GF_ISOFile *src_file, Bool in_moof);
+
+
 /*! sets roll information for a sample in a track fragment
 \param isom_file the target ISO file
 \param trackID the ID of the target track
 \param sample_number the sample number of the last sample
-\param is_roll set to GF_TRUE to indicate the sample is a roll sample, GF_FALSE otherwise
+\param roll_type indicate the sample roll type
 \param roll_distance set to the roll distance for a roll sample
 \return error if any
 */
-GF_Err gf_isom_fragment_set_sample_roll_group(GF_ISOFile *isom_file, GF_ISOTrackID trackID, u32 sample_number, Bool is_roll, s16 roll_distance);
+GF_Err gf_isom_fragment_set_sample_roll_group(GF_ISOFile *isom_file, GF_ISOTrackID trackID, u32 sample_number, GF_ISOSampleRollType roll_type, s16 roll_distance);
 
 /*! sets rap information for a sample in a track fragment
 \param isom_file the target ISO file
@@ -5885,10 +5900,10 @@ enum {
 \param trackNumber the target track
 \param sampleNumber the target sample number
 \param is_rap set to GF_TRUE if sample is a rap (open gop), GF_FALSE otherwise
-\param has_roll set to GF_TRUE of sample has roll information, GF_FALSE otherwise
+\param has_roll set to GF_ISOM_SAMPLE_ROLL if sample has roll information, GF_ISOM_SAMPLE_PREROLL if sample has preroll information, GF_ISOM_SAMPLE_ROLL_NONE otherwise
 \param roll_distance if sample has roll information, set to roll distance
 \return error if any*/
-GF_Err gf_isom_get_sample_rap_roll_info(GF_ISOFile *isom_file, u32 trackNumber, u32 sampleNumber, Bool *is_rap, Bool *has_roll, s32 *roll_distance);
+GF_Err gf_isom_get_sample_rap_roll_info(GF_ISOFile *isom_file, u32 trackNumber, u32 sampleNumber, Bool *is_rap, GF_ISOSampleRollType *roll_type, s32 *roll_distance);
 
 /*! returns opaque data of sample group
 \param isom_file the target ISO file
@@ -5943,11 +5958,11 @@ GF_Err gf_isom_set_sample_rap_group(GF_ISOFile *isom_file, u32 trackNumber, u32 
 \param isom_file the target ISO file
 \param trackNumber the target track
 \param sampleNumber the target sample number
-\param is_roll indicates that the sample is a roll recovery point
+\param roll_type indicates  the sample roll recovery type
 \param roll_distance indicates the roll distance before a correct decoding is produced
 \return error if any
 */
-GF_Err gf_isom_set_sample_roll_group(GF_ISOFile *isom_file, u32 trackNumber, u32 sampleNumber, Bool is_roll, s16 roll_distance);
+GF_Err gf_isom_set_sample_roll_group(GF_ISOFile *isom_file, u32 trackNumber, u32 sampleNumber, GF_ISOSampleRollType roll_type, s16 roll_distance);
 
 /*! sets encryption group for a sample number
 \param isom_file the target ISO file

src/filters/dasher.c
@@ -5829,10 +5829,16 @@ static GF_Err dasher_process(GF_Filter *filter)
 						if (ctx->sbound==DASHER_BOUNDS_CLOSEST) {
 							s64 diff = cts * base_ds->timescale / ds->timescale;
 							diff -= base_ds->adjusted_next_seg_start;
-							//this one may be negative
+							//this one may be negative, but we always want diff_next positive (next SAP in next segment)
 							if (diff<0)
 								diff = -diff;
-							if (diff<diff_next) {
+							//old arch was only using closest for tracks with sync points
+							if (gf_sys_old_arch_compat() && !base_ds->has_sync_points) {
+								if (diff_next > 0) {
+									force_seg_flush = GF_TRUE;
+								}
+							}
+							else if (diff<diff_next) {
 								force_seg_flush = GF_TRUE;
 							}
 						}

src/filters/dmx_gsf.c
@@ -719,7 +719,7 @@ GF_Err gsfdmx_read_data_pck(GSF_DemuxCtx *ctx, GSF_Stream *gst, GSF_Packet *gpck
 		dur = gf_bs_read_int(bs, durmodebits);
 	}
 
-	if (sap==GF_FILTER_SAP_4) {
+	if ((sap==GF_FILTER_SAP_4) || (sap==GF_FILTER_SAP_4_PROL)) {
 		roll = gf_bs_read_u16(bs);
 	}
 	if (has_dep)
@@ -819,7 +819,8 @@ GF_Err gsfdmx_read_data_pck(GSF_DemuxCtx *ctx, GSF_Stream *gst, GSF_Packet *gpck
 	if (seek) gf_filter_pck_set_seek_flag(gpck->pck, seek);
 	if (crypt) gf_filter_pck_set_crypt_flags(gpck->pck, crypt);
 	if (sap) gf_filter_pck_set_sap(gpck->pck, sap);
-	if (sap==GF_FILTER_SAP_4) gf_filter_pck_set_roll_info(gpck->pck, roll);
+	if ((sap==GF_FILTER_SAP_4) || (sap==GF_FILTER_SAP_4_PROL))
+		gf_filter_pck_set_roll_info(gpck->pck, roll);
 	return GF_OK;
 }
 

src/filters/inspect.c
@@ -1165,7 +1165,14 @@ static void inspect_dump_packet_fmt(GF_InspectCtx *ctx, FILE *dump, GF_FilterPac
 			else if (end) gf_fprintf(dump, "frame_end");
 			else gf_fprintf(dump, "frame_cont");
 		}
-		else if (!strcmp(key, "sap") || !strcmp(key, "rap")) gf_fprintf(dump, "%u", gf_filter_pck_get_sap(pck) );
+		else if (!strcmp(key, "sap") || !strcmp(key, "rap")) {
+			u32 sap = gf_filter_pck_get_sap(pck);
+			if (sap==GF_FILTER_SAP_4_PROL) {
+				gf_fprintf(dump, "4 (prol)");
+			} else {
+				gf_fprintf(dump, "%u", sap );
+			}
+		}
 		else if (!strcmp(key, "ilace")) gf_fprintf(dump, "%d", gf_filter_pck_get_interlaced(pck) );
 		else if (!strcmp(key, "corr")) gf_fprintf(dump, "%d", gf_filter_pck_get_corrupted(pck) );
 		else if (!strcmp(key, "seek")) gf_fprintf(dump, "%d", gf_filter_pck_get_seek_flag(pck) );
@@ -1409,7 +1416,7 @@ static void inspect_dump_tmcd(GF_InspectCtx *ctx, PidCtx *pctx, char *data, u32 
 
 static void inspect_dump_packet(GF_InspectCtx *ctx, FILE *dump, GF_FilterPacket *pck, u32 pid_idx, u64 pck_num, PidCtx *pctx)
 {
-	u32 idx=0, size;
+	u32 idx=0, size, sap;
 	u64 ts;
 	u8 dflags = 0;
 	GF_FilterClockType ck_type;
@@ -1471,7 +1478,12 @@ static void inspect_dump_packet(GF_InspectCtx *ctx, FILE *dump, GF_FilterPacket 
 	else DUMP_ATT_LLU("cts", ts )
 
 	DUMP_ATT_U("dur", gf_filter_pck_get_duration(pck) )
-	DUMP_ATT_U("sap", gf_filter_pck_get_sap(pck) )
+	sap = gf_filter_pck_get_sap(pck);
+	if (sap==GF_FILTER_SAP_4_PROL) {
+		DUMP_ATT_STR(sap, "4 (prol)");
+	} else {
+		DUMP_ATT_U("sap", gf_filter_pck_get_sap(pck) )
+	}
 	DUMP_ATT_D("ilace", gf_filter_pck_get_interlaced(pck) )
 	DUMP_ATT_D("corr", gf_filter_pck_get_corrupted(pck) )
 	DUMP_ATT_D("seek", gf_filter_pck_get_seek_flag(pck) )

src/filters/isoffin.h
@@ -137,7 +137,8 @@ typedef struct
 	GF_ISOSample *sample;
 	u64 sample_data_offset, last_valid_sample_data_offset;
 	GF_Err last_state;
-	Bool sap_3, sap_4;
+	Bool sap_3;
+	GF_ISOSampleRollType sap_4_type;
 	s32 roll;
 
 	Bool has_edit_list;

commit 2e4822805e4d775e84ae826d5fb02173de9fb53e

fixed bug introduced by 552315ee

src/filters/mux_isom.c
@@ -914,7 +914,7 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 			GF_LOG(GF_LOG_INFO, GF_LOG_CONTAINER, ("[MP4Mux] ProRes track detected, muxing to QTFF even though ISOBMFF was asked\n"));
 			ctx->make_qt = 2;
 		}
-		if (ctx->prores_track == tkw) {
+		if (ctx->prores_track && (ctx->prores_track != tkw)) {
 			GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[MP4Mux] More than one ProRes track detected, result might be non compliant\n"));
 		}
 		is_prores = GF_TRUE;
@@ -2603,6 +2603,7 @@ sample_entry_done:
 	tkw->probe_min_ctts = GF_FALSE;
 	if (is_true_pid && !tkw->nb_samples && !tkw->is_item) {
 		Bool use_negccts = GF_FALSE;
+		Bool remove_edits = GF_FALSE;
 		s64 moffset=0;
 		ctx->config_timing = GF_TRUE;
 		ctx->update_report = GF_TRUE;
@@ -2611,6 +2612,8 @@ sample_entry_done:
 		if (!gf_isom_get_edit_list_type(ctx->file, tkw->track_num, &moffset)) {
 			if (!gf_sys_old_arch_compat()) {
 				gf_isom_remove_edits(ctx->file, tkw->track_num);
+			} else {
+				remove_edits = GF_TRUE;
 			}
 		}
 		p = gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_DELAY);
@@ -2618,12 +2621,18 @@ sample_entry_done:
 			if (p->value.sint < 0) {
 				if (ctx->ctmode==MP4MX_CT_NEGCTTS) use_negccts = GF_TRUE;
 				else {
+					if (remove_edits) {
+						gf_isom_remove_edits(ctx->file, tkw->track_num);
+					}
 					gf_isom_set_edit(ctx->file, tkw->track_num, 0, 0, -p->value.sint, GF_ISOM_EDIT_NORMAL);
 				}
 			} else if (p->value.sint > 0) {
 				s64 dur = p->value.sint;
 				dur *= (u32) ctx->moovts;
 				dur /= tkw->src_timescale;
+				if (remove_edits) {
+					gf_isom_remove_edits(ctx->file, tkw->track_num);
+				}
 				gf_isom_set_edit(ctx->file, tkw->track_num, 0, dur, 0, GF_ISOM_EDIT_DWELL);
 				gf_isom_set_edit(ctx->file, tkw->track_num, 0, 0, 0, GF_ISOM_EDIT_NORMAL);
 			}