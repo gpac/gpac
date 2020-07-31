gpac/gpac

commit 0e6bd1c1fdd748aea7225c5d18c11437b1553ac2

fixed parsing of port in URL scheme when not escaped - cf #1559

src/filter_core/filter.c
@@ -1124,13 +1124,25 @@ static void filter_parse_dyn_args(GF_Filter *filter, const char *args, GF_Filter
 							|| !strncmp(args+4, "rtp://", 6)
 						) {
 							char *sep2 = sep ? strchr(sep+1, ':') : NULL;
-							if (sep2) {
-								u32 port = 0;;
-								sep2[0] = 0;
+							char *sep3 = sep ? strchr(sep+1, '/') : NULL;
+							if (sep2 && sep3 && (sep2>sep3)) {
+								sep2 = strchr(sep3, ':');
+							}
+							if (sep2 || sep3 || sep) {
+								char szPort[20];
+								u32 port = 0;
+								if (sep2) sep2[0] = 0;
+								else if (sep3) sep3[0] = 0;
 								if (sscanf(sep+1, "%d", &port)!=1) {
 									port = 0;
 								}
-								sep2[0] = ':';
+								snprintf(szPort, 20, "%d", port);
+								if (strcmp(sep+1, szPort))
+									port = 0;
+
+								if (sep2) sep2[0] = ':';
+								else if (sep3) sep3[0] = '/';
+
 								if (port) sep = sep2;
 							}
 						}

src/filters/out_sock.c
@@ -122,6 +122,11 @@ static GF_Err sockout_initialize(GF_Filter *filter)
 	else {
 		ext = gf_file_ext_start(ctx->dst);
 		if (ext) ext++;
+		if (ext) {
+			const char *port = strchr(ext, ':');
+			if (port)
+				ext = gf_file_ext_start(port);
+		}
 	}
 
 	if (!ext && !ctx->mime) {

commit b4b94447f37d2d730ebf5efde9180a2daf8be4e0

fixed bug introduced by 0e6bd1c1

src/filter_core/filter.c
@@ -1131,17 +1131,18 @@ static void filter_parse_dyn_args(GF_Filter *filter, const char *args, GF_Filter
 							if (sep2 || sep3 || sep) {
 								char szPort[20];
 								u32 port = 0;
-								if (sep2) sep2[0] = 0;
-								else if (sep3) sep3[0] = 0;
-								if (sscanf(sep+1, "%d", &port)!=1) {
-									port = 0;
+								if (sep2) {
+									sep2[0] = 0;
+									if (sep3) sep3[0] = 0;
 								}
-								snprintf(szPort, 20, "%d", port);
-								if (strcmp(sep+1, szPort))
-									port = 0;
-
+								else if (sep3) sep3[0] = 0;
+								if (sscanf(sep+1, "%d", &port)==1) {
+									snprintf(szPort, 20, "%d", port);
+									if (strcmp(sep+1, szPort))
+										port = 0;
+								}
 								if (sep2) sep2[0] = ':';
-								else if (sep3) sep3[0] = '/';
+								if (sep3) sep3[0] = '/';
 
 								if (port) sep = sep2;
 							}