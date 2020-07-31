593da62cbca3aaa9aa5eb9086f903227c1df8fa4
jeanlf <jeanlf@gpac.io>
2020-07-31 18:22:43 +0200

    fixed bug introduced by abcd00df

--- src/filter_core/filter.c
+++ src/filter_core/filter.c
@@ -1131,17 +1131,18 @@ static void filter_parse_dyn_args(GF_Filter *filter, const char *args, GF_Filter
 							if (sep2 || sep3 || sep) {
 								char szPort[20];
 								u32 port = 0;
-								if (sep2) sep2[0] = 0;
-								else if (sep3) sep3[0] = 0;
-								if (sscanf(sep+1, "%d", &port)!=1) {
-									port = 0;
+								if (sep2) {
+									sep2[0] = 0;
+									if (sep3) sep3[0] = 0;
 								}
-								snprintf(szPort, 20, "%d", port);
-								if (strcmp(sep+1, szPort))
-									port = 0;
-
+								else if (sep3) sep3[0] = 0;
+								if (sscanf(sep+1, "%d", &port)==1) {
+									snprintf(szPort, 20, "%d", port);
+									if (strcmp(sep+1, szPort))
+										port = 0;
+								}
 								if (sep2) sep2[0] = ':';
-								else if (sep3) sep3[0] = '/';
+								if (sep3) sep3[0] = '/';
 
 								if (port) sep = sep2;
 							}
