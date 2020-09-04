474eee2b07458c9a9c39eb79b999075eac393627
jeanlf <jeanlf@gpac.io>
2020-09-04 14:33:53 +0200

    fixed abusive delete of edit lists in negctts mode - cf #1405

--- src/isomedia/isom_write.c
+++ src/isomedia/isom_write.c
@@ -6456,7 +6456,24 @@ GF_Err gf_isom_set_ctts_v1(GF_ISOFile *file, u32 track, u32 ctts_shift)
 	}
 	if (!ctts->version) {
 		ctts->version = 1;
-		gf_isom_remove_edits(file, track);
+		//if we had edit lists, shift all media times by the given amount
+		if (trak->editBox && trak->editBox->editList) {
+			u32 i;
+			for (i=0; i<gf_list_count(trak->editBox->editList->entryList); i++) {
+				GF_EdtsEntry *ent = (GF_EdtsEntry*)gf_list_get(trak->editBox->editList->entryList, i);
+				//empty edit
+				if (ent->mediaRate<0) continue;
+				if (ent->mediaTime>=shift) ent->mediaTime -= shift;
+				else ent->mediaTime = 0;
+				//no offset and last entry, trash edit
+				if (!ent->mediaTime && (gf_list_count(trak->editBox->editList->entryList)==1)) {
+					gf_isom_box_del_parent(&trak->child_boxes, (GF_Box *)trak->editBox);
+					trak->editBox = NULL;
+					break;
+				}
+			}
+			SetTrackDuration(trak);
+		}
 	}
 
 	if (!trak->Media->information->sampleTable->CompositionToDecode) {
@@ -6513,11 +6530,24 @@ static GF_Err gf_isom_set_ctts_v0(GF_ISOFile *file, GF_TrackBox *trak)
 		gf_isom_box_del_parent(&trak->Media->information->sampleTable->child_boxes, (GF_Box *)cslg);
 		trak->Media->information->sampleTable->CompositionToDecode = NULL;
 	}
-	if (! trak->editBox && shift>0) {
-		u64 dur = trak->Media->mediaHeader->duration;
-		dur *= file->moov->mvhd->timeScale;
-		dur /= trak->Media->mediaHeader->timeScale;
-		gf_isom_set_edit(file, gf_list_find(file->moov->trackList, trak)+1, 0, dur, shift, GF_ISOM_EDIT_NORMAL);
+	if (shift>0) {
+		//no edits, insert one
+		if (! trak->editBox) {
+			u64 dur = trak->Media->mediaHeader->duration;
+			dur *= file->moov->mvhd->timeScale;
+			dur /= trak->Media->mediaHeader->timeScale;
+			gf_isom_set_edit(file, gf_list_find(file->moov->trackList, trak)+1, 0, dur, shift, GF_ISOM_EDIT_NORMAL);
+		} else {
+			u32 i;
+			//otherwise shift media times in all entries
+			for (i=0; i<gf_list_count(trak->editBox->editList->entryList); i++) {
+				GF_EdtsEntry *ent = (GF_EdtsEntry*)gf_list_get(trak->editBox->editList->entryList, i);
+				//empty edit
+				if (ent->mediaRate<0) continue;
+				ent->mediaTime += shift;
+			}
+			SetTrackDuration(trak);
+		}
 	}
 	ctts->version = 0;
 	gf_isom_modify_alternate_brand(file, GF_ISOM_BRAND_ISO4, GF_FALSE);
