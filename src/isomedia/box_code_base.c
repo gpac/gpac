27b2b975b7716fb74b75c8e201fd345c9186ee75
jeanlf <jeanlf@gpac.io>
2020-09-07 09:20:37 +0200

    fixed #1587

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -580,6 +580,10 @@ GF_Err url_box_read(GF_Box *s, GF_BitStream *bs)
 		ptr->location = (char*)gf_malloc((u32) ptr->size);
 		if (! ptr->location) return GF_OUT_OF_MEM;
 		gf_bs_read_data(bs, ptr->location, (u32)ptr->size);
+		if (ptr->location[ptr->size-1]) {
+			GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[iso file] url box location is not 0-terminated\n" ));
+			return GF_ISOM_INVALID_FILE;
+		}
 	}
 	return GF_OK;
 }
