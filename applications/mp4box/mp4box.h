c792f42164f540c035e79e8b01483790edfce441
jeanlf <jeanlf@gpac.io>
2020-10-13 18:00:38 +0200

    fixed end range adjustments in splitz - cf #224

--- applications/mp4box/mp4box.h
+++ applications/mp4box/mp4box.h
@@ -64,7 +64,7 @@ GF_Err import_file(GF_ISOFile *dest, char *inName, u32 import_flags, GF_Fraction
 	return GF_NOT_SUPPORTED;
 }
 #endif
-GF_Err split_isomedia_file(GF_ISOFile *mp4, Double split_dur, u64 split_size_kb, char *inName, Double interleaving_time, Double chunk_start, Bool adjust_split_end, char *outName, const char *tmpdir, Bool force_rap_split, const char *split_range_str);
+GF_Err split_isomedia_file(GF_ISOFile *mp4, Double split_dur, u64 split_size_kb, char *inName, Double interleaving_time, Double chunk_start, Bool adjust_split_end, char *outName, const char *tmpdir, Bool force_rap_split, const char *split_range_str, u32 fs_dump_flags);
 GF_Err cat_isomedia_file(GF_ISOFile *mp4, char *fileName, u32 import_flags, GF_Fraction force_fps, u32 frames_per_sample, char *tmp_dir, Bool force_cat, Bool align_timelines, Bool allow_add_in_command, Bool is_pl);
 
 #if !defined(GPAC_DISABLE_SCENE_ENCODER)
