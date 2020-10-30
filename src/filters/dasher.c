gpac/gpac

commit 833cea09e8c0dd094b69b6cb47c52aa4fb3f9545

fixed msvc build

src/filters/dasher.c
@@ -6061,7 +6061,7 @@ static GF_Err dasher_process(GF_Filter *filter)
 						GF_LOG(GF_LOG_ERROR, GF_LOG_DASH, ("[Dasher] Segments do not start with the same SAP types: set initialized with %d but first packet got %d - bitstream will not be compliant\n", set_start_with_sap, sap_type));
 					}
 					//TODO setup proper PTO, the code below will break sync by realigning first AU of each stream
-					if (cts > ds->pts_minus_cts) {
+					if ((s64) cts > ds->pts_minus_cts) {
 						u64 pto = cts - ds->pts_minus_cts;
 						if (ds->rep->segment_list)
 							ds->rep->segment_list->presentation_time_offset = pto;

include/gpac/dash.h
@@ -1016,14 +1016,14 @@ s32 gf_dash_group_get_as_id(GF_DashClient *dash, u32 group_idx);
 typedef s32 (*gf_dash_rate_adaptation)(void *udta, u32 group_idx, u32 base_group_idx,
 				u32 download_rate, u32 file_size, Double speed, Double max_available_speed,
 				u32 disp_width, u32 disp_height, Bool force_lower_complexity,
-				u32 active_quality_idx, u32 buffer_min_ms, u32 buffer_max_ms, u32 buffer_occupancy_ms
-								  );
+				u32 active_quality_idx, u32 buffer_min_ms, u32 buffer_max_ms, u32 buffer_occupancy_ms);
 
 /*! Callback function for custom rate monitor, not final yet
 \param udta user data
 \param group_idx index of group to adapt
+\return quality index (>=0) to switch to after abort, -1 to do nothing (no abort), -2 for internal algorithms having already setup the desired quality and requesting only abort
  */
-typedef GF_Err (*gf_dash_download_monitor)(void *udta, u32 group_idx, u32 bits_per_sec, u64 total_bytes, u64 bytes_done, u64 us_since_start, u32 buffer_dur_ms, u32 current_seg_dur);
+typedef s32 (*gf_dash_download_monitor)(void *udta, u32 group_idx, u32 bits_per_sec, u64 total_bytes, u64 bytes_done, u64 us_since_start, u32 buffer_dur_ms, u32 current_seg_dur);
 
 
 /*! sets custom rate adaptation logic

src/export.cpp
@@ -304,7 +304,6 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_url_is_local) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_url_get_absolute_path) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_url_concatenate) )
-#pragma comment (linker, EXPORT_SYMBOL(gf_url_get_resource_path) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_url_get_resource_name) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_url_to_fs_path) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_utc_time_since_1970) )

src/filters/dmx_dash.c
@@ -585,7 +585,7 @@ void dashdmx_io_manifest_updated(GF_DASHFileIO *dashio, const char *manifest_nam
 		char *man_pay_start = manifest_payload;
 		while (1) {
 			u32 end_len, offset;
-			manifest_payload_len = strlen(manifest_payload);
+			manifest_payload_len = (u32) strlen(manifest_payload);
 			char *base_url_start = strstr(man_pay_start, "<BaseURL>");
 			if (!base_url_start) break;
 			char *base_url_end = strstr(base_url_start, "</BaseURL>");
@@ -593,7 +593,7 @@ void dashdmx_io_manifest_updated(GF_DASHFileIO *dashio, const char *manifest_nam
 			offset = 10;
 			while (base_url_end[offset] == '\n')
 				offset++;
-			end_len = strlen(base_url_end+offset);
+			end_len = (u32) strlen(base_url_end+offset);
 			memmove(base_url_start, base_url_end+offset, end_len);
 			base_url_start[end_len]=0;
 			man_pay_start = base_url_start;
@@ -1420,7 +1420,7 @@ static s32 dashdmx_algo_js(void *udta, u32 group, u32 base_group,
 	return res;
 }
 
-GF_Err dashdmx_download_monitor_js(void *udta, u32 group, u32 bits_per_sec, u64 total_bytes, u64 bytes_done, u64 us_since_start, u32 buffer_dur_ms, u32 current_seg_dur)
+s32 dashdmx_download_monitor_js(void *udta, u32 group, u32 bits_per_sec, u64 total_bytes, u64 bytes_done, u64 us_since_start, u32 buffer_dur_ms, u32 current_seg_dur)
 {
 	s32 res = -1;
 	JSValue ret, args[2];

src/filters/mux_isom.c
@@ -3768,7 +3768,7 @@ static void mp4_mux_flush_frag(GF_MP4MuxCtx *ctx, Bool is_init, u64 idx_start_ra
 			gf_filter_pck_set_framing(ctx->dst_pck, s, GF_TRUE);
 			if (!is_init) {
 				u64 dur = ctx->next_seg_start - (ctx->min_cts_plus_one-1);
-				gf_filter_pck_set_duration(ctx->dst_pck, dur);
+				gf_filter_pck_set_duration(ctx->dst_pck, (u32) dur);
 			}
 			ctx->first_pck_sent = GF_FALSE;
 			ctx->current_offset = 0;
@@ -4672,8 +4672,8 @@ static GF_Err mp4_mux_process_fragmented(GF_Filter *filter, GF_MP4MuxCtx *ctx)
 		ref_start = (Double) next_ref_ts;
 		ref_start /= ctx->ref_tkw->src_timescale;
 
-		ctx->next_seg_start = ref_start * 1000;
-		ctx->min_cts_next_frag = ctx->next_frag_start * 1000;
+		ctx->next_seg_start = (u64) (ref_start * 1000);
+		ctx->min_cts_next_frag = (u64) (ctx->next_frag_start * 1000);
 
 		ctx->next_frag_start += ctx->cdur;
 		while (ctx->next_frag_start <= ctx->adjusted_next_frag_start) {
@@ -5216,7 +5216,7 @@ static GF_Err mp4_mux_on_data(void *cbk, u8 *data, u32 block_size)
 	if (ctx->min_cts_plus_one) {
 		u64 orig = ctx->min_cts_plus_one-1;
 		gf_filter_pck_set_cts(ctx->dst_pck, orig);
-		gf_filter_pck_set_duration(ctx->dst_pck, ctx->min_cts_next_frag - orig);
+		gf_filter_pck_set_duration(ctx->dst_pck, (u32) (ctx->min_cts_next_frag - orig) );
 	}
 
 	ctx->first_pck_sent = GF_TRUE;

src/filters/out_route.c
@@ -1107,7 +1107,7 @@ static GF_Err routeout_check_service_updates(GF_ROUTEOutCtx *ctx, ROUTEService *
 	//compress and store as final payload
 	if (serv->stsid_bundle) gf_free(serv->stsid_bundle);
 	serv->stsid_bundle = (u8 *) payload_text;
-	serv->stsid_bundle_size = 1+strlen(payload_text);
+	serv->stsid_bundle_size = 1 + (u32) strlen(payload_text);
 	gf_gz_compress_payload(&serv->stsid_bundle, serv->stsid_bundle_size, &serv->stsid_bundle_size);
 
 	serv->stsid_bundle_toi = 0x80000000; //compressed
@@ -1450,7 +1450,7 @@ retry:
 
 		//fragment start, store packed duration
 		if (has_ts) {
-			rpid->pck_dur_at_frame_start = pck_dur;
+			rpid->pck_dur_at_frame_start = (u32) pck_dur;
 		}
 		//compute estimated file size based on segment duration and rate, use 10% overhead
 		tot_est_size = rpid->bitrate;
@@ -1606,7 +1606,7 @@ next_packet:
 
 				//send child m3u8 asap
 				if (rpid->hld_child_pl) {
-					u32 hls_len = strlen(rpid->hld_child_pl);
+					u32 hls_len = (u32) strlen(rpid->hld_child_pl);
 					offset = 0;
 					GF_LOG(GF_LOG_DEBUG, GF_LOG_ROUTE, ("[ROUTE] Sending HLS sub playlist %s: \n%s\n", rpid->hld_child_pl_name, rpid->hld_child_pl));
 
@@ -1711,7 +1711,7 @@ next_packet:
 
 						if (seg_rate > rpid->bitrate) {
 							GF_LOG(GF_LOG_WARNING, GF_LOG_ROUTE, ("[ROUTE] Segment %s rate "LLU" but stream rate "LLU", updating bitrate\n", rpid->seg_name, seg_rate, rpid->bitrate));
-							rpid->bitrate = seg_rate;
+							rpid->bitrate = (u32) seg_rate;
 						}
 					}
 				}
@@ -1780,7 +1780,7 @@ static void routeout_send_lls(GF_ROUTEOutCtx *ctx)
 		gf_dynstrcat(&payload_text, "\"/>\n", NULL);
 
 		GF_LOG(GF_LOG_INFO, GF_LOG_ROUTE, ("[ROUTE] Updating ATSC3 LLS.SysTime:\n%s\n", payload_text));
-		len = strlen(payload_text);
+		len = (u32) strlen(payload_text);
 		comp_size = 2*len;
 		payload = gf_malloc(sizeof(char)*(comp_size+4));
 		pay_start = payload + 4;
@@ -1837,7 +1837,7 @@ static void routeout_send_lls(GF_ROUTEOutCtx *ctx)
 
 		GF_LOG(GF_LOG_INFO, GF_LOG_ROUTE, ("[ROUTE] Updating ATSC3 LLS.SLT:\n%s\n", payload_text));
 
-		len = strlen(payload_text);
+		len = (u32) strlen(payload_text);
 		comp_size = 2*len;
 		payload = gf_malloc(sizeof(char)*(comp_size+4));
 		pay_start = payload + 4;
@@ -1931,7 +1931,7 @@ static GF_Err routeout_process(GF_Filter *filter)
 		ctx->clock_stats = ctx->clock;
 	}
 	ctx->reschedule_us++;
-	gf_filter_ask_rt_reschedule(filter, ctx->reschedule_us);
+	gf_filter_ask_rt_reschedule(filter, (u32) ctx->reschedule_us);
 	return e;
 }
 

src/filters/out_rtp.c
@@ -953,16 +953,16 @@ GF_Err rtpout_process_rtp(GF_List *streams, GF_RTPOutStream **active_stream, Boo
 	dts += stream->rtp_ts_offset;
 	cts += stream->rtp_ts_offset;
 	if (stream->ts_delay>=0) {
-		dts += stream->ts_delay;
-		cts += stream->ts_delay;
+		dts += (u32) stream->ts_delay;
+		cts += (u32) stream->ts_delay;
 	} else {
 		if ((s32) dts >= -stream->ts_delay)
-			dts += stream->ts_delay;
+			dts += (s32) stream->ts_delay;
 		else
 			dts = 0;
 
 		if ((s32) cts >= -stream->ts_delay )
-			cts += stream->ts_delay;
+			cts += (s32) stream->ts_delay;
 		else
 			cts = 0;
 	}

src/media_tools/dash_client.c
@@ -739,7 +739,7 @@ setup_route:
 		}
 		if (mpd->type==GF_MPD_TYPE_STATIC) {
 			if (found)
-				group->dash->route_skip_segments_ms = timeline_offset_ms;
+				group->dash->route_skip_segments_ms = (u32) timeline_offset_ms;
 			group->timeline_setup = GF_TRUE;
 			return;
 		}
@@ -1245,7 +1245,7 @@ GF_Err gf_dash_group_check_bandwidth(GF_DashClient *dash, u32 group_idx, u32 bit
 	//force a call go query buffer
 	dash->dash_io->on_dash_event(dash->dash_io, GF_DASH_EVENT_CODEC_STAT_QUERY, group_idx, GF_OK);
 
-	res = dash->rate_adaptation_download_monitor(dash, group, bits_per_sec, total_bytes, bytes_done, us_since_start, group->buffer_occupancy_ms, group->current_downloaded_segment_duration);
+	res = dash->rate_adaptation_download_monitor(dash, group, bits_per_sec, total_bytes, bytes_done, us_since_start, group->buffer_occupancy_ms, (u32) group->current_downloaded_segment_duration);
 
 	if (res==-1) return GF_OK;
 
@@ -3012,7 +3012,7 @@ static void dash_store_stats(GF_DashClient *dash, GF_DASH_Group *group, u32 byte
 				time_sec /= bytes_per_sec;
 			}
 		} else {
-			time_sec = us_since_start;
+			time_sec = (Double) us_since_start;
 			time_sec /= 1000000;
 		}
 
@@ -3045,7 +3045,7 @@ static s32 dash_do_rate_monitor_default(GF_DashClient *dash, GF_DASH_Group *grou
 		return -1;
 	}
 
-	time_until_end = 8000*(total_bytes-bytes_done) / bits_per_sec;
+	time_until_end = (u32) (8000*(total_bytes-bytes_done) / bits_per_sec);
 
 	if (bits_per_sec<group->min_bitrate)
 		group->min_bitrate = bits_per_sec;
@@ -3073,7 +3073,7 @@ static s32 dash_do_rate_monitor_default(GF_DashClient *dash, GF_DASH_Group *grou
 	} else {
 		u32 target_rate;
 		//compute min bitrate needed to fetch the segment in another rep, with the time remaining
-		Double ratio = ((u32)current_seg_dur - us_since_start);
+		Double ratio = (Double) ((u32)current_seg_dur - us_since_start);
 		ratio /= (u32) current_seg_dur;
 
 		target_rate = (u32) (bits_per_sec * ratio);
@@ -8360,7 +8360,7 @@ char *gf_dash_group_get_template(GF_DashClient *dash, u32 idx)
 		if (ext) ext[0] = 0;
 		gf_dynstrcat(&solved_template, first_seg->media, NULL);
 		if (ext) ext[0] = '.';
-		len = strlen(solved_template);
+		len = (u32) strlen(solved_template);
 		last_num = last_non_num = 0;
 		for (i=len; i>0; i--) {
 			if (isdigit(solved_template[i-1])) {
@@ -8378,7 +8378,7 @@ char *gf_dash_group_get_template(GF_DashClient *dash, u32 idx)
 			solved_template[last_num] = 0;
 			num = atoi(solved_template+last_non_num+1);
 			snprintf(szVal, 100, "%u", num);
-			len = strlen(szVal);
+			len = (u32) strlen(szVal);
 			pad = 0;
 			if (len < last_num - last_non_num) {
 				pad = last_num - last_non_num - len;
@@ -8401,7 +8401,6 @@ GF_EXPORT
 GF_Err gf_dash_group_get_quality_info(GF_DashClient *dash, u32 idx, u32 quality_idx, GF_DASHQualityInfo *quality)
 {
 	GF_MPD_Fractional *sar;
-	u32 ast_offset, timescale;
 	GF_DASH_Group *group = gf_list_get(dash->groups, idx);
 	GF_MPD_Representation *rep;
 	if (!group || !quality) return GF_BAD_PARAM;
@@ -8439,22 +8438,15 @@ GF_Err gf_dash_group_get_quality_info(GF_DashClient *dash, u32 idx, u32 quality_
 	} else {
 		quality->is_selected = (quality_idx==group->active_rep_index) ? 1 : 0;
 	}
-	ast_offset = timescale = 0;
+
 	if (rep->segment_template) {
-		if (!ast_offset) ast_offset = rep->segment_template->availability_time_offset;
-		if (!timescale) timescale = rep->segment_template->timescale;
+		if (!quality->ast_offset) quality->ast_offset = rep->segment_template->availability_time_offset;
 	}
 	if (group->adaptation_set->segment_template) {
-		if (!ast_offset) ast_offset = group->adaptation_set->segment_template->availability_time_offset;
-		if (!timescale) timescale = group->adaptation_set->segment_template->timescale;
+		if (!quality->ast_offset) quality->ast_offset = group->adaptation_set->segment_template->availability_time_offset;
 	}
 	if (group->period->segment_template) {
-		if (!ast_offset) ast_offset = group->period->segment_template->availability_time_offset;
-		if (!timescale) timescale = group->period->segment_template->timescale;
-	}
-	if (timescale) {
-		quality->ast_offset = (Double) ast_offset;
-		quality->ast_offset /= timescale;
+		if (!quality->ast_offset) quality->ast_offset = group->period->segment_template->availability_time_offset;
 	}
 	return GF_OK;
 }

commit 93c5696a3de98a4e4aa561e2ca23019ca29e328b

fixed bug introduced by 833cea09

build/xcode/gpac.xcodeproj/project.pbxproj
@@ -2433,7 +2433,7 @@
 		9201F92918D5A359003D1ACA /* Project object */ = {
 			isa = PBXProject;
 			attributes = {
-				LastUpgradeCheck = 1020;
+				LastUpgradeCheck = 1210;
 				ORGANIZATIONNAME = "Telecom ParisTech";
 				TargetAttributes = {
 					927AACFC1E44EEDC008C8E69 = {
@@ -2992,6 +2992,7 @@
 				CLANG_WARN_NON_LITERAL_NULL_CONVERSION = YES;
 				CLANG_WARN_OBJC_IMPLICIT_RETAIN_SELF = YES;
 				CLANG_WARN_OBJC_LITERAL_CONVERSION = YES;
+				CLANG_WARN_QUOTED_INCLUDE_IN_FRAMEWORK_HEADER = YES;
 				CLANG_WARN_RANGE_LOOP_ANALYSIS = YES;
 				CLANG_WARN_STRICT_PROTOTYPES = NO;
 				CLANG_WARN_SUSPICIOUS_MOVE = YES;
@@ -3037,6 +3038,7 @@
 				CLANG_WARN_NON_LITERAL_NULL_CONVERSION = YES;
 				CLANG_WARN_OBJC_IMPLICIT_RETAIN_SELF = YES;
 				CLANG_WARN_OBJC_LITERAL_CONVERSION = YES;
+				CLANG_WARN_QUOTED_INCLUDE_IN_FRAMEWORK_HEADER = YES;
 				CLANG_WARN_RANGE_LOOP_ANALYSIS = YES;
 				CLANG_WARN_STRICT_PROTOTYPES = NO;
 				CLANG_WARN_SUSPICIOUS_MOVE = YES;
@@ -3122,10 +3124,11 @@
 				GCC_WARN_INHIBIT_ALL_WARNINGS = NO;
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				HEADER_SEARCH_PATHS = (
-					/opt/local/include,
 					../../include,
+					/opt/local/include,
 					/usr/local/include,
 					"/opt/local/include/openjpeg-1.5",
+					"/usr/local/include/openjpeg-1.5",
 				);
 				LIBRARY_SEARCH_PATHS = (
 					/usr/local/lib,
@@ -3226,10 +3229,11 @@
 				GCC_WARN_INHIBIT_ALL_WARNINGS = NO;
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				HEADER_SEARCH_PATHS = (
-					/opt/local/include,
 					../../include,
+					/opt/local/include,
 					/usr/local/include,
 					"/opt/local/include/openjpeg-1.5",
+					"/usr/local/include/openjpeg-1.5",
 				);
 				LIBRARY_SEARCH_PATHS = (
 					/usr/local/lib,
@@ -3379,8 +3383,8 @@
 				GCC_WARN_ABOUT_RETURN_TYPE = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				HEADER_SEARCH_PATHS = (
-					/usr/local/include/SDL2,
 					../../include,
+					/usr/local/include/SDL2,
 					/opt/local/include/SDL2,
 				);
 				INSTALL_PATH = "";
@@ -3417,8 +3421,8 @@
 				GCC_WARN_ABOUT_RETURN_TYPE = YES;
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				HEADER_SEARCH_PATHS = (
-					/usr/local/include/SDL2,
 					../../include,
+					/usr/local/include/SDL2,
 					/opt/local/include/SDL2,
 				);
 				INSTALL_PATH = "";
@@ -3457,12 +3461,15 @@
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				HEADER_SEARCH_PATHS = (
 					../../include,
-					/usr/include/freetype2,
 					/opt/local/include/freetype2,
+					/usr/local/include/freetype2,
 				);
 				INSTALL_PATH = "";
-				LIBRARY_SEARCH_PATHS = /opt/local/lib;
-				MACOSX_DEPLOYMENT_TARGET = 10.6;
+				LIBRARY_SEARCH_PATHS = (
+					/opt/local/lib,
+					/usr/local/lib,
+				);
+				MACOSX_DEPLOYMENT_TARGET = 10.15;
 				ONLY_ACTIVE_ARCH = YES;
 				OTHER_LDFLAGS = "-lfreetype";
 				PRODUCT_NAME = "$(TARGET_NAME)";
@@ -3488,12 +3495,15 @@
 				GCC_WARN_UNUSED_VARIABLE = YES;
 				HEADER_SEARCH_PATHS = (
 					../../include,
-					/usr/include/freetype2,
 					/opt/local/include/freetype2,
+					/usr/local/include/freetype2,
 				);
 				INSTALL_PATH = "";
-				LIBRARY_SEARCH_PATHS = /opt/local/lib;
-				MACOSX_DEPLOYMENT_TARGET = 10.6;
+				LIBRARY_SEARCH_PATHS = (
+					/opt/local/lib,
+					/usr/local/lib,
+				);
+				MACOSX_DEPLOYMENT_TARGET = 10.15;
 				OTHER_LDFLAGS = "-lfreetype";
 				PRODUCT_NAME = "$(TARGET_NAME)";
 				SDKROOT = macosx;
@@ -3505,6 +3515,7 @@
 			buildSettings = {
 				ALWAYS_SEARCH_USER_PATHS = NO;
 				CLANG_ENABLE_OBJC_WEAK = YES;
+				CODE_SIGN_IDENTITY = "-";
 				COPY_PHASE_STRIP = NO;
 				GCC_C_LANGUAGE_STANDARD = gnu99;
 				GCC_DYNAMIC_NO_PIC = NO;
@@ -3536,6 +3547,7 @@
 			buildSettings = {
 				ALWAYS_SEARCH_USER_PATHS = NO;
 				CLANG_ENABLE_OBJC_WEAK = YES;
+				CODE_SIGN_IDENTITY = "-";
 				COPY_PHASE_STRIP = YES;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
 				GCC_C_LANGUAGE_STANDARD = gnu99;
@@ -3560,6 +3572,7 @@
 			buildSettings = {
 				ALWAYS_SEARCH_USER_PATHS = NO;
 				CLANG_ENABLE_OBJC_WEAK = YES;
+				CODE_SIGN_IDENTITY = "-";
 				COPY_PHASE_STRIP = NO;
 				GCC_C_LANGUAGE_STANDARD = gnu99;
 				GCC_DYNAMIC_NO_PIC = NO;
@@ -3590,6 +3603,7 @@
 			buildSettings = {
 				ALWAYS_SEARCH_USER_PATHS = NO;
 				CLANG_ENABLE_OBJC_WEAK = YES;
+				CODE_SIGN_IDENTITY = "-";
 				COPY_PHASE_STRIP = YES;
 				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
 				GCC_C_LANGUAGE_STANDARD = gnu99;
@@ -3644,11 +3658,17 @@
 				GCC_WARN_ABOUT_MISSING_PROTOTYPES = NO;
 				GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR;
 				GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
+				HEADER_SEARCH_PATHS = (
+					../../include,
+					/opt/local/include,
+					/usr/local/include,
+					../../extra_lib/include,
+				);
 				LIBRARY_SEARCH_PATHS = (
 					/usr/local/lib,
 					/opt/local/lib,
 				);
-				MACOSX_DEPLOYMENT_TARGET = 10.13;
+				MACOSX_DEPLOYMENT_TARGET = 10.15;
 				MTL_ENABLE_DEBUG_INFO = INCLUDE_SOURCE;
 				MTL_FAST_MATH = YES;
 				OTHER_LDFLAGS = "-lopenhevc";
@@ -3692,11 +3712,17 @@
 				GCC_WARN_ABOUT_MISSING_PROTOTYPES = NO;
 				GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR;
 				GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
+				HEADER_SEARCH_PATHS = (
+					../../include,
+					/opt/local/include,
+					/usr/local/include,
+					../../extra_lib/include,
+				);
 				LIBRARY_SEARCH_PATHS = (
 					/usr/local/lib,
 					/opt/local/lib,
 				);
-				MACOSX_DEPLOYMENT_TARGET = 10.13;
+				MACOSX_DEPLOYMENT_TARGET = 10.15;
 				MTL_ENABLE_DEBUG_INFO = NO;
 				MTL_FAST_MATH = YES;
 				OTHER_LDFLAGS = "-lopenhevc";

src/filters/dasher.c
@@ -6061,7 +6061,7 @@ static GF_Err dasher_process(GF_Filter *filter)
 						GF_LOG(GF_LOG_ERROR, GF_LOG_DASH, ("[Dasher] Segments do not start with the same SAP types: set initialized with %d but first packet got %d - bitstream will not be compliant\n", set_start_with_sap, sap_type));
 					}
 					//TODO setup proper PTO, the code below will break sync by realigning first AU of each stream
-					if ((s64) cts > ds->pts_minus_cts) {
+					if ((s64) cts + ds->pts_minus_cts > 0) {
 						u64 pto = cts - ds->pts_minus_cts;
 						if (ds->rep->segment_list)
 							ds->rep->segment_list->presentation_time_offset = pto;