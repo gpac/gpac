abd2f10cd1f2dd93ba4a3a73b4d3b204cbe48752
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2020-11-01 18:01:14 +0100

    fix crash when importing ts to mp4: GF_MPEGSampleEntryBox is not inherited from GF_MPEGSampleEntryBox anymore so use Media_GetESD()

--- src/isomedia/track.c
+++ src/isomedia/track.c
@@ -1464,7 +1464,7 @@ GF_Err Track_SetStreamDescriptor(GF_TrackBox *trak, u32 StreamDescriptionIndex, 
 		esd->langDesc = NULL;
 	}
 
-	//we have a streamDescritpionIndex, use it
+	//we have a streamDescriptionIndex, use it
 	if (StreamDescriptionIndex) {
 		u32 entry_type;
 		entry = (GF_MPEGSampleEntryBox*)gf_list_get(trak->Media->information->sampleTable->SampleDescription->child_boxes, StreamDescriptionIndex - 1);
@@ -1539,9 +1539,12 @@ GF_Err Track_SetStreamDescriptor(GF_TrackBox *trak, u32 StreamDescriptionIndex, 
 		//need to check we're not in URL mode where only ONE description is allowed...
 		StreamDescriptionIndex = gf_list_count(trak->Media->information->sampleTable->SampleDescription->child_boxes);
 		if (StreamDescriptionIndex) {
+			GF_ESD *esd = NULL;
 			entry = (GF_MPEGSampleEntryBox*)gf_list_get(trak->Media->information->sampleTable->SampleDescription->child_boxes, StreamDescriptionIndex - 1);
 			if (!entry) return GF_ISOM_INVALID_FILE;
-			if (entry->esd && entry->esd->desc->URLString) return GF_BAD_PARAM;
+			e = Media_GetESD(trak->Media, StreamDescriptionIndex, &esd, 0);
+			if (e) return e;
+			if (esd->URLString) return GF_BAD_PARAM;
 		}
 
 		//OK, check the handler and create the entry
