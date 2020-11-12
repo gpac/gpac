gpac/gpac

commit 37b8e4e2fb985a49f0178e5d23b7c37234a6ba6a

added ABI number check in python and doc update

src/filters/mux_ts.c
@@ -1064,8 +1064,12 @@ static GF_Err tsmux_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_
 		ts_stream = prog->streams;
 		while (ts_stream) {
 			M2Pid *atspid = ts_stream->ifce->input_udta;
-			s64 media_skip = -atspid->media_delay;
-			if (media_skip  > max_media_skip) {
+			s64 media_skip;
+			if (atspid->media_delay>=0)
+				continue;
+
+			media_skip = -atspid->media_delay;
+			if (!max_media_skip || (media_skip * max_skip_ts > max_media_skip * atspid->esi.timescale) ) {
 				max_media_skip = media_skip ;
 				max_skip_ts = atspid->esi.timescale;
 			}
@@ -1076,7 +1080,7 @@ static GF_Err tsmux_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_
 		while (ts_stream) {
 			M2Pid *atspid = ts_stream->ifce->input_udta;
 			if (max_skip_ts) {
-				atspid->max_media_skip = (s32) (max_media_skip * atspid->esi.timescale / max_skip_ts);
+				atspid->max_media_skip = (max_media_skip * atspid->esi.timescale / max_skip_ts);
 			} else {
 				atspid->max_media_skip = 0;
 			}

include/gpac/dash.h
@@ -150,6 +150,7 @@ struct _gf_dash_io
 	/*! get the total size on bytes for the session*/
 	u32 (*get_bytes_done)(GF_DASHFileIO *dashio, GF_DASHFileIOSession session);
 
+	/*! callback when manifest (DASH, HLS) or sub-playlist (HLS) is updated*/
 	void (*manifest_updated)(GF_DASHFileIO *dashio, const char *manifest_name, const char *local_path, s32 group_idx);
 
 };
@@ -795,7 +796,7 @@ GF_Err gf_dash_group_get_quality_info(GF_DashClient *dash, u32 group_idx, u32 qu
 \param group_idx the 0-based index of the target group
 \return segment template, NULL if no templates used. Memory must be freed by caller
 */
-char *gf_dash_group_get_template(GF_DashClient *dash, u32 idx);
+char *gf_dash_group_get_template(GF_DashClient *dash, u32 group_idx);
 
 /*! checks automatic switching mode
 \param dash the target dash client

include/gpac/media_tools.h
@@ -120,7 +120,7 @@ In this case, any other flags are set to prefered values (typically, flag=0 or v
 \param color_matrix olor matrix
 \return error if any
 */
-GF_Err gf_media_change_color(GF_ISOFile *file, u32 track, s32 fullrange, s32 video_format, s32 color_primaries, s32 transfer, s32 color_matrix);
+GF_Err gf_media_change_color(GF_ISOFile *isom_file, u32 trackNumber, s32 fullrange, s32 video_format, s32 color_primaries, s32 transfer, s32 color_matrix);
 
 /*!
  *Removes all non rap samples (sync and other RAP sample group info) from the track.

include/gpac/tools.h
@@ -876,6 +876,7 @@ typedef struct
 \param blob_url URL of blob object (ie gmem://%p)
 \param out_data if success, set to blob data pointer
 \param out_size if success, set to blob data size
+\param blob_flags if success, set to blob flags - may be NULL
 \return error code
  */
 GF_Err gf_blob_get(const char *blob_url, u8 **out_data, u32 *out_size, u32 *blob_flags);

include/gpac/version.h
@@ -43,10 +43,12 @@
  */
 /*! Macro giving GPAC version name expressed as a printable string*/
 #define GPAC_VERSION          "1.1.0-DEV"
+
+// WARNING: when bumping, reflect the changes in share/python/libgpac.py !!
 /*! ABI Major number of libgpac */
 #define GPAC_VERSION_MAJOR 10
 /*! ABI Minor number of libgpac */
-#define GPAC_VERSION_MINOR 3
+#define GPAC_VERSION_MINOR 4
 /*! ABI Micro number of libgpac */
 #define GPAC_VERSION_MICRO 0
 
@@ -64,6 +66,18 @@ const char *gf_gpac_copyright();
 \return GPAC copyright
 */
 const char *gf_gpac_copyright_cite();
+
+/*!gets libgpac ABI major version
+\return major number of libgpac ABI version*/
+u32 gf_gpac_abi_major();
+
+/*!gets libgpac ABI minor version
+\return minor number of libgpac ABI version*/
+u32 gf_gpac_abi_minor();
+
+/*!gets libgpac ABI major version
+\return micro number of libgpac ABI version*/
+u32 gf_gpac_abi_micro();
 
 /*! @} */
 

src/export.cpp
@@ -41,6 +41,9 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_gpac_copyright) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_gpac_copyright_cite) )
 
+#pragma comment (linker, EXPORT_SYMBOL(gf_gpac_abi_major) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_gpac_abi_minor) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_gpac_abi_micro) )
 
 #pragma comment (linker, EXPORT_SYMBOL(gf_sys_init) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_sys_close) )

src/filters/in_route.c
@@ -912,9 +912,9 @@ GF_FilterRegister ROUTEInRegister = {
 	"\n"
 	"# Interface setup\n"
 	"On some systems (OSX), when using VM packet replay, you may need to force multicast routing on your local interface.\n"
-	"You will have to do this for the base ROUTE multicast (224.0.23.60):\n"
+	"For ATSC, you will have to do this for the base signaling multicast (224.0.23.60):\n"
 	"EX route add -net 224.0.23.60/32 -interface vboxnet0\n"
-	"and on each service multicast:\n"
+	"Then for each ROUTE service in the multicast:\n"
 	"EX route add -net 239.255.1.4/32 -interface vboxnet0\n"
 	"",
 #endif //GPAC_DISABLE_DOC

commit cde7dca4875d285b4b62afd9d63384144ae1a518

fixed bug introduced by 37b8e4e2

src/filters/mux_ts.c
@@ -1065,8 +1065,10 @@ static GF_Err tsmux_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_
 		while (ts_stream) {
 			M2Pid *atspid = ts_stream->ifce->input_udta;
 			s64 media_skip;
-			if (atspid->media_delay>=0)
+			if (atspid->media_delay>=0) {
+				ts_stream = ts_stream->next;
 				continue;
+			}
 
 			media_skip = -atspid->media_delay;
 			if (!max_media_skip || (media_skip * max_skip_ts > max_media_skip * atspid->esi.timescale) ) {