gpac/gpac

commit cfba2d1c3c7a9f3086571b3ebdeb7423b46f0ff6

fixes in 360 independent tiling and http abort in dash

src/filters/compose.c
@@ -882,8 +882,11 @@ static GF_FilterArgs CompositorArgs[] =
 	{ OFFS(rview), "reverse view order", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_UPDATE|GF_FS_ARG_HINT_EXPERT},
 
 	{ OFFS(tvtn), "number of point sampling for tile visibility algo", GF_PROP_UINT, "30", NULL, GF_FS_ARG_UPDATE|GF_FS_ARG_HINT_EXPERT},
-	{ OFFS(tvtt), "number of points above which the tile is considered visible", GF_PROP_UINT, "0", NULL, GF_FS_ARG_UPDATE|GF_FS_ARG_HINT_EXPERT},
-	{ OFFS(tvtd), "disable the tile having full coverage of the SRD, only displaying partial tiles", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_UPDATE|GF_FS_ARG_HINT_EXPERT},
+	{ OFFS(tvtt), "number of points above which the tile is considered visible", GF_PROP_UINT, "8", NULL, GF_FS_ARG_UPDATE|GF_FS_ARG_HINT_EXPERT},
+	{ OFFS(tvtd), "debug tiles and full coverage SRD\n"
+		"- off: regular draw\n"
+		"- partial: only displaying partial tiles, not the full sphere video\n"
+		"- full: only display the full sphere video", GF_PROP_UINT, "off", "off|partial|full", GF_FS_ARG_UPDATE|GF_FS_ARG_HINT_EXPERT},
 	{ OFFS(tvtf), "force all tiles to be considered visible, regardless of viewpoint", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_UPDATE|GF_FS_ARG_HINT_EXPERT},
 	{ OFFS(fov), "default field of view for VR", GF_PROP_FLOAT, "1.570796326794897", NULL, GF_FS_ARG_UPDATE},
 	{ OFFS(vertshader), "path to vertex shader file", GF_PROP_STRING, NULL, NULL, GF_FS_ARG_HINT_EXPERT },

src/filters/dmx_dash.c
@@ -2366,6 +2366,9 @@ GF_Err dashdmx_process(GF_Filter *filter)
 			}
 			has_pck = GF_TRUE;
 			check_eos = GF_FALSE;
+			if (gf_filter_pid_would_block(opid))
+				break;
+
 			dashdmx_forward_packet(ctx, pck, ipid, opid, group);
 			group->wait_for_pck = GF_FALSE;
 			dashdmx_update_group_stats(ctx, group);

src/filters/ff_dec.c
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2017-2018
+ *			Copyright (c) Telecom ParisTech 2017-2020
  *					All rights reserved
  *
  *  This file is part of GPAC / ffmpeg decode filter
@@ -209,6 +209,7 @@ static GF_Err ffdec_process_video(GF_Filter *filter, struct _gf_ffdec_ctx *ctx)
 
 		//seems ffmpeg is not properly handling the decoding after a flush, we close and reopen the codec
 		if (ctx->flush_done) {
+#if 0
 			AVDictionary *options = NULL;
 			const AVCodec *codec = ctx->decoder->codec;
 			avcodec_close(ctx->decoder);
@@ -216,6 +217,9 @@ static GF_Err ffdec_process_video(GF_Filter *filter, struct _gf_ffdec_ctx *ctx)
 			av_dict_copy(&options, ctx->options, 0);
 			avcodec_open2(ctx->decoder, codec, &options );
 			if (options) av_dict_free(&options);
+#else
+			avcodec_flush_buffers(ctx->decoder);
+#endif
 			ctx->flush_done = GF_FALSE;
 		}
 
@@ -938,16 +942,17 @@ static Bool ffdec_process_event(GF_Filter *filter, const GF_FilterEvent *evt)
 
 	if ((evt->base.type==GF_FEVT_PLAY) || (evt->base.type==GF_FEVT_SET_SPEED) || (evt->base.type==GF_FEVT_RESUME)) {
 		ctx->drop_non_refs = evt->play.drop_non_ref;
-		//play request, detach all pending source packets and trigger a reconfig to start from a clean state
-		if (evt->base.type==GF_FEVT_PLAY) {
-			while (gf_list_count(ctx->src_packets)) {
-				GF_FilterPacket *pck = gf_list_pop_back(ctx->src_packets);
-				gf_filter_pck_unref(pck);
-				//reconfig will only be set if we had a source packet pending
-				ctx->reconfig_pending = GF_TRUE;
-			}
+	}
+	//play request, detach all pending source packets and trigger a reconfig to start from a clean state
+	else if (evt->base.type==GF_FEVT_STOP) {
+		while (gf_list_count(ctx->src_packets)) {
+			GF_FilterPacket *pck = gf_list_pop_back(ctx->src_packets);
+			gf_filter_pck_unref(pck);
+			//for video, this will reset the decoder
+			ctx->flush_done = GF_TRUE;
 		}
 	}
+
 	return GF_FALSE;
 }
 

src/filters/ff_enc.c
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2018
+ *			Copyright (c) Telecom ParisTech 2018-2020
  *					All rights reserved
  *
  *  This file is part of GPAC / ffmpeg encode filter
@@ -1342,6 +1342,8 @@ static GF_Err ffenc_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_
 	if (ctx->all_intra) ctx->encoder->gop_size = 0;
 	else if (ctx->gop_size) ctx->encoder->gop_size = ctx->gop_size;
 
+	//by default let libavcodec decide - if single thread is required, let the user define -threads option
+	ctx->encoder->thread_count = 0;
 	res = avcodec_open2(ctx->encoder, codec, &ctx->options );
 	if (res < 0) {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CODEC, ("[FFEnc] PID %s failed to open codec context: %s\n", gf_filter_pid_get_name(pid), av_err2str(res) ));

include/gpac/internal/compositor_dev.h
@@ -150,6 +150,12 @@ typedef struct
 
 #define DOUBLECLICK_TIME_MS		250
 
+enum
+{
+	TILE_DEBUG_NONE=0,
+	TILE_DEBUG_PARTIAL,
+	TILE_DEBUG_FULL
+};
 
 enum
 {
@@ -646,8 +652,8 @@ struct __tag_compositor
 	u8 *screen_buffer, *line_buffer;
 	u32 screen_buffer_alloc_size;
 
-	u32 tvtn, tvtt;
-	Bool tvtd, tvtf;
+	u32 tvtn, tvtt, tvtd;
+	Bool tvtf;
 	u32 vrhud_mode;
 	Fixed fov;
 
@@ -2037,6 +2043,8 @@ enum
 
 	/*flag indicates the odm is a target passthrough*/
 	GF_ODM_PASSTHROUGH = (1<<15),
+	/*flag indicates the clock is shared between tiles and a play should not trigger a rebuffer*/
+	GF_ODM_TILED_SHARED_CLOCK = (1<<16),
 };
 
 enum
@@ -2133,7 +2141,8 @@ struct _od_manager
 	u32 timeshift_depth;
 
 	u32 action_type;
-	s64 delay;
+	//delay in PID timescale
+	s64 timestamp_offset;
 	
 	Fixed set_speed;
 	Bool disable_buffer_at_next_play;

share/doc/man/gpac-filters.1
@@ -6802,7 +6802,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev221-gacd879151-master
+build: 1.1.0-DEV-rev223-g210f598c9-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -3879,7 +3879,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev221-gacd879151-master
+build: 1.1.0-DEV-rev223-g210f598c9-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2393,7 +2393,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev221-gacd879151-master
+build: 1.1.0-DEV-rev223-g210f598c9-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

commit e93f253b54bd09e0cb3764c99f826e4f8f291110

fixed bugs introduced by cfba2d1c

share/python/libgpac.py
@@ -1716,7 +1716,9 @@ class DASHSegURL(Structure):
 
 
 _libgpac.gf_dash_group_get_num_qualities.argtypes = [c_void_p, c_uint]
+_libgpac.gf_dash_get_period_duration.argtypes = [c_void_p]
 _libgpac.gf_dash_group_get_quality_info.argtypes = [c_void_p, c_uint, c_uint, POINTER(DASHQualityInfoNat) ]
+
 
 _libgpac.gf_list_count.argtypes = [c_void_p]
 _libgpac.gf_list_count.restype = c_uint
@@ -1788,7 +1790,7 @@ class DASHGroup:
         self.idx = groupidx
         ## List of DASHQualityInfo for group
         self.qualities = []
-        ## period duration, 0 if unknwon
+        ## period duration in milliseconds, 0 if unknwon
         self.duration = _libgpac.gf_dash_get_period_duration(ptr_dash)
         ## \cond priv
         self._dash = ptr_dash

src/filters/compose.c
@@ -987,7 +987,7 @@ const GF_FilterRegister CompositorFilterRegister = {
 	"- views:// : creates an auto-stereo scene of N views from `views://v1:.:vN`. vN can be any type of URL supported by GPAC.\n"
 	"- mosaic:// : creates a mosaic of N views from `mosaic://v1:.:vN`. vN can be any type of URL supported by GPAC.\n"
 	"\n"
-	"The compositor can act as a source filter when the [-src]() option is explicitly set:\n"
+	"The compositor can act as a source filter when the [-src]() option is explicitly set, independently from the operating mode:\n"
 	"EX gpac compositor:src=source.mp4 vout\n"
 	"\n"
 	"The compositor can act as a source filter when the source url uses one of the compositor buildin protocol schemes:\n"

src/filters/dmx_dash.c
@@ -2366,9 +2366,6 @@ GF_Err dashdmx_process(GF_Filter *filter)
 			}
 			has_pck = GF_TRUE;
 			check_eos = GF_FALSE;
-			if (gf_filter_pid_would_block(opid))
-				break;
-
 			dashdmx_forward_packet(ctx, pck, ipid, opid, group);
 			group->wait_for_pck = GF_FALSE;
 			dashdmx_update_group_stats(ctx, group);

src/filters/ff_dec.c
@@ -794,7 +794,9 @@ static GF_Err ffdec_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_
 	}
 
 	//by default let libavcodec decide - if single thread is required, let the user define -threads option
-	ctx->decoder->thread_count=0;
+	if (codec->capabilities & (AV_CODEC_CAP_AUTO_THREADS|AV_CODEC_CAP_FRAME_THREADS|AV_CODEC_CAP_SLICE_THREADS))
+		ctx->decoder->thread_count = 0;
+
 	//clone options (in case we need to destroy/recreate the codec) and open codec
 	av_dict_copy(&options, ctx->options, 0);
 	res = avcodec_open2(ctx->decoder, codec, &options);

src/filters/ff_enc.c
@@ -1343,7 +1343,8 @@ static GF_Err ffenc_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_
 	else if (ctx->gop_size) ctx->encoder->gop_size = ctx->gop_size;
 
 	//by default let libavcodec decide - if single thread is required, let the user define -threads option
-	ctx->encoder->thread_count = 0;
+	if (codec->capabilities & (AV_CODEC_CAP_AUTO_THREADS|AV_CODEC_CAP_FRAME_THREADS|AV_CODEC_CAP_SLICE_THREADS))
+		ctx->encoder->thread_count = 0;
 	res = avcodec_open2(ctx->encoder, codec, &ctx->options );
 	if (res < 0) {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CODEC, ("[FFEnc] PID %s failed to open codec context: %s\n", gf_filter_pid_get_name(pid), av_err2str(res) ));