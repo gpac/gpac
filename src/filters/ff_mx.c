gpac/gpac

commit 5bc5dab1ba1dcfb08427d03833904abd8d1c873b

more asan fixes

src/filters/ff_mx.c
@@ -84,7 +84,6 @@ typedef struct
 	AVIOContext *avio_ctx;
 	FILE *gfio;
 
-
 	u32 cur_file_idx_plus_one;
 } GF_FFMuxCtx;
 
@@ -899,6 +898,9 @@ static void ffmx_finalize(GF_Filter *filter)
 			av_write_trailer(ctx->muxer);
 		}
 		ctx->status = FFMX_STATE_TRAILER_DONE;
+	} 
+	if (ctx->muxer->pb) {
+		ctx->muxer->io_close(ctx->muxer, ctx->muxer->pb);
 	}
 
 	if (ctx->options) av_dict_free(&ctx->options);

src/compositor/hardcoded_protos.c
@@ -1210,7 +1210,7 @@ static void TraverseCustomTexture(GF_Node *node, void *rs, Bool is_destroy)
 static void CustomTexture_update(GF_TextureHandler *txh)
 {
 #ifndef GPAC_DISABLE_3D
-    u8 data[12];
+    u8 data[16];
 #endif
     CustomTextureStack *stack = gf_node_get_private(txh->owner);
     //alloc texture

src/filters/dasher.c
@@ -1190,7 +1190,8 @@ GF_Err rfc6381_codec_name_default(char *szCodec, u32 subtype, u32 codec_id);
 
 static GF_Err dasher_get_rfc_6381_codec_name(GF_DasherCtx *ctx, GF_DashStream *ds, char *szCodec, Bool force_inband, Bool force_sbr)
 {
-	u32 subtype=0, subtype_src=0, mha_pl=0;
+	u32 subtype=0, subtype_src=0;
+	s32 mha_pl=-1;
 	const GF_PropertyValue *dcd, *dcd_enh, *dovi, *codec;
 
 	dcd = gf_filter_pid_get_property(ds->ipid, GF_PROP_PID_ISOM_SUBTYPE);
@@ -1370,14 +1371,14 @@ static GF_Err dasher_get_rfc_6381_codec_name(GF_DasherCtx *ctx, GF_DashStream *d
 		subtype = subtype_src ? subtype_src : GF_ISOM_SUBTYPE_MH3D_MHM1;
 		if (!dcd) {
 			const GF_PropertyValue *pl = gf_filter_pid_get_property(ds->ipid, GF_PROP_PID_PROFILE_LEVEL);
-			if (pl) mha_pl = pl->value.uint;
+			if (pl) mha_pl = (s32) pl->value.uint;
 		}
 		//fallthrough
 	case GF_CODECID_MPHA:
 		if (!subtype)
 			subtype = subtype_src ? subtype_src : GF_ISOM_SUBTYPE_MH3D_MHA1;
 
-		return rfc_6381_get_codec_mpegha(szCodec, subtype, dcd ? dcd->value.data.ptr : NULL, dcd ? dcd->value.data.size : 0, -1);
+		return rfc_6381_get_codec_mpegha(szCodec, subtype, dcd ? dcd->value.data.ptr : NULL, dcd ? dcd->value.data.size : 0, mha_pl);
 
 	case GF_CODECID_VVC:
 		if (!subtype) {

src/filters/ff_enc.c
@@ -450,11 +450,13 @@ static GF_Err ffenc_process_video(GF_Filter *filter, struct _gf_ffenc_ctx *ctx)
 	ctx->time_spent += now;
 
 	if (res<0) {
+		av_packet_free_side_data(&pkt);
 		ctx->nb_frames_out++;
 		return GF_SERVICE_ERROR;
 	}
 
 	if (!gotpck) {
+		av_packet_free_side_data(&pkt);
 		return GF_OK;
 	}
 
@@ -588,6 +590,8 @@ static GF_Err ffenc_process_video(GF_Filter *filter, struct _gf_ffenc_ctx *ctx)
 	}
 #endif
 	gf_filter_pck_send(dst_pck);
+
+	av_packet_free_side_data(&pkt);
 
 	//we're in final flush, request a process task until all frames flushe
 	//we could recursiveley call ourselves, same result
@@ -811,9 +815,11 @@ static GF_Err ffenc_process_audio(GF_Filter *filter, struct _gf_ffenc_ctx *ctx)
 
 	if (res<0) {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CODEC, ("[FFEnc] Error encoding frame: %s\n", av_err2str(res) ));
+		av_packet_free_side_data(&pkt);
 		return GF_SERVICE_ERROR;
 	}
 	if (!gotpck) {
+		av_packet_free_side_data(&pkt);
 		return GF_OK;
 	}
 	dst_pck = gf_filter_pck_new_alloc(ctx->out_pid, pkt.size, &output);
@@ -869,6 +875,7 @@ static GF_Err ffenc_process_audio(GF_Filter *filter, struct _gf_ffenc_ctx *ctx)
 
 	gf_filter_pck_send(dst_pck);
 
+	av_packet_free_side_data(&pkt);
 	//we're in final flush, request a process task until all frames flushe
 	//we could recursiveley call ourselves, same result
 	if (!pck) {

src/scenegraph/svg_attributes.c
@@ -5714,9 +5714,11 @@ static GF_Err svg_dasharray_copy(SVG_StrokeDashArray *a, SVG_StrokeDashArray *b)
 	a->type = b->type;
 	a->array.count = b->array.count;
 	a->array.units = (u8*)gf_malloc(sizeof(u8)*a->array.count);
-	memcpy(a->array.units, b->array.units, sizeof(u8)*a->array.count);
+	if (a->array.count)
+		memcpy(a->array.units, b->array.units, sizeof(u8)*a->array.count);
 	a->array.vals = (Fixed*)gf_malloc(sizeof(Fixed)*a->array.count);
-	memcpy(a->array.vals, b->array.vals, sizeof(Fixed)*a->array.count);
+	if (a->array.count)
+		memcpy(a->array.vals, b->array.vals, sizeof(Fixed)*a->array.count);
 	return GF_OK;
 }
 

commit 2ee934bd4aa110ebe8d96827decd841f40457b5a

fixed crash introduced by 5bc5dab1

src/filters/ff_mx.c
@@ -899,7 +899,7 @@ static void ffmx_finalize(GF_Filter *filter)
 		}
 		ctx->status = FFMX_STATE_TRAILER_DONE;
 	} 
-	if (ctx->muxer->pb) {
+	if (!ctx->gfio && ctx->muxer->pb) {
 		ctx->muxer->io_close(ctx->muxer, ctx->muxer->pb);
 	}
 