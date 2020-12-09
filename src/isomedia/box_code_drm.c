gpac/gpac

commit 5ed9799ecd0a886aac861b915625ebd1dc33fb1c

added support for CENC in HEIF

src/isomedia/box_code_drm.c
@@ -1366,6 +1366,7 @@ GF_Err senc_Parse(GF_BitStream *bs, GF_TrackBox *trak, void *traf, GF_SampleEncr
 			}
 		} else {
 			i--;
+			sai->isNotProtected = 1;
 		}
 		gf_list_add(senc->samp_aux_info, sai);
 	}
@@ -1421,6 +1422,8 @@ GF_Err senc_box_write(GF_Box *s, GF_BitStream *bs)
 
 	for (i = 0; i < sample_count; i++) {
 		GF_CENCSampleAuxInfo *sai = (GF_CENCSampleAuxInfo *)gf_list_get(ptr->samp_aux_info, i);
+		if (sai->isNotProtected)
+			continue;
 
 		//for cbcs scheme, IV_size is 0, constant IV shall be used. It is written in tenc box rather than in sai
 		if (sai->IV_size)
@@ -1455,6 +1458,8 @@ GF_Err senc_box_size(GF_Box *s)
 	ptr->size += 4; //sample count
 	for (i = 0; i < sample_count; i++) {
 		GF_CENCSampleAuxInfo *sai = (GF_CENCSampleAuxInfo *)gf_list_get(ptr->samp_aux_info, i);
+		if (sai->isNotProtected)
+			continue;
 
 		ptr->size += sai->IV_size;
 

src/isomedia/box_dump.c
@@ -6058,4 +6058,32 @@ GF_Err csgp_box_dump(GF_Box *a, FILE * trace)
 }
 
 
+GF_Err ienc_box_dump(GF_Box *a, FILE * trace)
+{
+	u32 i;
+	GF_ItemEncryptionPropertyBox *ptr = (GF_ItemEncryptionPropertyBox *)a;
+	if (!a) return GF_BAD_PARAM;
+	gf_isom_box_dump_start(a, "ItemEncryptionPropertyBox", trace);
+	if (ptr->version)
+		gf_fprintf(trace, " skip_byte_block=\"%d\" crypt_byte_block=\"%d\"", ptr->skip_byte_block, ptr->crypt_byte_block);
+	gf_fprintf(trace, ">\n");
+	for (i = 0; i < ptr->num_keys; i++) {
+		gf_fprintf(trace, "<KeyInfo KID=\"");
+		dump_data_hex(trace, ptr->keys[i].KeyID, 16);
+		gf_fprintf(trace, "\"");
+		if (ptr->keys[i].per_sample_IV_size) {
+			gf_fprintf(trace, " IV_size=\"%d\"/>\n", ptr->keys[i].per_sample_IV_size);
+		} else {
+			gf_fprintf(trace, " constant_IV_size=\"%d\" constant_IV=\"", ptr->keys[i].constant_IV_size);
+			dump_data_hex(trace, ptr->keys[i].constant_IV, ptr->keys[i].constant_IV_size);
+			gf_fprintf(trace, "\"/>\n");
+		}
+	}
+	if (!ptr->size)
+		gf_fprintf(trace, "<KeyInfo KID=\"\" IV_size=\"\" constant_IV_size=\"\" constant_IV=\"\" />\n");
+
+	gf_isom_box_dump_done("ItemEncryptionPropertyBox", a, trace);
+	return GF_OK;
+}
+
 #endif /*GPAC_DISABLE_ISOM_DUMP*/

src/isomedia/iff.c
@@ -991,6 +991,109 @@ GF_Err mdcv_box_size(GF_Box *s)
 #endif /*GPAC_DISABLE_ISOM_WRITE*/
 
 
+GF_Box *ienc_box_new()
+{
+	ISOM_DECL_BOX_ALLOC(GF_ItemEncryptionPropertyBox, GF_ISOM_BOX_TYPE_IENC);
+	return (GF_Box *)tmp;
+}
+
+void ienc_box_del(GF_Box *a)
+{
+	GF_ItemEncryptionPropertyBox *p = (GF_ItemEncryptionPropertyBox *)a;
+	if (p->keys) gf_free(p->keys);
+	gf_free(p);
+}
+
+GF_Err ienc_box_read(GF_Box *s, GF_BitStream *bs)
+{
+	u32 i;
+	GF_ItemEncryptionPropertyBox *p = (GF_ItemEncryptionPropertyBox *)s;
+
+	ISOM_DECREASE_SIZE(p, 3)
+	gf_bs_read_u8(bs);
+	if (p->version == 0) {
+		gf_bs_read_u8(bs);
+	} else {
+		p->skip_byte_block = gf_bs_read_int(bs, 4);
+		p->crypt_byte_block = gf_bs_read_int(bs, 4);
+	}
+	p->num_keys = gf_bs_read_u8(bs);
+	if (p->num_keys * (sizeof(bin128)+1) > p->size)
+		return GF_NON_COMPLIANT_BITSTREAM;
+	p->keys = gf_malloc(sizeof(IENCKeyInfo) * p->num_keys);
+	if (!p->keys) return GF_OUT_OF_MEM;
+
+	for (i=0; i<p->num_keys; i++) {
+		ISOM_DECREASE_SIZE(p, 17)
+		p->keys[i].per_sample_IV_size = gf_bs_read_u8(bs);
+		gf_bs_read_data(bs, p->keys[i].KeyID, 16);
+		if (!p->keys[i].per_sample_IV_size) {
+			ISOM_DECREASE_SIZE(p, 1)
+			p->keys[i].constant_IV_size = gf_bs_read_u8(bs);
+			switch (p->keys[i].constant_IV_size) {
+			case 0:
+			case 8:
+			case 16:
+				break;
+			default:
+				return GF_NON_COMPLIANT_BITSTREAM;
+			}
+			ISOM_DECREASE_SIZE(p, p->keys[i].constant_IV_size)
+			gf_bs_read_data(bs, p->keys[i].constant_IV, p->keys[i].constant_IV_size);
+		} else {
+			p->keys[i].constant_IV_size = 0;
+		}
+	}
+	return GF_OK;
+}
+
+#ifndef GPAC_DISABLE_ISOM_WRITE
+GF_Err ienc_box_write(GF_Box *s, GF_BitStream *bs)
+{
+	u32 i;
+	GF_Err e;
+	GF_ItemEncryptionPropertyBox *p = (GF_ItemEncryptionPropertyBox*)s;
+	if (p->skip_byte_block || p->crypt_byte_block)
+		p->version = 1;
+	e = gf_isom_full_box_write(s, bs);
+	if (e) return e;
+	gf_bs_write_u8(bs, 0);
+	if (p->version) {
+		gf_bs_write_int(bs, p->skip_byte_block, 4);
+		gf_bs_write_int(bs, p->crypt_byte_block, 4);
+	} else {
+		gf_bs_write_u8(bs, 0);
+	}
+	gf_bs_write_u8(bs, p->num_keys);
+	for (i = 0; i < p->num_keys; i++) {
+		gf_bs_write_u8(bs, p->keys[i].per_sample_IV_size);
+		gf_bs_write_data(bs, p->keys[i].KeyID, sizeof(bin128));
+		if (!p->keys[i].per_sample_IV_size) {
+			gf_bs_write_u8(bs, p->keys[i].constant_IV_size);
+			gf_bs_write_data(bs, p->keys[i].constant_IV, p->keys[i].constant_IV_size);
+		}
+	}
+	return GF_OK;
+}
+
+GF_Err ienc_box_size(GF_Box *s)
+{
+	u32 i;
+	GF_ItemEncryptionPropertyBox *p = (GF_ItemEncryptionPropertyBox*)s;
+
+	p->size += 3;
+	for (i=0; i<p->num_keys; i++) {
+		p->size += 17;
+		if (!p->keys[i].per_sample_IV_size)
+			p->size += 1 + p->keys[i].constant_IV_size;
+	}
+	return GF_OK;
+}
+
+#endif /*GPAC_DISABLE_ISOM_WRITE*/
+
+
+
 
 static GF_Err gf_isom_iff_create_image_item_from_track_internal(GF_ISOFile *movie, Bool root_meta, u32 meta_track_number, u32 imported_track, const char *item_name, u32 item_id, GF_ImageItemProperties *image_props, GF_List *item_extent_refs, u32 sample_number) {
 #ifndef GPAC_DISABLE_ISOM_WRITE
@@ -1003,9 +1106,13 @@ static GF_Err gf_isom_iff_create_image_item_from_track_internal(GF_ISOFile *movi
 	u32 timescale;
 	u32 item_type = 0;
 	GF_ImageItemProperties local_image_props;
+	GF_ImageItemProtection ipro, *orig_ipro = NULL;
 	Bool config_needed = 0;
 	GF_Box *config_box = NULL;
-	//u32 media_brand = 0;
+	Bool is_cenc = GF_FALSE;
+	Bool is_first = GF_TRUE;
+	u8 *sai = NULL;
+	u32 sai_size = 0, sai_alloc_size = 0;
 	u32 sample_desc_index = 0;
 
 	if (image_props && image_props->tile_mode != TILE_ITEM_NONE) {
@@ -1066,6 +1173,9 @@ static GF_Err gf_isom_iff_create_image_item_from_track_internal(GF_ISOFile *movi
 	if (!image_props) {
 		image_props = &local_image_props;
 		memset(image_props, 0, sizeof(GF_ImageItemProperties));
+	} else {
+		orig_ipro = image_props->cenc_info;
+		image_props->cenc_info = NULL;
 	}
 
 import_next_sample:
@@ -1079,7 +1189,7 @@ import_next_sample:
 		sample = gf_isom_get_sample(movie, imported_track, sample_number, &sample_desc_index);
 		e = gf_isom_last_error(movie);
 	} else {
-		e = gf_isom_get_sample_for_media_time(movie, imported_track, (u64)(image_props->time*timescale), &sample_desc_index, GF_ISOM_SEARCH_SYNC_FORWARD, &sample, NULL, NULL);
+		e = gf_isom_get_sample_for_media_time(movie, imported_track, (u64)(image_props->time*timescale), &sample_desc_index, GF_ISOM_SEARCH_SYNC_FORWARD, &sample, &sample_number, NULL);
 	}
 	if (e || !sample || !sample->IsRAP) {
 		if (!sample) {
@@ -1089,14 +1199,26 @@ import_next_sample:
 		} else {
 			GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("Error no sync sample found after time %g\n", image_props->time));
 		}
-		if (sample) gf_isom_sample_del(&sample);
-		if (config_box) gf_isom_box_del(config_box);
-		return GF_BAD_PARAM;
+		if (!e) e = GF_BAD_PARAM;
+		goto exit;
 	}
 
 	/* Check if the track type is supported as item type */
 	/* Get the config box if needed */
 	subtype = gf_isom_get_media_subtype(movie, imported_track, sample_desc_index);
+	if (gf_isom_is_media_encrypted(movie, imported_track, sample_desc_index)) {
+		if (gf_isom_is_cenc_media(movie, imported_track, sample_desc_index)) {
+			e = gf_isom_get_original_format_type(movie, imported_track, sample_desc_index, &subtype);
+			if (e) goto exit;
+			is_cenc = GF_TRUE;
+		} else {
+			GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("Protected sample not using CENC, cannot add as item\n"));
+			e = GF_BAD_PARAM;
+			goto exit;
+		}
+	}
+
+
 	switch (subtype) {
 	case GF_ISOM_SUBTYPE_AVC_H264:
 	case GF_ISOM_SUBTYPE_AVC2_H264:
@@ -1104,9 +1226,9 @@ import_next_sample:
 	case GF_ISOM_SUBTYPE_AVC4_H264:
 		//FIXME: in avc1 with multiple descriptor, we should take the right description index
 		config_box = gf_isom_box_new(GF_ISOM_BOX_TYPE_AVCC);
-		if (!config_box) return GF_OUT_OF_MEM;
+		if (!config_box) { e = GF_OUT_OF_MEM; goto exit; }
 		((GF_AVCConfigurationBox *)config_box)->config = gf_isom_avc_config_get(movie, imported_track, sample_desc_index);
-		if (! ((GF_AVCConfigurationBox *)config_box)->config) return GF_OUT_OF_MEM;
+		if (! ((GF_AVCConfigurationBox *)config_box)->config) { e = GF_OUT_OF_MEM; goto exit; }
 		item_type = GF_ISOM_SUBTYPE_AVC_H264;
 		config_needed = 1;
 		num_channels = 3;
@@ -1116,9 +1238,9 @@ import_next_sample:
 		break;
 	case GF_ISOM_SUBTYPE_SVC_H264:
 		config_box = gf_isom_box_new(GF_ISOM_BOX_TYPE_SVCC);
-		if (!config_box) return GF_OUT_OF_MEM;
+		if (!config_box) { e = GF_OUT_OF_MEM; goto exit; }
 		((GF_AVCConfigurationBox *)config_box)->config = gf_isom_svc_config_get(movie, imported_track, sample_desc_index);
-		if (! ((GF_AVCConfigurationBox *)config_box)->config) return GF_OUT_OF_MEM;
+		if (! ((GF_AVCConfigurationBox *)config_box)->config) { e = GF_OUT_OF_MEM; goto exit; }
 		item_type = GF_ISOM_SUBTYPE_SVC_H264;
 		config_needed = 1;
 		num_channels = 3;
@@ -1128,9 +1250,9 @@ import_next_sample:
 		break;
 	case GF_ISOM_SUBTYPE_MVC_H264:
 		config_box = gf_isom_box_new(GF_ISOM_BOX_TYPE_MVCC);
-		if (!config_box) return GF_OUT_OF_MEM;
+		if (!config_box) { e = GF_OUT_OF_MEM; goto exit; }
 		((GF_AVCConfigurationBox *)config_box)->config = gf_isom_mvc_config_get(movie, imported_track, sample_desc_index);
-		if (! ((GF_AVCConfigurationBox *)config_box)->config) return GF_OUT_OF_MEM;
+		if (! ((GF_AVCConfigurationBox *)config_box)->config) { e = GF_OUT_OF_MEM; goto exit; }
 		item_type = GF_ISOM_SUBTYPE_MVC_H264;
 		config_needed = 1;
 		num_channels = 3;
@@ -1146,9 +1268,9 @@ import_next_sample:
 	case GF_ISOM_SUBTYPE_LHV1:
 	case GF_ISOM_SUBTYPE_LHE1:
 		config_box = gf_isom_box_new(GF_ISOM_BOX_TYPE_HVCC);
-		if (!config_box) return GF_OUT_OF_MEM;
+		if (!config_box) { e = GF_OUT_OF_MEM; goto exit; }
 		((GF_HEVCConfigurationBox *)config_box)->config = gf_isom_hevc_config_get(movie, imported_track, sample_desc_index);
-		if (! ((GF_HEVCConfigurationBox *)config_box)->config) return GF_OUT_OF_MEM;
+		if (! ((GF_HEVCConfigurationBox *)config_box)->config) { e = GF_OUT_OF_MEM; goto exit; }
 		if (subtype == GF_ISOM_SUBTYPE_HVT1) {
 			item_type = GF_ISOM_SUBTYPE_HVT1;
 		}
@@ -1158,7 +1280,7 @@ import_next_sample:
 		config_needed = 1;
 		if (!((GF_HEVCConfigurationBox *)config_box)->config) {
 			((GF_HEVCConfigurationBox *)config_box)->config = gf_isom_lhvc_config_get(movie, imported_track, sample_desc_index);
-			if (! ((GF_HEVCConfigurationBox *)config_box)->config) return GF_OUT_OF_MEM;
+			if (! ((GF_HEVCConfigurationBox *)config_box)->config) { e = GF_OUT_OF_MEM; goto exit; }
 			item_type = GF_ISOM_SUBTYPE_LHV1;
 		}
 		num_channels = 3;
@@ -1170,9 +1292,9 @@ import_next_sample:
 	case GF_ISOM_SUBTYPE_AV01:
 		{
 			config_box = gf_isom_box_new(GF_ISOM_BOX_TYPE_AV1C);
-			if (!config_box) return GF_OUT_OF_MEM;
+			if (!config_box) { e = GF_OUT_OF_MEM; goto exit; }
 			((GF_AV1ConfigurationBox *)config_box)->config = gf_isom_av1_config_get(movie, imported_track, sample_desc_index);
-			if (! ((GF_AV1ConfigurationBox *)config_box)->config) return GF_OUT_OF_MEM;
+			if (! ((GF_AV1ConfigurationBox *)config_box)->config) { e = GF_OUT_OF_MEM; goto exit; }
 			item_type = GF_ISOM_SUBTYPE_AV01;
 			config_needed = 1;
 			u8 depth = ((GF_AV1ConfigurationBox *)config_box)->config->high_bitdepth ? (((GF_AV1ConfigurationBox *)config_box)->config->twelve_bit ? 12 : 10 ) : 8;
@@ -1193,9 +1315,9 @@ import_next_sample:
 
 	case GF_ISOM_SUBTYPE_VVC1:
 		config_box = gf_isom_box_new(GF_ISOM_BOX_TYPE_VVCC);
-		if (!config_box) return GF_OUT_OF_MEM;
+		if (!config_box) { e = GF_OUT_OF_MEM; goto exit; }
 		((GF_VVCConfigurationBox *)config_box)->config = gf_isom_vvc_config_get(movie, imported_track, sample_desc_index);
-		if (! ((GF_VVCConfigurationBox *)config_box)->config) return GF_OUT_OF_MEM;
+		if (! ((GF_VVCConfigurationBox *)config_box)->config) { e = GF_OUT_OF_MEM; goto exit; }
 		item_type = GF_ISOM_SUBTYPE_VVC1;
 
 		config_needed = 1;
@@ -1206,24 +1328,24 @@ import_next_sample:
 		break;
 	default:
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("Error: Codec not supported to create HEIF image items\n"));
-		return GF_NOT_SUPPORTED;
+		e = GF_NOT_SUPPORTED;
+		goto exit;
 	}
 	if (config_needed && !config_box && !((GF_AVCConfigurationBox *)config_box)->config) {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("Error: Image type %s requires a missing configuration box\n", gf_4cc_to_str(item_type)));
-		return GF_BAD_PARAM;
+		e = GF_BAD_PARAM;
+		goto exit;
 	}
 	/* Get some images properties from the track data */
 	e = gf_isom_get_visual_info(movie, imported_track, sample_desc_index, &w, &h);
 	if (e) {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("Error determining image size\n"));
-		if (config_box) gf_isom_box_del(config_box);
-		return e;
+		goto exit;
 	}
 	e = gf_isom_get_pixel_aspect_ratio(movie, imported_track, sample_desc_index, &hSpacing, &vSpacing);
 	if (e) {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("Error determining image aspect ratio\n"));
-		if (config_box) gf_isom_box_del(config_box);
-		return e;
+		goto exit;
 	}
 	if (!image_props->width && !image_props->height) {
 		image_props->width = w;
@@ -1240,10 +1362,50 @@ import_next_sample:
 		image_props->bits_per_channel[1] = bits_per_channel[1];
 		image_props->bits_per_channel[2] = bits_per_channel[2];
 	}
+	if (is_cenc) {
+		Bool Is_Encrypted;
+
+		memset(&ipro, 0, sizeof(GF_ImageItemProtection));
+		gf_isom_get_cenc_info(movie, imported_track, sample_desc_index, NULL, &ipro.scheme_type, &ipro.scheme_version, NULL);
+		e = gf_isom_get_sample_cenc_info(movie, imported_track, sample_desc_index, &Is_Encrypted, &ipro.IV_size, &ipro.KID, &ipro.crypt_byte_block, &ipro.skip_byte_block, &ipro.constant_IV_size, &ipro.constant_IV);
+		if (e) goto exit;
+
+		if (Is_Encrypted) {
+			sai_size = sai_alloc_size;
+			e = gf_isom_cenc_get_sample_aux_info_buffer(movie, imported_track, sample_number, sample_desc_index, NULL, &sai, &sai_size);
+			if (e) goto exit;
+
+			if (sai_size > sai_alloc_size)
+				sai_alloc_size = sai_size;
+
+			ipro.sai_data = sai;
+			ipro.sai_data_size = sai_size;
+			image_props->cenc_info = &ipro;
+
+			if (is_first) {
+				u32 i, nb_pssh = gf_isom_get_pssh_count(movie);
+				for (i=0; i<nb_pssh; i++) {
+					bin128 SystemID;
+					u32 version;
+					u32 KID_count;
+					const bin128 *KIDs;
+					const u8 *private_data;
+					u32 private_data_size;
+
+					gf_isom_get_pssh_info(movie, i+1, SystemID, &version, &KID_count, &KIDs, &private_data, &private_data_size);
+					
+					gf_cenc_set_pssh(movie, SystemID, version, KID_count, (bin128 *) KIDs, (u8 *) private_data, private_data_size, 2);
+				}
+			}
+
+		} else {
+			image_props->cenc_info = NULL;
+		}
+	}
 
 	if (!item_id) {
 		e = gf_isom_meta_get_next_item_id(movie, root_meta, meta_track_number, &item_id);
-		if (e) return e;
+		if (e) goto exit;
 	}
 	if (image_props->sample_num && item_name && !strcmp(item_name, "ref")) {
 		GF_LOG(GF_LOG_INFO, GF_LOG_CONTAINER, ("Refering trackID %d sample %d as item %d\n", imported_track, sample_number, item_id));
@@ -1266,17 +1428,26 @@ import_next_sample:
 	//if (media_brand) {
 	//	gf_isom_modify_alternate_brand(movie, media_brand, GF_TRUE);
 	//}
-	gf_isom_sample_del(&sample);
-	if (config_box) gf_isom_box_del(config_box);
 
-	if (!image_props->sample_num && (image_props->time<0)) {
+	if (!e && !image_props->sample_num && (image_props->time<0)) {
 		if (sample_number >= gf_isom_get_sample_count(movie, imported_track)) return e;
 		sample_number++;
 		item_id=0;
+		gf_isom_sample_del(&sample);
+		if (config_box) gf_isom_box_del(config_box);
+		is_first = GF_FALSE;
 		//avoid recursion this could get quite big
 		goto import_next_sample;
 	}
+
+exit:
+	if (sai) gf_free(sai);
+	gf_isom_sample_del(&sample);
+	if (config_box) gf_isom_box_del(config_box);
+	if (orig_ipro) image_props->cenc_info = orig_ipro;
 	return e;
+
+
 #else
 	return GF_NOT_SUPPORTED;
 #endif

applications/mp4box/filedump.c
@@ -2016,14 +2016,30 @@ static void DumpMetaItem(GF_ISOFile *file, Bool root_meta, u32 tk_num, char *nam
 	for (i=0; i<count; i++) {
 		const char *it_name, *mime, *enc, *url, *urn;
 		Bool self_ref;
-		u32 ID;
-		u32 it_type;
-		gf_isom_get_meta_item_info(file, root_meta, tk_num, i+1, &ID, &it_type, NULL, &self_ref, &it_name, &mime, &enc, &url, &urn);
+		u32 ID, j;
+		u32 it_type, cenc_scheme, cenc_version;
+		gf_isom_get_meta_item_info(file, root_meta, tk_num, i+1, &ID, &it_type, &cenc_scheme, &cenc_version, &self_ref, &it_name, &mime, &enc, &url, &urn);
 		fprintf(stderr, "Item #%d - ID %d - type %s ", i+1, ID, gf_4cc_to_str(it_type));
 		if (self_ref) fprintf(stderr, " - Self-Reference");
-		else if (it_name) fprintf(stderr, " - Name: %s", it_name);
+		else if (it_name && it_name[0]) fprintf(stderr, " - Name: %s", it_name);
 		if (mime) fprintf(stderr, " - MimeType: %s", mime);
 		if (enc) fprintf(stderr, " - ContentEncoding: %s", enc);
+		if (cenc_scheme) {
+			Bool is_protected;
+			u8 skip_byte_block, crypt_byte_block, IV_size, constant_IV_size;
+			bin128 KeyID, constant_IV;
+			fprintf(stderr, " - Protection scheme: %s v0x%08X", gf_4cc_to_str(cenc_scheme), cenc_version);
+
+			gf_isom_extract_meta_item_get_cenc_info(file, root_meta, tk_num, ID, &is_protected, &skip_byte_block, &crypt_byte_block, &IV_size, &KeyID, &constant_IV_size, &constant_IV, NULL, NULL, NULL);
+
+			fprintf(stderr, "\n\t");
+			fprintf(stderr, "KID ");
+			for (j=0; j<16; j++) fprintf(stderr, "%02X", KeyID[j]);
+			fprintf(stderr, " - %sIV size %d ", constant_IV_size ? "const " : "", constant_IV_size ? constant_IV_size : IV_size);
+			if (skip_byte_block && crypt_byte_block)
+				fprintf(stderr, " - Pattern %d:%d", skip_byte_block, crypt_byte_block);
+		}
+
 		fprintf(stderr, "\n");
 		if (url) fprintf(stderr, "URL: %s\n", url);
 		if (urn) fprintf(stderr, "URN: %s\n", urn);

applications/mp4box/fileimport.c
@@ -1069,7 +1069,7 @@ GF_Err import_file(GF_ISOFile *dest, char *inName, u32 import_flags, GF_Fraction
 		import.in_name = szName;
 		import.flags = GF_IMPORT_PROBE_ONLY;
 		e = gf_media_import(&import);
-		GOTO_EXIT("media import failure");
+		GOTO_EXIT("importing import");
 
 		if (!strnicmp(ext, "audio", 5)) do_audio = 1;
 		else if (!strnicmp(ext, "video", 5)) do_video = 1;
@@ -1205,7 +1205,7 @@ GF_Err import_file(GF_ISOFile *dest, char *inName, u32 import_flags, GF_Fraction
 		e = gf_media_import(&import);
 		if (e) {
 			if (import.update_mux_args) gf_free(import.update_mux_args);
-			GOTO_EXIT("media import failure");
+			GOTO_EXIT("importing media");
 		}
 
 		if (fsess) {

applications/mp4box/main.c
@@ -688,6 +688,7 @@ void PrintEncryptUsage()
 	gf_sys_format_help(helpout, help_flags, "# Encryption/Decryption Options\n"
 	"MP4Box supports encryption and decryption of ISMA, OMA and CENC content, see [encryption filter `gpac -h cecrypt`](cecrypt).\n"
 	"It requires a specific XML file called `CryptFile`, whose syntax is available at https://wiki.gpac.io/Common-Encryption\n"
+	"Image files (HEIF) can also be crypted / decrypted, using CENC only.\n"
 	"  \n"
 	"Options:\n"
 	);

include/gpac/internal/isomedia_dev.h
@@ -430,6 +430,8 @@ enum
 	GF_ISOM_BOX_TYPE_AUXI	= GF_4CC( 'a', 'u', 'x', 'i' ),
 	GF_ISOM_BOX_TYPE_OINF	= GF_4CC( 'o', 'i', 'n', 'f' ),
 	GF_ISOM_BOX_TYPE_TOLS	= GF_4CC( 't', 'o', 'l', 's' ),
+	GF_ISOM_BOX_TYPE_IENC	= GF_4CC( 'i', 'e', 'n', 'c' ),
+	GF_ISOM_BOX_TYPE_IAUX 	= GF_4CC('i', 'a', 'u', 'x'),
 
 	/* MIAF Boxes */
 	GF_ISOM_BOX_TYPE_CLLI	= GF_4CC('c', 'l', 'l', 'i'),
@@ -3574,6 +3576,22 @@ typedef struct {
 	u32 data_size;
 	u8 *data;
 } GF_AuxiliaryTypePropertyBox;
+
+
+typedef struct
+{
+	u8 per_sample_IV_size;
+	bin128 KeyID;
+	u8 constant_IV_size;
+	bin128 constant_IV;
+} IENCKeyInfo;
+
+typedef struct {
+	GF_ISOM_FULL_BOX
+	u8 skip_byte_block, crypt_byte_block;
+	u32 num_keys;
+	IENCKeyInfo *keys;
+} GF_ItemEncryptionPropertyBox;
 
 typedef struct {
 	GF_ISOM_FULL_BOX

include/gpac/isomedia.h
@@ -142,8 +142,13 @@ enum
 	GF_ISOM_REF_LYRA = GF_4CC( 'l', 'y', 'r', 'a' ),
 	/*! File Delivery Item Information Extension */
 	GF_ISOM_REF_FDEL = GF_4CC( 'f', 'd', 'e', 'l' ),
+#ifdef GF_ENABLE_CTRN
 	/*! Track fragment inherit */
 	GF_ISOM_REF_TRIN = GF_4CC( 't', 'r', 'i', 'n' ),
+#endif
+
+	/*! Item auxiliary reference */
+	GF_ISOM_REF_AUXR = GF_4CC( 'a', 'u', 'x', 'r' ),
 };
 
 /*! Track Edit list type*/
@@ -344,6 +349,10 @@ enum
 	GF_QT_SUBTYPE_YVYU = GF_4CC('Y','V','Y','U'),
 	GF_QT_SUBTYPE_RGBA = GF_4CC('R','G','B','A'),
 	GF_QT_SUBTYPE_ABGR = GF_4CC('A','B','G','R'),
+
+
+	GF_ISOM_ITEM_TYPE_AUXI 	= GF_4CC('a', 'u', 'x', 'i'),
+
 };
 
 
@@ -5156,6 +5165,8 @@ typedef struct __cenc_sample_aux_info
 {
  	/*! IV size: 0, 8 or 16; it MUST NOT be written to file*/
 	u8 IV_size;
+ 	/*! flag set if sample is clear - it MUST NOT be written to file*/
+	u8 isNotProtected;
  	/*! IV can be 0, 64 or 128 bits - if 64, bytes 0-7 are used and 8-15 are 0-padded*/
 	bin128 IV;
 	/*! number of subsamples*/
@@ -5279,10 +5290,10 @@ GF_Err gf_isom_set_cenc_protection(GF_ISOFile *isom_file, u32 trackNumber, u32 s
 \param KID the list of key IDs
 \param data opaque data for the protection system
 \param len size of the opaque data
-\param use_piff if GF_TRUE, use PIFF pssh UUID box when creating the pssh
+\param pssh_mode 0: regular PSSH in moov, 1: PIFF PSSH in moov, 2: regular PSSH in meta
 \return error if any
 */
-GF_Err gf_cenc_set_pssh(GF_ISOFile *isom_file, bin128 systemID, u32 version, u32 KID_count, bin128 *KID, u8 *data, u32 len, Bool use_piff);
+GF_Err gf_cenc_set_pssh(GF_ISOFile *isom_file, bin128 systemID, u32 version, u32 KID_count, bin128 *KID, u8 *data, u32 len, u32 pssh_mode);
 
 /*! removes CENC senc box info
 \param isom_file the target ISO file
@@ -5510,7 +5521,8 @@ u32 gf_isom_get_meta_item_count(GF_ISOFile *isom_file, Bool root_meta, u32 track
 \param item_num 1-based index of item to query
 \param itemID set to item ID in file (optional, can be NULL)
 \param type set to item 4CC type
-\param protection_idx set to the 1-based index of the protection in used, 0 if not protected
+\param protection_scheme set to 0 if not protected, or scheme type used if item is protected. If protected but scheme type not present, set to 'unkw'
+\param protection_scheme_version set to 0 if not protected, or scheme version used if item is protected
 \param is_self_reference set to item is the file itself
 \param item_name set to the item name (optional, can be NULL)
 \param item_mime_type set to the item mime type (optional, can be NULL)
@@ -5520,10 +5532,9 @@ u32 gf_isom_get_meta_item_count(GF_ISOFile *isom_file, Bool root_meta, u32 track
 \return error if any
 */
 GF_Err gf_isom_get_meta_item_info(GF_ISOFile *isom_file, Bool root_meta, u32 track_num, u32 item_num,
-                                  u32 *itemID, u32 *type, u32 *protection_idx, Bool *is_self_reference,
+                                  u32 *itemID, u32 *type, u32 *protection_scheme, u32 *protection_scheme_version, Bool *is_self_reference,
                                   const char **item_name, const char **item_mime_type, const char **item_encoding,
                                   const char **item_url, const char **item_urn);
-
 
 /*! gets item index from item ID
 \param isom_file the target ISO file
@@ -5558,6 +5569,28 @@ GF_Err gf_isom_extract_meta_item(GF_ISOFile *isom_file, Bool root_meta, u32 trac
 */
 GF_Err gf_isom_extract_meta_item_mem(GF_ISOFile *isom_file, Bool root_meta, u32 track_num, u32 item_id, u8 **out_data, u32 *out_size, u32 *out_alloc_size, const char **mime_type, Bool use_annex_b);
 
+
+/*! fetch CENC info for an item
+\param isom_file the target ISO file
+\param root_meta if GF_TRUE uses meta at the file, otherwise uses meta at the movie level if track number is 0
+\param track_num if GF_TRUE and root_meta is GF_FALSE, uses meta at the track level
+\param item_id the ID of the item to dump
+\param is_protected set to GF_TRUE if item is protected
+\param skip_byte_block set to skip_byte_block or 0 if no pattern
+\param crypt_byte_block set to crypt_byte_block or 0 if no pattern
+\param IV_size set to per-sample IV size, or 0 if constant IV
+\param KeyID set to sample KEY ID
+\param constant_IV_size set to constant IV size, or 0 if per-sample IV
+\param constant_IV set to constant IV used, not modified if per-sample IV
+\param sai_out_data set to allocated buffer containing the item, shall be freeed by user - may be NULL to only retrieve the info
+\param sai_out_size set to the size of the allocated buffer - may be NULL if  sai_out_data is NULL
+\param sai_out_alloc_size set to the allocated size of the buffer (this allows passing an existing buffer without always reallocating it) - may be NULL if  sai_out_data is NULL
+\return error if any
+*/
+GF_Err gf_isom_extract_meta_item_get_cenc_info(GF_ISOFile *isom_file, Bool root_meta, u32 track_num, u32 item_id, Bool *is_protected,
+	u8 *skip_byte_block, u8 *crypt_byte_block, u8 *IV_size, bin128 *KeyID, u8 *constant_IV_size, bin128 *constant_IV,
+	u8 **sai_out_data, u32 *sai_out_size, u32 *sai_out_alloc_size);
+
 /*! gets primary item ID
 \param isom_file the target ISO file
 \param root_meta if GF_TRUE uses meta at the file, otherwise uses meta at the movie level if track number is 0
@@ -5578,6 +5611,22 @@ typedef enum {
 	/*! a tile item single*/
 	TILE_ITEM_SINGLE
 } GF_TileItemMode;
+
+
+/*! Image protection item properties*/
+typedef struct
+{
+	u32 scheme_type;
+	u32 scheme_version;
+	u8 IV_size;
+	bin128 KID;
+	u8 crypt_byte_block;
+	u8 skip_byte_block;
+	u8 constant_IV_size;
+	bin128 constant_IV;
+	const u8 *sai_data;
+	u32 sai_data_size;
+} GF_ImageItemProtection;
 
 /*! Image item properties*/
 typedef struct
@@ -5620,8 +5669,12 @@ typedef struct
 	u8 num_channels;
 	/*! bits per channels in bits*/
 	u8 bits_per_channel[3];
+	/*! number of columns in grid*/
 	u32 num_grid_columns;
+	/*! number of rows in grid*/
 	u32 num_grid_rows;
+	/*! protection info, NULL if item is not protected*/
+	GF_ImageItemProtection *cenc_info;
 } GF_ImageItemProperties;
 
 

commit c4421daf99bb9c32bf4ac4e5c016b5bf86e0365d

fixed bugs introduced by 5ed9799e

src/isomedia/box_code_drm.c
@@ -1400,11 +1400,17 @@ GF_Err senc_box_write(GF_Box *s, GF_BitStream *bs)
 {
 	GF_Err e;
 	u32 i, j;
-	u32 sample_count;
+	u32 sample_count, nb_crypt_samples;
 	GF_SampleEncryptionBox *ptr = (GF_SampleEncryptionBox *) s;
 
 	sample_count = gf_list_count(ptr->samp_aux_info);
-	if (!sample_count) {
+	//temp patch until we cleanup the spec...
+	nb_crypt_samples = 0;
+	for (i = 0; i < sample_count; i++) {
+		GF_CENCSampleAuxInfo *sai = (GF_CENCSampleAuxInfo *)gf_list_get(ptr->samp_aux_info, i);
+		if (!sai->isNotProtected) nb_crypt_samples++;
+	}
+	if (!nb_crypt_samples) {
 		ptr->size = 0;
 		return GF_OK;
 	}
@@ -1415,7 +1421,7 @@ GF_Err senc_box_write(GF_Box *s, GF_BitStream *bs)
 	gf_bs_write_u8(bs, ptr->version);
 	gf_bs_write_u24(bs, ptr->flags);
 
-	gf_bs_write_u32(bs, sample_count);
+	gf_bs_write_u32(bs, nb_crypt_samples);
 
 	e = store_senc_info(ptr, bs);
 	if (e) return e;
@@ -1443,11 +1449,17 @@ GF_Err senc_box_write(GF_Box *s, GF_BitStream *bs)
 GF_Err senc_box_size(GF_Box *s)
 {
 	u32 sample_count;
-	u32 i;
+	u32 i, nb_crypt_samples;
 	GF_SampleEncryptionBox *ptr = (GF_SampleEncryptionBox*)s;
 	sample_count = gf_list_count(ptr->samp_aux_info);
+	//temp patch until we cleanup the spec...
+	nb_crypt_samples=0;
+	for (i = 0; i < sample_count; i++) {
+		GF_CENCSampleAuxInfo *sai = (GF_CENCSampleAuxInfo *)gf_list_get(ptr->samp_aux_info, i);
+		if (!sai->isNotProtected) nb_crypt_samples++;
+	}
 
-	if (!sample_count) {
+	if (!nb_crypt_samples) {
 		ptr->size = 0;
 		return GF_OK;
 	}

src/isomedia/box_dump.c
@@ -5102,6 +5102,7 @@ GF_Err senc_box_dump(GF_Box *a, FILE * trace)
 	gf_fprintf(trace, "<FullBoxInfo Version=\"%d\" Flags=\"0x%X\"/>\n", ptr->version, ptr->flags);
 	for (i=0; i<sample_count; i++) {
 		GF_CENCSampleAuxInfo *cenc_sample = (GF_CENCSampleAuxInfo *)gf_list_get(ptr->samp_aux_info, i);
+		if (cenc_sample->isNotProtected) continue;
 
 		if (cenc_sample) {
 			gf_fprintf(trace, "<SampleEncryptionEntry sampleNumber=\"%d\" IV_size=\"%u\"", i+1, cenc_sample->IV_size);

src/isomedia/iff.c
@@ -1430,12 +1430,15 @@ import_next_sample:
 	//}
 
 	if (!e && !image_props->sample_num && (image_props->time<0)) {
-		if (sample_number >= gf_isom_get_sample_count(movie, imported_track)) return e;
-		sample_number++;
 		item_id=0;
 		gf_isom_sample_del(&sample);
-		if (config_box) gf_isom_box_del(config_box);
+		if (config_box) {
+			gf_isom_box_del(config_box);
+			config_box = NULL;
+		}
 		is_first = GF_FALSE;
+		if (sample_number >= gf_isom_get_sample_count(movie, imported_track)) return e;
+		sample_number++;
 		//avoid recursion this could get quite big
 		goto import_next_sample;
 	}