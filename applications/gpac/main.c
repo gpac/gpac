gpac/gpac

commit 15119f209102c46480f768e6ff4c6e454dadcbc6

fixed bug in user-assigned pid property inheritance

applications/gpac/main.c
@@ -336,7 +336,10 @@ const char *gpac_doc =
 "- `bxml@FOO` will be declared as data with a value set to the binarized content of `FOO`.\n"
 "- `FOO` will be declared as string with a value set to `FOO`.\n"
 "- `TYPE@FOO` will be parsed according to `TYPE`. If the type is not recognized, the entire value is copied as string. See `gpac -h props` for defined types.\n"
-
+"\n"
+"User-assigned PID properties on filter `fA` will be inherited by all filters dynamically loaded to solve `fA -> fB` connection.\n"
+"If `fB` also has user-assigned PID properties, these only apply starting from `fB` in the chain and are not inherited by filters between fA and fB.\n"
+"\n"
 "Warning: Properties are not filtered and override the properties of the filter's output PIDs, be carefull not to break "
 "the session by overriding core properties such as width/height/samplerate/... !\n"
 "EX -i v1.mp4:#ServiceID=4 -i v2.mp4:#ServiceID=2 -o dump.ts\n"

share/doc/man/gpac-filters.1
@@ -7106,7 +7106,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev355-g67ae77d0b-master
+build: 1.1.0-DEV-rev357-g1ddbb50d1-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -1695,6 +1695,14 @@ Non built-in properties are parsed as follows:
 .br
 - TYPE@FOO will be parsed according to TYPE. If the type is not recognized, the entire value is copied as string. See gpac -h props for defined types.
 .br
+
+.br
+User-assigned PID properties on filter fA will be inherited by all filters dynamically loaded to solve fA -> fB connection.
+.br
+If fB also has user-assigned PID properties, these only apply starting from fB in the chain and are not inherited by filters between fA and fB.
+.br
+
+.br
 Warning: Properties are not filtered and override the properties of the filter's output PIDs, be carefull not to break the session by overriding core properties such as width/height/samplerate/... !
 .br
 Example
@@ -3927,7 +3935,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev355-g67ae77d0b-master
+build: 1.1.0-DEV-rev357-g1ddbb50d1-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2414,7 +2414,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev355-g67ae77d0b-master
+build: 1.1.0-DEV-rev357-g1ddbb50d1-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4client.1
@@ -261,7 +261,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev355-g67ae77d0b-master
+build: 1.1.0-DEV-rev357-g1ddbb50d1-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

src/filter_core/filter.c
@@ -160,6 +160,54 @@ char *gf_filter_get_dst_name(GF_Filter *filter)
 	return res;
 }
 
+/*push arguments from source and dest into the final argument string. We strip the following
+- FID: will be inherited from last explicit filter in the chain
+- SID: is cloned during the resolution while loading the filter chain
+- user-assigned PID properties on destination (they only apply after the destination, not in the new chain).
+
+Note that user-assigned PID properties assigned on source are inherited so that
+	-i SRC:#PidProp=PidVal -o dst
+
+will have all intermediate filters loaded for the resolution (eg demux) set this property.
+
+*/
+static void filter_push_args(GF_FilterSession *fsess, char **out_args, char *in_args, Bool is_src, Bool first_sep_inserted)
+{
+	char szSep[2];
+	Bool prev_is_db_sep = GF_FALSE;
+	szSep[0] = fsess->sep_args;
+	szSep[1] = 0;
+	while (in_args) {
+		char *sep = strchr(in_args, fsess->sep_args);
+		if (sep) sep[0] = 0;
+
+		if (!strncmp(in_args, "FID", 3) && (in_args[3]==fsess->sep_name)) {
+		}
+		else if (!strncmp(in_args, "SID", 3) && (in_args[3]==fsess->sep_name)) {
+		}
+		else if (!is_src && (in_args[0]==fsess->sep_frag)) {
+
+		} else {
+			if (*out_args && !first_sep_inserted) {
+				gf_dynstrcat(out_args, szSep, NULL);
+				if (prev_is_db_sep)
+					gf_dynstrcat(out_args, szSep, NULL);
+			}
+			gf_dynstrcat(out_args, in_args, NULL);
+			first_sep_inserted = GF_FALSE;
+		}
+		if (!sep) break;
+		sep[0] = fsess->sep_args;
+		in_args = sep+1;
+		prev_is_db_sep = GF_FALSE;
+		if (in_args[0] == fsess->sep_args) {
+			in_args ++;
+			prev_is_db_sep = GF_TRUE;
+		}
+	}
+
+}
+
 GF_Filter *gf_filter_new(GF_FilterSession *fsess, const GF_FilterRegister *freg, const char *src_args, const char *dst_args, GF_FilterArgType arg_type, GF_Err *err, GF_Filter *multi_sink_target, Bool is_dynamic_filter)
 {
 	char szName[200];
@@ -249,8 +297,9 @@ GF_Filter *gf_filter_new(GF_FilterSession *fsess, const GF_FilterRegister *freg,
 		u32 nb_db_sep=0, src_arg_len;
 		char szDBSep[3];
 		Bool insert_escape = GF_FALSE;
+		Bool dst_sep_inserted = GF_FALSE;
 		u32 len = 2 + (u32) strlen(src_striped) + (u32) strlen(dst_striped);
-		//source has a URL (locat or not), escape it to make sure we don't pass dst args as params to the URL
+		//source has a URL (local or not), escape it to make sure we don't pass dst args as params to the URL
 		if ((strstr(src_striped, "src=") || strstr(src_striped, "dst=")) && strstr(src_striped, "://")){
 			char szEscape[10];
 			sprintf(szEscape, "%cgpac", fsess->sep_args);
@@ -272,7 +321,6 @@ GF_Filter *gf_filter_new(GF_FilterSession *fsess, const GF_FilterRegister *freg,
 		if (nb_db_sep % 2) nb_db_sep=1;
 		else nb_db_sep=0;
 
-		all_args = gf_malloc(sizeof(char)*(len+nb_db_sep));
 		if (!nb_db_sep) {
 			szDBSep[1] = 0;
 		}
@@ -282,12 +330,23 @@ GF_Filter *gf_filter_new(GF_FilterSession *fsess, const GF_FilterRegister *freg,
 			szDBSep[0] = 0;
 		}
 
-		if (insert_escape) {
-			sprintf(all_args, "%s%sgpac%c%s", src_striped, szDBSep, fsess->sep_args, dst_striped);
-		} else {
-			sprintf(all_args, "%s%s%s", src_striped, szDBSep, dst_striped);
+		//push src args
+		all_args = NULL;
+		filter_push_args(fsess, &all_args, (char *) src_striped, GF_TRUE, GF_FALSE);
+
+		if (all_args && insert_escape) {
+			gf_dynstrcat(&all_args, szDBSep, NULL);
+			gf_dynstrcat(&all_args, "gpac", NULL);
+			dst_sep_inserted = GF_TRUE;
+		} else if (all_args) {
+			gf_dynstrcat(&all_args, szDBSep, NULL);
+			dst_sep_inserted = GF_TRUE;
 		}
-		localarg_marker = strstr(all_args, "gfloc");
+		//push dst args
+		filter_push_args(fsess, &all_args, (char *) dst_striped, GF_FALSE, dst_sep_inserted);
+
+
+		localarg_marker = all_args ? strstr(all_args, "gfloc") : NULL;
 		if (localarg_marker) {
 			localarg_marker[0]=0;
 			if (strlen(all_args) && (localarg_marker[-1]==fsess->sep_args))
@@ -318,7 +377,8 @@ GF_Filter *gf_filter_new(GF_FilterSession *fsess, const GF_FilterRegister *freg,
 		}
 		return NULL;
 	}
-	if (filter && src_striped) filter->orig_args = gf_strdup(src_striped);
+	if (filter && src_striped)
+		filter->orig_args = gf_strdup(src_striped);
 
 	for (i=0; i<freg->nb_caps; i++) {
 		if (freg->caps[i].flags & GF_CAPFLAG_OUTPUT) {
@@ -1333,6 +1393,7 @@ skip_date:
 			value++;
 		}
 
+		//arg is a PID property assignment
 		if (szArg[0] == filter->session->sep_frag) {
 			filter->user_pid_props = GF_TRUE;
 			goto skip_arg;

commit c4f8bc6e61348a951d37756e2d831c0b9c1b2c16

fixed bug in local opts introduced by 15119f20

applications/gpac/main.c
@@ -149,9 +149,9 @@ const char *gpac_doc =
 "EX filter::opt1=UDP://IP:PORT/:someopt=VAL::opt2=VAL2\n"
 "This will pass `UDP://IP:PORT/:someopt=VAL` to `opt1` without inspecting it, and `VAL2` to `opt2`.\n"
 "  \n"
-"A filter may be assigned a name (for inspection purposes) using `:N=name` option. This name is not used in link resolution and may be changed at runtime by the filter instance.\n"
+"A filter may be assigned a name (for inspection purposes, not inherited) using `:N=name` option. This name is not used in link resolution and may be changed at runtime by the filter instance.\n"
 "  \n"
-"A filter may be assigned a tag (any string) using `:TAG=name` option. This tag does not need to be unique, and can be used to exclude filter in link resolution.\n"
+"A filter may be assigned a tag (any string) using `:TAG=name` option. This tag does not need to be unique, and can be used to exclude filter in link resolution. Tags are not inherited, therefore dynamically loaded filters never have a tag.\n"
 "  \n"
 "## Source and Sink filters\n"
 "Source and sink filters do not need to be addressed by the filter name, specifying `src=` or `dst=` instead is enough. "
@@ -286,15 +286,14 @@ const char *gpac_doc =
 "This will pass the `:OPTBAR` to all filters loaded between `file.mp4` source and `file.aac` destination, but not `OPTFOO`.\n"
 "Arguments inheriting can be stopped by using the keyword `gfloc`: arguments after the keyword will not be inherited.\n"
 "EX src=file.mp4 dst=file.aac:OPTFOO:gfloc:OPTBAR dst=file.264\n"
-"This will pass the `:OPTFOO` to all filters loaded between `file.mp4`source and `file.aac` destination, but not `OPTBAR`\n"
+"This will pass `:OPTFOO` to all filters loaded between `file.mp4`source and `file.aac` destination, but not `OPTBAR`\n"
 "Arguments are by default tracked to check if they were used by the filter chain, and a warning is thrown if this is not the case.\n"
 "It may be usefull to specify arguments which may not be consumed depending on the graph resolution; the specific keyword `gfopt` indicates that arguments after the keyword will not be tracked.\n"
 "EX src=file.mp4 dst=file.aac:OPTFOO:gfopt:OPTBAR dst=file.264\n"
 "This will warn if `OPTFOO` is not consumed, but will not track `OPTBAR`.\n"
 "# URL templating\n"
 "Destination URLs can be templated using the same mechanism as MPEG-DASH, where `$KEYWORD$` is replaced in the template with the "
-"resolved value and `$KEYWORD%%0Nd$` is replaced in the template with the resolved integer, padded with N zeros if needed. "
-"`$$` is an escape for $\n"
+"resolved value and `$KEYWORD%%0Nd$` is replaced in the template with the resolved integer, padded with up to N zeros if needed.\n"
 "`KEYWORD` is **case sensitive**, and may be present multiple times in the string. Supported `KEYWORD` are:\n"
 "- num: replaced by file number if defined, 0 otherwise\n"
 "- PID: ID of the source PID\n"
@@ -303,7 +302,10 @@ const char *gpac_doc =
 "- p4cc=ABCD: uses PID property with 4CC value `ABCD`\n"
 "- pname=VAL: uses PID property with name `VAL`\n"
 "- OTHER: locates property 4CC for the given name, or property name if no 4CC matches.\n"
-"\n  \nTemplating can be useful when encoding several qualities in one pass.\n"
+"  \n"
+"`$$` is an escape for $\n"
+"\n"
+"Templating can be useful when encoding several qualities in one pass.\n"
 "EX src=dump.yuv:size=640x360 vcrop:wnd=0x0x320x180 enc:c=avc:b=1M @2 enc:c=avc:b=750k dst=dump_$CropOrigin$x$Width$x$Height$.264:clone\n"
 "This will create a croped version of the source, encoded in AVC at 1M, and a full version of the content in AVC at 750k. "
 "Outputs will be `dump_0x0x320x180.264` for the croped version and `dump_0x0x640x360.264` for the non-croped one.\n"
@@ -319,9 +321,9 @@ const char *gpac_doc =
 "EX src=vid.mpd enc:c=avc:FID=1:clone dst=transcode.mpd:SID=1\n"
 "In this case, the encoder will be cloned for each video PIDs in the source, and the destination will only use PIDs coming from the encoders.\n"
 "# Templating filter chains\n"
-"There can be cases where the number of desired outputs depends on the source content, for example dumping a multiplex of N services into N files. When the destination involves multiplexing the input PIDs, the `:clone`option is not enough since the muxer will always accept the input PIDs.\n"
+"There can be cases where the number of desired outputs depends on the source content, for example dumping a multiplex of N services into N files. When the destination involves multiplexing the input PIDs, the `:clone` option is not enough since the muxer will always accept the input PIDs.\n"
 "To handle this, it is possible to use a PID property name in the sourceID of a filter with the value `*` or an empty value. In this case, whenever a new PID with a new value for the property is found, the filter with such sourceID will be dynamically cloned.\n"
-"Warning: This feature should only be called with a single property set to `*` per source ID, results are undefined otherwise.\n"
+"Warning: This feature should only be called with a single property set to `*` (or empty) per source ID, results are undefined otherwise.\n"
 "EX src=source.ts dst=file_$ServiceID$.mp4:SID=*#ServiceID=*\n"
 "EX src=source.ts dst=file_$ServiceID$.mp4:SID=#ServiceID=\n"
 "In this case, each new `ServiceID` value found when connecting PIDs to the destination will create a new destination file.\n"
@@ -376,7 +378,7 @@ const char *gpac_doc =
 "Warning: These keywords do not apply to PID properties. Multiple keywords cannot be defined for a single option.\n"
 "Defined keywords:\n"
 "- $GSHARE: replaced by system path to GPAC shared directory (e.g. /usr/share/gpac)\n"
-"- $GJS: replaced by the first path from global share directory and set through [-js-dirs](CORE) that contains the file name following the macro, e.g. $GJS/source.js\n"
+"- $GJS: replaced by the first path from global share directory and paths set through [-js-dirs](CORE) that contains the file name following the macro, e.g. $GJS/source.js\n"
 "- $GLANG: replaced by the global config language option [-lang](CORE)\n"
 "- $GUA: replaced by the global config user agent option [-user-agent](CORE)\n"
 "- $GINC(init_val[,inc]): replaced by `init_val` and increment `init_val` by `inc` (positive or negative number, 1 if not specified) each time a new filter using this string is created.\n"

share/doc/man/gpac-filters.1
@@ -7108,7 +7108,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev359-gdcd7c11e5-master
+build: 1.1.0-DEV-rev360-g6680ea709-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -1202,11 +1202,11 @@ This will pass UDP://IP:PORT/:someopt=VAL to opt1 without inspecting it, and VAL
 .br
   
 .br
-A filter may be assigned a name (for inspection purposes) using :N=name option. This name is not used in link resolution and may be changed at runtime by the filter instance.
+A filter may be assigned a name (for inspection purposes, not inherited) using :N=name option. This name is not used in link resolution and may be changed at runtime by the filter instance.
 .br
   
 .br
-A filter may be assigned a tag (any string) using :TAG=name option. This tag does not need to be unique, and can be used to exclude filter in link resolution.
+A filter may be assigned a tag (any string) using :TAG=name option. This tag does not need to be unique, and can be used to exclude filter in link resolution. Tags are not inherited, therefore dynamically loaded filters never have a tag.
 .br
   
 .br
@@ -1578,7 +1578,7 @@ src=file.mp4 dst=file.aac:OPTFOO:gfloc:OPTBAR dst=file.264
 .br
 
 .br
-This will pass the :OPTFOO to all filters loaded between file.mp4source and file.aac destination, but not OPTBAR
+This will pass :OPTFOO to all filters loaded between file.mp4source and file.aac destination, but not OPTBAR
 .br
 Arguments are by default tracked to check if they were used by the filter chain, and a warning is thrown if this is not the case.
 .br
@@ -1595,7 +1595,7 @@ This will warn if OPTFOO is not consumed, but will not track OPTBAR.
 .SH URL templating
 .LP
 .br
-Destination URLs can be templated using the same mechanism as MPEG-DASH, where $KEYWORD$ is replaced in the template with the resolved value and $KEYWORD%%0Nd$ is replaced in the template with the resolved integer, padded with N zeros if needed. $$ is an escape for $
+Destination URLs can be templated using the same mechanism as MPEG-DASH, where $KEYWORD$ is replaced in the template with the resolved value and $KEYWORD%%0Nd$ is replaced in the template with the resolved integer, padded with up to N zeros if needed.
 .br
 KEYWORD is case sensitive, and may be present multiple times in the string. Supported KEYWORD are:
 .br
@@ -1613,9 +1613,11 @@ KEYWORD is case sensitive, and may be present multiple times in the string. Supp
 .br
 * OTHER: locates property 4CC for the given name, or property name if no 4CC matches.
 .br
-
-.br
   
+.br
+$$ is an escape for $
+.br
+
 .br
 Templating can be useful when encoding several qualities in one pass.
 .br
@@ -1663,11 +1665,11 @@ In this case, the encoder will be cloned for each video PIDs in the source, and 
 .SH Templating filter chains
 .LP
 .br
-There can be cases where the number of desired outputs depends on the source content, for example dumping a multiplex of N services into N files. When the destination involves multiplexing the input PIDs, the :cloneoption is not enough since the muxer will always accept the input PIDs.
+There can be cases where the number of desired outputs depends on the source content, for example dumping a multiplex of N services into N files. When the destination involves multiplexing the input PIDs, the :clone option is not enough since the muxer will always accept the input PIDs.
 .br
 To handle this, it is possible to use a PID property name in the sourceID of a filter with the value * or an empty value. In this case, whenever a new PID with a new value for the property is found, the filter with such sourceID will be dynamically cloned.
 .br
-Warning: This feature should only be called with a single property set to * per source ID, results are undefined otherwise.
+Warning: This feature should only be called with a single property set to * (or empty) per source ID, results are undefined otherwise.
 .br
 Example
 .br
@@ -1800,7 +1802,7 @@ Defined keywords:
 .br
 * $GSHARE: replaced by system path to GPAC shared directory (e.g. /usr/share/gpac)
 .br
-* $GJS: replaced by the first path from global share directory and set through .I -js-dirs that contains the file name following the macro, e.g. $GJS/source.js
+* $GJS: replaced by the first path from global share directory and paths set through .I -js-dirs that contains the file name following the macro, e.g. $GJS/source.js
 .br
 * $GLANG: replaced by the global config language option .I -lang
 .br
@@ -3940,7 +3942,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev359-gdcd7c11e5-master
+build: 1.1.0-DEV-rev360-g6680ea709-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2414,7 +2414,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev359-gdcd7c11e5-master
+build: 1.1.0-DEV-rev360-g6680ea709-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4client.1
@@ -261,7 +261,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit http://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev359-gdcd7c11e5-master
+build: 1.1.0-DEV-rev360-g6680ea709-master
 .br
 Copyright: (c) 2000-2020 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

src/filter_core/filter.c
@@ -163,6 +163,8 @@ char *gf_filter_get_dst_name(GF_Filter *filter)
 /*push arguments from source and dest into the final argument string. We strip the following
 - FID: will be inherited from last explicit filter in the chain
 - SID: is cloned during the resolution while loading the filter chain
+- TAG: TAG is never inherited
+- local options: by definition these options only apply to the loaded filter, and are never inherited
 - user-assigned PID properties on destination (they only apply after the destination, not in the new chain).
 
 Note that user-assigned PID properties assigned on source are inherited so that
@@ -181,9 +183,15 @@ static void filter_push_args(GF_FilterSession *fsess, char **out_args, char *in_
 		char *sep = strchr(in_args, fsess->sep_args);
 		if (sep) sep[0] = 0;
 
+		if (!strncmp(in_args, "gfloc", 5) && (!in_args[5] || (in_args[5]==fsess->sep_args))) {
+			if (sep) sep[0] = fsess->sep_args;
+			return;
+		}
 		if (!strncmp(in_args, "FID", 3) && (in_args[3]==fsess->sep_name)) {
 		}
 		else if (!strncmp(in_args, "SID", 3) && (in_args[3]==fsess->sep_name)) {
+		}
+		else if (!strncmp(in_args, "TAG", 3) && (in_args[3]==fsess->sep_name)) {
 		}
 		else if (!is_src && (in_args[0]==fsess->sep_frag)) {
 
@@ -205,7 +213,6 @@ static void filter_push_args(GF_FilterSession *fsess, char **out_args, char *in_
 			prev_is_db_sep = GF_TRUE;
 		}
 	}
-
 }
 
 GF_Filter *gf_filter_new(GF_FilterSession *fsess, const GF_FilterRegister *freg, const char *src_args, const char *dst_args, GF_FilterArgType arg_type, GF_Err *err, GF_Filter *multi_sink_target, Bool is_dynamic_filter)
@@ -1488,11 +1495,6 @@ skip_date:
 				found = GF_TRUE;
 				internal_arg = GF_TRUE;
 			}
-			//generic encoder load
-			else if (!strcmp("c", szArg)) {
-				found = GF_TRUE;
-				internal_arg = GF_TRUE;
-			}
 			//filter name
 			else if (!strcmp("N", szArg)) {
 				if ((arg_type==GF_FILTER_ARG_EXPLICIT_SINK) || (arg_type==GF_FILTER_ARG_EXPLICIT) || (arg_type==GF_FILTER_ARG_EXPLICIT_SOURCE)) {
@@ -1501,8 +1503,15 @@ skip_date:
 				found = GF_TRUE;
 				internal_arg = GF_TRUE;
 			}
-			//prefered registry to use
-			else if (!strcmp("gfreg", szArg)) {
+			//internal options, nothing to do here
+			else if (
+				//generic encoder load
+				!strcmp("c", szArg)
+				//prefered registry to use
+				|| !strcmp("gfreg", szArg)
+				//non inherited options
+				|| !strcmp("gfloc", szArg)
+			) {
 				found = GF_TRUE;
 				internal_arg = GF_TRUE;
 			}