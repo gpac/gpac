gpac/gpac

commit 2c660c14ba7084a1ce916b917dfdd9c72ab3d1d7

added ttml track export

src/media_tools/media_export.c
@@ -1181,8 +1181,8 @@ static GF_Err gf_media_export_filters(GF_MediaExporter *dumper)
 			case GF_CODECID_SUBS_TEXT:
 			case GF_CODECID_SUBS_XML:
 			case GF_CODECID_SIMPLE_TEXT:
-				//szExt[0] = 0;
-				strcpy(szExt, "*");
+				//use dynamic extension
+				szExt[0] = 0;
 				break;
 			}
 			break;
@@ -1277,6 +1277,8 @@ static GF_Err gf_media_export_filters(GF_MediaExporter *dumper)
 				e |= gf_dynstrcat(&args, ":ext=", NULL);
 				e |= gf_dynstrcat(&args, szExt, NULL);
 			}
+		} else if (dumper->trackID && !strlen(szExt) ) {
+			e |= gf_dynstrcat(&args, ":dynext", NULL);
 		}
 		if (e) {
 			GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("[Exporter] Cannot load arguments for output file dumper\n"));

include/gpac/xml.h
@@ -297,6 +297,11 @@ u32 gf_xml_dom_get_root_nodes_count(GF_DOMParser *parser);
 */
 GF_XMLNode *gf_xml_dom_get_root_idx(GF_DOMParser *parser, u32 idx);
 
+/*! Gets the root node of the document and assign the parser root to NULL.
+\param parser the DOM parser to use
+\return the root element at the given index, or NULL if error. The element must be freed by the caller
+*/
+GF_XMLNode *gf_xml_dom_detach_root(GF_DOMParser *parser);
 
 /*! Serialize a node to a string
 \param node the node to flush

src/export.cpp
@@ -467,6 +467,7 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_xml_dom_parse_string) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_xml_dom_get_root_nodes_count) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_xml_dom_get_root_idx) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_xml_dom_detach_root) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_xml_parse_bit_sequence) )
 #endif
 

src/filters/write_generic.c
@@ -26,6 +26,8 @@
 #include <gpac/filters.h>
 #include <gpac/constants.h>
 #include <gpac/bitstream.h>
+#include <gpac/xml.h>
+#include <gpac/base_coding.h>
 
 #include <gpac/internal/isomedia_dev.h>
 
@@ -71,6 +73,9 @@ typedef struct
 	Bool dash_mode;
 
 	u64 first_dts_plus_one;
+
+	Bool ttml_agg;
+	GF_XMLNode *ttml_root;
 } GF_GenDumpCtx;
 
 
@@ -238,11 +243,7 @@ GF_Err writegen_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remo
 			gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_MIME, &PROP_STRING(mimetype) );
 
 		if (!ctx->frame) {
-			p = gf_filter_pid_get_property(pid, GF_PROP_PID_NB_FRAMES);
-			if (!p || (p->value.uint>1)) {
-				GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("Subtitles re-assembling is not supported yet\n"));
-				return GF_NOT_SUPPORTED;
-			}
+			ctx->ttml_agg = GF_TRUE;
 		} else {
 			ctx->split = GF_TRUE;
 		}
@@ -585,10 +586,305 @@ static void writegen_write_wav_header(GF_GenDumpCtx *ctx)
 	gf_filter_pck_send(dst_pck);
 }
 
+static GF_XMLNode *ttml_get_div(GF_XMLNode *root)
+{
+	u32 i=0;
+	GF_XMLNode *child;
+	while (root && (child = gf_list_enum(root->content, &i)) ) {
+		GF_XMLNode *div;
+		if (child->type) continue;
+		if (!strcmp(child->name, "div")) return child;
+		div = ttml_get_div(child);
+		if (div) return div;
+	}
+	return NULL;
+}
+
+static GF_XMLAttribute *ttml_get_attr(GF_XMLNode *node, char *name)
+{
+	u32 i=0;
+	GF_XMLAttribute *att;
+	while (node && (att = gf_list_enum(node->attributes, &i)) ) {
+		if (!strcmp(att->name, name)) return att;
+	}
+	return NULL;
+}
+static Bool ttml_same_attr(GF_XMLNode *n1, GF_XMLNode *n2)
+{
+	u32 i=0;
+	GF_XMLAttribute *att1;
+	while ( (att1 = gf_list_enum(n1->attributes, &i))) {
+		Bool found = GF_FALSE;
+		u32 j=0;
+		GF_XMLAttribute *att2;
+		while ( (att2 = gf_list_enum(n2->attributes, &j))) {
+			if (!strcmp(att1->name, att2->name) && !strcmp(att1->value, att2->value)) {
+				found = GF_TRUE;
+				break;
+			}
+		}
+		if (!found) return GF_FALSE;
+	}
+	return GF_TRUE;
+}
+
+static GF_Err ttml_embed_data(GF_XMLNode *node, u8 *aux_data, u32 aux_data_size, u8 *subs_data, u32 subs_data_size)
+{
+	u32 i=0;
+	GF_XMLNode *child;
+	u32 subs_idx=0;
+	GF_XMLAttribute *att, *type;
+	Bool is_source = GF_FALSE;
+	Bool has_src_att = GF_FALSE;
+	if (!node || node->type) return GF_BAD_PARAM;
+
+	if (!strcmp(node->name, "source")) {
+		is_source = GF_TRUE;
+	}
+
+	while ((att = gf_list_enum(node->attributes, &i))) {
+		char *sep, *fext;
+		if (strcmp(att->name, "src")) continue;
+		has_src_att = GF_TRUE;
+		if (strncmp(att->value, "urn:", 4)) continue;
+		sep = strrchr(att->value, ':');
+		if (!sep) continue;
+		sep++;
+		fext = gf_file_ext_start(sep);
+		if (fext) fext[0] = 0;
+		subs_idx = atoi(sep);
+		if (fext) fext[0] = '.';
+		if (!subs_idx || ((subs_idx-1) * 14 > subs_data_size)) {
+			subs_idx = 0;
+			continue;
+		}
+		break;
+	}
+	if (subs_idx) {
+		GF_XMLNode *data;
+		u32 subs_offset = 0;
+		u32 subs_size = 0;
+		u32 idx = 1;
+		//fetch subsample info
+		for (i=0; i<subs_data_size; i+=14) {
+			u32 a_subs_size = subs_data[i+4];
+			a_subs_size <<= 8;
+			a_subs_size |= subs_data[i+5];
+			a_subs_size <<= 8;
+			a_subs_size |= subs_data[i+6];
+			a_subs_size <<= 8;
+			a_subs_size |= subs_data[i+7];
+			if (idx==subs_idx) {
+				subs_size = a_subs_size;
+				break;
+			}
+			subs_offset += a_subs_size;
+			idx++;
+		}
+		if (!subs_size || (subs_offset + subs_size > aux_data_size)) {
+			if (!subs_size) {
+				GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("No subsample with index %u in sample\n", subs_idx));
+			} else {
+				GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("Corrupted subsample %u info, size %u offset %u but sample size %u\n", subs_idx, subs_size, subs_offset, aux_data_size));
+			}
+			return GF_NON_COMPLIANT_BITSTREAM;
+		}
+
+		//remove src attribute
+		gf_list_del_item(node->attributes, att);
+		if (att->name) gf_free(att->name);
+		if (att->value) gf_free(att->value);
+		gf_free(att);
+
+		//create a source node
+		if (!is_source) {
+			GF_XMLNode *s;
+			GF_SAFEALLOC(s, GF_XMLNode);
+			if (!s) return GF_OUT_OF_MEM;
+			s->attributes = gf_list_new();
+			s->content = gf_list_new();
+			s->name = gf_strdup("source");
+			gf_list_add(node->content, s);
+			if (!s->name || !s->content || !s->attributes) return GF_OUT_OF_MEM;
+			//move @type to source
+			type = ttml_get_attr(node, "type");
+			if (type) {
+				gf_list_del_item(node->attributes, type);
+				gf_list_add(s->attributes, type);
+			}
+			node = s;
+		}
+
+		//create a data node
+		GF_SAFEALLOC(data, GF_XMLNode);
+		if (!data) return GF_OUT_OF_MEM;
+		data->attributes = gf_list_new();
+		data->content = gf_list_new();
+		data->name = gf_strdup("data");
+		gf_list_add(node->content, data);
+		if (!data->name || !data->content || !data->attributes) return GF_OUT_OF_MEM;
+		//move @type to data
+		type = ttml_get_attr(node, "type");
+		if (type) {
+			gf_list_del_item(node->attributes, type);
+			gf_list_add(data->attributes, type);
+		}
+		//base64 encode in a child
+		GF_SAFEALLOC(node, GF_XMLNode)
+		if (!node) return GF_OUT_OF_MEM;
+		node->type = GF_XML_TEXT_TYPE;
+		node->name = gf_malloc(sizeof(u8) * subs_size * 2);
+		if (!node->name) {
+			gf_free(node);
+			return GF_OUT_OF_MEM;
+		}
+		subs_size = gf_base64_encode(aux_data + subs_offset, subs_size, (u8*) node->name, subs_size * 2);
+		node->name[subs_size] = 0;
+		return gf_list_add(data->content, node);
+	}
+	//src was present but not an embedded data, do not recurse
+	if (has_src_att) return GF_OK;
+
+	i=0;
+	while ((child = gf_list_enum(node->content, &i))) {
+		if (child->type) continue;
+		ttml_embed_data(child, aux_data, aux_data_size, subs_data, subs_data_size);
+	}
+	return GF_OK;
+}
+
+static GF_Err writegen_push_ttml(GF_GenDumpCtx *ctx, char *data, u32 data_size, GF_FilterPacket *in_pck)
+{
+	const GF_PropertyValue *subs = gf_filter_pck_get_property(in_pck, GF_PROP_PCK_SUBS);
+	char *ttml_text = NULL;
+	GF_Err e = GF_OK;
+	u32 ttml_text_size;
+	GF_DOMParser *dom;
+	u32 txt_size;
+	u32 i;
+	GF_XMLNode *div_global, *div_pck, *root_pck, *p_global, *p_pck;
+
+	if (subs) {
+		if (subs->value.data.size < 14) return GF_NON_COMPLIANT_BITSTREAM;
+		txt_size = subs->value.data.ptr[4];
+		txt_size <<= 8;
+		txt_size |= subs->value.data.ptr[5];
+		txt_size <<= 8;
+		txt_size |= subs->value.data.ptr[6];
+		txt_size <<= 8;
+		txt_size |= subs->value.data.ptr[7];
+		if (txt_size>data_size) return GF_NON_COMPLIANT_BITSTREAM;
+	} else {
+		txt_size = data_size;
+	}
+	ttml_text_size = txt_size + 2;
+	ttml_text = gf_malloc(sizeof(char) * ttml_text_size);
+	if (!ttml_text) return GF_OUT_OF_MEM;
+
+	memcpy(ttml_text, data, txt_size);
+	ttml_text[txt_size] = 0;
+	ttml_text[txt_size+1] = 0;
+
+	dom = gf_xml_dom_new();
+	if (!dom) return GF_OUT_OF_MEM;
+	e = gf_xml_dom_parse_string(dom, ttml_text);
+	if (e) goto exit;
+
+
+	root_pck = gf_xml_dom_get_root(dom);
+
+	//subsamples, replace all data embedded as subsample with base64 encoding
+	if (subs && root_pck) {
+		e = ttml_embed_data(root_pck, data+txt_size, data_size-txt_size, subs->value.data.ptr+14, subs->value.data.size-14);
+		if (e) goto exit;
+	}
+
+	//merge input doc - for now we only support single div
+	div_global = ttml_get_div(ctx->ttml_root);
+	div_pck = ttml_get_div(root_pck);
+
+	if (!div_global) {
+		if (ctx->ttml_root) gf_xml_dom_node_del(ctx->ttml_root);
+		ctx->ttml_root = gf_xml_dom_detach_root(dom);
+		goto exit;
+	}
+	if (!div_pck)
+		goto exit;
+
+
+	i=0;
+	while ( (p_pck = gf_list_enum(div_pck->content, &i)) ) {
+		s32 idx;
+		GF_XMLNode *txt;
+		u32 j=0;
+		Bool found = GF_FALSE;
+		if (p_pck->type) continue;
+		if (strcmp(p_pck->name, "p")) continue;
+		while ( (p_global = gf_list_enum(div_global->content, &j)) ) {
+			if (p_global->type) continue;
+			if (strcmp(p_global->name, "p")) continue;
+
+			if (ttml_same_attr(p_pck, p_global)) {
+				found = GF_TRUE;
+				break;
+			}
+		}
+		if (found) continue;
+
+		//insert this p - if last entry in global div is text, insert before
+		idx=0;
+		txt = gf_list_last(div_global->content);
+		if (txt && txt->type) {
+			idx = gf_list_count(div_global->content) - 1;
+		} else {
+			idx = gf_list_count(div_global->content);
+		}
+
+		i--;
+		gf_list_rem(div_pck->content, i);
+		if (i) {
+			txt = gf_list_get(div_pck->content, i-1);
+			if (txt->type) {
+				i--;
+				gf_list_rem(div_pck->content, i);
+				gf_list_insert(div_global->content, txt, idx);
+				idx++;
+			}
+		}
+		gf_list_insert(div_global->content, p_pck, idx);
+
+	}
+
+exit:
+	if (ttml_text) gf_free(ttml_text);
+	gf_xml_dom_del(dom);
+	return e;
+}
+
+static GF_Err writegen_flush_ttml(GF_GenDumpCtx *ctx)
+{
+	char *data;
+	u8 *output;
+	u32 size;
+	GF_FilterPacket *pck;
+	if (!ctx->ttml_root) return GF_OK;
+
+	data = gf_xml_dom_serialize(ctx->ttml_root, GF_FALSE);
+	if (!data) return GF_OK;
+	size = (u32) strlen(data);
+	pck = gf_filter_pck_new_alloc(ctx->opid, size, &output);
+	memcpy(output, data, size);
+	gf_free(data);
+	gf_filter_pck_set_framing(pck, GF_TRUE, GF_TRUE);
+	gf_xml_dom_node_del(ctx->ttml_root);
+	ctx->ttml_root = NULL;
+	return gf_filter_pck_send(pck);
+}
+
 GF_Err writegen_process(GF_Filter *filter)
 {
 	GF_GenDumpCtx *ctx = gf_filter_get_udta(filter);
-	GF_FilterPacket *pck, *dst_pck;
+	GF_FilterPacket *pck, *dst_pck = NULL;
 	char *data;
 	u32 pck_size;
 	Bool do_abort = GF_FALSE;
@@ -599,6 +895,7 @@ GF_Err writegen_process(GF_Filter *filter)
 	if (!pck || !ctx->codecid) {
 		if (gf_filter_pid_is_eos(ctx->ipid)) {
 			if (ctx->is_wav) writegen_write_wav_header(ctx);
+			else if (ctx->ttml_agg) writegen_flush_ttml(ctx);
 			gf_filter_pid_set_eos(ctx->opid);
 			return GF_EOS;
 		}
@@ -675,6 +972,14 @@ GF_Err writegen_process(GF_Filter *filter)
 		gf_filter_pck_send(dst_pck);
 		ctx->nb_bytes += 44;
 		return GF_OK;
+	} else if (ctx->ttml_agg) {
+		GF_Err e = writegen_push_ttml(ctx, data, pck_size, pck);
+		ctx->first = GF_FALSE;
+		if (e) {
+			gf_filter_pid_drop_packet(ctx->ipid);
+			return e;
+		}
+		goto no_output;
 	} else {
 		dst_pck = gf_filter_pck_new_ref(ctx->opid, 0, 0, pck);
 	}
@@ -690,10 +995,11 @@ GF_Err writegen_process(GF_Filter *filter)
 		ctx->first = GF_FALSE;
 	}
 
-	gf_filter_pck_send(dst_pck);
 	gf_filter_pck_set_seek_flag(dst_pck, 0);
+	gf_filter_pck_send(dst_pck);
 	ctx->nb_bytes += pck_size;
 
+no_output:
 	if (ctx->exporter) {
 		u32 timescale = gf_filter_pck_get_timescale(pck);
 		u64 ts = gf_filter_pck_get_dts(pck);
@@ -1055,6 +1361,8 @@ void writegen_finalize(GF_Filter *filter)
 {
 	GF_GenDumpCtx *ctx = gf_filter_get_udta(filter);
 	if (ctx->bs) gf_bs_del(ctx->bs);
+	if (ctx->ttml_root)
+		gf_xml_dom_node_del(ctx->ttml_root);
 }
 
 GF_FilterRegister GenDumpRegister = {

src/utils/xml_parser.c
@@ -1791,14 +1791,17 @@ void gf_xml_dom_del(GF_DOMParser *parser)
 	gf_free(parser);
 }
 
-#if 0 //unused
+GF_EXPORT
 GF_XMLNode *gf_xml_dom_detach_root(GF_DOMParser *parser)
 {
-	GF_XMLNode *root = parser->root;
-	parser->root = NULL;
+	GF_XMLNode *root;
+	if (!parser)
+		return NULL;
+	root = parser->root;
+	gf_list_del_item(parser->root_nodes, root);
+	parser->root = gf_list_get(parser->root_nodes, 0);
 	return root;
 }
-#endif
 
 static void dom_on_progress(void *cbck, u64 done, u64 tot)
 {
@@ -1881,8 +1884,9 @@ static void gf_xml_dom_node_serialize(GF_XMLNode *node, Bool content_only, char 
 
 #define SET_STRING(v)	\
 	vlen = (u32) strlen(v);	\
-	if (vlen+ (*size) >= (*alloc_size)) {	\
+	if (vlen + (*size) >= (*alloc_size)) {	\
 		(*alloc_size) += 1024;	\
+		if (vlen + (*size) >= (*alloc_size)) (*alloc_size) = vlen + (*size) + 1;\
 		(*str) = gf_realloc((*str), (*alloc_size));	\
 		(*str)[(*size)] = 0;	\
 	}	\

commit 6f959b6e46930422d6c807b0f8b6b372e4c85f37

fixed bug introduced by 2c660c14

src/media_tools/media_export.c
@@ -1090,6 +1090,7 @@ static GF_Err gf_media_export_filters(GF_MediaExporter *dumper)
 	u32 sample_count=0;
 	Bool skip_write_filter = GF_FALSE;
 	Bool ext_forced = GF_FALSE;
+	Bool use_dynext = GF_FALSE;
 
 	args = NULL;
 	strcpy(szExt, "");
@@ -1183,6 +1184,7 @@ static GF_Err gf_media_export_filters(GF_MediaExporter *dumper)
 			case GF_CODECID_SIMPLE_TEXT:
 				//use dynamic extension
 				szExt[0] = 0;
+				use_dynext = GF_TRUE;
 				break;
 			}
 			break;
@@ -1277,7 +1279,7 @@ static GF_Err gf_media_export_filters(GF_MediaExporter *dumper)
 				e |= gf_dynstrcat(&args, ":ext=", NULL);
 				e |= gf_dynstrcat(&args, szExt, NULL);
 			}
-		} else if (dumper->trackID && !strlen(szExt) ) {
+		} else if (dumper->trackID && use_dynext) {
 			e |= gf_dynstrcat(&args, ":dynext", NULL);
 		}
 		if (e) {