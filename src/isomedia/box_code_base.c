3a3e8637f070f96174211d62e4bb3f1dc2ac9b4c
jeanlf <jeanlf@gpac.io>
2021-02-03 11:45:18 +0100

    partial revert of 4529c600

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -3087,7 +3087,6 @@ GF_Err mdia_box_write(GF_Box *s, GF_BitStream *bs)
 
 GF_Err mdia_box_size(GF_Box *s)
 {
-	GF_Box *elng;
 	u32 pos = 0;
 	GF_MediaBox *ptr = (GF_MediaBox *)s;
 	//Header first
@@ -3095,10 +3094,12 @@ GF_Err mdia_box_size(GF_Box *s)
 	//then handler
 	gf_isom_check_position(s, (GF_Box*)ptr->handler, &pos);
 
-	//elng before info for CMAF info
-	elng = gf_isom_box_find_child(ptr->child_boxes, GF_ISOM_BOX_TYPE_ELNG);
+#if 0
+	//elng before info for CMAF info - we deactiveate for now, no specific errors raised and CMAF should not impose any order
+	GF_Box *elng = gf_isom_box_find_child(ptr->child_boxes, GF_ISOM_BOX_TYPE_ELNG);
 	if (elng)
 		gf_isom_check_position(s, elng, &pos);
+#endif
 
 	//then info
 	gf_isom_check_position(s, (GF_Box*)ptr->information, &pos);
