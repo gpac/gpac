70010007e114f529c9f21c9bbb6f3715f6738bc5
jeanlf <jeanlf@gpac.io>
2021-02-08 11:07:44 +0100

    fixed bug checking for ignored unused args

--- src/filter_core/filter_session.c
+++ src/filter_core/filter_session.c
@@ -2236,16 +2236,27 @@ void gf_fs_print_unused_args(GF_FilterSession *fsess, const char *ignore_args)
 	u32 argtype;
 
 	while (1) {
+		Bool found = GF_FALSE;
+		const char *loc_arg;
 		if (gf_fs_enum_unmapped_options(fsess, &idx, &argname, &argtype)==GF_FALSE)
 			break;
-		if (ignore_args) {
-			char *match = strstr(ignore_args, argname);
-			if (match) {
-				u32 len = (u32) strlen(argname);
-				if (!match[len] || (match[len]==','))
-					continue;
+
+		loc_arg = ignore_args;
+		while (loc_arg) {
+			u32 len;
+			char *sep;
+			char *match = strstr(loc_arg, argname);
+			if (!match) break;
+			len = (u32) strlen(argname);
+			if (!match[len] || (match[len]==',')) {
+				found = GF_TRUE;
+				break;
 			}
+			sep = strchr(loc_arg, ',');
+			if (!sep) break;
+			loc_arg = sep+1;
 		}
+		if (found) continue;;
 
 		GF_LOG(GF_LOG_ERROR, GF_LOG_APP, ("Arg %s set but not used\n", argname));
 	}
