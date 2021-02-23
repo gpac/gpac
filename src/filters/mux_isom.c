gpac/gpac

commit 54f21e366c3991850273c6d6d833fc920357df4a

fixed #1686

src/filters/mux_isom.c
@@ -330,6 +330,8 @@ typedef struct
 	u32 next_file_idx;
 	const char *next_file_suffix;
 
+	u32 nb_src_tracks;
+
 	//for route scheduling
 	u64 min_cts_plus_one, next_seg_start;
 	u64 min_cts_next_frag;
@@ -1096,7 +1098,7 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 
 		p = gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_MUX_INDEX);
 		if (p) {
-			tk_idx = p->value.uint;
+			tk_idx = ctx->nb_src_tracks + p->value.uint;
 		}
 
 		if (ctx->keep_utc) {
@@ -5620,6 +5622,7 @@ static GF_Err mp4_mux_initialize(GF_Filter *filter)
 			return GF_BAD_PARAM;
 		}
 		ctx->owns_mov = GF_FALSE;
+		ctx->nb_src_tracks = gf_isom_get_track_count(ctx->file);
 	} else {
 		u32 open_mode = GF_ISOM_OPEN_WRITE;
 		ctx->owns_mov = GF_TRUE;

src/isomedia/box_code_base.c
@@ -3670,6 +3670,7 @@ GF_Err moov_on_child_box(GF_Box *s, GF_Box *a)
 	case GF_ISOM_BOX_TYPE_TRAK:
 		//set our pointer to this obj
 		((GF_TrackBox *)a)->moov = ptr;
+		((GF_TrackBox *)a)->index = gf_list_count(ptr->trackList);
 		return gf_list_add(ptr->trackList, a);
 	}
 	return GF_OK;

commit da84c99d3c5b6a6b23101c1de8c14bb0d96be903

fixed bug introduced by 54f21e3

src/filters/mux_isom.c
@@ -330,8 +330,6 @@ typedef struct
 	u32 next_file_idx;
 	const char *next_file_suffix;
 
-	u32 nb_src_tracks;
-
 	//for route scheduling
 	u64 min_cts_plus_one, next_seg_start;
 	u64 min_cts_next_frag;
@@ -1098,7 +1096,13 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 
 		p = gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_MUX_INDEX);
 		if (p) {
-			tk_idx = ctx->nb_src_tracks + p->value.uint;
+			tk_idx = p->value.uint;
+			if (!ctx->owns_mov) {
+				u32 nb_dst_tk = gf_isom_get_track_count(ctx->file);
+				if (tk_idx < nb_dst_tk) {
+					tk_idx = nb_dst_tk;
+				}
+			}
 		}
 
 		if (ctx->keep_utc) {
@@ -5622,7 +5626,6 @@ static GF_Err mp4_mux_initialize(GF_Filter *filter)
 			return GF_BAD_PARAM;
 		}
 		ctx->owns_mov = GF_FALSE;
-		ctx->nb_src_tracks = gf_isom_get_track_count(ctx->file);
 	} else {
 		u32 open_mode = GF_ISOM_OPEN_WRITE;
 		ctx->owns_mov = GF_TRUE;