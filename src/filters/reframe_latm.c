1a91f4dad49edb8240bdb2e133a4319611baae33
jeanlf <jeanlf@gpac.io>
2021-03-09 18:56:06 +0100

    fixed latm data prober wrong detection

--- src/filters/reframe_latm.c
+++ src/filters/reframe_latm.c
@@ -584,18 +584,29 @@ static void latm_dmx_finalize(GF_Filter *filter)
 static const char *latm_dmx_probe_data(const u8 *data, u32 size, GF_FilterProbeScore *score)
 {
 	u32 nb_frames=0;
+	u32 nb_skip=0;
 	GF_M4ADecSpecInfo acfg;
 	GF_BitStream *bs = gf_bs_new(data, size, GF_BITSTREAM_READ);
 	while (1) {
 		u32 nb_skipped = 0;
 		if (!latm_dmx_sync_frame_bs(bs, &acfg, 0, NULL, &nb_skipped)) break;
-		if (nb_skipped)
-			nb_frames=0;
+		if (! GF_M4ASampleRates[acfg.base_sr_index]) {
+			nb_frames = 0;
+			break;
+		}
+
+		if (nb_skipped) {
+			if (nb_skip) {
+				nb_frames = 0;
+				break;
+			}
+			nb_skip++;
+		}
 		nb_frames++;
 	}
 	gf_bs_del(bs);
 	if (nb_frames>=2) {
-		*score = GF_FPROBE_SUPPORTED;
+		*score = nb_skip ? GF_FPROBE_MAYBE_SUPPORTED : GF_FPROBE_SUPPORTED;
 		return "audio/aac+latm";
 	}
 	return NULL;
