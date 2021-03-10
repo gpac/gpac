gpac/gpac

commit b59fcfacf909a267156ce6b0d97532b1d7093916

fixed #1695

src/filters/inspect.c
@@ -1564,7 +1564,7 @@ static void finalize_dump(GF_InspectCtx *ctx, u32 streamtype, Bool concat)
 		}
 		gf_fclose(pctx->tmp);
 		if (ctx->xml)
-			gf_fprintf(ctx->dump, "</PIDInspect>");
+			gf_fprintf(ctx->dump, "</PIDInspect>\n");
 		pctx->tmp = NULL;
 	}
 }
@@ -1865,11 +1865,12 @@ static void inspect_dump_property(GF_InspectCtx *ctx, FILE *dump, u32 p4cc, cons
 		if (ctx->dtype)
 			gf_fprintf(dump, " type=\"%s\"", gf_props_get_type_name(att->type) );
 			
-		if (pname && strchr(pname, ' ')) {
+		if (pname && (strchr(pname, ' ') || strchr(pname, ':'))) {
 			u32 i=0, k;
 			char *pname_no_space = gf_strdup(pname);
 			while (pname_no_space[i]) {
 				if (pname_no_space[i]==' ') pname_no_space[i]='_';
+				if (pname_no_space[i]==':') pname_no_space[i]='_';
 				i++;
 			}
 
@@ -2205,55 +2206,89 @@ static void inspect_dump_mpeg124(PidCtx *pctx, char *data, u32 size, FILE *dump)
 }
 #endif
 
-static void inspect_dump_tmcd(GF_InspectCtx *ctx, PidCtx *pctx, char *data, u32 size, FILE *dump)
+static void inspect_format_tmcd_internal(const u8 *data, u32 size, u32 tmcd_flags, u32 tc_num, u32 tc_den, u32 tmcd_fpt, char szFmt[100], GF_BitStream *bs, Bool force_ff, FILE *dump)
 {
-	u32 h, m, s, f;
+	u32 h, m, s, f, value;
 	Bool neg=GF_FALSE;
+	Bool parse_fmt = 1;
+	Bool is_drop = GF_FALSE;
+	GF_BitStream *loc_bs = NULL;
 
-	if (!pctx->tmcd_rate.den || !pctx->tmcd_rate.num)
+	if (szFmt)
+		szFmt[0] = 0;
+
+	if (!tc_num || !tc_den)
 		return;
-	gf_bs_reassign_buffer(pctx->bs, data, size);
 
-	u32 value = gf_bs_read_u32(pctx->bs);
-	gf_bs_seek(pctx->bs, 0);
-	gf_fprintf(dump, "<TimeCode");
+	if (!bs) {
+		loc_bs = gf_bs_new(data, size, GF_BITSTREAM_READ);
+		bs = loc_bs;
+	} else {
+		gf_bs_reassign_buffer(bs, data, size);
+	}
 
-	if (ctx->fftmcd || (pctx->tmcd_flags & 0x00000008)) {
+	value = gf_bs_read_u32(bs);
+	gf_bs_seek(bs, 0);
+
+	if (tmcd_flags & 0x00000001)
+		is_drop = GF_TRUE;
+
+	if (tmcd_flags & 0x00000004)
+		neg = GF_TRUE;
+
+	if (dump)
+		gf_fprintf(dump, "<TimeCode");
+
+	if (!force_ff && !(tmcd_flags & 0x00000008)) {
+		h = gf_bs_read_u8(bs);
+		neg = gf_bs_read_int(bs, 1);
+		m = gf_bs_read_int(bs, 7);
+		s = gf_bs_read_u8(bs);
+		f = gf_bs_read_u8(bs);
+
+		if (tmcd_fpt && (f > tmcd_fpt))
+			parse_fmt = 2;
+		else if ((m>=60) || (s>=60))
+			parse_fmt = 2;
+		else
+			parse_fmt = GF_FALSE;
+	}
+
+	if (parse_fmt) {
 		u64 nb_secs, nb_frames = value;
-		Bool is_drop = GF_FALSE;
-		if (!ctx->fftmcd && pctx->tmcd_fpt)
-			nb_frames *= pctx->tmcd_fpt;
+		neg = GF_FALSE;
+		if (parse_fmt==2) force_ff = GF_TRUE;
 
-		if (ctx->fftmcd) {
-			u32 fps = pctx->tmcd_rate.num / pctx->tmcd_rate.den;
-			if (fps * pctx->tmcd_rate.den != pctx->tmcd_rate.num)
-				is_drop = GF_TRUE;
+		if (!force_ff && tmcd_fpt)
+			nb_frames *= tmcd_fpt;
+
+		if (is_drop && tc_num) {
+			u32 tc_drop_frames, min_10;
+
+			nb_secs = nb_frames;
+			nb_secs *= tc_den;
+			nb_secs /= tc_num;
+
+			m = (u32) (nb_secs/60);
+
+			min_10 = m / 10;
+			m-= min_10;
+
+			tc_drop_frames = m*2;
+			nb_frames += tc_drop_frames;
 		}
-		else if (pctx->tmcd_flags & 0x00000001) {
-			is_drop = GF_TRUE;
-		}
 
-		if (is_drop) {
-			u32 drop_frames, frame_base;
+		if (!tmcd_fpt) tmcd_fpt = 30;
 
-			frame_base = 100 * pctx->tmcd_rate.num;
-			frame_base /= pctx->tmcd_rate.den;
-
-			drop_frames = (u32) (nb_frames / frame_base);
-			nb_frames -= 3*drop_frames;
-		}
-
-		nb_secs = nb_frames * pctx->tmcd_rate.den / pctx->tmcd_rate.num;
-
-		gf_fprintf(dump, " counter=\"%d\"", value);
+		nb_secs = nb_frames / tmcd_fpt;
 		h = (u32) nb_secs/3600;
 		m = (u32) (nb_secs/60 - h*60);
 		s = (u32) (nb_secs - m*60 - h*3600);
 
-		nb_secs *= pctx->tmcd_rate.num;
-		nb_secs /= pctx->tmcd_rate.den;
+		nb_secs *= tmcd_fpt;
+
 		f = (u32) (nb_frames - nb_secs);
-		if (pctx->tmcd_fpt && (f==pctx->tmcd_fpt)) {
+		if (tmcd_fpt && (f==tmcd_fpt)) {
 			f = 0;
 			s++;
 			if (s==60) {
@@ -2265,14 +2300,25 @@ static void inspect_dump_tmcd(GF_InspectCtx *ctx, PidCtx *pctx, char *data, u32 
 				}
 			}
 		}
-	} else {
-		h = gf_bs_read_u8(pctx->bs);
-		neg = gf_bs_read_int(pctx->bs, 1);
-		m = gf_bs_read_int(pctx->bs, 7);
-		s = gf_bs_read_u8(pctx->bs);
-		f = gf_bs_read_u8(pctx->bs);
 	}
-	gf_fprintf(dump, " time=\"%s%02d:%02d:%02d:%02d\"/>\n", neg ? "-" : "", h, m, s, f);
+	if (dump)
+		gf_fprintf(dump, " time=\"%s%02d:%02d:%02d%c%02d\"/>\n", neg ? "-" : "", h, m, s, is_drop ? ';' : ':', f);
+	else if (szFmt)
+		sprintf(szFmt, "%s%02d:%02d:%02d%c%02d", neg ? "-" : "", h, m, s, is_drop ? ';' : ':', f);
+
+	if (loc_bs) gf_bs_del(loc_bs);
+}
+
+
+GF_EXPORT
+void gf_inspect_format_timecode(const u8 *data, u32 size, u32 tmcd_flags, u32 tc_num, u32 tc_den, u32 tmcd_fpt, char szFmt[100])
+{
+	inspect_format_tmcd_internal(data, size, tmcd_flags, tc_num, tc_den, tmcd_fpt, szFmt, NULL, GF_FALSE, NULL);
+}
+
+static void inspect_dump_tmcd(GF_InspectCtx *ctx, PidCtx *pctx, const u8 *data, u32 size, FILE *dump)
+{
+	inspect_format_tmcd_internal(data, size, pctx->tmcd_flags, pctx->tmcd_rate.num, pctx->tmcd_rate.den, pctx->tmcd_flags, NULL, pctx->bs, ctx->fftmcd, dump);
 }
 
 static void inspect_dump_vpx(GF_InspectCtx *ctx, FILE *dump, u8 *ptr, u64 frame_size, Bool dump_crc, PidCtx *pctx, u32 vpversion)
@@ -3126,7 +3172,7 @@ static void inspect_dump_pid(GF_InspectCtx *ctx, FILE *dump, GF_FilterPid *pid, 
 			InspectLogCbk lcbk;
 
 			gf_fprintf(dump, ">\n");
-			gf_fprintf(dump, "<MPEG4AudioConfiguration");
+			gf_fprintf(dump, "<MPEG4AudioConfiguration ");
 
 			if (!pctx->bs)
 				pctx->bs = gf_bs_new(dsi->value.data.ptr, dsi->value.data.size, GF_BITSTREAM_READ);
@@ -3430,7 +3476,7 @@ static GF_Err inspect_config_input(GF_Filter *filter, GF_FilterPid *pid, Bool is
 	if (! ctx->interleave && !pctx->tmp && ctx->dump) {
 		pctx->tmp = gf_file_temp(NULL);
 		if (ctx->xml)
-			gf_fprintf(ctx->dump, "<PIDInspect ID=\"%d\" name=\"%s\">\n", pctx->idx, gf_filter_pid_get_name(pid) );
+			gf_fprintf(pctx->tmp, "<PIDInspect ID=\"%d\" name=\"%s\">\n", pctx->idx, gf_filter_pid_get_name(pid) );
 	}
 
 	switch (ctx->mode) {

src/filters/reframer.c
@@ -75,7 +75,7 @@ typedef struct
 	u64 ts_at_range_end;
 
 	GF_List *pck_queue;
-	//0: not comuted, 1: computed and valid TS, 2: end of stream on pid
+	//0: not computed, 1: computed and valid TS, 2: end of stream on pid
 	u32 range_start_computed;
 	u64 range_end_reached_ts;
 	u64 prev_sap_ts;
@@ -90,8 +90,10 @@ typedef struct
 	u32 split_end;
 
 	GF_FilterPacket *split_pck;
-	GF_FilterPacket *resinsert_single_pck;
+	GF_FilterPacket *reinsert_single_pck;
 	Bool is_playing;
+
+	u32 codec_id, stream_type;
 } RTStream;
 
 typedef struct
@@ -105,7 +107,7 @@ typedef struct
 	Double speed;
 	Bool raw;
 	GF_PropStringList xs, xe;
-	Bool nosap, splitrange, xadjust;
+	Bool nosap, splitrange, xadjust, tcmdrw;
 	u32 xround;
 	Double seeksafe;
 
@@ -147,6 +149,9 @@ typedef struct
 	u32 wait_video_range_adjust;
 	Bool is_eos, has_seen_eos;
 	u32 nb_non_saps;
+
+	u32 nb_video_frames_since_start_at_range_start;
+	u32 nb_video_frames_since_start;
 } GF_ReframerCtx;
 
 static void reframer_reset_stream(GF_ReframerCtx *ctx, RTStream *st)
@@ -159,7 +164,7 @@ static void reframer_reset_stream(GF_ReframerCtx *ctx, RTStream *st)
 		gf_list_del(st->pck_queue);
 	}
 	if (st->split_pck) gf_filter_pck_unref(st->split_pck);
-	if (st->resinsert_single_pck) gf_filter_pck_unref(st->resinsert_single_pck);
+	if (st->reinsert_single_pck) gf_filter_pck_unref(st->reinsert_single_pck);
 	gf_free(st);
 }
 GF_Err reframer_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remove)
@@ -208,13 +213,16 @@ GF_Err reframer_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remo
 	}
 	st->can_split = GF_FALSE;
 	p = gf_filter_pid_get_property(pid, GF_PROP_PID_STREAM_TYPE);
-	if (p) {
-		switch (p->value.uint) {
-		case GF_STREAM_TEXT:
-			st->can_split = GF_TRUE;
-			break;
-		}
+	st->stream_type = p ? p->value.uint : 0;
+	switch (st->stream_type) {
+	case GF_STREAM_TEXT:
+		st->can_split = GF_TRUE;
+		break;
 	}
+
+	p = gf_filter_pid_get_property(pid, GF_PROP_PID_CODECID);
+	st->codec_id = p ? p->value.uint : 0;
+
 	st->needs_adjust = ctx->xadjust ? GF_TRUE : GF_FALSE;
 
 	st->tk_delay = 0;
@@ -345,6 +353,8 @@ static void reframer_load_range(GF_ReframerCtx *ctx)
 	u64 prev_frame = ctx->start_frame_idx_plus_one;
 	GF_Fraction64 prev_end;
 	char *start_date=NULL, *end_date=NULL;
+
+	ctx->nb_video_frames_since_start_at_range_start = ctx->nb_video_frames_since_start;
 
 	if (ctx->extract_mode==EXTRACT_DUR) {
 		ctx->cur_start.num += (ctx->extract_dur.num * ctx->cur_start.den) / ctx->extract_dur.den;
@@ -589,8 +599,26 @@ Bool reframer_send_packet(GF_Filter *filter, GF_ReframerCtx *ctx, RTStream *st, 
 
 	//range processing
 	if (st->ts_at_range_start_plus_one) {
+		Bool is_split = GF_FALSE;
 		s64 ts;
-		GF_FilterPacket *new_pck = gf_filter_pck_new_ref(st->opid, 0, 0, pck);
+		GF_FilterPacket *new_pck;
+
+		//tmcd, rewrite sample
+		if (ctx->tcmdrw && (st->codec_id==GF_CODECID_TMCD) && st->split_start && ctx->nb_video_frames_since_start_at_range_start) {
+			GF_BitStream *bs;
+			u32 nb_frames;
+			u8 *tcmd_data = NULL;
+			new_pck = gf_filter_pck_new_copy(st->opid, pck, &tcmd_data);
+			bs = gf_bs_new(tcmd_data, 4, GF_BITSTREAM_READ);
+			nb_frames = gf_bs_read_u32(bs);
+			gf_bs_del(bs);
+			bs = gf_bs_new(tcmd_data, 4, GF_BITSTREAM_WRITE);
+			gf_bs_seek(bs, 0);
+			gf_bs_write_u32(bs, nb_frames+ctx->nb_video_frames_since_start_at_range_start);
+			gf_bs_del(bs);
+		} else {
+			new_pck = gf_filter_pck_new_ref(st->opid, 0, 0, pck);
+		}
 		gf_filter_pck_merge_properties(pck, new_pck);
 
 		//signal chunk start boundary
@@ -678,6 +706,7 @@ Bool reframer_send_packet(GF_Filter *filter, GF_ReframerCtx *ctx, RTStream *st, 
 				gf_filter_pck_set_dts(new_pck, (u64) ts);
 			}
 		}
+		//packet was split or was re-inserted
 		if (st->split_start) {
 			u32 dur = gf_filter_pck_get_duration(pck);
 			assert(dur>st->split_start);
@@ -685,20 +714,38 @@ Bool reframer_send_packet(GF_Filter *filter, GF_ReframerCtx *ctx, RTStream *st, 
 			gf_filter_pck_set_duration(new_pck, dur);
 			st->ts_at_range_start_plus_one += st->split_start;
 			st->split_start = 0;
+			is_split = GF_TRUE;
 		}
 		//last packet and forced duration
 		if (st->split_end && (gf_list_count(st->pck_queue)==1)) {
 			gf_filter_pck_set_duration(new_pck, st->split_end);
 			st->split_end = 0;
+			is_split = GF_TRUE;
+		}
+		//packet reinserted (not split), adjust duration and store offset in split start
+		if (!is_split && st->reinsert_single_pck) {
+			u64 ndur = st->range_end_reached_ts;
+			ndur -= st->ts_at_range_start_plus_one-1;
+			gf_filter_pck_set_duration(new_pck, (u32) ndur);
+			st->split_start = (u32) ndur;
 		}
 
 		gf_filter_pck_send(new_pck);
+
 	} else {
 		gf_filter_pck_forward(pck, st->opid);
 	}
 
+
 	reframer_drop_packet(ctx, st, pck, pck_is_ref);
 	st->nb_frames++;
+
+	if (st->stream_type==GF_STREAM_VISUAL) {
+		if (st->nb_frames > ctx->nb_video_frames_since_start) {
+			ctx->nb_video_frames_since_start = st->nb_frames;
+		}
+	}
+
 	return GF_TRUE;
 }
 
@@ -746,7 +793,7 @@ void reframer_purge_queues(GF_ReframerCtx *ctx, u64 ts, u32 timescale)
 	for (i=0; i<count; i++) {
 		RTStream *st = gf_list_get(ctx->streams, i);
 		u64 ts_rescale = ts;
-		if (st->resinsert_single_pck)
+		if (st->reinsert_single_pck)
 			continue;
 
 		if (st->timescale != timescale) {
@@ -817,7 +864,7 @@ static void check_gop_split(GF_ReframerCtx *ctx)
 			}
 			//in SAP split, flush as soon as we no longer have 2 consecutive saps
 			if (!last_sap_ts) {
-				if (st->in_eos && !flush_all && !st->resinsert_single_pck) {
+				if (st->in_eos && !flush_all && !st->reinsert_single_pck) {
 					flush_all = GF_TRUE;
 				} else if (!st->all_saps) {
 					wait_for_sap = GF_TRUE;
@@ -890,7 +937,7 @@ static void check_gop_split(GF_ReframerCtx *ctx)
 			GF_FilterPacket *pck;
 			RTStream *st = gf_list_get(ctx->streams, i);
 			if (st->range_start_computed==2) continue;
-			if (st->resinsert_single_pck) continue;
+			if (st->reinsert_single_pck) continue;
 			pck = gf_list_last(st->pck_queue);
 			assert(pck);
 			ts = gf_filter_pck_get_dts(pck);
@@ -1098,11 +1145,11 @@ GF_Err reframer_process(GF_Filter *filter)
 				if (gf_filter_pid_is_eos(ipid)) {
 					//special case for PIDs with a single packet, we reinsert them at the begining of each extracted range
 					//this allows dealing with BIFS/OD/JPEG/PNG tracks
-					if (st->resinsert_single_pck) {
+					if (st->reinsert_single_pck) {
 						if (!ctx->in_range && !st->range_start_computed) {
 							st->range_start_computed = 3;
 							if (!gf_list_count(st->pck_queue)) {
-								pck = st->resinsert_single_pck;
+								pck = st->reinsert_single_pck;
 								gf_filter_pck_ref(&pck);
 								gf_list_add(st->pck_queue, pck);
 								if (!ctx->is_range_extraction) {
@@ -1175,10 +1222,10 @@ GF_Err reframer_process(GF_Filter *filter)
 				//keep ref to first packet until we see a second one
 				if (st->nb_frames_range==1) {
 					gf_filter_pck_ref(&pck);
-					st->resinsert_single_pck = pck;
-				} else if (st->resinsert_single_pck) {
-					gf_filter_pck_unref(st->resinsert_single_pck);
-					st->resinsert_single_pck = NULL;
+					st->reinsert_single_pck = pck;
+				} else if (st->reinsert_single_pck) {
+					gf_filter_pck_unref(st->reinsert_single_pck);
+					st->reinsert_single_pck = NULL;
 				}
 				continue;
 			}
@@ -1301,10 +1348,10 @@ GF_Err reframer_process(GF_Filter *filter)
 				//keep ref to first packet until we see a second one
 				if (st->nb_frames_range==1) {
 					gf_filter_pck_ref(&pck);
-					st->resinsert_single_pck = pck;
-				} else if (st->resinsert_single_pck) {
-					gf_filter_pck_unref(st->resinsert_single_pck);
-					st->resinsert_single_pck = NULL;
+					st->reinsert_single_pck = pck;
+				} else if (st->reinsert_single_pck) {
+					gf_filter_pck_unref(st->reinsert_single_pck);
+					st->reinsert_single_pck = NULL;
 				}
 			} else {
 				assert(pck == st->split_pck);
@@ -1599,7 +1646,7 @@ load_next_range:
 			RTStream *st = gf_filter_pid_get_udta(ipid);
 			//we reinsert the same PCK, so the ts_at_range_start_plus is always the packet cts
 			//we therefore need to compute the ts at and as the target end time minus the target start time
-			if (st->resinsert_single_pck) {
+			if (st->reinsert_single_pck) {
 				u64 start = ctx->cur_start.num;
 				start *= st->timescale;
 				start /= ctx->cur_start.den;
@@ -1764,6 +1811,7 @@ static const GF_FilterArgs ReframerArgs[] =
 	{ OFFS(nosap), "do not cut at SAP when extracting range (may result in broken streams)", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_EXPERT},
 	{ OFFS(splitrange), "signal file boundary at each extraction first packet for template-base file generation", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_EXPERT},
 	{ OFFS(seeksafe), "rewind play requests by given seconds (to make sur I-frame preceeding start is catched)", GF_PROP_DOUBLE, "10.0", NULL, GF_FS_ARG_HINT_EXPERT},
+	{ OFFS(tcmdrw), "rewrite TCMD samples when splitting", GF_PROP_BOOL, "true", NULL, GF_FS_ARG_HINT_EXPERT},
 	{0}
 };
 

applications/mp4box/filedump.c
@@ -2305,6 +2305,7 @@ void dump_vvc_track_info(GF_ISOFile *file, u32 trackNum, GF_VVCConfig *vvccfg
 	}
 }
 
+void gf_inspect_format_timecode(const u8 *data, u32 size, u32 tmcd_flags, u32 tc_num, u32 tc_den, u32 tmcd_fpt, char szFmt[100]);
 
 void DumpTrackInfo(GF_ISOFile *file, GF_ISOTrackID trackID, Bool full_dump, Bool is_track_num, Bool dump_m4sys)
 {
@@ -2345,7 +2346,7 @@ void DumpTrackInfo(GF_ISOFile *file, GF_ISOTrackID trackID, Bool full_dump, Bool
 
 	nb_edits = gf_isom_get_edits_count(file, trackNum);
 	if (nb_edits)
-		fprintf(stderr, "Track has %d edit lists: track duration is %s\n", nb_edits, format_duration(gf_isom_get_track_duration(file, trackNum), gf_isom_get_timescale(file), szDur));
+		fprintf(stderr, "Track has %d edits: track duration is %s\n", nb_edits, format_duration(gf_isom_get_track_duration(file, trackNum), gf_isom_get_timescale(file), szDur));
 
 	cts_shift = gf_isom_get_composition_offset_shift(file, trackNum);
 	if (cts_shift)
@@ -3074,7 +3075,7 @@ void DumpTrackInfo(GF_ISOFile *file, GF_ISOTrackID trackID, Bool full_dump, Bool
 		if (gf_isom_truehd_config_get(file, trackNum, 1, &fmt, &prate) != GF_OK) {
 			fprintf(stderr, "\tInvalid TrueHD audio config\n");
 		}
-		fprintf(stderr, "\tTrueHD Audio stream - Sample Rate %u - channels %u - format %u peak rate %u\n", sr, nb_ch, fmt, prate);
+		fprintf(stderr, "TrueHD Audio stream - Sample Rate %u - channels %u - format %u peak rate %u\n", sr, nb_ch, fmt, prate);
 	} else if (codecid) {
 		if (gf_isom_is_video_handler_type(mtype) ) {
 			u32 w, h;
@@ -3090,6 +3091,21 @@ void DumpTrackInfo(GF_ISOFile *file, GF_ISOTrackID trackID, Bool full_dump, Bool
 		u32 w, h;
 		gf_isom_get_visual_info(file, trackNum, 1, &w, &h);
 		fprintf(stderr, "Raw video %s - Resolution %d x %d\n", gf_pixel_fmt_name(pfmt), w, h);
+	} else if (msub_type==GF_QT_SUBTYPE_TMCD) {
+		u32 stsd_idx;
+		GF_ISOSample *sample = gf_isom_get_sample(file, trackNum, 1, &stsd_idx);
+		fprintf(stderr, "Time Code stream\n");
+		if (sample) {
+			char szTimecode[100];
+			u32 tmcd_flags, tmcd_num, tmcd_den, tmcd_fpt;
+
+			gf_isom_get_tmcd_config(file, trackNum, stsd_idx, &tmcd_flags, &tmcd_num, &tmcd_den, &tmcd_fpt);
+
+			gf_inspect_format_timecode(sample->data, sample->dataLength, tmcd_flags, tmcd_num, tmcd_den, tmcd_fpt, szTimecode);
+
+			gf_isom_sample_del(&sample);
+			fprintf(stderr, "\tFirst timecode: %s\n", szTimecode);
+		}
 	} else {
 		GF_GenericSampleDescription *udesc;
 

applications/mp4box/main.c
@@ -834,7 +834,7 @@ void PrintImportUsage()
 		"If you wish to create an interleaved new file with no temporary storage, use the [-newfs](MP4B_GEN) option. The interleaving might not be as precise as when using [-new]() since it is dependent on muxer input scheduling (each execution might lead to a slightly different result). Additionally in this mode: \n"
 		" - Some muxing options (marked with `X` below) will be activated for all inputs (e.g it is not possible to import one AVC track with `xps_inband` and another without).\n"
 		" - Some muxing options (marked as `D` below) cannot be used as they require temporary storage for file edition.\n"
-		" - Usage of [-cat]() is possible, but concatenated sources will not be interleaved in the output. If you wish to perforom more complex cat/add operations without temp file, use the [gpac application](Filters).\n"
+		" - Usage of [-cat]() is possible, but concatenated sources will not be interleaved in the output. If you wish to perform more complex cat/add operations without temp file, use a [playlist](flist).\n"
 		"  \n"
 		"Source URL can be any URL supported by GPAC, not limited to local files.\n"
 		"  \n"

include/gpac/isomedia.h
@@ -355,6 +355,8 @@ enum
 
 	GF_ISOM_ITEM_TYPE_AUXI 	= GF_4CC('a', 'u', 'x', 'i'),
 
+	GF_QT_SUBTYPE_TMCD = GF_4CC( 't', 'm', 'c', 'd' ),
+
 };
 
 

share/doc/man/gpac-filters.1
@@ -3074,6 +3074,8 @@ splitrange (bool, default: false): signal file boundary at each extraction first
 .br
 seeksafe (dbl, default: 10.0): rewind play requests by given seconds (to make sur I-frame preceeding start is catched)
 .br
+tcmdrw (bool, default: true):  rewrite TCMD samples when splitting
+.br
 
 .br
 .SH writegen
@@ -7403,149 +7405,6 @@ reset_switch (bool, default: false): reset decoder at config change
 .br
 
 .br
-.SH avgen
-.LP
-.br
-Description: AV Counter Generator
-.br
-Version: 1.0
-.br
-Author: GPAC Team
-.br
-
-.br
-This filter generates AV streams representing a counter. Streams can be enabled or disabled using .I type.
-.br
-The filter is software-based and does not use GPU.
-.br
-
-.br
-When .I adjust is set, the first video frame is adjusted such that a full circle happens at each exact second according to the system UTC clock.
-.br
-By default, video UTC and date are computed at each frame generation from current clock and not from frame number.
-.br
-This will result in broken timing when playing at speeds other than 1.0.
-.br
-This can be changed using .I lock.
-.br
-
-.br
-Audio beep is generated every second, with octave (2xfreq) of even beep used every 10 seconds.
-.br
-When video is generated, beep is synchronized to video at each exact second.
-.br
-
-.br
-If NTP injection is used, each video packet (but not audio ones) has a SenderNTP property set; if video is not used, each audio packet has a SenderNTP property set.
-.br
-
-.br
-.SH Multistream generation
-.LP
-.br
-More than one output size can be specified. This will result in multiple sources being generated, one per size.
-.br
-A size can be specified more than once, resulting in packet references when [-copy] is not set, or full copies otherwise.
-.br
-Target encoding bitrates can be assigned to each output using .I rates. This can be usefull when generating dash:
-.br
-Example
-.br
-gpac avgen:sizes=1280x720,1920x1080:rates=2M,5M enc:c=aac:FID=1 enc:c=264:FID=2:clone -o live.mpd:SID=1,2
-.br
-
-.br
-
-.br
-.SH Multiview generation
-.LP
-.br
-In multiview mode, only the animated counter will move in depth backward and forward, as indicated by the .I disparity value.
-.br
-When .I pack is set, a packed stereo couple is generated for each video packet.
-.br
-Otherwise, when .I views is greater than 2, each view is generated on a dedicated output pid with the property ViewIdx set in [1, views].
-.br
-Multi-view output forces usage of .I copy mode.
-.br
-
-.br
-.SH PID Naming
-.LP
-.br
-The audio PID is assigned the name audio and ID 1.
-.br
-If a single video PID is produced, it is assigned the name video and ID 2.
-.br
-If multiple video PIDs are produced, they are assigned the names videoN and ID N+1, N in [1, sizes].
-.br
-If multiple .I views are generated, they are assigned the names videoN_vK and ID N*views+K-1, N in [1, sizes], K in [1, views].
-.br
-
-.br
-
-.br
-.SH Options (expert):
-.LP
-.br
-type (enum, default: av):      output selection
-.br
-* a: audio only
-.br
-* v: video only
-.br
-* av: audio and video
-.br
-
-.br
-freq (uint, default: 440):     frequency of beep
-.br
-freq2 (uint, default: 659):    frequency of odd beep
-.br
-sr (uint, default: 44100):     output samplerate
-.br
-flen (uint, default: 1024):    output frame length in samples
-.br
-ch (uint, default: 1):         number of channels
-.br
-alter (bool, default: false):  beep alternatively on each channel
-.br
-blen (uint, default: 50):      length of beep in milliseconds
-.br
-fps (frac, default: 25):       video frame rate
-.br
-sizes (v2il, default: 1280x720): video size in pixels
-.br
-lock (bool, default: false):   lock timing to video generation
-.br
-dyn (bool, default: true):     move bottom banner
-.br
-ntp (bool, default: true):     send NTP along with packets
-.br
-copy (bool, default: false):   copy the framebuffer into each video packet instead of using packet references
-.br
-dur (frac, default: 0/0):      run for the given time in second
-.br
-adjust (bool, default: true):  adjust start time to synchronize counter and UTC - see filter help
-.br
-pack (enum, default: no):      packing mode for stereo views
-.br
- * no: no packing
-.br
- * ss: side by side packing, forces .I views to 2
-.br
- * tb: top-bottom packing, forces .I views to 2
-.br
-
-.br
-disparity (uint, default: 20): disparity in pixels between left-most and right-most views
-.br
-views (uint, default: 1):      number of views
-.br
-rates (strl):                  number of target bitrates to assign, one per size
-.br
-
-.br
 .SH EXAMPLES
 .TP
 Basic and advanced examples are available at https://wiki.gpac.io/Filters
@@ -7555,7 +7414,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev551-g1752299a3-master
+build: 1.1.0-DEV-rev561-g2c13bbf42-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -4001,7 +4001,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev551-g1752299a3-master
+build: 1.1.0-DEV-rev561-g2c13bbf42-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -1310,7 +1310,7 @@ If you wish to create an interleaved new file with no temporary storage, use the
 .br
  - Some muxing options (marked as D below) cannot be used as they require temporary storage for file edition.
 .br
- - Usage of .I cat is possible, but concatenated sources will not be interleaved in the output. If you wish to perforom more complex cat/add operations without temp file, use the gpac application.
+ - Usage of .I cat is possible, but concatenated sources will not be interleaved in the output. If you wish to perform more complex cat/add operations without temp file, use a playlist.
 .br
   
 .br
@@ -2659,7 +2659,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev551-g1752299a3-master
+build: 1.1.0-DEV-rev561-g2c13bbf42-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

commit ccd6ea6eda5476c921976cf17c602d85c4c1f88f

fixed bugs introduced by b59fcfac

src/filters/inspect.c
@@ -2318,7 +2318,7 @@ void gf_inspect_format_timecode(const u8 *data, u32 size, u32 tmcd_flags, u32 tc
 
 static void inspect_dump_tmcd(GF_InspectCtx *ctx, PidCtx *pctx, const u8 *data, u32 size, FILE *dump)
 {
-	inspect_format_tmcd_internal(data, size, pctx->tmcd_flags, pctx->tmcd_rate.num, pctx->tmcd_rate.den, pctx->tmcd_flags, NULL, pctx->bs, ctx->fftmcd, dump);
+	inspect_format_tmcd_internal(data, size, pctx->tmcd_flags, pctx->tmcd_rate.num, pctx->tmcd_rate.den, pctx->tmcd_fpt, NULL, pctx->bs, ctx->fftmcd, dump);
 }
 
 static void inspect_dump_vpx(GF_InspectCtx *ctx, FILE *dump, u8 *ptr, u64 frame_size, Bool dump_crc, PidCtx *pctx, u32 vpversion)

src/filters/reframer.c
@@ -709,8 +709,9 @@ Bool reframer_send_packet(GF_Filter *filter, GF_ReframerCtx *ctx, RTStream *st, 
 		//packet was split or was re-inserted
 		if (st->split_start) {
 			u32 dur = gf_filter_pck_get_duration(pck);
-			assert(dur>st->split_start);
-			dur -= st->split_start;
+			//can happen if source packet is less than split period duration, we just copy with no timing adjustment
+			if (dur > st->split_start)
+				dur -= st->split_start;
 			gf_filter_pck_set_duration(new_pck, dur);
 			st->ts_at_range_start_plus_one += st->split_start;
 			st->split_start = 0;
@@ -723,10 +724,12 @@ Bool reframer_send_packet(GF_Filter *filter, GF_ReframerCtx *ctx, RTStream *st, 
 			is_split = GF_TRUE;
 		}
 		//packet reinserted (not split), adjust duration and store offset in split start
-		if (!is_split && st->reinsert_single_pck) {
+		if (!st->can_split && !is_split && st->reinsert_single_pck) {
+			u32 dur = gf_filter_pck_get_duration(pck);
 			u64 ndur = st->range_end_reached_ts;
 			ndur -= st->ts_at_range_start_plus_one-1;
-			gf_filter_pck_set_duration(new_pck, (u32) ndur);
+			if (ndur && (ndur < dur))
+				gf_filter_pck_set_duration(new_pck, (u32) ndur);
 			st->split_start = (u32) ndur;
 		}
 