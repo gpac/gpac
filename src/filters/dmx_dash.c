gpac/gpac

commit 6c887386197d4917341947944eb7f0bc287efd70

fixes in http2 support

src/filters/dmx_dash.c
@@ -363,6 +363,7 @@ static void dashdmx_on_filter_setup_error(GF_Filter *failed_filter, void *udta, 
 			group->eos_detected = GF_TRUE;
 		} else {
 			group->in_error = GF_TRUE;
+			group->seg_filter_src = NULL;
 		}
 	}
 }

include/gpac/dash.h
@@ -682,9 +682,10 @@ GF_Err gf_dash_set_min_timeout_between_404(GF_DashClient *dash, u32 min_timeout_
 */
 GF_Err gf_dash_set_segment_expiration_threshold(GF_DashClient *dash, u32 expire_after_ms);
 
-/*! only enables the given group - this shall be set before calling \ref gf_dash_open. If group_idx is <0 (default) no groups will be disabled
+/*! only enables the given groups - this shall be set before calling \ref gf_dash_open. If NULL, no groups will be disabled
 \param dash the target dash client
-\param group_idx the 0-based index of the target group
+\param groups_idx list of  0-based index of the target groups to enable,
+\param nb_groups number of group indexes in list
 */
 void gf_dash_debug_groups(GF_DashClient *dash, const u32 *groups_idx, u32 nb_groups);
 
@@ -1085,6 +1086,12 @@ typedef s32 (*gf_dash_rate_adaptation)(void *udta, u32 group_idx, u32 base_group
 /*! Callback function for custom rate monitor, not final yet
 \param udta user data
 \param group_idx index of group to adapt
+\param bits_per_sec estimated download rate (not premultiplied by playback speed)
+\param total_bytes size of segment being downloaded, 0 if unknown
+\param bytes_done bytes received for segment
+\param us_since_start microseconds ellapse since segment was sheduled for download
+\param buffer_dur_ms current buffer duration in milliseconds
+\param current_seg_dur duration of segment being downloaded, 0 if unknown
 \return quality index (>=0) to switch to after abort, -1 to do nothing (no abort), -2 for internal algorithms having already setup the desired quality and requesting only abort
  */
 typedef s32 (*gf_dash_download_monitor)(void *udta, u32 group_idx, u32 bits_per_sec, u64 total_bytes, u64 bytes_done, u64 us_since_start, u32 buffer_dur_ms, u32 current_seg_dur);

include/gpac/download.h
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2000-2019
+ *			Copyright (c) Telecom ParisTech 2000-2021
  *					All rights reserved
  *
  *  This file is part of GPAC / common tools sub-project
@@ -154,7 +154,12 @@ typedef enum
 	/*!signal that the session has been deconnected*/
 	GF_NETIO_DISCONNECTED,
 	/*!downloader session failed (error code set) or done/destroyed (no error code)*/
-	GF_NETIO_STATE_ERROR
+	GF_NETIO_STATE_ERROR,
+	/*!signal that a new session is being requested on that same connection (h2, h3)
+	This is only used for server sessions*/
+	GF_NETIO_REQUEST_SESSION,
+	/*! stream has been canceled by remote peer*/
+	GF_NETIO_CANCEL_STREAM,
 } GF_NetIOStatus;
 
 /*!session download flags*/

share/doc/man/gpac-filters.1
@@ -1781,7 +1781,7 @@ switch_count (uint, default: 1): indicate how many segments the client shall wai
 .br
 aggressive (bool, default: no): if enabled, switching algo targets the closest bandwidth fitting the available download rate. If no, switching algo targets the lowest bitrate representation that is above the currently played (eg does not try to switch to max bandwidth)
 .br
-debug_as (uint, default: -1):  play only the adaptation set indicated by its index in the MPD; if negative, all sets are used
+debug_as (uintl):              play only the adaptation sets indicated by their indices (0-based) in the MPD
 .br
 speedadapt (bool, default: no): enable adaptation based on playback speed
 .br
@@ -5961,7 +5961,7 @@ The server currently only operates in either HTTPS or HTTP mode and cannot run b
 .br
 dst (cstr):                    location of destination resource - see filter help
 .br
-port (uint, default: 80):      server port
+port (uint, default: 0):       server port
 .br
 ifce (str):                    default network inteface to use
 .br
@@ -6013,7 +6013,7 @@ sutc (bool, default: false):   insert server UTC in response headers as Server-U
 .br
 cors (bool, default: false):   insert CORS header allowing all domains
 .br
-reqlog (str):                  provide short log of the requests indicated in this option (comma separated list, * for all) regardless of HTTP log settings. Value REClogs file writing start/end
+reqlog (str):                  provide short log of the requests indicated in this option (comma separated list, * for all) regardless of HTTP log settings. Value REC logs file writing start/end
 .br
 ice (bool, default: false):    insert ICE meta-data in response headers in sink mode - see filter help
 .br
@@ -7337,7 +7337,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev494-gb6505589f-master
+build: 1.1.0-DEV-rev509-g95f9e26d5-http2
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -709,6 +709,21 @@ force using threads for async download requests rather than session scheduler
 set window analysis length in milliseconds for chunk-transfer encoding rate estimation
 .br
 .TP
+.B \-no-h2
+.br
+disable HTTP2
+.br
+.TP
+.B \-no-h2c
+.br
+disable HTTP2 upgrade (i.e. over non-TLS)
+.br
+.TP
+.B \-h2-copy
+.br
+enable intermediate copy of data in nghttp2 (default is disabled but may report as broken frames in wireshark)
+.br
+.TP
 .B \-dbg-edges
 .br
 log edges status in filter graph before dijkstra resolution (for debug). Edges are logged as edge_source(status, weight, src_cap_idx, dst_cap_idx)
@@ -1302,7 +1317,7 @@ This will force decoding media from file.mp4 and trash (send to null) the result
 .br
 
 .br
-When a filter uses an option defined as a string using the same separator character as gpac, you can either modify the set of separators, or escape the seperator by duplicating it. The options enclosed by duplicated separator are not parsed. This is mostly used for meta filters, such as ffmpeg, to pass options to subfilters such as libx264 (cf x264opts parameter).
+When a filter uses an option defined as a string using the same separator character as gpac, you can either modify the set of separators, or escape the separator by duplicating it. The options enclosed by duplicated separator are not parsed. This is mostly used for meta filters, such as ffmpeg, to pass options to subfilters such as libx264 (cf x264opts parameter).
 .br
 Example
 .br
@@ -3976,7 +3991,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev494-gb6505589f-master
+build: 1.1.0-DEV-rev509-g95f9e26d5-http2
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -71,7 +71,7 @@ fragment file, producing track fragments of given duration in ms. This disables 
 .TP
 .B \-out (string)
 .br
-specify output file name. By default input file is overwritten
+specify ISOBMFF output file name. By default input file is overwritten
 .br
 .TP
 .B \-tmp (string)
@@ -454,19 +454,14 @@ extract given track to a new mp4 file
 extract given track as raw media in experimental XML streaming instructions
 .br
 .TP
-.B \-avi (int)
+.B \-mux (string)
 .br
-extract given track to an avi file
+mux input to given destination
 .br
 .TP
 .B \-qcp (int)
 .br
 same as .I raw but defaults to QCP file for EVRC/SMV
-.br
-.TP
-.B \-aviraw (string)
-.br
-extract AVI track in raw format; parameter can be video, audio or audioN
 .br
 .TP
 .B \-saf
@@ -2655,7 +2650,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev494-gb6505589f-master
+build: 1.1.0-DEV-rev509-g95f9e26d5-http2
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4client.1
@@ -261,7 +261,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev494-gb6505589f-master
+build: 1.1.0-DEV-rev509-g95f9e26d5-http2
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

src/filters/in_http.c
@@ -532,7 +532,8 @@ static GF_Err httpin_process(GF_Filter *filter)
 
 	ctx->nb_read += nb_read;
 	if (ctx->file_size && (ctx->nb_read==ctx->file_size)) {
-		ctx->is_end = GF_TRUE;
+		if (net_status!=GF_NETIO_DATA_EXCHANGE)
+			ctx->is_end = GF_TRUE;
 	} else if (e==GF_EOS) {
 		ctx->is_end = GF_TRUE;
 	}

commit a51113217ed8379b1abda0a8bce1d1e0e92b47bd

fixed bug closing dash session on 404 introduced by 6c88738

src/filters/dmx_dash.c
@@ -138,6 +138,7 @@ typedef struct
 	u32 last_bw_check;
 	u64 us_at_seg_start;
 	Bool signal_seg_name;
+	Bool init_ok;
 
 	u32 seg_discard_state;
 
@@ -367,7 +368,9 @@ static void dashdmx_on_filter_setup_error(GF_Filter *failed_filter, void *udta, 
 			group->eos_detected = GF_TRUE;
 		} else {
 			group->in_error = GF_TRUE;
-			group->seg_filter_src = NULL;
+			//failure at init, abort group
+			if (!group->init_ok)
+				group->seg_filter_src = NULL;
 		}
 	}
 }
@@ -2314,6 +2317,8 @@ static void dashdmx_switch_segment(GF_DASHDmxCtx *ctx, GF_DASHGroup *group)
 	u64 start_range, end_range, switch_start_range, switch_end_range;
 	bin128 key_IV;
 	u32 group_idx;
+
+	group->init_ok = GF_TRUE;
 
 fetch_next:
 	assert(group->nb_eos || group->seg_was_not_ready || group->in_error);