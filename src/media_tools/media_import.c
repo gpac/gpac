gpac/gpac

commit 4a5ff64b7fc00c4d44a3ab97156d2b62a0de4eab

map fmt MP4Box option to no probe in filter session

src/media_tools/media_import.c
@@ -1241,7 +1241,7 @@ GF_Err gf_media_import(GF_MediaImporter *importer)
 	if (importer->run_in_session) {
 		fsess = importer->run_in_session;
 	} else {
-		fsess = gf_fs_new_defaults(0);
+		fsess = gf_fs_new_defaults(fmt ? GF_FS_FLAG_NO_PROBE : 0);
 		if (!fsess) {
 			return gf_import_message(importer, GF_BAD_PARAM, "[Importer] Cannot load filter session for import");
 		}
@@ -1409,6 +1409,10 @@ GF_Err gf_media_import(GF_MediaImporter *importer)
 	e = gf_dynstrcat(&args, "importer:index=0", ":");
 	if (importer->trackID && !source_id_set) {
 		sprintf(szSubArg, "FID=%s", szFilterID);
+		e |= gf_dynstrcat(&args, szSubArg, ":");
+	}
+	if (fmt) {
+		sprintf(szSubArg, "ext=%s", fmt);
 		e |= gf_dynstrcat(&args, szSubArg, ":");
 	}
 	if (importer->filter_src_opts) e |= gf_dynstrcat(&args, importer->filter_src_opts, ":");

applications/mp4box/main.c
@@ -755,7 +755,7 @@ static MP4BoxArg m4b_imp_fileopt_args [] = {
 	GF_DEF_ARG("moovts", NULL, "`S` set movie timescale to TS. A negative value picks the media timescale of the first track imported", NULL, NULL, GF_ARG_INT, 0),
 	GF_DEF_ARG("noedit", NULL, "`XS` do not set edit list when importing B-frames video tracks", NULL, NULL, GF_ARG_BOOL, 0),
 	GF_DEF_ARG("rvc", NULL, "`S` set RVC configuration for the media", NULL, NULL, GF_ARG_STRING, 0),
-	GF_DEF_ARG("fmt", NULL, "override format detection with given format (cf BT/XMTA doc)", NULL, NULL, GF_ARG_STRING, 0),
+	GF_DEF_ARG("fmt", NULL, "override format detection with given format - disable data probing and force `ext` option on source", NULL, NULL, GF_ARG_STRING, 0),
 	GF_DEF_ARG("profile", NULL, "`S` override AVC profile. Integer value, or `high444`, `high`, `extended`, `main`, `baseline`", NULL, NULL, GF_ARG_INT, 0),
 	GF_DEF_ARG("level", NULL, "`S` override AVC level, if value < 6, interpreted as decimal expression", NULL, NULL, GF_ARG_INT, 0),
 	GF_DEF_ARG("compat", NULL, "`S` force the profile compatibity flags for the H.264 content", NULL, NULL, GF_ARG_INT, 0),

share/doc/man/gpac-filters.1
@@ -7589,7 +7589,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev635-g9c51f2274-master
+build: 1.1.0-DEV-rev642-gc9d796610-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -4018,7 +4018,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev635-g9c51f2274-master
+build: 1.1.0-DEV-rev642-gc9d796610-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -1681,7 +1681,7 @@ S set RVC configuration for the media
 .TP
 .B fmt (string)
 .br
-override format detection with given format (cf BT/XMTA doc)
+override format detection with given format - disable data probing and force ext option on source
 .br
 .TP
 .B profile (int)
@@ -2654,7 +2654,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev635-g9c51f2274-master
+build: 1.1.0-DEV-rev642-gc9d796610-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4client.1
@@ -261,7 +261,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev635-g9c51f2274-master
+build: 1.1.0-DEV-rev642-gc9d796610-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

src/filter_core/filter_session.c
@@ -449,6 +449,8 @@ GF_FilterSession *gf_fs_new_defaults(u32 inflags)
 
 	if (gf_opts_get_bool("core", "no-probe"))
 		flags |= GF_FS_FLAG_NO_PROBE;
+	else if (inflags & GF_FS_FLAG_NO_PROBE)
+		flags |= GF_FS_FLAG_NO_PROBE;
 
 	if (gf_opts_get_bool("core", "no-argchk"))
 		flags |= GF_FS_FLAG_NO_ARG_CHECK;

commit 007065cd587d6e232c348ed707141e3589c548bc

fixed bug introduced by 4a5ff64b

src/media_tools/media_import.c
@@ -1098,7 +1098,7 @@ GF_Err gf_media_import(GF_MediaImporter *importer)
 	GF_Filter *isobmff_mux, *source;
 	GF_Filter *filter_orig;
 	char *ext;
-	char *fmt = "";
+	char *fmt = NULL;
 
 	if (!importer || (!importer->dest && (importer->flags!=GF_IMPORT_PROBE_ONLY)) || (!importer->in_name && !importer->orig) ) return GF_BAD_PARAM;
 
@@ -1134,10 +1134,10 @@ GF_Err gf_media_import(GF_MediaImporter *importer)
 	}
 
 	/*SC3DMC*/
-	if (!strnicmp(ext, ".s3d", 4) || !stricmp(fmt, "SC3DMC") )
+	if (!strnicmp(ext, ".s3d", 4) || (fmt && !stricmp(fmt, "SC3DMC")) )
 		return gf_import_afx_sc3dmc(importer, GF_TRUE);
 	/* chapter */
-	else if (!strnicmp(ext, ".txt", 4) || !strnicmp(ext, ".chap", 5) || !stricmp(fmt, "CHAP") ) {
+	else if (!strnicmp(ext, ".txt", 4) || !strnicmp(ext, ".chap", 5) || (fmt && !stricmp(fmt, "CHAP")) ) {
 		e =  gf_media_import_chapters_file(importer);
 		if (!strnicmp(ext, ".txt", 4) && (e==GF_NOT_FOUND)) {
 