gpac/gpac

commit b8a0faad363d9ce9b7f28116b2461b3205f57dd4

made raw option of reframer and flist an enum rather than a boolean

src/filters/reframer.c
@@ -58,6 +58,14 @@ enum
 	EXTRACT_DUR,
 };
 
+enum
+{
+	RAW_AV=0,
+	RAW_AUDIO,
+	RAW_VIDEO,
+	RAW_NONE,
+};
+
 #define RT_PRECISION_US	2000
 
 typedef struct
@@ -112,7 +120,7 @@ typedef struct
 	Bool refs;
 	u32 rt;
 	Double speed;
-	Bool raw;
+	u32 raw;
 	GF_PropStringList xs, xe;
 	Bool nosap, splitrange, xadjust, tcmdrw;
 	u32 xround;
@@ -1896,37 +1904,75 @@ load_next_range:
 	return GF_OK;
 }
 
-static const GF_FilterCapability ReframerRAWCaps[] =
+static const GF_FilterCapability ReframerCaps_RAW_AV[] =
 {
-	//raw audio and video
+	//raw audio and video only
 	CAP_UINT(GF_CAPS_INPUT_OUTPUT,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_AUDIO),
 	CAP_UINT(GF_CAPS_INPUT_OUTPUT,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_VISUAL),
 	CAP_UINT(GF_CAPS_INPUT_OUTPUT,  GF_PROP_PID_CODECID, GF_CODECID_RAW),
 	{0},
 	//no restriction for media other than audio and video - cf regular caps for comments
-	CAP_UINT(GF_CAPS_INPUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_AUDIO),
-	CAP_UINT(GF_CAPS_INPUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_VISUAL),
-	CAP_UINT(GF_CAPS_INPUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_FILE),
+	CAP_UINT(GF_CAPS_IN_OUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_AUDIO),
+	CAP_UINT(GF_CAPS_IN_OUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_VISUAL),
+	CAP_UINT(GF_CAPS_IN_OUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_FILE),
 	CAP_UINT(GF_CAPS_INPUT_EXCLUDED,  GF_PROP_PID_UNFRAMED, GF_TRUE),
-	CAP_UINT(GF_CAPS_OUTPUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_AUDIO),
-	CAP_UINT(GF_CAPS_OUTPUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_VISUAL),
-	CAP_UINT(GF_CAPS_OUTPUT_EXCLUDED, GF_PROP_PID_STREAM_TYPE, GF_STREAM_FILE),
+	CAP_UINT(GF_CAPS_OUTPUT_EXCLUDED, GF_PROP_PID_CODECID, GF_CODECID_RAW),
+	CAP_UINT(GF_CAPS_OUTPUT_LOADED_FILTER, GF_PROP_PID_CODECID, GF_CODECID_RAW)
+};
+
+
+static const GF_FilterCapability ReframerCaps_RAW_A[] =
+{
+	//raw audio only
+	CAP_UINT(GF_CAPS_INPUT_OUTPUT,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_AUDIO),
+	CAP_UINT(GF_CAPS_INPUT_OUTPUT,  GF_PROP_PID_CODECID, GF_CODECID_RAW),
+	{0},
+	//no restriction for media other than audio - cf regular caps for comments
+	CAP_UINT(GF_CAPS_IN_OUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_AUDIO),
+	CAP_UINT(GF_CAPS_IN_OUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_FILE),
+	CAP_UINT(GF_CAPS_INPUT_EXCLUDED,  GF_PROP_PID_UNFRAMED, GF_TRUE),
+	CAP_UINT(GF_CAPS_OUTPUT_EXCLUDED, GF_PROP_PID_CODECID, GF_CODECID_RAW),
+	CAP_UINT(GF_CAPS_OUTPUT_LOADED_FILTER, GF_PROP_PID_CODECID, GF_CODECID_RAW)
+};
+
+
+static const GF_FilterCapability ReframerCaps_RAW_V[] =
+{
+	//raw video only
+	CAP_UINT(GF_CAPS_INPUT_OUTPUT,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_VISUAL),
+	CAP_UINT(GF_CAPS_INPUT_OUTPUT,  GF_PROP_PID_CODECID, GF_CODECID_RAW),
+	{0},
+	//no restriction for media other than video - cf regular caps for comments
+	CAP_UINT(GF_CAPS_IN_OUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_VISUAL),
+	CAP_UINT(GF_CAPS_IN_OUT_EXCLUDED,  GF_PROP_PID_STREAM_TYPE, GF_STREAM_FILE),
+	CAP_UINT(GF_CAPS_INPUT_EXCLUDED,  GF_PROP_PID_UNFRAMED, GF_TRUE),
 	CAP_UINT(GF_CAPS_OUTPUT_EXCLUDED, GF_PROP_PID_CODECID, GF_CODECID_RAW),
 	CAP_UINT(GF_CAPS_OUTPUT_LOADED_FILTER, GF_PROP_PID_CODECID, GF_CODECID_RAW)
 };
 
 static GF_Err reframer_initialize(GF_Filter *filter)
 {
+	GF_Err e;
 	GF_ReframerCtx *ctx = gf_filter_get_udta(filter);
 
 	ctx->streams = gf_list_new();
 	ctx->seekable = GF_TRUE;
 	reframer_load_range(ctx);
 
-	if (ctx->raw) {
-		gf_filter_override_caps(filter, ReframerRAWCaps, sizeof(ReframerRAWCaps) / sizeof(GF_FilterCapability) );
+	switch (ctx->raw) {
+	case RAW_AV:
+		e = gf_filter_override_caps(filter, ReframerCaps_RAW_AV, GF_ARRAY_LENGTH(ReframerCaps_RAW_AV));
+		break;
+	case RAW_VIDEO:
+		e = gf_filter_override_caps(filter, ReframerCaps_RAW_V, GF_ARRAY_LENGTH(ReframerCaps_RAW_V));
+		break;
+	case RAW_AUDIO:
+		e = gf_filter_override_caps(filter, ReframerCaps_RAW_A, GF_ARRAY_LENGTH(ReframerCaps_RAW_A));
+		break;
+	default:
+		e = GF_OK;
 	}
-	return GF_OK;
+	return e;
 }
 
 static Bool reframer_process_event(GF_Filter *filter, const GF_FilterEvent *evt)
@@ -2003,7 +2049,11 @@ static const GF_FilterArgs ReframerArgs[] =
 	{ OFFS(saps), "drop non-SAP packets, off by default. The list gives the SAP types (0,1,2,3,4) to forward. Note that forwarding only sap 0 will break the decoding", GF_PROP_UINT_LIST, NULL, "0|1|2|3|4", GF_FS_ARG_HINT_NORMAL},
 	{ OFFS(refs), "forward only frames used as reference frames, if indicated in the input stream", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_NORMAL},
 	{ OFFS(speed), "speed for real-time regulation mode - only positive value", GF_PROP_DOUBLE, "1.0", NULL, GF_FS_ARG_HINT_ADVANCED},
-	{ OFFS(raw), "force input AV streams to be in raw format (i.e. forces decoding of AV input)", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_NORMAL},
+	{ OFFS(raw), "force input AV streams to be in raw format\n"
+	"- no: do not force decoding of inputs\n"
+	"- av: force decoding of audio and video inputs\n"
+	"- a: force decoding of audio inputs\n"
+	"- v: force decoding of video inputs", GF_PROP_UINT, "no", "av|a|v|no", GF_FS_ARG_HINT_NORMAL},
 	{ OFFS(frames), "drop all except listed frames (first being 1), off by default", GF_PROP_UINT_LIST, NULL, NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(xs), "extraction start time(s), see filter help", GF_PROP_STRING_LIST, NULL, NULL, GF_FS_ARG_HINT_NORMAL},
 	{ OFFS(xe), "extraction end time(s). If less values than start times, the last time interval extracted is an open range", GF_PROP_STRING_LIST, NULL, NULL, GF_FS_ARG_HINT_NORMAL},
@@ -2042,7 +2092,7 @@ GF_FilterRegister ReframerRegister = {
 		"  \n"
 		"# Frame decoding\n"
 		"This filter can force input media streams to be decoded using the [-raw]() option.\n"
-		"EX gpac src=m.mp4 reframer:raw @ [dst]\n"
+		"EX gpac src=m.mp4 reframer:raw=av @ [dst]\n"
 		"# Real-time Regulation\n"
 		"The filter can perform real-time regulation of input packets, based on their timescale and timestamps.\n"
 		"For example to simulate a live DASH:\n"

applications/gpac/main.c
@@ -185,7 +185,7 @@ const char *gpac_doc =
 "This creates a 25 fps AVC at 175kbps with a gop duration of 2 seconds, using closed gop and fast encoding settings for ffmpeg.\n"
 "\n"
 "The inverse operation (forcing a decode to happen) is possible using the __reframer__ filter.\n"
-"EX src=file.mp4 reframer:raw @ -o null\n"
+"EX src=file.mp4 reframer:raw=av @ -o null\n"
 "This will force decoding media from `file.mp4` and trash (send to `null`) the result (doing a decoder benchmark for example).\n"
 "\n"
 "When a filter uses an option defined as a string using the same separator character as gpac, you can either "

include/gpac/filters.h
@@ -1861,6 +1861,8 @@ enum
 #define GF_CAPS_INPUT_OUTPUT	(GF_CAPFLAG_IN_BUNDLE|GF_CAPFLAG_INPUT|GF_CAPFLAG_OUTPUT)
 /*! Shortcut macro to set for optional input and output capability flags*/
 #define GF_CAPS_INPUT_OUTPUT_OPT	(GF_CAPFLAG_IN_BUNDLE|GF_CAPFLAG_INPUT|GF_CAPFLAG_OUTPUT|GF_CAPFLAG_OPTIONAL)
+/*! Shortcut macro to set for excluded input capability flags*/
+#define GF_CAPS_IN_OUT_EXCLUDED	(GF_CAPFLAG_IN_BUNDLE|GF_CAPFLAG_INPUT|GF_CAPFLAG_OUTPUT|GF_CAPFLAG_EXCLUDED)
 
 /*! Filter capability description*/
 typedef struct
@@ -2445,7 +2447,10 @@ GF_Err gf_filter_assign_id(GF_Filter *filter, const char *filter_id);
 const char *gf_filter_get_id(GF_Filter *filter);
 
 
-/*! Overrides the filter register caps with new caps for this instance. Typically used when an option of the filter changes the capabilities
+/*! Overrides the filter caps with new caps for this instance. Typically used when an option of the filter changes the capabilities
+
+The new caps are only taken into account for future graph resolutions, any current link from/to the target filter will not be re-solved when calling this function.
+
 \param filter the target filter
 \param caps the new set of capabilities to use for the filter. These are NOT copied and shall be valid for the lifetime of the filter
 \param nb_caps number of capabilities set

share/default.cfg
@@ -3,7 +3,7 @@
 -plist=flist:srcs=@{-:N} vout aout
 -info=src=@{+1:N} inspect
 -mplay=src=@{+1:N} aout vout
--bench=reframer:raw @ -o null -stats -i @{1}
+-bench=reframer:raw=av @ -o null -stats -i @{1}
 -vbench=vout:!vsync -i @{1} -stats
 -gui=-noprog compositor:player=gui -xopt
 [gpac.aliasdoc]

share/doc/man/gpac-filters.1
@@ -2867,7 +2867,7 @@ This filter can force input media streams to be decoded using the .I raw option.
 .br
 Example
 .br
-gpac src=m.mp4 reframer:raw @ [dst]
+gpac src=m.mp4 reframer:raw=av @ [dst]
 .br
 
 .br
@@ -3031,7 +3031,17 @@ refs (bool, default: false):   forward only frames used as reference frames, if 
 .br
 speed (dbl, default: 1.0):     speed for real-time regulation mode - only positive value
 .br
-raw (bool, default: false):    force input AV streams to be in raw format (i.e. forces decoding of AV input)
+raw (enum, default: no):       force input AV streams to be in raw format
+.br
+* no: do not force decoding of inputs
+.br
+* av: force decoding of audio and video inputs
+.br
+* a: force decoding of audio inputs
+.br
+* v: force decoding of video inputs
+.br
+
 .br
 frames (uintl):                drop all except listed frames (first being 1), off by default
 .br
@@ -4014,7 +4024,17 @@ sigcues (bool, default: false): inject CueStart property at each source begin (n
 .br
 fdel (bool, default: false):   delete source files after processing in playlist mode (does not delete the playlist)
 .br
-raw (bool, default: false):    force input AV streams to be in raw format (i.e. forces decoding of AV input)
+raw (enum, default: no):       force input AV streams to be in raw format
+.br
+* no: do not force decoding of inputs
+.br
+* av: force decoding of audio and video inputs
+.br
+* a: force decoding of audio inputs
+.br
+* v: force decoding of video inputs
+.br
+
 .br
 
 .br
@@ -7602,7 +7622,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev737-g7ee960336-master
+build: 1.1.0-DEV-rev738-g1ae60f1a9-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -1338,7 +1338,7 @@ The inverse operation (forcing a decode to happen) is possible using the reframe
 .br
 Example
 .br
-src=file.mp4 reframer:raw @ -o null
+src=file.mp4 reframer:raw=av @ -o null
 .br
 
 .br
@@ -4039,7 +4039,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev737-g7ee960336-master
+build: 1.1.0-DEV-rev738-g1ae60f1a9-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2701,7 +2701,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev737-g7ee960336-master
+build: 1.1.0-DEV-rev738-g1ae60f1a9-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4client.1
@@ -261,7 +261,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev737-g7ee960336-master
+build: 1.1.0-DEV-rev738-g1ae60f1a9-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

commit 8e77f423fbad973bbedda441935c27dd9ca725a6

fixed bug introduced by b8a0faad3

src/filters/reframer.c
@@ -105,6 +105,7 @@ typedef struct
 	GF_FilterPacket *reinsert_single_pck;
 	Bool is_playing;
 
+	Bool is_raw;
 	u32 codec_id, stream_type;
 	u32 nb_ch, sample_rate, abps;
 	Bool audio_planar;
@@ -250,7 +251,9 @@ GF_Err reframer_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remo
 	st->codec_id = p ? p->value.uint : 0;
 	st->nb_ch = st->abps = st->sample_rate = 0;
 	st->audio_planar = GF_FALSE;
-	if ((st->codec_id==GF_CODECID_RAW) && (st->stream_type==GF_STREAM_AUDIO)) {
+	st->is_raw = (st->codec_id==GF_CODECID_RAW) ? GF_TRUE : GF_FALSE;
+
+	if (st->is_raw && (st->stream_type==GF_STREAM_AUDIO)) {
 		p = gf_filter_pid_get_property(pid, GF_PROP_PID_AUDIO_FORMAT);
 		if (p) st->abps = gf_audio_fmt_bit_depth(p->value.uint) / 8;
 		p = gf_filter_pid_get_property(pid, GF_PROP_PID_NUM_CHANNELS);
@@ -811,11 +814,11 @@ Bool reframer_send_packet(GF_Filter *filter, GF_ReframerCtx *ctx, RTStream *st, 
 			}
 
 			gf_filter_pck_set_cts(new_pck, (u64) ts);
-			if (ctx->raw) {
+			if (st->is_raw) {
 				gf_filter_pck_set_dts(new_pck, ts);
 			}
 		}
-		if (!ctx->raw) {
+		if (!st->is_raw) {
 			ts = gf_filter_pck_get_dts(pck) + cts_offset;
 			if (ts != GF_FILTER_NO_TS) {
 				ts += st->tk_delay;
@@ -987,7 +990,7 @@ static void check_gop_split(GF_ReframerCtx *ctx)
 			for (j=0; j<nb_pck; j++) {
 				u64 ts;
 				GF_FilterPacket *pck = gf_list_get(st->pck_queue, j);
-				if (!ctx->raw && !gf_filter_pck_get_sap(pck) ) {
+				if (!st->is_raw && !gf_filter_pck_get_sap(pck) ) {
 					continue;
 				}
 				ts = gf_filter_pck_get_dts(pck);
@@ -1344,7 +1347,16 @@ GF_Err reframer_process(GF_Filter *filter)
 			}
 
 			//if nosap is set, consider all packet SAPs
-			is_sap = (ctx->nosap || ctx->raw || gf_filter_pck_get_sap(pck)) ? GF_TRUE : GF_FALSE;
+			if (ctx->nosap || st->is_raw) {
+				is_sap = GF_TRUE;
+			} else {
+				GF_FilterSAPType sap = gf_filter_pck_get_sap(pck);
+				if ((sap==GF_FILTER_SAP_1) || (sap==GF_FILTER_SAP_2) || (sap==GF_FILTER_SAP_3)) {
+					is_sap = GF_TRUE;
+				} else {
+					is_sap = GF_FALSE;
+				}
+			}
 
 			if (!is_sap) {
 				if (st->all_saps) {