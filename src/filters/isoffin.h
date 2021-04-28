5410725ffe9addb76bce4befff6c9d5823867678
jeanlf <jeanlf@gpac.io>
2021-04-28 11:56:26 +0200

    added nodata option to isobmf reader

--- src/filters/isoffin.h
+++ src/filters/isoffin.h
@@ -70,7 +70,7 @@ typedef struct
 	u32 xps_check;
 	char *catseg;
 	Bool sigfrag;
-	Bool nocrypt, strtxt;
+	Bool nocrypt, strtxt, nodata;
 	u32 mstore_purge, mstore_samples, mstore_size;
 
 	//internal
