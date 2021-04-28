gpac/gpac

commit 9fae1aed8444bd445c9ec293395946c329280124

added safety suffix in dasher for reused resolved templates (same source in multiple periods)

src/filters/dasher.c
@@ -180,6 +180,7 @@ typedef struct
 	Bool template_use_source;
 	s32 period_idx;
 
+	GF_List *tpl_records;
 	Bool use_xlink, use_cenc, check_main_role;
 
 	//options for muxers, constrained by profile
@@ -3006,6 +3007,29 @@ static void dasher_set_content_components(GF_DashStream *ds)
 	gf_list_add(base_ds->set->content_component, component);
 }
 
+typedef struct
+{
+	char *tpl;
+	u32 nb_reused;
+} DashTemplateRecord;
+
+static u32 dasher_check_template_reuse(GF_DasherCtx *ctx, const char *tpl)
+{
+	DashTemplateRecord *tr;
+	u32 i, count = gf_list_count(ctx->tpl_records);
+	for (i=0; i<count; i++) {
+		tr = gf_list_get(ctx->tpl_records, i);
+		if (!strcmp(tr->tpl, tpl)) {
+			tr->nb_reused++;
+			return tr->nb_reused;
+		}
+	}
+	GF_SAFEALLOC(tr, DashTemplateRecord)
+	tr->tpl = gf_strdup(tpl);
+	gf_list_add(ctx->tpl_records, tr);
+	return 0;
+}
+
 static void dasher_setup_sources(GF_Filter *filter, GF_DasherCtx *ctx, GF_MPD_AdaptationSet *set)
 {
 	char szDASHTemplate[GF_MAX_PATH];
@@ -3266,6 +3290,7 @@ static void dasher_setup_sources(GF_Filter *filter, GF_DasherCtx *ctx, GF_MPD_Ad
 		Bool skip_init = GF_FALSE;
 		GF_DashStream *tile_base_ds = NULL;
 		Bool is_bs_switch;
+		u32 reused_template_idx;
 		u32 init_template_mode = GF_DASH_TEMPLATE_INITIALIZATION_TEMPLATE;
 		rep = gf_list_get(set->representations, i);
 		ds = rep->playback.udta;
@@ -3336,6 +3361,8 @@ static void dasher_setup_sources(GF_Filter *filter, GF_DasherCtx *ctx, GF_MPD_Ad
 			ds->done = 1;
 			continue;
 		}
+
+
 		if (single_template && ds->split_set_names && !use_dash_suffix) {
 			char szStrName[20];
 			sprintf(szStrName, "_set%d", 1 + gf_list_find(ctx->current_period->period->adaptation_sets, set)  );
@@ -3398,6 +3425,17 @@ static void dasher_setup_sources(GF_Filter *filter, GF_DasherCtx *ctx, GF_MPD_Ad
 		is_bs_switch = set->bitstream_switching;
 		//only used to force _init in default templates
 		if (ds->tile_base) is_bs_switch = GF_FALSE;
+
+
+		//check we are not reusing an existing template from previous periods, if so append a suffix
+		//we check the final init name
+		gf_media_mpd_format_segment_name(GF_DASH_TEMPLATE_INITIALIZATION, is_bs_switch, szInitSegmentFilename, ds->rep_id, NULL, szDASHTemplate, init_ext, 0, ds->bitrate, 0, ctx->stl);
+		reused_template_idx = dasher_check_template_reuse(ctx, szInitSegmentFilename);
+		if (reused_template_idx) {
+			char szExName[20];
+			sprintf(szExName, "_r%d_", reused_template_idx);
+			strcat(szDASHTemplate, szExName);
+		}
 
 		//get final segment template with path resolution - output file name is NULL, we already have solved this
 		gf_media_mpd_format_segment_name(GF_DASH_TEMPLATE_TEMPLATE_WITH_PATH, is_bs_switch, szSegmentName, ds->rep_id, NULL, szDASHTemplate, seg_ext, 0, 0, 0, ctx->stl);
@@ -8344,6 +8382,7 @@ static GF_Err dasher_initialize(GF_Filter *filter)
 
 	ctx->pids = gf_list_new();
 	ctx->postponed_pids = gf_list_new();
+	ctx->tpl_records = gf_list_new();
 
 	if (!ctx->initext && (ctx->muxtype==DASHER_MUX_AUTO))
 		ctx->muxtype = DASHER_MUX_ISOM;
@@ -8425,6 +8464,13 @@ static void dasher_finalize(GF_Filter *filter)
 	}
 	gf_list_del(ctx->pids);
 	if (ctx->mpd) gf_mpd_del(ctx->mpd);
+
+	while (gf_list_count(ctx->tpl_records)) {
+		DashTemplateRecord *tr = gf_list_pop_back(ctx->tpl_records);
+		gf_free(tr->tpl);
+		gf_free(tr);
+	}
+	gf_list_del(ctx->tpl_records);
 
 	if (ctx->next_period->period) gf_mpd_period_free(ctx->next_period->period);
 	gf_list_del(ctx->current_period->streams);

commit db20cb278c34e50acd05a5ba9fb990fa42d29056

fixed bug introduced by 9fae1aed

src/filters/dasher.c
@@ -329,6 +329,7 @@ typedef struct _dash_stream
 	//segment start time in target MPD timescale
 	u64 seg_start_time;
 	Bool split_set_names;
+	Bool skip_tpl_reuse;
 	u64 max_period_dur;
 
 	GF_Filter *dst_filter;
@@ -3429,14 +3430,16 @@ static void dasher_setup_sources(GF_Filter *filter, GF_DasherCtx *ctx, GF_MPD_Ad
 
 		//check we are not reusing an existing template from previous periods, if so append a suffix
 		//we check the final init name
-		gf_media_mpd_format_segment_name(GF_DASH_TEMPLATE_INITIALIZATION, is_bs_switch, szInitSegmentFilename, ds->rep_id, NULL, szDASHTemplate, init_ext, 0, ds->bitrate, 0, ctx->stl);
-		reused_template_idx = dasher_check_template_reuse(ctx, szInitSegmentFilename);
-		if (reused_template_idx) {
-			char szExName[20];
-			sprintf(szExName, "_r%d_", reused_template_idx);
-			strcat(szDASHTemplate, szExName);
+		if (!ds->skip_tpl_reuse) {
+			gf_media_mpd_format_segment_name(GF_DASH_TEMPLATE_INITIALIZATION, is_bs_switch, szInitSegmentFilename, ds->rep_id, NULL, szDASHTemplate, init_ext, 0, ds->bitrate, 0, ctx->stl);
+			reused_template_idx = dasher_check_template_reuse(ctx, szInitSegmentFilename);
+			if (reused_template_idx) {
+				char szExName[20];
+				sprintf(szExName, "_r%d_", reused_template_idx);
+				strcat(szDASHTemplate, szExName);
+			}
 		}
-
+		
 		//get final segment template with path resolution - output file name is NULL, we already have solved this
 		gf_media_mpd_format_segment_name(GF_DASH_TEMPLATE_TEMPLATE_WITH_PATH, is_bs_switch, szSegmentName, ds->rep_id, NULL, szDASHTemplate, seg_ext, 0, 0, 0, ctx->stl);
 		ds->seg_template = gf_strdup(szSegmentName);
@@ -5530,6 +5533,7 @@ static GF_Err dasher_setup_period(GF_Filter *filter, GF_DasherCtx *ctx, GF_DashS
 		if (ds->stream_type==GF_STREAM_VISUAL)
 			ds_video = ds;
 
+		ds->skip_tpl_reuse = GF_FALSE;
 		// period resume (end of content replacement/splice/...): if using templates, check if period ID is used, if not force startNumber to resume
 		prop = gf_filter_pid_get_property_str(ds->ipid, "period_resume");
 		if (prop && prop->value.string && ctx->tpl && ds->mpd_timescale) {
@@ -5547,6 +5551,7 @@ static GF_Err dasher_setup_period(GF_Filter *filter, GF_DasherCtx *ctx, GF_DashS
 				if (num * seg_duration < period_start)
 					num++;
 				ds->startNumber = (u32) (num+1);
+				ds->skip_tpl_reuse = GF_TRUE;
 			}
 		}
 

src/filters/mux_isom.c
@@ -5923,7 +5923,8 @@ static void mp4_mux_get_min_max_cts(GF_MP4MuxCtx *ctx, TrackWriter *tkw, u64 *om
 	min_cts = (u64) -1;
 	for (i=0; i<count; i++) {
 		GF_ISOSample *s = gf_isom_get_sample_info(ctx->file, tkw->track_num, i+1, &di, &doff);
-		if (!s) return;
+		if (!s)
+			break;
 		if (tkw->clamp_ts_plus_one) {
 			if (s->DTS + s->CTS_Offset + 1 >= tkw->clamp_ts_plus_one ) {
 				gf_isom_sample_del(&s);
@@ -5937,7 +5938,6 @@ static void mp4_mux_get_min_max_cts(GF_MP4MuxCtx *ctx, TrackWriter *tkw, u64 *om
 		if (min_cts > s->DTS + s->CTS_Offset)
 			min_cts = s->DTS + s->CTS_Offset;
 
-		fprintf(stderr, "checking samp DTS "LLU" CTS "LLU"\n", s->DTS, s->DTS + s->CTS_Offset);
 		gf_isom_sample_del(&s);
 	}
 	*omax_cts = max_cts;