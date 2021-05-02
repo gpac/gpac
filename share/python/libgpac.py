fe0ae2b40748e86247c4f612902b68b5b8be91c0
jeanlf <jeanlf@gpac.io>
2021-05-02 12:44:28 +0200

    fixed bugs from previous commit

--- share/python/libgpac.py
+++ share/python/libgpac.py
@@ -727,7 +727,7 @@ class FEVT_AttachScene(Structure):
     _fields_ =  [
         ("type", c_uint),
         ("on_pid", _gf_filter_pid),
-        ("odm", c_void_p)
+        ("odm", c_void_p),
         ("node", c_void_p)
     ]
     ## \endcond
