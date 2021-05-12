e2703f2e80bbb1f7baabb6626ecd091b4ba8ecb7
jeanlf <jeanlf@gpac.io>
2021-05-12 11:28:28 +0200

    fixed #1787

--- src/isomedia/avc_ext.c
+++ src/isomedia/avc_ext.c
@@ -2727,59 +2727,67 @@ GF_Err avcc_box_read(GF_Box *s, GF_BitStream *bs)
 		ptr->size -= sl->size;
 	}
 
-	if (ptr->type==GF_ISOM_BOX_TYPE_AVCC) {
-		if (gf_avc_is_rext_profile(ptr->config->AVCProfileIndication)) {
-			if (!ptr->size) {
+	//not avcC (svcC; mvcC), no check for rext signaling
+	if (ptr->type!=GF_ISOM_BOX_TYPE_AVCC)
+		return GF_OK;
+
+	//not REXT profile, no check for rext signaling
+	if (!gf_avc_is_rext_profile(ptr->config->AVCProfileIndication))
+		return GF_OK;
+
+	//non-compliant file (rext signaling missing), try to fill in values from SPS
+	if (!ptr->size) {
+		/*set default values ...*/
+		ptr->config->chroma_format = 1;
+		ptr->config->luma_bit_depth = 8;
+		ptr->config->chroma_bit_depth = 8;
+
 #ifndef GPAC_DISABLE_AV_PARSERS
-				AVCState avc;
-				s32 idx;
-				GF_NALUFFParam *sl = (GF_NALUFFParam*)gf_list_get(ptr->config->sequenceParameterSets, 0);
-				idx = sl ? gf_avc_read_sps(sl->data+1, sl->size-1, &avc, 0, NULL) : -1;
-
-				if (idx>=0) {
-					ptr->config->chroma_format = avc.sps[idx].chroma_format;
-					ptr->config->luma_bit_depth = 8 + avc.sps[idx].luma_bit_depth_m8;
-					ptr->config->chroma_bit_depth = 8 + avc.sps[idx].chroma_bit_depth_m8;
-				}
-#else
-				/*set default values ...*/
-				ptr->config->chroma_format = 1;
-				ptr->config->luma_bit_depth = 8;
-				ptr->config->chroma_bit_depth = 8;
+		GF_NALUFFParam *sl = (GF_NALUFFParam*)gf_list_get(ptr->config->sequenceParameterSets, 0);
+		if (sl) {
+			AVCState avc;
+			s32 idx;
+			memset(&avc, 0, sizeof(AVCState));
+			idx = gf_avc_read_sps(sl->data, sl->size, &avc, 0, NULL);
+			if (idx>=0) {
+				ptr->config->chroma_format = avc.sps[idx].chroma_format;
+				ptr->config->luma_bit_depth = 8 + avc.sps[idx].luma_bit_depth_m8;
+				ptr->config->chroma_bit_depth = 8 + avc.sps[idx].chroma_bit_depth_m8;
+			}
+		}
 #endif
-				return GF_OK;
-			}
-			ISOM_DECREASE_SIZE(ptr, 4)
-			gf_bs_read_int(bs, 6);
-			ptr->config->chroma_format = gf_bs_read_int(bs, 2);
-			gf_bs_read_int(bs, 5);
-			ptr->config->luma_bit_depth = 8 + gf_bs_read_int(bs, 3);
-			gf_bs_read_int(bs, 5);
-			ptr->config->chroma_bit_depth = 8 + gf_bs_read_int(bs, 3);
+		GF_LOG(GF_LOG_WARNING, GF_LOG_CODING, ("AVCC: invalid syntax for REXT profile, patching.\n"));
+		return GF_OK;
+	}
+	ISOM_DECREASE_SIZE(ptr, 4)
+	gf_bs_read_int(bs, 6);
+	ptr->config->chroma_format = gf_bs_read_int(bs, 2);
+	gf_bs_read_int(bs, 5);
+	ptr->config->luma_bit_depth = 8 + gf_bs_read_int(bs, 3);
+	gf_bs_read_int(bs, 5);
+	ptr->config->chroma_bit_depth = 8 + gf_bs_read_int(bs, 3);
 
-			count = gf_bs_read_int(bs, 8);
-			if (count*2 > ptr->size) {
-				//ffmpeg just ignores this part while allocating bytes (filled with garbage?)
-				GF_LOG(GF_LOG_WARNING, GF_LOG_CODING, ("AVCC: invalid numOfSequenceParameterSetExt value. Skipping.\n"));
-				return GF_OK;
+	count = gf_bs_read_int(bs, 8);
+	if (count*2 > ptr->size) {
+		//ffmpeg just ignores this part while allocating bytes (filled with garbage?)
+		GF_LOG(GF_LOG_WARNING, GF_LOG_CODING, ("AVCC: invalid numOfSequenceParameterSetExt value. Skipping.\n"));
+		return GF_OK;
+	}
+	if (count) {
+		ptr->config->sequenceParameterSetExtensions = gf_list_new();
+		for (i=0; i<count; i++) {
+			ISOM_DECREASE_SIZE(ptr, 2)
+			GF_NALUFFParam *sl = (GF_NALUFFParam *)gf_malloc(sizeof(GF_NALUFFParam));
+			sl->size = gf_bs_read_u16(bs);
+			if ((gf_bs_available(bs) < sl->size) || (ptr->size<sl->size)) {
+				gf_free(sl);
+				GF_LOG(GF_LOG_ERROR, GF_LOG_CODING, ("AVCC: Not enough bits to parse. Aborting.\n"));
+				return GF_ISOM_INVALID_FILE;
 			}
-			if (count) {
-				ptr->config->sequenceParameterSetExtensions = gf_list_new();
-				for (i=0; i<count; i++) {
-					ISOM_DECREASE_SIZE(ptr, 2)
-					GF_NALUFFParam *sl = (GF_NALUFFParam *)gf_malloc(sizeof(GF_NALUFFParam));
-					sl->size = gf_bs_read_u16(bs);
-					if ((gf_bs_available(bs) < sl->size) || (ptr->size<sl->size)) {
-						gf_free(sl);
-						GF_LOG(GF_LOG_ERROR, GF_LOG_CODING, ("AVCC: Not enough bits to parse. Aborting.\n"));
-						return GF_ISOM_INVALID_FILE;
-					}
-					sl->data = (char *)gf_malloc(sizeof(char) * sl->size);
-					gf_bs_read_data(bs, sl->data, sl->size);
-					gf_list_add(ptr->config->sequenceParameterSetExtensions, sl);
-					ptr->size -= sl->size;
-				}
-			}
+			sl->data = (char *)gf_malloc(sizeof(char) * sl->size);
+			gf_bs_read_data(bs, sl->data, sl->size);
+			gf_list_add(ptr->config->sequenceParameterSetExtensions, sl);
+			ptr->size -= sl->size;
 		}
 	}
 	return GF_OK;
