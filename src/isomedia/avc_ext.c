9b8c7b7a4ad25dc93121ff616ec4e125aa5906a1
jeanlf <jeanlf@gpac.io>
2021-05-17 16:45:19 +0200

    fixed #1792

--- src/isomedia/avc_ext.c
+++ src/isomedia/avc_ext.c
@@ -2420,7 +2420,7 @@ GF_VPConfig *gf_isom_vp_config_get(GF_ISOFile *the_file, u32 trackNumber, u32 De
 	trak = gf_isom_get_track_from_file(the_file, trackNumber);
 	if (!trak || !trak->Media || !DescriptionIndex) return NULL;
 	entry = (GF_MPEGVisualSampleEntryBox*)gf_list_get(trak->Media->information->sampleTable->SampleDescription->child_boxes, DescriptionIndex - 1);
-	if (!entry || !entry->vp_config) return NULL;
+	if (!entry || !entry->vp_config || !entry->vp_config->config) return NULL;
 	return VP_DuplicateConfig(entry->vp_config->config);
 }
 
