gpac/gpac

commit 42a291da6f54133040baa21ea560b4ceb40e3d9e

added image data ref and image item import for HEIF

src/isomedia/meta.c
@@ -1302,6 +1302,13 @@ GF_Err gf_isom_add_meta_item_extended(GF_ISOFile *file, Bool root_meta, u32 trac
 	
 	if (tk_id && sample_num) {
 		data_len = gf_isom_get_sample_size(file, tk_id, sample_num);
+		if (item_name)
+			infe->item_name = gf_strdup(item_name);
+	}
+	else if (!tk_id && sample_num) {
+		data_len = 0;
+		if (item_name)
+			infe->item_name = gf_strdup(item_name);
 	}
 	/*get relative name*/
 	else if (item_name) {
@@ -1498,7 +1505,18 @@ GF_Err gf_isom_add_meta_item_extended(GF_ISOFile *file, Bool root_meta, u32 trac
 			infe->data_len = data_len;
 			file->no_inplace_rewrite = GF_TRUE;
 		}
-		meta->use_item_sample_sharing = GF_TRUE;
+		meta->use_item_sample_sharing = 1;
+	}
+	else if (!tk_id && sample_num) {
+		GF_ItemExtentEntry *entry;
+		GF_SAFEALLOC(entry, GF_ItemExtentEntry);
+		if (!entry) return GF_OUT_OF_MEM;
+
+		entry->extent_length = data_len;
+		gf_list_add(location_entry->extent_entries, entry);
+		infe->ref_it_id = sample_num;
+		infe->data_len = data_len;
+		meta->use_item_item_sharing = 1;
 	}
 	else if (data || resource_path){
 		/*capture mode, write to disk*/
@@ -1595,7 +1613,10 @@ GF_Err gf_isom_remove_meta_item(GF_ISOFile *file, Bool root_meta, u32 track_num,
 	if (!meta || !meta->item_infos || !meta->item_locations) return GF_BAD_PARAM;
 
 	item_num = gf_isom_get_meta_item_by_id(file, root_meta, track_num, item_id);
-	if (!item_num) return GF_BAD_PARAM;
+	if (!item_num) {
+		GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("Error: No item with ID %d in file, cannnot remove\n"));
+		return GF_NOT_FOUND;
+	}
 	iinf = (GF_ItemInfoEntryBox *)gf_list_get(meta->item_infos->item_infos, item_num-1);
 	gf_list_rem(meta->item_infos->item_infos, item_num-1);
 
@@ -1611,6 +1632,43 @@ GF_Err gf_isom_remove_meta_item(GF_ISOFile *file, Bool root_meta, u32 track_num,
 			break;
 		}
 	}
+
+	if (meta->item_props->property_association) {
+		GF_ItemPropertyAssociationBox *ipma = meta->item_props->property_association;
+		count = gf_list_count(ipma->entries);
+		for (i=0; i<count; i++) {
+			GF_ItemPropertyAssociationEntry *pa_ent = gf_list_get(ipma->entries, i);
+			if (pa_ent->item_id == iinf->item_ID) {
+				gf_free(pa_ent->associations);
+				gf_free(pa_ent);
+				gf_list_rem(ipma->entries, i);
+				break;
+			}
+		}
+	}
+
+	//rewrite item sharing ids
+	if (meta->use_item_item_sharing) {
+		u32 new_ref_id = 0;
+		count = gf_list_count(meta->item_infos->item_infos);
+		for (i=0; i<count; i++) {
+			GF_ItemInfoEntryBox *iinf2 = (GF_ItemInfoEntryBox *) gf_list_get(meta->item_infos->item_infos, i);
+			if (iinf2->ref_it_id != iinf->item_ID) continue;
+
+			if (new_ref_id) {
+				iinf2->ref_it_id = new_ref_id;
+			} else {
+				new_ref_id = iinf2->item_ID;
+				iinf2->ref_it_id = 0;
+				if (iinf->tk_id) {
+					iinf2->tk_id = iinf->tk_id;
+					iinf2->sample_num = iinf->sample_num;
+					iinf2->data_len = iinf->data_len;
+				}
+			}
+		}
+	}
+
 	gf_isom_box_del_parent(&meta->item_infos->child_boxes, (GF_Box *)iinf);
 	return GF_OK;
 }
@@ -1683,7 +1741,7 @@ void gf_isom_meta_restore_items_ref(GF_ISOFile *movie, GF_MetaBox *meta)
 {
 	u32 i, nb_items, nb_tracks;
 	if (!meta->item_locations || !meta->item_infos) return;
-	nb_tracks = gf_list_count(movie->moov->trackList);
+	nb_tracks = movie->moov ? gf_list_count(movie->moov->trackList) : 0;
 	nb_items = gf_list_count(meta->item_locations->location_entries);
 	for (i=0; i<nb_items; i++) {
 		u32 j;
@@ -1698,6 +1756,8 @@ void gf_isom_meta_restore_items_ref(GF_ISOFile *movie, GF_MetaBox *meta)
 			iinf = NULL;
 		}
 		if (!iinf) continue;
+		if (iinf->ref_it_id) continue;
+
 		if (gf_list_count(iloc->extent_entries) != 1) continue;
 		entry = (GF_ItemExtentEntry *)gf_list_get(iloc->extent_entries, 0);
 		if (!entry) continue;
@@ -1712,13 +1772,13 @@ void gf_isom_meta_restore_items_ref(GF_ISOFile *movie, GF_MetaBox *meta)
 				continue;
 
 			stsz = trak->Media->information->sampleTable->SampleSize;
-			if (stsz->sampleSize) continue;
 			if (!stsz->sampleCount) continue;
 			for (k=0; k<stsz->sampleCount; k++) {
 				GF_Err e;
-				u32 chunk, di;
+				u32 chunk, di, samp_size;
 				u64 samp_offset;
-				if (stsz->sizes[k] != entry->extent_length)
+				samp_size = stsz->sampleSize ? stsz->sampleSize : stsz->sizes[k];
+				if (samp_size != entry->extent_length)
 					continue;
 
 				e = stbl_GetSampleInfos(trak->Media->information->sampleTable, k+1, &samp_offset, &chunk, &di, NULL);
@@ -1727,14 +1787,38 @@ void gf_isom_meta_restore_items_ref(GF_ISOFile *movie, GF_MetaBox *meta)
 					iinf->tk_id = trak->Header->trackID;
 					iinf->sample_num = k+1;
 					iinf->data_len = (u32) entry->extent_length;
-					meta->use_item_sample_sharing = GF_TRUE;
+					meta->use_item_sample_sharing = 1;
 					break;
 				}
 			}
 			if (iinf->tk_id) break;
 		}
-	}
 
+		//restore all item refs
+		for (j=i+1;j<nb_items; j++) {
+			u32 k;
+			u64 item_offset2;
+			GF_ItemExtentEntry *entry2;
+			GF_ItemLocationEntry *iloc2 = (GF_ItemLocationEntry *)gf_list_get(meta->item_locations->location_entries, j);
+			/*get item info*/
+			GF_ItemInfoEntryBox *iinf2 = NULL;
+			k=0;
+			while ((iinf2 = (GF_ItemInfoEntryBox *)gf_list_enum(meta->item_infos->item_infos, &k))) {
+				if (iinf2->item_ID==iloc2->item_ID) break;
+				iinf2 = NULL;
+			}
+			if (!iinf2) continue;
+			if (gf_list_count(iloc2->extent_entries) != 1) continue;
+			entry2 = (GF_ItemExtentEntry *)gf_list_get(iloc2->extent_entries, 0);
+			if (!entry2) continue;
+			item_offset2 = iloc2->base_offset + entry2->extent_offset;
+
+			if (item_offset == item_offset2) {
+				iinf2->ref_it_id = iinf->item_ID;
+				meta->use_item_item_sharing = 1;
+			}
+		}
+	}
 }
 
 GF_EXPORT
@@ -1781,6 +1865,45 @@ GF_Err gf_isom_meta_add_item_group(GF_ISOFile *file, Bool root_meta, u32 track_n
 	group->entity_id_count++;
 
 	return GF_OK;
+}
+
+GF_EXPORT
+u32 gf_isom_meta_get_item_ref_count(GF_ISOFile *file, Bool root_meta, u32 track_num, u32 from_id, u32 type)
+{
+	u32 i, count;
+	GF_ItemReferenceTypeBox *ref;
+	GF_MetaBox *meta = gf_isom_get_meta(file, root_meta, track_num);
+	if (!meta || !type || !from_id) return 0;
+	if (!meta->item_refs) return 0;
+
+	count = gf_list_count(meta->item_refs->references);
+	for (i = 0; i < count; i++) {
+		ref = (GF_ItemReferenceTypeBox *)gf_list_get(meta->item_refs->references, i);
+		if (ref->from_item_id == from_id && ref->reference_type == type) {
+			return ref->reference_count;
+		}
+	}
+	return 0;
+}
+
+GF_EXPORT
+u32 gf_isom_meta_get_item_ref_id(GF_ISOFile *file, Bool root_meta, u32 track_num, u32 from_id, u32 type, u32 ref_idx)
+{
+	u32 i, count;
+	GF_ItemReferenceTypeBox *ref;
+	GF_MetaBox *meta = gf_isom_get_meta(file, root_meta, track_num);
+	if (!meta || !type || !from_id || !ref_idx) return 0;
+	if (!meta->item_refs) return 0;
+
+	count = gf_list_count(meta->item_refs->references);
+	for (i = 0; i < count; i++) {
+		ref = (GF_ItemReferenceTypeBox *)gf_list_get(meta->item_refs->references, i);
+		if (ref->from_item_id == from_id && ref->reference_type == type) {
+			if (ref_idx>ref->reference_count) return 0;
+			return ref->to_item_IDs[ref_idx-1];
+		}
+	}
+	return 0;
 }
 
 #endif /*GPAC_DISABLE_ISOM*/

applications/mp4box/main.c
@@ -1188,9 +1188,10 @@ MP4BoxArg m4b_meta_args[] =
 		"- icc_path: path to icc data to add as color info\n"
 		"- alpha: indicate that the image is an alpha image (should use ref=auxl also)\n"
 		"- depth: indicate that the image is a depth image (should use ref=auxl also)\n"
+		"- it=ID: indicate the item ID of the source item to import\n"
 		"- tk=tkID: indicate the track ID of the source sample. If 0, uses the first video track in the file\n"
 		"- samp=N: indicate the sample number of the source sample\n"
-		"- ref: do not copy the data but refer to the final sample location\n"
+		"- ref: do not copy the data but refer to the final sample/item location, ignored if `filepath` is set\n"
 		"- agrid[=AR]: creates an automatic grid from the image items present in the file, in their declaration order. The grid will **try to** have `AR` aspect ratio if specified (float), or the aspect ratio of the source otherwise. The grid will be the primary item and all other images will be hidden\n"
 		"- av1_op_index: select the AV1 operating point to use via a1op box\n"
 		"- any other options will be passed as options to the media importer, see [-add]()"
@@ -1718,7 +1719,8 @@ static u32 parse_meta_args(char *opts, MetaActionType act_type)
 
 		if (!strnicmp(szSlot, "tk=", 3)) {
 			sscanf(szSlot, "tk=%u", &meta->trackID);
-			meta->root_meta = 0;
+			if (act_type == META_ACTION_ADD_ITEM)
+				meta->root_meta = 0;
 		}
 		else if (!strnicmp(szSlot, "id=", 3)) {
 			meta->item_id = atoi(szSlot+3);
@@ -1893,6 +1895,13 @@ static u32 parse_meta_args(char *opts, MetaActionType act_type)
 				if (!meta->image_props) return 2;
 			}
 			meta->image_props->use_reference = GF_TRUE;
+		}
+		else if (!strnicmp(szSlot, "it=", 3)) {
+			if (!meta->image_props) {
+				GF_SAFEALLOC(meta->image_props, GF_ImageItemProperties);
+				if (!meta->image_props) return 2;
+			}
+			meta->image_props->item_ref_id = atoi(szSlot+3);
 		}
 		else if (!strnicmp(szSlot, "time=", 5)) {
 			Float s=0, e=0, step=0;
@@ -4801,6 +4810,8 @@ static GF_Err do_meta_act()
 					meta->image_props->src_file = gf_isom_open(meta->szPath, GF_ISOM_OPEN_READ, NULL);
 					e = gf_isom_last_error(meta->image_props->src_file);
 					fsrc = meta->image_props->src_file;
+					if (meta->image_props->item_ref_id)
+						src_tk_id = 0;
 				} else {
 					e = import_file(file, meta->szPath, 0, _frac, 0, NULL, NULL, 0);
 				}
@@ -4823,7 +4834,7 @@ static GF_Err do_meta_act()
 						e = gf_isom_meta_get_next_item_id(file, meta->root_meta, tk, &meta->item_id);
 					}
 					if (e == GF_OK) {
-						if (!src_tk_id) {
+						if (!src_tk_id && (!meta->image_props || !meta->image_props->item_ref_id) ) {
 							u32 j;
 							for (j=0; j<gf_isom_get_track_count(fsrc); j++) {
 								if (gf_isom_is_video_handler_type (gf_isom_get_media_type(fsrc, j+1))) {

include/gpac/internal/isomedia_dev.h
@@ -2365,7 +2365,7 @@ typedef struct
 	// if not 0, full_path is actually the data to write.
 	u32 data_len;
 
-	u32 tk_id, sample_num;
+	u32 tk_id, sample_num, ref_it_id;
 } GF_ItemInfoEntryBox;
 
 typedef struct
@@ -2489,7 +2489,7 @@ typedef struct __tag_meta_box
 	GF_ItemReferenceBox *item_refs;
 	GF_GroupListBox *groups_list;
 
-	Bool use_item_sample_sharing;
+	u8 use_item_sample_sharing, use_item_item_sharing;
 } GF_MetaBox;
 
 typedef struct

include/gpac/isomedia.h
@@ -5816,6 +5816,25 @@ GF_Err gf_isom_extract_meta_item_get_cenc_info(GF_ISOFile *isom_file, Bool root_
 \return primary item ID, 0 if none found (primary can also be stored through meta XML)*/
 u32 gf_isom_get_meta_primary_item_id(GF_ISOFile *isom_file, Bool root_meta, u32 track_num);
 
+/*! gets number of references of a given type from a given item ID
+\param isom_file the target ISO file
+\param root_meta if GF_TRUE uses meta at the file, otherwise uses meta at the movie level if track number is 0
+\param track_num if GF_TRUE and root_meta is GF_FALSE, uses meta at the track level
+\param from_id item ID to check
+\param type reference type to check
+\return number of referenced items*/
+u32 gf_isom_meta_get_item_ref_count(GF_ISOFile *isom_file, Bool root_meta, u32 track_num, u32 from_id, u32 type);
+
+/*! gets ID  of reference of a given type and index from a given item ID
+\param isom_file the target ISO file
+\param root_meta if GF_TRUE uses meta at the file, otherwise uses meta at the movie level if track number is 0
+\param track_num if GF_TRUE and root_meta is GF_FALSE, uses meta at the track level
+\param from_id item ID to check
+\param type reference type to check
+\param ref_idx 1-based index of reference to check
+\return ID if the refered item*/
+u32 gf_isom_meta_get_item_ref_id(GF_ISOFile *isom_file, Bool root_meta, u32 track_num, u32 from_id, u32 type, u32 ref_idx);
+
 /*! item tile mode*/
 typedef enum {
 	/*! not a tile item*/
@@ -5913,13 +5932,16 @@ typedef struct
 	GF_ImageItemProtection *cenc_info;
 	/*! If set, reference image from sample sample_num (same file data used for sample and item)*/
 	Bool use_reference;
+	/*ID of item to use as source*/
+	u32 item_ref_id;
 	/*only set when importing non-ref from ISOBMF*/
 	GF_ISOFile *src_file;
 	Bool auto_grid;
 	Double auto_grid_ratio;
-	/*AV1 specific data*/
+	/*AV1 layer sizes except last layer - set during import*/
 	u32 av1_layer_size[3];
-  u8 av1_op_index;
+	/*AV1 operation point index*/
+	u8 av1_op_index;
 } GF_ImageItemProperties;
 
 
@@ -6120,7 +6142,7 @@ GF_Err gf_isom_meta_add_item_group(GF_ISOFile *isom_file, Bool root_meta, u32 tr
 
 #endif /*GPAC_DISABLE_ISOM_WRITE*/
 
-/*!
+/*! gets image item properties
 \param isom_file the target ISO file
 \param root_meta if GF_TRUE uses meta at the file, otherwise uses meta at the movie level if track number is 0
 \param track_num if GF_TRUE and root_meta is GF_FALSE, uses meta at the track level

share/doc/man/gpac-filters.1
@@ -1214,6 +1214,8 @@ This filter demultiplexes OGG files/data into a set of media PIDs and frames.
 .br
 index (dbl, default: 1.0):     indexing window length (unimplemented, only 0 disables stream probing for duration), 
 .br
+expart (bool, default: false): expose pictures as a dedicated video pid
+.br
 
 .br
 .SH vorbisdec
@@ -7691,7 +7693,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev919-g2715401c9-master
+build: 1.1.0-DEV-rev933-g4b501c165-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -4066,7 +4066,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev919-g2715401c9-master
+build: 1.1.0-DEV-rev933-g4b501c165-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2389,11 +2389,13 @@ add the given file as HEIF image item, with parameter syntax file_path[:opt1:opt
 .br
 * depth: indicate that the image is a depth image (should use ref=auxl also)
 .br
+* it=ID: indicate the item ID of the source item to import
+.br
 * tk=tkID: indicate the track ID of the source sample. If 0, uses the first video track in the file
 .br
 * samp=N: indicate the sample number of the source sample
 .br
-* ref: do not copy the data but refer to the final sample location
+* ref: do not copy the data but refer to the final sample/item location, ignored if filepath is set
 .br
 * agrid[=AR]: creates an automatic grid from the image items present in the file, in their declaration order. The grid will try to have AR aspect ratio if specified (float), or the aspect ratio of the source otherwise. The grid will be the primary item and all other images will be hidden
 .br
@@ -2772,7 +2774,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev919-g2715401c9-master
+build: 1.1.0-DEV-rev933-g4b501c165-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4client.1
@@ -261,7 +261,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev919-g2715401c9-master
+build: 1.1.0-DEV-rev933-g4b501c165-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

commit 171d15f1913ff9bf0cb586d3482ab79687ed6782

fixed crash introduced by 42a291da

src/isomedia/meta.c
@@ -1633,7 +1633,7 @@ GF_Err gf_isom_remove_meta_item(GF_ISOFile *file, Bool root_meta, u32 track_num,
 		}
 	}
 
-	if (meta->item_props->property_association) {
+	if (meta->item_props && meta->item_props->property_association) {
 		GF_ItemPropertyAssociationBox *ipma = meta->item_props->property_association;
 		count = gf_list_count(ipma->entries);
 		for (i=0; i<count; i++) {