gpac/gpac

commit 528d8ee3d7952b3f90877488c8ad8a81d721bf05

fixed HLS M2TS demux droping AAC frame at seg boundaries

include/gpac/mpegts.h
@@ -866,6 +866,8 @@ typedef struct tag_m2ts_pes
 	GF_M2TS_TemiTimecodeDescriptor temi_tc;
 	/*! flag set to indicate a TEMI descriptor should be flushed with next packet*/
 	Bool temi_pending;
+	/*! flag set to indicate the last PES packet was not flushed (HLS) to avoid warning on same PTS/DTS used*/
+	Bool is_resume;
 } GF_M2TS_PES;
 
 /*! reserved streamID for PES headers*/
@@ -1180,8 +1182,9 @@ GF_M2TS_SDT *gf_m2ts_get_sdt_info(GF_M2TS_Demuxer *demux, u32 program_id);
 /*! flushes a given stream. This is used to flush internal demultiplexer buffers on end of stream
 \param demux the target MPEG-2 demultiplexer
 \param pes the target stream to flush
+\param force_flush if GF_TRUE, flushes all streams, otherwise do not flush stream with known PES length and not yet completed (for HLS)
 */
-void gf_m2ts_flush_pes(GF_M2TS_Demuxer *demux, GF_M2TS_PES *pes);
+void gf_m2ts_flush_pes(GF_M2TS_Demuxer *demux, GF_M2TS_PES *pes, Bool force_flush);
 
 /*! flushes all streams in the mux. This is used to flush internal demultiplexer buffers on end of stream
 \param demux the target MPEG-2 demultiplexer

src/media_tools/mpegts.c
@@ -1989,7 +1989,7 @@ static void gf_m2ts_store_temi(GF_M2TS_Demuxer *ts, GF_M2TS_PES *pes)
 	pes->temi_pending = 1;
 }
 
-void gf_m2ts_flush_pes(GF_M2TS_Demuxer *ts, GF_M2TS_PES *pes)
+void gf_m2ts_flush_pes(GF_M2TS_Demuxer *ts, GF_M2TS_PES *pes, Bool force_flush)
 {
 	GF_M2TS_PESHeader pesh;
 	if (!ts) return;
@@ -2035,27 +2035,29 @@ void gf_m2ts_flush_pes(GF_M2TS_Demuxer *ts, GF_M2TS_PES *pes)
 			if (pesh.PTS) {
 				if (pesh.PTS == pes->PTS) {
 					same_pts = GF_TRUE;
-					GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[MPEG-2 TS] PID %d - same PTS "LLU" for two consecutive PES packets \n", pes->pid, pes->PTS));
+					if (!pes->is_resume) {
+						GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[MPEG-2 TS] PID %d - same PTS "LLU" for two consecutive PES packets \n", pes->pid, pes->PTS));
+					}
 				}
-	#ifndef GPAC_DISABLE_LOG
+#ifndef GPAC_DISABLE_LOG
 				/*FIXME - this test should only be done for non bi-directionally coded media
 				else if (pesh.PTS < pes->PTS) {
 					GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[MPEG-2 TS] PID %d - PTS "LLU" less than previous packet PTS "LLU"\n", pes->pid, pesh.PTS, pes->PTS) );
 				}
 				*/
-	#endif
+#endif
 
 				pes->PTS = pesh.PTS;
-	#ifndef GPAC_DISABLE_LOG
+#ifndef GPAC_DISABLE_LOG
 				{
-					if (pes->DTS && (pesh.DTS == pes->DTS)) {
+					if (!pes->is_resume && pes->DTS && (pesh.DTS == pes->DTS)) {
 						GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[MPEG-2 TS] PID %d - same DTS "LLU" for two consecutive PES packets \n", pes->pid, pes->DTS));
 					}
 					if (pesh.DTS < pes->DTS) {
 						GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[MPEG-2 TS] PID %d - DTS "LLU" less than previous DTS "LLU"\n", pes->pid, pesh.DTS, pes->DTS));
 					}
 				}
-	#endif
+#endif
 				pes->DTS = pesh.DTS;
 			}
 			/*no PTSs were coded, same time*/
@@ -2063,6 +2065,7 @@ void gf_m2ts_flush_pes(GF_M2TS_Demuxer *ts, GF_M2TS_PES *pes)
 				same_pts = GF_TRUE;
 			}
 
+			pes->is_resume = GF_FALSE;
 
 			/*3-byte start-code + 6 bytes header + hdr extensions*/
 			len = 9 + pesh.hdr_data_len;
@@ -2091,6 +2094,11 @@ void gf_m2ts_flush_pes(GF_M2TS_Demuxer *ts, GF_M2TS_PES *pes)
 			u32 offset = len;
 
 			if (pesh.pck_len && (pesh.pck_len-3-pesh.hdr_data_len != pes->pck_data_len-len)) {
+				if (!force_flush) {
+					pes->is_resume = GF_TRUE;
+					return;
+				}
+
 				GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[MPEG-2 TS] PID %d PES payload size %d but received %d bytes\n", pes->pid, (u32) ( pesh.pck_len-3-pesh.hdr_data_len), pes->pck_data_len-len));
 			}
 			//copy over the remaining of previous PES payload before start of this PES payload
@@ -2204,7 +2212,7 @@ static void gf_m2ts_process_pes(GF_M2TS_Demuxer *ts, GF_M2TS_PES *pes, GF_M2TS_H
 
 	/*PES first fragment: flush previous packet*/
 	if (flush_pes && pes->pck_data_len) {
-		gf_m2ts_flush_pes(ts, pes);
+		gf_m2ts_flush_pes(ts, pes, GF_TRUE);
 		if (!data_size) return;
 	}
 	/*we need to wait for first packet of PES*/
@@ -2226,7 +2234,7 @@ static void gf_m2ts_process_pes(GF_M2TS_Demuxer *ts, GF_M2TS_PES *pes, GF_M2TS_H
 		GF_LOG(GF_LOG_DEBUG, GF_LOG_CONTAINER, ("[MPEG-2 TS] PID %d: Got PES packet len %d\n", pes->pid, pes->pes_len));
 
 		if (pes->pes_len + 6 == pes->pck_data_len) {
-			gf_m2ts_flush_pes(ts, pes);
+			gf_m2ts_flush_pes(ts, pes, GF_TRUE);
 		}
 	}
 }
@@ -2237,7 +2245,7 @@ void gf_m2ts_flush_all(GF_M2TS_Demuxer *ts)
 	for (i=0; i<GF_M2TS_MAX_STREAMS; i++) {
 		GF_M2TS_ES *stream = ts->ess[i];
 		if (stream && (stream->flags & GF_M2TS_ES_IS_PES)) {
-			gf_m2ts_flush_pes(ts, (GF_M2TS_PES *) stream);
+			gf_m2ts_flush_pes(ts, (GF_M2TS_PES *) stream, GF_FALSE);
 		}
 	}
 }

src/filters/mux_ts.c
@@ -157,6 +157,7 @@ typedef struct
 
 	u32 sid;
 	u32 codec_id;
+	u32 dsi_crc;
 	u32 pmt_pid;
 	u32 nb_pck;
 	Bool is_repeat;
@@ -681,6 +682,7 @@ void update_m4sys_info(GF_TSMuxCtx *ctx, GF_M2TS_Mux_Program *prog)
 static void tsmux_setup_esi(GF_TSMuxCtx *ctx, GF_M2TS_Mux_Program *prog, M2Pid *tspid, u32 stream_type)
 {
 	const GF_PropertyValue *p;
+	GF_ESInterface bckp = tspid->esi;
 
 	memset(&tspid->esi, 0, sizeof(GF_ESInterface));
 	tspid->esi.stream_type = stream_type;
@@ -692,6 +694,7 @@ static void tsmux_setup_esi(GF_TSMuxCtx *ctx, GF_M2TS_Mux_Program *prog, M2Pid *
 	if (p) {
 		tspid->esi.decoder_config = p->value.data.ptr;
 		tspid->esi.decoder_config_size = p->value.data.size;
+		tspid->dsi_crc = gf_crc_32(p->value.data.ptr, p->value.data.size);
 	}
 	p = gf_filter_pid_get_property(tspid->ipid, GF_PROP_PID_ID);
 	if (p) tspid->esi.stream_id = p->value.uint;
@@ -745,6 +748,9 @@ static void tsmux_setup_esi(GF_TSMuxCtx *ctx, GF_M2TS_Mux_Program *prog, M2Pid *
 	tspid->esi.input_ctrl = tsmux_esi_ctrl;
 	tspid->esi.input_udta = tspid;
 	tspid->prog = prog;
+
+	tspid->esi.output_ctrl = bckp.output_ctrl;
+	tspid->esi.output_udta = bckp.output_udta;
 }
 
 static void tsmux_setup_temi(GF_TSMuxCtx *ctx, M2Pid *tspid)
@@ -1028,8 +1034,15 @@ static GF_Err tsmux_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_
 		GF_LOG(GF_LOG_INFO, GF_LOG_CONTAINER, ("[M2TSMux] Setting up program ID %d - send rates: PSI %d ms PCR every %d ms max - PCR offset %d\n", service_id, ctx->pmt_rate, ctx->max_pcr, ctx->pcr_offset));
 	}
 	//no changes in codec ID or stream type
-	if ((tspid->codec_id == codec_id) && (tspid->esi.stream_type == streamtype))
-		return GF_OK;
+	if ((tspid->codec_id == codec_id) && (tspid->esi.stream_type == streamtype)) {
+		u32 crc = 0;
+		p = gf_filter_pid_get_property(pid, GF_PROP_PID_DECODER_CONFIG);
+		if (p && p->value.data.ptr) {
+			crc = gf_crc_32(p->value.data.ptr, p->value.data.size);
+		}
+		if (crc == tspid->dsi_crc) return GF_OK;
+		tspid->dsi_crc = crc;
+	}
 
 	if (!tspid->codec_id) {
 		Bool is_pcr=GF_FALSE;

src/filters/reframe_adts.c
@@ -822,7 +822,7 @@ GF_Err adts_dmx_process(GF_Filter *filter)
 
 		//truncated last frame
 		if (bytes_to_drop>remain) {
-			GF_LOG(GF_LOG_WARNING, GF_LOG_PARSER, ("[ADTSDmx] truncated ADTS frame!\n"));
+			GF_LOG(GF_LOG_WARNING, GF_LOG_PARSER, ("[ADTSDmx] truncated ADTS frame %d bytes but only %d left!\n", bytes_to_drop, remain));
 			bytes_to_drop=remain;
 		}
 

commit 99c6dbd9bbca67a25705858a58fba3157514a72b

fixed bug introduced by 528d8ee3

include/gpac/mpegts.h
@@ -1188,8 +1188,9 @@ void gf_m2ts_flush_pes(GF_M2TS_Demuxer *demux, GF_M2TS_PES *pes, Bool force_flus
 
 /*! flushes all streams in the mux. This is used to flush internal demultiplexer buffers on end of stream
 \param demux the target MPEG-2 demultiplexer
+\param no_force_flush do not force a flush of incomplete PES (used for HLS)
 */
-void gf_m2ts_flush_all(GF_M2TS_Demuxer *demux);
+void gf_m2ts_flush_all(GF_M2TS_Demuxer *demux, Bool no_force_flush);
 
 
 /*! MPEG-2 TS packet header*/

src/filters/dmx_m2ts.c
@@ -85,6 +85,8 @@ typedef struct
 
 	u32 mux_tune_state;
 	u32 wait_for_progs;
+
+	Bool is_dash;
 } GF_M2TSDmxCtx;
 
 
@@ -1006,6 +1008,8 @@ static Bool m2tsdmx_process_event(GF_Filter *filter, const GF_FilterEvent *com)
 			}
 			return GF_FALSE;
 		}
+		if (com->play.no_byterange_forward)
+			ctx->is_dash = GF_TRUE;
 		/*mark pcr as not initialized*/
 		if (pes->program->pcr_pid==pes->pid) pes->program->first_dts=0;
 		gf_m2ts_set_pes_framing(pes, GF_M2TS_PES_FRAMING_DEFAULT);
@@ -1113,7 +1117,7 @@ static GF_Err m2tsdmx_process(GF_Filter *filter)
 		if (gf_filter_pid_is_eos(ctx->ipid)) {
 			u32 i, nb_streams = gf_filter_get_opid_count(filter);
 
-			gf_m2ts_flush_all(ctx->ts);
+			gf_m2ts_flush_all(ctx->ts, ctx->is_dash);
 			for (i=0; i<nb_streams; i++) {
 				GF_FilterPid *opid = gf_filter_get_opid(filter, i);
 				gf_filter_pid_set_eos(opid);

src/media_tools/mpegts.c
@@ -2239,13 +2239,13 @@ static void gf_m2ts_process_pes(GF_M2TS_Demuxer *ts, GF_M2TS_PES *pes, GF_M2TS_H
 	}
 }
 
-void gf_m2ts_flush_all(GF_M2TS_Demuxer *ts)
+void gf_m2ts_flush_all(GF_M2TS_Demuxer *ts, Bool no_force_flush)
 {
 	u32 i;
 	for (i=0; i<GF_M2TS_MAX_STREAMS; i++) {
 		GF_M2TS_ES *stream = ts->ess[i];
 		if (stream && (stream->flags & GF_M2TS_ES_IS_PES)) {
-			gf_m2ts_flush_pes(ts, (GF_M2TS_PES *) stream, GF_FALSE);
+			gf_m2ts_flush_pes(ts, (GF_M2TS_PES *) stream, no_force_flush ? GF_FALSE : GF_TRUE);
 		}
 	}
 }