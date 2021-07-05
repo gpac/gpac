gpac/gpac

commit 11a3f75f0c64b938317e5a3747a958a9a834acd8

fixed possible overflows for large timestamps and timescales

src/utils/constants.c
@@ -1940,3 +1940,118 @@ const char *gf_cicp_color_matrix_all_names()
 	}
 	return szCICPMXAllNames;
 }
+
+
+GF_EXPORT
+u64 gf_timestamp_rescale(u64 value, u64 timescale, u64 new_timescale)
+{
+	if (!timescale || !new_timescale)
+		return 0;
+	//no timestamp
+	if (value==0xFFFFFFFFFFFFFFFFUL)
+		return value;
+
+	if (new_timescale == timescale)
+		return value;
+		
+	if (! (new_timescale % timescale)) {
+		u32 div = new_timescale / timescale;
+		return value * div;
+	}
+	if (! (timescale % new_timescale)) {
+		u32 div = timescale / new_timescale;
+		return value / div;
+	}
+
+	if (value <= GF_INT_MAX) {
+		return (value * new_timescale) / timescale;
+	}
+
+	u64 int_part = value / timescale;
+	u64 frac_part = (value % timescale * new_timescale) / timescale;
+	if (int_part >= GF_INT_MAX) {
+		Double res = value;
+		res *= new_timescale;
+		res /= timescale;
+		return (u64) res;
+	}
+	return int_part * new_timescale + frac_part;
+}
+
+GF_EXPORT
+s64 gf_timestamp_rescale_signed(s64 value, u64 timescale, u64 new_timescale)
+{
+	if (!timescale || !new_timescale)
+		return 0;
+
+	if (! (new_timescale % timescale)) {
+		u32 div = new_timescale / timescale;
+		return value * div;
+	}
+	if (! (timescale % new_timescale)) {
+		u32 div = timescale / new_timescale;
+		return value / div;
+	}
+
+	if (value <= GF_INT_MAX) {
+		return (value * new_timescale) / timescale;
+	}
+
+	s64 int_part = value / timescale;
+	u64 frac_part = ((value % timescale) * new_timescale) / timescale;
+	if ((int_part >= GF_INT_MAX) || (int_part <= GF_INT_MIN)) {
+		Double res = value;
+		res *= new_timescale;
+		res /= timescale;
+		return (s64) res;
+	}
+	return int_part * new_timescale + frac_part;
+}
+
+#define TIMESTAMP_COMPARE(_op) \
+	if (timescale1==timescale2) { \
+		return (value1 _op value2); \
+	} \
+	\
+	if ((value1 <= GF_INT_MAX) && (value2 <= GF_INT_MAX)) { \
+		return (value1 * timescale2 _op value2 * timescale1); \
+	} \
+	\
+	if ((value1==0xFFFFFFFFFFFFFFFFUL) || (value2==0xFFFFFFFFFFFFFFFFUL)) \
+		return GF_FALSE; \
+	\
+	if (!timescale1 || !timescale2) return GF_FALSE; \
+	\
+	u64 v1_rescale = gf_timestamp_rescale(value1, timescale1, timescale2); \
+	return (v1_rescale _op value2); \
+
+
+GF_EXPORT
+Bool gf_timestamp_less(u64 value1, u64 timescale1, u64 value2, u64 timescale2)
+{
+	TIMESTAMP_COMPARE(<)
+}
+
+GF_EXPORT
+Bool gf_timestamp_less_or_equal(u64 value1, u64 timescale1, u64 value2, u64 timescale2)
+{
+	TIMESTAMP_COMPARE(<=)
+}
+
+GF_EXPORT
+Bool gf_timestamp_greater(u64 value1, u64 timescale1, u64 value2, u64 timescale2)
+{
+	TIMESTAMP_COMPARE(>)
+}
+
+GF_EXPORT
+Bool gf_timestamp_greater_or_equal(u64 value1, u64 timescale1, u64 value2, u64 timescale2)
+{
+	TIMESTAMP_COMPARE(>=)
+}
+
+GF_EXPORT
+Bool gf_timestamp_equal(u64 value1, u64 timescale1, u64 value2, u64 timescale2)
+{
+	TIMESTAMP_COMPARE(==)
+}

applications/mp4box/fileimport.c
@@ -2270,7 +2270,7 @@ GF_Err cat_isomedia_file(GF_ISOFile *dest, char *fileName, u32 import_flags, GF_
 	for (i=0; i<gf_isom_get_track_count(dest); i++) {
 		u64 track_dur = gf_isom_get_media_duration(dest, i+1);
 		u32 track_ts = gf_isom_get_media_timescale(dest, i+1);
-		if ((u64)aligned_to_DTS_frac.num * track_ts < track_dur * aligned_to_DTS_frac.den) {
+		if (gf_timestamp_less(aligned_to_DTS_frac.num, aligned_to_DTS_frac.den, track_dur, track_ts)) {
 			aligned_to_DTS_frac.num = track_dur;
 			aligned_to_DTS_frac.den = track_ts;
 		}

include/gpac/tools.h
@@ -279,6 +279,88 @@ Parse a 32 bit fraction from string
  */
 Bool gf_parse_frac(const char *str, GF_Fraction *frac);
 
+/*!
+\brief safe timestamp rescale
+
+Rescale a 64 bit timestamp value to new timescale, i.e. performs value * new_timescale / timescale
+\param value value to rescale. A value of -1 means no timestamp defined and is returned unmodified
+\param timescale timescale of value. Assumed to be less than 0xFFFFFFFF
+\param new_timescale new timescale? Assumed to be less than 0xFFFFFFFF
+\return new value
+ */
+u64 gf_timestamp_rescale(u64 value, u64 timescale, u64 new_timescale);
+
+/*!
+\brief safe signed timestamp rescale
+
+Rescale a 64 bit timestamp value to new timescale, i.e. performs value * new_timescale / timescale
+\param value value to rescale
+\param timescale timescale of value. Assumed to be less than 0xFFFFFFFF
+\param new_timescale new timescale. Assumed to be less than 0xFFFFFFFF
+\return new value
+ */
+s64 gf_timestamp_rescale_signed(s64 value, u64 timescale, u64 new_timescale);
+
+/*!
+\brief compare timestamps
+
+Compares two timestamps
+\param value1 value to rescale
+\param timescale1 timescale of value. Assumed to be less than 0xFFFFFFFF
+\param value2 value to rescale
+\param timescale2 timescale of value. Assumed to be less than 0xFFFFFFFF
+\return GF_TRUE if (value1 / timescale1) is stricly less than (value2 / timescale2)
+ */
+Bool gf_timestamp_less(u64 value1, u64 timescale1, u64 value2, u64 timescale2);
+
+/*!
+\brief compare timestamps
+
+Compares two timestamps
+\param value1 value to rescale
+\param timescale1 timescale of value. Assumed to be less than 0xFFFFFFFF
+\param value2 value to rescale
+\param timescale2 timescale of value. Assumed to be less than 0xFFFFFFFF
+\return GF_TRUE if (value1 / timescale1) is stricly less than or equal to (value2 / timescale2)
+ */
+Bool gf_timestamp_less_or_equal(u64 value1, u64 timescale1, u64 value2, u64 timescale2);
+
+/*!
+\brief compare timestamps
+
+Compares two timestamps
+\param value1 value to rescale
+\param timescale1 timescale of value. Assumed to be less than 0xFFFFFFFF
+\param value2 value to rescale
+\param timescale2 timescale of value. Assumed to be less than 0xFFFFFFFF
+\return GF_TRUE if (value1 / timescale1) is stricly greater than (value2 / timescale2)
+ */
+Bool gf_timestamp_greater(u64 value1, u64 timescale1, u64 value2, u64 timescale2);
+
+/*!
+\brief compare timestamps
+
+Compares two timestamps
+\param value1 value to rescale
+\param timescale1 timescale of value. Assumed to be less than 0xFFFFFFFF
+\param value2 value to rescale
+\param timescale2 timescale of value. Assumed to be less than 0xFFFFFFFF
+\return GF_TRUE if (value1 / timescale1) is stricly greater than or equal to (value2 / timescale2)
+ */
+Bool gf_timestamp_greater_or_equal(u64 value1, u64 timescale1, u64 value2, u64 timescale2);
+
+/*!
+\brief compare timestamps
+
+Compares two timestamps
+\param value1 value to rescale
+\param timescale1 timescale of value. Assumed to be less than 0xFFFFFFFF
+\param value2 value to rescale
+\param timescale2 timescale of value. Assumed to be less than 0xFFFFFFFF
+\return GF_TRUE if (value1 / timescale1) is equal to (value2 / timescale2)
+ */
+Bool gf_timestamp_equal(u64 value1, u64 timescale1, u64 value2, u64 timescale2);
+
 /*! @} */
 
 /*!

src/compositor/compositor.c
@@ -2941,9 +2941,7 @@ void gf_sc_render_frame(GF_Compositor *compositor)
 				}
 			}
 			if (pck_frame_ts) {
-				u64 ts = pck_frame_ts;
-				ts *= 1000;
-				ts /= compositor->passthrough_timescale;
+				u64 ts = gf_timestamp_rescale(pck_frame_ts, compositor->passthrough_timescale, 1000);
 				frame_ts = (u32) ts;
 			}
 			gf_filter_pck_send(pck);

src/compositor/media_object.c
@@ -398,10 +398,8 @@ static u64 convert_ts_to_ms(GF_MediaObject *mo, u64 ts, u32 timescale, Bool *dis
 		}
 	}
 
-
-	ts *= 1000;
-	ts /= timescale;
-
+	ts = gf_timestamp_rescale(ts, timescale, 1000);
+	
 	//if addon, translate back into main content timing
 	if (mo->odm->parentscene && mo->odm->parentscene->root_od->addon) {
 		if (!mo->odm->parentscene->root_od->addon->timeline_ready) {

src/export.cpp
@@ -156,6 +156,13 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_lz_compress_payload) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_lz_decompress_payload) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_file_handles_count) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_timestamp_rescale) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_timestamp_rescale_signed) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_timestamp_less) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_timestamp_less_or_equal) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_timestamp_greater) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_timestamp_greater_or_equal) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_timestamp_equal) )
 
 /* Memory */
 #ifdef GPAC_MEMORY_TRACKING

src/filter_core/filter.c
@@ -3162,8 +3162,7 @@ void gf_filter_forward_clock(GF_Filter *filter)
 
 		clock_val = filter->next_clock_dispatch;
 		if (map->timescale != filter->next_clock_dispatch_timescale) {
-			clock_val *= map->timescale;
-			clock_val /= filter->next_clock_dispatch_timescale;
+			clock_val = gf_timestamp_rescale(clock_val, filter->next_clock_dispatch_timescale, map->timescale);
 		}
 		GF_LOG(GF_LOG_DEBUG, GF_LOG_FILTER, ("Filter %s PID %s internal forward of clock reference\n", pid->filter->name, pid->name));
 		pck = gf_filter_pck_new_shared((GF_FilterPid *)pid, NULL, 0, NULL);

src/filter_core/filter_pck.c
@@ -691,8 +691,7 @@ Bool gf_filter_aggregate_packets(GF_FilterPidInst *dst)
 		GF_FilterPacketInstance *pcki = gf_list_pop_back(dst->pck_reassembly);
 		safe_int_inc(&dst->filter->pending_packets);
 		if (pcki->pck->info.duration && pcki->pck->pid_props->timescale) {
-			s64 duration = ((u64)pcki->pck->info.duration) * 1000000;
-			duration /= pcki->pck->pid_props->timescale;
+			u64 duration = gf_timestamp_rescale(pcki->pck->info.duration, pcki->pck->pid_props->timescale, 1000000);
 			safe_int64_add(&dst->buffer_duration, duration);
 		}
 		pcki->pck->info.flags |= GF_PCKF_BLOCK_START | GF_PCKF_BLOCK_END;
@@ -775,8 +774,7 @@ Bool gf_filter_aggregate_packets(GF_FilterPidInst *dst)
 			safe_int_inc(&dst->filter->pending_packets);
 
 			if (info.duration && pck->pid_props && pck->pid_props->timescale) {
-				s64 duration = ((u64) info.duration) * 1000000;
-				duration /= pck->pid_props->timescale;
+				u64 duration = gf_timestamp_rescale(info.duration, pck->pid_props->timescale, 1000000);
 				safe_int64_add(&dst->buffer_duration, duration);
 			}
 			//not continous set of bytes reaggregated
@@ -1185,8 +1183,7 @@ GF_Err gf_filter_pck_send_internal(GF_FilterPacket *pck, Bool from_filter)
 						dst->nb_reagg_pck++;
 
 					if (pck->info.duration && timescale) {
-						duration = ((u64)pck->info.duration) * 1000000;
-						duration /= timescale;
+						duration = gf_timestamp_rescale(pck->info.duration, timescale, 1000000);
 						safe_int64_add(&dst->buffer_duration, duration);
 					}
 					inst->pck->info.flags |= GF_PCKF_BLOCK_START;
@@ -1267,8 +1264,7 @@ GF_Err gf_filter_pck_send_internal(GF_FilterPacket *pck, Bool from_filter)
 			}
 
 			if (duration && timescale) {
-				duration *= 1000000;
-				duration /= timescale;
+				duration = gf_timestamp_rescale(duration, timescale, 1000000);
 				safe_int64_add(&dst->buffer_duration, duration);
 			}
 			safe_int_inc(&dst->filter->pending_packets);

commit 05bbc2ccb3e06be19243fa95e64410b931db9b97

fixed bug in timestamp rescale introduced by 11a3f75

src/utils/constants.c
@@ -1994,7 +1994,7 @@ s64 gf_timestamp_rescale_signed(s64 value, u64 timescale, u64 new_timescale)
 	}
 
 	if (value <= GF_INT_MAX) {
-		return (value * new_timescale) / timescale;
+		return (value * (s32) new_timescale) / (s32) timescale;
 	}
 
 	s64 int_part = value / timescale;