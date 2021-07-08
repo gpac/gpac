81b66bbc952358caa3a0de90b0df96401d25cab8
jeanlf <jeanlf@gpac.io>
2021-07-08 09:59:53 +0200

    added helper for pixel format component depth and fixed wrong log levels

--- include/gpac/constants.h
+++ include/gpac/constants.h
@@ -304,6 +304,12 @@ Bool gf_pixel_get_size_info(GF_PixelFormat pixfmt, u32 width, u32 height, u32 *o
 */
 u32 gf_pixel_get_bytes_per_pixel(GF_PixelFormat pixfmt);
 
+/*! Gets the number of bits per component
+\param pixfmt  pixel format code
+\return number of bits per component
+*/
+u32 gf_pixel_is_wide_depth(GF_PixelFormat pixfmt);
+
 /*! Gets the number of component per pixel
 \param pixfmt  pixel format code
 \return number of bytes per pixel
