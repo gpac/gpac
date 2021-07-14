d2ec927d8628e0c29ef49b66efb7c31e4a186b0c
jeanlf <jeanlf@gpac.io>
2021-07-14 09:37:14 +0200

    fixed potential bug demuxing mp4 from cache causing sample lost

--- src/filters/isoffin_read.c
+++ src/filters/isoffin_read.c
@@ -117,13 +117,22 @@ static GF_Err isoffin_setup(GF_Filter *filter, ISOMReader *read)
 		read->end_range = prop->value.lfrac.den;
 	}
 
+	read->missing_bytes = 0;
 	e = gf_isom_open_progressive(szURL, read->start_range, read->end_range, read->sigfrag, &read->mov, &read->missing_bytes);
 
 	if (e == GF_ISOM_INCOMPLETE_FILE) {
 		read->moov_not_loaded = 1;
 		return GF_OK;
 	}
+
 	read->input_loaded = GF_TRUE;
+	//if missing bytes is set, file is incomplete, check if cache is complete
+	if (read->missing_bytes) {
+		read->input_loaded = GF_FALSE;
+		prop = gf_filter_pid_get_property(read->pid, GF_PROP_PID_FILE_CACHED);
+		if (prop && prop->value.boolean)
+			read->input_loaded = GF_TRUE;
+	}
 
 	if (e != GF_OK) {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[IsoMedia] error while opening %s, error=%s\n", szURL,gf_error_to_string(e)));
