gpac/gpac

commit 025596bb063e39e86d2972e163f9f112aa6ddb5e

added rawsub option to dasher to output wvtt or ttml text rather than fmp4

src/filters/dasher.c
@@ -141,6 +141,7 @@ typedef struct
 	char *segext;
 	char *initext;
 	u32 muxtype;
+	Bool rawsub;
 	char *profX;
 	Double asto;
 	char *ast;
@@ -256,7 +257,7 @@ typedef struct _dash_stream
 	u32 width, height;
 	u32 sr, nb_ch;
 	char *lang;
-	Bool interlaced;
+	Bool interlaced, rawmux;
 	const GF_PropertyValue *p_role;
 	const GF_PropertyValue *p_period_desc;
 	const GF_PropertyValue *p_as_desc;
@@ -2785,14 +2786,14 @@ static void dasher_open_destination(GF_Filter *filter, GF_DasherCtx *ctx, GF_MPD
 	//patch for old arch: make sure we don't have any extra free box before the sidx
 	//we could also use vodcache=insert but this might break http outputs
 	if (gf_sys_old_arch_compat() && !has_vodcache && ctx->sseg) {
-		sprintf(szSRC, "%cvodcache=on", sep_args );
+		sprintf(szSRC, "%cvodcache%con", sep_args, sep_name );
 		if (!strstr(szDST, szSRC))
 			gf_dynstrcat(&szDST, szSRC, NULL);
 	}
 
 	//we don't append mime in case of raw streams, raw format (writegen doesn't use mime types for raw media, only file ext)
-	if ((ctx->muxtype!=DASHER_MUX_RAW) || (ds->codec_id != GF_CODECID_RAW)) {
-		sprintf(szSRC, "%cmime=%s", sep_args, rep->mime_type);
+	if (!ds->rawmux && ((ctx->muxtype!=DASHER_MUX_RAW) || (ds->codec_id != GF_CODECID_RAW)) ) {
+		sprintf(szSRC, "%cmime%c%s", sep_args, sep_name, rep->mime_type);
 		gf_dynstrcat(&szDST, szSRC, NULL);
 	}
 
@@ -3435,6 +3436,7 @@ static void dasher_setup_sources(GF_Filter *filter, GF_DasherCtx *ctx, GF_MPD_Ad
 			strcat(szDASHTemplate, szStrName);
 		}
 
+		ds->rawmux = GF_FALSE;
 		idx_ext = NULL;
 		if (ctx->m2ts) {
 			seg_ext = init_ext = "ts";
@@ -3467,6 +3469,22 @@ static void dasher_setup_sources(GF_Filter *filter, GF_DasherCtx *ctx, GF_MPD_Ad
 				ext = strchr(szRawExt, '|');
 				if (ext) ext[0] = 0;
 				def_ext = szRawExt;
+			}
+
+			if (!ds->muxed_base && ctx->rawsub && (ds->stream_type==GF_STREAM_TEXT) ) {
+				char *ext_sub = (char *) gf_codecid_file_ext(ds->codec_id);
+				if (ext_sub) {
+					if (!strcmp(ext_sub, "subx"))
+						ext_sub = "ttml";
+
+					strncpy(szRawExt, ext_sub, 19);
+					szRawExt[19] = 0;
+					ext_sub = strchr(szRawExt, '|');
+					if (ext_sub) ext_sub[0] = 0;
+					def_ext = szRawExt;
+					skip_init = GF_TRUE;
+					ds->rawmux = GF_TRUE;
+				}
 			}
 
 			if (ctx->segext && !stricmp(ctx->segext, "null")) {
@@ -8698,6 +8716,7 @@ static const GF_FilterArgs DasherArgs[] =
 		"- ogg: uses OGG format\n"
 		"- raw: uses raw media format (disables muxed representations)\n"
 		"- auto: guess format based on extension, default to mp4 if no extension", GF_PROP_UINT, "auto", "mp4|ts|mkv|webm|ogg|raw|auto", 0},
+	{ OFFS(rawsub), "use raw subtitle format instead of encapsulating in container", GF_PROP_BOOL, "no", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(asto), "availabilityStartTimeOffset to use in seconds. A negative value simply increases the AST, a positive value sets the ASToffset to representations", GF_PROP_DOUBLE, "0", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(profile), "target DASH profile. This will set default option values to ensure conformance to the desired profile. For MPEG-2 TS, only main and live are used, others default to main\n"
 		"- auto: turns profile to live for dynamic and full for non-dynamic\n"

src/filters/out_file.c
@@ -40,7 +40,7 @@ typedef struct
 	//options
 	Double start, speed;
 	char *dst, *mime, *ext;
-	Bool append, dynext, ow, redund;
+	Bool append, dynext, ow, redund, noinit;
 	u32 cat;
 	u32 mvbk;
 
@@ -177,6 +177,7 @@ static void fileout_setup_file(GF_FileOutCtx *ctx, Bool explicit_overwrite)
 {
 	const char *dst = ctx->dst;
 	const GF_PropertyValue *p, *ext;
+
 	p = gf_filter_pid_get_property(ctx->pid, GF_PROP_PID_OUTPATH);
 	ext = gf_filter_pid_get_property(ctx->pid, GF_PROP_PID_FILE_EXT);
 
@@ -382,7 +383,7 @@ static GF_Err fileout_process(GF_Filter *filter)
 	}
 
 	gf_filter_pck_get_framing(pck, &start, &end);
-	if (!ctx->redund) {
+	if (!ctx->redund && !ctx->dash_mode) {
 		u32 dep_flags = gf_filter_pck_get_dependency_flags(pck);
 		//redundant packet, do not store
 		if ((dep_flags & 0x3) == 1) {
@@ -469,7 +470,7 @@ static GF_Err fileout_process(GF_Filter *filter)
 
 		if (name) {
 			fileout_open_close(ctx, name, ext ? ext->value.string : NULL, fnum ? fnum->value.uint : 0, explicit_overwrite, fsuf ? fsuf->value.string : NULL);
-		} else if (!ctx->file) {
+		} else if (!ctx->file && !ctx->noinit) {
 			fileout_setup_file(ctx, explicit_overwrite);
 		}
 	}
@@ -671,6 +672,7 @@ static const GF_FilterArgs FileOutArgs[] =
 	{ OFFS(ow), "overwrite output if existing", GF_PROP_BOOL, "true", NULL, 0},
 	{ OFFS(mvbk), "block size used when moving parts of the file around in patch mode", GF_PROP_UINT, "8192", NULL, 0},
 	{ OFFS(redund), "keep redundant packet in output file", GF_PROP_BOOL, "false", NULL, 0},
+	{ OFFS(noinit), "do not produce initial segment", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_HIDE},
 
 	{0}
 };

share/doc/man/gpac-filters.1
@@ -4873,6 +4873,8 @@ muxtype (enum, default: auto): muxtype to use for the segments
 .br
 
 .br
+rawsub (bool, default: no):    use raw subtitle format instead of encapsulating in container
+.br
 asto (dbl, default: 0):        availabilityStartTimeOffset to use in seconds. A negative value simply increases the AST, a positive value sets the ASToffset to representations
 .br
 profile (enum, default: auto): target DASH profile. This will set default option values to ensure conformance to the desired profile. For MPEG-2 TS, only main and live are used, others default to main
@@ -7715,7 +7717,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1103-gc9b19a6f5-master
+build: 1.1.0-DEV-rev1110-g042dd0966-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -4246,7 +4246,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1103-gc9b19a6f5-master
+build: 1.1.0-DEV-rev1110-g042dd0966-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2789,7 +2789,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1103-gc9b19a6f5-master
+build: 1.1.0-DEV-rev1110-g042dd0966-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4client.1
@@ -261,7 +261,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1103-gc9b19a6f5-master
+build: 1.1.0-DEV-rev1110-g042dd0966-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

src/filters/write_generic.c
@@ -77,6 +77,8 @@ typedef struct
 
 	Bool ttml_agg;
 	GF_XMLNode *ttml_root;
+
+	GF_FilterPacket *ttml_dash_pck;
 } GF_GenDumpCtx;
 
 
@@ -243,7 +245,10 @@ GF_Err writegen_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remo
 		if (!gf_filter_pid_get_property(pid, GF_PROP_PID_MIME))
 			gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_MIME, &PROP_STRING(mimetype) );
 
-		if (!ctx->frame) {
+		if (ctx->dash_mode) {
+			ctx->ttml_agg = GF_TRUE;
+		}
+		else if (!ctx->frame) {
 			ctx->ttml_agg = GF_TRUE;
 		} else {
 			ctx->split = GF_TRUE;
@@ -946,6 +951,14 @@ static GF_Err writegen_flush_ttml(GF_GenDumpCtx *ctx)
 
 	memcpy(output, data, size);
 	gf_free(data);
+
+	if (ctx->ttml_dash_pck) {
+		gf_filter_pck_merge_properties(ctx->ttml_dash_pck, pck);
+		gf_filter_pck_set_byte_offset(pck, GF_FILTER_NO_BO);
+		gf_filter_pck_set_dependency_flags(pck, 0);
+		gf_filter_pck_unref(ctx->ttml_dash_pck);
+		ctx->ttml_dash_pck = NULL;
+	}
 	gf_filter_pck_set_framing(pck, GF_TRUE, GF_TRUE);
 	gf_xml_dom_node_del(ctx->ttml_root);
 	ctx->ttml_root = NULL;
@@ -973,6 +986,19 @@ GF_Err writegen_process(GF_Filter *filter)
 		return GF_OK;
 	}
 	ctx->sample_num++;
+
+	if (ctx->dash_mode && ctx->ttml_agg && gf_filter_pck_get_property(pck, GF_PROP_PCK_FILENUM)) {
+		if (ctx->ttml_dash_pck)
+			writegen_flush_ttml(ctx);
+
+		if (ctx->ttml_dash_pck) {
+			gf_filter_pck_unref(ctx->ttml_dash_pck);
+			ctx->ttml_dash_pck = NULL;
+		}
+
+		ctx->ttml_dash_pck = pck;
+		gf_filter_pck_ref_props(&ctx->ttml_dash_pck);
+	}
 
 	if (ctx->sstart) {
 		if (ctx->sstart > ctx->sample_num) {
@@ -1487,6 +1513,11 @@ void writegen_finalize(GF_Filter *filter)
 	if (ctx->bs) gf_bs_del(ctx->bs);
 	if (ctx->ttml_root)
 		gf_xml_dom_node_del(ctx->ttml_root);
+
+	if (ctx->ttml_dash_pck) {
+		gf_filter_pck_unref(ctx->ttml_dash_pck);
+		ctx->ttml_dash_pck = NULL;
+	}
 }
 
 GF_FilterRegister GenDumpRegister = {

src/filters/write_vtt.c
@@ -52,6 +52,7 @@ typedef struct
 
 	GF_WebVTTParser *parser;
 
+	Bool dash_mode;
 } GF_WebVTTMxCtx;
 
 static void vttmx_write_cue(void *ctx, GF_WebVTTCue *cue);
@@ -111,6 +112,10 @@ GF_Err vttmx_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remove)
 		gf_webvtt_parser_cue_callback(ctx->parser, vttmx_write_cue, ctx);
 	}
 	gf_filter_pid_set_framing_mode(pid, GF_TRUE);
+
+	p = gf_filter_pid_get_property(pid, GF_PROP_PID_DASH_MODE);
+	ctx->dash_mode = (p && p->value.uint) ? GF_TRUE : GF_FALSE;
+
 	return GF_OK;
 }
 
@@ -226,6 +231,11 @@ GF_Err vttmx_process(GF_Filter *filter)
 		return GF_OK;
 	}
 
+	if (ctx->dash_mode && gf_filter_pck_get_property(pck, GF_PROP_PCK_FILENUM)) {
+		ctx->first = GF_TRUE;
+	}
+
+
 	data = (char *) gf_filter_pck_get_data(pck, &pck_size);
 
 	if (ctx->first && ctx->dcd) {
@@ -275,7 +285,11 @@ GF_Err vttmx_process(GF_Filter *filter)
 		memcpy(output, ctx->cues_buffer, size);
 		gf_filter_pck_merge_properties(pck, dst_pck);
 		gf_filter_pck_set_byte_offset(dst_pck, GF_FILTER_NO_BO);
-
+		if (ctx->dash_mode) {
+			gf_filter_pck_set_property(dst_pck, GF_PROP_PCK_FILENUM, NULL);
+			gf_filter_pck_set_property(dst_pck, GF_PROP_PCK_FILENAME, NULL);
+		}
+		
 		gf_filter_pck_set_framing(dst_pck, ctx->first, GF_FALSE);
 		ctx->first = GF_FALSE;
 

commit 65112356a020104d3a49c73435ae54967d1ec252

fixed bug introduced by 025596bb

src/filters/dasher.c
@@ -2739,7 +2739,10 @@ static void dasher_open_destination(GF_Filter *filter, GF_DasherCtx *ctx, GF_MPD
 	}
 
 	if (trash_init) {
-		sprintf(szSRC, "%cnoinit", sep_args);
+		if (ds->rawmux)
+			sprintf(szSRC, "%cnoinitraw", sep_args);
+		else
+			sprintf(szSRC, "%cnoinit", sep_args);
 		gf_dynstrcat(&szDST, szSRC, NULL);
 	}
 	if (!has_frag) {

src/filters/out_file.c
@@ -40,7 +40,7 @@ typedef struct
 	//options
 	Double start, speed;
 	char *dst, *mime, *ext;
-	Bool append, dynext, ow, redund, noinit;
+	Bool append, dynext, ow, redund, noinitraw;
 	u32 cat;
 	u32 mvbk;
 
@@ -470,7 +470,7 @@ static GF_Err fileout_process(GF_Filter *filter)
 
 		if (name) {
 			fileout_open_close(ctx, name, ext ? ext->value.string : NULL, fnum ? fnum->value.uint : 0, explicit_overwrite, fsuf ? fsuf->value.string : NULL);
-		} else if (!ctx->file && !ctx->noinit) {
+		} else if (!ctx->file && !ctx->noinitraw) {
 			fileout_setup_file(ctx, explicit_overwrite);
 		}
 	}
@@ -672,7 +672,7 @@ static const GF_FilterArgs FileOutArgs[] =
 	{ OFFS(ow), "overwrite output if existing", GF_PROP_BOOL, "true", NULL, 0},
 	{ OFFS(mvbk), "block size used when moving parts of the file around in patch mode", GF_PROP_UINT, "8192", NULL, 0},
 	{ OFFS(redund), "keep redundant packet in output file", GF_PROP_BOOL, "false", NULL, 0},
-	{ OFFS(noinit), "do not produce initial segment", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_HIDE},
+	{ OFFS(noinitraw), "do not produce initial segment", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_HIDE},
 
 	{0}
 };