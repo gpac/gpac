f3e974d99f3704d8313d3f94f89211a1ec310b1f
jeanlf <jeanlf@gpac.io>
2021-07-21 14:26:49 +0200

    cosmetics

--- applications/mp4box/filedump.c
+++ applications/mp4box/filedump.c
@@ -2058,28 +2058,27 @@ void print_udta(GF_ISOFile *file, u32 track_number, Bool has_itags)
 	fprintf(stderr, "%d UDTA types: ", count);
 
 	for (i=0; i<count; i++) {
-		u32 j, type, nb_items, first=GF_TRUE;
+		u32 j, type, nb_items;
 		bin128 uuid;
 		gf_isom_get_udta_type(file, track_number, i+1, &type, &uuid);
 		nb_items = gf_isom_get_user_data_count(file, track_number, type, uuid);
-		fprintf(stderr, "%s (%d) ", gf_4cc_to_str(type), nb_items);
+		if (!nb_items) continue;
+
+		fprintf(stderr, "\n\t%s: ", gf_4cc_to_str(type));
 		for (j=0; j<nb_items; j++) {
 			u8 *udta=NULL;
 			u32 udta_size;
 			gf_isom_get_user_data(file, track_number, type, uuid, j+1, &udta, &udta_size);
 			if (!udta) continue;
+			if (j) fprintf(stderr, ", ");
 			if (udta_size && gf_utf8_is_legal(udta, udta_size)) {
 				u32 idx;
-				if (first) {
-					fprintf(stderr, "\n");
-					first = GF_FALSE;
-				}
-				fprintf(stderr, "\t");
 				for (idx=0; idx<udta_size; idx++) {
 					if (!udta[idx]) break;
 					fprintf(stderr, "%c", udta[idx]);
 				}
-				fprintf(stderr, "\n");
+			} else {
+				fprintf(stderr, " unknown type (%d bytes)", udta_size);
 			}
 			gf_free(udta);
 		}
@@ -2631,6 +2630,31 @@ void DumpTrackInfo(GF_ISOFile *file, GF_ISOTrackID trackID, Bool full_dump, Bool
 	}
 
 	print_udta(file, trackNum, GF_FALSE);
+
+	DumpMetaItem(file, 0, trackNum, "\tTrack Meta");
+
+	gf_isom_get_track_switch_group_count(file, trackNum, &alt_group, &nb_groups);
+	if (alt_group) {
+		fprintf(stderr, "Alternate Group ID %d\n", alt_group);
+		for (i=0; i<nb_groups; i++) {
+			u32 nb_crit, switchGroupID;
+			const u32 *criterias = gf_isom_get_track_switch_parameter(file, trackNum, i+1, &switchGroupID, &nb_crit);
+			if (!nb_crit) {
+				fprintf(stderr, "\tNo criteria in %s group\n", switchGroupID ? "switch" : "alternate");
+			} else {
+				if (switchGroupID) {
+					fprintf(stderr, "\tSwitchGroup ID %d criterias: ", switchGroupID);
+				} else {
+					fprintf(stderr, "\tAlternate Group criterias: ");
+				}
+				for (j=0; j<nb_crit; j++) {
+					if (j) fprintf(stderr, " ");
+					fprintf(stderr, "%s", gf_4cc_to_str(criterias[j]) );
+				}
+				fprintf(stderr, "\n");
+			}
+		}
+	}
 
 	if (gf_isom_is_video_handler_type(mtype) ) {
 		s32 tx, ty;
@@ -3202,10 +3226,12 @@ void DumpTrackInfo(GF_ISOFile *file, GF_ISOTrackID trackID, Bool full_dump, Bool
 			if (auxiliary_mimes != NULL) {
 				fprintf(stderr, " - auxiliary-mime-types %s", auxiliary_mimes);
 			}
+		} else if (mtype == GF_ISOM_MEDIA_SUBT) {
+			fprintf(stderr, "QT/3GPP subtitle");
 		} else {
 			fprintf(stderr, "Unknown Text Stream");
 		}
-		fprintf(stderr, "\n Size %d x %d - Translation X=%d Y=%d - Layer %d\n", w, h, tx, ty, l);
+		fprintf(stderr, "\n\tSize %d x %d - Translation X=%d Y=%d - Layer %d\n", w, h, tx, ty, l);
 	} else if (mtype == GF_ISOM_MEDIA_META) {
 		const char *content_encoding = NULL;
 		if (msub_type == GF_ISOM_SUBTYPE_METT) {
@@ -3454,31 +3480,6 @@ void DumpTrackInfo(GF_ISOFile *file, GF_ISOTrackID trackID, Bool full_dump, Bool
 		fprintf(stderr, "\tRFC6381 Codec Parameters: %s\n", szCodec);
 	}
 
-
-	DumpMetaItem(file, 0, trackNum, "\tTrack Meta");
-
-	gf_isom_get_track_switch_group_count(file, trackNum, &alt_group, &nb_groups);
-	if (alt_group) {
-		fprintf(stderr, "Alternate Group ID %d\n", alt_group);
-		for (i=0; i<nb_groups; i++) {
-			u32 nb_crit, switchGroupID;
-			const u32 *criterias = gf_isom_get_track_switch_parameter(file, trackNum, i+1, &switchGroupID, &nb_crit);
-			if (!nb_crit) {
-				fprintf(stderr, "\tNo criteria in %s group\n", switchGroupID ? "switch" : "alternate");
-			} else {
-				if (switchGroupID) {
-					fprintf(stderr, "\tSwitchGroup ID %d criterias: ", switchGroupID);
-				} else {
-					fprintf(stderr, "\tAlternate Group criterias: ");
-				}
-				for (j=0; j<nb_crit; j++) {
-					if (j) fprintf(stderr, " ");
-					fprintf(stderr, "%s", gf_4cc_to_str(criterias[j]) );
-				}
-				fprintf(stderr, "\n");
-			}
-		}
-	}
 
 	switch (gf_isom_has_sync_points(file, trackNum)) {
 	case 0:
