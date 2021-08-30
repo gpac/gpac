f19668964bf422cf5a63e4dbe1d3c6c75edadcbb
jeanlf <jeanlf@gpac.io>
2021-08-30 14:34:37 +0200

    fixed #1879

--- src/isomedia/box_code_drm.c
+++ src/isomedia/box_code_drm.c
@@ -145,9 +145,10 @@ GF_Err schm_box_read(GF_Box *s, GF_BitStream *bs)
 
 	if (ptr->size && (ptr->flags & 0x000001)) {
 		u32 len = (u32) (ptr->size);
-		ptr->URI = (char*)gf_malloc(sizeof(char)*len);
+		ptr->URI = (char*)gf_malloc(sizeof(char)*(len+1));
 		if (!ptr->URI) return GF_OUT_OF_MEM;
 		gf_bs_read_data(bs, ptr->URI, len);
+		ptr->URI[len] = 0;
 	}
 	return GF_OK;
 }
