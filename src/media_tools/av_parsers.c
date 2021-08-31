gpac/gpac

commit 0ff42a8992500d2a402ffb0cc317c34214ccbc7b

fixed #1893

src/media_tools/av_parsers.c
@@ -3980,10 +3980,8 @@ void gf_av1_reset_state(AV1State *state, Bool is_destroy)
 		gf_list_del(l1);
 		gf_list_del(l2);
 		if (state->bs) {
-			if (gf_bs_get_position(state->bs)) {
-				u32 size;
-				gf_bs_get_content_no_truncate(state->bs, &state->frame_obus, &size, &state->frame_obus_alloc);
-			}
+			u32 size;
+			gf_bs_get_content_no_truncate(state->bs, &state->frame_obus, &size, &state->frame_obus_alloc);
 			gf_bs_del(state->bs);
 		}
 		state->bs = NULL;

commit 6aa9181bd275f7e7b5fd41f0454d546aca614b99

fixed mem leak introduced by 0ff42a89

src/media_tools/av_parsers.c
@@ -3981,6 +3981,11 @@ void gf_av1_reset_state(AV1State *state, Bool is_destroy)
 		gf_list_del(l2);
 		if (state->bs) {
 			u32 size;
+			if (!gf_bs_get_position(state->bs) && state->frame_obus) {
+				gf_free(state->frame_obus);
+				state->frame_obus = NULL;
+				state->frame_obus_alloc = 0;
+			}
 			gf_bs_get_content_no_truncate(state->bs, &state->frame_obus, &size, &state->frame_obus_alloc);
 			gf_bs_del(state->bs);
 		}