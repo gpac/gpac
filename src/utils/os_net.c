82867510a2abd7538f8bd829aecf12347c57de38
jeanlf <jeanlf@gpac.io>
2021-09-03 15:09:44 +0200

    added timeout keep-alive checking in downloader

--- src/utils/os_net.c
+++ src/utils/os_net.c
@@ -1462,7 +1462,7 @@ GF_Err gf_sk_receive_internal(GF_Socket *sock, char *buffer, u32 length, u32 *By
 		case ENOTCONN:
 		case ECONNRESET:
 		case ECONNABORTED:
-			GF_LOG(GF_LOG_ERROR, GF_LOG_NETWORK, ("[socket] error reading: %s\n", gf_errno_str(LASTSOCKERROR)));
+			GF_LOG(GF_LOG_WARNING, GF_LOG_NETWORK, ("[socket] error reading: %s\n", gf_errno_str(LASTSOCKERROR)));
 			return GF_IP_CONNECTION_CLOSED;
 #endif
 		default:
