gpac/gpac

commit 93b10bc5cdd29a2d76b02378860c6f345265acaf

added timeout keep-alive checking in downloader

src/utils/downloader.c
@@ -260,6 +260,8 @@ struct __gf_download_session
 	//in bytes per seconds
 	Double cumulated_chunk_rate;
 
+	u32 connection_timeout_ms;
+
 #ifdef GPAC_HAS_HTTP2
 	//HTTP/2 session used by this download session. f not NULL, the mutex, socket and ssl context are moved along sessions
 	GF_H2_Session *h2_sess;
@@ -2160,6 +2162,14 @@ GF_Err gf_dm_sess_setup_from_url(GF_DownloadSession *sess, const char *url, Bool
 		socket_changed = GF_TRUE;
 	else if (sess->status>GF_NETIO_DISCONNECTED)
 		socket_changed = GF_TRUE;
+
+	else if (sess->connection_timeout_ms) {
+		u32 diff = (u32) ( gf_sys_clock_high_res() - sess->last_fetch_time) / 1000;
+		if (diff >= sess->connection_timeout_ms) {
+			GF_LOG(GF_LOG_INFO, GF_LOG_HTTP, ("[HTTP] Timeout reached (%d ms since last vs %d ms timeout), reconnecting\n", diff, sess->connection_timeout_ms));
+			socket_changed = GF_TRUE;
+		}
+	}
 
 	assert(sess->status != GF_NETIO_WAIT_FOR_REPLY);
 	assert(sess->status != GF_NETIO_DATA_EXCHANGE);
@@ -4746,6 +4756,8 @@ static GF_Err wait_for_header_and_parse(GF_DownloadSession *sess, char * sHTTP)
 	u32 method=0;
 	u32 rsp_code=0, ContentLength, first_byte, last_byte, total_size, range, no_range;
 	Bool connection_closed = GF_FALSE;
+	Bool connection_keep_alive = GF_FALSE;
+	u32 connection_timeout=0;
 	char buf[1025];
 	char comp[400];
 	GF_Err e;
@@ -5098,6 +5110,23 @@ static GF_Err wait_for_header_and_parse(GF_DownloadSession *sess, char * sHTTP)
 			else if (!stricmp(hdr->name, "Connection") ) {
 				if (strstr(hdr->value, "close"))
 					connection_closed = GF_TRUE;
+				else if (strstr(hdr->value, "Keep-Alive"))
+					connection_keep_alive = GF_TRUE;
+			}
+			else if (!stricmp(hdr->name, "Keep-Alive") ) {
+				char *tout = strstr(hdr->value, "timeout=");
+				if (tout) {
+					char c=0;
+					s32 end = gf_token_find(tout, 0, (u32) strlen(tout), ", ");
+					if (end) {
+						c = tout[end];
+						tout[end] = 0;
+					}
+					connection_timeout = atoi(tout+8);
+					if (end) tout[end] = c;
+				}
+				if (strstr(hdr->value, "close"))
+					connection_closed = GF_TRUE;
 			}
 #ifdef GPAC_HAS_HTTP2
 			else if (!stricmp(hdr->name, "Upgrade") ) {
@@ -5126,6 +5155,16 @@ static GF_Err wait_for_header_and_parse(GF_DownloadSession *sess, char * sHTTP)
 
 
 		gf_cache_set_headers_processed(sess->cache_entry);
+
+		if (connection_keep_alive
+			&& !gf_opts_get_bool("core", "no-timeout")
+			&& !sess->server_mode
+#ifdef GPAC_HAS_HTTP2
+			&& !sess->h2_sess
+#endif
+		) {
+			sess->connection_timeout_ms = connection_timeout*1000;
+		}
 	}
 
 	par.msg_type = GF_NETIO_PARSE_REPLY;

share/doc/man/gpac-filters.1
@@ -7758,7 +7758,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1213-gc731bfca1-master
+build: 1.1.0-DEV-rev1214-g867f626f7-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -698,6 +698,11 @@ set HTTP head request timeout in milliseconds
 set HTTP/RTSP request timeout in milliseconds
 .br
 .TP
+.B \-no-timeout
+.br
+ignore HTTP 1.1 timeout in keep-alive
+.br
+.TP
 .B \-broken-cert
 .br
 enable accepting broken SSL certificates
@@ -4268,7 +4273,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1213-gc731bfca1-master
+build: 1.1.0-DEV-rev1214-g867f626f7-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2791,7 +2791,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1213-gc731bfca1-master
+build: 1.1.0-DEV-rev1214-g867f626f7-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4client.1
@@ -261,7 +261,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1213-gc731bfca1-master
+build: 1.1.0-DEV-rev1214-g867f626f7-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

src/utils/os_config_init.c
@@ -1220,6 +1220,7 @@ GF_GPACArg GPAC_Args[] = {
  GF_DEF_ARG("cache-size", NULL, "specify cache size in bytes", "100M", NULL, GF_ARG_INT, GF_ARG_HINT_ADVANCED|GF_ARG_SUBSYS_HTTP),
  GF_DEF_ARG("head-timeout", NULL, "set HTTP head request timeout in milliseconds", "5000", NULL, GF_ARG_INT, GF_ARG_HINT_EXPERT|GF_ARG_SUBSYS_HTTP),
  GF_DEF_ARG("req-timeout", NULL, "set HTTP/RTSP request timeout in milliseconds", "20000", NULL, GF_ARG_INT, GF_ARG_HINT_EXPERT|GF_ARG_SUBSYS_HTTP),
+ GF_DEF_ARG("no-timeout", NULL, "ignore HTTP 1.1 timeout in keep-alive", "false", NULL, GF_ARG_BOOL, GF_ARG_HINT_EXPERT|GF_ARG_SUBSYS_HTTP),
  GF_DEF_ARG("broken-cert", NULL, "enable accepting broken SSL certificates", NULL, NULL, GF_ARG_BOOL, GF_ARG_HINT_EXPERT|GF_ARG_SUBSYS_HTTP),
  GF_DEF_ARG("user-agent", "ua", "set user agent name for HTTP/RTSP", NULL, NULL, GF_ARG_STRING, GF_ARG_HINT_ADVANCED|GF_ARG_SUBSYS_HTTP),
  GF_DEF_ARG("user-profileid", NULL, "set user profile ID (through **X-UserProfileID** entity header) in HTTP requests", NULL, NULL, GF_ARG_STRING, GF_ARG_HINT_EXPERT|GF_ARG_SUBSYS_HTTP),

src/utils/os_net.c
@@ -1462,7 +1462,7 @@ GF_Err gf_sk_receive_internal(GF_Socket *sock, char *buffer, u32 length, u32 *By
 		case ENOTCONN:
 		case ECONNRESET:
 		case ECONNABORTED:
-			GF_LOG(GF_LOG_ERROR, GF_LOG_NETWORK, ("[socket] error reading: %s\n", gf_errno_str(LASTSOCKERROR)));
+			GF_LOG(GF_LOG_WARNING, GF_LOG_NETWORK, ("[socket] error reading: %s\n", gf_errno_str(LASTSOCKERROR)));
 			return GF_IP_CONNECTION_CLOSED;
 #endif
 		default:

commit 174f863bb1a9fb3f15123a1a96b505cbbf1604a4

fixed potential crash introduced by 93b10bc5

src/utils/downloader.c
@@ -5118,12 +5118,12 @@ static GF_Err wait_for_header_and_parse(GF_DownloadSession *sess, char * sHTTP)
 				if (tout) {
 					char c=0;
 					s32 end = gf_token_find(tout, 0, (u32) strlen(tout), ", ");
-					if (end) {
+					if (end>=0) {
 						c = tout[end];
 						tout[end] = 0;
 					}
 					connection_timeout = atoi(tout+8);
-					if (end) tout[end] = c;
+					if (end>=0) tout[end] = c;
 				}
 				if (strstr(hdr->value, "close"))
 					connection_closed = GF_TRUE;