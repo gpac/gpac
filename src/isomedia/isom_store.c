gpac/gpac

commit 09b3b2a7fbabe1e74f289212cab4edeca0e820c5

fixed inplace rewrite bug when output file less than input and mdat first - cf 1924

src/isomedia/isom_store.c
@@ -2252,7 +2252,7 @@ static GF_Err WriteInplace(MovieWriter *mw, GF_BitStream *bs)
 		case GF_ISOM_BOX_TYPE_MDAT:
 			break;
 		default:
-			if (((s32) i < moov_meta_pos) && ((s32)i < mdat_pos)) {
+			if (((s32) (i-1) < moov_meta_pos) && ((s32)(i-1) < mdat_pos)) {
 				e = gf_isom_box_write(a, bs);
 				if (e) return e;
 			}
@@ -2284,6 +2284,19 @@ static GF_Err WriteInplace(MovieWriter *mw, GF_BitStream *bs)
 			if (e) return e;
 		}
 		//trash all boxes after moov/meta
+		size = gf_bs_get_size(bs);
+		offset = gf_bs_get_position(bs);
+		//size has reduced, pad at end with a free box
+		if (offset < size) {
+			u32 free_size = 0;
+			if (size - offset > 8)
+				free_size = (u32) (size - offset - 8);
+
+			gf_bs_write_u32(bs, free_size+8);
+			gf_bs_write_u32(bs, GF_ISOM_BOX_TYPE_FREE);
+			while (free_size)
+				gf_bs_write_u8(bs, 0);
+		}
 		return GF_OK;
 	}
 

commit 4a6146a07b85b524afb2f74d5a98f78c55c885a8

fixed bug introduced in 09b3b2a - cf #1924

src/isomedia/isom_store.c
@@ -2294,8 +2294,10 @@ static GF_Err WriteInplace(MovieWriter *mw, GF_BitStream *bs)
 
 			gf_bs_write_u32(bs, free_size+8);
 			gf_bs_write_u32(bs, GF_ISOM_BOX_TYPE_FREE);
-			while (free_size)
+			while (free_size) {
 				gf_bs_write_u8(bs, 0);
+				free_size--;
+			}
 		}
 		return GF_OK;
 	}