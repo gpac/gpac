f69f86b84d30c81d9d3fe9fcc20efdf05967d89b
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2021-10-04 10:25:36 +0200

    don't use compact ternary condition as msvc doesn't support it

--- src/filters/isoffin_load.c
+++ src/filters/isoffin_load.c
@@ -1210,18 +1210,18 @@ static void isor_declare_track(ISOMReader *read, ISOMChannel *ch, u32 track, u32
 
 
 	if (ch->check_avc_ps) {
-		ch->avcc = gf_isom_avc_config_get(ch->owner->mov, ch->track, ch->last_sample_desc_index ?: 1);
+		ch->avcc = gf_isom_avc_config_get(ch->owner->mov, ch->track, ch->last_sample_desc_index ? ch->last_sample_desc_index : 1);
 	}
 	else if (ch->check_hevc_ps) {
-		ch->hvcc = gf_isom_hevc_config_get(ch->owner->mov, ch->track, ch->last_sample_desc_index ?: 1);
+		ch->hvcc = gf_isom_hevc_config_get(ch->owner->mov, ch->track, ch->last_sample_desc_index ? ch->last_sample_desc_index: 1);
 	}
 	else if (ch->check_vvc_ps) {
-		ch->vvcc = gf_isom_vvc_config_get(ch->owner->mov, ch->track, ch->last_sample_desc_index ?: 1);
+		ch->vvcc = gf_isom_vvc_config_get(ch->owner->mov, ch->track, ch->last_sample_desc_index ? ch->last_sample_desc_index : 1);
 	}
 
 	if (streamtype==GF_STREAM_VISUAL) {
 		u32 cwn, cwd, chn, chd, cxn, cxd, cyn, cyd;
-		gf_isom_get_clean_aperture(ch->owner->mov, ch->track, ch->last_sample_desc_index ?: 1, &cwn, &cwd, &chn, &chd, &cxn, &cxd, &cyn, &cyd);
+		gf_isom_get_clean_aperture(ch->owner->mov, ch->track, ch->last_sample_desc_index ? ch->last_sample_desc_index : 1, &cwn, &cwd, &chn, &chd, &cxn, &cxd, &cyn, &cyd);
 
 		if (cwd && chd && cxd && cyd) {
 			gf_filter_pid_set_property(ch->pid, GF_PROP_PID_CLAP_W, &PROP_FRAC_INT(cwn, cwd) );
