gpac/gpac

commit 9fd9fe05b420107eb214c59d2df832b661293e1b

fixed bug introduced by 9e14ae6a

src/compositor/media_object.c
@@ -263,7 +263,7 @@ Bool gf_mo_get_audio_info(GF_MediaObject *mo, u32 *sample_rate, u32 *bits_per_sa
 }
 
 
-void gf_mo_update_caps(GF_MediaObject *mo)
+void gf_mo_update_caps_ex(GF_MediaObject *mo, Bool check_unchanged)
 {
 	Bool changed = GF_FALSE;
 	const GF_PropertyValue *v, *v2;
@@ -415,12 +415,16 @@ void gf_mo_update_caps(GF_MediaObject *mo)
 		memset(&evt, 0, sizeof(GF_Event));
 		evt.type = GF_EVENT_QUALITY_SWITCHED;
 		gf_sc_send_event(scene->compositor, &evt);
-	} else {
+	} else if (check_unchanged) {
 		//reset config changed if nothing changed, this avoid reseting up texture or mixer input
 		mo->config_changed = GF_FALSE;
 	}
 }
+void gf_mo_update_caps(GF_MediaObject *mo)
+{
+	gf_mo_update_caps_ex(mo, GF_FALSE);
 
+}
 static u64 convert_ts_to_ms(GF_MediaObject *mo, u64 ts, u32 timescale, Bool *discard)
 {
 	if (mo->odm->timestamp_offset) {

src/filters/compose.c
@@ -266,7 +266,8 @@ static GF_Err compose_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool i
 				return GF_NOT_SUPPORTED;
 			if (odm->mo) {
 				odm->mo->config_changed = GF_TRUE;
-				gf_mo_update_caps(odm->mo);
+				gf_mo_update_caps_ex(odm->mo, GF_TRUE);
+				odm->mo->config_changed = GF_TRUE;
 				if (odm->mo->config_changed && (odm->type == GF_STREAM_VISUAL) && odm->parentscene && odm->parentscene->is_dynamic_scene) {
 					gf_scene_force_size_to_video(odm->parentscene, odm->mo);
 				}

include/gpac/internal/compositor_dev.h
@@ -1869,6 +1869,7 @@ void gf_inline_restart(GF_Scene *scene);
 Bool gf_mo_is_same_url(GF_MediaObject *obj, MFURL *an_url, Bool *keep_fragment, u32 obj_hint_type);
 
 void gf_mo_update_caps(GF_MediaObject *mo);
+void gf_mo_update_caps_ex(GF_MediaObject *mo, Bool check_unchanged);
 
 
 const char *gf_scene_get_fragment_uri(GF_Node *node);

commit b9158f89f52e527aca97f6b4720c66697bb7a5c6

fixed bug introduced by 9fd9fe05

src/compositor/compositor.c
@@ -2966,6 +2966,8 @@ void gf_sc_render_frame(GF_Compositor *compositor)
 			gf_sc_ar_update_video_clock(compositor->audio_renderer, frame_ts);
 
 			if (!compositor->player) {
+				//in regular mode don't change fps once we sent one frame
+				compositor->autofps = GF_FALSE;
 				if (compositor->passthrough_txh) {
 					// update clock if not buffering
 					if (!gf_mo_is_buffering(compositor->passthrough_txh->stream))

src/compositor/media_object.c
@@ -274,7 +274,7 @@ void gf_mo_update_caps_ex(GF_MediaObject *mo, Bool check_unchanged)
 #define UPDATE_CAP(_code, _field) \
 		v = gf_filter_pid_get_property(mo->odm->pid, _code);\
 		if (v) {\
-			if (mo->_field && (mo->_field != v->value.uint)) changed=GF_TRUE;\
+			if (mo->_field != v->value.uint) changed=GF_TRUE;\
 			mo->_field = v->value.uint;\
 		} else if (mo->_field) {\
 			changed=GF_TRUE;\

src/filters/compose.c
@@ -267,7 +267,6 @@ static GF_Err compose_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool i
 			if (odm->mo) {
 				odm->mo->config_changed = GF_TRUE;
 				gf_mo_update_caps_ex(odm->mo, GF_TRUE);
-				odm->mo->config_changed = GF_TRUE;
 				if (odm->mo->config_changed && (odm->type == GF_STREAM_VISUAL) && odm->parentscene && odm->parentscene->is_dynamic_scene) {
 					gf_scene_force_size_to_video(odm->parentscene, odm->mo);
 				}