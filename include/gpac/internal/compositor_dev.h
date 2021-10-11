gpac/gpac

commit 9e14ae6a1b6c066654c18206518e3aa6bdd6f3da

fixed extra texture reload in compositor upon pid reconfigure

src/compositor/media_object.c
@@ -415,6 +415,9 @@ void gf_mo_update_caps(GF_MediaObject *mo)
 		memset(&evt, 0, sizeof(GF_Event));
 		evt.type = GF_EVENT_QUALITY_SWITCHED;
 		gf_sc_send_event(scene->compositor, &evt);
+	} else {
+		//reset config changed if nothing changed, this avoid reseting up texture or mixer input
+		mo->config_changed = GF_FALSE;
 	}
 }
 

src/filters/compose.c
@@ -266,9 +266,9 @@ static GF_Err compose_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool i
 				return GF_NOT_SUPPORTED;
 			if (odm->mo) {
 				odm->mo->config_changed = GF_TRUE;
-				if ((odm->type == GF_STREAM_VISUAL) && odm->parentscene && odm->parentscene->is_dynamic_scene) {
+				gf_mo_update_caps(odm->mo);
+				if (odm->mo->config_changed && (odm->type == GF_STREAM_VISUAL) && odm->parentscene && odm->parentscene->is_dynamic_scene) {
 					gf_scene_force_size_to_video(odm->parentscene, odm->mo);
-					odm->mo->config_changed = GF_TRUE;
 				}
 			}
 			gf_odm_update_duration(odm, pid);

commit 9fd9fe05b420107eb214c59d2df832b661293e1b

fixed bug introduced by 9e14ae6a

include/gpac/internal/compositor_dev.h
@@ -1869,6 +1869,7 @@ void gf_inline_restart(GF_Scene *scene);
 Bool gf_mo_is_same_url(GF_MediaObject *obj, MFURL *an_url, Bool *keep_fragment, u32 obj_hint_type);
 
 void gf_mo_update_caps(GF_MediaObject *mo);
+void gf_mo_update_caps_ex(GF_MediaObject *mo, Bool check_unchanged);
 
 
 const char *gf_scene_get_fragment_uri(GF_Node *node);

src/compositor/media_object.c
@@ -263,7 +263,7 @@ Bool gf_mo_get_audio_info(GF_MediaObject *mo, u32 *sample_rate, u32 *bits_per_sa
 }
 
 
-void gf_mo_update_caps(GF_MediaObject *mo)
+void gf_mo_update_caps_ex(GF_MediaObject *mo, Bool check_unchanged)
 {
 	Bool changed = GF_FALSE;
 	const GF_PropertyValue *v, *v2;
@@ -415,12 +415,16 @@ void gf_mo_update_caps(GF_MediaObject *mo)
 		memset(&evt, 0, sizeof(GF_Event));
 		evt.type = GF_EVENT_QUALITY_SWITCHED;
 		gf_sc_send_event(scene->compositor, &evt);
-	} else {
+	} else if (check_unchanged) {
 		//reset config changed if nothing changed, this avoid reseting up texture or mixer input
 		mo->config_changed = GF_FALSE;
 	}
 }
+void gf_mo_update_caps(GF_MediaObject *mo)
+{
+	gf_mo_update_caps_ex(mo, GF_FALSE);
 
+}
 static u64 convert_ts_to_ms(GF_MediaObject *mo, u64 ts, u32 timescale, Bool *discard)
 {
 	if (mo->odm->timestamp_offset) {

src/filters/compose.c
@@ -266,7 +266,8 @@ static GF_Err compose_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool i
 				return GF_NOT_SUPPORTED;
 			if (odm->mo) {
 				odm->mo->config_changed = GF_TRUE;
-				gf_mo_update_caps(odm->mo);
+				gf_mo_update_caps_ex(odm->mo, GF_TRUE);
+				odm->mo->config_changed = GF_TRUE;
 				if (odm->mo->config_changed && (odm->type == GF_STREAM_VISUAL) && odm->parentscene && odm->parentscene->is_dynamic_scene) {
 					gf_scene_force_size_to_video(odm->parentscene, odm->mo);
 				}