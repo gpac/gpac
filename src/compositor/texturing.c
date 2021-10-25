gpac/gpac

commit 0e87af75d1c11286e906b2c24f415f3a8192294d

fixed bug triggering texture not available while data is pending in input chain - cf #1934

src/compositor/texturing.c
@@ -260,15 +260,24 @@ void gf_sc_texture_update_frame(GF_TextureHandler *txh, Bool disable_resync)
 
 	/*if no frame or muted don't draw*/
 	if (!txh->data && !txh->frame_ifce) {
-		GF_LOG(txh->stream->connect_failure ? GF_LOG_DEBUG : GF_LOG_INFO, GF_LOG_COMPOSE, ("[Texture %p] No output frame available \n", txh));
+		GF_LOG(txh->stream->connect_state ? GF_LOG_DEBUG : GF_LOG_INFO, GF_LOG_COMPOSE, ("[Texture %p] No output frame available \n", txh));
 
 		if (txh->compositor->use_step_mode || !txh->compositor->player) {
-			if (!txh->stream->connect_failure && ((s32)txh->last_frame_time<0) ) {
-				if (!txh->probe_time_ms) txh->probe_time_ms = gf_sys_clock();
-				else if (gf_sys_clock() - txh->probe_time_ms > txh->compositor->timeout / 2) {
-					GF_LOG(GF_LOG_WARNING, GF_LOG_COMPOSE, ("[Texture %p] No output frame in %d ms, considering stream not available\n", txh, txh->compositor->timeout / 2));
-					txh->stream->connect_failure = GF_TRUE;
-					gf_sc_texture_stop_no_unregister(txh);
+			if (!txh->stream->connect_state && ((s32)txh->last_frame_time<0) ) {
+				//if we have anything bending in the source graph, reset timeout
+				//typical use case: a filter aggregates packets for a long time before dispatching
+				//example: tileagg+dash (cf issue #1934)
+				u64 buffered = gf_filter_pid_query_buffer_duration(txh->stream->odm->pid, GF_FALSE);
+				if (buffered) txh->stream->connect_state = MO_CONNECT_BUFFERING;
+
+				if (txh->compositor->timeout) {
+					if (buffered || !txh->probe_time_ms) {
+						txh->probe_time_ms = gf_sys_clock();
+					} else if (gf_sys_clock() - txh->probe_time_ms > txh->compositor->timeout) {
+						GF_LOG(GF_LOG_WARNING, GF_LOG_COMPOSE, ("[Texture %p] No output frame in %d ms, considering stream not available\n", txh, txh->compositor->timeout));
+						txh->stream->connect_state = MO_CONNECT_TIMEOUT;
+						gf_sc_texture_stop_no_unregister(txh);
+					}
 				}
 				txh->compositor->ms_until_next_frame = -1;
 			}

include/gpac/internal/compositor_dev.h
@@ -2260,6 +2260,17 @@ enum
 /*! All Media Objects inserted through URLs and not MPEG-4 OD Framework use this ODID*/
 #define GF_MEDIA_EXTERNAL_ID		1050
 
+enum
+{
+	//no connection error, no frames seen in input pipeline
+	MO_CONNECT_OK=0,
+	//no connection error, frames seen in input pipeline but no frame yet available for object
+	MO_CONNECT_BUFFERING,
+	//explicit source setup failure
+	MO_CONNECT_FAILED,
+	//timeout of input pipeline (no frames seen after compositor->timeout ms)
+	MO_CONNECT_TIMEOUT
+};
 
 /*GF_MediaObject: link between real object manager and scene. although there is a one-to-one mapping between a
 MediaObject and an ObjectManager, we have to keep them separated in order to handle OD remove commands which destroy
@@ -2334,7 +2345,7 @@ struct _mediaobj
 	GF_FilterFrameInterface *frame_ifce;
 
 	Float c_x, c_y, c_w, c_h;
-	Bool connect_failure;
+	u32 connect_state;
 };
 
 GF_MediaObject *gf_mo_new();

share/doc/man/gpac-filters.1
@@ -395,7 +395,7 @@ sgaze (bool, default: false, updatable): simulate gaze events through mouse
 .br
 ckey (uint, default: 0, updatable): color key to use in windowless mode (0xFFRRGGBB). GPAC currently does not support true alpha blitting to desktop due to limitations in most windowing toolkit, it therefore uses color keying mechanism. The alpha part of the key is used for global transparency of the output, if supported
 .br
-timeout (uint, default: 10000, updatable): timeout in ms after which a source is considered dead
+timeout (uint, default: 10000, updatable): timeout in ms after which a source is considered dead (0 disable timeout)
 .br
 fps (frac, default: 30/1, updatable): simulation frame rate when animation-only sources are played (ignored when video is present)
 .br
@@ -5106,6 +5106,8 @@ This filter reaggregates a set of split tiled HEVC streams (hvt1 or hvt2 in isob
 .br
 tiledrop (uintl, default: , updatable): specify indexes of tiles to drop
 .br
+ttimeout (uint, default: 10000, updatable): number of milliseconds to wait until considering a tile packet lost, 0 waits forever
+.br
 
 .br
 .SH tilesplit
@@ -9039,7 +9041,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1387-g36800cd3c-master
+build: 1.1.0-DEV-rev1401-gb25e2d812-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -4286,7 +4286,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1387-g36800cd3c-master
+build: 1.1.0-DEV-rev1401-gb25e2d812-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2798,7 +2798,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1387-g36800cd3c-master
+build: 1.1.0-DEV-rev1401-gb25e2d812-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4client.1
@@ -261,7 +261,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1387-g36800cd3c-master
+build: 1.1.0-DEV-rev1401-gb25e2d812-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

src/compositor/mpeg4_inline.c
@@ -60,7 +60,7 @@ static Bool gf_inline_set_scene(M_Inline *root)
 
 	mo = gf_scene_get_media_object_ex(parent, &root->url, GF_MEDIA_OBJECT_SCENE, GF_FALSE, NULL, GF_FALSE, (GF_Node*)root, NULL);
 	if (!mo) return GF_FALSE;
-	if (mo->connect_failure) {
+	if (mo->connect_state > MO_CONNECT_BUFFERING) {
 		gf_sg_vrml_mf_reset(&root->url, GF_SG_VRML_MFURL);
 		return GF_FALSE;
 	}

src/compositor/mpeg4_mediacontrol.c
@@ -412,7 +412,7 @@ void RenderMediaControl(GF_Node *node, void *rs, Bool is_destroy)
 			stack->changed = 0;
 			return;
 		}
-		if (stack->stream->connect_failure) {
+		if (stack->stream->connect_state > MO_CONNECT_BUFFERING) {
 			gf_sg_vrml_mf_reset(&stack->control->url, GF_SG_VRML_MFURL);
 			stack->enabled = GF_FALSE;
 			stack->stream = NULL;

commit a1a80a83ef68f0eebba5fc90c71d6082c3940677

fixed crash introduced by 0e87af75

src/compositor/texturing.c
@@ -267,8 +267,12 @@ void gf_sc_texture_update_frame(GF_TextureHandler *txh, Bool disable_resync)
 				//if we have anything bending in the source graph, reset timeout
 				//typical use case: a filter aggregates packets for a long time before dispatching
 				//example: tileagg+dash (cf issue #1934)
-				u64 buffered = gf_filter_pid_query_buffer_duration(txh->stream->odm->pid, GF_FALSE);
-				if (buffered) txh->stream->connect_state = MO_CONNECT_BUFFERING;
+				//warning, mo->odm can be null at this point (bifs & co)
+				u64 buffered = 0;
+				if (txh->stream->odm && txh->stream->odm->pid) {
+					buffered = gf_filter_pid_query_buffer_duration(txh->stream->odm->pid, GF_FALSE);
+					if (buffered) txh->stream->connect_state = MO_CONNECT_BUFFERING;
+				}
 
 				if (txh->compositor->timeout) {
 					if (buffered || !txh->probe_time_ms) {