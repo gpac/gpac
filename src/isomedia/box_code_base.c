d5b96f37a228f4b75195aa77010c6ba1efb57e35
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2021-11-24 17:18:23 +0100

    tfxd: force version=1 as the default timebase from microsoft is 10000000 and will quickly overflow 32 bits

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -6316,6 +6316,7 @@ GF_Box *tfxd_box_new()
 {
 	ISOM_DECL_BOX_ALLOC(GF_MSSTimeExtBox, GF_ISOM_BOX_TYPE_UUID);
 	tmp->internal_4cc = GF_ISOM_BOX_UUID_TFXD;
+	tmp->version = 1;
 	return (GF_Box *)tmp;
 }
 
