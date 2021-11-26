16929a985fd6bf5110dc96e636baa9e8e23bb3bf
jeanlf <jeanlf@gpac.io>
2021-11-26 11:59:07 +0100

    fixed bug from acffb229

--- src/filters/in_rtp.c
+++ src/filters/in_rtp.c
@@ -601,8 +601,9 @@ static GF_Err rtpin_process(GF_Filter *filter)
 	if (ctx->max_sleep<0)
 		gf_filter_ask_rt_reschedule(filter, (u32) ((-ctx->max_sleep) *1000) );
 	else if (!ctx->min_frame_dur_ms) {
-		gf_filter_ask_rt_reschedule(filter, ctx->max_sleep*1000);
-	} else {
+		gf_filter_ask_rt_reschedule(filter, 1000);
+	}
+	else {
 		assert(ctx->min_frame_dur_ms <= (u32) ctx->max_sleep);
 		gf_filter_ask_rt_reschedule(filter, ctx->min_frame_dur_ms*1000);
 	}
