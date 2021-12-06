gpac/gpac

commit d215f08c0f1d75636624bed600c7c329196043d2

fix audio extra forwarding

src/filters/mux_isom.c
@@ -2651,6 +2651,10 @@ sample_entry_setup:
 			udesc.h_res = 72;
 			udesc.depth = 24;
 		}
+		if (dsi) {
+			udesc.extension_buf = dsi->value.data.ptr;
+			udesc.extension_buf_size = dsi->value.data.size;
+		}
 		if (unknown_generic) {
 			GF_LOG(GF_LOG_WARNING, GF_LOG_CONTAINER, ("[MP4Mux] muxing unknown codec ID %s, using generic sample entry with 4CC \"%s\"\n", gf_codecid_name(codec_id), gf_4cc_to_str(m_subtype) ));
 		}

src/isomedia/box_code_base.c
@@ -6627,7 +6627,7 @@ static void gf_isom_check_sample_desc(GF_TrackBox *trak)
 			gf_bs_set_cookie(bs, GF_ISOM_BS_COOKIE_NO_LOGS);\
 			e = gf_isom_box_array_read((GF_Box *) _box, bs); \
 			count_subb = _box->child_boxes ? gf_list_count(_box->child_boxes) : 0; \
-			if (!count_subb || e) { \
+			if (count_subb & !e) { \
 				gf_bs_seek(bs, pos); \
 				_box->data_size = (u32) gf_bs_available(bs); \
 				if (_box->data_size) { \

commit 640ab0e8c0ce555e82652e1cce5f8def48d3e849

fixed bug introduced by d215f08

src/filters/mux_isom.c
@@ -1545,10 +1545,14 @@ sample_entry_setup:
 	case GF_CODECID_JPEG:
 		m_subtype = GF_ISOM_BOX_TYPE_JPEG;
 		comp_name = "JPEG";
+		//use generic sample desc, but don't inject dsi
+		dsi = NULL;
 		break;
 	case GF_CODECID_PNG:
 		m_subtype = GF_ISOM_BOX_TYPE_PNG;
 		comp_name = "PNG";
+		//use generic sample desc, but don't inject dsi
+		dsi = NULL;
 		break;
 	case GF_CODECID_J2K:
 		m_subtype = GF_ISOM_BOX_TYPE_MJP2;