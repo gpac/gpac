gpac/gpac

commit 640ab0e8c0ce555e82652e1cce5f8def48d3e849

fixed bug introduced by d215f08

src/filters/mux_isom.c
@@ -1545,10 +1545,14 @@ sample_entry_setup:
 	case GF_CODECID_JPEG:
 		m_subtype = GF_ISOM_BOX_TYPE_JPEG;
 		comp_name = "JPEG";
+		//use generic sample desc, but don't inject dsi
+		dsi = NULL;
 		break;
 	case GF_CODECID_PNG:
 		m_subtype = GF_ISOM_BOX_TYPE_PNG;
 		comp_name = "PNG";
+		//use generic sample desc, but don't inject dsi
+		dsi = NULL;
 		break;
 	case GF_CODECID_J2K:
 		m_subtype = GF_ISOM_BOX_TYPE_MJP2;

commit 2c0ac090f59ae4bcfd16fca14474baa01d2e3888

fixed bug introduced by 640ab0e8

src/filters/mux_isom.c
@@ -1546,7 +1546,8 @@ sample_entry_setup:
 		m_subtype = GF_ISOM_BOX_TYPE_JPEG;
 		comp_name = "JPEG";
 		//use generic sample desc, but don't inject dsi
-		dsi = NULL;
+		if (!use_m4sys)
+			dsi = NULL;
 		break;
 	case GF_CODECID_PNG:
 		m_subtype = GF_ISOM_BOX_TYPE_PNG;