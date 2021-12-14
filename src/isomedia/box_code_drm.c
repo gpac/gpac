gpac/gpac

commit 6bde8a2d709c5e3a41b7896ecc56d79bc5b46110

fixed #1972

src/isomedia/box_code_drm.c
@@ -1358,6 +1358,11 @@ GF_Err senc_Parse(GF_BitStream *bs, GF_TrackBox *trak, void *traf, GF_SampleEncr
 		if (trak) {
 			e = gf_isom_get_sample_cenc_info_internal(trak, traf, senc, sample_number, &is_encrypted, NULL, NULL, &key_info, &key_info_size);
 			if (! key_info) {
+				if (!key_info_size) {
+					GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[isobmf] no key info and no associated IV size !\n" ));
+					gf_free(sai);
+					return GF_ISOM_INVALID_FILE;
+				}
 				IV_size = key_info_size; //piff default
 				use_multikey = GF_FALSE;
 				senc->piff_type = 2;

commit 01c07612b00dc8f8a779194ca94aaa232137792f

fixed bug introduced by 6bde8a2

src/isomedia/box_code_drm.c
@@ -1358,7 +1358,7 @@ GF_Err senc_Parse(GF_BitStream *bs, GF_TrackBox *trak, void *traf, GF_SampleEncr
 		if (trak) {
 			e = gf_isom_get_sample_cenc_info_internal(trak, traf, senc, sample_number, &is_encrypted, NULL, NULL, &key_info, &key_info_size);
 			if (! key_info) {
-				if (!key_info_size) {
+				if (!key_info_size && is_encrypted) {
 					GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[isobmf] no key info and no associated IV size !\n" ));
 					gf_free(sai);
 					return GF_ISOM_INVALID_FILE;