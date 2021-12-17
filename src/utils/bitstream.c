391e48d7d89f2e75a8f367d6d336b6af0fd28e3a
jeanlf <jeanlf@gpac.io>
2021-12-17 17:51:17 +0100

    fixed #1995

--- src/utils/bitstream.c
+++ src/utils/bitstream.c
@@ -609,7 +609,23 @@ u64 gf_bs_read_long_int(GF_BitStream *bs, u32 nBits)
 {
 	u64 ret = 0;
 	if (nBits>64) {
-		gf_bs_read_long_int(bs, nBits-64);
+		u32 skip = nBits-64;
+		if (gf_bs_available(bs) * 8 < nBits-8) {
+			if (bs->EndOfStream) bs->EndOfStream(bs->par);
+			bs->position = bs->size;
+			return 0;
+		}
+		GF_LOG(GF_LOG_ERROR, GF_LOG_CORE, ("Reading %d bits but max should be 64, skipping %d most significants bits\n", nBits, nBits-64));
+		//avoid recursion
+		while (skip) {
+			if (skip>32) {
+				gf_bs_read_int(bs, 32);
+				skip-=32;
+			} else {
+				gf_bs_read_int(bs, skip);
+				skip=0;
+			}
+		}
 		ret = gf_bs_read_long_int(bs, 64);
 	} else {
 		while (nBits-- > 0) {
