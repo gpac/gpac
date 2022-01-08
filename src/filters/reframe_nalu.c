gpac/gpac

commit 0b99209493b1dfd52bbcfd16335165cfce9c4a60

cleanup DolbyVision import - cf #1991

src/filters/reframe_nalu.c
@@ -65,6 +65,7 @@ typedef struct
 	u32 strict_poc;
 	u32 bsdbg;
 	GF_Fraction dur;
+	u32 dv_mode, dv_profile, dv_compatid;
 
 	//only one input pid declared
 	GF_FilterPid *ipid;
@@ -224,11 +225,14 @@ typedef struct
 	u8 last_layer_id, last_temporal_id;
 
 	u32 clli_crc, mdcv_crc;
+
+	u32 nb_dv_rpu, nb_dv_el;
 } GF_NALUDmxCtx;
 
 static void naludmx_enqueue_or_dispatch(GF_NALUDmxCtx *ctx, GF_FilterPacket *n_pck, Bool flush_ref);
 static void naludmx_finalize_au_flags(GF_NALUDmxCtx *ctx);
 static void naludmx_reset_param_sets(GF_NALUDmxCtx *ctx, Bool do_free);
+static void naludmx_set_dolby_vision(GF_NALUDmxCtx *ctx);
 
 
 GF_Err naludmx_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remove)
@@ -1487,6 +1491,39 @@ static void naludmx_update_clli_mdcv(GF_NALUDmxCtx *ctx, Bool reset_crc)
 	}
 }
 
+static void naludmx_set_dolby_vision(GF_NALUDmxCtx *ctx)
+{
+	u8 dv_cfg[24];
+
+	if (!ctx->dv_mode)
+		return;
+	//auto mode
+	if (ctx->dv_mode==1) {
+		if (!ctx->nb_dv_rpu && !ctx->nb_dv_el) return;
+	}
+
+	u32 dv_level = gf_dolby_vision_level(ctx->width, ctx->height, ctx->cur_fps.num, ctx->cur_fps.den, ctx->codecid);
+
+	memset(dv_cfg, 0, sizeof(u8)*24);
+	GF_BitStream *bs = gf_bs_new(dv_cfg, 24, GF_BITSTREAM_WRITE);
+	gf_bs_write_u8(bs, 1); //version major
+	gf_bs_write_u8(bs, 0); //version minor
+	gf_bs_write_int(bs, ctx->dv_profile, 7);
+	gf_bs_write_int(bs, dv_level, 6);
+	gf_bs_write_int(bs, ctx->nb_dv_rpu, 1);
+	gf_bs_write_int(bs, ctx->nb_dv_el, 1);
+	gf_bs_write_int(bs, 1, 1); //bl_present_flag always true, we don't split streams
+	gf_bs_write_int(bs, ctx->dv_compatid, 4); //bl_present_flag always true, we don't split streams
+	//the rest is zero-reserved
+	gf_bs_write_int(bs, 0, 28);
+	gf_bs_write_u32(bs, 0);
+	gf_bs_write_u32(bs, 0);
+	gf_bs_write_u32(bs, 0);
+	gf_bs_write_u32(bs, 0);
+	gf_bs_del(bs);
+	gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_DOLBY_VISION, &PROP_DATA(dv_cfg, 24));
+}
+
 static void naludmx_check_pid(GF_Filter *filter, GF_NALUDmxCtx *ctx, Bool force_au_flush)
 {
 	u32 w, h, ew, eh;
@@ -1526,6 +1563,12 @@ static void naludmx_check_pid(GF_Filter *filter, GF_NALUDmxCtx *ctx, Bool force_
 	crc_cfg = crc_cfg_enh = 0;
 	if (dsi) crc_cfg = gf_crc_32(dsi, dsi_size);
 	if (dsi_enh) crc_cfg_enh = gf_crc_32(dsi_enh, dsi_enh_size);
+
+	if (!w || !h) {
+		if (dsi) gf_free(dsi);
+		if (dsi_enh) gf_free(dsi_enh);
+		return;
+	}
 
 	if (!ctx->opid) {
 		ctx->opid = gf_filter_pid_new(filter);
@@ -1645,6 +1688,9 @@ static void naludmx_check_pid(GF_Filter *filter, GF_NALUDmxCtx *ctx, Bool force_
 	}
 
 	naludmx_update_clli_mdcv(ctx, GF_TRUE);
+
+	naludmx_set_dolby_vision(ctx);
+
 }
 
 static Bool naludmx_process_event(GF_Filter *filter, const GF_FilterEvent *evt)
@@ -2216,10 +2262,14 @@ static s32 naludmx_parse_nal_hevc(GF_NALUDmxCtx *ctx, char *data, u32 size, Bool
 
 	//parsing is partial, see https://github.com/DolbyLaboratories/dlb_mp4base/blob/70a2e1d4d99a8439b7b8087bf50dd503eeea2291/src/esparser/parser_hevc.c#L1233
 	case GF_HEVC_NALU_DV_RPU:
-		ctx->hevc_state->dv_rpu = GF_TRUE;
+		ctx->nb_dv_rpu ++;
+		if (ctx->nb_dv_rpu==1)
+			naludmx_set_dolby_vision(ctx);
 		break;
 	case GF_HEVC_NALU_DV_EL:
-		ctx->hevc_state->dv_el = GF_TRUE;
+		ctx->nb_dv_el ++;
+		if (ctx->nb_dv_el==1)
+			naludmx_set_dolby_vision(ctx);
 		break;
 
 	default:
@@ -2576,6 +2626,19 @@ static s32 naludmx_parse_nal_avc(GF_NALUDmxCtx *ctx, char *data, u32 size, u32 n
 	case GF_AVC_NALU_SLICE_AUX:
 		*is_slice = GF_TRUE;
 		break;
+
+	case GF_AVC_NALU_DV_RPU:
+		ctx->nb_dv_rpu ++;
+		if (ctx->nb_dv_rpu==1)
+			naludmx_set_dolby_vision(ctx);
+		break;
+	case GF_AVC_NALU_DV_EL:
+		ctx->nb_dv_el ++;
+		if (ctx->nb_dv_el==1)
+			naludmx_set_dolby_vision(ctx);
+		break;
+		break;
+
 	}
 	return res;
 }
@@ -3790,6 +3853,20 @@ static const GF_FilterArgs NALUDmxArgs[] =
 	{ OFFS(analyze), "skip reformat of decoder config and SEI and dispatch all NAL in input order - shall only be used with inspect filter analyze mode!", GF_PROP_UINT, "off", "off|on|bs|full", GF_FS_ARG_HINT_HIDE},
 	{ OFFS(notime), "ignore input timestamps, rebuild from 0", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 
+	{ OFFS(dv_mode), "profile injection for DolbyVision\n"
+	"- none: never signal DV profile\n"
+	"- auto: signal DV profile if RPU or EL are found\n"
+	"- force: always signal DV profile"
+	, GF_PROP_UINT, "auto", "none|auto|force", GF_FS_ARG_HINT_ADVANCED},
+	{ OFFS(dv_profile), "profile for DolbyVision (currently defined profiles are 4, 5, 7, 8, 9)", GF_PROP_UINT, "5", NULL, GF_FS_ARG_HINT_ADVANCED},
+	{ OFFS(dv_compatid), "cross-compatibility ID for DolbyVision\n"
+		"- none: do not signal compatibility\n"
+		"- hdr10: CTA HDR10, as specified by EBU TR 03\n"
+		"- bt709: SDR BT.709\n"
+		"- hlg709: HLG BT.709 gamut in ITU-R BT.2020\n"
+		"- hlg2100: HLG BT.2100 gamut in ITU-R BT.2020\n"
+		"- bt2020: SDR BT.2020\n"
+		"- brd: Ultra HD Blu-ray Disc HDR", GF_PROP_UINT, "none", "none|hdr10|bt709|hlg709|hlg2100|bt2020|brd", GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(bsdbg), "debug NAL parsing in parser@debug logs\n"
 		"- off: not enabled\n"
 		"- on: enabled\n"

applications/mp4box/filedump.c
@@ -2670,9 +2670,11 @@ void DumpTrackInfo(GF_ISOFile *file, GF_ISOTrackID trackID, Bool full_dump, Bool
 
 		GF_DOVIDecoderConfigurationRecord *dovi = gf_isom_dovi_config_get(file, trackNum, 1);
 		if (dovi) {
-			fprintf(stderr, "DolbyVision version %d.%d profile %d level %d (RPU: %d Base Layer: %d Enhancement Layer: %d)\n",
+			fprintf(stderr, "DolbyVision version %d.%d profile %d level %d (RPU: %d Base Layer: %d Enhancement Layer: %d Compatibility: %d)\n",
 				dovi->dv_version_major, dovi->dv_version_minor, dovi->dv_profile, dovi->dv_level,
-				dovi->rpu_present_flag, dovi->bl_present_flag, dovi->el_present_flag);
+				dovi->rpu_present_flag, dovi->bl_present_flag, dovi->el_present_flag, dovi->dv_bl_signal_compatibility_id);
+
+			gf_odf_dovi_cfg_del(dovi);
 		}
 	}
 

applications/mp4box/fileimport.c
@@ -343,6 +343,114 @@ Bool scan_color(char *val, u32 *clr_prim, u32 *clr_tranf, u32 *clr_mx, Bool *clr
 	return GF_FALSE;
 }
 
+static GF_Err set_dv_profile(GF_ISOFile *dest, u32 track, char *dv_profile_str)
+{
+	GF_Err e;
+	u32 dv_profile = 0;
+	u32 dv_compat_id=0;
+	char *sep = strchr(dv_profile_str, '.');
+	if (sep) {
+		sep[0] = 0;
+		if (!strcmp(sep+1, "none")) dv_compat_id=0;
+		else if (!strcmp(sep+1, "hdr10")) dv_compat_id=1;
+		else if (!strcmp(sep+1, "bt709")) dv_compat_id=2;
+		else if (!strcmp(sep+1, "hlg709")) dv_compat_id=3;
+		else if (!strcmp(sep+1, "hlg2100")) dv_compat_id=4;
+		else if (!strcmp(sep+1, "bt2020")) dv_compat_id=5;
+		else if (!strcmp(sep+1, "brd")) dv_compat_id=6;
+		else {
+			M4_LOG(GF_LOG_WARNING, ("DV compatibility mode %s not recognized, using none\n", sep+1));
+		}
+	}
+	dv_profile = atoi(dv_profile_str);
+
+	GF_DOVIDecoderConfigurationRecord *dovi = gf_isom_dovi_config_get(dest, track, 1);
+	if (dovi) {
+		dovi->dv_profile = dv_profile;
+		dovi->dv_bl_signal_compatibility_id = dv_compat_id;
+		e = gf_isom_set_dolby_vision_profile(dest, track, 1, dovi);
+		gf_odf_dovi_cfg_del(dovi);
+		return e;
+	}
+	if (!dv_profile) return GF_OK;
+	u32 nb_samples = gf_isom_get_sample_count(dest, track);
+	if (!nb_samples) {
+		M4_LOG(GF_LOG_ERROR, ("No DV config in file and no samples, cannot guess DV config\n"));
+		return GF_NOT_SUPPORTED;
+	}
+
+	GF_DOVIDecoderConfigurationRecord _dovi;
+	memset(&_dovi, 0, sizeof(GF_DOVIDecoderConfigurationRecord));
+	_dovi.dv_version_major = 1;
+	_dovi.dv_version_minor = 0;
+	_dovi.dv_profile = dv_profile;
+	_dovi.dv_bl_signal_compatibility_id = dv_compat_id;
+
+	u32 w, h;
+	Bool is_avc = GF_FALSE;
+
+	e = gf_isom_get_visual_info(dest, track, 1, &w, &h);
+	if (e) return e;
+	//no DV profile present in file, we need to guess
+	GF_AVCConfig *avcc = gf_isom_avc_config_get(dest, track, 1);
+	GF_HEVCConfig *hvcc = gf_isom_hevc_config_get(dest, track, 1);
+
+	if (!avcc && !hvcc) {
+		M4_LOG(GF_LOG_WARNING, ("DV profile can only be set on AVC or HEVC tracks\n"));
+		return GF_BAD_PARAM;
+	}
+
+	u32 nalu_length = avcc ? avcc->nal_unit_size : hvcc->nal_unit_size;
+	if (avcc) {
+		is_avc = GF_TRUE;
+		gf_odf_avc_cfg_del(avcc);
+	}
+	if (hvcc) gf_odf_hevc_cfg_del(hvcc);
+
+	//inspect at most first 50 samples
+	u32 i;
+	for (i=0; i<50; i++) {
+		u32 stsd_idx;
+		GF_BitStream *bs;
+
+		GF_ISOSample *samp = gf_isom_get_sample(dest, track, i+1, &stsd_idx);
+		if (!samp) break;
+
+		bs = gf_bs_new(samp->data, samp->dataLength, GF_BITSTREAM_READ);
+
+		while (gf_bs_available(bs)) {
+			u32 nal_type;
+			u32 nal_size = gf_bs_read_int(bs, nalu_length*8);
+			if (is_avc) {
+				nal_type = gf_bs_read_u8(bs);
+				nal_type = nal_type & 0x1F;
+				nal_size--;
+				if (nal_type == GF_AVC_NALU_DV_RPU) _dovi.rpu_present_flag = 1;
+				else if (nal_type == GF_AVC_NALU_DV_EL) _dovi.el_present_flag = 1;
+				else if (nal_type <= GF_AVC_NALU_IDR_SLICE) _dovi.bl_present_flag = 1;
+			} else {
+				gf_bs_read_int(bs, 1);
+				nal_type = gf_bs_read_int(bs, 6);
+				gf_bs_read_int(bs, 1);
+				nal_size--;
+				if (nal_type == GF_HEVC_NALU_DV_RPU) _dovi.rpu_present_flag = 1;
+				else if (nal_type == GF_HEVC_NALU_DV_EL) _dovi.el_present_flag = 1;
+				else if (nal_type <= GF_HEVC_NALU_SLICE_CRA) _dovi.bl_present_flag = 1;
+			}
+			gf_bs_skip_bytes(bs, nal_size);
+		}
+		gf_bs_del(bs);
+		gf_isom_sample_del(&samp);
+	}
+
+	u64 mdur = gf_isom_get_media_duration(dest, track);
+	mdur /= nb_samples;
+	u32 timescale = gf_isom_get_media_timescale(dest, track);
+
+	_dovi.dv_level = gf_dolby_vision_level(w, h, timescale, mdur, is_avc ? GF_CODECID_AVC : GF_CODECID_HEVC);
+	return gf_isom_set_dolby_vision_profile(dest, track, 1, &_dovi);
+}
+
 
 
 GF_Err apply_edits(GF_ISOFile *dest, u32 track, char *edits)
@@ -479,7 +587,7 @@ GF_Err import_file(GF_ISOFile *dest, char *inName, u32 import_flags, GF_Fraction
 	Bool has_mx=GF_FALSE;
 	s32 mx[9];
 	u32 bitdepth=0;
-	u32 dv_profile=0; /*Dolby Vision*/
+	char dv_profile[100]; /*Dolby Vision*/
 	u32 clr_type=0;
 	u32 clr_prim;
 	u32 clr_tranf;
@@ -511,6 +619,7 @@ GF_Err import_file(GF_ISOFile *dest, char *inName, u32 import_flags, GF_Fraction
 	u32 roll = 0;
 	Bool src_is_isom = GF_FALSE;
 
+	dv_profile[0] = 0;
 	rvc_predefined = 0;
 	chapter_name = NULL;
 	new_timescale = 1;
@@ -1007,7 +1116,7 @@ GF_Err import_file(GF_ISOFile *dest, char *inName, u32 import_flags, GF_Fraction
 			}
 		}
 		else if (!strnicmp(ext + 1, "dv-profile=", 11)) {
-			dv_profile = atoi(ext + 12);
+			strncpy(dv_profile, ext + 12, 100);
 		}
 		else if (!strnicmp(ext+1, "fullrange=", 10)) {
 			if (!stricmp(ext+11, "off") || !stricmp(ext+11, "no")) fullrange = 0;
@@ -1389,8 +1498,8 @@ GF_Err import_file(GF_ISOFile *dest, char *inName, u32 import_flags, GF_Fraction
 				e = gf_isom_set_visual_color_info(dest, track, 1, clr_type, clr_prim, clr_tranf, clr_mx, clr_full_range, icc_data, icc_size);
 				GOTO_EXIT("changing color info")
 			}
-			if (dv_profile) {
-				e = gf_isom_set_dolby_vision_profile(dest, track, 1, dv_profile);
+			if (dv_profile[0]) {
+				e = set_dv_profile(dest, track, dv_profile);
 				GOTO_EXIT("setting DV profile")
 			}
 

applications/mp4box/main.c
@@ -825,7 +825,10 @@ static MP4BoxArg m4b_imp_fileopt_args [] = {
 		"  - nclx,p,t,m,r: same as `nclx` with r full range flag (`yes`, `on` or `no`, `off`)\n"
 		"  - prof,path: with path indicating the file containing the ICC color profile\n"
 		"  - rICC,path: with path indicating the file containing the restricted ICC color profile", NULL, NULL, GF_ARG_STRING, 0),
-	GF_DEF_ARG("dv-profile", NULL, "`S` set the Dolby Vision profile", NULL, NULL, GF_ARG_INT, 0),
+	GF_DEF_ARG("dv-profile", NULL, "`S` set the Dolby Vision profile on imported track\n"
+	"- Profile is an integer, 0 removes DV signaling\n"
+	"- Profile can be suffixed with compatibility ID, e.g. `5.hdr10`\n"
+	"- Allowed compatibility ID are `none`, `hdr10`, `bt709`, `hlg709`, `hlg2100`, `bt2020`, `brd`", NULL, NULL, GF_ARG_STRING, 0),
 	GF_DEF_ARG("fullrange", NULL, "`S` force the video fullrange type in VUI for the AVC|H264 content (value `yes`, `on` or `no`, `off`)", NULL, NULL, GF_ARG_STRING, 0),
 	GF_DEF_ARG("videofmt", NULL, "`S` force the video format in VUI for AVC|H264 and HEVC content, value can be `component`, `pal`, `ntsc`, `secam`, `mac`, `undef`", NULL, NULL, GF_ARG_STRING, 0),
 	GF_DEF_ARG("colorprim", NULL, "`S` force the colour primaries in VUI for AVC|H264 and HEVC (int or string, cf `-h cicp`)", NULL, NULL, GF_ARG_STRING, 0),

include/gpac/internal/media_dev.h
@@ -567,11 +567,6 @@ typedef struct _hevc_state
 	s32 last_parsed_sps_id;
 	s32 last_parsed_pps_id;
 
-	// Dolby Vision
-	Bool dv_rpu;
-	Bool dv_el;
-
-
 	u8 clli_data[4];
 	u8 mdcv_data[24];
 	u8 clli_valid, mdcv_valid;

include/gpac/isomedia.h
@@ -2450,10 +2450,10 @@ GF_Err gf_isom_set_high_dynamic_range_info(GF_ISOFile *isom_file, u32 trackNumbe
 \param isom_file the target ISO file
 \param trackNumber the target track number
 \param sampleDescriptionIndex the target sample description index
-\param dv_profile the Dolby Vision profile
+\param dvcc the Dolby Vision configuration
 \return error if any
 */
-GF_Err gf_isom_set_dolby_vision_profile(GF_ISOFile* isom_file, u32 trackNumber, u32 sampleDescriptionIndex, u32 dv_profile);
+GF_Err gf_isom_set_dolby_vision_profile(GF_ISOFile* isom_file, u32 trackNumber, u32 sampleDescriptionIndex, GF_DOVIDecoderConfigurationRecord *dvcc);
 
 
 /*! sets image sequence coding constraints (mostly used for HEIF image files)

include/gpac/media_tools.h
@@ -1437,6 +1437,16 @@ GF_Err gf_saf_mux_for_time(GF_SAFMuxer *mux, u32 time_ms, Bool force_end_of_sess
 */
 void gf_media_get_video_timing(Double fps, u32 *timescale, u32 *ts_inc);
 
+/*! gets dolby vision level
+ \param width width in pixels of video
+ \param height height in pixels of video
+ \param fps_num framerate numerator
+ \param fps_den framerate denominator
+ \param codecid GPAC codec ID
+ \return dv level
+*/
+u32 gf_dolby_vision_level(u32 width, u32 height, u32 fps_num, u32 fps_den, u32 codecid);
+
 #ifdef __cplusplus
 }
 #endif

share/doc/man/gpac-filters.1
@@ -2398,6 +2398,36 @@ audelim (bool, default: false): keep Access Unit delimiter in payload
 .br
 notime (bool, default: false): ignore input timestamps, rebuild from 0
 .br
+dv_mode (enum, default: auto): profile injection for DolbyVision
+.br
+* none: never signal DV profile
+.br
+* auto: signal DV profile if RPU or EL are found
+.br
+* force: always signal DV profile
+.br
+
+.br
+dv_profile (uint, default: 5): profile for DolbyVision (currently defined profiles are 4, 5, 7, 8, 9)
+.br
+dv_compatid (enum, default: none): cross-compatibility ID for DolbyVision
+.br
+* none: do not signal compatibility
+.br
+* hdr10: CTA HDR10, as specified by EBU TR 03
+.br
+* bt709: SDR BT.709
+.br
+* hlg709: HLG BT.709 gamut in ITU-R BT.2020
+.br
+* hlg2100: HLG BT.2100 gamut in ITU-R BT.2020
+.br
+* bt2020: SDR BT.2020
+.br
+* brd: Ultra HD Blu-ray Disc HDR
+.br
+
+.br
 bsdbg (enum, default: off):    debug NAL parsing in parser@debug logs
 .br
 * off: not enabled
@@ -9475,7 +9505,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 1.1.0-DEV-rev1593-g786b21cdb-master
+build: 1.1.0-DEV-rev1599-g28c31ce97-master
 .br
 Copyright: (c) 2000-2021 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

commit 54c2427ed6ddef6a408fccb75434a1d9dd94b7fd

fixed bug introduced by 0b992094

src/filters/reframe_nalu.c
@@ -1584,7 +1584,7 @@ static void naludmx_check_pid(GF_Filter *filter, GF_NALUDmxCtx *ctx, Bool force_
 	if (dsi) crc_cfg = gf_crc_32(dsi, dsi_size);
 	if (dsi_enh) crc_cfg_enh = gf_crc_32(dsi_enh, dsi_enh_size);
 
-	if (!w || !h) {
+	if (!ctx->analyze && (!w || !h)) {
 		if (dsi) gf_free(dsi);
 		if (dsi_enh) gf_free(dsi_enh);
 		return;