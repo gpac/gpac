ad6f0e495f15d57061b91990de6549a560c9717b
Aurelien David <aurelien.david@telecom-paristech.fr>
2022-01-17 15:35:59 +0100

    fix overflow on script_dec (#2052)

--- src/bifs/script_dec.c
+++ src/bifs/script_dec.c
@@ -72,13 +72,13 @@ static void SFS_AddString(ScriptParser *parser, char *str)
 	char *new_str;
 	if (!str) return;
 	if (strlen(parser->string) + strlen(str) >= parser->length) {
-		parser->length += PARSER_STEP_ALLOC;
+		parser->length = strlen(parser->string) + strlen(str) + PARSER_STEP_ALLOC;
 		new_str = (char *)gf_malloc(sizeof(char)*parser->length);
 		strcpy(new_str, parser->string);
 		gf_free(parser->string);
 		parser->string = new_str;
 	}
-	strcat(parser->string, str);
+	strncat(parser->string, str, parser->length - strlen(parser->string) - 1);
 }
 
 static void SFS_AddInt(ScriptParser *parser, s32 val)
