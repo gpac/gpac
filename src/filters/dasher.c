gpac/gpac

commit a6b64082b43226f51dff69a5e241049cf5e7eb83

added master/leaf key scheme support - cf 2043

src/filters/encrypt_cenc_isma.c
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2018-2021
+ *			Copyright (c) Telecom ParisTech 2018-2022
  *					All rights reserved
  *
  *  This file is part of GPAC / CENC and ISMA encrypt module
@@ -121,6 +121,10 @@ typedef struct
 	Bool slice_header_clear;
 
 	GF_PropUIntList mkey_indices;
+
+	//0: no generation, >0: use v-1 as key idx
+	u32 pssh_template_plus_one;
+	GF_List *pssh_templates;
 } GF_CENCStream;
 
 typedef struct
@@ -411,6 +415,15 @@ static GF_Err cenc_parse_pssh(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, const cha
 	pssh_bs = gf_bs_new(NULL, 0, GF_BITSTREAM_WRITE);
 	gf_bs_write_u32(pssh_bs, 0);
 
+	if (cstr->pssh_templates) {
+		while (gf_list_count(cstr->pssh_templates)) {
+			GF_XMLNode *n = gf_list_pop_back(cstr->pssh_templates);
+			gf_xml_dom_node_del(n);
+		}
+		gf_list_del(cstr->pssh_templates);
+	}
+	cstr->pssh_templates = NULL;
+
 	i=0;
 	while ((node = (GF_XMLNode *) gf_list_enum(root->content, &i))) {
 		Bool is_pssh;
@@ -423,6 +436,14 @@ static GF_Err cenc_parse_pssh(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, const cha
 		s32 cypherOffset = -1;
 		Bool has_key = GF_FALSE, has_IV = GF_FALSE;
 
+		if (!strcmp(node->name, "DRMInfoTemplate")) {
+			if (!cstr->pssh_templates) cstr->pssh_templates = gf_list_new();
+			if (gf_list_add(cstr->pssh_templates, node) == GF_OK) {
+				i--;
+				gf_list_rem(root->content, i);
+			}
+			continue;
+		}
 		if (strcmp(node->name, "DRMInfo")) continue;
 
 		j = 0;
@@ -447,14 +468,14 @@ static GF_Err cenc_parse_pssh(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, const cha
 				e = gf_bin128_parse(att->value, cypherKey);
                 if (e != GF_OK) {
                     GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("[CENC] Cannnot parse cypherKey\n"));
-                    return e;
+                    break;
                 }
 				has_key = GF_TRUE;
 			} else if (!strcmp(att->name, "cypherIV")) {
 				e = gf_bin128_parse(att->value, cypherIV);
                 if (e != GF_OK) {
                     GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("[CENC] Cannnot parse cypherIV\n"));
-                    return e;
+                    break;
                 }
 				has_IV = GF_TRUE;
 			} else if (!strcmp(att->name, "cypherOffset")) {
@@ -470,15 +491,27 @@ static GF_Err cenc_parse_pssh(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, const cha
 		e = gf_xml_parse_bit_sequence(node, cfile_name, &specInfo, &specInfoSize);
 		if (e) {
 			if (specInfo) gf_free(specInfo);
-			gf_xml_dom_del(parser);
-			return e;
+			break;
 		}
 
 		bs = gf_bs_new(specInfo, specInfoSize, GF_BITSTREAM_READ);
 		gf_bs_read_data(bs, (char *)systemID, 16);
 		if (version) {
 			KID_count = gf_bs_read_u32(bs);
+			if (KID_count*16 > gf_bs_available(bs)) {
+				GF_LOG(GF_LOG_WARNING, GF_LOG_AUTHOR, ("[CENC/ISMA] Invalid PSSH blob, KID count %d but only %d bytes available\n", KID_count, gf_bs_available(bs)));
+				if (specInfo) gf_free(specInfo);
+				gf_bs_del(bs);
+				e = GF_NON_COMPLIANT_BITSTREAM;
+				break;
+			}
 			KIDs = (bin128 *)gf_malloc(KID_count*sizeof(bin128));
+			if (!KIDs) {
+				if (specInfo) gf_free(specInfo);
+				gf_bs_del(bs);
+				e = GF_OUT_OF_MEM;
+				break;
+			}
 			for (j = 0; j < KID_count; j++) {
 				gf_bs_read_data(bs, (char *)KIDs[j], 16);
 			}
@@ -489,17 +522,33 @@ static GF_Err cenc_parse_pssh(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, const cha
 		}
 		if (specInfoSize < 16 + (version ? 4 + 16*KID_count : 0)) {
 			GF_LOG(GF_LOG_WARNING, GF_LOG_AUTHOR, ("[CENC/ISMA] Invalid PSSH blob in version %d: size %d key count %d - ignoring PSSH\n", version, specInfoSize, KID_count));
+
+			if (specInfo) gf_free(specInfo);
+			gf_bs_del(bs);
+			if (KIDs) gf_free(KIDs);
 			continue;
 		}
 		len = specInfoSize - 16 - (version ? 4 + 16*KID_count : 0);
 		data = (char *)gf_malloc(len*sizeof(char));
+		if (!data) {
+			e = GF_OUT_OF_MEM;
+			if (specInfo) gf_free(specInfo);
+			gf_bs_del(bs);
+			if (KIDs) gf_free(KIDs);
+			break;
+		}
 		gf_bs_read_data(bs, data, len);
 
 		if (has_key && has_IV && (cypherOffset >= 0) && (cypherMode != 1)) {
 			GF_Crypt *gc = gf_crypt_open(GF_AES_128, GF_CTR);
 			if (!gc) {
 				GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("[CENC/ISMA] Cannot open AES-128 CTR\n"));
-				return GF_IO_ERR;
+				if (specInfo) gf_free(specInfo);
+				gf_bs_del(bs);
+				if (KIDs) gf_free(KIDs);
+				if (data) gf_free(data);
+				e = GF_IO_ERR;
+				break;
 			}
 			e = gf_crypt_init(gc, cypherKey, cypherIV);
 			gf_crypt_encrypt(gc, data+cypherOffset, len-cypherOffset);
@@ -526,7 +575,7 @@ static GF_Err cenc_parse_pssh(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, const cha
 
 	gf_xml_dom_del(parser);
 
-	if (nb_pssh) {
+	if (!e && nb_pssh) {
 		u8 *pssh=NULL;
 		u32 pssh_size=0;
 		u32 pos = (u32) gf_bs_get_position(pssh_bs);
@@ -799,6 +848,13 @@ static GF_Err cenc_enc_configure(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, const 
 		cstr->use_subsamples = GF_TRUE;
 	//CBCS mode with skip byte block may be used for any track, in which case we need subsamples
 	else if (cstr->tci->scheme_type == GF_CRYPT_TYPE_CBCS) {
+		const GF_PropertyValue *prop = gf_filter_pid_get_property(cstr->ipid, GF_PROP_PID_STREAM_TYPE);
+		if (prop && prop->value.uint != GF_STREAM_VISUAL) {
+			if (cstr->tci->skip_byte_block) {
+				GF_LOG(GF_LOG_WARNING, GF_LOG_AUTHOR, ("\n[CENC] Using cbcs pattern mode on-video track is disabled in GPAC, using whole-block full encryption\n"));
+				cstr->tci->skip_byte_block = 0;
+			}
+		}
 		if (cstr->tci->skip_byte_block) {
 			cstr->use_subsamples = GF_TRUE;
 			GF_LOG(GF_LOG_WARNING, GF_LOG_AUTHOR, ("\n[CENC] Using cbcs pattern mode on non NAL video track, this may not be supported by most devices; consider setting skip_byte_block to 0\n\n"));
@@ -982,8 +1038,30 @@ static void cenc_free_pid_context(GF_CENCStream *cstr)
 
 	if (cstr->mkey_indices.vals) gf_free(cstr->mkey_indices.vals);
 	cenc_pid_reset_codec_states(cstr);
-
+	if (cstr->pssh_templates) {
+		while (gf_list_count(cstr->pssh_templates)) {
+			GF_XMLNode *n = gf_list_pop_back(cstr->pssh_templates);
+			gf_xml_dom_node_del(n);
+		}
+		gf_list_del(cstr->pssh_templates);
+	}
 	gf_free(cstr);
+}
+
+void cenc_gen_bin128(bin128 data)
+{
+	u64 *low = (u64 *) &data[0];
+	u64 *high = (u64 *) &data[8];
+
+	u64 val = gf_rand();
+	val <<= 32;
+	val |= gf_rand();
+	*low = val;
+
+	val = gf_rand();
+	val <<= 32;
+	val |= gf_rand();
+	*high = val;
 }
 
 static GF_Err cenc_enc_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remove)
@@ -1086,6 +1164,26 @@ static GF_Err cenc_enc_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool 
 	cstr->cinfo = (cinfo != ctx->cinfo) ? cinfo : NULL;
 	cstr->tci = tci;
 	cstr->passthrough = tci ? GF_FALSE : GF_TRUE;
+
+	if (tci->rand_keys) {
+		if (!tci->nb_keys) {
+			tci->keys = gf_realloc(tci->keys, sizeof(GF_CryptKeyInfo));
+			memset(&tci->keys[0], 0, sizeof(GF_CryptKeyInfo));
+		}
+		tci->nb_keys = 1;
+		if (tci->scheme_type == GF_CRYPT_TYPE_CBCS) {
+			tci->keys[0].IV_size = 0;
+			tci->keys[0].constant_IV_size = 16;
+		} else {
+			if (!tci->keys[0].IV_size) tci->keys[0].IV_size = 16;
+			tci->keys[0].constant_IV_size = 0;
+		}
+		tci->nb_keys = 1;
+		gf_rand_init(GF_TRUE);
+		cenc_gen_bin128(tci->keys[0].key);
+		cenc_gen_bin128(tci->keys[0].KID);
+		cenc_gen_bin128(tci->keys[0].IV);
+	}
 
 	//copy properties at init or reconfig
 	gf_filter_pid_copy_properties(cstr->opid, pid);
@@ -2067,6 +2165,149 @@ static GF_Err cenc_encrypt_packet(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, GF_Fi
 		}
 	}
 
+	if (cstr->pssh_template_plus_one) {
+		u32 key_idx = cstr->pssh_template_plus_one-1;
+		cstr->pssh_template_plus_one = 0;
+
+		if (cstr->pssh_templates) {
+			GF_XMLNode *pssh_tpl=NULL;
+			char szKID[33];
+			const char *cfile_name = ctx->cfile;
+			const GF_PropertyValue *prop = gf_filter_pid_get_property(cstr->ipid, GF_PROP_PID_CRYPT_INFO);
+			if (prop)  cfile_name = prop->value.string;
+			GF_BitStream *bs = gf_bs_new(NULL, 0, GF_BITSTREAM_WRITE);
+			u32 nb_systems=0;
+			//nb systems
+			gf_bs_write_u32(bs, 0);
+
+			szKID[0]=0;
+			for (i=0; i<16; i++) {
+				char szTmp[3];
+				sprintf(szTmp, "%02x", cstr->tci->keys[key_idx].KID[i]);
+				strcat(szKID, szTmp);
+			}
+			i=0;
+			while ((pssh_tpl = gf_list_enum(cstr->pssh_templates, &i))) {
+				u32 j=0;
+				GF_XMLNode *bs_node;
+				char szCryptKey[33];
+				u32 pssh_version=1;
+				u32 crypt_mode=GF_CBC;
+				char *key_att_backup = NULL;
+				char *kid_att_backup = NULL;
+				Bool valid=GF_TRUE;
+				GF_XMLAttribute *att, *sys_att=NULL, *key_val = NULL, *key_att = NULL, *kid_att = NULL, *iv_val=NULL;
+				while ((att = gf_list_enum(pssh_tpl->attributes, &j))) {
+					if (!stricmp(att->name, "system")) {
+						sys_att = att;
+					}
+					else if (!stricmp(att->name, "key")) {
+						key_val = att;
+					}
+					else if (!stricmp(att->name, "mode")) {
+						if (strcmp(att->value, "cbc")) crypt_mode = GF_CBC;
+						else if (strcmp(att->value, "ctr")) crypt_mode = GF_CTR;
+						else if (strcmp(att->value, "ecb")) crypt_mode = GF_ECB;
+						else {
+							GF_LOG(GF_LOG_WARNING, GF_LOG_AUTHOR, ("[ISMACrypt] Unsupported mode %s, ignoring DRMInfoTemplate\n", att->value));
+							valid=GF_FALSE;
+						}
+					}
+					else if (!stricmp(att->name, "IV")) {
+						iv_val = att;
+					}
+					else if (!stricmp(att->name, "version")) {
+						pssh_version = atoi(att->value);
+					}
+				}
+				if (!valid) continue;
+
+				if (!sys_att) {
+					GF_LOG(GF_LOG_WARNING, GF_LOG_AUTHOR, ("[ISMACrypt] Missing systemID, ignoring DRMInfoTemplate\n"));
+					continue;
+				}
+				if (!key_val) {
+					GF_LOG(GF_LOG_WARNING, GF_LOG_AUTHOR, ("[ISMACrypt] Missing keyval, ignoring DRMInfoTemplate\n"));
+					continue;
+				}
+
+				j=0;
+				while ((bs_node = gf_list_enum(pssh_tpl->content, &j))) {
+					u32 k=0;
+					if (strcmp(bs_node->name, "BS")) continue;
+					while ((att = gf_list_enum(bs_node->attributes, &k))) {
+						if (stricmp(att->name, "ID128")) continue;
+						if (!stricmp(att->value, "KEY"))
+							key_att = att;
+						else if (!stricmp(att->value, "KID"))
+							kid_att = att;
+					}
+				}
+
+				if (kid_att) {
+					kid_att_backup = kid_att->value;
+					kid_att->value = szKID;
+				}
+
+				if (key_val && key_att) {
+					bin128 master_key, leaf_key;
+					key_att_backup = key_att->value;
+					gf_bin128_parse(key_val->value, master_key);
+					GF_Crypt *crypto = gf_crypt_open(GF_AES_128, crypt_mode);
+					if (iv_val) {
+						bin128 IV;
+						gf_bin128_parse(iv_val->value, IV);
+						gf_crypt_init(crypto, master_key, IV);
+					} else {
+						gf_crypt_init(crypto, master_key, NULL);
+					}
+					memcpy(leaf_key, cstr->tci->keys[key_idx].key, 16);
+					gf_crypt_encrypt(crypto, leaf_key, 16);
+					gf_crypt_close(crypto);
+
+					szCryptKey[0]=0;
+					for (j=0; j<16; j++) {
+						char szTmp[3];
+						sprintf(szTmp, "%02x", leaf_key[j]);
+						strcat(szCryptKey, szTmp);
+					}
+					key_att->value = szCryptKey;
+				}
+
+				u8 *pssh_kid=NULL;
+				u32 pssh_kid_size;
+				GF_Err e = gf_xml_parse_bit_sequence(pssh_tpl, cfile_name, &pssh_kid, &pssh_kid_size);
+				if (e) {
+					GF_LOG(GF_LOG_WARNING, GF_LOG_AUTHOR, ("[ISMACrypt] Failed to get binary XML for PSSH template: %s\n", gf_error_to_string(e) ));
+				} else {
+					bin128 system_ID;
+					gf_bin128_parse(sys_att->value, system_ID);
+					gf_bs_write_data(bs, system_ID, 16); //systemID
+					gf_bs_write_u32(bs, pssh_version); //version
+					if (pssh_version) {
+						gf_bs_write_u32(bs, 1); //nb KIDs
+						gf_bs_write_data(bs, (const u8 *) cstr->tci->keys[key_idx].KID, 16); //kid
+					}
+					gf_bs_write_u32(bs, pssh_kid_size);
+					gf_bs_write_data(bs, pssh_kid, pssh_kid_size);
+					nb_systems++;
+				}
+				if (pssh_kid) gf_free(pssh_kid);
+				if (key_att_backup) key_att->value = key_att_backup;
+				if (kid_att_backup) kid_att->value = kid_att_backup;
+			}
+			u32 pssh_pos = (u32) gf_bs_get_position(bs);
+			gf_bs_seek(bs, 0);
+			gf_bs_write_u32(bs, nb_systems);
+			gf_bs_seek(bs, pssh_pos);
+			u8 *pssh;
+			u32 pssh_len;
+			gf_bs_get_content(bs, &pssh, &pssh_len);
+			gf_bs_del(bs);
+			gf_filter_pck_set_property(dst_pck, GF_PROP_PID_CENC_PSSH, &PROP_DATA_NO_COPY(pssh, pssh_len) );
+		}
+	}
+
 	gf_filter_pck_send(dst_pck);
 	return GF_OK;
 }
@@ -2166,6 +2407,8 @@ static GF_Err cenc_process(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, GF_FilterPac
 		return GF_OK;
 	}
 
+	cstr->pssh_template_plus_one = 0;
+
 	/*load initialization vector for the first sample in track ... */
 	if (!cstr->cenc_init) {
 		u32 i, nb_keys = cstr->multi_key ? cstr->tci->nb_keys : 1;
@@ -2193,23 +2436,32 @@ static GF_Err cenc_process(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, GF_FilterPac
 				GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("[CENC] Cannot initialize AES-128 %s (%s)\n", cstr->ctr_mode ? "CTR" : "CBC", gf_error_to_string(e)) );
 				return GF_IO_ERR;
 			}
+
+			cstr->pssh_template_plus_one = 1;
 		}
 		cstr->cenc_init = GF_TRUE;
 	} else if (!cstr->multi_key) {
 		u32 new_idx = cstr->kidx;
+		u32 nb_keys = cstr->tci->rand_keys ? 2 : cstr->tci->nb_keys;
 		Bool key_changed = GF_FALSE;
 
 		if (cstr->tci->keyRoll) {
-			new_idx = (cstr->nb_pck_encrypted / cstr->tci->keyRoll) % cstr->tci->nb_keys;
+			new_idx = (cstr->nb_pck_encrypted / cstr->tci->keyRoll) % nb_keys;
 		} else if (cstr->rap_roll) {
 			if ((sap==GF_FILTER_SAP_1) || (sap==GF_FILTER_SAP_2)) {
-				new_idx = (new_idx + 1) % cstr->tci->nb_keys;
+				new_idx = (new_idx + 1) % nb_keys;
 			}
 		}
 		if (cstr->kidx != new_idx) {
+			if (cstr->tci->rand_keys) {
+				new_idx = 0;
+				cenc_gen_bin128(cstr->tci->keys[0].KID);
+				cenc_gen_bin128(cstr->tci->keys[0].key);
+			}
 			cstr->kidx = new_idx;
 			memcpy(cstr->keys[0].key, cstr->tci->keys[cstr->kidx].key, 16);
 			key_changed = GF_TRUE;
+			cstr->pssh_template_plus_one = new_idx+1;
 			e = gf_crypt_set_key(cstr->keys[0].crypt, cstr->keys[0].key);
 			if (e) {
 				GF_LOG(GF_LOG_ERROR, GF_LOG_AUTHOR, ("[CENC] Cannot set key AES-128 %s (%s)\n", cstr->ctr_mode ? "CTR" : "CBC", gf_error_to_string(e)) );

src/filters/isoffin_read.c
@@ -1437,6 +1437,16 @@ static GF_Err isoffin_process(GF_Filter *filter)
 				if ((ch->streamType==GF_STREAM_AUDIO) && (ch->sample_num == gf_isom_get_sample_count(read->mov, ch->track))) {
 					gf_filter_pck_set_property(pck, GF_PROP_PCK_END_RANGE, &PROP_BOOL(GF_TRUE));
 				}
+				u32 pssh_idx=0;
+				gf_isom_get_sample_to_group_info(read->mov, ch->track, ch->sample_num, GF_4CC('P','S','S','H'), 0, &pssh_idx);
+				if (pssh_idx) {
+					const u8 *pssh;
+					u32 pssh_size;
+					gf_isom_get_sample_group_info(read->mov, ch->track, pssh_idx, GF_4CC('P','S','S','H'), NULL, &pssh, &pssh_size);
+					if (pssh) {
+						gf_filter_pck_set_property(pck, GF_PROP_PID_CENC_PSSH, &PROP_DATA((u8*)pssh, pssh_size) );
+					}
+				}
 
 				gf_filter_pck_send(pck);
 				isor_reader_release_sample(ch);

src/filters/mux_isom.c
@@ -185,6 +185,9 @@ typedef struct
 
 	u32 w_or_sr, h_or_ch, pf_or_af;
 	u32 xps_inband;
+
+	u8 *dyn_pssh;
+	u32 dyn_pssh_len;
 } TrackWriter;
 
 enum
@@ -569,6 +572,7 @@ static void mp4_mux_track_writer_del(TrackWriter *tkw)
 	if (tkw->vvcc) gf_odf_vvc_cfg_del(tkw->vvcc);
 	if (tkw->inband_hdr) gf_free(tkw->inband_hdr);
 	if (tkw->inband_hdr_non_rap) gf_free(tkw->inband_hdr_non_rap);
+	if (tkw->dyn_pssh) gf_free(tkw->dyn_pssh);
 	gf_free(tkw);
 }
 
@@ -3415,15 +3419,34 @@ enum
 	CENC_ADD_FRAG,
 };
 
-static void mp4_mux_cenc_insert_pssh(GF_MP4MuxCtx *ctx, TrackWriter *tkw)
+static void mp4_mux_cenc_insert_pssh(GF_MP4MuxCtx *ctx, TrackWriter *tkw, const GF_PropertyValue *pssh, Bool dyn_pssh_only)
 {
 	bin128 *keyIDs=NULL;
 	u32 max_keys = 0;
 	u32 i, nb_pssh;
+	GF_PropertyValue _the_prop;
 
 	//set pssh
-	const GF_PropertyValue *p = gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_CENC_PSSH);
-	if (!p) return;
+	const GF_PropertyValue *p;
+
+	if (dyn_pssh_only) {
+		GF_FilterPacket *pck;
+		//nothing to inject
+		if (!tkw->dyn_pssh) return;
+		pck = gf_filter_pid_get_packet(tkw->ipid);
+		if (pck) {
+			pssh = gf_filter_pck_get_property(pck, GF_PROP_PID_CENC_PSSH);
+			//change of dynamic pssh is pending, don't inject the old one
+			if (pssh) return;
+		}
+		_the_prop.type = GF_PROP_DATA;
+		_the_prop.value.data.ptr = tkw->dyn_pssh;
+		_the_prop.value.data.size = tkw->dyn_pssh_len;
+		p = &_the_prop;
+	} else {
+		p = pssh ? pssh : gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_CENC_PSSH);
+		if (!p) return;
+	}
 
 	if (!ctx->bs_r) ctx->bs_r = gf_bs_new(p->value.data.ptr, p->value.data.size, GF_BITSTREAM_READ);
 	else gf_bs_reassign_buffer(ctx->bs_r, p->value.data.ptr, p->value.data.size);
@@ -3458,6 +3481,16 @@ static void mp4_mux_cenc_insert_pssh(GF_MP4MuxCtx *ctx, TrackWriter *tkw)
 		gf_bs_skip_bytes(ctx->bs_r, len);
 	}
 	if (keyIDs) gf_free(keyIDs);
+
+
+	if (pssh) {
+		if (tkw->dyn_pssh) gf_free(tkw->dyn_pssh);
+		tkw->dyn_pssh = gf_malloc(sizeof(u8) * pssh->value.data.size);
+		if (!tkw->dyn_pssh) return;
+		memcpy(tkw->dyn_pssh, pssh->value.data.ptr, sizeof(u8) * pssh->value.data.size);
+		tkw->dyn_pssh_len = pssh->value.data.size;
+	}
+
 }
 
 static GF_Err mp4_mux_cenc_update(GF_MP4MuxCtx *ctx, TrackWriter *tkw, GF_FilterPacket *pck, u32 act_type, u32 pck_size, u32 injected_hdr_size)
@@ -3544,7 +3577,7 @@ static GF_Err mp4_mux_cenc_update(GF_MP4MuxCtx *ctx, TrackWriter *tkw, GF_Filter
 		}
 
 		if (ctx->psshs == MP4MX_PSSH_MOOV)
-			mp4_mux_cenc_insert_pssh(ctx, tkw);
+			mp4_mux_cenc_insert_pssh(ctx, tkw, NULL, GF_FALSE);
 
 		tkw->def_crypt_byte_block = crypt_byte_block;
 		tkw->def_skip_byte_block = skip_byte_block;
@@ -3625,6 +3658,15 @@ static GF_Err mp4_mux_cenc_update(GF_MP4MuxCtx *ctx, TrackWriter *tkw, GF_Filter
 	if (e) {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CONTAINER, ("[MP4Mux] Failed to set sample encryption group entry: %s)\n", gf_error_to_string(e) ));
 		return e;
+	}
+
+	p = gf_filter_pck_get_property(pck, GF_PROP_PID_CENC_PSSH);
+	if (p && (p->type == GF_PROP_DATA) && p->value.data.ptr) {
+		if (ctx->store>=MP4MX_MODE_FRAG) {
+			mp4_mux_cenc_insert_pssh(ctx, tkw, p, GF_FALSE);
+		} else {
+			gf_isom_set_sample_group_description(ctx->file, tkw->track_num, sample_num, GF_4CC('P','S','S','H'), 0, p->value.data.ptr, p->value.data.size);
+		}
 	}
 
 	if (!sai) {
@@ -4516,7 +4558,7 @@ static GF_Err mp4_mux_process_item(GF_MP4MuxCtx *ctx, TrackWriter *tkw, GF_Filte
 
 
 		if (tkw->insert_pssh) {
-			mp4_mux_cenc_insert_pssh(ctx, tkw);
+			mp4_mux_cenc_insert_pssh(ctx, tkw, NULL, GF_FALSE);
 			tkw->insert_pssh = GF_FALSE;
 		}
 	}
@@ -5131,8 +5173,11 @@ static GF_Err mp4_mux_start_fragment(GF_MP4MuxCtx *ctx, GF_FilterPacket *pck)
 		if (ctx->tfdt64)
 			gf_isom_set_fragment_option(ctx->file, tkw->track_id, GF_ISOM_TRAF_USE_LARGE_TFDT, ctx->tfdt64);
 
-		if (ctx->insert_pssh)
-			mp4_mux_cenc_insert_pssh(ctx, tkw);
+		if (tkw->dyn_pssh) {
+			mp4_mux_cenc_insert_pssh(ctx, tkw, NULL, GF_TRUE);
+		}
+		else if (ctx->insert_pssh)
+			mp4_mux_cenc_insert_pssh(ctx, tkw, NULL, GF_FALSE);
 	}
 	ctx->fragment_started = GF_TRUE;
 	ctx->insert_tfdt = GF_FALSE;
@@ -5499,6 +5544,20 @@ static GF_Err mp4_mux_process_fragmented(GF_Filter *filter, GF_MP4MuxCtx *ctx)
 					break;
 				}
 			}
+
+			if ((ctx->store>=MP4MX_MODE_FRAG) && tkw->samples_in_frag) {
+				p = gf_filter_pck_get_property(pck, GF_PROP_PID_CENC_PSSH);
+				if (p && (p->type == GF_PROP_DATA) && p->value.data.ptr && !ctx->flush_seg && !ctx->dash_mode) {
+					tkw->fragment_done = GF_TRUE;
+					tkw->samples_in_frag = 0;
+					nb_done ++;
+					if (ctx->store==MP4MX_MODE_SFRAG) {
+						ctx->adjusted_next_frag_start = gf_timestamp_rescale(cts - tkw->ts_delay, tkw->src_timescale, ctx->cdur.den);
+					}
+					break;
+				}
+			}
+
 			if (tkw->insert_tfdt) {
 				u64 odts = gf_filter_pck_get_dts(pck);
 				if (odts==GF_FILTER_NO_TS)

include/gpac/crypt.h
@@ -2,7 +2,7 @@
 *			GPAC - Multimedia Framework C SDK
 *
 *			Authors: Jean Le Feuvre
-*			Copyright (c) Telecom ParisTech 2000-2019
+*			Copyright (c) Telecom ParisTech 2000-2022
 *					All rights reserved
 *
 *  This file is part of GPAC / Crypto Tools sub-project
@@ -65,7 +65,9 @@ typedef enum {
 	/*! CBC chaining mode*/
 	GF_CBC = 0,
 	/*! CTR chaining mode*/
-	GF_CTR = 1
+	GF_CTR = 1,
+	/*! ECB (no chaining), payload must be a multiple of 16-bytes blocks*/
+	GF_ECB = 2,
 } GF_CRYPTO_MODE;
 
 /*! Algorithm mode to use*/

include/gpac/crypt_tools.h
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2000-2020
+ *			Copyright (c) Telecom ParisTech 2000-2022
  *					All rights reserved
  *
  *  This file is part of GPAC / Media Tools sub-project
@@ -98,18 +98,20 @@ enum
 /*! Key info structure, one per defined key in the DRM XML doc*/
 typedef struct
 {
+	//keep first 3 bin128 at the begining for data alignment
 	/*! KEY ID*/
 	bin128 KID;
 	/*! key value*/
 	bin128 key;
+	/*! constant IV or initial IV if not constant*/
+	u8 IV[16];
+
 	/*! hls_info defined*/
 	char *hls_info;
 	/*!IV size */
 	u8 IV_size;
 	/*! constant IV size */
 	u8 constant_IV_size;
-	/*! constant IV or initial IV if not constant*/
-	unsigned char IV[16];
 } GF_CryptKeyInfo;
 
 
@@ -184,6 +186,9 @@ typedef struct
 
 	/*! force using type set in XML rather than type indicated in file when decrypting*/
 	Bool force_type;
+
+	/*! generate random keys and key values*/
+	Bool rand_keys;
 
 	/*! randomly encrypts subsample if rand() % subs_rand is 0*/
 	u32 subs_rand;

include/gpac/utf.h
@@ -49,6 +49,7 @@ The wide characters in GPAC are unsignad shorts, in other words GPAC only suppor
 
 #include <gpac/tools.h>
 
+/*! error code for UTF-8 conversion errors*/
 #define GF_UTF8_FAIL	0xFFFFFFFF
 /*!
 \brief wide-char to multibyte conversion

src/crypto/g_crypt_openssl.c
@@ -1,8 +1,9 @@
 /*
 *			GPAC - Multimedia Framework C SDK
 *
-*			Authors: Rodolphe Fouquet
+*			Authors: Rodolphe Fouquet, Jean Le Feuvre
 *			Copyright (c) Motion Spell 2016
+*                     (c) Telecom Paris 2022
 *					All rights reserved
 *
 *  This file is part of GPAC / crypto lib sub-project
@@ -190,6 +191,73 @@ GF_Err gf_crypt_decrypt_openssl_ctr(GF_Crypt* td, u8 *ciphertext, u32 len)
 	return gf_crypt_crypt_openssl_ctr(td, ciphertext, len);
 }
 
+/* ECB */
+typedef struct {
+	AES_KEY key;
+} Openssl_ctx_ecb;
+
+/** CBC STUFF **/
+
+GF_Err gf_crypt_init_openssl_ecb(GF_Crypt* td, void *key, const void *iv)
+{
+	Openssl_ctx_ecb *ctx = (Openssl_ctx_ecb*)td->context;
+	if (!ctx) {
+		GF_SAFEALLOC(ctx, Openssl_ctx_ecb);
+		if (ctx == NULL) return GF_OUT_OF_MEM;
+		td->context = ctx;
+	}
+	return GF_OK;
+}
+
+void gf_crypt_deinit_openssl_ecb(GF_Crypt* td)
+{
+}
+
+void gf_set_key_openssl_ecb(GF_Crypt* td, void *key)
+{
+	Openssl_ctx_ecb* ctx = (Openssl_ctx_ecb*)td->context;
+	AES_set_encrypt_key(key, 128, &(ctx->key));
+	AES_set_decrypt_key(key, 128, &(ctx->key));
+}
+
+GF_Err gf_crypt_set_IV_openssl_ecb(GF_Crypt* td, const u8 *iv, u32 iv_size)
+{
+	return GF_OK;
+}
+
+GF_Err gf_crypt_get_IV_openssl_ecb(GF_Crypt* td, u8 *iv, u32 *iv_size)
+{
+	*iv_size = AES_BLOCK_SIZE;
+	memset(iv, 0, AES_BLOCK_SIZE);
+	return GF_OK;
+}
+
+GF_Err gf_crypt_crypt_openssl_ecb(GF_Crypt* td, u8 *plaintext, u32 len, u32 aes_crypt_type)
+{
+	Openssl_ctx_ecb* ctx = (Openssl_ctx_ecb*)td->context;
+	u32 iteration;
+	u32 numberOfIterations = len / AES_BLOCK_SIZE;
+	if (numberOfIterations * AES_BLOCK_SIZE < len) {
+		return GF_BAD_PARAM;
+	}
+
+	for (iteration = 0; iteration < numberOfIterations; ++iteration) {
+		AES_ecb_encrypt(plaintext + iteration*AES_BLOCK_SIZE, plaintext + iteration*AES_BLOCK_SIZE, &ctx->key, aes_crypt_type);
+	}
+	return GF_OK;
+}
+
+GF_Err gf_crypt_encrypt_openssl_ecb(GF_Crypt* td, u8 *plaintext, u32 len)
+{
+	return gf_crypt_crypt_openssl_ecb(td, plaintext, len, AES_ENCRYPT);
+}
+
+GF_Err gf_crypt_decrypt_openssl_ecb(GF_Crypt* td, u8 *ciphertext, u32 len)
+{
+	return gf_crypt_crypt_openssl_ecb(td, ciphertext, len, AES_DECRYPT);
+}
+
+
 
 GF_Err gf_crypt_open_open_openssl(GF_Crypt* td, GF_CRYPTO_MODE mode)
 {
@@ -212,6 +280,15 @@ GF_Err gf_crypt_open_open_openssl(GF_Crypt* td, GF_CRYPTO_MODE mode)
 		td->_decrypt = gf_crypt_decrypt_openssl_ctr;
 		td->_get_state = gf_crypt_get_IV_openssl_ctr;
 		td->_set_state = gf_crypt_set_IV_openssl_ctr;
+		break;
+	case GF_ECB:
+		td->_init_crypt = gf_crypt_init_openssl_ecb;
+		td->_deinit_crypt = gf_crypt_deinit_openssl_ecb;
+		td->_set_key = gf_set_key_openssl_ecb;
+		td->_crypt = gf_crypt_encrypt_openssl_ecb;
+		td->_decrypt = gf_crypt_decrypt_openssl_ecb;
+		td->_get_state = gf_crypt_get_IV_openssl_ecb;
+		td->_set_state = gf_crypt_set_IV_openssl_ecb;
 		break;
 	default:
 		return GF_BAD_PARAM;

commit c660dd2e64b5df5c9d7b930f66c135603fceafe4

fixed bugs introduced by a6b64082

src/filters/dasher.c
@@ -2117,8 +2117,7 @@ static GF_List *dasher_get_content_protection_desc(GF_DasherCtx *ctx, GF_DashStr
 				gf_bs_read_data(bs_r, sysID, 16);
 				version = gf_bs_read_u32(bs_r);
 
-				k_count = gf_bs_read_u32(bs_r);
-				if (k_count) version = 1;
+				k_count = version ? gf_bs_read_u32(bs_r) : 0;
 				gf_bs_write_u8(bs_w, version);
 				gf_bs_write_u24(bs_w, 0);
 				gf_bs_write_data(bs_w, sysID, 16);

src/filters/encrypt_cenc_isma.c
@@ -558,9 +558,11 @@ static GF_Err cenc_parse_pssh(GF_CENCEncCtx *ctx, GF_CENCStream *cstr, const cha
 			nb_pssh++;
 			gf_bs_write_data(pssh_bs, systemID, 16);
 			gf_bs_write_u32(pssh_bs, version);
-			gf_bs_write_u32(pssh_bs, KID_count);
-			for (j=0; j<KID_count; j++) {
-				gf_bs_write_data(pssh_bs, KIDs[j], 16);
+			if (version) {
+				gf_bs_write_u32(pssh_bs, KID_count);
+				for (j=0; j<KID_count; j++) {
+					gf_bs_write_data(pssh_bs, KIDs[j], 16);
+				}
 			}
 			gf_bs_write_u32(pssh_bs, len);
 			gf_bs_write_data(pssh_bs, data, len);
@@ -1165,7 +1167,7 @@ static GF_Err cenc_enc_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool 
 	cstr->tci = tci;
 	cstr->passthrough = tci ? GF_FALSE : GF_TRUE;
 
-	if (tci->rand_keys) {
+	if (tci && tci->rand_keys) {
 		if (!tci->nb_keys) {
 			tci->keys = gf_realloc(tci->keys, sizeof(GF_CryptKeyInfo));
 			memset(&tci->keys[0], 0, sizeof(GF_CryptKeyInfo));

src/filters/isoffin_read.c
@@ -532,9 +532,11 @@ void isor_declare_pssh(ISOMChannel *ch)
 
 		gf_bs_write_data(pssh_bs, SystemID, 16);
 		gf_bs_write_u32(pssh_bs, version);
-		gf_bs_write_u32(pssh_bs, KID_count);
-		for (s=0; s<KID_count; s++) {
-			gf_bs_write_data(pssh_bs, KIDs[s], 16);
+		if (version) {
+			gf_bs_write_u32(pssh_bs, KID_count);
+			for (s=0; s<KID_count; s++) {
+				gf_bs_write_data(pssh_bs, KIDs[s], 16);
+			}
 		}
 		gf_bs_write_u32(pssh_bs, private_data_size);
 		gf_bs_write_data(pssh_bs, private_data, private_data_size);

src/filters/mux_isom.c
@@ -3461,7 +3461,7 @@ static void mp4_mux_cenc_insert_pssh(GF_MP4MuxCtx *ctx, TrackWriter *tkw, const 
 
 		gf_bs_read_data(ctx->bs_r, sysID, 16);
 		version = gf_bs_read_u32(ctx->bs_r);
-		kid_count = gf_bs_read_u32(ctx->bs_r);
+		kid_count = version ? gf_bs_read_u32(ctx->bs_r) : 0;
 
 		if (kid_count>=max_keys) {
 			max_keys = kid_count;
@@ -3479,6 +3479,8 @@ static void mp4_mux_cenc_insert_pssh(GF_MP4MuxCtx *ctx, TrackWriter *tkw, const 
 
 		gf_cenc_set_pssh(ctx->file, sysID, version, kid_count, keyIDs, data, len, mode);
 		gf_bs_skip_bytes(ctx->bs_r, len);
+		if (gf_bs_is_overflow(ctx->bs_r))
+			break;
 	}
 	if (keyIDs) gf_free(keyIDs);
 