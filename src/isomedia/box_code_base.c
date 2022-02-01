64a2e1b799352ac7d7aad1989bc06e7b0f2b01db
jeanlf <jeanlf@gpac.io>
2022-02-01 15:28:23 +0100

    fixed #2092

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -11083,10 +11083,12 @@ void gitn_box_del(GF_Box *s)
 	u32 i;
 	GroupIdToNameBox *ptr = (GroupIdToNameBox *)s;
 	if (ptr == NULL) return;
-	for (i=0; i<ptr->nb_entries; i++) {
-		if (ptr->entries[i].name) gf_free(ptr->entries[i].name);
+	if (ptr->entries) {
+		for (i=0; i<ptr->nb_entries; i++) {
+			if (ptr->entries[i].name) gf_free(ptr->entries[i].name);
+		}
+		gf_free(ptr->entries);
 	}
-	if (ptr->entries) gf_free(ptr->entries);
 	gf_free(ptr);
 }
 
