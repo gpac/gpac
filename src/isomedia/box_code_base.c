a1cb693584c00dad0189b23019e0d57b3747082d
jeanlf <jeanlf@gpac.io>
2022-03-29 17:07:14 +0200

    fixed #2155

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -4050,18 +4050,20 @@ GF_Err audio_sample_entry_box_read(GF_Box *s, GF_BitStream *bs)
 	for (i=0; i<size-8; i++) {
 		if (GF_4CC((u32)data[i+4], (u8)data[i+5], (u8)data[i+6], (u8)data[i+7]) == GF_ISOM_BOX_TYPE_ESDS) {
 			GF_BitStream *mybs = gf_bs_new(data + i, size - i, GF_BITSTREAM_READ);
+			gf_bs_set_cookie(mybs, GF_ISOM_BS_COOKIE_NO_LOGS);
 			if (ptr->esd) gf_isom_box_del_parent(&ptr->child_boxes, (GF_Box *)ptr->esd);
 			ptr->esd = NULL;
 			e = gf_isom_box_parse((GF_Box **)&ptr->esd, mybs);
 			gf_bs_del(mybs);
 
-			if ((e==GF_OK) && (ptr->esd->type == GF_ISOM_BOX_TYPE_ESDS)) {
+			if ((e==GF_OK) && ptr->esd && (ptr->esd->type == GF_ISOM_BOX_TYPE_ESDS)) {
 				if (!ptr->child_boxes) ptr->child_boxes = gf_list_new();
 				gf_list_add(ptr->child_boxes, ptr->esd);
 			} else if (ptr->esd) {
 				gf_isom_box_del((GF_Box *)ptr->esd);
 				ptr->esd = NULL;
 			}
+			e = GF_OK;
 			break;
 		}
 	}
