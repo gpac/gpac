a5b930fc9237170f6eeb2521728a1d7c6ed7a021
Aurelien David <aurelien.david@telecom-paristech.fr>
2022-04-01 16:41:13 +0200

    make installable package with python bindings (#2161)

---
+++ share/python/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="libgpac",
+    version="2.2",
+    author="Jean Le Feuvre",
+    author_email="trac-gpac@listes.telecom-paristech.fr",
+    description="Python ctypes bindings for libgpac",
+    url="http://gpac.io",
+    packages=['libgpac'],
+    install_requires=[],
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+    "Operating System :: OS Independent",
+],
+
+)
