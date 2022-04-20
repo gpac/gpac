gpac/gpac

commit c4aa245659caee8ac60d2cbe775d3102c45d44ce

early detect qt-style meta when parsing and write meta as QT for mov output

include/gpac/internal/isomedia_dev.h
@@ -486,6 +486,8 @@ enum
 	GF_QT_BOX_TYPE_FIEL = GF_4CC('f','i','e','l'),
 	GF_QT_BOX_TYPE_GAMA = GF_4CC('g','a','m','a'),
 	GF_QT_BOX_TYPE_CHRM = GF_4CC('c','h','r','m'),
+	GF_QT_BOX_TYPE_STPS = GF_4CC('s','t','p','s'),
+	GF_QT_BOX_TYPE_CIOS = GF_4CC('c','i','o','s'),
 
 	/* from drm_sample.c */
 	GF_ISOM_BOX_TYPE_264B 	= GF_4CC('2','6','4','b'),
@@ -2506,7 +2508,7 @@ typedef struct __tag_meta_box
 	GF_ItemReferenceBox *item_refs;
 	GF_GroupListBox *groups_list;
 
-	u8 use_item_sample_sharing, use_item_item_sharing;
+	u8 use_item_sample_sharing, use_item_item_sharing, write_qt;
 } GF_MetaBox;
 
 typedef struct

src/isomedia/box_code_meta.c
@@ -101,12 +101,34 @@ GF_Err meta_on_child_box(GF_Box *s, GF_Box *a, Bool is_rem)
 
 GF_Err meta_box_read(GF_Box *s, GF_BitStream *bs)
 {
+	GF_MetaBox *ptr = (GF_MetaBox *)s;
 	u64 pos = gf_bs_get_position(bs);
 	u64 size = s->size;
-	GF_Err e = gf_isom_box_array_read(s, bs);
-	/*try to hack around QT files which don't use a full box for meta, rewind 4 bytes*/
-	if (e && (pos>4) ) {
-		gf_bs_seek(bs, pos-4);
+	//probe 4->8 bytes, if equal to one of the following this is a QT meta
+	u32 qt_type = (pos>8) ? gf_bs_peek_bits(bs, 32, 4) : 0;
+	switch (qt_type) {
+	case GF_4CC('h', 'd', 'l', 'r'):
+	case GF_4CC('m', 'h', 'd', 'r'):
+	case GF_4CC('k', 'e', 'y', 's'):
+	case GF_4CC('i', 'l', 's', 't'):
+	case GF_4CC('c', 't', 'r', 'y'):
+	case GF_4CC('l', 'a', 'n', 'g'):
+		break;
+	default:
+		qt_type = 0;
+		break;
+	}
+	GF_Err e = GF_EOS;
+	if (!qt_type) {
+		ISOM_DECREASE_SIZE(s, 4)
+		ptr->version = gf_bs_read_u8(bs);
+		ptr->flags = gf_bs_read_u24(bs);
+		e = gf_isom_box_array_read(s, bs);
+	}
+
+	/*try to hack around QT files which don't use a full box for meta, reparse*/
+	if (e && (pos>8) ) {
+		gf_bs_seek(bs, pos);
 		meta_reset(s);
 		s->size = size+4;
 		e = gf_isom_box_array_read(s, bs);
@@ -117,6 +139,10 @@ GF_Err meta_box_read(GF_Box *s, GF_BitStream *bs)
 #ifndef GPAC_DISABLE_ISOM_WRITE
 GF_Err meta_box_write(GF_Box *s, GF_BitStream *bs)
 {
+	GF_MetaBox *ptr = (GF_MetaBox *)s;
+	if (ptr->write_qt) {
+		return gf_isom_box_write_header(s, bs);
+	}
 	return gf_isom_full_box_write(s, bs);
 }
 
@@ -134,6 +160,9 @@ GF_Err meta_box_size(GF_Box *s)
 	gf_isom_check_position(s, (GF_Box *)ptr->item_refs, &pos);
 	gf_isom_check_position(s, (GF_Box *)ptr->item_props, &pos);
 	gf_isom_check_position(s, (GF_Box *)ptr->groups_list, &pos);
+
+	if (!ptr->write_qt)
+		s->size+=4;
 	return GF_OK;
 }
 #endif /*GPAC_DISABLE_ISOM_WRITE*/

src/isomedia/box_dump.c
@@ -1206,25 +1206,34 @@ GF_Err stss_box_dump(GF_Box *a, FILE * trace)
 {
 	GF_SyncSampleBox *p;
 	u32 i;
+	const char *name, *entname;
 
 	if (dump_skip_samples)
 		return GF_OK;
 
 	p = (GF_SyncSampleBox *)a;
-	gf_isom_box_dump_start(a, "SyncSampleBox", trace);
+	if (a->type==GF_ISOM_BOX_TYPE_STSS) {
+		name = "SyncSampleBox";
+		entname = "SyncSampleEntry";
+	} else {
+		name = "PartialSyncSampleBox";
+		entname = "PartialSyncSampleEntry";
+	}
+	gf_isom_box_dump_start(a, name, trace);
 	gf_fprintf(trace, "EntryCount=\"%d\">\n", p->nb_entries);
 
 	if (!p->sampleNumbers && p->size) {
-		gf_fprintf(trace, "<!--Warning: No Key Frames indications-->\n");
+		if (a->type==GF_ISOM_BOX_TYPE_STSS)
+			gf_fprintf(trace, "<!--Warning: No Key Frames indications-->\n");
 	} else if (p->sampleNumbers) {
 		for (i=0; i<p->nb_entries; i++) {
-			gf_fprintf(trace, "<SyncSampleEntry sampleNumber=\"%u\"/>\n", p->sampleNumbers[i]);
+			gf_fprintf(trace, "<%s sampleNumber=\"%u\"/>\n", entname, p->sampleNumbers[i]);
 		}
 	}
 	if (!p->size) {
-			gf_fprintf(trace, "<SyncSampleEntry sampleNumber=\"\"/>\n");
+			gf_fprintf(trace, "<%s sampleNumber=\"\"/>\n", entname);
 	}
-	gf_isom_box_dump_done("SyncSampleBox", a, trace);
+	gf_isom_box_dump_done(name, a, trace);
 	return GF_OK;
 }
 

src/isomedia/box_funcs.c
@@ -1077,7 +1077,7 @@ static struct box_registry_entry {
 	BOX_DEFINE( GF_ISOM_BOX_TYPE_BTRT, btrt, "sample_entry"),
 	BOX_DEFINE( GF_ISOM_BOX_TYPE_PASP, pasp, "video_sample_entry ipco"),
 	BOX_DEFINE( GF_ISOM_BOX_TYPE_CLAP, clap, "video_sample_entry ipco"),
-	FBOX_DEFINE_CHILD( GF_ISOM_BOX_TYPE_META, meta, "file moov trak moof traf udta", 0),	//apple uses meta in moov->udta
+	BOX_DEFINE_CHILD( GF_ISOM_BOX_TYPE_META, meta, "file moov trak moof traf udta"),	//apple uses meta in moov->udta
 	FBOX_DEFINE( GF_ISOM_BOX_TYPE_XML, xml, "meta", 0),
 	FBOX_DEFINE( GF_ISOM_BOX_TYPE_BXML, bxml, "meta", 0),
 	FBOX_DEFINE( GF_ISOM_BOX_TYPE_ILOC, iloc, "meta", 2),
@@ -1455,6 +1455,7 @@ static struct box_registry_entry {
 	BOX_DEFINE_S(GF_QT_BOX_TYPE_TAPT, def_parent, "trak", "apple"),
 	FBOX_DEFINE_S( GF_QT_BOX_TYPE_GMIN, gmin, "gmhd", 0, "apple"),
 	FBOX_DEFINE_FLAGS_S( GF_QT_BOX_TYPE_ALIS, alis, "dref", 0, 1, "apple"),
+	FBOX_DEFINE_FLAGS_S( GF_QT_BOX_TYPE_CIOS, alis, "dref", 0, 1, "apple"),
 	FBOX_DEFINE_S( GF_QT_BOX_TYPE_CLEF, clef, "tapt", 0, "apple"),
 	FBOX_DEFINE_S( GF_QT_BOX_TYPE_PROF, clef, "tapt", 0, "apple"),
 	FBOX_DEFINE_S( GF_QT_BOX_TYPE_ENOF, clef, "tapt", 0, "apple"),
@@ -1513,7 +1514,8 @@ static struct box_registry_entry {
 	BOX_DEFINE_S_CHILD( GF_QT_SUBTYPE_RGBA, video_sample_entry, "stsd", "apple"),
 	BOX_DEFINE_S_CHILD( GF_QT_SUBTYPE_ABGR, video_sample_entry, "stsd", "apple"),
 	
-	
+	FBOX_DEFINE_S(GF_QT_BOX_TYPE_STPS, stss, "stbl", 0, "apple"),
+
 	//dolby boxes
 	BOX_DEFINE_S_CHILD( GF_ISOM_BOX_TYPE_AC3, audio_sample_entry, "stsd", "dolby"),
 	BOX_DEFINE_S_CHILD( GF_ISOM_BOX_TYPE_EC3, audio_sample_entry, "stsd", "dolby"),

src/compositor/media_object.c
@@ -647,7 +647,7 @@ retry:
 		gf_filter_pid_try_pull(mo->odm->pid);
 	}
 	if (!retry_pull && (force_decode_mode==1)) {
-		GF_LOG(GF_LOG_INFO, GF_LOG_COMPTIME, ("[ODM%d] At %d could not force a pull from pid - POTENTIAL blank frame after TS %u\n", mo->odm->ID, gf_clock_time(mo->odm->ck), mo->timestamp));
+		GF_LOG(GF_LOG_INFO, GF_LOG_COMPTIME, ("[ODM%d] Could not force a pull from pid - POTENTIAL blank frame after TS %u\n", mo->odm->ID, mo->timestamp));
 	}
 
 	/*resync*/

src/isomedia/isom_write.c
@@ -4269,7 +4269,7 @@ GF_Err gf_isom_clone_track(GF_ISOFile *orig_file, u32 orig_track, GF_ISOFile *de
 		Bool use_alis = GF_FALSE;
 		if (! (flags & GF_ISOM_CLONE_TRACK_NO_QT)) {
 			GF_Box *b = gf_list_get(new_tk->Media->information->dataInformation->dref->child_boxes, 0);
-			if (b && b->type==GF_QT_BOX_TYPE_ALIS)
+			if (b && (b->type==GF_QT_BOX_TYPE_ALIS))
 				use_alis = GF_TRUE;
 		}
 		gf_isom_box_array_del(new_tk->Media->information->dataInformation->dref->child_boxes);
@@ -8568,3 +8568,22 @@ GF_Err gf_isom_add_sample_aux_info(GF_ISOFile *file, u32 track, u32 sampleNumber
 }
 
 #endif	/*!defined(GPAC_DISABLE_ISOM) && !defined(GPAC_DISABLE_ISOM_WRITE)*/
+
+
+GF_Err gf_isom_set_meta_qt(GF_ISOFile *file)
+{
+	u32 i, count;
+	if (!file) return GF_BAD_PARAM;
+	GF_Err e = CanAccessMovie(file, GF_ISOM_OPEN_WRITE);
+	if (e) return e;
+	if (file->moov->meta)
+		file->moov->meta->write_qt = 1;
+
+	count = gf_list_count(file->moov->trackList);
+	for (i=0; i<count; i++) {
+		GF_TrackBox *trak = gf_list_get(file->moov->trackList, i);
+		if (trak->meta)
+			trak->meta->write_qt = 1;
+	}
+	return GF_OK;
+}

src/isomedia/media.c
@@ -694,8 +694,14 @@ GF_Err Media_CheckDataEntry(GF_MediaBox *mdia, u32 dataEntryIndex)
 
 	//ok, not self contained, let's go for it...
 	//we only support alias and URL boxes
-	if ((entry->type != GF_ISOM_BOX_TYPE_URL) && (entry->type != GF_QT_BOX_TYPE_ALIS) )
+	switch (entry->type) {
+	case GF_ISOM_BOX_TYPE_URL:
+	case GF_QT_BOX_TYPE_ALIS:
+	case GF_QT_BOX_TYPE_CIOS:
+		break;
+	default:
 		return GF_NOT_SUPPORTED;
+	}
 
 	if (mdia->mediaTrack->moov->mov->openMode == GF_ISOM_OPEN_WRITE) {
 		e = gf_isom_datamap_new(entry->location, NULL, GF_ISOM_DATA_MAP_READ, &map);
@@ -730,6 +736,7 @@ Bool Media_IsSelfContained(GF_MediaBox *mdia, u32 StreamDescIndex)
 	if (a->flags & 1) return 1;
 	/*QT specific*/
 	if (a->type == GF_QT_BOX_TYPE_ALIS) return 1;
+	if (a->type == GF_QT_BOX_TYPE_CIOS) return 1;
 	return 0;
 }
 

src/media_tools/isom_tools.c
@@ -1080,6 +1080,7 @@ GF_Err gf_media_get_color_info(GF_ISOFile *file, u32 track, u32 sampleDescriptio
 	return GF_NOT_SUPPORTED;
 }
 
+GF_Err gf_isom_set_meta_qt(GF_ISOFile *file);
 
 GF_EXPORT
 GF_Err gf_media_check_qt_prores(GF_ISOFile *mp4)
@@ -1149,6 +1150,8 @@ GF_Err gf_media_check_qt_prores(GF_ISOFile *mp4)
 			GF_LOG(GF_LOG_WARNING, GF_LOG_MEDIA, ("[ProRes] Cannot change brand from \"%s\" to \"qt  \", flat storage used. Try using different storage mode\n", gf_4cc_to_str(brand)));
 		}
 	}
+
+	gf_isom_set_meta_qt(mp4);
 
 	if (!video_tk) {
 		GF_LOG(GF_LOG_DEBUG, GF_LOG_MEDIA, ("[QTFF] No visual track\n"));

commit d2f75dd7b930beafdbb2494edb9cc5e6f3a87f01

fixed meta dump bug introduced by c4aa2456

include/gpac/internal/isomedia_dev.h
@@ -2508,7 +2508,7 @@ typedef struct __tag_meta_box
 	GF_ItemReferenceBox *item_refs;
 	GF_GroupListBox *groups_list;
 
-	u8 use_item_sample_sharing, use_item_item_sharing, write_qt;
+	u8 use_item_sample_sharing, use_item_item_sharing, is_qt, write_qt;
 } GF_MetaBox;
 
 typedef struct
@@ -4660,6 +4660,7 @@ GF_Box *gf_isom_create_meta_extensions(GF_ISOFile *mov, Bool for_xtra);
 #ifndef GPAC_DISABLE_ISOM_DUMP
 GF_Err gf_isom_box_dump_ex(void *ptr, FILE * trace, u32 box_4cc);
 GF_Err gf_isom_box_dump_start(GF_Box *a, const char *name, FILE * trace);
+GF_Err gf_isom_box_dump_start_ex(GF_Box *a, const char *name, FILE * trace, Bool force_version);
 void gf_isom_box_dump_done(const char *name, GF_Box *ptr, FILE *trace);
 Bool gf_isom_box_is_file_level(GF_Box *s);
 #endif

src/isomedia/box_code_meta.c
@@ -131,6 +131,7 @@ GF_Err meta_box_read(GF_Box *s, GF_BitStream *bs)
 		gf_bs_seek(bs, pos);
 		meta_reset(s);
 		s->size = size+4;
+		ptr->is_qt = GF_TRUE;
 		e = gf_isom_box_array_read(s, bs);
 	}
 	return e;

src/isomedia/box_dump.c
@@ -2132,7 +2132,8 @@ GF_Err twrp_box_dump(GF_Box *a, FILE * trace)
 
 GF_Err meta_box_dump(GF_Box *a, FILE * trace)
 {
-	gf_isom_box_dump_start(a, "MetaBox", trace);
+	GF_MetaBox *ptr = (GF_MetaBox *)a;
+	gf_isom_box_dump_start_ex(a, "MetaBox", trace, ptr->is_qt ? GF_FALSE : GF_TRUE);
 	gf_fprintf(trace, ">\n");
 	gf_isom_box_dump_done("MetaBox", a, trace);
 	return GF_OK;

src/isomedia/box_funcs.c
@@ -2053,7 +2053,7 @@ u32 gf_isom_get_supported_box_type(u32 idx)
 
 #ifndef GPAC_DISABLE_ISOM_DUMP
 
-GF_Err gf_isom_box_dump_start(GF_Box *a, const char *name, FILE * trace)
+GF_Err gf_isom_box_dump_start_ex(GF_Box *a, const char *name, FILE * trace, Bool force_version)
 {
 	gf_fprintf(trace, "<%s ", name);
 	if (a->size > 0xFFFFFFFF) {
@@ -2077,7 +2077,7 @@ GF_Err gf_isom_box_dump_start(GF_Box *a, const char *name, FILE * trace)
 		gf_fprintf(trace, "}\" ");
 	}
 
-	if (a->registry->max_version_plus_one) {
+	if (a->registry->max_version_plus_one || force_version) {
 		gf_fprintf(trace, "Version=\"%d\" Flags=\"%d\" ", ((GF_FullBox*)a)->version,((GF_FullBox*)a)->flags);
 	}
 	gf_fprintf(trace, "Specification=\"%s\" ", a->registry->spec);
@@ -2087,6 +2087,10 @@ GF_Err gf_isom_box_dump_start(GF_Box *a, const char *name, FILE * trace)
 		gf_fprintf(trace, "Container=\"%s\" ", a->registry->parents_4cc);
 	}
 	return GF_OK;
+}
+GF_Err gf_isom_box_dump_start(GF_Box *a, const char *name, FILE * trace)
+{
+	return gf_isom_box_dump_start_ex(a, name, trace, GF_FALSE);
 }
 
 GF_Err gf_isom_box_dump(void *ptr, FILE * trace)