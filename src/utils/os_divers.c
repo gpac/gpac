60380d22b3e143f91c07a8edb3740d94acc89c4a
jeanlf <jeanlf@gpac.io>
2022-07-19 19:32:16 +0200

    fixed compil for 85247cf

--- src/utils/os_divers.c
+++ src/utils/os_divers.c
@@ -2694,10 +2694,10 @@ u64 gf_net_parse_date(const char *val)
 	else if (sscanf(val, "%3s %3s %d %02d:%02d:%02d %d", szDay, szMonth, &day, &year, &h, &m, &s)==7) {
 		secs  = (Float) s;
 	}
-	else if (sscanf(val, LLU, &current_time) == 1 && current_time > 1000000000 && current_time < INT32_MAX) {
+	else if (sscanf(val, LLU, &current_time) == 1 && current_time > 1000000000 && current_time < GF_INT_MAX) {
 		return current_time * 1000; // guessed raw duration since UTC0 in seconds
 	}
-	else if (sscanf(val, LLU, &current_time) == 1 && current_time > 1000000000000ULL && current_time < INT32_MAX * 1000ULL) {
+	else if (sscanf(val, LLU, &current_time) == 1 && current_time > 1000000000000ULL && current_time < GF_INT_MAX * 1000ULL) {
 		return current_time; // guessed duration since UTC0 in milliseconds
 	} else {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CORE, ("[Core] Cannot parse date string %s\n", val));
