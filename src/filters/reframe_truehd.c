gpac/gpac

commit c68d8a53ec84af125fb51e4fe58d4d09254a58d6

fixed TrueHD in m2ts and cleanup of eac3 parsing - cf #2217

src/filters/reframe_truehd.c
@@ -51,11 +51,15 @@ typedef struct
 {
 	//filter args
 	Double index;
+	Bool auxac3;
 
-	//only one input pid declared
+	//only one input pid
 	GF_FilterPid *ipid;
-	//only one output pid declared
+	//truehd output pid
 	GF_FilterPid *opid;
+	//aux AC3 output pid
+	GF_FilterPid *aux_opid;
+	GF_Filter *filter;
 
 	GF_BitStream *bs;
 	u64 file_pos, cts;
@@ -73,11 +77,16 @@ typedef struct
 	Bool is_file, file_loaded;
 	Bool initial_play_done;
 
+	//ref src pck
 	GF_FilterPacket *src_pck;
+	//current src, not ref
+	GF_FilterPacket *src_current;
 
 	TrueHDIdx *indexes;
 	u32 index_alloc_size, index_size;
 	Bool copy_props;
+
+	GF_AC3Header ac3_hdr;
 } GF_TrueHDDmxCtx;
 
 
@@ -99,6 +108,7 @@ GF_Err truehd_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remove
 	if (! gf_filter_pid_check_caps(pid))
 		return GF_NOT_SUPPORTED;
 
+	ctx->filter = filter;
 	ctx->ipid = pid;
 	p = gf_filter_pid_get_property(pid, GF_PROP_PID_TIMESCALE);
 	if (p) ctx->timescale = p->value.uint;
@@ -112,15 +122,75 @@ GF_Err truehd_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remove
 	return GF_OK;
 }
 
-static GF_Err truehd_parse_frame(GF_BitStream *bs, TrueHDHdr *hdr)
+static void truehd_aux_ac3(GF_TrueHDDmxCtx *ctx, GF_BitStream *bs, GF_AC3Header *hdr)
+{
+	u8 *data;
+	u32 size;
+
+	if (!ctx->aux_opid) {
+		ctx->aux_opid = gf_filter_pid_new(ctx->filter);
+		if (!ctx->aux_opid) return;
+		gf_filter_pid_set_name(ctx->aux_opid, "aux_ac3");
+	}
+
+	if (memcmp(&ctx->ac3_hdr, hdr, sizeof(GF_AC3Header))) {
+		memcpy(&ctx->ac3_hdr, hdr, sizeof(GF_AC3Header));
+
+		//copy properties from output pid
+		gf_filter_pid_copy_properties(ctx->aux_opid, ctx->opid);
+		//don't change codec type if reframing an ES (for HLS SAES)
+		gf_filter_pid_set_property(ctx->aux_opid, GF_PROP_PID_STREAM_TYPE, & PROP_UINT( GF_STREAM_AUDIO));
+		gf_filter_pid_set_property(ctx->aux_opid, GF_PROP_PID_CODECID, & PROP_UINT( GF_CODECID_AC3));
+		gf_filter_pid_set_property(ctx->aux_opid, GF_PROP_PID_UNFRAMED, & PROP_BOOL(GF_FALSE) );
+
+		if (ctx->duration.num)
+			gf_filter_pid_set_property(ctx->aux_opid, GF_PROP_PID_DURATION, & PROP_FRAC64(ctx->duration));
+
+		gf_filter_pid_set_property(ctx->aux_opid, GF_PROP_PID_TIMESCALE, & PROP_UINT(ctx->timescale ? ctx->timescale : hdr->sample_rate));
+		gf_filter_pid_set_property(ctx->aux_opid, GF_PROP_PID_SAMPLE_RATE, & PROP_UINT(hdr->sample_rate));
+		gf_filter_pid_set_property(ctx->aux_opid, GF_PROP_PID_NUM_CHANNELS, & PROP_UINT(hdr->streams[0].channels) );
+
+		gf_odf_ac3_cfg_write(hdr, &data, &size);
+		gf_filter_pid_set_property(ctx->aux_opid, GF_PROP_PID_DECODER_CONFIG, & PROP_DATA_NO_COPY(data, size) );
+	}
+
+	GF_FilterPacket *dst_pck = gf_filter_pck_new_alloc(ctx->aux_opid, hdr->framesize, &data);
+	gf_bs_read_data(bs, data, hdr->framesize);
+	gf_filter_pck_merge_properties(ctx->src_pck ? ctx->src_pck : ctx->src_current, dst_pck);
+	gf_filter_pck_set_byte_offset(dst_pck, GF_FILTER_NO_BO);
+
+	gf_filter_pck_set_dts(dst_pck, ctx->cts);
+	gf_filter_pck_set_cts(dst_pck, ctx->cts);
+	gf_filter_pck_set_sap(dst_pck, GF_FILTER_SAP_1);
+	gf_filter_pck_set_framing(dst_pck, GF_TRUE, GF_TRUE);
+	gf_filter_pck_send(dst_pck);
+}
+
+static GF_Err truehd_parse_frame(GF_TrueHDDmxCtx *ctx, GF_BitStream *bs, TrueHDHdr *hdr)
 {
 	memset(hdr, 0, sizeof(TrueHDHdr));
 
 	u32 avail = (u32) gf_bs_available(bs);
+	hdr->frame_size = 0;
 	//we need 8 bytes for base header (up to sync marker)
-	if (avail<8) {
-		hdr->frame_size = 0;
+	if (avail<8)
 		return GF_OK;
+
+	u32 sync = gf_bs_peek_bits(bs, 16, 0);
+	if (sync==0x0B77) {
+		GF_AC3Header ac3hdr;
+		if (!gf_ac3_parser_bs(bs, &ac3hdr, GF_TRUE))
+			return GF_OK;
+		//make sure we have the complete frame + 8 byte of TrueHD header
+		if (gf_bs_available(bs) < ac3hdr.framesize + 8)
+			return GF_BUFFER_TOO_SMALL;
+
+		if (ctx && ctx->auxac3) {
+			truehd_aux_ac3(ctx, bs, &ac3hdr);
+		} else {
+			gf_bs_skip_bytes(bs, ac3hdr.framesize);
+		}
+		avail = (u32) gf_bs_available(bs);
 	}
 
 	/*u8 nibble = */gf_bs_read_int(bs, 4);
@@ -226,7 +296,7 @@ static void truehd_check_dur(GF_Filter *filter, GF_TrueHDDmxCtx *ctx)
 	while (	gf_bs_available(bs) > 8 ) {
 		TrueHDHdr hdr;
 		u64 pos = gf_bs_get_position(bs);
-		GF_Err e = truehd_parse_frame(bs, &hdr);
+		GF_Err e = truehd_parse_frame(NULL, bs, &hdr);
 		if (e) break;
 
 		if (hdr.sync) {
@@ -458,6 +528,8 @@ GF_Err truehd_process(GF_Filter *filter)
 			if (!ctx->truehd_buffer_size) {
 				if (ctx->opid)
 					gf_filter_pid_set_eos(ctx->opid);
+				if (ctx->aux_opid)
+					gf_filter_pid_set_eos(ctx->aux_opid);
 				if (ctx->src_pck) gf_filter_pck_unref(ctx->src_pck);
 				ctx->src_pck = NULL;
 				return GF_EOS;
@@ -511,6 +583,7 @@ GF_Err truehd_process(GF_Filter *filter)
 
 	remain = ctx->truehd_buffer_size;
 	start = ctx->truehd_buffer;
+	ctx->src_current = pck;
 
 	if (ctx->resume_from) {
 		start += ctx->resume_from - 1;
@@ -529,7 +602,11 @@ GF_Err truehd_process(GF_Filter *filter)
 		u32 frame_start, bytes_to_drop=0;
 
 		frame_start = (u32) gf_bs_get_position(ctx->bs);
-		e = truehd_parse_frame(ctx->bs, &hdr);
+		e = truehd_parse_frame(ctx, ctx->bs, &hdr);
+		if (e==GF_BUFFER_TOO_SMALL) {
+			e = GF_OK;
+			break;
+		}
 		if (e) {
 			remain = 0;
 			break;
@@ -568,11 +645,16 @@ GF_Err truehd_process(GF_Filter *filter)
 				ctx->cts = cts;
 			cts = GF_FILTER_NO_TS;
 		}
+		if (!ctx->sample_rate) {
+			GF_LOG(GF_LOG_WARNING, GF_LOG_MEDIA, ("[TrueHDDmx] Invalid TrueHD frame, no sample rate found\n"));
+			remain = 0;
+			break;
+		}
 
 		if (!ctx->in_seek) {
 			dst_pck = gf_filter_pck_new_alloc(ctx->opid, hdr.frame_size, &output);
 			if (!dst_pck) return GF_OUT_OF_MEM;
-			if (ctx->src_pck) gf_filter_pck_merge_properties(ctx->src_pck, dst_pck);
+			gf_filter_pck_merge_properties(ctx->src_pck ? ctx->src_pck : ctx->src_current, dst_pck);
 
 			memcpy(output, frame, hdr.frame_size);
 			gf_filter_pck_set_dts(dst_pck, ctx->cts);
@@ -583,6 +665,8 @@ GF_Err truehd_process(GF_Filter *filter)
 
 			if (ctx->byte_offset != GF_FILTER_NO_BO) {
 				gf_filter_pck_set_byte_offset(dst_pck, ctx->byte_offset + hdr.frame_size);
+			} else {
+				gf_filter_pck_set_byte_offset(dst_pck, GF_FILTER_NO_BO);
 			}
 
 			gf_filter_pck_send(dst_pck);
@@ -620,7 +704,7 @@ GF_Err truehd_process(GF_Filter *filter)
 		ctx->truehd_buffer_size = 0;
 		return truehd_process(filter);
 	} else {
-		if (remain) {
+		if (remain && (remain<ctx->truehd_buffer_size)) {
 			memmove(ctx->truehd_buffer, start, remain);
 		}
 		ctx->truehd_buffer_size = remain;
@@ -644,7 +728,7 @@ static const char *truehd_probe_data(const u8 *data, u32 size, GF_FilterProbeSco
 	while (gf_bs_available(bs) > 8 ) {
 		TrueHDHdr hdr;
 		u64 pos = gf_bs_get_position(bs);
-		GF_Err e = truehd_parse_frame(bs, &hdr);
+		GF_Err e = truehd_parse_frame(NULL, bs, &hdr);
 		if (e || !hdr.frame_size) {
 			nb_frames = 0;
 			break;
@@ -678,6 +762,7 @@ static const GF_FilterCapability TrueHDDmxCaps[] =
 static const GF_FilterArgs TrueHDDmxArgs[] =
 {
 	{ OFFS(index), "indexing window length", GF_PROP_DOUBLE, "1.0", NULL, 0},
+	{ OFFS(auxac3), "expose auxiliary AC-3 stream if present", GF_PROP_BOOL, "false", NULL, 0},
 	{0}
 };
 

applications/mp4box/filedump.c
@@ -3347,12 +3347,11 @@ void DumpTrackInfo(GF_ISOFile *file, GF_ISOTrackID trackID, Bool full_dump, Bool
 		GF_AC3Config *ac3 = gf_isom_ac3_config_get(file, trackNum, 1);
 		if (ac3) {
 			nb_ch = gf_ac3_get_channels(ac3->streams[0].acmod);
-			for (i=0; i<ac3->streams[0].nb_dep_sub; ++i) {
-				assert(ac3->streams[0].nb_dep_sub == 1);
-				nb_ch += gf_ac3_get_channels(ac3->streams[0].chan_loc);
+			if (ac3->streams[0].nb_dep_sub) {
+				nb_ch += gf_eac3_get_chan_loc_count(ac3->streams[0].chan_loc);
 			}
 			if (ac3->streams[0].lfon) lfe = ".1";
-			br = ac3->is_ec3 ? ac3->brcode : gf_ac3_get_bitrate(ac3->brcode);
+			br = ac3->is_ec3 ? (ac3->brcode*1000) : gf_ac3_get_bitrate(ac3->brcode);
 			is_ec3 = ac3->is_ec3;
 			if (is_ec3) {
 				atmos = ac3->atmos_ec3_ext;

include/gpac/avparse.h
@@ -678,6 +678,12 @@ Bool gf_eac3_parser(u8 *buffer, u32 buffer_size, u32 *pos, GF_AC3Config *hdr, Bo
 */
 Bool gf_eac3_parser_bs(GF_BitStream *bs, GF_AC3Config *hdr, Bool full_parse);
 
+/*! gets the number of channels from chan_loc info of EAC3 config
+\param chan_loc acmod of the associated frame header
+\return number of channels
+*/
+u32 gf_eac3_get_chan_loc_count(u32 chan_loc);
+
 /*! gets the number of channels in an AC3 frame
 \param acmod acmod of the associated frame header
 \return number of channels
@@ -685,7 +691,7 @@ Bool gf_eac3_parser_bs(GF_BitStream *bs, GF_AC3Config *hdr, Bool full_parse);
 u32 gf_ac3_get_channels(u32 acmod);
 /*! gets the bitrate of an AC3 frame
 \param brcode brcode of the associated frame header
-\return bitrate
+\return bitrate in bps
 */
 u32 gf_ac3_get_bitrate(u32 brcode);
 

include/gpac/mpeg4_odf.h
@@ -1566,7 +1566,7 @@ GF_Err gf_odf_dovi_cfg_write_bs(GF_DOVIDecoderConfigurationRecord *cfg, GF_BitSt
 void gf_odf_dovi_cfg_del(GF_DOVIDecoderConfigurationRecord *cfg);
 
 
-/*! AC-3 config record extension for EAC-3 - see dolby specs*/
+/*! AC-3 and E-AC3 stream info */
 typedef struct __ec3_stream
 {
 	/*! AC3 fs code*/
@@ -1581,36 +1581,37 @@ typedef struct __ec3_stream
 	u8 lfon;
 	/*! asvc mode, only for EC3*/
 	u8 asvc;
-	/*! deps, only for EC3*/
+	/*! number of channels, including lfe*/
+	u8 channels;
+	/*! number of dependent substreams, only for EC3*/
 	u8 nb_dep_sub;
-	/*! channel loc, only for EC3*/
-	u8 chan_loc;
+	/*! channel locations for dependent substreams, only for EC3*/
+	u16 chan_loc;
 } GF_AC3StreamInfo;
 
-/*! AC3 config record*/
+/*! AC3 config record  - see dolby specs ETSI TS 102 366 */
 typedef struct __ac3_config
 {
-	/*! indicates if ec3*/
-	u8 is_ec3;
-	/*! if AC3 this is the bitrate code, otherwise cumulated data rate of EC3 streams*/
-	u16 brcode;
-	/*! number of streams :
+	/*! streams info - for AC3, always the first*/
+	GF_AC3StreamInfo streams[8];
+	/*! number of independent streams :
 		1 for AC3
-		max 8 for EC3, main stream is included
+		max 8 for EC3
 	*/
 	u8 nb_streams;
-	/*! streams info*/
-	GF_AC3StreamInfo streams[8];
-
-	//! \cond  used in parsing not part of the AC3 config record
-	u32 bitrate;
+	/*! indicates if frame is ec3*/
+	u8 is_ec3;
+	/*! if AC3 this is the bitrate code
+	,	 otherwise cumulated data rate of EAC3 streams in kbps
+	*/
+	u16 brcode;
+	/*! sample rate - all additional streams shall have the same sample rate as first independent stream in EC3*/
 	u32 sample_rate;
+	/*! size of the complete frame*/
 	u32 framesize;
-	u32 channels;
-	u16 substreams; //bit-mask, used for channel map > 5.1
-
-	//! \endcond private
-	Bool atmos_ec3_ext;
+	/*! atmos EC3 flag*/
+	u8 atmos_ec3_ext;
+	/*! atmos complexity index*/
 	u8 complexity_index_type;
 } GF_AC3Config;
 

include/gpac/version.h
@@ -48,7 +48,7 @@
 /*! ABI Major number of libgpac */
 #define GPAC_VERSION_MAJOR 12
 /*! ABI Minor number of libgpac */
-#define GPAC_VERSION_MINOR 1
+#define GPAC_VERSION_MINOR 2
 /*! ABI Micro number of libgpac */
 #define GPAC_VERSION_MICRO 0
 

share/doc/man/gpac-filters.1
@@ -8256,6 +8256,8 @@ This filter parses Dolby TrueHD files/data and outputs corresponding audio PID a
 .br
 index (dbl, default: 1.0):     indexing window length
 .br
+auxac3 (bool, default: false): expose auxiliary AC-3 stream if present
+.br
 
 .br
 .SH cryptin
@@ -10629,7 +10631,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.1-DEV-rev293-gd05a666a6-master
+build: 2.1-DEV-rev318-g147511483-master
 .br
 Copyright: (c) 2000-2022 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -4714,7 +4714,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.1-DEV-rev293-gd05a666a6-master
+build: 2.1-DEV-rev318-g147511483-master
 .br
 Copyright: (c) 2000-2022 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2870,7 +2870,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.1-DEV-rev293-gd05a666a6-master
+build: 2.1-DEV-rev318-g147511483-master
 .br
 Copyright: (c) 2000-2022 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

commit 9400cfb05645fadf8297e988a218647d71050af3

fixed bug introduced by c68d8a53

src/filters/reframe_truehd.c
@@ -659,8 +659,7 @@ GF_Err truehd_process(GF_Filter *filter)
 			memcpy(output, frame, hdr.frame_size);
 			gf_filter_pck_set_dts(dst_pck, ctx->cts);
 			gf_filter_pck_set_cts(dst_pck, ctx->cts);
-			if (hdr.sync)
-				gf_filter_pck_set_sap(dst_pck, GF_FILTER_SAP_1);
+			gf_filter_pck_set_sap(dst_pck, hdr.sync ? GF_FILTER_SAP_1 : GF_FILTER_SAP_NONE);
 			gf_filter_pck_set_framing(dst_pck, GF_TRUE, GF_TRUE);
 
 			if (ctx->byte_offset != GF_FILTER_NO_BO) {