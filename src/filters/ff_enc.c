gpac/gpac

commit 3f16d2f23214f08087bc4e6d36329b5c38c9a396

allow argument updates for ffenc ffdec ffdmx ffmx and ffbsf

src/filters/ff_enc.c
@@ -39,13 +39,12 @@ typedef struct _gf_ffenc_ctx
 	//opts
 	Bool all_intra;
 	char *c;
-	Bool ls;
+	Bool ls, rld;
 	u32 pfmt;
 	GF_Fraction fintra;
 	Bool rc;
 
 	//internal data
-	Bool initialized;
 	Bool gen_dsi;
 
 	u32 gop_size;
@@ -138,6 +137,7 @@ typedef struct _gf_ffenc_ctx
 #endif
 
 	u32 premul_timescale;
+	Bool args_updated;
 
 	FILE *logfile_pass1;
 } GF_FFEncodeCtx;
@@ -174,7 +174,6 @@ static GF_Err ffenc_initialize(GF_Filter *filter)
 {
 	u32 codec_id;
 	GF_FFEncodeCtx *ctx = (GF_FFEncodeCtx *) gf_filter_get_udta(filter);
-	ctx->initialized = GF_TRUE;
 	ctx->src_packets = gf_list_new();
 	ctx->sdbs = gf_bs_new((u8*)ctx, 1, GF_BITSTREAM_READ);
 
@@ -493,6 +492,10 @@ static GF_Err ffenc_process_video(GF_Filter *filter, struct _gf_ffenc_ctx *ctx)
 	}
 
 	if (force_intra) {
+		if (ctx->args_updated) {
+			force_intra = 2;
+			ctx->args_updated = GF_FALSE;
+		}
 		//file switch we force a full reset to force injecting xPS in the stream
 		//we could also inject them manually but we don't have them !!
 		if ((ctx->rc && ctx->nb_frames_in) || (force_intra==2)) {
@@ -1947,6 +1950,8 @@ static GF_Err ffenc_update_arg(GF_Filter *filter, const char *arg_name, const GF
 	Bool do_override = GF_FALSE;
 	GF_FFEncodeCtx *ctx = gf_filter_get_udta(filter);
 
+	if (!strcmp(arg_name, "rld")) return GF_OK;
+
 	if (!strcmp(arg_name, "global_header"))	return GF_OK;
 	else if (!strcmp(arg_name, "local_header"))	return GF_OK;
 	else if (!strcmp(arg_name, "low_delay"))	ctx->low_delay = GF_TRUE;
@@ -1974,7 +1979,7 @@ static GF_Err ffenc_update_arg(GF_Filter *filter, const char *arg_name, const GF
 	}
 
 	//initial parsing of arguments
-	if (!ctx->initialized) {
+	if (!ctx->encoder) {
 		const char *arg_val_str;
 		switch (arg_val->type) {
 		case GF_PROP_STRING:
@@ -1995,8 +2000,45 @@ static GF_Err ffenc_update_arg(GF_Filter *filter, const char *arg_name, const GF
 		}
 		return GF_OK;
 	}
-	//updates of arguments, not supported for ffmpeg decoders
-	return GF_NOT_SUPPORTED;
+	//updates of arguments
+	u64 value=0;
+	switch (arg_val->type) {
+	case GF_PROP_STRING:
+	{
+		GF_PropertyValue res = gf_props_parse_value(GF_PROP_LUINT, arg_name, arg_val->value.string, NULL, 0);
+		value = res.value.longuint;
+	}
+		break;
+	case GF_PROP_UINT: value = arg_val->value.uint; break;
+	case GF_PROP_LUINT: value = arg_val->value.longuint; break;
+	default:
+		return GF_NOT_SUPPORTED;
+	}
+
+	if (ctx->rld) {
+		Bool skip=GF_FALSE;
+		char szVal[100];
+		sprintf(szVal, LLU, value);
+		AVDictionaryEntry *key = av_dict_get(ctx->options, arg_name, NULL, 0);
+		if (key) {
+			if ((arg_val->type==GF_PROP_STRING) && !strcmp(key->value, arg_val->value.string))
+				skip=GF_TRUE;
+			else if (!strcmp(key->value, szVal))
+				skip=GF_TRUE;
+		}
+		if (!skip) {
+			av_dict_set(&ctx->options, arg_name, szVal, 0);
+			//if video and fintra is set, reload args at next intra
+			if (ctx->width && (ctx->fintra.num>=0)) {
+				ctx->args_updated = GF_TRUE;
+			} else {
+				ctx->reconfig_pending = GF_TRUE;
+				ctx->force_reconfig = GF_TRUE;
+			}
+		}
+		return GF_OK;
+	}
+	return ffmpeg_update_arg(ctx->encoder, arg_name, arg_val);
 }
 
 static Bool ffenc_process_event(GF_Filter *filter, const GF_FilterEvent *evt)
@@ -2053,6 +2095,10 @@ GF_FilterRegister FFEncodeRegister = {
 		"If [-fintra]() is not set and the output of the encoder is a DASH session in live profile without segment timeline, [-fintra]() will be set to the target segment duration and [-rc]() will be set.\n"
 		"\n"
 		"The filter will look for property `logpass` on input PID to set 2-pass log filename, otherwise defaults to `ffenc2pass-PID.log`.\n"
+		"\n"
+		"Arguments may be updated at runtime. If [-rld]() is set, the encoder will be flushed then reloaded with new options.\n"
+		"If codec is video and [-fintra]() is set, reload will happen at next forced intra; otherwise, reload happens at next encode.\n"
+		"The [-rld]() option is usually needed for dynamic updates of rate control parameters, since most encoders in ffmpeg do not support it.\n"
 	)
 	.private_size = sizeof(GF_FFEncodeCtx),
 	SETCAPS(FFEncodeCaps),
@@ -2075,6 +2121,7 @@ static const GF_FilterArgs FFEncodeArgs[] =
 	{ OFFS(all_intra), "only produce intra frames", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_UPDATE|GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(ls), "log stats", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(rc), "reset encoder when forcing intra frame (some encoders might not support intra frame forcing)", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
+	{ OFFS(rld), "force reloading of encoder when arguments are updated", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_EXPERT|GF_FS_ARG_UPDATE},
 
 	{ "*", -1, "any possible options defined for AVCodecContext and sub-classes. see `gpac -hx ffenc` and `gpac -hx ffenc:*`", GF_PROP_STRING, NULL, NULL, GF_FS_ARG_META},
 	{0}

share/doc/man/gpac-filters.1
@@ -1485,6 +1485,8 @@ ofmt (pfmt, default: nv12):    set default pixel format for decoded video. If no
 .br
 disable_hw (bool, default: false): disable hardware decoding
 .br
+wait_sync (bool, default: false, updatable): wait for sync frame before decoding
+.br
 
 .br
 .SH mcdec
@@ -2935,9 +2937,11 @@ rtcp_timeout (uint, default: 5000): default timeout for RTCP traffic in ms. Afte
 .br
 autortsp (bool, default: true): automatically reconfig RTSP interleaving if UDP timeout
 .br
-first_packet_drop (uint, default: 0): set number of first RTP packet to drop (0 if no drop)
+first_packet_drop (uint, default: 0, updatable): set number of first RTP packet to drop (0 if no drop)
 .br
-frequency_drop (uint, default: 0): drop 1 out of N packet (0 disable dropping)
+frequency_drop (uint, default: 0, updatable): drop 1 out of N packet (0 disable dropping)
+.br
+loss_rate (sint, default: -1, updatable): loss rate to signal in RTCP, -1 means real loss rate, otherwise a per-thousand of packet lost
 .br
 user_agent (str, default: $GUA): user agent string, by default solved from GPAC preferences
 .br
@@ -6542,6 +6546,10 @@ When enabled, a GET on a directory will return a simple HTML listing of the cont
 .br
   
 .br
+Warning: the server is currently using blocking IOs for sockets and will likely not scale for many input clients.
+.br
+  
+.br
 .SH Simple HTTP server
 .LP
 .br
@@ -6768,7 +6776,7 @@ cert (str):                    certificate file in PEM format to use for TLS mod
 .br
 pkey (str):                    private key file in PEM format to use for TLS mode
 .br
-block_size (uint, default: 10000): block size used to read and write TCP socket
+block_size (uint, default: 100000): block size used to read and write TCP socket
 .br
 user_agent (str, default: $GUA): user agent string, by default solved from GPAC preferences
 .br
@@ -7796,6 +7804,14 @@ The filter will look for property logpass on input PID to set 2-pass log filenam
 .br
 
 .br
+Arguments may be updated at runtime. If .I rld is set, the encoder will be flushed then reloaded with new options.
+.br
+If codec is video and .I fintra is set, reload will happen at next forced intra; otherwise, reload happens at next encode.
+.br
+The .I rld option is usually needed for dynamic updates of rate control parameters, since most encoders in ffmpeg do not support it.
+.br
+
+.br
 .SH Options (expert):
 .LP
 .br
@@ -7810,6 +7826,8 @@ all_intra (bool, default: false, updatable): only produce intra frames
 ls (bool, default: false):     log stats
 .br
 rc (bool, default: false):     reset encoder when forcing intra frame (some encoders might not support intra frame forcing)
+.br
+rld (bool, default: false, updatable): force reloading of encoder when arguments are updated
 .br
 * (str):                       any possible options defined for AVCodecContext and sub-classes. see gpac -hx ffenc and gpac -hx ffenc:*
 .br
@@ -10765,7 +10783,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.1-DEV-rev367-g0fef0797e-master
+build: 2.1-DEV-rev372-ge17d36881-master
 .br
 Copyright: (c) 2000-2022 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -4723,7 +4723,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.1-DEV-rev367-g0fef0797e-master
+build: 2.1-DEV-rev372-ge17d36881-master
 .br
 Copyright: (c) 2000-2022 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2870,7 +2870,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.1-DEV-rev367-g0fef0797e-master
+build: 2.1-DEV-rev372-ge17d36881-master
 .br
 Copyright: (c) 2000-2022 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

src/filters/ff_bsf.c
@@ -380,17 +380,20 @@ static GF_Err ffbsf_update_arg(GF_Filter *filter, const char *arg_name, const GF
 	char *arg_value;
 	GF_FFBSFCtx *ctx = gf_filter_get_udta(filter);
 
-	arg_value = NULL;
-	if  (arg_val->type == GF_PROP_STRING) {
-		arg_value = arg_val->value.string;
-		if (!arg_value) arg_value = "1";
-	}
+	if (!ctx->bsfc) {
+		arg_value = NULL;
+		if  (arg_val->type == GF_PROP_STRING) {
+			arg_value = arg_val->value.string;
+			if (!arg_value) arg_value = "1";
+		}
 
-	ret = av_dict_set(&ctx->options, arg_name, arg_value, 0);
-	if (ret<0) {
-		GF_LOG(GF_LOG_ERROR, GF_LOG_MEDIA, ("[FFBSF] Failed to set option %s:%s\n", arg_name, arg_val ));
+		ret = av_dict_set(&ctx->options, arg_name, arg_value, 0);
+		if (ret<0) {
+			GF_LOG(GF_LOG_ERROR, GF_LOG_MEDIA, ("[FFBSF] Failed to set option %s:%s\n", arg_name, arg_val ));
+		}
+		return GF_OK;
 	}
-	return GF_OK;
+	return ffmpeg_update_arg(ctx->bsfc, arg_name, arg_val);
 }
 
 static Bool ffbsf_process_event(GF_Filter *filter, const GF_FilterEvent *evt)

src/filters/ff_common.c
@@ -724,6 +724,9 @@ GF_FilterArgs ffmpeg_arg_translate(const struct AVOption *opt)
 	if (opt->name[0] == 0)
 		arg.flags = GF_FS_ARG_META;
 
+	if (!(opt->flags & AV_OPT_FLAG_READONLY))
+		arg.flags |= GF_FS_ARG_UPDATE;
+
 	switch (opt->type) {
 	case AV_OPT_TYPE_INT64:
 	case AV_OPT_TYPE_INT:
@@ -2059,6 +2062,20 @@ GF_Err ffmpeg_codec_par_to_gpac(AVCodecParameters *codecpar, GF_FilterPid *opid,
 		if (codecpar->bits_per_raw_sample)
 			gf_filter_pid_set_property(opid, GF_PROP_PID_AUDIO_BPS, &PROP_UINT(codecpar->bits_per_raw_sample));
 	}
+	return GF_OK;
+}
+
+
+GF_Err ffmpeg_update_arg(void *ctx, const char *arg_name, const GF_PropertyValue *arg_val)
+{
+	char dumpbuf[GF_PROP_DUMP_ARG_SIZE];
+	int ret;
+	const char *val = gf_props_dump_val(arg_val, dumpbuf, GF_PROP_DUMP_DATA_NONE, NULL);
+
+	if (!val && (arg_val->type == GF_PROP_STRING)) val = "1";
+
+	ret = av_opt_set(ctx, arg_name, val, AV_OPT_SEARCH_CHILDREN);
+	if (ret<0) return GF_NOT_SUPPORTED;
 	return GF_OK;
 }
 #endif

src/filters/ff_common.h
@@ -125,3 +125,5 @@ GF_Err ffmpeg_codec_par_from_gpac(GF_FilterPid *pid, AVCodecParameters *codecpar
 codec_id, codec_type, and extradata are NOT set by this function
 */
 GF_Err ffmpeg_codec_par_to_gpac(AVCodecParameters *codecpar, GF_FilterPid *opid, u32 ffmpeg_timescale);
+
+GF_Err ffmpeg_update_arg(void *avclass_ctx, const char *arg_name, const GF_PropertyValue *arg_val);

src/filters/ff_dec.c
@@ -54,9 +54,6 @@ typedef struct _gf_ffdec_ctx
 {
 	GF_PropStringList ffcmap;
 
-	//internal data
-	Bool initialized;
-
 	Bool owns_context;
 	AVCodecContext *decoder;
 	//decode options
@@ -116,7 +113,6 @@ typedef struct _gf_ffdec_ctx
 static GF_Err ffdec_initialize(GF_Filter *filter)
 {
 	GF_FFDecodeCtx *ctx = (GF_FFDecodeCtx *) gf_filter_get_udta(filter);
-	ctx->initialized = GF_TRUE;
 	ctx->src_packets = gf_list_new();
 	ctx->sar.den = 1;
 
@@ -1346,7 +1342,7 @@ static GF_Err ffdec_update_arg(GF_Filter *filter, const char *arg_name, const GF
 	GF_FFDecodeCtx *ctx = gf_filter_get_udta(filter);
 
 	//initial parsing of arguments
-	if (!ctx->initialized) {
+	if (!ctx->decoder) {
 		switch (arg_val->type) {
 		case GF_PROP_STRING:
 			res = av_dict_set(&ctx->options, arg_name, arg_val->value.string, 0);
@@ -1360,8 +1356,7 @@ static GF_Err ffdec_update_arg(GF_Filter *filter, const char *arg_name, const GF
 		}
 		return GF_OK;
 	}
-	//updates of arguments, not supported for ffmpeg decoders
-	return GF_NOT_SUPPORTED;
+	return ffmpeg_update_arg(ctx->decoder, arg_name, arg_val);
 }
 
 static Bool ffdec_process_event(GF_Filter *filter, const GF_FilterEvent *evt)

commit 06429f133cd001b6ef376fd2cb15ac8c543401b4

fixed bug introduced by 3f16d2f2

src/filters/ff_enc.c
@@ -1946,7 +1946,7 @@ static GF_Err ffenc_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_
 static GF_Err ffenc_update_arg(GF_Filter *filter, const char *arg_name, const GF_PropertyValue *arg_val)
 {
 	char szOverrideOpt[1000];
-	Bool do_override = GF_FALSE;
+	GF_PropertyValue arg_val_override;
 	GF_FFEncodeCtx *ctx = gf_filter_get_udta(filter);
 
 	if (!strcmp(arg_name, "rld")) return GF_OK;
@@ -1974,7 +1974,9 @@ static GF_Err ffenc_update_arg(GF_Filter *filter, const char *arg_name, const GF
 			ctx->target_rate *= 1000;
 
 		sprintf(szOverrideOpt, "%d", ctx->target_rate);
-		do_override = GF_TRUE;
+		arg_val_override.type = GF_PROP_STRING;
+		arg_val_override.value.string = szOverrideOpt;
+		arg_val = &arg_val_override;
 	}
 
 	//initial parsing of arguments