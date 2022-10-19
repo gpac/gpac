gpac/gpac

commit 7ea9ab665e113eceb9de0543d311aae898d1cc86

do not update track/media duration when adding samples or seting last sample duration

src/isomedia/isom_write.c
@@ -1033,7 +1033,8 @@ GF_Err gf_isom_add_sample(GF_ISOFile *movie, u32 trackNumber, u32 StreamDescript
 
 	if (!movie->keep_utc)
 		trak->Media->mediaHeader->modificationTime = gf_isom_get_mp4time();
-	return SetTrackDuration(trak);
+	//do not update track duration yet, this is don one close
+	return GF_OK;
 }
 
 GF_EXPORT
@@ -1308,7 +1309,8 @@ static GF_Err gf_isom_set_last_sample_duration_internal(GF_ISOFile *movie, u32 t
 	if (!movie->keep_utc)
 		trak->Media->mediaHeader->modificationTime = gf_isom_get_mp4time();
 	trak->Media->mediaHeader->duration = mdur;
-	return SetTrackDuration(trak);
+	//do not update track duration yet, this is don one close
+	return GF_OK;
 }
 
 GF_EXPORT

applications/mp4box/fileimport.c
@@ -2914,6 +2914,8 @@ GF_Err cat_isomedia_file(GF_ISOFile *dest, char *fileName, u32 import_flags, GF_
 			gf_set_progress("Appending", nb_done, nb_samp);
 			nb_done++;
 		}
+		gf_isom_update_duration(dest);
+
 		/*scene description and text: compute last sample duration based on original media duration*/
 		if (!use_ts_dur) {
 			u64 extend_dur = gf_isom_get_media_duration(orig, i+1) - last_DTS;

include/gpac/isomedia.h
@@ -2044,7 +2044,7 @@ are of same sizes (typically in 3GP speech tracks)
 */
 GF_Err gf_isom_refresh_size_info(GF_ISOFile *isom_file, u32 trackNumber);
 
-/*! updates the duration of the movie
+/*! updates the duration of the movie.This is done automatically when storing the file or editing timesales/edit list, but it is not done when adding samples.
 \param isom_file the target ISO file
 \return error if any
 */

src/isomedia/isom_read.c
@@ -561,10 +561,21 @@ GF_Err gf_isom_get_bs(GF_ISOFile *movie, GF_BitStream **out_bs)
 
 
 GF_EXPORT
-GF_Err gf_isom_write(GF_ISOFile *movie) {
+GF_Err gf_isom_write(GF_ISOFile *movie)
+{
 	GF_Err e;
 	if (movie == NULL) return GF_ISOM_INVALID_FILE;
 	e = GF_OK;
+
+	//update duration of each track
+	if (movie->moov) {
+		u32 i, count = gf_list_count(movie->moov->trackList);
+		for (i=0; i<count; i++) {
+			GF_TrackBox *trak = gf_list_get(movie->moov->trackList, i);
+			e = SetTrackDuration(trak);
+			if (e) return e;
+		}
+	}
 
 #ifndef GPAC_DISABLE_ISOM_WRITE
 	//write our movie to the file

src/media_tools/media_import.c
@@ -282,6 +282,7 @@ static GF_Err gf_import_afx_sc3dmc(GF_MediaImporter *import, Bool mult_desc_allo
 	gf_set_progress("Importing SC3DMC", 1, 1);
 
 	gf_isom_sample_del(&samp);
+	gf_isom_update_duration(import->dest);
 
 exit:
 	gf_free(data);
@@ -741,6 +742,7 @@ static GF_Err gf_import_isomedia_track(GF_MediaImporter *import)
 			if (e) goto exit;
 		}
 	}
+	gf_isom_update_duration(import->dest);
 
 exit:
 	if (sai_buffer) gf_free(sai_buffer);

src/scene_manager/encode_isom.c
@@ -994,6 +994,7 @@ force_scene_rap:
 			esd = NULL;
 		}
 	}
+	gf_isom_update_duration(mp4);
 
 	/*to do - proper PL setup according to node used...*/
 	gf_isom_set_pl_indication(mp4, GF_ISOM_PL_SCENE, 1);
@@ -1368,6 +1369,8 @@ static GF_Err gf_sm_encode_od(GF_SceneManager *ctx, GF_ISOFile *mp4, char *media
 			rap_codec = NULL;
 		}
 	}
+	gf_isom_update_duration(mp4);
+
 	e = gf_isom_set_pl_indication(mp4, GF_ISOM_PL_OD, 1);
 
 err_exit:

commit f3b2aad8dd44d9a6cf8a95361b35471a3e9c488b

fixed media duration compute bug introduced by 7ea9ab66

src/isomedia/isom_write.c
@@ -1033,7 +1033,18 @@ GF_Err gf_isom_add_sample(GF_ISOFile *movie, u32 trackNumber, u32 StreamDescript
 
 	if (!movie->keep_utc)
 		trak->Media->mediaHeader->modificationTime = gf_isom_get_mp4time();
-	//do not update track duration yet, this is don one close
+
+	//update media duration
+	if (sample->DTS + sample->CTS_Offset>=0) {
+		GF_TimeToSampleBox *stts = trak->Media->information->sampleTable->TimeToSample;
+		u64 dur = sample->DTS + sample->CTS_Offset;
+		dur += stts->entries[stts->nb_entries-1].sampleDelta;
+
+		if (dur > trak->Media->mediaHeader->duration) {
+			trak->Media->mediaHeader->duration = dur;
+		}
+	}
+	//do not update track duration yet, this is done on close
 	return GF_OK;
 }
 
@@ -1308,8 +1319,11 @@ static GF_Err gf_isom_set_last_sample_duration_internal(GF_ISOFile *movie, u32 t
 	}
 	if (!movie->keep_utc)
 		trak->Media->mediaHeader->modificationTime = gf_isom_get_mp4time();
-	trak->Media->mediaHeader->duration = mdur;
-	//do not update track duration yet, this is don one close
+
+	//update media duration if duration was set
+	if (trak->Media->mediaHeader->duration)
+		trak->Media->mediaHeader->duration = mdur;
+	//do not update track duration yet, this is done on close
 	return GF_OK;
 }
 
@@ -4905,6 +4919,8 @@ GF_Err gf_isom_shift_cts_offset(GF_ISOFile *the_file, u32 trackNumber, s32 offse
 		/*we're in unpack mode: one entry per sample*/
 		trak->Media->information->sampleTable->CompositionOffset->entries[i].decodingOffset -= offset_shift;
 	}
+	if (trak->Media->mediaHeader->duration >= -offset_shift)
+		trak->Media->mediaHeader->duration -= offset_shift;
 	return GF_OK;
 }
 