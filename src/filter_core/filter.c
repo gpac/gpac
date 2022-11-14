7ff6fbc5cfcbaf6e32b9e94404f0fd8dbc011ed6
jeanlf <jeanlf@gpac.io>
2022-11-14 18:01:40 +0100

    fixed typo in 390a8324

--- src/filter_core/filter.c
+++ src/filter_core/filter.c
@@ -1482,6 +1482,7 @@ static void filter_parse_dyn_args(GF_Filter *filter, const char *args, GF_Filter
 							//if in the form scheme://FOO/:, don't inspect FOO. This allows escaping port nmber or IPv6 double colon
 							if (sep3 && ((sep3[1]==':') || (sep3[1]==0)) ) {
 								sep = sep3+1;
+								if (!sep[0]) sep = NULL;
 							} else if (sep2 || sep3 || sep) {
 								u32 port = 0;
 								if (sep2) {
