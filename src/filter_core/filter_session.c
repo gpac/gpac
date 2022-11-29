gpac/gpac

commit 772b233c520a78e51f4ab33c6766ec202c2121f2

Improvements for JSF - multiple windows support in user events, SDL and vout.js - allow computing filter chains without laoding them - allow querying all arguments on a registry, including meta arguments - expose codec being used in ffdec and ffenc as filter option

src/filter_core/filter_session.c
@@ -2942,7 +2942,7 @@ static GF_Filter *locate_alias_sink(GF_Filter *filter, const char *url, const ch
 
 Bool filter_solve_gdocs(const char *url, char szPath[GF_MAX_PATH]);
 
-GF_Filter *gf_fs_load_source_dest_internal(GF_FilterSession *fsess, const char *url, const char *user_args, const char *parent_url, GF_Err *err, GF_Filter *filter, GF_Filter *dst_filter, Bool for_source, Bool no_args_inherit, Bool *probe_only)
+GF_Filter *gf_fs_load_source_dest_internal(GF_FilterSession *fsess, const char *url, const char *user_args, const char *parent_url, GF_Err *err, GF_Filter *filter, GF_Filter *dst_filter, Bool for_source, Bool no_args_inherit, Bool *probe_only, const GF_FilterRegister **probe_reg)
 {
 	GF_FilterProbeScore score = GF_FPROBE_NOT_SUPPORTED;
 	GF_FilterRegister *candidate_freg=NULL;
@@ -3146,6 +3146,8 @@ restart:
 	}
 	if (probe_only) {
 		*probe_only = candidate_freg ? GF_TRUE : GF_FALSE;
+		if (probe_reg) *probe_reg = candidate_freg;
+
 		if (free_url)
 			gf_free(sURL);
 		if (err) *err = GF_OK;
@@ -3258,13 +3260,13 @@ restart:
 GF_EXPORT
 GF_Filter *gf_fs_load_source(GF_FilterSession *fsess, const char *url, const char *args, const char *parent_url, GF_Err *err)
 {
-	return gf_fs_load_source_dest_internal(fsess, url, args, parent_url, err, NULL, NULL, GF_TRUE, GF_FALSE, NULL);
+	return gf_fs_load_source_dest_internal(fsess, url, args, parent_url, err, NULL, NULL, GF_TRUE, GF_FALSE, NULL, NULL);
 }
 
 GF_EXPORT
 GF_Filter *gf_fs_load_destination(GF_FilterSession *fsess, const char *url, const char *args, const char *parent_url, GF_Err *err)
 {
-	return gf_fs_load_source_dest_internal(fsess, url, args, parent_url, err, NULL, NULL, GF_FALSE, GF_FALSE, NULL);
+	return gf_fs_load_source_dest_internal(fsess, url, args, parent_url, err, NULL, NULL, GF_FALSE, GF_FALSE, NULL, NULL);
 }
 
 
@@ -4206,7 +4208,7 @@ Bool gf_fs_is_supported_source(GF_FilterSession *session, const char *url, const
 {
 	GF_Err e;
 	Bool is_supported = GF_FALSE;
-	gf_fs_load_source_dest_internal(session, url, NULL, parent_url, &e, NULL, NULL, GF_TRUE, GF_TRUE, &is_supported);
+	gf_fs_load_source_dest_internal(session, url, NULL, parent_url, &e, NULL, NULL, GF_TRUE, GF_TRUE, &is_supported, NULL);
 	return is_supported;
 }
 

src/filters/ff_common.c
@@ -720,7 +720,7 @@ static void ffmpeg_register_free(GF_FilterSession *session, GF_FilterRegister *r
 		if (arg->flags & GF_FS_ARG_META_ALLOC) gf_free((void *) arg->arg_desc);
 	}
 	if (reg->args) gf_free((void *) reg->args);
-
+	gf_free(reg);
 }
 
 GF_FilterArgs ffmpeg_arg_translate(const struct AVOption *opt)
@@ -1412,7 +1412,7 @@ second_pass:
 #endif
 }
 
-void ffmpeg_build_register(GF_FilterSession *session, GF_FilterRegister *orig_reg, const GF_FilterArgs *default_args, u32 nb_def_args, u32 reg_type)
+GF_FilterRegister *ffmpeg_build_register(GF_FilterSession *session, GF_FilterRegister *orig_reg, const GF_FilterArgs *default_args, u32 nb_def_args, u32 reg_type)
 {
 	GF_FilterArgs *args;
 	u32 i=0, idx=0, nb_args;
@@ -1423,7 +1423,7 @@ void ffmpeg_build_register(GF_FilterSession *session, GF_FilterRegister *orig_re
 	AVCodecContext *codec_ctx = NULL;
 	const AVClass *av_class = NULL;
 	GF_FFRegistryExt *ffregext;
-
+	GF_FilterRegister *new_reg;
 	ffmpeg_initialize();
 
 #ifndef GPAC_DISABLE_DOC
@@ -1434,8 +1434,12 @@ void ffmpeg_build_register(GF_FilterSession *session, GF_FilterRegister *orig_re
 	if (!load_meta_filters) {
 		orig_reg->args = default_args;
 		orig_reg->register_free = NULL;
-		return;
+		return orig_reg;
 	}
+
+	GF_SAFEALLOC(new_reg, GF_FilterRegister);
+	memcpy(new_reg, orig_reg, sizeof(GF_FilterRegister));
+	orig_reg = new_reg;
 
 	if (reg_type==FF_REG_TYPE_ENCODE) opt_type = AV_OPT_FLAG_ENCODING_PARAM;
 	else if (reg_type==FF_REG_TYPE_MUX) opt_type = AV_OPT_FLAG_ENCODING_PARAM;
@@ -1580,15 +1584,15 @@ void ffmpeg_build_register(GF_FilterSession *session, GF_FilterRegister *orig_re
 		avformat_free_context(format_ctx);
 	}
 
-	GF_SAFEALLOC(ffregext, GF_FFRegistryExt);
-	if (!ffregext) return;
-	
-	orig_reg->udta = ffregext;
-	ffregext->nb_arg_skip = nb_def_args-1;
 	orig_reg->register_free = ffmpeg_register_free;
 
-	ffmpeg_expand_register(session, orig_reg, reg_type);
-
+	GF_SAFEALLOC(ffregext, GF_FFRegistryExt);
+	if (ffregext) {
+		orig_reg->udta = ffregext;
+		ffregext->nb_arg_skip = nb_def_args-1;
+		ffmpeg_expand_register(session, orig_reg, reg_type);
+	}
+	return orig_reg;
 }
 
 void ffmpeg_set_enc_dec_flags(const AVDictionary *options, AVCodecContext *ctx)

src/filters/ff_dmx.c
@@ -642,6 +642,12 @@ GF_Err ffdmx_init_common(GF_Filter *filter, GF_FFDemuxCtx *ctx, u32 grab_type)
 	u32 nb_a, nb_v, nb_t, clock_id;
 	char szName[50];
 
+
+	if (gf_filter_is_temporary(filter)) {
+		gf_filter_meta_set_instances(filter, ctx->demuxer->av_class->class_name);
+		return GF_OK;
+	}
+
 #if (LIBAVCODEC_VERSION_MAJOR >= 59)
 	ctx->pkt = av_packet_alloc();
 #endif
@@ -961,6 +967,7 @@ static int64_t ffavio_seek(void *opaque, int64_t offset, int whence)
 	return (int64_t) gf_fseek(ctx->gfio, offset, whence);
 }
 
+#include <libavutil/avstring.h>
 static GF_Err ffdmx_initialize(GF_Filter *filter)
 {
 	GF_FFDemuxCtx *ctx = gf_filter_get_udta(filter);
@@ -1040,6 +1047,27 @@ static GF_Err ffdmx_initialize(GF_Filter *filter)
 		e = GF_NOT_SUPPORTED;
 		break;
 	}
+
+	if (e && gf_filter_is_temporary(filter)) {
+		char *ext = strrchr(ctx->src, '.');
+		const AVInputFormat *ifmt = av_find_input_format(ctx->src);
+		if (!ifmt && ext) ifmt = av_find_input_format(ext+1);
+		if (!ifmt && ext) {
+			const AVInputFormat *fmt = NULL;
+			void *i = 0;
+			while ((fmt = av_demuxer_iterate(&i))) {
+				if (av_match_name(ext+1, fmt->extensions)) {
+					ifmt = fmt;
+					break;
+				}
+			}
+		}
+		if (ifmt) {
+			gf_filter_meta_set_instances(filter, ifmt->name);
+			return GF_OK;
+		}
+	}
+
 	if (e) {
 		GF_LOG(GF_LOG_ERROR, ctx->log_class, ("[%s] Fail to open %s - error %s\n", ctx->fname, ctx->src, av_err2str(res) ));
 		avformat_close_input(&ctx->demuxer);
@@ -1054,6 +1082,7 @@ static GF_Err ffdmx_initialize(GF_Filter *filter)
 		e = GF_NOT_SUPPORTED;
 		avformat_close_input(&ctx->demuxer);
 		avformat_free_context(ctx->demuxer);
+		ctx->demuxer = NULL;
 		if (options) av_dict_free(&options);
 		return e;
 	}
@@ -1229,8 +1258,7 @@ static const GF_FilterArgs FFDemuxArgs[] =
 
 const GF_FilterRegister *ffdmx_register(GF_FilterSession *session)
 {
-	ffmpeg_build_register(session, &FFDemuxRegister, FFDemuxArgs, 2, FF_REG_TYPE_DEMUX);
-	return &FFDemuxRegister;
+	return ffmpeg_build_register(session, &FFDemuxRegister, FFDemuxArgs, 2, FF_REG_TYPE_DEMUX);
 }
 
 
@@ -1659,13 +1687,13 @@ static void ffavin_log_none(void *avcl, int level, const char *fmt, va_list vl)
 
 const GF_FilterRegister *ffavin_register(GF_FilterSession *session)
 {
-	ffmpeg_build_register(session, &FFAVInRegister, FFAVInArgs, FFAVIN_STATIC_ARGS, FF_REG_TYPE_DEV_IN);
+	GF_FilterRegister *res_reg = ffmpeg_build_register(session, &FFAVInRegister, FFAVInArgs, FFAVIN_STATIC_ARGS, FF_REG_TYPE_DEV_IN);
 
 	if (gf_opts_get_bool("temp", "get_proto_schemes")) {
 		gf_opts_set_key("temp_in_proto", FFAVInRegister.name, "video,audio,av");
 	}
 	if (!gf_opts_get_bool("temp", "helponly") || gf_opts_get_bool("temp", "gendoc"))
-		return &FFAVInRegister;
+		return res_reg;
 	
 #ifdef FF_PROBE_DEVICES
 	Bool audio_pass=GF_FALSE;
@@ -1697,7 +1725,7 @@ const GF_FilterRegister *ffavin_register(GF_FilterSession *session)
 		ffmpeg_register_set_dyn_help(&FFAVInRegister);
 	}
 #endif
-	return &FFAVInRegister;
+	return res_reg;
 }
 
 #else

include/gpac/events.h
@@ -74,6 +74,8 @@ typedef struct
 	u32 button;
 	/*key modifier*/
 	u32 key_states;
+	/*ID of window the event occured in (driver to app only)*/
+	u32 window_id;
 } GF_EventMouse;
 
 /*! Mouse event structure
@@ -91,6 +93,8 @@ typedef struct
 	Fixed pinch;
 	/*number of fingers detected*/
 	u32 num_fingers;
+	/*ID of window the event occured in (driver to app only)*/
+	u32 window_id;
 } GF_EventMultiTouch;
 
 /*! Keyboard key event
@@ -106,6 +110,8 @@ typedef struct
 	u32 hw_code;
 	/*key modifier*/
 	u32 flags;
+	/*ID of window the event occured in (driver to app only)*/
+	u32 window_id;
 } GF_EventKey;
 
 /*! Keyboard character event
@@ -117,6 +123,8 @@ typedef struct
 	u8 type;
 	/*above virtual key code*/
 	u32 unicode_char;
+	/*ID of window the event occured in (driver to app only)*/
+	u32 window_id;
 } GF_EventChar;
 
 /*! Display orientation */
@@ -145,6 +153,8 @@ typedef struct
 	u32 width, height;
 	/*display orientation */
 	GF_DisplayOrientationType orientation;
+	/*ID of window the event occured in (driver to app only)*/
+	u32 window_id;
 } GF_EventSize;
 
 /*! Video setup (2D or 3D) event
@@ -182,6 +192,8 @@ typedef struct
 	u8 type;
 	/*0: hidden - 1: visible - 2: fullscreen*/
 	u32 show_type;
+	/*ID of window the event occured in (driver to app only)*/
+	u32 window_id;
 } GF_EventShow;
 
 /*! Mouse cursor event
@@ -217,6 +229,8 @@ typedef struct
 	u32 relative;
 	/*0: left/top, 1: middle, 2: right/bottom*/
 	u8 align_x, align_y;
+	/*ID of window the event occured in (driver to app only)*/
+	u32 window_id;
 } GF_EventMove;
 
 /*! Media duration event
@@ -367,6 +381,8 @@ typedef struct {
 	u8 type;
 	u32 nb_files;
 	char **files;
+	/*ID of window the event occured in (driver to app only)*/
+	u32 window_id;
 } GF_EventOpenFile;
 
 /*! Orientation sensor change event
@@ -422,6 +438,8 @@ typedef struct
 	- char * for COPY_TEXT, must be freed by caller
 	*/
 	char *text;
+	/*ID of window the event occured in (driver to app only)*/
+	u32 window_id;
 } GF_EventClipboard;
 
 

include/gpac/filters.h
@@ -2185,6 +2185,8 @@ typedef enum
 	GF_FS_REG_FORCE_REMUX = 1<<12,
 	/*! Indicates the filter must always be run by the same thread, except for the initialize and finalize methods*/
 	GF_FS_REG_SINGLE_THREAD = 1<<13,
+	/*! Indicates the filter needs to be initialized even if temoorary - see \ref gf_filter_is_temporary. Always enabled if GF_FS_REG_META is set */
+	GF_FS_REG_TEMP_INIT = 1<<14,
 
 
 	/*! flag dynamically set at runtime for custom filters*/
@@ -2400,6 +2402,20 @@ void *gf_filter_get_udta(GF_Filter *filter);
 \param name new name to assign. If NULL, name is reset to register name
 */
 void gf_filter_set_name(GF_Filter *filter, const char *name);
+
+/*! Checks is a filter is temporary, ie only loaded to solve caps but will not be used.
+
+ Only filter classes setting the GF_FS_REG_TEMP_INIT or GF_FS_REG_META need to check this, and only during the initialize call.
+
+if filter is temprary, it is loaded only to probe for a filter chain setup and will be finalized immediately after.
+This implies that most filter resources (sockets, file handles, etc) must not be created in this case.
+
+A temporary filter will be finalized as any other filter.
+
+\param filter target filter
+\return GF_TRUE if filter is temporary
+*/
+Bool gf_filter_is_temporary(GF_Filter *filter);
 
 /*! Gets filter name
 \param filter target filter
@@ -3186,6 +3202,19 @@ GF_Err gf_filter_tag_subsession(GF_Filter *filter, u32 subsession_id, u32 source
 \return GF_TRUE if parent session has seen connection errors, GF_FALSE otherwise
 */
 Bool gf_filter_has_connect_errors(GF_Filter *filter);
+
+
+/*! Sets names of sub-instances involved in a meta-filter instance
+\param filter target filter
+\param instance_names_list space-separated names of meta filter instances, without meta registry name. eg "negate" for ffavf::f=negate
+*/
+void gf_filter_meta_set_instances(GF_Filter *filter, const char *instance_names_list);
+
+/*! Gets names of sub-instances involved in a meta-filter instance
+\param filter target filter
+\return NULL or space-separated names of meta filter instances, without meta registry name. eg "negate" for ffavf::f=negate
+*/
+const char *gf_filter_meta_get_instances(GF_Filter *filter);
 
 /*! @} */
 

include/gpac/modules/video_out.h
@@ -90,7 +90,7 @@ typedef struct
 typedef struct _gf_sc_texture_handler GF_TextureH;
 
 /*interface name and version for video output*/
-#define GF_VIDEO_OUTPUT_INTERFACE	GF_4CC('G','V','O','6')
+#define GF_VIDEO_OUTPUT_INTERFACE	GF_4CC('G','V','O','7')
 
 /*! window creation flags*/
 typedef enum
@@ -244,6 +244,9 @@ typedef struct _video_out
 	u32 disparity;
 	/*nominal display viewing distance in cm*/
 	Fixed dispdist;
+
+	/*window identifier (one window only per instance of this module)*/
+	u32 window_id;
 
 	/*driver private*/
 	void *opaque;

modules/sdl_out/audio.c
@@ -51,7 +51,6 @@ void sdl_fill_audio(void *udata, Uint8 *stream, int len)
 	}
 }
 
-
 static GF_Err SDLAud_Setup(GF_AudioOutput *dr, void *os_handle, u32 num_buffers, u32 total_duration)
 {
 	u32 flags;

modules/sdl_out/sdl_out.c
@@ -53,7 +53,8 @@ void SDLOUT_CloseSDL()
 	if (!is_init) return;
 	assert(num_users);
 	num_users--;
-	if (!num_users) SDL_Quit();
+	if (!num_users)
+		SDL_Quit();
 	return;
 }
 

commit c07c795888b3074eb36316329a678a094cb8b594

fixed mem leak introduced by 772b233c

src/filter_core/filter_session.c
@@ -129,12 +129,11 @@ void gf_fs_add_filter_register(GF_FilterSession *fsess, const GF_FilterRegister 
 			}
 			blacklist = fname+len;
 		}
+		if (is_whitelist) match = !match;
+
 		if (match) {
-			if (!is_whitelist)
-				return;
-		} else {
-			if (is_whitelist)
-				return;
+			if (freg->register_free) freg->register_free(fsess, (GF_FilterRegister *)freg);
+			return;
 		}
 	}
 	//except for meta filters, don't accept a filter with input caps but no output caps
@@ -145,6 +144,7 @@ void gf_fs_add_filter_register(GF_FilterSession *fsess, const GF_FilterRegister 
 		&& gf_filter_has_in_caps(freg->caps, freg->nb_caps)
 	) {
 		GF_LOG(GF_LOG_ERROR, GF_LOG_FILTER, ("Filter %s missing configure_pid function but has input capabilities - ignoring\n", freg->name));
+		if (freg->register_free) freg->register_free(fsess, (GF_FilterRegister *)freg);
 		return;
 	}
 

src/filters/ff_common.c
@@ -51,6 +51,7 @@ typedef struct
 	GF_List *all_filters;
 	u32 nb_arg_skip;
 	Bool free_help;
+	Bool is_building;
 } GF_FFRegistryExt;
 
 typedef struct
@@ -682,8 +683,11 @@ static void ffmpeg_register_free(GF_FilterSession *session, GF_FilterRegister *r
 	GF_FFRegistryExt *ffregext = reg->udta;
 	GF_List *all_filters = ffregext->all_filters;
 	u32 nb_skip_begin = ffregext->nb_arg_skip;
+	if (ffregext->is_building) return;
+
 #if !defined(GPAC_DISABLE_DOC)
-	if (ffregext->free_help) gf_free((char *)reg->help);
+	if (ffregext->free_help)
+		gf_free((char *)reg->help);
 #endif
 	gf_free(ffregext);
 	reg->udta = NULL;
@@ -1590,7 +1594,9 @@ GF_FilterRegister *ffmpeg_build_register(GF_FilterSession *session, GF_FilterReg
 	if (ffregext) {
 		orig_reg->udta = ffregext;
 		ffregext->nb_arg_skip = nb_def_args-1;
+		ffregext->is_building = GF_TRUE;
 		ffmpeg_expand_register(session, orig_reg, reg_type);
+		ffregext->is_building = GF_FALSE;
 	}
 	return orig_reg;
 }

src/filters/ff_dmx.c
@@ -1744,8 +1744,8 @@ const GF_FilterRegister *ffavin_register(GF_FilterSession *session)
 		gf_dynstrcat(&out_doc, FFAVInRegister.help, NULL);
 		gf_dynstrcat(&out_doc, dev_desc, "\n");
 		gf_free(dev_desc);
-		FFAVInRegister.help = out_doc;
-		ffmpeg_register_set_dyn_help(&FFAVInRegister);
+		res_reg->help = out_doc;
+		ffmpeg_register_set_dyn_help(res_reg);
 	}
 #endif
 	return res_reg;