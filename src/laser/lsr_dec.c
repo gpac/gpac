gpac/gpac

commit fc511483d189284b4db0a2dcf752b2ead45f4b95

cleanup GF_Event doc

src/laser/lsr_dec.c
@@ -820,7 +820,7 @@ static void lsr_read_id(GF_LASeRCodec *lsr, GF_Node *n)
 			if (gf_node_get_attribute_by_tag(listener, TAG_XMLEV_ATT_event, GF_FALSE, GF_FALSE, &info) == GF_OK) {
 				XMLEV_Event *ev = (XMLEV_Event *)info.far_ptr;
 				/*all non-UI get attched to root*/
-				if (ev && (ev->type>GF_EVENT_MOUSEWHEEL)) {
+				if (ev && (ev->type > GF_EVENT_MOUSEWHEEL)) {
 					par = (GF_Node*) lsr->current_root;
 				}
 			}
@@ -4431,7 +4431,7 @@ static GF_Node *lsr_read_listener(GF_LASeRCodec *lsr, SVG_Element *parent)
 		/*FIXME - double check with XML events*/
 		if (!par && !observer) {
 			/*all non-UI get attched to root*/
-			if (ev && (ev->type>GF_EVENT_MOUSEWHEEL)) {
+			if (ev && (ev->type > GF_EVENT_MOUSEWHEEL)) {
 				par = (SVG_Element*) lsr->current_root;
 			}
 			else if (parent) par = parent;

applications/gpac/compositor_tools.c
@@ -766,14 +766,6 @@ Bool mp4c_event_proc(void *ptr, GF_Event *evt)
 			gf_sc_navigate_to(compositor, evt->open_file.files[0]);
 		}
 		return 1;
-
-	case GF_EVENT_DISCONNECT:
-		gf_sc_disconnect(compositor);
-		break;
-	case GF_EVENT_MIGRATE:
-	{
-	}
-	break;
 	case GF_EVENT_NAVIGATE_INFO:
 		if (evt->navigate.to_url)
 			fprintf(stderr, "Go to URL: \"%s\"\r", evt->navigate.to_url);

applications/gpac_android/src/main/jni/gpac_jni.cpp
@@ -740,8 +740,6 @@ static Bool gpac_jni_event_proc(void *cbk, GF_Event *evt)
 		break;
 	case GF_EVENT_EOS:
 		break;
-	case GF_EVENT_DISCONNECT:
-		break;
 	case GF_EVENT_NAVIGATE:
 		if (gf_sc_is_supported_url(gpac->compositor, evt->navigate.to_url, GF_TRUE)) {
 			gf_sc_navigate_to(gpac->compositor, evt->navigate.to_url);

include/gpac/events.h
@@ -394,7 +394,7 @@ typedef struct
 	u8 type;
 	/* device orientation as quaternion if w is not 0, or as radians otherwise*/
 	Float x, y, z, w;
-} GF_EventSensor;
+} GF_EventOrientationSensor;
 
 /*! GPS sensor change event
 	event proc return value: ignored
@@ -451,7 +451,7 @@ typedef union
 	GF_EventMultiTouch mtouch;
 	GF_EventKey key;
 	GF_EventChar character;
-	GF_EventSensor sensor;
+	GF_EventOrientationSensor sensor;
 	GF_EventGPS gps;
 	GF_EventSize size;
 	GF_EventShow show;

src/compositor/compositor.c
@@ -3479,7 +3479,7 @@ static Bool gf_sc_handle_event_intern(GF_Compositor *compositor, GF_Event *event
 {
 	Bool ret;
 
-	if ( (compositor->interaction_level & GF_INTERACT_INPUT_SENSOR) && (event->type<=GF_EVENT_MOUSEWHEEL)) {
+	if ( (compositor->interaction_level & GF_INTERACT_INPUT_SENSOR) && (event->type <= GF_EVENT_LAST_MOUSE)) {
 		GF_Event evt = *event;
 		gf_sc_input_sensor_mouse_input(compositor, &evt.mouse);
 	}

src/compositor/events.c
@@ -43,7 +43,7 @@ static void gf_sc_reset_collide_cursor(GF_Compositor *compositor)
 
 static Bool exec_text_selection(GF_Compositor *compositor, GF_Event *event)
 {
-	if (event->type>GF_EVENT_MOUSEMOVE) return GF_FALSE;
+	if (event->type > GF_EVENT_MOUSEMOVE) return GF_FALSE;
 
 	if (compositor->edited_text)
 		return GF_FALSE;
@@ -655,7 +655,7 @@ static Bool exec_event_dom(GF_Compositor *compositor, GF_Event *event)
 
 	cursor_type = GF_CURSOR_NORMAL;
 	/*all mouse events*/
-	if (event->type<=GF_EVENT_MOUSEWHEEL) {
+	if (event->type <= GF_EVENT_LAST_MOUSE) {
 		Fixed X = compositor->hit_world_point.x;
 		Fixed Y = compositor->hit_world_point.y;
 		/*flip back to origin at top-left*/
@@ -785,7 +785,7 @@ static Bool exec_event_dom(GF_Compositor *compositor, GF_Event *event)
 			compositor->sensor_type = cursor_type;
 		}
 	}
-	else if ((event->type>=GF_EVENT_KEYUP) && (event->type<=GF_EVENT_LONGKEYPRESS)) {
+	else if ((event->type >= GF_EVENT_KEYUP) && (event->type <= GF_EVENT_LONGKEYPRESS)) {
 		GF_Node *target;
 		memset(&evt, 0, sizeof(GF_DOM_Event));
 		evt.key_flags = event->key.flags;
@@ -1068,7 +1068,7 @@ Bool visual_execute_event(GF_VisualManager *visual, GF_TraverseState *tr_state, 
 #endif
 
 	/*preprocess text selection and edition*/
-	if ((ev->type<GF_EVENT_MOUSEWHEEL) && (ev->mouse.button==GF_MOUSE_LEFT)) {
+	if ((ev->type < GF_EVENT_LAST_MOUSE_COORDS) && (ev->mouse.button==GF_MOUSE_LEFT)) {
 		if (compositor->text_selection) {
 			if (ev->type==GF_EVENT_MOUSEUP) {
 				if (compositor->store_text_state==GF_SC_TSEL_ACTIVE)
@@ -1889,7 +1889,7 @@ u32 gf_sc_focus_switch_ring(GF_Compositor *compositor, Bool move_prev, GF_Node *
 Bool gf_sc_execute_event(GF_Compositor *compositor, GF_TraverseState *tr_state, GF_Event *ev, GF_ChildNodeItem *children)
 {
 	/*filter mouse events and other events (key...)*/
-	if (ev->type>GF_EVENT_MOUSEWHEEL) {
+	if (ev->type > GF_EVENT_MOUSEWHEEL) {
 		Bool ret = GF_FALSE;
 		/*send text edit*/
 		if (compositor->edited_text) {
@@ -1989,7 +1989,7 @@ Bool gf_sc_exec_event(GF_Compositor *compositor, GF_Event *evt)
 	s32 x=0, y=0;
 	Bool switch_coords = GF_FALSE;
 	Bool ret = GF_FALSE;
-	if (evt->type<=GF_EVENT_MOUSEWHEEL) {
+	if (evt->type <= GF_EVENT_LAST_MOUSE) {
 		if (compositor->sgaze) {
 			compositor->gaze_x = evt->mouse.x;
 			compositor->gaze_y = evt->mouse.y;

src/compositor/mpeg4_sensors.c
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2000-2012
+ *			Copyright (c) Telecom ParisTech 2000-2022
  *					All rights reserved
  *
  *  This file is part of GPAC / Scene Compositor sub-project
@@ -256,7 +256,7 @@ static Bool ds_is_enabled(GF_Node *n)
 
 static Bool OnDiscSensor(GF_SensorHandler *sh, Bool is_over, Bool is_cancel, GF_Event *ev, GF_Compositor *compositor)
 {
-	Bool is_mouse = (ev->type<=GF_EVENT_MOUSEWHEEL) ? 1 : 0;
+	Bool is_mouse = (ev->type <= GF_EVENT_LAST_MOUSE) ? 1 : 0;
 	M_DiscSensor *ds = (M_DiscSensor *)sh->sensor;
 	DiscSensorStack *stack = (DiscSensorStack *) gf_node_get_private(sh->sensor);
 
@@ -395,7 +395,7 @@ static Bool ps2D_is_enabled(GF_Node *n)
 
 static Bool OnPlaneSensor2D(GF_SensorHandler *sh, Bool is_over, Bool is_cancel, GF_Event *ev, GF_Compositor *compositor)
 {
-	Bool is_mouse = (ev->type<=GF_EVENT_MOUSEWHEEL) ? 1 : 0;
+	Bool is_mouse = (ev->type <= GF_EVENT_LAST_MOUSE) ? 1 : 0;
 	M_PlaneSensor2D *ps = (M_PlaneSensor2D *)sh->sensor;
 	PS2DStack *stack = (PS2DStack *) gf_node_get_private(sh->sensor);
 
@@ -646,7 +646,7 @@ static Bool ts_is_enabled(GF_Node *n)
 
 static Bool OnTouchSensor(GF_SensorHandler *sh, Bool is_over, Bool is_cancel, GF_Event *ev, GF_Compositor *compositor)
 {
-	Bool is_mouse = (ev->type<=GF_EVENT_MOUSEWHEEL);
+	Bool is_mouse = (ev->type <= GF_EVENT_LAST_MOUSE);
 	M_TouchSensor *ts = (M_TouchSensor *)sh->sensor;
 
 	/*this is not specified in VRML, however we consider that a de-enabled sensor will not sent deactivation events*/
@@ -826,7 +826,7 @@ static Bool ps_is_enabled(GF_Node *n)
 
 static Bool OnPlaneSensor(GF_SensorHandler *sh, Bool is_over, Bool is_cancel, GF_Event *ev, GF_Compositor *compositor)
 {
-	Bool is_mouse = (ev->type<=GF_EVENT_MOUSEWHEEL) ? 1 : 0;
+	Bool is_mouse = (ev->type <= GF_EVENT_LAST_MOUSE) ? 1 : 0;
 	M_PlaneSensor *ps = (M_PlaneSensor *)sh->sensor;
 	PSStack *stack = (PSStack *) gf_node_get_private(sh->sensor);
 
@@ -982,7 +982,7 @@ static Bool cs_is_enabled(GF_Node *n)
 
 static Bool OnCylinderSensor(GF_SensorHandler *sh, Bool is_over, Bool is_cancel, GF_Event *ev, GF_Compositor *compositor)
 {
-	Bool is_mouse = (ev->type<=GF_EVENT_MOUSEWHEEL) ? 1 : 0;
+	Bool is_mouse = (ev->type <= GF_EVENT_LAST_MOUSE) ? 1 : 0;
 	M_CylinderSensor *cs = (M_CylinderSensor *)sh->sensor;
 	CylinderSensorStack *st = (CylinderSensorStack *) gf_node_get_private(sh->sensor);
 
@@ -1185,7 +1185,7 @@ static Bool sphere_is_enabled(GF_Node *n)
 
 static Bool OnSphereSensor(GF_SensorHandler *sh, Bool is_over, Bool is_cancel, GF_Event *ev, GF_Compositor *compositor)
 {
-	Bool is_mouse = (ev->type<=GF_EVENT_MOUSEWHEEL) ? 1 : 0;
+	Bool is_mouse = (ev->type <= GF_EVENT_LAST_MOUSE) ? 1 : 0;
 	M_SphereSensor *sphere = (M_SphereSensor *)sh->sensor;
 	SphereSensorStack *st = (SphereSensorStack *) gf_node_get_private(sh->sensor);
 

commit 33b8f7e629f1ed7c8363d748e71d2d9c180bf633

fixed bug introduced by fc511483

src/laser/lsr_dec.c
@@ -820,7 +820,11 @@ static void lsr_read_id(GF_LASeRCodec *lsr, GF_Node *n)
 			if (gf_node_get_attribute_by_tag(listener, TAG_XMLEV_ATT_event, GF_FALSE, GF_FALSE, &info) == GF_OK) {
 				XMLEV_Event *ev = (XMLEV_Event *)info.far_ptr;
 				/*all non-UI get attched to root*/
-				if (ev && (ev->type > GF_EVENT_MOUSEWHEEL)) {
+				if (ev
+					&& (ev->type > GF_EVENT_MOUSEWHEEL)
+					&& (ev->type!=GF_EVENT_MOUSEOUT)
+					&& (ev->type!=GF_EVENT_MOUSEOVER)
+				) {
 					par = (GF_Node*) lsr->current_root;
 				}
 			}
@@ -4431,7 +4435,10 @@ static GF_Node *lsr_read_listener(GF_LASeRCodec *lsr, SVG_Element *parent)
 		/*FIXME - double check with XML events*/
 		if (!par && !observer) {
 			/*all non-UI get attched to root*/
-			if (ev && (ev->type > GF_EVENT_MOUSEWHEEL)) {
+			if (ev && (ev->type > GF_EVENT_MOUSEWHEEL)
+				&& (ev->type!=GF_EVENT_MOUSEOUT)
+				&& (ev->type!=GF_EVENT_MOUSEOVER)
+			) {
 				par = (SVG_Element*) lsr->current_root;
 			}
 			else if (parent) par = parent;

src/laser/lsr_enc.c
@@ -4122,7 +4122,10 @@ static GF_Err lsr_write_command_list(GF_LASeRCodec *lsr, GF_List *com_list, SVG_
 			} else {
 				GF_LSR_WRITE_INT(lsr, 0, 1, "has_intvalue");
 			}
-			if (com->send_event_name<=GF_EVENT_MOUSEWHEEL) {
+			if ((com->send_event_name<=GF_EVENT_MOUSEWHEEL)
+				|| (com->send_event_name==GF_EVENT_MOUSEOUT)
+				|| (com->send_event_name==GF_EVENT_MOUSEOVER)
+			) {
 				GF_LSR_WRITE_INT(lsr, 1, 1, "has_pointvalue");
 				lsr_write_coordinate(lsr, INT2FIX(com->send_event_x), 0, "x");
 				lsr_write_coordinate(lsr, INT2FIX(com->send_event_y), 0, "y");

src/scene_manager/scene_dump.c
@@ -2779,9 +2779,13 @@ static GF_Err DumpLSRSendEvent(GF_SceneDumper *sdump, GF_Command *com)
 	        lsr_format_node_id(com->node, com->RouteID, szID),
 	        gf_dom_event_get_name(com->send_event_name)
 	       );
-	if (com->send_event_name <= GF_EVENT_MOUSEWHEEL)
+	if ((com->send_event_name <= GF_EVENT_MOUSEWHEEL)
+		|| (com->send_event_name == GF_EVENT_MOUSEOUT)
+		|| (com->send_event_name == GF_EVENT_MOUSEOVER)
+	) {
 		gf_fprintf(sdump->trace, " pointvalue=\"%g %g\"", FIX2FLT(com->send_event_x), FIX2FLT(com->send_event_y) );
-
+	}
+	
 	switch (com->send_event_name) {
 	case GF_EVENT_KEYDOWN:
 	case GF_EVENT_LONGKEYPRESS: