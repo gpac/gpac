gpac/gpac

commit 33f5633e0f1426dd07e15ed0cd9fc0853d30a9c3

added vvc as nalu-based entry in isobmf and added nal isobmf sample SAP type check - cf #2370

src/isomedia/movie_fragments.c
@@ -763,9 +763,36 @@ u32 UpdateRuns(GF_ISOFile *movie, GF_TrackFragmentBox *traf)
 	return sampleCount;
 }
 
+static s32 get_earliest_cts_following(GF_TrackFragmentBox *traf, u32 trun_idx, u32 samp_idx)
+{
+	GF_TrackFragmentRunBox *trun;
+	GF_TrunEntry *ent;
+	s32 earliest_cts = 0;
+	s32 delta = 0;
+	u32 j;
+	u32 i=trun_idx;
+	while ((trun = (GF_TrackFragmentRunBox*)gf_list_enum(traf->TrackRuns, &i))) {
+		j=samp_idx;
+		samp_idx=0;
+		for (; j<trun->nb_samples; j++) {
+			ent = &trun->samples[j];
+			if (!j && (i==1)) {
+				earliest_cts = ent->CTS_Offset;
+			} else {
+				s32 cts = ent->CTS_Offset+delta;
+				if (earliest_cts > cts)
+					earliest_cts = cts;
+			}
+			delta += ent->Duration;
+		}
+	}
+	return earliest_cts;
+}
+
 static u32 moof_get_sap_info(GF_MovieFragmentBox *moof, GF_ISOTrackID refTrackID, u32 *sap_delta, Bool *starts_with_sap)
 {
-	u32 i, j, count, delta, earliest_cts, sap_type, sap_sample_num, cur_sample;
+	u32 i, j, count, sap_type, sap_sample_num, cur_sample;
+	s32 delta, earliest_cts;
 	Bool first = GF_TRUE;
 	GF_TrunEntry *ent;
 	GF_TrackFragmentBox *traf=NULL;
@@ -821,6 +848,8 @@ static u32 moof_get_sap_info(GF_MovieFragmentBox *moof, GF_ISOTrackID refTrackID
 		}
 	}
 
+	earliest_cts = get_earliest_cts_following(traf, 0, 0);
+
 	/*then browse all samples, looking for SYNC flag or sap_sample_num*/
 	cur_sample = 1;
 	delta = 0;
@@ -829,8 +858,9 @@ static u32 moof_get_sap_info(GF_MovieFragmentBox *moof, GF_ISOTrackID refTrackID
 		if (trun->flags & GF_ISOM_TRUN_FIRST_FLAG) {
 			if (GF_ISOM_GET_FRAG_SYNC(trun->flags)) {
 				ent = &trun->samples[0];
-//				if (!delta) earliest_cts = ent->CTS_Offset;
-				*sap_delta = delta + ent->CTS_Offset - ent->CTS_Offset;
+				s32 earliest_at_or_after_sap = get_earliest_cts_following(traf, i-1, 0);
+				*sap_delta = (u32) (earliest_at_or_after_sap - earliest_cts);
+
 				*starts_with_sap = first;
 				sap_type = ent->SAP_type;
 				return sap_type;
@@ -841,14 +871,15 @@ static u32 moof_get_sap_info(GF_MovieFragmentBox *moof, GF_ISOTrackID refTrackID
 			if (!delta) earliest_cts = ent->CTS_Offset;
 
 			if (GF_ISOM_GET_FRAG_SYNC(ent->flags)) {
-				*sap_delta = delta + ent->CTS_Offset - earliest_cts;
+				s32 earliest_at_or_after_sap = get_earliest_cts_following(traf, i-1, j);
+				*sap_delta = (u32) (earliest_at_or_after_sap - earliest_cts);
 				*starts_with_sap = first;
 				sap_type = ent->SAP_type;
 				return sap_type;
 			}
 			/*we found our roll or rap sample*/
 			if (cur_sample==sap_sample_num) {
-				*sap_delta = delta + ent->CTS_Offset - earliest_cts;
+				*sap_delta = (u32) (delta + ent->CTS_Offset - earliest_cts);
 				return sap_type;
 			}
 			delta += ent->Duration;

src/isomedia/avc_ext.c
@@ -57,6 +57,9 @@ Bool gf_isom_is_nalu_based_entry(GF_MediaBox *mdia, GF_SampleEntryBox *_entry)
 	case GF_ISOM_BOX_TYPE_MHC1:
 	case GF_ISOM_BOX_TYPE_HVT1:
 	case GF_ISOM_BOX_TYPE_LHT1:
+	case GF_ISOM_BOX_TYPE_VVC1:
+	case GF_ISOM_BOX_TYPE_VVI1:
+	case GF_ISOM_BOX_TYPE_VVS1:
 		return GF_TRUE;
 	case GF_ISOM_BOX_TYPE_GNRV:
 	case GF_ISOM_BOX_TYPE_GNRA:
@@ -284,9 +287,10 @@ static GF_ISOSAPType sap_type_from_nal_type(u8 nal_type) {
 }
 #endif
 
-static GF_ISOSAPType is_sample_idr(GF_MediaBox *mdia, GF_ISOSample *sample, GF_MPEGVisualSampleEntryBox *entry)
+GF_ISOSAPType gf_isom_nalu_get_sample_sap(GF_MediaBox *mdia, GF_ISOSample *sample, GF_MPEGVisualSampleEntryBox *entry)
 {
 	Bool is_hevc = GF_FALSE;
+	Bool is_vvc = GF_FALSE;
 	u32 nalu_size_field = 0;
 	if (entry->avc_config && entry->avc_config->config) nalu_size_field = entry->avc_config->config->nal_unit_size;
 	else if (entry->svc_config && entry->svc_config->config) nalu_size_field = entry->svc_config->config->nal_unit_size;
@@ -298,6 +302,10 @@ static GF_ISOSAPType is_sample_idr(GF_MediaBox *mdia, GF_ISOSample *sample, GF_M
 	else if (entry->lhvc_config && entry->lhvc_config->config) {
 		nalu_size_field = entry->lhvc_config->config->nal_unit_size;
 		is_hevc = GF_TRUE;
+	}
+	else if (entry->vvc_config && entry->vvc_config->config) {
+		nalu_size_field = entry->vvc_config->config->nal_unit_size;
+		is_vvc = GF_TRUE;
 	}
 	if (!nalu_size_field) return RAP_NO;
 
@@ -339,6 +347,28 @@ static GF_ISOSAPType is_sample_idr(GF_MediaBox *mdia, GF_ISOSample *sample, GF_M
 			}
 			gf_bs_skip_bytes(mdia->nalu_parser, size - 2);
 #endif
+		} else if (is_vvc) {
+			u16 nal_hdr = gf_bs_read_u16(mdia->nalu_parser);
+			nal_type = (nal_hdr >> 3) & 0x1F;
+
+			switch (nal_type) {
+			case GF_VVC_NALU_SLICE_CRA:
+				return SAP_TYPE_3;
+			case GF_VVC_NALU_SLICE_IDR_N_LP:
+				return SAP_TYPE_1;
+			case GF_VVC_NALU_SLICE_IDR_W_RADL:
+				return SAP_TYPE_2;
+			case GF_VVC_NALU_SLICE_GDR:
+				return SAP_TYPE_3;
+			case GF_VVC_NALU_SLICE_TRAIL:
+			case GF_VVC_NALU_SLICE_STSA:
+			case GF_VVC_NALU_SLICE_RADL:
+			case GF_VVC_NALU_SLICE_RASL:
+				return RAP_NO;
+			default:
+				break;
+			}
+			gf_bs_skip_bytes(mdia->nalu_parser, size - 2);
 		} else {
 			u8 nal_hdr = gf_bs_read_u8(mdia->nalu_parser);
 			nal_type = nal_hdr & 0x1F;
@@ -516,7 +546,7 @@ GF_Err gf_isom_nalu_sample_rewrite(GF_MediaBox *mdia, GF_ISOSample *sample, u32 
 
 	if (sample->IsRAP < SAP_TYPE_2) {
 		if (mdia->information->sampleTable->no_sync_found || (!sample->IsRAP && check_cra_bla) ) {
-			sample->IsRAP = is_sample_idr(mdia, sample, entry);
+			sample->IsRAP = gf_isom_nalu_get_sample_sap(mdia, sample, entry);
 		}
 	}
 	if (!sample->IsRAP)

src/isomedia/isom_read.c
@@ -5692,6 +5692,7 @@ u32 gf_isom_get_nalu_length_field(GF_ISOFile *file, u32 track, u32 StreamDescrip
 	if (ve->svc_config) return ve->svc_config->config->nal_unit_size;
 	if (ve->hevc_config) return ve->hevc_config->config->nal_unit_size;
 	if (ve->lhvc_config) return ve->lhvc_config->config->nal_unit_size;
+	if (ve->vvc_config) return ve->vvc_config->config->nal_unit_size;
 	return 0;
 }
 

src/isomedia/isom_write.c
@@ -8056,6 +8056,7 @@ GF_Err gf_isom_set_nalu_length_field(GF_ISOFile *file, u32 track, u32 StreamDesc
 	if (ve->svc_config) ve->svc_config->config->nal_unit_size = nalu_size_length;
 	if (ve->hevc_config) ve->hevc_config->config->nal_unit_size = nalu_size_length;
 	if (ve->lhvc_config) ve->lhvc_config->config->nal_unit_size = nalu_size_length;
+	if (ve->vvc_config) ve->vvc_config->config->nal_unit_size = nalu_size_length;
 	return GF_OK;
 }
 

src/isomedia/media.c
@@ -656,11 +656,18 @@ GF_Err Media_GetSample(GF_MediaBox *mdia, u32 sampleNumber, GF_ISOSample **samp,
 			if (e) return e;
 		}
 	}
-	else if (gf_isom_is_nalu_based_entry(mdia, entry)
-		&& !gf_isom_is_encrypted_entry(entry->type)
-	) {
-		e = gf_isom_nalu_sample_rewrite(mdia, *samp, sampleNumber, (GF_MPEGVisualSampleEntryBox *)entry);
-		if (e) return e;
+	else if (gf_isom_is_nalu_based_entry(mdia, entry)) {
+		GF_ISOSAPType gf_isom_nalu_get_sample_sap(GF_MediaBox *mdia, GF_ISOSample *sample, GF_MPEGVisualSampleEntryBox *entry);
+
+		if (!gf_isom_is_encrypted_entry(entry->type)) {
+			e = gf_isom_nalu_sample_rewrite(mdia, *samp, sampleNumber, (GF_MPEGVisualSampleEntryBox *)entry);
+			if (e) return e;
+		}
+		if (!gf_sys_old_arch_compat()) {
+			GF_ISOSAPType sap = gf_isom_nalu_get_sample_sap(mdia, *samp, (GF_MPEGVisualSampleEntryBox *)entry);
+			if (sap && ! (*samp)->IsRAP) (*samp)->IsRAP = sap;
+			else if ((*samp)->IsRAP < sap) (*samp)->IsRAP = sap;
+		}
 	}
 	else if (mdia->mediaTrack->moov->mov->convert_streaming_text
 	         && ((mdia->handler->handlerType == GF_ISOM_MEDIA_TEXT) || (mdia->handler->handlerType == GF_ISOM_MEDIA_SCENE) || (mdia->handler->handlerType == GF_ISOM_MEDIA_SUBT))

commit 413aa6a077e9e257968abb663bcf4afc8571dfe5

fixed bug introduced in 33f5633e

src/isomedia/movie_fragments.c
@@ -847,7 +847,7 @@ static u32 moof_get_sap_info(GF_MovieFragmentBox *moof, GF_ISOTrackID refTrackID
 			break;
 		}
 	}
-
+	//compute earliest cts in segment
 	earliest_cts = get_earliest_cts_following(traf, 0, 0);
 
 	/*then browse all samples, looking for SYNC flag or sap_sample_num*/
@@ -868,7 +868,6 @@ static u32 moof_get_sap_info(GF_MovieFragmentBox *moof, GF_ISOTrackID refTrackID
 		}
 		for (j=0; j<trun->nb_samples; j++) {
 			ent = &trun->samples[j];
-			if (!delta) earliest_cts = ent->CTS_Offset;
 
 			if (GF_ISOM_GET_FRAG_SYNC(ent->flags)) {
 				s32 earliest_at_or_after_sap = get_earliest_cts_following(traf, i-1, j);