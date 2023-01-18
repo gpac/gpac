gpac/gpac

commit 0f9ba8c63ddc9296032c4c67ae21003b654c362c

added default track and same track order in mkv import - cf #2371

src/filters/mux_isom.c
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2017-2022
+ *			Copyright (c) Telecom ParisTech 2017-2023
  *					All rights reserved
  *
  *  This file is part of GPAC / ISOBMF mux filter
@@ -376,6 +376,8 @@ typedef struct
 	u32 seg_flush_state;
 	u64 flush_idx_start_range, flush_idx_end_range;
 	Bool flush_ll_hls;
+
+	Bool has_def_vid, has_def_aud, has_def_txt;
 } GF_MP4MuxCtx;
 
 static void mp4_mux_update_init_edit(GF_MP4MuxCtx *ctx, TrackWriter *tkw, u64 min_ts_service, Bool skip_adjust);
@@ -1328,7 +1330,7 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 			if (!ctx->owns_mov) {
 				u32 nb_dst_tk = gf_isom_get_track_count(ctx->file);
 				if (tk_idx < nb_dst_tk) {
-					tk_idx = nb_dst_tk;
+					//tk_idx = nb_dst_tk;
 				}
 			}
 		}
@@ -1422,9 +1424,12 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 		} else {
 			gf_isom_set_track_enabled(ctx->file, tkw->track_num, GF_TRUE);
 		}
+		Bool is_disabled = GF_FALSE;
 		p = gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_DISABLED);
-		if (p && p->value.boolean)
+		if (p && p->value.boolean) {
 			gf_isom_set_track_enabled(ctx->file, tkw->track_num, GF_FALSE);
+			is_disabled = GF_TRUE;
+		}
 
 		//if we have a subtype set for the pid, use it
 		p = gf_filter_pid_get_property(pid, GF_PROP_PID_SUBTYPE);
@@ -1448,6 +1453,43 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 			}
 			if (tkw->stream_type==GF_STREAM_AUDIO) {
 				gf_isom_set_alternate_group_id(ctx->file, tkw->track_num, 1);
+			}
+		}
+
+		//check if we have default flag set
+		if (!is_disabled && !ctx->cmaf) {
+			Bool *has_def = NULL;
+			if (tkw->stream_type==GF_STREAM_VISUAL) has_def = &ctx->has_def_vid;
+			else if (tkw->stream_type==GF_STREAM_AUDIO) has_def = &ctx->has_def_aud;
+			else if (tkw->stream_type==GF_STREAM_TEXT) has_def = &ctx->has_def_txt;
+
+			Bool set_def = GF_FALSE;
+			Bool set_all_def = GF_FALSE;
+			p = gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_IS_DEFAULT);
+			//first track of this kind set to default, deactivate all except this track
+			if (has_def && ! *has_def && p && p->value.boolean) {
+				*has_def = GF_TRUE;
+				set_def = GF_TRUE;
+			}
+			//second or more track of this kind set to default, re-activate all
+			else if (has_def && *has_def && p && p->value.boolean) {
+				set_all_def = GF_TRUE;
+			}
+			if (set_def || set_all_def) {
+				u32 nb_tk = gf_list_count(ctx->tracks);
+				for (i=0; i<nb_tk; i++) {
+					TrackWriter *atk = gf_list_get(ctx->tracks, i);
+					if (atk->stream_type != tkw->stream_type) continue;
+					if (set_all_def || (tkw == atk)) {
+						gf_isom_set_track_flags(ctx->file, atk->track_num, GF_ISOM_TK_IN_MOVIE|GF_ISOM_TK_IN_PREVIEW, GF_ISOM_TKFLAGS_ADD);
+					} else {
+						gf_isom_set_track_flags(ctx->file, atk->track_num, GF_ISOM_TK_IN_MOVIE|GF_ISOM_TK_IN_PREVIEW, GF_ISOM_TKFLAGS_REM);
+					}
+				}
+			}
+			//no default prop and a default exists for this kind, update flags
+			else if (has_def && *has_def) {
+				gf_isom_set_track_flags(ctx->file, tkw->track_num, GF_ISOM_TK_IN_MOVIE|GF_ISOM_TK_IN_PREVIEW, GF_ISOM_TKFLAGS_REM);
 			}
 		}
 

applications/mp4box/mp4box.c
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2000-2022
+ *			Copyright (c) Telecom ParisTech 2000-2023
  *					All rights reserved
  *
  *  This file is part of GPAC / mp4box application
@@ -4469,7 +4469,7 @@ static u32 do_add_cat(int argc, char **argv)
 	}
 
 	for (ipass=0; ipass<nb_pass; ipass++) {
-		u32 tk_idx = 1;
+		u32 tk_idx = 0;
 		for (i=0; i<(u32) argc; i++) {
 			char *margs=NULL;
 			char *msid = NULL;

include/gpac/filters.h
@@ -1103,6 +1103,7 @@ enum
 	GF_PROP_PID_HAS_SYNC = GF_4CC('P','S','Y','N'),
 	GF_PROP_SERVICE_WIDTH = GF_4CC('D','W','D','T'),
 	GF_PROP_SERVICE_HEIGHT = GF_4CC('D','H','G','T'),
+	GF_PROP_PID_IS_DEFAULT = GF_4CC('P','D','E','F'),
 	GF_PROP_PID_CAROUSEL_RATE = GF_4CC('C','A','R','A'),
 	GF_PROP_PID_AUDIO_VOLUME = GF_4CC('A','V','O','L'),
 	GF_PROP_PID_AUDIO_PAN = GF_4CC('A','P','A','N'),

share/doc/man/gpac-filters.1
@@ -11118,9 +11118,9 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.3-DEV-rev9-g92411f61d-master
+build: 2.3-DEV-rev21-ga62df36f6-master
 .br
-Copyright: (c) 2000-2022 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
+Copyright: (c) 2000-2023 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br
 .SH SEE ALSO
 .LP

share/doc/man/gpac.1
@@ -3142,6 +3142,11 @@ Display width of service
 Display height of service
 .br
 .TP
+.B IsDefault (PDEF,bool,D )
+.br
+Default PID for this stream type
+.br
+.TP
 .B CarouselRate (CARA,uint,D )
 .br
 Repeat rate in ms for systems carousel data
@@ -3670,7 +3675,7 @@ ICC profile (see ISO 15076-1 or ICC.1)
 Magic number to store in the track, only used by importers
 .br
 .TP
-.B MuxIndex (TIDX,luint,D )
+.B MuxIndex (TIDX,uint,D )
 .br
 Target track index in destination file, stored by lowest value first (not set by demultiplexers)
 .br
@@ -4955,9 +4960,9 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.3-DEV-rev9-g92411f61d-master
+build: 2.3-DEV-rev21-ga62df36f6-master
 .br
-Copyright: (c) 2000-2022 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
+Copyright: (c) 2000-2023 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br
 .SH SEE ALSO
 .LP

share/doc/man/mp4box.1
@@ -2924,9 +2924,9 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.3-DEV-rev9-g92411f61d-master
+build: 2.3-DEV-rev21-ga62df36f6-master
 .br
-Copyright: (c) 2000-2022 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
+Copyright: (c) 2000-2023 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br
 .SH SEE ALSO
 .LP

src/filter_core/filter_props.c
@@ -1446,6 +1446,7 @@ GF_BuiltInProperty GF_BuiltInProps [] =
 	{ GF_PROP_PID_HAS_SYNC, "HasSync", "PID has sync points", GF_PROP_BOOL, GF_PROP_FLAG_GSF_REM},
 	{ GF_PROP_SERVICE_WIDTH, "ServiceWidth", "Display width of service", GF_PROP_UINT, GF_PROP_FLAG_GSF_REM},
 	{ GF_PROP_SERVICE_HEIGHT, "ServiceHeight", "Display height of service", GF_PROP_UINT, GF_PROP_FLAG_GSF_REM},
+	{ GF_PROP_PID_IS_DEFAULT, "IsDefault", "Default PID for this stream type", GF_PROP_BOOL, GF_PROP_FLAG_GSF_REM},
 	{ GF_PROP_PID_CAROUSEL_RATE, "CarouselRate", "Repeat rate in ms for systems carousel data", GF_PROP_UINT, GF_PROP_FLAG_GSF_REM},
 	{ GF_PROP_PID_AUDIO_VOLUME, "AudioVolume", "Volume of audio", GF_PROP_UINT, GF_PROP_FLAG_GSF_REM},
 	{ GF_PROP_PID_AUDIO_PAN, "AudioPan", "Balance/Pan of audio", GF_PROP_UINT, GF_PROP_FLAG_GSF_REM},
@@ -1557,7 +1558,7 @@ GF_BuiltInProperty GF_BuiltInProps [] =
 	{ GF_PROP_PID_ICC_PROFILE, "ICC", "ICC profile (see ISO 15076-1 or ICC.1)", GF_PROP_DATA, GF_PROP_FLAG_GSF_REM},
 
 	{ GF_PROP_PID_SRC_MAGIC, "SrcMagic", "Magic number to store in the track, only used by importers", GF_PROP_LUINT, GF_PROP_FLAG_GSF_REM},
-	{ GF_PROP_PID_MUX_INDEX, "MuxIndex", "Target track index in destination file, stored by lowest value first (not set by demultiplexers)", GF_PROP_LUINT, GF_PROP_FLAG_GSF_REM},
+	{ GF_PROP_PID_MUX_INDEX, "MuxIndex", "Target track index in destination file, stored by lowest value first (not set by demultiplexers)", GF_PROP_UINT, GF_PROP_FLAG_GSF_REM},
 	{ GF_PROP_NO_TS_LOOP, "NoTSLoop", "Timestamps on this PID are adjusted in case of loops (used by TS multiplexer output)", GF_PROP_BOOL, 0},
 	{ GF_PROP_PID_MHA_COMPATIBLE_PROFILES, "MHAProfiles", "List of compatible profiles for this MPEG-H Audio object", GF_PROP_UINT_LIST, GF_PROP_FLAG_GSF_REM},
 

src/filters/ff_dmx.c
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2017-2022
+ *			Copyright (c) Telecom ParisTech 2017-2023
  *					All rights reserved
  *
  *  This file is part of GPAC / ffmpeg demux filter
@@ -986,6 +986,10 @@ GF_Err ffdmx_init_common(GF_Filter *filter, GF_FFDemuxCtx *ctx, u32 grab_type)
 		if (codec_blockalign)
 			gf_filter_pid_set_property(pid, GF_PROP_PID_META_DEMUX_OPAQUE, &PROP_UINT(codec_blockalign));
 
+		if ((stream->disposition & AV_DISPOSITION_DEFAULT) && !gf_sys_is_test_mode()) {
+			gf_filter_pid_set_property(pid, GF_PROP_PID_IS_DEFAULT, &PROP_BOOL(GF_TRUE));
+		}
+		gf_filter_pid_set_property(pid, GF_PROP_PID_MUX_INDEX, &PROP_UINT(i+1));
 
 		for (j=0; j<(u32) stream->nb_side_data; j++) {
 			ffdmx_parse_side_data(&stream->side_data[i], pid);

commit 906ee02d517e02b8c5d2b431fe15a4a213f5c21d

fixed bug introduced in 0f9ba8c6

src/filters/mux_isom.c
@@ -614,8 +614,11 @@ static void mp4mux_track_reorder(void *udta, u32 old_track_num, u32 new_track_nu
 	count = gf_list_count(ctx->tracks);
 	for (i=0; i<count; i++) {
 		TrackWriter *tkw = gf_list_get(ctx->tracks, i);
+		if (!tkw->track_id) continue;
 		if (tkw->track_num==old_track_num) {
 			tkw->track_num = new_track_num;
+			//prevent any further changes, trackID is restored in mp4mux_reorder_tracks
+			tkw->track_id = 0;
 			return;
 		}
 	}
@@ -629,6 +632,9 @@ static void mp4mux_reorder_tracks(GF_MP4MuxCtx *ctx)
 	count = gf_list_count(ctx->tracks);
 	for (i=0; i<count; i++) {
 		TrackWriter *tkw = gf_list_get(ctx->tracks, i);
+		if (!tkw->track_id)
+			tkw->track_id = gf_isom_get_track_id(ctx->file, tkw->track_num);
+
 		if (tkw->track_num<prev_num) {
 			gf_list_insert(new_tracks, tkw, prev_pos);
 		} else {