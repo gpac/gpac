gpac/gpac

commit 7127d9aaaf7573e5057e6bda70cb3fe3110e458a

added option to load reduce set of properties in mp4dmx

src/isomedia/isom_read.c
@@ -1105,6 +1105,14 @@ u64 gf_isom_get_track_duration(GF_ISOFile *movie, u32 trackNumber)
 }
 
 GF_EXPORT
+u64 gf_isom_get_track_duration_orig(GF_ISOFile *movie, u32 trackNumber)
+{
+	GF_TrackBox *trak;
+	trak = gf_isom_get_track_from_file(movie, trackNumber);
+	if (!trak) return 0;
+	return trak->Header->duration;
+}
+GF_EXPORT
 GF_Err gf_isom_get_media_language(GF_ISOFile *the_file, u32 trackNumber, char **lang)
 {
 	u32 count;
@@ -4031,6 +4039,8 @@ u64 gf_isom_get_media_data_size(GF_ISOFile *movie, u32 trackNumber)
 	if (!tk) return 0;
 	stsz = tk->Media->information->sampleTable->SampleSize;
 	if (!stsz) return 0;
+	if (movie->openMode==GF_ISOM_OPEN_READ)
+		return stsz->total_size;
 	if (stsz->sampleSize) return stsz->sampleSize*stsz->sampleCount;
 	size = 0;
 	for (i=0; i<stsz->sampleCount; i++) size += stsz->sizes[i];

include/gpac/isomedia.h
@@ -877,11 +877,17 @@ u8 gf_isom_is_track_enabled(GF_ISOFile *isom_file, u32 trackNumber);
 */
 u32 gf_isom_get_track_flags(GF_ISOFile *isom_file, u32 trackNumber);
 
-/*! gets the track duration
+/*! gets the track duration - this will try to fix any discrepencies between media duration+edit lists vs track duration
 \param isom_file the target ISO file
 \param trackNumber the target track
 \return the track duration in movie timescale, or 0 if error*/
 u64 gf_isom_get_track_duration(GF_ISOFile *isom_file, u32 trackNumber);
+
+/*! gets the unmodified track duration - mus be called before any call to \ref gf_isom_get_track_duration
+\param isom_file the target ISO file
+\param trackNumber the target track
+\return the track duration in movie timescale, or 0 if error*/
+u64 gf_isom_get_track_duration_orig(GF_ISOFile *isom_file, u32 trackNumber);
 
 /*! gets the media type (audio, video, etc) of a track
 \param isom_file the target ISO file

src/filters/isoffin.h
@@ -70,7 +70,7 @@ typedef struct
 	u32 xps_check;
 	char *catseg;
 	Bool sigfrag;
-	Bool nocrypt, strtxt, nodata;
+	Bool nocrypt, strtxt, nodata, lightp;
 	u32 mstore_purge, mstore_samples, mstore_size;
 
 	//internal

src/filters/isoffin_load.c
@@ -566,7 +566,7 @@ static void isor_declare_track(ISOMReader *read, ISOMChannel *ch, u32 track, u32
 		if (use_iod)
 			gf_filter_pid_set_property(pid, GF_PROP_PID_ESID, &PROP_UINT(esid));
 
-		if (gf_isom_is_track_in_root_od(read->mov, track)) {
+		if (gf_isom_is_track_in_root_od(read->mov, track) && !read->lightp) {
 			switch (streamtype) {
 			case GF_STREAM_SCENE:
 			case GF_STREAM_OD:
@@ -616,34 +616,40 @@ static void isor_declare_track(ISOMReader *read, ISOMChannel *ch, u32 track, u32
 		ch->streamType = streamtype;
 //		ch->clock_id = ocr_es_id;
 
-		if (has_scalable_layers)
-			gf_filter_pid_set_property(pid, GF_PROP_PID_SCALABLE, &PROP_BOOL(GF_TRUE));
+		if (!read->lightp) {
+			if (has_scalable_layers)
+				gf_filter_pid_set_property(pid, GF_PROP_PID_SCALABLE, &PROP_BOOL(GF_TRUE));
 
-		if (gf_isom_get_reference_count(read->mov, track, GF_ISOM_REF_SABT)>0) {
-			gf_filter_pid_set_property(pid, GF_PROP_PID_TILE_BASE, &PROP_BOOL(GF_TRUE));
-		}
-		else if (gf_isom_get_reference_count(read->mov, track, GF_ISOM_REF_SUBPIC)>0) {
-			gf_filter_pid_set_property(pid, GF_PROP_PID_TILE_BASE, &PROP_BOOL(GF_TRUE));
-		}
+			if (gf_isom_get_reference_count(read->mov, track, GF_ISOM_REF_SABT)>0) {
+				gf_filter_pid_set_property(pid, GF_PROP_PID_TILE_BASE, &PROP_BOOL(GF_TRUE));
+			}
+			else if (gf_isom_get_reference_count(read->mov, track, GF_ISOM_REF_SUBPIC)>0) {
+				gf_filter_pid_set_property(pid, GF_PROP_PID_TILE_BASE, &PROP_BOOL(GF_TRUE));
+			}
 
-		if (srd_w && srd_h) {
-			gf_filter_pid_set_property(pid, GF_PROP_PID_CROP_POS, &PROP_VEC2I_INT(srd_x, srd_y) );
-			if (base_tile_track) {
-				gf_isom_get_visual_info(read->mov, base_tile_track, stsd_idx, &w, &h);
-				if (w && h) {
-					gf_filter_pid_set_property(pid, GF_PROP_PID_ORIG_SIZE, &PROP_VEC2I_INT(w, h) );
+			if (srd_w && srd_h) {
+				gf_filter_pid_set_property(pid, GF_PROP_PID_CROP_POS, &PROP_VEC2I_INT(srd_x, srd_y) );
+				if (base_tile_track) {
+					gf_isom_get_visual_info(read->mov, base_tile_track, stsd_idx, &w, &h);
+					if (w && h) {
+						gf_filter_pid_set_property(pid, GF_PROP_PID_ORIG_SIZE, &PROP_VEC2I_INT(w, h) );
+					}
 				}
 			}
+
+
+			if (codec_id !=GF_CODECID_LHVC)
+				isor_export_ref(read, ch, GF_ISOM_REF_SCAL, "isom:scal");
+			isor_export_ref(read, ch, GF_ISOM_REF_SABT, "isom:sabt");
+			isor_export_ref(read, ch, GF_ISOM_REF_TBAS, "isom:tbas");
+			isor_export_ref(read, ch, GF_ISOM_REF_SUBPIC, "isom:subp");
 		}
 
-
-		if (codec_id !=GF_CODECID_LHVC)
-			isor_export_ref(read, ch, GF_ISOM_REF_SCAL, "isom:scal");
-		isor_export_ref(read, ch, GF_ISOM_REF_SABT, "isom:sabt");
-		isor_export_ref(read, ch, GF_ISOM_REF_TBAS, "isom:tbas");
-		isor_export_ref(read, ch, GF_ISOM_REF_SUBPIC, "isom:subp");
-
-		ch->duration = gf_isom_get_track_duration(read->mov, ch->track);
+		if (read->lightp) {
+			ch->duration = gf_isom_get_track_duration_orig(read->mov, ch->track);
+		} else {
+			ch->duration = gf_isom_get_track_duration(read->mov, ch->track);
+		}
 		if (!ch->duration) {
 			ch->duration = gf_isom_get_duration(read->mov);
 		}
@@ -666,8 +672,7 @@ static void isor_declare_track(ISOMReader *read, ISOMChannel *ch, u32 track, u32
 			if (!ch->has_edit_list && !use_sidx_dur && !ch->ts_offset) {
 				//no specific edit list type but edit present, use the duration in the edit
 				if (gf_isom_get_edits_count(read->mov, ch->track)) {
-					u64 dur = gf_isom_get_track_duration(read->mov, ch->track);
-					gf_filter_pid_set_property(pid, GF_PROP_PID_DURATION, &PROP_FRAC64_INT(dur, read->timescale));
+					gf_filter_pid_set_property(pid, GF_PROP_PID_DURATION, &PROP_FRAC64_INT(ch->duration, read->timescale));
 				} else {
 					u64 dur = gf_isom_get_media_duration(read->mov, ch->track);
 					gf_filter_pid_set_property(pid, GF_PROP_PID_DURATION, &PROP_FRAC64_INT(dur, ch->timescale));
@@ -717,6 +722,7 @@ static void isor_declare_track(ISOMReader *read, ISOMChannel *ch, u32 track, u32
 			gf_filter_pid_set_property(pid, GF_PROP_PID_HAS_SYNC, &PROP_BOOL(GF_FALSE) );
 		}
 
+		if (read->lightp) goto props_done;
 
 		w = gf_isom_get_constant_sample_size(read->mov, track);
 		if (w)
@@ -976,6 +982,8 @@ static void isor_declare_track(ISOMReader *read, ISOMChannel *ch, u32 track, u32
 				}
 			}
 		}
+
+props_done:
 
 		if (read->sigfrag) {
 			u64 start, end;

src/filters/isoffin_read.c
@@ -1705,6 +1705,7 @@ static const GF_FilterArgs ISOFFInArgs[] =
 	"- auto: resolves to `keep` for `smode=splix` (dasher mode), `rem` otherwise"
 	, GF_PROP_UINT, "auto", "auto|keep|rem", GF_FS_ARG_HINT_EXPERT},
 	{ OFFS(nodata), "do not load sample data", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_EXPERT},
+	{ OFFS(lightp), "load minimal set of properties", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_EXPERT},
 	{ OFFS(initseg), "local init segment name when input is a single ISOBMFF segment", GF_PROP_STRING, NULL, NULL, GF_FS_ARG_HINT_EXPERT},
 	{0}
 };

src/filters/isoffin_read_ch.c
@@ -945,7 +945,7 @@ void isor_reader_check_config(ISOMChannel *ch)
 	Bool needs_reset;
 	if (!ch->check_hevc_ps && !ch->check_avc_ps && !ch->check_vvc_ps && !ch->check_mhas_pl) return;
 
-	if (!ch->sample) return;
+	if (!ch->sample || !ch->sample->data) return;
 	ch->xps_mask = 0;
 
 	//we cannot touch the payload if encrypted but not CENC !!

commit f81f8d8136c5be02425145a1bd83e0bb0835d823

fixed bug introduced in 7127d9aa

src/isomedia/isom_read.c
@@ -4039,11 +4039,13 @@ u64 gf_isom_get_media_data_size(GF_ISOFile *movie, u32 trackNumber)
 	if (!tk) return 0;
 	stsz = tk->Media->information->sampleTable->SampleSize;
 	if (!stsz) return 0;
-	if (movie->openMode==GF_ISOM_OPEN_READ)
+	if (!movie->moov->mvex && (movie->openMode==GF_ISOM_OPEN_READ) && stsz->total_size)
 		return stsz->total_size;
 	if (stsz->sampleSize) return stsz->sampleSize*stsz->sampleCount;
 	size = 0;
 	for (i=0; i<stsz->sampleCount; i++) size += stsz->sizes[i];
+	if (!movie->moov->mvex && (movie->openMode==GF_ISOM_OPEN_READ))
+		stsz->total_size = size;
 	return size;
 }
 