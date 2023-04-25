gpac/gpac

commit f8ce5083c2ea216b26015771faccf7c488068be0

fixed wrong truncation of percent-encoded URL in mp4box - cf #2419

src/utils/error.c
@@ -1872,6 +1872,11 @@ Bool gf_parse_lfrac(const char *value, GF_Fraction64 *frac)
 	sep = strchr(value, '.');
 	if (!sep) sep = strchr(value, ',');
 	if (!sep) {
+		len = (u32) strlen(value);
+		for (i=0; i<len; i++) {
+			if ((value[i]<'0') || (value[i]>'9'))
+				return GF_FALSE;
+		}
 		frac->num = atol(value);
 		frac->den = 1;
 		return GF_TRUE;

applications/mp4box/fileimport.c
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2000-2022
+ *			Copyright (c) Telecom ParisTech 2000-2023
  *					All rights reserved
  *
  *  This file is part of GPAC / mp4box application
@@ -1352,9 +1352,11 @@ GF_Err import_file(GF_ISOFile *dest, char *inName, u32 import_flags, GF_Fraction
 
 	/*check duration import (old syntax)*/
 	ext = strrchr(final_name, '%');
-	if (ext) {
-		gf_parse_frac(ext+1, &import.duration);
-		ext[0] = 0;
+	if (ext && gf_sys_old_arch_compat()) {
+		if (gf_parse_frac(ext+1, &import.duration))
+			ext[0] = 0;
+		else
+			ext = NULL;
 	}
 
 	/*select switches for av containers import*/
@@ -3190,7 +3192,8 @@ GF_Err cat_multiple_files(GF_ISOFile *dest, char *fileName, u32 import_flags, GF
 	}
 	strcpy(cat_enum.szRad2, sep+1);
 	sep[0] = 0;
-	sep = strchr(cat_enum.szRad2, '%');
+	sep = NULL;
+	if (gf_sys_old_arch_compat()) sep = strchr(cat_enum.szRad2, '%');
 	if (!sep) sep = strchr(cat_enum.szRad2, '#');
 	if (!sep) sep = gf_url_colon_suffix(cat_enum.szRad2, '=');
 	strcpy(cat_enum.szOpt, "");

commit 2a9436ba354964936b6ebc953afa5f6117c4e267

fixed bug introduced by f8ce5083

src/utils/error.c
@@ -1874,7 +1874,7 @@ Bool gf_parse_lfrac(const char *value, GF_Fraction64 *frac)
 	if (!sep) {
 		len = (u32) strlen(value);
 		for (i=0; i<len; i++) {
-			if ((value[i]<'0') || (value[i]>'9'))
+			if (((value[i]<'0') || (value[i]>'9')) && (value[i]!='-') && (value[i]!='+'))
 				return GF_FALSE;
 		}
 		frac->num = atol(value);