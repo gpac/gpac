gpac/gpac

commit 410262db4592461998cdf64a858808f15c186c3e

allow chapter track creation from PIDs in mp4mx - cf #1692

src/filters/mux_isom.c
@@ -192,6 +192,7 @@ typedef struct
 	u32 dyn_pssh_len;
 
 	Bool sparse_inject;
+	Bool is_chap;
 
 	GF_FilterPacket *dgl_copy;
 	u32 all_stsd_crc;
@@ -392,7 +393,9 @@ typedef struct
 
 	Bool has_def_vid, has_def_aud, has_def_txt;
 
+	//created from chapters prop
 	u32 chap_track_num;
+	Bool has_chap_tracks;
 
 	GF_List *ref_pcks;
 } GF_MP4MuxCtx;
@@ -889,6 +892,20 @@ static void mp4_mux_set_udta(GF_MP4MuxCtx *ctx, TrackWriter *tkw)
 	}
 }
 
+static void update_chap_refs(GF_MP4MuxCtx *ctx)
+{
+	u32 i, j, count = gf_list_count(ctx->tracks);
+	for (i=0; i<count; i++) {
+		TrackWriter *tkw = gf_list_get(ctx->tracks, i);
+		if (!tkw->is_chap) continue;
+		for (j=0; j<count; j++) {
+			TrackWriter *atkw = gf_list_get(ctx->tracks, j);
+			if (atkw->is_chap) continue;
+			if ((atkw->stream_type==GF_STREAM_AUDIO) || (atkw->stream_type==GF_STREAM_VISUAL))
+				gf_isom_set_track_reference(ctx->file, atkw->track_num, GF_ISOM_REF_CHAP, tkw->track_id);
+		}
+	}
+}
 
 static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_true_pid)
 {
@@ -1505,11 +1522,25 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 		}
 
 		//if we have a subtype set for the pid, use it
-		p = gf_filter_pid_get_property(pid, GF_PROP_PID_SUBTYPE);
-		if (p) {
-			mtype = p->value.uint;
+		p = NULL;
+		if ((tkw->stream_type==GF_STREAM_TEXT) && (tkw->codecid==GF_CODECID_TX3G)) {
+			p = gf_filter_pid_get_property(pid, GF_PROP_PID_IS_CHAP);
+
+			mtype = GF_ISOM_MEDIA_TEXT;
 			gf_isom_set_media_type(ctx->file, tkw->track_num, mtype);
+			tkw->is_chap = GF_TRUE;
+			ctx->has_chap_tracks = GF_TRUE;
+			gf_isom_set_track_enabled(ctx->file, tkw->track_num, GF_FALSE);
 		}
+		if (!p) {
+			p = gf_filter_pid_get_property(pid, GF_PROP_PID_SUBTYPE);
+			if (p) {
+				mtype = p->value.uint;
+				gf_isom_set_media_type(ctx->file, tkw->track_num, mtype);
+			}
+		}
+		if (ctx->has_chap_tracks)
+			update_chap_refs(ctx);
 
 		p = gf_filter_pid_get_property(tkw->ipid, GF_PROP_PID_ISOM_HANDLER);
 		if (p && p->value.string) {
@@ -8016,6 +8047,15 @@ GF_FilterRegister MP4MuxRegister = {
 	"- if `Sparse` is absent, empty packet is inserted for unknown text and metadata streams\n"
 	"- if `Sparse` is true, empty packet is inserted for all stream types\n"
 	"- if `Sparse` is false, empty packet is never injected\n"
+	"  \n"
+	"The default media type used for a PID can be overriden using property `StreamSubtype`. \n"
+	"EX -i src.srt:#StreamSubtype=sbtl [-i ...]  -o test.mp4 \n"
+	"This will force the text stream to use `sbtl` handler type instead of default `text` one."
+	"\n"
+	"Subtitle streams may be used as chapters by setting the property `IsChap` on the desired PID.\n"
+	"EX -i src.srt:#IsChap  [-i ...] -o test.mp4 \n"
+	"This will force the text stream to be used as a QT chapter track."
+	"  \n"
 	)
 	.private_size = sizeof(GF_MP4MuxCtx),
 	.args = MP4MuxArgs,

include/gpac/filters.h
@@ -1245,6 +1245,7 @@ enum
 
 	GF_PROP_PID_CHAP_TIMES = GF_4CC('C','H','P','T'),
 	GF_PROP_PID_CHAP_NAMES = GF_4CC('C','H','P','N'),
+	GF_PROP_PID_IS_CHAP = GF_4CC('P','C','H','P'),
 
 	//internal for HLS playlist reference, gives a unique ID identifying media mux, and indicated in packets carrying child playlists
 	GF_PROP_PCK_HLS_REF = GF_4CC('H','P','L','R'),

share/doc/man/gpac-filters.1
@@ -2150,6 +2150,28 @@ Such packets are only injected when a whole in the timeline is detected.
 .br
 - if Sparse is false, empty packet is never injected
 .br
+  
+.br
+The default media type used for a PID can be overriden using property StreamSubtype. 
+.br
+Example
+.br
+-i src.srt:#StreamSubtype=sbtl [-i ...]  -o test.mp4 
+.br
+
+.br
+This will force the text stream to use sbtl handler type instead of default text one.
+.br
+Subtitle streams may be used as chapters by setting the property IsChap on the desired PID.
+.br
+Example
+.br
+-i src.srt:#IsChap  [-i ...] -o test.mp4 
+.br
+
+.br
+This will force the text stream to be used as a QT chapter track.  
+.br
 
 .br
 .SH Options (expert):
@@ -11469,7 +11491,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.3-DEV-rev179-ged4c942f8-master
+build: 2.3-DEV-rev196-g8ddd05544-master
 .br
 Copyright: (c) 2000-2023 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -97,6 +97,11 @@ print stats after execution
 print graph after execution
 .br
 .TP
+.B \-qe
+.br
+enable quick exit (no mem cleanup)
+.br
+.TP
 .B \-k
 .br
 enable keyboard interaction from command line
@@ -3876,6 +3881,11 @@ Chapter start times
 Chapter names
 .br
 .TP
+.B IsChap (PCHP,bool,D )
+.br
+Subtitle PID is chapter (for QT-like chapters)
+.br
+.TP
 .B SkipBegin (PCKS,uint, P)
 .br
 Amount of media to skip from beginning of packet in PID timescale
@@ -4996,7 +5006,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.3-DEV-rev179-ged4c942f8-master
+build: 2.3-DEV-rev196-g8ddd05544-master
 .br
 Copyright: (c) 2000-2023 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2931,7 +2931,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.3-DEV-rev179-ged4c942f8-master
+build: 2.3-DEV-rev196-g8ddd05544-master
 .br
 Copyright: (c) 2000-2023 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

src/filter_core/filter_props.c
@@ -1644,6 +1644,7 @@ GF_BuiltInProperty GF_BuiltInProps [] =
 
 	DEC_PROP_F( GF_PROP_PID_CHAP_TIMES, "ChapTimes", "Chapter start times", GF_PROP_UINT_LIST, GF_PROP_FLAG_GSF_REM),
 	DEC_PROP_F( GF_PROP_PID_CHAP_NAMES, "ChapNames", "Chapter names", GF_PROP_STRING_LIST, GF_PROP_FLAG_GSF_REM),
+	DEC_PROP_F( GF_PROP_PID_IS_CHAP, "IsChap", "Subtitle PID is chapter (for QT-like chapters)", GF_PROP_BOOL, GF_PROP_FLAG_GSF_REM),
 
 	DEC_PROP_F( GF_PROP_PCK_SKIP_BEGIN, "SkipBegin", "Amount of media to skip from beginning of packet in PID timescale", GF_PROP_UINT, GF_PROP_FLAG_PCK),
 	DEC_PROP_F( GF_PROP_PCK_SKIP_PRES, "SkipPres", "Packet and any following with CTS greater than this packet shall not be presented (used by reframer to create edit lists)", GF_PROP_BOOL, GF_PROP_FLAG_PCK),

commit 956122ffebcd7f22f82e536f5e31724f8f097ffa

fixed bug introduced in 410262db

src/filters/mux_isom.c
@@ -1525,12 +1525,15 @@ static GF_Err mp4_mux_setup_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_tr
 		p = NULL;
 		if ((tkw->stream_type==GF_STREAM_TEXT) && (tkw->codecid==GF_CODECID_TX3G)) {
 			p = gf_filter_pid_get_property(pid, GF_PROP_PID_IS_CHAP);
-
-			mtype = GF_ISOM_MEDIA_TEXT;
-			gf_isom_set_media_type(ctx->file, tkw->track_num, mtype);
-			tkw->is_chap = GF_TRUE;
-			ctx->has_chap_tracks = GF_TRUE;
-			gf_isom_set_track_enabled(ctx->file, tkw->track_num, GF_FALSE);
+			if (p && p->value.boolean) {
+				mtype = GF_ISOM_MEDIA_TEXT;
+				gf_isom_set_media_type(ctx->file, tkw->track_num, mtype);
+				tkw->is_chap = GF_TRUE;
+				ctx->has_chap_tracks = GF_TRUE;
+				gf_isom_set_track_enabled(ctx->file, tkw->track_num, GF_FALSE);
+			} else {
+				p = NULL;
+			}
 		}
 		if (!p) {
 			p = gf_filter_pid_get_property(pid, GF_PROP_PID_SUBTYPE);