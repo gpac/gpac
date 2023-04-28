gpac/gpac

commit 63c7a926c05e1bc1c6e0a6245080eb2ac1757b96

updates for GHI
- mkv and TS indexing
- move GHI hints to play event
- fix M2TS timeline init when generating segments independently
- allow ffenc to generate decoder config when no packets
- do not dispatch packets until their dts is known in ffdmx
- fixes for indexing of text streams (mp4, srt, ttxt, vtt, ttml)
- fixes for JIT encription and transcoding
- allow independent generation of manifest, variant playlist (HLS), init segments

src/filters/mux_ts.c
@@ -100,6 +100,7 @@ typedef struct
 	char *name, *provider, *temi;
 	u32 log_freq;
 	s32 subs_sidx;
+	Bool keepts;
 	GF_Fraction cdur;
 
 	//internal
@@ -1238,6 +1239,14 @@ static GF_Err tsmux_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_
 
 		if (sname) gf_m2ts_mux_program_set_name(prog, sname, pname);
 
+		Bool keepts = ctx->keepts;
+		p = gf_filter_pid_get_property(tspid->ipid, GF_PROP_PID_DASH_SPARSE);
+		if (p && p->value.boolean) keepts = GF_TRUE;
+		if (keepts) {
+			//move to NO_TS
+			prog->force_first_pts = GF_FILTER_NO_TS;
+		}
+
 		GF_LOG(GF_LOG_INFO, GF_LOG_CONTAINER, ("[M2TSMux] Setting up program ID %d - send rates: PSI %d ms PCR every %d ms max - PCR offset %d\n", service_id, ctx->pmt_rate, ctx->max_pcr, ctx->pcr_offset));
 	}
 	if (tspid->esi.stream_type != streamtype)
@@ -1375,6 +1384,7 @@ static Bool tsmux_init_buffering(GF_Filter *filter, GF_TSMuxCtx *ctx)
 	u32 not_ready=0;
 	u32 mbuf = ctx->breq*1000;
 	u32 i, count = gf_list_count(ctx->pids);
+
 	for (i=0; i<count; i++) {
 		u32 buf;
 		Bool buf_ok;
@@ -1383,11 +1393,26 @@ static Bool tsmux_init_buffering(GF_Filter *filter, GF_TSMuxCtx *ctx)
 		if (buf_ok && (buf < mbuf) && !gf_filter_pid_has_seen_eos(tspid->ipid)) {
 			if (!tspid->is_sparse) not_ready++;
 		}
+		GF_FilterPacket *pck = gf_filter_pid_get_packet(tspid->ipid);
+		if (!pck) {
+			if (!tspid->is_sparse && !gf_filter_pid_has_seen_eos(tspid->ipid)) not_ready++;
+			continue;
+		}
+		u64 min_ts = gf_filter_pck_get_cts(pck);
+		if (min_ts==GF_FILTER_NO_TS) min_ts = gf_filter_pck_get_dts(pck);
+		if (min_ts==GF_FILTER_NO_TS) continue;
+ 		min_ts += tspid->max_media_skip + tspid->media_delay;
+
+		min_ts = gf_timestamp_rescale(min_ts, gf_filter_pid_get_timescale(tspid->ipid), 90000);
+		if ((tspid->prog->force_first_pts == GF_FILTER_NO_TS) || (tspid->prog->force_first_pts>min_ts)) {
+			tspid->prog->force_first_pts = min_ts;
+		}
 	}
 	if (not_ready) {
 		u32 now = gf_sys_clock();
 		if (!ctx->sync_init_time) {
 			ctx->sync_init_time = now;
+			return GF_FALSE;
 		} else if (now - ctx->sync_init_time < 5000) {
 			return GF_FALSE;
 		} else {
@@ -2035,7 +2060,9 @@ static const GF_FilterArgs TSMuxArgs[] =
 	{ OFFS(log_freq), "delay between logs for realtime mux", GF_PROP_UINT, "500", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(latm), "use LATM AAC encapsulation instead of regular ADTS", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(subs_sidx), "number of subsegments per sidx (negative value disables sidx)", GF_PROP_SINT, "-1", NULL, GF_FS_ARG_HINT_ADVANCED},
+	{ OFFS(keepts), "keep cts/dts untouched and adjust PCR accordingly, used to keep TS unmodified when dashing", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_EXPERT},
 	{ OFFS(cdur), "chunk duration for fragmentation modes", GF_PROP_FRACTION, "-1/1", NULL, GF_FS_ARG_HINT_HIDE},
+
 	{0}
 };
 

include/gpac/download.h
@@ -663,8 +663,10 @@ void gf_dm_del(GF_DownloadManager *dm);
 #endif //GPAC_CONFIG_EMSCRIPTEN
 
 #else
+//! @cond Doxygen_Suppress
 typedef void GF_DownloadManager;
 typedef void GF_DownloadSession;
+//! @endcond
 #endif //GPAC_USE_DOWNLOADER
 
 

include/gpac/filters.h
@@ -1266,6 +1266,8 @@ enum
 	GF_PROP_PID_CLEARKEY_URI = GF_4CC('C','C','K','U'),
 	//internal
 	GF_PROP_PID_CLEARKEY_KID = GF_4CC('C','C','K','I'),
+	//internal, indicate DASH segments are generated in sparse mode ( from context)
+	GF_PROP_PID_DASH_SPARSE = GF_4CC('D','S','S','G'),
 
 
 	//internal property indicating pointer to associated GF_DownloadSession
@@ -1649,9 +1651,8 @@ typedef struct
 	/*! params for GF_FEVT_PLAY and GF_FEVT_SET_SPEED*/
 	Double speed;
 
-	/*! GF_FEVT_PLAY only, indicates playback should start from given packet number - used by dasher when reloading sources*/
+	/*! GF_FEVT_PLAY only, indicates playback should start from given packet number - used by dasher and GHI when reloading sources*/
 	u32 from_pck;
-	u32 to_pck;
 
 	/*! GF_FEVT_PLAY only, set when PLAY event is sent upstream to audio out, indicates HW buffer reset*/
 	u8 hw_buffer_reset;
@@ -1679,6 +1680,18 @@ typedef struct
 	/*! GF_FEVT_PLAY only, indicates  that a demuxer must not forward this event as a source seek because seek has already been done
 	(typically this play request is a segment play and byte range access within the file has already been performed by DASH client)*/
 	u8 no_byterange_forward;
+
+	/*! GF_FEVT_PLAY only, indicates playback should stop from given packet number - used by GHI when loading sources*/
+	u32 to_pck;
+	/*! GF_FEVT_PLAY only, indicates orginal delay applied to dts - used by GHI when loading sources*/
+	u32 orig_delay;
+	/*! GF_FEVT_PLAY only, hint DTS of first sample at ot just after start offset, in media timescale*/
+	u64 hint_first_dts;
+	/*! GF_FEVT_PLAY only, start offset in source - used by GHI when loading sources*/
+	u64 hint_start_offset;
+	/*! GF_FEVT_PLAY only, end offset in source - used by GHI when loading sources*/
+	u64 hint_end_offset;
+
 } GF_FEVT_Play;
 
 /*! Event structure for GF_FEVT_SOURCE_SEEK and GF_FEVT_SOURCE_SWITCH*/
@@ -1698,8 +1711,6 @@ typedef struct
 	u8 skip_cache_expiration;
 	/*! GF_FEVT_SOURCE_SEEK only,  hint block size for source, might not be respected*/
 	u32 hint_block_size;
-	/*! GF_FEVT_SOURCE_SEEK only,  hint tfdt of first sample*/
-	u64 hint_first_tfdt;
 } GF_FEVT_SourceSeek;
 
 /*! Event structure for GF_FEVT_SEGMENT_SIZE*/
@@ -1819,7 +1830,8 @@ typedef struct
 
 	/*! duration of intra (IDR, closed GOP) as expected by the dasher */
 	GF_Fraction intra_period;
-
+	/*! if TRUE codec should only generate DSI (possibly no input frame, and all output packets will be discarded) */
+	Bool gen_dsi_only;
 } GF_FEVT_EncodeHints;
 
 

include/gpac/mpd.h
@@ -293,10 +293,11 @@ typedef struct
 	char *dasher_segment_name;
 	/*! GPAC internal, we store the previous xlink before resolution*/
 	char *previous_xlink_href;
-	/*! GPAC internal, work in index mode*/
+	/*! GPAC internal for index mode*/
 	Bool index_mode;
 	Bool use_split_dur;
-	u32 sample_duration, src_timescale;
+	u32 sample_duration, src_timescale, pid_delay;
+	s32 first_cts_offset;
 } GF_MPD_SegmentList;
 
 /*! SegmentTemplate*/
@@ -921,6 +922,7 @@ typedef struct {
 	Double llhls_part_holdback;
 	//als absolute url flag
 	u32 hls_abs_url;
+	Bool m3u8_use_repid;
 
 	/*! requested segment duration for index mode */
 	u32 segment_duration;
@@ -995,15 +997,28 @@ GF_Err gf_mpd_write(GF_MPD const * const mpd, FILE *out, Bool compact);
 */
 GF_Err gf_mpd_write_file(GF_MPD const * const mpd, const char *file_name);
 
+/*! write mode for M3U8 */
+typedef enum
+{
+	/*! write both master and child playlists */
+	GF_M3U8_WRITE_ALL=0,
+	/*! write master  playlist only */
+	GF_M3U8_WRITE_MASTER,
+	/*! write child playlist only */
+	GF_M3U8_WRITE_CHILD,
+} GF_M3U8WriteMode;
+
 /*! writes an MPD to a m3u8 playlist
 GF_Err gf_mpd_write(GF_MPD const * const mpd, FILE *out, Bool compact);
 \param mpd the target MPD to write
 \param out the target file object
 \param m3u8_name the base m3u8 name to use (needed when generating variant playlist file names)
 \param period the MPD period for that m3u8
+\param mode the write operation desired
 \return error if any
 */
-GF_Err gf_mpd_write_m3u8_master_playlist(GF_MPD const * const mpd, FILE *out, const char* m3u8_name, GF_MPD_Period *period);
+GF_Err gf_mpd_write_m3u8_master_playlist(GF_MPD const * const mpd, FILE *out, const char* m3u8_name, GF_MPD_Period *period, GF_M3U8WriteMode mode);
+
 
 /*! parses an MPD Period and appends it to the MPD period list
 GF_Err gf_mpd_write(GF_MPD const * const mpd, FILE *out, Bool compact);

include/gpac/mpegts.h
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre, Cyril Concolato, Romain Bouqueau
- *			Copyright (c) Telecom ParisTech 2006-2022
+ *			Copyright (c) Telecom ParisTech 2006-2023
  *
  *  This file is part of GPAC / MPEG2-TS sub-project
  *
@@ -863,7 +863,8 @@ typedef struct tag_m2ts_pes
 	u64 before_last_pcr_value;
 	/*! packet number of before last PCR*/
 	u32 before_last_pcr_value_pck_number;
-
+	/*! packet number of preceeding PAT before the PES header*/
+	u32 last_pat_packet_number, before_last_pat_pn, before_last_pes_start_pn;
 
 	/*! PES reframer callback. If NULL, pes processing is skiped
 
@@ -1136,6 +1137,8 @@ struct tag_m2ts_demux
 	/*! current TS packet number*/
 	u32 pck_number;
 
+	/*! TS packet number of last seen packet containing PAT start */
+	u32 last_pat_start_num;
 	/*! remote file handling - created and destroyed by user*/
 	struct __gf_download_session *dnload;
 	/*! channel config path*/
@@ -2000,6 +2003,11 @@ GF_Err gf_m2ts_mux_enable_pcr_only_packets(GF_M2TS_Mux *muxer, Bool enable_force
 \param mux_provider_name the provider name (UTF-8)
 */
 void gf_m2ts_mux_program_set_name(GF_M2TS_Mux_Program *program, const char *program_name, const char *mux_provider_name);
+
+/*! forces keeping DTS CTS untouched (no remap of first packet to 0) - needed when doing stateless dashing
+\param program the target program
+*/
+void gf_m2ts_mux_program_force_keep_ts(GF_M2TS_Mux_Program *program);
 
 /*! enables sending DVB SDT
 \param muxer the target program

share/doc/man/gpac-filters.1
@@ -4838,6 +4838,8 @@ latm (bool, default: false):   use LATM AAC encapsulation instead of regular ADT
 .br
 subs_sidx (sint, default: -1): number of subsegments per sidx (negative value disables sidx)
 .br
+keepts (bool, default: false): keep cts/dts untouched and adjust PCR accordingly, used to keep TS unmodified when dashing
+.br
 
 .br
 .SH dasher
@@ -9017,6 +9019,10 @@ This filter handles pre-indexed content for just-in-time processing of HAS manif
 .br
 
 .br
+Warning: This is work in progress, the format of indexes (binary or XML) may change until finalization of this filter.
+.br
+
+.br
 .SH Generating indexes
 .LP
 .br
@@ -9044,20 +9050,24 @@ Warning: XML indexes should only be used for debug purposes as they can take a s
 .br
 
 .br
+When indexing, the default template used is $Representation$_$Number$. The template is stored in the index and shouldn't be re-specified when generating content.
+.br
+
+.br
 .SH Using indexes
 .LP
 .br
-To generate manifest(s) and init segment(s) from the index, use:
+The index can be used to generate manifest, child variants for HLS, init segments and segments.
 .br
 Example
 .br
-gpac -i index.ghi:genman -o dash/vod.mpd
+gpac -i index.ghi:gm=all -o dash/vod.mpd
 .br
 
 .br
-
+This generates manifest(s) and init segment(s).
 .br
-To generate the 10th segment of representation with ID FOO, use:
+
 .br
 Example
 .br
@@ -9065,7 +9075,41 @@ gpac -i index.ghi:rep=FOO:sn=10 -o dash/vod.mpd
 .br
 
 .br
+This generates the 10th segment of representation with ID FOO.
+.br
+
+.br
 Note: The manifest file(s) and init segment(s) are not written when generating a segment. The manifest target (mpd or m3u8) is only used to setup the filter chain and target output path.
+.br
+
+.br
+Example
+.br
+gpac -i index.ghi:gm=main -o dash/vod.m3u8
+.br
+
+.br
+This generates main manifest only (MPD or master HLS playlist).
+.br
+
+.br
+Example
+.br
+gpac -i index.ghi:gm=child:rep=FOO:out=BAR -o dash/vod.m3u8
+.br
+
+.br
+This generates child manifest for representation FOO in file BAR.
+.br
+
+.br
+Example
+.br
+gpac -i index.ghi:gm=init:rep=FOO:out=BAR2 -o dash/vod.m3u8
+.br
+
+.br
+This generates init segment for representation FOO in file BAR2.
 .br
 
 .br
@@ -9075,7 +9119,7 @@ The filter outputs are PIDs using framed packets marked with segment boundaries 
 .br
 If representation IDs are not assigned during index creation, they default to the 1-based index of the source. You can check them using:
 .br
-EX: gpac -i src.ghi:genman inspect:full
+EX: gpac -i src.ghi inspect:full
 .br
 
 .br
@@ -9084,33 +9128,33 @@ EX: gpac -i src.ghi:genman inspect:full
 .br
 The filter can be used to generate muxed representations, either at manifest generation time or when generating a segment.
 .br
-To generate a manifest muxing representations A with representations V1 and V2 use:
+Example
+.br
+gpac -i index.ghi:mux=A@V1@V2 -o dash/vod.mpd
+.br
+
+.br
+This will generate a manifest muxing representations A with representations V1 and V2.
+.br
+
 .br
 Example
 .br
-gpac -i index.ghi:genman:mux=A-V1-V2 -o dash/vod.mpd
+gpac -i index.ghi:mux=A@V1@V2,T@V1@V2 -o dash/vod.mpd
 .br
 
 .br
-
+This will generate a manifest muxing representations A and T with representations V1 and V2.
 .br
-To generate a manifest muxing representations A and T with representations V1 and V2 use:
+
 .br
 Example
 .br
-gpac -i index.ghi:genman:mux=A-V1-V2,T-V1-V2 -o dash/vod.mpd
+gpac -i index.ghi:rep=V2:sn=5:mux=A@V2 -o dash/vod.mpd
 .br
 
 .br
-
-.br
-To generate the 5th segment containing representations A and V2 use:
-.br
-Example
-.br
-gpac -i index.ghi:rep=V2:sn=5:mux=A-V2 -o dash/vod.mpd
-.br
-
+This will generate the 5th segment containing representations A and V2.
 .br
 
 .br
@@ -9125,26 +9169,44 @@ The filter does not store any state, it is the user respansability to use consis
 .SH Recommandations
 .LP
 .br
-Indexing currently supports as sources fragmented and non-fragmented MP4 inputs.
+Indexing supports fragmented and non-fragmented MP4, MPEG-2 TS and seekable inputs.
 .br
-It is recommended to use fragmented MP4 as input format since this greatly reduces file loading times.
+- It is recommended to use fragmented MP4 as input format since this greatly reduces file loading times.
 .br
-If non-fragmented MP4 are used, it is recommended to use single-track files to decrease the movie box size and speedup parsing.
+- If non-fragmented MP4 are used, it is recommended to use single-track files to decrease the movie box size and speedup parsing.
+.br
+- MPEG-2 TS sources will be slower since they require PES reframing and AU reformating, resulting in more IOs than with mp4.
+.br
+- other seekable sources will likely be slower (seeking, reframing) and are not recommended.
 .br
 
 .br
 .SH Options (expert):
 .LP
 .br
-genman (bool, default: false): manifest generation mode
+gm (enum, default: main):      manifest generation mode
 .br
-force (bool, default: false):  force loading sources in manifest generation
+* none: no manifest generation (implied if sn is not 0)
+.br
+* all: generate all manifests and init segments
+.br
+* main: generate main manifest (MPD or master M3U8)
+.br
+* child: generate child playlist for HLS
+.br
+* init: generate init segment
+.br
+
+.br
+force (bool, default: false):  force loading sources in manifest generation for debug
 .br
 rep (str):                     representation to generate
 .br
-sn (uint):                     segment number to generate
+sn (uint):                     segment number to generate, 0 means init segment
 .br
 mux (strl):                    representation to mux - cf filter help
+.br
+out (str):                     output filename to generate
 .br
 
 .br
@@ -11491,7 +11553,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.3-DEV-rev196-g8ddd05544-master
+build: 2.3-DEV-rev203-ge886e4520-master
 .br
 Copyright: (c) 2000-2023 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/gpac.1
@@ -5006,7 +5006,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.3-DEV-rev196-g8ddd05544-master
+build: 2.3-DEV-rev203-ge886e4520-master
 .br
 Copyright: (c) 2000-2023 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

share/doc/man/mp4box.1
@@ -2931,7 +2931,7 @@ Authors: GPAC developers, see git repo history (-log)
 .br
 For bug reports, feature requests, more information and source code, visit https://github.com/gpac/gpac
 .br
-build: 2.3-DEV-rev196-g8ddd05544-master
+build: 2.3-DEV-rev203-ge886e4520-master
 .br
 Copyright: (c) 2000-2023 Telecom Paris distributed under LGPL v2.1+ - http://gpac.io
 .br

commit d0f838a4225a37d1032a4114bd383d3821666171

fixed bug introduced in 63c7a926

src/filters/mux_ts.c
@@ -1239,11 +1239,10 @@ static GF_Err tsmux_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_
 
 		if (sname) gf_m2ts_mux_program_set_name(prog, sname, pname);
 
-		Bool keepts = ctx->keepts;
 		p = gf_filter_pid_get_property(tspid->ipid, GF_PROP_PID_DASH_SPARSE);
-		if (p && p->value.boolean) keepts = GF_TRUE;
-		if (keepts) {
-			//move to NO_TS
+		if (p && p->value.boolean) ctx->keepts = GF_TRUE;
+		//move to NO_TS, adjust min when checking buffering
+		if (ctx->keepts) {
 			prog->force_first_pts = GF_FILTER_NO_TS;
 		}
 
@@ -1393,6 +1392,8 @@ static Bool tsmux_init_buffering(GF_Filter *filter, GF_TSMuxCtx *ctx)
 		if (buf_ok && (buf < mbuf) && !gf_filter_pid_has_seen_eos(tspid->ipid)) {
 			if (!tspid->is_sparse) not_ready++;
 		}
+		if (!ctx->keepts) continue;
+
 		GF_FilterPacket *pck = gf_filter_pid_get_packet(tspid->ipid);
 		if (!pck) {
 			if (!tspid->is_sparse && !gf_filter_pid_has_seen_eos(tspid->ipid)) not_ready++;