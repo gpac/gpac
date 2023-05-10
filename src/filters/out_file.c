gpac/gpac

commit 6cf3f338ec4eadd9153f5c5c38711dac178a578b

avoid double open of files in write mode when fd are used

src/filters/out_file.c
@@ -195,11 +195,9 @@ static GF_Err fileout_open_close(GF_FileOutCtx *ctx, const char *filename, const
 		if (!ctx->no_fd && !is_gfio && !append && !gf_opts_get_bool("core", "no-fd")
 			&& (!ctx->original_url || strncmp(ctx->original_url, "gfio://", 7))
 		) {
-			//make sure output dir exists - we create the file as well due to some perm issues with the created file with O_CREAT on osx
-			ctx->file = gf_fopen(szFinalName, "wb");
-			gf_fclose(ctx->file);
-			ctx->file = NULL;
-			ctx->fd = open(szFinalName, O_RDWR);
+			//make sure output dir exists
+			gf_fopen(szFinalName, "mkdir");
+			ctx->fd = open(szFinalName, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 		} else
 #endif
 			ctx->file = gf_fopen_ex(szFinalName, ctx->original_url, append ? "a+b" : "w+b", GF_FALSE);

src/isomedia/data_map.c
@@ -449,11 +449,9 @@ GF_DataMap *gf_isom_fdm_new(const char *sPath, u8 mode)
 			}
 #ifdef GPAC_HAS_FD
 			if (strncmp(sPath, "gfio://", 7) && !gf_opts_get_bool("core", "no-fd")) {
-				//make sure output dir exists - we create the file as well due to some perm issues with the created file with O_CREAT on osx
-				tmp->stream = gf_fopen(sPath, "wb");
-				gf_fclose(tmp->stream);
-				tmp->stream = NULL;
-				tmp->fd = open(sPath, O_RDWR);
+				//make sure output dir exists
+				gf_fopen(sPath, "mkdir");
+				tmp->fd = open(sPath, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 				if (tmp->fd<0) break;
 				tmp->bs = gf_bs_from_fd(tmp->fd, GF_BITSTREAM_WRITE);
 			} else

include/gpac/tools.h
@@ -1555,7 +1555,7 @@ FILE *gf_fopen(const char *file_name, const char *mode);
 Opens a file, potentially using file IO if the parent URL is a File IO wrapper
 \param file_name same as fopen
 \param parent_url URL of parent file. If not a file io wrapper (gfio://), the function is equivalent to gf_fopen
-\param mode same as fopen
+\param mode same as fopen - value "mkdir" checks if parent dir(s) need to be created, create them if needed and returns NULL (no file open)
 \param no_warn if GF_TRUE, do not throw log message if failure
 \return stream handle of the file object
 \note You only need to call this function if you're suspecting the file to be a large one (usually only media files), otherwise use regular stdio.

src/utils/os_file.c
@@ -1474,8 +1474,13 @@ FILE *gf_fopen_ex(const char *file_name, const char *parent_name, const char *mo
 {
 	FILE *res = NULL;
 	u32 gfio_type = 0;
+	Bool is_mkdir = GF_FALSE;
 
-	if (!file_name) return NULL;
+	if (!file_name || !mode) return NULL;
+	if (!strcmp(mode, "mkdir")) {
+		is_mkdir = GF_TRUE;
+		mode = "w";
+	}
 
 	if (!strncmp(file_name, "gmem://", 7)) {
 		GF_FileIO *new_gfio;
@@ -1552,6 +1557,7 @@ FILE *gf_fopen_ex(const char *file_name, const char *parent_name, const char *mo
 			sep = n_sep;
 		}
 		gf_free(fname);
+		if (is_mkdir) return NULL;
 	}
 
 #ifdef GPAC_CONFIG_EMSCRIPTEN

commit 270665cb39f15e9a517926db8dec823acb8e654d

fixed bug introduced in 6cf3f338

src/filters/out_file.c
@@ -208,7 +208,7 @@ static GF_Err fileout_open_close(GF_FileOutCtx *ctx, const char *filename, const
 		) {
 			//make sure output dir exists
 			gf_fopen(szFinalName, "mkdir");
-			ctx->fd = open(szFinalName, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+			ctx->fd = open(szFinalName, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH );
 		} else
 #endif
 			ctx->file = gf_fopen_ex(szFinalName, ctx->original_url, append ? "a+b" : "w+b", GF_FALSE);

src/isomedia/data_map.c
@@ -451,7 +451,7 @@ GF_DataMap *gf_isom_fdm_new(const char *sPath, u8 mode)
 			if (strncmp(sPath, "gfio://", 7) && !gf_opts_get_bool("core", "no-fd")) {
 				//make sure output dir exists
 				gf_fopen(sPath, "mkdir");
-				tmp->fd = open(sPath, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+				tmp->fd = open(sPath, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH );
 				if (tmp->fd<0) break;
 				tmp->bs = gf_bs_from_fd(tmp->fd, GF_BITSTREAM_WRITE);
 			} else