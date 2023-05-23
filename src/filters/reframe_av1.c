a22b23293d111427eed87a8748a56420c2183cf9
jeanlf <jeanlf@gpac.io>
2023-05-23 14:34:55 +0200

    fixed bug in rfav1 pid props

--- src/filters/reframe_av1.c
+++ src/filters/reframe_av1.c
@@ -649,6 +649,7 @@ static void av1dmx_check_pid(GF_Filter *filter, GF_AV1DmxCtx *ctx)
 	gf_filter_pid_copy_properties(ctx->opid, ctx->ipid);
 	ctx->copy_props = GF_FALSE;
 
+	gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_UNFRAMED, NULL);
 	gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_STREAM_TYPE, & PROP_UINT(GF_STREAM_VISUAL));
 
 	gf_filter_pid_set_property(ctx->opid, GF_PROP_PID_CODECID, & PROP_UINT(ctx->codecid));
