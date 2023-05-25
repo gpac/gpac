gpac/gpac

commit 4ae3c6128f35e4bc1fa28de7ef743fc38831565c

Cleanup of raw audio support - cf #2470 - added big-endian formats for s24, s32, flt and dbl - completed endianness support in mixer - added qt v7 lpcm support - added big-endian support for ipcm and fpcm (mp4 raw pcm) - added missing wave and enda boxes for qtv1 pcm - MP4Box -info now shows correct info for all pcm muxing types

src/isomedia/sample_descs.c
@@ -1803,14 +1803,65 @@ GF_Err gf_isom_get_pcm_config(GF_ISOFile *movie, u32 trackNumber, u32 descriptio
 
 	aent = (GF_AudioSampleEntryBox *)gf_list_get(trak->Media->information->sampleTable->SampleDescription->child_boxes, descriptionIndex-1);
 	if (!aent) return GF_BAD_PARAM;
-	if ((aent->type != GF_ISOM_BOX_TYPE_IPCM) && (aent->type != GF_ISOM_BOX_TYPE_FPCM))
-		return GF_BAD_PARAM;
+	if ((aent->type != GF_ISOM_BOX_TYPE_IPCM) && (aent->type != GF_ISOM_BOX_TYPE_FPCM)) {
+		GF_Box *b = gf_isom_box_find_child(aent->child_boxes, GF_QT_BOX_TYPE_WAVE);
+		if (!b) return GF_BAD_PARAM;
+		GF_ChromaInfoBox *enda = (GF_ChromaInfoBox *)gf_isom_box_find_child(b->child_boxes, GF_QT_BOX_TYPE_ENDA);
+		u32 bits=0;
+		Bool is_le = (enda && enda->chroma) ? GF_TRUE : GF_FALSE;
+		switch (aent->type) {
+		case GF_QT_SUBTYPE_FL32: bits = 32; break;
+		case GF_QT_SUBTYPE_FL64: bits = 64; break;
+		case GF_QT_SUBTYPE_TWOS: bits = 16; is_le = GF_FALSE; break;
+		case GF_QT_SUBTYPE_SOWT: bits = 16; is_le = GF_TRUE; break;
+		case GF_QT_SUBTYPE_IN24: bits = 24; break;
+		case GF_QT_SUBTYPE_IN32: bits = 32; break;
+		default:
+			return GF_BAD_PARAM;
+		}
+		if (pcm_size) *pcm_size = bits;
+		if (flags) *flags = is_le ? 1 : 0;
+		return GF_OK;
+	}
 
 	pcmC = (GF_PCMConfigBox *) gf_isom_box_find_child(aent->child_boxes, GF_ISOM_BOX_TYPE_PCMC);
 	if (!pcmC) return GF_NON_COMPLIANT_BITSTREAM;
 
 	if (flags) *flags = pcmC->format_flags;
 	if (pcm_size) *pcm_size = pcmC->PCM_sample_size;
+	return GF_OK;
+}
+
+GF_EXPORT
+GF_Err gf_isom_get_lpcm_config(GF_ISOFile *movie, u32 trackNumber, u32 descriptionIndex, Double *sample_rate, u32 *nb_channels, u32 *flags, u32 *pcm_size)
+{
+	GF_AudioSampleEntryBox *aent;
+	GF_TrackBox *trak;
+	GF_BitStream *bs;
+	trak = gf_isom_get_track_from_file(movie, trackNumber);
+	if (!trak || !descriptionIndex) return GF_BAD_PARAM;
+
+	aent = (GF_AudioSampleEntryBox *)gf_list_get(trak->Media->information->sampleTable->SampleDescription->child_boxes, descriptionIndex-1);
+	if (!aent) return GF_BAD_PARAM;
+	if (aent->type != GF_QT_SUBTYPE_LPCM)
+		return GF_BAD_PARAM;
+	if (aent->version != 2)
+		return GF_BAD_PARAM;
+
+	bs = gf_bs_new(aent->extensions, 36, GF_BITSTREAM_READ);
+	/*offset*/gf_bs_read_u32(bs);
+	Double sr = gf_bs_read_double(bs);
+	if (sample_rate) *sample_rate = sr;
+	u32 nb_ch = gf_bs_read_u32(bs);
+	if (nb_channels) *nb_channels = nb_ch;
+	/*res*/gf_bs_read_u32(bs);
+	u32 constBitsPerChannel = gf_bs_read_u32(bs);
+	if (pcm_size) *pcm_size=constBitsPerChannel;
+	u32 PCM_flags = gf_bs_read_u32(bs);
+	if (flags) *flags = PCM_flags;
+	/*constBytesPerAudioPacket*/gf_bs_read_u32(bs);
+	/*constLPCMFramesPerAudioPacket*/gf_bs_read_u32(bs);
+	gf_bs_del(bs);
 	return GF_OK;
 }
 

applications/mp4box/filedump.c
@@ -3501,14 +3501,28 @@ static void DumpStsdInfo(GF_ISOFile *file, u32 trackNum, Bool full_dump, Bool du
 			fprintf(stderr, "\tInvalid TrueHD audio config\n");
 		}
 		fprintf(stderr, "\tTrueHD Audio stream - Sample Rate %u - channels %u - format %u peak rate %u\n", sr, nb_ch, fmt, prate);
+	} else if (msub_type==GF_QT_SUBTYPE_LPCM) {
+		Double pcm_sr;
+		u32 pcm_flags, pcm_size;
+		if (gf_isom_get_lpcm_config(file, trackNum, stsd_idx, &pcm_sr, &nb_ch, &pcm_flags, &pcm_size) == GF_OK) {
+			fprintf(stderr, "\tPCM Audio - Sample Rate %f channels %u %u-bits %s %s-endian\n", pcm_sr, nb_ch, pcm_size, (pcm_flags & 1) ? "float" : "int", (pcm_flags & (1<<1)) ? "big" : "little");
+		} else {
+			fprintf(stderr, "\tInvalid LPCM audio config\n");
+		}
 	} else if (codecid) {
 		if (gf_isom_is_video_handler_type(mtype) ) {
 			u32 w, h;
 			gf_isom_get_visual_info(file, trackNum, stsd_idx, &w, &h);
 			fprintf(stderr, "\t%s - Resolution %d x %d\n", gf_codecid_name(codecid), w, h);
 		} else if (mtype==GF_ISOM_MEDIA_AUDIO) {
+			u32 size, flags;
 			gf_isom_get_audio_info(file, trackNum, stsd_idx, &sr, &nb_ch, NULL);
-			fprintf(stderr, "\t%s - Sample Rate %d - %d channel(s)\n", gf_codecid_name(codecid), sr, nb_ch);
+			if (gf_isom_get_pcm_config(file, trackNum, stsd_idx, &flags, &size)==GF_OK) {
+				fprintf(stderr, "\tPCM Audio Sample Rate %d %d channel(s) %u-bits %s-endian", sr, nb_ch, size, (flags&1) ? "little" : "big");
+			} else {
+				fprintf(stderr, "\t%s - Sample Rate %d - %d channel(s)", gf_codecid_name(codecid), sr, nb_ch);
+			}
+			fprintf(stderr, "\n");
 		} else {
 			fprintf(stderr, "\t%s\n", gf_codecid_name(codecid) );
 		}

include/gpac/constants.h
@@ -837,12 +837,20 @@ typedef enum
 	GF_AUDIO_FMT_S16_BE,
 	/*! sample = signed integer, interleaved channels*/
 	GF_AUDIO_FMT_S32,
+	/*! sample = signed integer big-endian, interleaved channels*/
+	GF_AUDIO_FMT_S32_BE,
 	/*! sample = 1 float, interleaved channels*/
 	GF_AUDIO_FMT_FLT,
+	/*! sample = 1 float bytes in big endian, interleaved channels*/
+	GF_AUDIO_FMT_FLT_BE,
 	/*! sample = 1 double, interleaved channels*/
 	GF_AUDIO_FMT_DBL,
+	/*! sample = 1 double bytes in big-endian order, interleaved channels*/
+	GF_AUDIO_FMT_DBL_BE,
 	/*! sample = signed integer, interleaved channels*/
 	GF_AUDIO_FMT_S24,
+	/*! sample = signed integer gig-endian, interleaved channels*/
+	GF_AUDIO_FMT_S24_BE,
 	/*! not a format, indicates the value of last packed format*/
 	GF_AUDIO_FMT_LAST_PACKED,
 	/*! sample = unsigned byte, planar channels*/

include/gpac/isomedia.h
@@ -362,6 +362,7 @@ enum
 	GF_QT_SUBTYPE_RGBA = GF_4CC('R','G','B','A'),
 	GF_QT_SUBTYPE_ABGR = GF_4CC('A','B','G','R'),
 	GF_QT_SUBTYPE_ALAC =  GF_4CC('a','l','a','c'),
+	GF_QT_SUBTYPE_LPCM =  GF_4CC('l','p','c','m'),
 	GF_ISOM_SUBTYPE_FFV1		= GF_4CC( 'F', 'F', 'V', '1' ),
 
 	GF_ISOM_ITEM_TYPE_AUXI 	= GF_4CC('a', 'u', 'x', 'i'),
@@ -3116,6 +3117,8 @@ typedef struct
 	u16 bits_per_sample;
 	/*! indicates if QTFF signaling should be used, audio codecs only*/
 	Bool is_qtff;
+	/*! for lpcm only, indicates format flags*/
+	u32 lpcm_flags;
 
 	/*optional, sample description specific configuration*/
 	u8 *extension_buf;
@@ -3913,12 +3916,23 @@ GF_Err gf_isom_get_tmcd_config(GF_ISOFile *isom_file, u32 trackNumber, u32 sampl
 \param isom_file the target ISO file
 \param trackNumber the target track
 \param sampleDescriptionIndex the target sample description index
-\param flags set to the pcm config flags (0: big endian, 1: little endian)
-\param pcm_size  set to PCM sample size (per channel, 16, 24, 32, 64
+\param flags set to the pcm config flags (0: big endian, 1: little endian), may be NULL
+\param pcm_size  set to PCM sample size (per channel, 16, 24, 32, 64, may be NULL
 \return error if any
 */
 GF_Err gf_isom_get_pcm_config(GF_ISOFile *isom_file, u32 trackNumber, u32 sampleDescriptionIndex, u32 *flags, u32 *pcm_size);
 
+/*! gets information of a raw PCM  sample description, QT style (lpcm codecid)
+\param isom_file the target ISO file
+\param trackNumber the target track
+\param sampleDescriptionIndex the target sample description index
+\param sample_rate set to the pcm sample rate, may be NULL
+\param nb_channels set to the pcm channel count, may be NULL
+\param flags set to the pcm config flags (1: float, 2: big endian, 4: signed, other flags cf QTFF), may be NULL
+\param pcm_size  set to PCM sample size (per channel, 16, 24, 32, 64, may be NULL
+\return error if any
+*/
+GF_Err gf_isom_get_lpcm_config(GF_ISOFile *movie, u32 trackNumber, u32 descriptionIndex, Double *sample_rate, u32 *nb_channels, u32 *flags, u32 *pcm_size);
 
 
 #ifndef GPAC_DISABLE_ISOM_WRITE

src/compositor/audio_mixer.c
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2000-2021
+ *			Copyright (c) Telecom ParisTech 2000-2023
  *					All rights reserved
  *
  *  This file is part of GPAC / Scene Compositor sub-project
@@ -83,6 +83,10 @@ struct __audiomix
 	s32 *output;
 	u32 output_size;
 };
+
+#define swap_16(x) (( (x) << 8 & 0xff00) | ((x) >> 8 & 0x00ff))
+#define swap_32(x) (swap_16(x) << 16 | swap_16((x) >> 16))
+#define swap_64(x) ( swap_32(x) << 32 | swap_32((x) >> 32))
 
 #define GF_S24_MAX	8388607
 #define GF_S24_MIN	-8388608
@@ -306,47 +310,45 @@ static GFINLINE s32 make_s24_int(u8 *ptr)
 	return val;
 }
 
+static GFINLINE s32 make_s24_be_int(u8 *ptr)
+{
+	s32 val = (s8) ptr[0];
+	val *= 256;
+	val |= ptr[1];
+	val *= 256;
+	val |= ptr[2];
+	return val;
+}
 #define MIX_S16_SCALE	65535
 #define MIX_S24_SCALE	255
 #define MIX_U8_SCALE	16777215
 
 s32 input_sample_s32(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
+	u32 psamp = * ((u32*) (data + 4 * (sample_offset*nb_ch + channel)));
 #ifdef GPAC_BIG_ENDIAN
-	u8 *src = data + 2*(sample_offset*nb_ch + channel);
-	s32 res = src[4];
-	res<<=8;
-	res |= src[3];
-	res<<=8;
-	res |= src[2];
-	res<<=8;
-	res |= src[1];
-	res<<=8;
-	res |= src[0];
-	return res;
-#else
-	s32 *src = (s32 *)data;
-	return src[sample_offset*nb_ch + channel];
+	psamp = swap_32(psamp);
 #endif
+	s32 res = *(s32 *)&psamp;
+	return res;
 }
 s32 input_sample_s32p(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
+	u32 psamp = * ((u32*) (data + 4 * (sample_offset + planar_stride*channel/4)));
 #ifdef GPAC_BIG_ENDIAN
-	u8 *src = data + 4 * (sample_offset + planar_stride*channel/4);
-	s32 res = src[4];
-	res<<=8;
-	res |= src[3];
-	res<<=8;
-	res |= src[2];
-	res<<=8;
-	res |= src[1];
-	res<<=8;
-	res |= src[0];
-	return res;
-#else
-	s32 *src = (s32 *)data;
-	return src[sample_offset + planar_stride*channel/4];
+	psamp = swap_32(psamp);
 #endif
+	s32 res = *(s32 *)&psamp;
+	return res;
+}
+s32 input_sample_s32_be(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
+{
+	u32 psamp = * ((u32*) (data + 4 * (sample_offset*nb_ch + channel)));
+#ifndef GPAC_BIG_ENDIAN
+	psamp = swap_32(psamp);
+#endif
+	s32 res = *(s32 *)&psamp;
+	return res;
 }
 s32 input_sample_s24(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
@@ -356,6 +358,11 @@ s32 input_sample_s24p(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 p
 {
 	return make_s24_int(&data[sample_offset*3 + channel*planar_stride]) * MIX_S24_SCALE;
 }
+s32 input_sample_s24_be(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
+{
+	return make_s24_be_int(&data[sample_offset*nb_ch*3 + 3*channel]) * MIX_S24_SCALE;
+}
+
 
 #define TRUNC_FLT_DBL(_a) \
 	if (_a<-1.0) return GF_INT_MIN;\
@@ -364,70 +371,86 @@ s32 input_sample_s24p(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 p
 
 s32 input_sample_flt(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
-	Float *src = (Float *)data;
-	Float samp = src[sample_offset*nb_ch + channel];
+	u32 psamp = *((u32 *) (data + 4*(sample_offset*nb_ch + channel)));
+#ifdef GPAC_BIG_ENDIAN
+	psamp = swap_32(psamp);
+#endif
+	Float samp = *(Float *)&psamp;
 	TRUNC_FLT_DBL(samp);
 }
 s32 input_sample_fltp(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
-	Float *src = (Float *)data;
-	Float samp = src[sample_offset + planar_stride*channel/4];
+	u32 psamp = * ((u32 *) (data + 4 * (sample_offset + planar_stride*channel/4)));
+#ifdef GPAC_BIG_ENDIAN
+	psamp = swap_32(psamp);
+#endif
+	Float samp = *(Float *)&psamp;
+	TRUNC_FLT_DBL(samp);
+}
+s32 input_sample_flt_be(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
+{
+	u32 psamp = *((u32 *) (data + 4*(sample_offset*nb_ch + channel)));
+#ifndef GPAC_BIG_ENDIAN
+	psamp = swap_32(psamp);
+#endif
+	Float samp = *(Float *)&psamp;
 	TRUNC_FLT_DBL(samp);
 }
 s32 input_sample_dbl(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
-	Double *src = (Double *)data;
-	Double samp = src[sample_offset*nb_ch + channel];
+	u64 psamp = *((u64 *) (data + 8*(sample_offset*nb_ch + channel)));
+#ifdef GPAC_BIG_ENDIAN
+	psamp = swap_64(psamp);
+#endif
+	Double samp = *(Double *)&psamp;
 	TRUNC_FLT_DBL(samp);
 }
 s32 input_sample_dblp(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
-	Double *src = (Double *)data;
-	Double samp = src[sample_offset + planar_stride * channel / 8];
+	u64 psamp = *((u64 *) (data + 8*(sample_offset + planar_stride * channel / 8)));
+#ifdef GPAC_BIG_ENDIAN
+	psamp = swap_64(psamp);
+#endif
+	Double samp = *(Double *)&psamp;
 	TRUNC_FLT_DBL(samp);
 }
+s32 input_sample_dbl_be(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
+{
+	u64 psamp = *((u64 *) (data + 8*(sample_offset*nb_ch + channel)));
+#ifndef GPAC_BIG_ENDIAN
+	psamp = swap_64(psamp);
+#endif
+	Double samp = *(Double *)&psamp;
+	TRUNC_FLT_DBL(samp);
+}
+
 s32 input_sample_s16(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
+	u16 psamp = * ((u16 *) (data + 2*(sample_offset*nb_ch + channel)));
 #ifdef GPAC_BIG_ENDIAN
-	u8 *src = data + 2*(sample_offset*nb_ch + channel);
-	s16 res = src[1];
-	res<<=8;
-	res |= src[0];
-	return ((s32)res) * MIX_S16_SCALE;
-#else
-	s16 *src = (s16 *)data;
-	s32 res = src[sample_offset*nb_ch + channel];
-	return res * MIX_S16_SCALE;
+	psamp = swap_16(psamp);
 #endif
+	s32 res = * (s16*)&psamp;
+	return res * MIX_S16_SCALE;
 }
 s32 input_sample_s16_be(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
-#ifdef GPAC_BIG_ENDIAN
-	s16 *src = (s16 *)data;
-	s32 res = src[sample_offset*nb_ch + channel];
-	return res * MIX_S16_SCALE;
-#else
-	u8 *src = data + 2*(sample_offset*nb_ch + channel);
-	s16 res = src[0];
-	res<<=8;
-	res |= src[1];
-	return ((s32)res) * MIX_S16_SCALE;
+	u16 psamp = * ((u16 *) (data + 2*(sample_offset*nb_ch + channel)));
+#ifndef GPAC_BIG_ENDIAN
+	psamp = swap_16(psamp);
 #endif
+	s32 res = * (s16*)&psamp;
+	return res * MIX_S16_SCALE;
 }
 
 s32 input_sample_s16p(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
+	u16 psamp = * ((u16*) (data + 2* (sample_offset + planar_stride*channel / 2)));
 #ifdef GPAC_BIG_ENDIAN
-	u8 *src = data + 2 * (sample_offset + planar_stride*channel / 2);
-	s16 res = src[1];
-	res<<=8;
-	res |= src[0];
-	return ((s32)res) * MIX_S16_SCALE;
-#else
-	s16 *src = (s16 *)data;
-	s32 res = src[sample_offset + planar_stride*channel / 2];
-	return res * MIX_S16_SCALE;
+	psamp = swap_16(psamp);
 #endif
+	s32 res = *(s16 *) &psamp;
+	return res * MIX_S16_SCALE;
 }
 s32 input_sample_u8(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
 {
@@ -440,6 +463,10 @@ s32 input_sample_u8p(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 pl
 	s32 res = data[sample_offset + planar_stride*channel];
 	res -= 128;
 	return res * MIX_U8_SCALE;
+}
+s32 input_sample_null(u8 *data, u32 nb_ch, u32 sample_offset, u32 channel, u32 planar_stride)
+{
+	return 0;
 }
 
 static void gf_am_configure_source(MixerInput *in)
@@ -457,6 +484,9 @@ static void gf_am_configure_source(MixerInput *in)
 	case GF_AUDIO_FMT_S24:
 		in->get_sample = input_sample_s24;
 		break;
+	case GF_AUDIO_FMT_S24_BE:
+		in->get_sample = input_sample_s24_be;
+		break;
 	case GF_AUDIO_FMT_S24P:
 		in->get_sample = input_sample_s24p;
 		break;
@@ -466,17 +496,26 @@ static void gf_am_configure_source(MixerInput *in)
 	case GF_AUDIO_FMT_FLTP:
 		in->get_sample = input_sample_fltp;
 		break;
+	case GF_AUDIO_FMT_FLT_BE:
+		in->get_sample = input_sample_flt_be;
+		break;
 	case GF_AUDIO_FMT_DBL:
 		in->get_sample = input_sample_dbl;
 		break;
 	case GF_AUDIO_FMT_DBLP:
 		in->get_sample = input_sample_dblp;
 		break;
+	case GF_AUDIO_FMT_DBL_BE:
+		in->get_sample = input_sample_dbl_be;
+		break;
 	case GF_AUDIO_FMT_S16:
 		in->get_sample = input_sample_s16;
 		break;
 	case GF_AUDIO_FMT_S16_BE:
 		in->get_sample = input_sample_s16_be;
+		break;
+	case GF_AUDIO_FMT_S32_BE:
+		in->get_sample = input_sample_s32_be;
 		break;
 	case GF_AUDIO_FMT_S16P:
 		in->get_sample = input_sample_s16p;
@@ -486,6 +525,10 @@ static void gf_am_configure_source(MixerInput *in)
 		break;
 	case GF_AUDIO_FMT_U8P:
 		in->get_sample = input_sample_u8p;
+		break;
+	default:
+		GF_LOG(GF_LOG_ERROR, GF_LOG_AUDIO, ("[AudioMixer] Unsupported input format %s\n", gf_audio_fmt_sname(in->src->afmt) ));
+		in->get_sample = input_sample_null;
 		break;
 	}
 }
@@ -897,12 +940,6 @@ static void gf_mixer_fetch_input(GF_AudioMixer *am, MixerInput *in, u32 audio_de
 	in->in_bytes_used += 1;
 }
 
-#ifdef GPAC_BIG_ENDIAN
-#endif
-
-#define swap_16(x) (( (x) << 8 & 0xff00) | ((x) >> 8 & 0x00ff))
-#define swap_32(x) (swap_16(x) << 16 | swap_16((x) >> 16))
-
 GF_EXPORT
 u32 gf_mixer_get_output(GF_AudioMixer *am, void *buffer, u32 buffer_size, u32 delay)
 {
@@ -1134,8 +1171,6 @@ do_mix:
 		return 0;
 	}
 
-	//TODO big-endian support (output is assumed to be little endian PCM)
-
 	//we do not re-normalize based on the number of input, this is the author's responsability
 	out_mix = am->output;
 	if (am->afmt == GF_AUDIO_FMT_S32) {
@@ -1169,53 +1204,136 @@ do_mix:
 			}
 		}
 	}
+	if (am->afmt == GF_AUDIO_FMT_S32_BE) {
+		s32 *out_s32 = (s32 *)buffer;
+		for (i = 0; i < nb_written; i++) {
+			for (j = 0; j < am->nb_channels; j++) {
+				s32 samp = (*out_mix);
+#ifndef GPAC_BIG_ENDIAN
+				(*out_s32) = swap_32(samp);
+#else
+				(*out_s32) = samp;
+#endif
+				out_s32 += 1;
+				out_mix += 1;
+			}
+		}
+	}
 	else if (am->afmt == GF_AUDIO_FMT_FLT) {
 		Float *out_flt = (Float *)buffer;
+#ifdef GPAC_BIG_ENDIAN
+		s32 *out_u32 = (s32 *)buffer;
+#endif
 		for (i = 0; i < nb_written; i++) {
 			for (j = 0; j < am->nb_channels; j++) {
 				s32 samp = (*out_mix);
 				(*out_flt) = ((Float)samp) / GF_INT_MAX;
 				out_flt += 1;
+
+#ifdef GPAC_BIG_ENDIAN
+				(*out_u32) = swap_32(*out_u32);
+				out_u32 += 1;
+#endif
+				out_mix += 1;
+			}
+		}
+	}
+	else if (am->afmt == GF_AUDIO_FMT_FLT_BE) {
+		Float *out_flt = (Float *)buffer;
+#ifndef GPAC_BIG_ENDIAN
+		u32 *out_u32 = (s32 *)buffer;
+#endif
+		for (i = 0; i < nb_written; i++) {
+			for (j = 0; j < am->nb_channels; j++) {
+				s32 samp = (*out_mix);
+				(*out_flt) = ((Float)samp) / GF_INT_MAX;
+				out_flt += 1;
+
+#ifndef GPAC_BIG_ENDIAN
+				(*out_u32) = swap_32(*out_u32);
+				out_u32 += 1;
+#endif
 				out_mix += 1;
 			}
 		}
 	}
 	else if (am->afmt == GF_AUDIO_FMT_FLTP) {
 		Float *out_flt = (Float *)buffer;
+#ifdef GPAC_BIG_ENDIAN
+		u32 *out_s32 = (u32 *)buffer;
+#endif
 		for (j = 0; j < am->nb_channels; j++) {
 			out_mix = am->output + j;
 			for (i = 0; i < nb_written; i++) {
 				s32 samp = (*out_mix);
 				(*out_flt) = ((Float)samp) / GF_INT_MAX;
 				out_flt += 1;
+
+#ifdef GPAC_BIG_ENDIAN
+				(*out_u32) = swap_32(*out_u32);
+				out_u32 += 1;
+#endif
 				out_mix += am->nb_channels;
 			}
 		}
 	}
 	else if (am->afmt == GF_AUDIO_FMT_DBL) {
 		Double *out_dbl = (Double *)buffer;
+#ifdef GPAC_BIG_ENDIAN
+		u64 *out_u64 = (u64 *)buffer;
+#endif
 		for (i = 0; i < nb_written; i++) {
 			for (j = 0; j < am->nb_channels; j++) {
 				s32 samp = (*out_mix);
 				(*out_dbl) = ((Double)samp) / GF_INT_MAX;
 				out_dbl += 1;
+
+#ifdef GPAC_BIG_ENDIAN
+				(*out_u64) = swap_64(*out_u64);
+				out_u64 += 1;
+#endif
 				out_mix += 1;
 			}
 		}
 	}
 	else if (am->afmt == GF_AUDIO_FMT_DBLP) {
 		Double *out_dbl = (Double *)buffer;
+#ifdef GPAC_BIG_ENDIAN
+		u64 *out_u64 = (u64 *)buffer;
+#endif
 		for (j = 0; j < am->nb_channels; j++) {
 			out_mix = am->output + j;
 			for (i = 0; i < nb_written; i++) {
 				s32 samp = (*out_mix);
 				(*out_dbl) = ((Double)samp) / GF_INT_MAX;
 				out_dbl += 1;
+#ifdef GPAC_BIG_ENDIAN
+				(*out_u64) = swap_64(*out_u64);
+				out_u64 += 1;
+#endif
 				out_mix += am->nb_channels;
 			}
 		}
 	}
-	else if (am->afmt == GF_AUDIO_FMT_S24) {
+	else if (am->afmt == GF_AUDIO_FMT_DBL_BE) {
+		Double *out_dbl = (Double *)buffer;
+#ifndef GPAC_BIG_ENDIAN
+		u64 *out_u64 = (u64 *)buffer;
+#endif
+		for (i = 0; i < nb_written; i++) {
+			for (j = 0; j < am->nb_channels; j++) {
+				s32 samp = (*out_mix);
+				(*out_dbl) = ((Double)samp) / GF_INT_MAX;
+				out_dbl += 1;
+
+#ifndef GPAC_BIG_ENDIAN
+				(*out_u64) = swap_64(*out_u64);
+				out_u64 += 1;
+#endif
+				out_mix += 1;
+			}
+		}
+	} else if (am->afmt == GF_AUDIO_FMT_S24) {
 		s8 *out_s24 = (s8 *)buffer;
 		for (i = 0; i<nb_written; i++) {
 			for (j = 0; j<am->nb_channels; j++) {
@@ -1244,6 +1362,21 @@ do_mix:
 				out_s24[0] = samp & 0xFF;
 				out_s24 += 3;
 				out_mix += am->nb_channels;
+			}
+		}
+	} else if (am->afmt == GF_AUDIO_FMT_S24_BE) {
+		s8 *out_s24 = (s8 *)buffer;
+		for (i = 0; i<nb_written; i++) {
+			for (j = 0; j<am->nb_channels; j++) {
+				s32 samp = (*out_mix);
+				samp /= MIX_S24_SCALE;
+				if (samp > GF_S24_MAX) samp = GF_S24_MAX;
+				else if (samp < GF_S24_MIN) samp = GF_S24_MIN;
+				out_s24[0] = (samp>>16) & 0xFF;
+				out_s24[1] = (samp>>8) & 0xFF;
+				out_s24[2] = samp & 0xFF;
+				out_s24 += 3;
+				out_mix += 1;
 			}
 		}
 	} else if (am->afmt == GF_AUDIO_FMT_S16) {
@@ -1280,6 +1413,24 @@ do_mix:
 #endif
 				out_s16 += 1;
 				out_mix += am->nb_channels;
+			}
+		}
+	} else if (am->afmt == GF_AUDIO_FMT_S16_BE) {
+		s16 *out_s16 = (s16 *)buffer;
+		for (i = 0; i<nb_written; i++) {
+			for (j = 0; j<am->nb_channels; j++) {
+				s32 samp = (*out_mix);
+				samp /= MIX_S16_SCALE;
+				if (samp > GF_SHORT_MAX) samp = GF_SHORT_MAX;
+				else if (samp < GF_SHORT_MIN) samp = GF_SHORT_MIN;
+
+#ifndef GPAC_BIG_ENDIAN
+				(*out_s16) = swap_16(samp);
+#else
+				(*out_s16) = samp;
+#endif
+				out_s16 += 1;
+				out_mix += 1;
 			}
 		}
 	} else if (am->afmt == GF_AUDIO_FMT_U8) {

src/compositor/mpeg4_audio.c
@@ -541,9 +541,11 @@ static Bool audiobuffer_get_config(GF_AudioInterface *aifc, Bool for_reconf)
 			aifc->afmt = GF_AUDIO_FMT_S16;
 			break;
 		case GF_AUDIO_FMT_S24P:
+		case GF_AUDIO_FMT_S24_BE:
 			aifc->afmt = GF_AUDIO_FMT_S24;
 			break;
 		case GF_AUDIO_FMT_S32P:
+		case GF_AUDIO_FMT_S32_BE:
 			aifc->afmt = GF_AUDIO_FMT_S32;
 			break;
 		case GF_AUDIO_FMT_FLTP:

src/export.cpp
@@ -981,6 +981,8 @@
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_clean_aperture) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_dovi_config_get) )
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_opus_config_get_desc) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_pcm_config) )
+#pragma comment (linker, EXPORT_SYMBOL(gf_isom_get_lpcm_config) )
 
 # ifndef GPAC_DISABLE_ISOM_DUMP
 #pragma comment (linker, EXPORT_SYMBOL(gf_isom_dump) )

src/filters/ff_common.c
@@ -406,6 +406,7 @@ typedef struct
 	u32 ff_codec_id;
 	u32 gpac_codec_id;
 	u32 ff_codectag;
+	u32 gpac_audio_format;
 } GF_FF_CIDREG;
 
 static const GF_FF_CIDREG FF2GPAC_CodecIDs[] =
@@ -460,30 +461,30 @@ static const GF_FF_CIDREG FF2GPAC_CodecIDs[] =
 
 	//RAW codecs
 	{AV_CODEC_ID_RAWVIDEO, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_S16LE, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_S16BE, GF_CODECID_RAW, 0},
+	{AV_CODEC_ID_PCM_S16LE, GF_CODECID_RAW, 0, GF_AUDIO_FMT_S16},
+	{AV_CODEC_ID_PCM_S16BE, GF_CODECID_RAW, 0, GF_AUDIO_FMT_S16_BE},
 	{AV_CODEC_ID_PCM_U16LE, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_PCM_U16BE, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_PCM_S8, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_U8, GF_CODECID_RAW, 0},
+	{AV_CODEC_ID_PCM_U8, GF_CODECID_RAW, 0, GF_AUDIO_FMT_U8},
 	{AV_CODEC_ID_PCM_MULAW, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_PCM_ALAW, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_S32LE, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_S32BE, GF_CODECID_RAW, 0},
+	{AV_CODEC_ID_PCM_S32LE, GF_CODECID_RAW, 0, GF_AUDIO_FMT_S32},
+	{AV_CODEC_ID_PCM_S32BE, GF_CODECID_RAW, 0, GF_AUDIO_FMT_S32_BE},
 	{AV_CODEC_ID_PCM_U32LE, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_PCM_U32BE, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_S24LE, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_S24BE, GF_CODECID_RAW, 0},
+	{AV_CODEC_ID_PCM_S24LE, GF_CODECID_RAW, 0, GF_AUDIO_FMT_S24},
+	{AV_CODEC_ID_PCM_S24BE, GF_CODECID_RAW, 0, GF_AUDIO_FMT_S24_BE},
 	{AV_CODEC_ID_PCM_U24LE, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_PCM_U24BE, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_PCM_S24DAUD, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_PCM_ZORK, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_PCM_S16LE_PLANAR, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_PCM_DVD, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_F32BE, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_F32LE, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_F64BE, GF_CODECID_RAW, 0},
-	{AV_CODEC_ID_PCM_F64LE, GF_CODECID_RAW, 0},
+	{AV_CODEC_ID_PCM_F32BE, GF_CODECID_RAW, 0, GF_AUDIO_FMT_FLT},
+	{AV_CODEC_ID_PCM_F32LE, GF_CODECID_RAW, 0, GF_AUDIO_FMT_FLT_BE},
+	{AV_CODEC_ID_PCM_F64BE, GF_CODECID_RAW, 0, GF_AUDIO_FMT_DBL},
+	{AV_CODEC_ID_PCM_F64LE, GF_CODECID_RAW, 0, GF_AUDIO_FMT_DBL_BE},
 	{AV_CODEC_ID_PCM_BLURAY, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_PCM_LXF, GF_CODECID_RAW, 0},
 	{AV_CODEC_ID_S302M, GF_CODECID_RAW, 0},
@@ -554,6 +555,16 @@ u32 ffmpeg_codecid_to_gpac(u32 codec_id)
 	return 0;
 }
 
+u32 ffmpeg_codecid_to_gpac_audio_fmt(u32 codec_id)
+{
+	u32 i=0;
+	while (FF2GPAC_CodecIDs[i].ff_codec_id) {
+		if (FF2GPAC_CodecIDs[i].ff_codec_id == codec_id)
+			return FF2GPAC_CodecIDs[i].gpac_audio_format;
+		i++;
+	}
+	return 0;
+}
 
 typedef struct
 {

commit c583f3cb425724e98b6beec5ffb58d3e9425ae56

fixed bug introduced in 4ae3c612

src/isomedia/sample_descs.c
@@ -1804,11 +1804,14 @@ GF_Err gf_isom_get_pcm_config(GF_ISOFile *movie, u32 trackNumber, u32 descriptio
 	aent = (GF_AudioSampleEntryBox *)gf_list_get(trak->Media->information->sampleTable->SampleDescription->child_boxes, descriptionIndex-1);
 	if (!aent) return GF_BAD_PARAM;
 	if ((aent->type != GF_ISOM_BOX_TYPE_IPCM) && (aent->type != GF_ISOM_BOX_TYPE_FPCM)) {
+		Bool is_le = GF_FALSE;
 		GF_Box *b = gf_isom_box_find_child(aent->child_boxes, GF_QT_BOX_TYPE_WAVE);
-		if (!b) return GF_BAD_PARAM;
-		GF_ChromaInfoBox *enda = (GF_ChromaInfoBox *)gf_isom_box_find_child(b->child_boxes, GF_QT_BOX_TYPE_ENDA);
+		//if no wave, assume big endian
+		if (b) {
+			GF_ChromaInfoBox *enda = (GF_ChromaInfoBox *)gf_isom_box_find_child(b->child_boxes, GF_QT_BOX_TYPE_ENDA);
+			is_le = (enda && enda->chroma) ? GF_TRUE : GF_FALSE;
+		}
 		u32 bits=0;
-		Bool is_le = (enda && enda->chroma) ? GF_TRUE : GF_FALSE;
 		switch (aent->type) {
 		case GF_QT_SUBTYPE_FL32: bits = 32; break;
 		case GF_QT_SUBTYPE_FL64: bits = 64; break;