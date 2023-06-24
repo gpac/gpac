e759e0ef946ceb7a344a5c4f0692d7f072d073da
Rodolphe Fouquet <rodolphe@unicowd.com>
2023-06-24 01:01:38 +0100

    add development shell to the flake

--- flake.nix
+++ flake.nix
@@ -1,45 +1,54 @@
 {
   description = "GPAC Multimedia Open Source Project";
-
-  outputs = { self, nixpkgs }: {
-
+  inputs =  {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }@inputs: 
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    buildInputs = with pkgs; [ 
+      zlib 
+      git
+      freetype 
+      libjpeg_turbo 
+      libpng 
+      libmad 
+      faad2 
+      libogg 
+      libvorbis 
+      libtheora
+      a52dec
+      ffmpeg
+      xorg.libX11
+      xorg.libXv
+      xorg.xorgproto
+      mesa
+      xvidcore
+      openssl
+      alsa-lib
+      jack2
+      pulseaudio
+      SDL2
+      mesa-demos
+      swig4
+    ];
+    nativeBuildInputs = with pkgs; [ pkg-config ];
+ in
+  with pkgs; {  
+    devShells.x86_64-linux.default = mkShell {
+        inherit buildInputs nativeBuildInputs;
+    };
     packages.x86_64-linux.default =  with import nixpkgs { system = "x86_64-linux"; };
       stdenv.mkDerivation {
         name = "gpac";
         src = self;
-        nativeBuildInputs = [ pkg-config ];
-        buildInputs = [ 
-          zlib 
-          git
-          freetype 
-          libjpeg_turbo 
-          libpng 
-          libmad 
-          faad2 
-          libogg 
-          libvorbis 
-          libtheora
-          a52dec
-          ffmpeg
-          xorg.libX11
-          xorg.libXv
-          xorg.xorgproto
-          mesa
-          xvidcore
-          openssl
-          alsa-lib
-          jack2
-          pulseaudio
-          SDL2
-          mesa-demos
-        ];
-
+        inherit buildInputs nativeBuildInputs;
         configurePhase = ''
           runHook preConfigure
           ./configure --prefix=$out
           runHook postConfigure
         '';
-        buildPhase = "make";
+        buildPhase = "make -j $NIX_BUILD_CORES";
         installPhase = "make install";
       }; 
 
