gpac/gpac

commit 0d9fd4fc1cccfea75ee097f50ac6e58a4a169d7f

Align module documentation with the rest of the code, bump interface 4CCs and added libcaca output module

modules/jack/jack.c
@@ -215,11 +215,9 @@ Jack_Setup (GF_AudioOutput * dr, void *os_handle, u32 num_buffers,
 	memset (ctx->jackClientName, 0, MAX_JACK_CLIENT_NAME_SZ);
 	snprintf (ctx->jackClientName, MAX_JACK_CLIENT_NAME_SZ, "gpac-%d", gf_sys_get_process_id() );
 
-	ctx->autoConnect = GF_TRUE;
-	if (gf_opts_get_bool("Jack", "NoAutoConnect"))
-		ctx->autoConnect = GF_FALSE;
+	ctx->autoConnect = gf_module_get_bool((GF_BaseInterface *)dr, "auto-connect"))
 
-	if (gf_opts_get_bool("Jack", "NoStartServer"))
+	if (!gf_module_get_bool((GF_BaseInterface *)dr, "start-server"))
 		options |= JackNoStartServer;
 
 	ctx->jack = jack_client_open (ctx->jackClientName, options, &status, NULL);
@@ -413,6 +411,12 @@ Jack_QueryOutputSampleRate (GF_AudioOutput * dr, u32 * desired_sr,
 	return GF_OK;
 }
 
+static GF_GPACArg JackArgs[] = {
+	GF_DEF_ARG("auto-connect", NULL, "automatically connect to jackd", "true", NULL, GF_ARG_BOOL, 0),
+	GF_DEF_ARG("start-server", NULL, "automatically start JACK server if not running", "true", NULL, GF_ARG_BOOL, 0),
+	{0},
+};
+
 void *
 NewJackOutput ()
 {
@@ -437,6 +441,8 @@ NewJackOutput ()
 	driv->SetPan = Jack_SetPan;
 	driv->SetPriority = Jack_SetPriority;
 	driv->QueryOutputSampleRate = Jack_QueryOutputSampleRate;
+	driv->args = JackArgs;
+	driv->description = "Audio output using JACK";
 
 	ctx->jack = NULL;
 	ctx->numChannels = 0;
@@ -450,8 +456,7 @@ NewJackOutput ()
 	ctx->autoConnect = GF_FALSE;
 	ctx->volume = 1.0;
 
-	GF_REGISTER_MODULE_INTERFACE (driv, GF_AUDIO_OUTPUT_INTERFACE,
-	                              "Jack Audio Output", "gpac distribution");
+	GF_REGISTER_MODULE_INTERFACE (driv, GF_AUDIO_OUTPUT_INTERFACE, "jack", "gpac distribution");
 	return driv;
 }
 

modules/pulseaudio/pulseaudio.c
@@ -67,10 +67,10 @@ PulseAudio_Setup (GF_AudioOutput * dr, void *os_handle,
 	PulseAudioContext *ctx = (PulseAudioContext *) dr->opaque;
 	if (ctx == NULL)
 		return GF_BAD_PARAM;
-	opt = gf_opts_get_key("PulseAudio", "Name");
+	opt = gf_module_get_key(dr, "name");
 	ctx->output_name = opt ? ctx->output_name : "GPAC";
 
-	opt = gf_opts_get_key("PulseAudio", "Description");
+	opt = gf_module_get_key(dr, "description");
 	ctx->output_description = opt  ? opt  : "GPAC Output";
 	return GF_OK;
 }
@@ -212,6 +212,11 @@ PulseAudio_GetAudioDelay (GF_AudioOutput * dr)
 	return ms_delay;
 }
 
+static GF_GPACArg PulseAudioArgs[] = {
+	GF_DEF_ARG("name", NULL, "name for PulseAudio", "GPAC", NULL, GF_ARG_STRING, 0),
+	GF_DEF_ARG("description", NULL, "description for PulseAudio", "GPAC output", NULL, GF_ARG_STRING, 0),
+	{0},
+};
 
 void *
 NewPulseAudioOutput ()
@@ -237,7 +242,10 @@ NewPulseAudioOutput ()
 	driv->Configure = PulseAudio_Configure;
 	driv->GetAudioDelay = PulseAudio_GetAudioDelay;
 	driv->WriteAudio = PulseAudio_WriteAudio;
-	GF_REGISTER_MODULE_INTERFACE (driv, GF_AUDIO_OUTPUT_INTERFACE, "PulseAudio", "gpac distribution");
+	driv->description = "Audio output using PulseAudio";
+	driv->args = PulseAudioArgs;
+
+	GF_REGISTER_MODULE_INTERFACE (driv, GF_AUDIO_OUTPUT_INTERFACE, "pulse", "gpac distribution");
 	return driv;
 }
 

modules/x11_out/x11_out.c
@@ -748,7 +748,7 @@ static GF_Err X11_SetupGL(GF_VideoOutput *vout)
 	if ( ! glXMakeCurrent(xWin->display, xWin->fullscreen ? xWin->full_wnd : xWin->wnd, xWin->glx_context) ) return GF_IO_ERR;
 
 #ifdef GPAC_HAS_OPENGL
-	if (gf_opts_get_bool("core", "disable-vsync")) {
+	if (gf_module_get_bool((GF_BaseInterface*)vout, "no-vsync")) {
 		my_glXSwapIntervalEXT = (PFNGLXSWAPINTERVALEXTPROC)glXGetProcAddress( (const GLubyte*)"glXSwapIntervalEXT");
 		if (my_glXSwapIntervalEXT != NULL) {
 			my_glXSwapIntervalEXT(xWin->display, xWin->wnd, 0);
@@ -1306,8 +1306,8 @@ X11_SetupWindow (GF_VideoOutput * vout)
 	xWindow->use_shared_memory = 0;
 
 #ifdef GPAC_HAS_X11_SHM
-	sOpt = gf_opts_get_key("core", "hwvmem");
-	if (!sOpt || strcmp(sOpt, "no")) {
+	sOpt = gf_module_get_key((GF_BaseInterface*)vout, "hwvmem");
+	if (!sOpt || strcmp(sOpt, "never")) {
 		int XShmMajor, XShmMinor;
 		Bool XShmPixmaps;
 		if (XShmQueryVersion(xWindow->display, &XShmMajor, &XShmMinor, &XShmPixmaps)) {
@@ -1322,8 +1322,7 @@ X11_SetupWindow (GF_VideoOutput * vout)
 #endif
 
 #ifdef GPAC_HAS_X11_XV
-	sOpt = gf_opts_get_key("core", "no-colorkey");
-	if (sOpt && !strcmp(sOpt, "yes")) {
+	if (!gf_module_get_bool((GF_BaseInterface *vout), "colorkey")) {
 		xWindow->xvport = X11_GetXVideoPort(vout, GF_PIXEL_YV12, 0);
 	} else {
 		xWindow->xvport = X11_GetXVideoPort(vout, GF_PIXEL_YV12, 1);
@@ -1345,7 +1344,7 @@ X11_SetupWindow (GF_VideoOutput * vout)
 
 #ifdef GPAC_HAS_X11_SHM
 		/*if user asked for YUV->RGB on offscreen, do it (it may crash the system)*/
-		if (gf_opts_get_bool("core", "offscreen-yuv")) {
+		if (gf_module_get_bool((GF_BaseInterface *)vout, "offscreen-yuv")) {
 			vout->hw_caps |= GF_VIDEO_HW_HAS_YUV;
 			GF_LOG(GF_LOG_INFO, GF_LOG_MMIO, ("[X11] Using XV Offscreen YUV2RGB acceleration\n"));
 		}
@@ -1566,6 +1565,16 @@ void X11_Shutdown (struct _video_out *vout)
 }
 
 
+static GF_GPACArg X11Args[] = {
+	GF_DEF_ARG("offscreen-yuv", NULL, "enable offscreen yuv", "true", NULL, GF_ARG_BOOL, 0),
+	GF_DEF_ARG("no-vsync", NULL, "disable vertical synchro", "false", NULL, GF_ARG_BOOL, 0),
+	GF_DEF_ARG("hwvmem", NULL, "specify (2D rendering only) memory type of main video backbuffer. Depending on the scene type, this may drastically change the playback speed\n"
+ "- always: always on hardware\n"
+ "- never: always on system memory\n"
+ "- auto: selected by GPAC based on content type (graphics or video)", "auto", "auto|always|never", GF_ARG_INT, GF_ARG_HINT_EXPERT|GF_ARG_SUBSYS_VIDEO),
+	GF_DEF_ARG("colorkey", NULL, "enable colorkey for overlays", "true", NULL, GF_ARG_BOOL, 0),
+	{0},
+};
 
 
 void *NewX11VideoOutput ()
@@ -1579,7 +1588,7 @@ void *NewX11VideoOutput ()
 		gf_free(driv);
 		return NULL;
 	}
-	GF_REGISTER_MODULE_INTERFACE(driv, GF_VIDEO_OUTPUT_INTERFACE, "X11 Video Output", "gpac distribution")
+	GF_REGISTER_MODULE_INTERFACE(driv, GF_VIDEO_OUTPUT_INTERFACE, "x11", "gpac distribution")
 
 	driv->opaque = xWindow;
 
@@ -1598,6 +1607,9 @@ void *NewX11VideoOutput ()
 		x11_translate_key(XK_BackSpace, &evt.key);
 		X11_BadAccess_ByPass(NULL, NULL);
 	}
+	driv->args = X11Args;
+	driv->description = "Video output using X11";
+
 	return (void *) driv;
 
 }

applications/gpac/gpac.c
@@ -653,8 +653,13 @@ int gpac_main(int _argc, char **_argv)
 			} else if (!strcmp(argv[i+1], "core")) {
 				gpac_core_help(argmode, GF_FALSE);
 				gpac_exit(0);
-			} else if (!strcmp(argv[i+1], "modules")) {
-				gpac_modules_help();
+			} else if (!strcmp(argv[i+1], "modules") || !strcmp(argv[i+1], "module")) {
+				if (i+2<argc) {
+					gf_sys_mark_arg_used(i+2, GF_TRUE);
+					gpac_modules_help(argv[i+2]);
+				} else {
+					gpac_modules_help(NULL);
+				}
 				gpac_exit(0);
 			} else if (!strcmp(argv[i+1], "doc")) {
 				gpac_filter_help();

applications/gpac/gpac.h
@@ -62,7 +62,7 @@ void mp4c_help(u32 argmode);
 #include <gpac/main.h>
 
 void gpac_filter_help(void);
-void gpac_modules_help(void);
+void gpac_modules_help(char *mod_name);
 void gpac_alias_help(GF_SysArgMode argmode);
 void gpac_credentials_help(GF_SysArgMode argmode);
 void gpac_core_help(GF_SysArgMode mode, Bool for_logs);

applications/gpac/gpac_help.c
@@ -516,38 +516,64 @@ void gpac_filter_help(void)
 #include <gpac/modules/hardcoded_proto.h>
 #include <gpac/modules/font.h>
 
-void gpac_modules_help(void)
+void gpac_modules_help(char *mod_name)
 {
 	u32 i;
+	if (mod_name) {
+		gf_opts_set_key("temp", "gendoc", "yes");
+		GF_BaseInterface *ifce = gf_modules_load_by_name(mod_name, GF_VIDEO_OUTPUT_INTERFACE, GF_TRUE);
+		if (!ifce) ifce = gf_modules_load_by_name(mod_name, GF_AUDIO_OUTPUT_INTERFACE, GF_TRUE);
+		if (!ifce) ifce = gf_modules_load_by_name(mod_name, GF_FONT_READER_INTERFACE, GF_TRUE);
+		if (!ifce) ifce = gf_modules_load_by_name(mod_name, GF_COMPOSITOR_EXT_INTERFACE, GF_TRUE);
+		if (!ifce) ifce = gf_modules_load_by_name(mod_name, GF_HARDCODED_PROTO_INTERFACE, GF_TRUE);
+
+		if (!ifce) {
+			fprintf(stderr, "No such module %s\n", mod_name);
+			return;
+		}
+		gf_sys_format_help(helpout, help_flags, "%s\n", ifce->description ? ifce->description : "No description available");
+		if (ifce->args) {
+			gf_sys_format_help(helpout, help_flags, "Options:\n");
+			i=0;
+			while (ifce->args && ifce->args[i].name) {
+				gf_sys_print_arg(helpout, help_flags, &ifce->args[i], ifce->module_name);
+				i++;
+			}
+		} else {
+			gf_sys_format_help(helpout, help_flags, "No Option\n");
+		}
+		gf_modules_close_interface(ifce);
+		return;
+	}
 	gf_sys_format_help(helpout, help_flags, "Available modules:\n");
 	for (i=0; i<gf_modules_count(); i++) {
 		char *str = (char *) gf_modules_get_file_name(i);
 		if (!str) continue;
 		gf_sys_format_help(helpout, help_flags | GF_PRINTARG_HIGHLIGHT_FIRST, "%s: implements ", str);
 		if (!strncmp(str, "gm_", 3) || !strncmp(str, "gsm_", 4)) {
-			GF_BaseInterface *ifce = gf_modules_load_by_name(str, GF_VIDEO_OUTPUT_INTERFACE);
+			GF_BaseInterface *ifce = gf_modules_load_by_name(str, GF_VIDEO_OUTPUT_INTERFACE, GF_FALSE);
 			if (ifce) {
-				gf_sys_format_help(helpout, help_flags, "VideoOutput ");
+				gf_sys_format_help(helpout, help_flags, "VideoOutput (%s) ", ifce->module_name);
 				gf_modules_close_interface(ifce);
 			}
-			ifce = gf_modules_load_by_name(str, GF_AUDIO_OUTPUT_INTERFACE);
+			ifce = gf_modules_load_by_name(str, GF_AUDIO_OUTPUT_INTERFACE, GF_FALSE);
 			if (ifce) {
-				gf_sys_format_help(helpout, help_flags, "AudioOutput ");
+				gf_sys_format_help(helpout, help_flags, "AudioOutput (%s) ", ifce->module_name);
 				gf_modules_close_interface(ifce);
 			}
-			ifce = gf_modules_load_by_name(str, GF_FONT_READER_INTERFACE);
+			ifce = gf_modules_load_by_name(str, GF_FONT_READER_INTERFACE, GF_FALSE);
 			if (ifce) {
-				gf_sys_format_help(helpout, help_flags, "FontReader ");
+				gf_sys_format_help(helpout, help_flags, "FontReader (%s) ", ifce->module_name);
 				gf_modules_close_interface(ifce);
 			}
-			ifce = gf_modules_load_by_name(str, GF_COMPOSITOR_EXT_INTERFACE);
+			ifce = gf_modules_load_by_name(str, GF_COMPOSITOR_EXT_INTERFACE, GF_FALSE);
 			if (ifce) {
-				gf_sys_format_help(helpout, help_flags, "CompositorExtension ");
+				gf_sys_format_help(helpout, help_flags, "CompositorExtension (%s) ", ifce->module_name);
 				gf_modules_close_interface(ifce);
 			}
-			ifce = gf_modules_load_by_name(str, GF_HARDCODED_PROTO_INTERFACE);
+			ifce = gf_modules_load_by_name(str, GF_HARDCODED_PROTO_INTERFACE, GF_FALSE);
 			if (ifce) {
-				gf_sys_format_help(helpout, help_flags, "HardcodedProto ");
+				gf_sys_format_help(helpout, help_flags, "HardcodedProto (%s) ", ifce->module_name);
 				gf_modules_close_interface(ifce);
 			}
 		} else {
@@ -719,6 +745,7 @@ static GF_GPACArg gpac_args[] =
 			"- net: print network interfaces\n"
 			"- prompt: print the GPAC prompt help when running in interactive mode (see [-k](GPAC) )\n"
 			"- modules: print available modules\n"
+			"- module NAME: print info and options of module `NAME`\n"
 			"- creds: print credential help\n"
 			"- filters: print name of all available filters\n"
 			"- filters:*: print name of all available filters, including meta filters\n"

build/xcode/gpac.xcodeproj/project.pbxproj
@@ -179,6 +179,8 @@
 		9225DD752326790C00F94C42 /* mpd.h in Headers */ = {isa = PBXBuildFile; fileRef = 9225DD742326790C00F94C42 /* mpd.h */; };
 		92261A87239A6E4600C14199 /* out_http.c in Sources */ = {isa = PBXBuildFile; fileRef = 92261A86239A6E4600C14199 /* out_http.c */; };
 		922B0FAA2A5E81EB003690BB /* dec_mpeghdec.c in Sources */ = {isa = PBXBuildFile; fileRef = 922B0FA92A5E81EB003690BB /* dec_mpeghdec.c */; };
+		922B0FC72A602176003690BB /* caca_out.c in Sources */ = {isa = PBXBuildFile; fileRef = 922B0FC62A602176003690BB /* caca_out.c */; };
+		922B0FC82A6028C5003690BB /* libgpac.dylib in Frameworks */ = {isa = PBXBuildFile; fileRef = 9201F93718D5A38A003D1ACA /* libgpac.dylib */; };
 		922B27722445D5CE001D4F72 /* dec_ttml.c in Sources */ = {isa = PBXBuildFile; fileRef = 922B27712445D5CE001D4F72 /* dec_ttml.c */; };
 		922CF1251FCEE5D600F2F6A4 /* dmx_mpegps.c in Sources */ = {isa = PBXBuildFile; fileRef = 922CF1241FCEE5D600F2F6A4 /* dmx_mpegps.c */; };
 		922CF1271FCF169900F2F6A4 /* dmx_avi.c in Sources */ = {isa = PBXBuildFile; fileRef = 922CF1261FCF169900F2F6A4 /* dmx_avi.c */; };
@@ -556,11 +558,18 @@
 			isEditable = 1;
 			outputFiles = (
 			);
-			script = "";
+			script = "# Type a script or drag a script file from your workspace to insert its path.\n";
 		};
 /* End PBXBuildRule section */
 
 /* Begin PBXContainerItemProxy section */
+		922B0FC92A6029AD003690BB /* PBXContainerItemProxy */ = {
+			isa = PBXContainerItemProxy;
+			containerPortal = 9201F92918D5A359003D1ACA /* Project object */;
+			proxyType = 1;
+			remoteGlobalIDString = 922B0FC02A602153003690BB;
+			remoteInfo = caca_out;
+		};
 		92BD5562205AB89D007C4C06 /* PBXContainerItemProxy */ = {
 			isa = PBXContainerItemProxy;
 			containerPortal = 9201F92918D5A359003D1ACA /* Project object */;
@@ -805,6 +814,8 @@
 		9225DD742326790C00F94C42 /* mpd.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = mpd.h; path = ../include/gpac/mpd.h; sourceTree = "<group>"; };
 		92261A86239A6E4600C14199 /* out_http.c */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.c; name = out_http.c; path = filters/out_http.c; sourceTree = "<group>"; };
 		922B0FA92A5E81EB003690BB /* dec_mpeghdec.c */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.c; name = dec_mpeghdec.c; path = filters/dec_mpeghdec.c; sourceTree = "<group>"; };
+		922B0FC12A602153003690BB /* gm_caca_out.dylib */ = {isa = PBXFileReference; explicitFileType = "compiled.mach-o.dylib"; includeInIndex = 0; path = gm_caca_out.dylib; sourceTree = BUILT_PRODUCTS_DIR; };
+		922B0FC62A602176003690BB /* caca_out.c */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.c; name = caca_out.c; path = ../../modules/caca_out/caca_out.c; sourceTree = "<group>"; };
 		922B27712445D5CE001D4F72 /* dec_ttml.c */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.c; name = dec_ttml.c; path = filters/dec_ttml.c; sourceTree = "<group>"; };
 		922B48781A5ADAB3006CBBBA /* os_module.c */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.c; path = os_module.c; sourceTree = "<group>"; };
 		922CF1241FCEE5D600F2F6A4 /* dmx_mpegps.c */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.c; name = dmx_mpegps.c; path = filters/dmx_mpegps.c; sourceTree = "<group>"; };
@@ -1199,6 +1210,14 @@
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
+		922B0FBF2A602153003690BB /* Frameworks */ = {
+			isa = PBXFrameworksBuildPhase;
+			buildActionMask = 2147483647;
+			files = (
+				922B0FC82A6028C5003690BB /* libgpac.dylib in Frameworks */,
+			);
+			runOnlyForDeploymentPostprocessing = 0;
+		};
 		927AACFA1E44EEDC008C8E69 /* Frameworks */ = {
 			isa = PBXFrameworksBuildPhase;
 			buildActionMask = 2147483647;
@@ -1419,6 +1438,7 @@
 				92AAF16518D6F40400E2FF12 /* gm_ft_font.dylib */,
 				927AACFD1E44EEDC008C8E69 /* gpac */,
 				92C403D72181F10500331F39 /* gf_openhevc_dec.dylib */,
+				922B0FC12A602153003690BB /* gm_caca_out.dylib */,
 			);
 			name = Products;
 			sourceTree = "<group>";
@@ -1613,6 +1633,14 @@
 				9201000D18D5A444003D1ACA /* tx3g.c */,
 			);
 			path = isomedia;
+			sourceTree = "<group>";
+		};
+		922B0FC52A60215F003690BB /* caca_out */ = {
+			isa = PBXGroup;
+			children = (
+				922B0FC62A602176003690BB /* caca_out.c */,
+			);
+			name = caca_out;
 			sourceTree = "<group>";
 		};
 		92334C8E2333A8B800DA6051 /* quickjs */ = {
@@ -1882,6 +1910,7 @@
 		92BE50AD18D6376A00AA59D8 /* modules */ = {
 			isa = PBXGroup;
 			children = (
+				922B0FC52A60215F003690BB /* caca_out */,
 				92C403DB2181F15A00331F39 /* dec_openhevc */,
 				92586A6E1EA7663A00A0838A /* ft_font */,
 				92AAF0D718D6DB3400E2FF12 /* sdl_out */,
@@ -2236,6 +2265,13 @@
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
+		922B0FBD2A602153003690BB /* Headers */ = {
+			isa = PBXHeadersBuildPhase;
+			buildActionMask = 2147483647;
+			files = (
+			);
+			runOnlyForDeploymentPostprocessing = 0;
+		};
 		92AAF0D118D6DB2D00E2FF12 /* Headers */ = {
 			isa = PBXHeadersBuildPhase;
 			buildActionMask = 2147483647;
@@ -2280,6 +2316,23 @@
 			productReference = 9201F93718D5A38A003D1ACA /* libgpac.dylib */;
 			productType = "com.apple.product-type.library.dynamic";
 		};
+		922B0FC02A602153003690BB /* caca_out */ = {
+			isa = PBXNativeTarget;
+			buildConfigurationList = 922B0FC22A602154003690BB /* Build configuration list for PBXNativeTarget "caca_out" */;
+			buildPhases = (
+				922B0FBD2A602153003690BB /* Headers */,
+				922B0FBE2A602153003690BB /* Sources */,
+				922B0FBF2A602153003690BB /* Frameworks */,
+			);
+			buildRules = (
+			);
+			dependencies = (
+			);
+			name = caca_out;
+			productName = caca_out;
+			productReference = 922B0FC12A602153003690BB /* gm_caca_out.dylib */;
+			productType = "com.apple.product-type.library.dynamic";
+		};
 		927AACFC1E44EEDC008C8E69 /* gpac */ = {
 			isa = PBXNativeTarget;
 			buildConfigurationList = 927AAD011E44EEDC008C8E69 /* Build configuration list for PBXNativeTarget "gpac" */;
@@ -2291,6 +2344,7 @@
 			buildRules = (
 			);
 			dependencies = (
+				922B0FCA2A6029AD003690BB /* PBXTargetDependency */,
 				92C403DF2181F1ED00331F39 /* PBXTargetDependency */,
 				92BD5563205AB89D007C4C06 /* PBXTargetDependency */,
 			);
@@ -2378,6 +2432,10 @@
 				LastUpgradeCheck = 1210;
 				ORGANIZATIONNAME = "Telecom ParisTech";
 				TargetAttributes = {
+					922B0FC02A602153003690BB = {
+						CreatedOnToolsVersion = 14.3.1;
+						ProvisioningStyle = Automatic;
+					};
 					927AACFC1E44EEDC008C8E69 = {
 						CreatedOnToolsVersion = 8.2.1;
 						DevelopmentTeam = Q46G522WGV;
@@ -2408,6 +2466,7 @@
 				92AAF16418D6F40400E2FF12 /* ft_font */,
 				927AACFC1E44EEDC008C8E69 /* gpac */,
 				92C403D62181F10500331F39 /* openhevc_dec */,
+				922B0FC02A602153003690BB /* caca_out */,
 			);
 		};
 /* End PBXProject section */
@@ -2833,6 +2892,14 @@
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
+		922B0FBE2A602153003690BB /* Sources */ = {
+			isa = PBXSourcesBuildPhase;
+			buildActionMask = 2147483647;
+			files = (
+				922B0FC72A602176003690BB /* caca_out.c in Sources */,
+			);
+			runOnlyForDeploymentPostprocessing = 0;
+		};
 		927AACF91E44EEDC008C8E69 /* Sources */ = {
 			isa = PBXSourcesBuildPhase;
 			buildActionMask = 2147483647;
@@ -2884,6 +2951,11 @@
 /* End PBXSourcesBuildPhase section */
 
 /* Begin PBXTargetDependency section */
+		922B0FCA2A6029AD003690BB /* PBXTargetDependency */ = {
+			isa = PBXTargetDependency;
+			target = 922B0FC02A602153003690BB /* caca_out */;
+			targetProxy = 922B0FC92A6029AD003690BB /* PBXContainerItemProxy */;
+		};
 		92BD5563205AB89D007C4C06 /* PBXTargetDependency */ = {
 			isa = PBXTargetDependency;
 			target = 92AAF0D218D6DB2D00E2FF12 /* sdl_out */;
@@ -3137,6 +3209,14 @@
 					"-lUIManager",
 					"-lgVBAPRenderer",
 					"-lMpegTPDec",
+					"-lxcb",
+					"-lxcb-shm",
+					"-lxcb-shape",
+					"-lxcb-xfixes",
+					"-lXv",
+					"-lX11",
+					"-lXext",
+					"-lcaca",
 				);
 				PRODUCT_NAME = gpac;
 				SDKROOT = macosx;
@@ -3280,11 +3360,122 @@
 					"-lUIManager",
 					"-lgVBAPRenderer",
 					"-lMpegTPDec",
+					"-lxcb",
+					"-lxcb-shm",
+					"-lxcb-shape",
+					"-lxcb-xfixes",
+					"-lXv",
+					"-lX11",
+					"-lXext",
+					"-lcaca",
 				);
 				PRODUCT_NAME = gpac;
 				SDKROOT = macosx;
 				SYMROOT = ../../bin/osx/;
 				WARNING_LDFLAGS = "";
+			};
+			name = Release;
+		};
+		922B0FC32A602154003690BB /* Debug */ = {
+			isa = XCBuildConfiguration;
+			buildSettings = {
+				ALWAYS_SEARCH_USER_PATHS = NO;
+				CLANG_ANALYZER_NONNULL = YES;
+				CLANG_ANALYZER_NUMBER_OBJECT_CONVERSION = YES_AGGRESSIVE;
+				CLANG_CXX_LANGUAGE_STANDARD = "compiler-default";
+				CLANG_ENABLE_MODULES = NO;
+				CLANG_ENABLE_OBJC_ARC = YES;
+				CLANG_ENABLE_OBJC_WEAK = YES;
+				CLANG_WARN_DIRECT_OBJC_ISA_USAGE = YES_ERROR;
+				CLANG_WARN_DOCUMENTATION_COMMENTS = YES;
+				CLANG_WARN_OBJC_ROOT_CLASS = YES_ERROR;
+				CLANG_WARN_STRICT_PROTOTYPES = NO;
+				CLANG_WARN_UNGUARDED_AVAILABILITY = YES_AGGRESSIVE;
+				CODE_SIGN_IDENTITY = "-";
+				CODE_SIGN_STYLE = Automatic;
+				COPY_PHASE_STRIP = NO;
+				DEBUG_INFORMATION_FORMAT = dwarf;
+				DEVELOPMENT_TEAM = "";
+				DYLIB_COMPATIBILITY_VERSION = 1;
+				DYLIB_CURRENT_VERSION = 1;
+				DYLIB_INSTALL_NAME_BASE = "";
+				EXECUTABLE_PREFIX = gm_;
+				GCC_C_LANGUAGE_STANDARD = gnu11;
+				GCC_DYNAMIC_NO_PIC = NO;
+				GCC_OPTIMIZATION_LEVEL = 0;
+				GCC_PREPROCESSOR_DEFINITIONS = (
+					"DEBUG=1",
+					"$(inherited)",
+				);
+				GCC_WARN_ABOUT_MISSING_PROTOTYPES = NO;
+				GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR;
+				GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
+				HEADER_SEARCH_PATHS = (
+					../../include,
+					../../extra_lib/include,
+					/opt/local/include,
+				);
+				INSTALL_PATH = "";
+				LIBRARY_SEARCH_PATHS = (
+					../../extra_lib/lib/gcc,
+					/opt/local/lib,
+				);
+				MACOSX_DEPLOYMENT_TARGET = 10.9;
+				MTL_ENABLE_DEBUG_INFO = INCLUDE_SOURCE;
+				MTL_FAST_MATH = YES;
+				OTHER_LDFLAGS = " -lcaca ";
+				PRODUCT_NAME = "$(TARGET_NAME)";
+				SDKROOT = macosx;
+				SKIP_INSTALL = YES;
+			};
+			name = Debug;
+		};
+		922B0FC42A602154003690BB /* Release */ = {
+			isa = XCBuildConfiguration;
+			buildSettings = {
+				ALWAYS_SEARCH_USER_PATHS = NO;
+				CLANG_ANALYZER_NONNULL = YES;
+				CLANG_ANALYZER_NUMBER_OBJECT_CONVERSION = YES_AGGRESSIVE;
+				CLANG_CXX_LANGUAGE_STANDARD = "compiler-default";
+				CLANG_ENABLE_MODULES = NO;
+				CLANG_ENABLE_OBJC_ARC = YES;
+				CLANG_ENABLE_OBJC_WEAK = YES;
+				CLANG_WARN_DIRECT_OBJC_ISA_USAGE = YES_ERROR;
+				CLANG_WARN_DOCUMENTATION_COMMENTS = YES;
+				CLANG_WARN_OBJC_ROOT_CLASS = YES_ERROR;
+				CLANG_WARN_STRICT_PROTOTYPES = NO;
+				CLANG_WARN_UNGUARDED_AVAILABILITY = YES_AGGRESSIVE;
+				CODE_SIGN_IDENTITY = "-";
+				CODE_SIGN_STYLE = Automatic;
+				COPY_PHASE_STRIP = NO;
+				DEBUG_INFORMATION_FORMAT = "dwarf-with-dsym";
+				DEVELOPMENT_TEAM = "";
+				DYLIB_COMPATIBILITY_VERSION = 1;
+				DYLIB_CURRENT_VERSION = 1;
+				DYLIB_INSTALL_NAME_BASE = "";
+				ENABLE_NS_ASSERTIONS = NO;
+				EXECUTABLE_PREFIX = gm_;
+				GCC_C_LANGUAGE_STANDARD = gnu11;
+				GCC_WARN_ABOUT_MISSING_PROTOTYPES = NO;
+				GCC_WARN_ABOUT_RETURN_TYPE = YES_ERROR;
+				GCC_WARN_UNINITIALIZED_AUTOS = YES_AGGRESSIVE;
+				HEADER_SEARCH_PATHS = (
+					../../include,
+					../../extra_lib/include,
+					/opt/local/include,
+				);
+				INSTALL_PATH = "";
+				LIBRARY_SEARCH_PATHS = (
+					../../extra_lib/lib/gcc,
+					/opt/local/lib,
+				);
+				MACOSX_DEPLOYMENT_TARGET = 10.9;
+				MTL_ENABLE_DEBUG_INFO = NO;
+				MTL_FAST_MATH = YES;
+				OTHER_LDFLAGS = " -lcaca ";
+				PRODUCT_NAME = "$(TARGET_NAME)";
+				SDKROOT = macosx;
+				SKIP_INSTALL = YES;
 			};
 			name = Release;
 		};
@@ -3722,6 +3913,15 @@
 			buildConfigurations = (
 				9201F93918D5A38A003D1ACA /* Debug */,
 				9201F93A18D5A38A003D1ACA /* Release */,
+			);
+			defaultConfigurationIsVisible = 0;
+			defaultConfigurationName = Release;
+		};
+		922B0FC22A602154003690BB /* Build configuration list for PBXNativeTarget "caca_out" */ = {
+			isa = XCConfigurationList;
+			buildConfigurations = (
+				922B0FC32A602154003690BB /* Debug */,
+				922B0FC42A602154003690BB /* Release */,
 			);
 			defaultConfigurationIsVisible = 0;
 			defaultConfigurationName = Release;

configure
@@ -339,7 +339,7 @@ get_size()
 
 #zlib MUST be first
 all_packages="zlib opensvc openhevc platinum freetype ssl jpeg openjpeg png mad a52 xvid faad ffmpeg freenect vorbis theora nghttp2"
-all_packages="$all_packages oss dvb4linux alsa pulseaudio jack directfb hid lzma tinygl vtb ogg sdl caption mpeghdec"
+all_packages="$all_packages oss dvb4linux alsa pulseaudio jack directfb hid lzma tinygl vtb ogg sdl caption mpeghdec libcaca"
 disabled_packages=""
 
 #init all has_FOO=no and set FOO_cflags=
@@ -1699,6 +1699,10 @@ int main( void ) {  nghttp2_session *ng_sess; nghttp2_session_del(ng_sess); retu
 config_package caption "libcaption" "" "-lcaption" "" '#include <caption/cea708.h>
 int main( void ) { caption_frame_t ccframe; caption_frame_init(&ccframe); return 0; }'
 
+config_package libcaca "libcaca" "" "-lcaca" "" '#include <caca.h>
+int main( void ) { caca_canvas_t *canvas = caca_create_canvas(20, 20); return 0; }'
+
+
 config_package mpeghdec "mpeghdec" "" " -lSYS -lFDK -lDRCdec -lPCMutils -lMpeghDec -lIGFdec -lArithCoding -lFormatConverter -lUIManager -lgVBAPRenderer -lMpegTPDec" "" '
 #ifndef bool
 typedef int bool;
@@ -2493,6 +2497,7 @@ echo "FAAD: $has_faad"
 echo "XVID: $has_xvid"
 echo "libcaption: $has_caption"
 echo "MpeghDecoder: $has_mpeghdec"
+echo "libcaca: $has_libcaca"
 
 ffopts=""
 if test "$has_ffmpeg" != "no" ; then
@@ -2865,6 +2870,14 @@ fi
 
 if test "$has_caption" != "no" ; then
     echo "#define GPAC_HAS_LIBCAPTION" >> $TMPH
+fi
+
+
+if test "$has_libcaca" != "no" ; then
+    echo "#define GPAC_HAS_LIBCACAC" >> $TMPH
+    echo "CONFIG_CACA=yes" >> config.mak
+else
+    echo "CONFIG_CACA=no" >> config.mak
 fi
 
 if test "$has_mpeghdec" != "no" ; then

commit c378fc45fe241f641fe49ab86239cd78a1c00a3d

fixed compil bugs introduced in 0d9fd4fc

modules/jack/jack.c
@@ -215,7 +215,7 @@ Jack_Setup (GF_AudioOutput * dr, void *os_handle, u32 num_buffers,
 	memset (ctx->jackClientName, 0, MAX_JACK_CLIENT_NAME_SZ);
 	snprintf (ctx->jackClientName, MAX_JACK_CLIENT_NAME_SZ, "gpac-%d", gf_sys_get_process_id() );
 
-	ctx->autoConnect = gf_module_get_bool((GF_BaseInterface *)dr, "auto-connect"))
+	ctx->autoConnect = gf_module_get_bool((GF_BaseInterface *)dr, "auto-connect");
 
 	if (!gf_module_get_bool((GF_BaseInterface *)dr, "start-server"))
 		options |= JackNoStartServer;

modules/pulseaudio/pulseaudio.c
@@ -67,10 +67,10 @@ PulseAudio_Setup (GF_AudioOutput * dr, void *os_handle,
 	PulseAudioContext *ctx = (PulseAudioContext *) dr->opaque;
 	if (ctx == NULL)
 		return GF_BAD_PARAM;
-	opt = gf_module_get_key(dr, "name");
+	opt = gf_module_get_key((GF_BaseInterface *)dr, "name");
 	ctx->output_name = opt ? ctx->output_name : "GPAC";
 
-	opt = gf_module_get_key(dr, "description");
+	opt = gf_module_get_key((GF_BaseInterface *)dr, "description");
 	ctx->output_description = opt  ? opt  : "GPAC Output";
 	return GF_OK;
 }

modules/x11_out/x11_out.c
@@ -1322,7 +1322,7 @@ X11_SetupWindow (GF_VideoOutput * vout)
 #endif
 
 #ifdef GPAC_HAS_X11_XV
-	if (!gf_module_get_bool((GF_BaseInterface *vout), "colorkey")) {
+	if (!gf_module_get_bool((GF_BaseInterface *)vout, "colorkey")) {
 		xWindow->xvport = X11_GetXVideoPort(vout, GF_PIXEL_YV12, 0);
 	} else {
 		xWindow->xvport = X11_GetXVideoPort(vout, GF_PIXEL_YV12, 1);