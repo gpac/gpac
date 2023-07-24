18dcc81aa45717891efdbe07f2c4e82639fd1fb6
jeanlf <jeanlf@gpac.io>
2023-07-24 12:42:57 +0200

    fixed possible crash introduced by 317c0c69

--- src/filter_core/filter_pid.c
+++ src/filter_core/filter_pid.c
@@ -4032,7 +4032,7 @@ static void gf_filter_pid_set_args_internal(GF_Filter *filter, GF_FilterPid *pid
 		if (!parse_prop)
 			goto skip_arg;
 
-		if (strchr(value, '$') || strchr(value, '@')) {
+		if (value && (strchr(value, '$') || strchr(value, '@'))) {
 			char *a_value = gf_strdup(value);
 			filter_solve_prop_template(filter, pid, &a_value);
 			strncpy(ref_prop_dump, a_value, GF_PROP_DUMP_ARG_SIZE-1);
