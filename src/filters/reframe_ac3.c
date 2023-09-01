gpac/gpac

commit 20f4275ec4396c8143485a056de066bae14cbf28

fixed potential crash in ac3 reframer on very short streams

src/filters/reframe_ac3.c
@@ -448,13 +448,13 @@ restart:
 				}
 				break;
 			}
-			ac3dmx_check_pid(filter, ctx);
 		}
 
 		if (!ctx->is_playing) {
 			ctx->resume_from = 1 + ctx->ac3_buffer_size - remain;
 			return GF_OK;
 		}
+		ac3dmx_check_pid(filter, ctx);
 
 		if (sync_pos) {
 			GF_LOG(GF_LOG_WARNING, GF_LOG_MEDIA, ("[AC3Dmx] %d bytes unrecovered before sync word\n", sync_pos));

commit 7f4848d80e6db926170c728936e9ad10750f4eb9

fixed bug introduced in 20f4275e

src/filters/reframe_ac3.c
@@ -449,12 +449,12 @@ restart:
 				break;
 			}
 		}
+		ac3dmx_check_pid(filter, ctx);
 
 		if (!ctx->is_playing) {
 			ctx->resume_from = 1 + ctx->ac3_buffer_size - remain;
 			return GF_OK;
 		}
-		ac3dmx_check_pid(filter, ctx);
 
 		if (sync_pos) {
 			GF_LOG(GF_LOG_WARNING, GF_LOG_MEDIA, ("[AC3Dmx] %d bytes unrecovered before sync word\n", sync_pos));