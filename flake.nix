f953b68848b87049656c0ca2e148f43f71f43b62
Niklas Korz <niklas@niklaskorz.de>
2023-09-13 16:38:22 +0200

    Fix nix flake for macOS

--- flake.nix
+++ flake.nix
@@ -1,55 +1,54 @@
 {
   description = "GPAC Multimedia Open Source Project";
-  inputs =  {
+  inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }@inputs: 
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    buildInputs = with pkgs; [ 
-      zlib 
-      git
-      freetype 
-      libjpeg_turbo 
-      libpng 
-      libmad 
-      faad2 
-      libogg 
-      libvorbis 
-      libtheora
-      a52dec
-      ffmpeg
-      xorg.libX11
-      xorg.libXv
-      xorg.xorgproto
-      mesa
-      xvidcore
-      openssl
-      alsa-lib
-      jack2
-      pulseaudio
-      SDL2
-      mesa-demos
-    ];
-    nativeBuildInputs = with pkgs; [ pkg-config ];
- in
-  with pkgs; {  
-    devShells.x86_64-linux.default = mkShell {
-        inherit buildInputs nativeBuildInputs;
-    };
-    packages.x86_64-linux.default =  with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "gpac";
-        src = self;
-        inherit buildInputs nativeBuildInputs;
-        configurePhase = ''
-          runHook preConfigure
-          ./configure --prefix=$out
-          runHook postConfigure
-        '';
-        buildPhase = "make -j $NIX_BUILD_CORES";
-        installPhase = "make install";
-      }; 
-
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        buildInputs = with pkgs; [
+          zlib
+          freetype
+          libjpeg_turbo
+          libpng
+          libmad
+          faad2
+          libaom
+          libogg
+          libvorbis
+          libtheora
+          a52dec
+          ffmpeg
+          xvidcore
+          openssl
+          SDL2
+        ] ++ (lib.optionals stdenv.isLinux [
+          xorg.libX11
+          xorg.libXv
+          xorg.xorgproto
+          mesa
+          alsa-lib
+          jack2
+          pulseaudio
+          mesa-demos
+        ]) ++ (lib.optionals stdenv.isDarwin [
+          darwin.apple_sdk_11_0.frameworks.Carbon
+        ]);
+        nativeBuildInputs = with pkgs; [ pkg-config git ];
+      in
+      with pkgs; {
+        devShells.default = mkShell {
+          inherit buildInputs nativeBuildInputs;
+        };
+        packages.default = stdenv.mkDerivation {
+          name = "gpac";
+          src = self;
+          enableParallelBuilding = true;
+          inherit buildInputs nativeBuildInputs;
+        };
+      });
 }
