671976fccc971b3dff8d3dcf6ebd600472ca64bf
Aurelien David <aurelien.david@telecom-paristech.fr>
2023-10-26 12:28:38 +0200

    ffdmx: fix loop index mismatch on side data parsing (fixes #2664)

--- src/filters/ff_dmx.c
+++ src/filters/ff_dmx.c
@@ -1237,7 +1237,7 @@ GF_Err ffdmx_init_common(GF_Filter *filter, GF_FFDemuxCtx *ctx, u32 grab_type)
 		gf_filter_pid_set_property(pid, GF_PROP_PID_MUX_INDEX, &PROP_UINT(i+1));
 
 		for (j=0; j<(u32) stream->nb_side_data; j++) {
-			ffdmx_parse_side_data(&stream->side_data[i], pid);
+			ffdmx_parse_side_data(&stream->side_data[j], pid);
 		}
 
 		if (ctx->demuxer->nb_chapters) {
