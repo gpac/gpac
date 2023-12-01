154cfa30dca31ed1bc0bf40aed51d3616b83f7fa
jeanlf <jeanlf@gpac.io>
2023-12-01 13:58:01 +0100

    rework 8d178342

--- src/media_tools/av_parsers.c
+++ src/media_tools/av_parsers.c
@@ -1635,14 +1635,15 @@ static u32 av1_uvlc(GF_BitStream *bs, const char *fname)
 {
 	u32 res;
 	u8 leadingZeros = 0;
-	while (gf_bs_available(bs)) {
+	while (1) {
 		Bool done = gf_bs_read_int(bs, 1);
 		if (done)
 			break;
 		leadingZeros++;
-	}
-	if (leadingZeros >= 32) {
-		return 0xFFFFFFFF;
+		//avoid calls to bs_available or bs_is_overflow - see #2698 for poc
+		if (leadingZeros >= 32) {
+			return 0xFFFFFFFF;
+		}
 	}
 	res = gf_bs_read_int(bs, leadingZeros) + (1 << leadingZeros) - 1;
 	gf_bs_log(bs, 2*leadingZeros, fname, res);
