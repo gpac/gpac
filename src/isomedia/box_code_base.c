66a99e89c7aac42ac619545a44bf56d7de4491b7
Aurelien David <aurelien.david@telecom-paristech.fr>
2023-12-11 16:24:49 +0100

    fix use-after-free in audio_sample_entry_read (from oss-fuzz)

--- src/isomedia/box_code_base.c
+++ src/isomedia/box_code_base.c
@@ -4217,7 +4217,19 @@ GF_Err audio_sample_entry_box_read(GF_Box *s, GF_BitStream *bs)
 		if (GF_4CC((u32)data[i+4], (u8)data[i+5], (u8)data[i+6], (u8)data[i+7]) == GF_ISOM_BOX_TYPE_ESDS) {
 			GF_BitStream *mybs = gf_bs_new(data + i, size - i, GF_BITSTREAM_READ);
 			gf_bs_set_cookie(mybs, GF_ISOM_BS_COOKIE_NO_LOGS);
-			if (ptr->esd) gf_isom_box_del_parent(&ptr->child_boxes, (GF_Box *)ptr->esd);
+			if (ptr->esd) {
+
+				gf_list_del_item(ptr->child_boxes, (GF_Box *)ptr->esd);
+
+				for (u32 i=0; i<gf_list_count(ptr->child_boxes); i++) {
+					GF_Box *inner_box = (GF_Box *)gf_list_get(ptr->child_boxes, i);
+					if (inner_box->child_boxes) {
+						gf_list_del_item(inner_box->child_boxes, (GF_Box *)ptr->esd);
+					}
+				}
+
+				gf_isom_box_del((GF_Box *)ptr->esd);
+			}
 			ptr->esd = NULL;
 			e = gf_isom_box_parse((GF_Box **)&ptr->esd, mybs);
 			gf_bs_del(mybs);
