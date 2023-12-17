bdb76c881a4fb63af3f209118871b24b890cbbd6
jeanlf <jeanlf@gpac.io>
2023-12-17 11:39:47 +0100

    fixed possible undefined shift

--- src/media_tools/av_parsers.c
+++ src/media_tools/av_parsers.c
@@ -1645,7 +1645,7 @@ static u32 av1_uvlc(GF_BitStream *bs, const char *fname)
 			return 0xFFFFFFFF;
 		}
 	}
-	res = gf_bs_read_int(bs, leadingZeros) + (1 << leadingZeros) - 1;
+	res = gf_bs_read_int(bs, leadingZeros) + ((u32)1 << leadingZeros) - 1;
 	gf_bs_log(bs, 2*leadingZeros, fname, res);
 	return res;
 }
