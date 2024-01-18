c2c6f6ca6208f1ec103467994d1f7ab18b834dc3
jeanlf <jeanlf@gpac.io>
2024-01-18 08:53:39 +0100

    fixed bug in aieb box read

--- src/isomedia/box_code_drm.c
+++ src/isomedia/box_code_drm.c
@@ -1757,14 +1757,15 @@ GF_Err aeib_box_read(GF_Box *s, GF_BitStream *bs)
 	GF_AdobeEncryptionInfoBox *ptr = (GF_AdobeEncryptionInfoBox*)s;
 	u32 len;
 
-	ISOM_DECREASE_SIZE(ptr, 1);
+	if (!ptr->size)
+		ISOM_DECREASE_SIZE(ptr, 1);
+
 	len = (u32) ptr->size - 1;
 	if (len) {
 		ptr->enc_algo = (char *)gf_malloc(len*sizeof(char));
 		if (!ptr->enc_algo) return GF_OUT_OF_MEM;
 		gf_bs_read_data(bs, ptr->enc_algo, len);
 	}
-	ISOM_DECREASE_SIZE(ptr, 1);
 	ptr->key_length = gf_bs_read_u8(bs);
 	ptr->size = 0;
 	return GF_OK;
