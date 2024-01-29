gpac/gpac

commit 71d351afa0654cca01366b9cc7b1d113c1554ea8

fixed bug in daisy-chain sidx - cf #2733

src/isomedia/movie_fragments.c
@@ -1870,7 +1870,7 @@ GF_Err gf_isom_close_segment(GF_ISOFile *movie, s32 subsegments_per_sidx, GF_ISO
 			sidx->earliest_presentation_time = prev_earliest_cts;
 
 			/*if more subsegments requested than fragments available, make a single sidx*/
-			if ((s32) count <= subsegments_per_sidx)
+			if ((s32) count < subsegments_per_sidx)
 				subsegments_per_sidx = 0;
 
 			if (daisy_chain_sidx && (subsegments_per_sidx<2))

commit b529eff15e0149818ff364d262ac195906bb4cbb

fixed bug introduced in 71d351af

src/isomedia/movie_fragments.c
@@ -1869,12 +1869,18 @@ GF_Err gf_isom_close_segment(GF_ISOFile *movie, s32 subsegments_per_sidx, GF_ISO
 		} else {
 			sidx->earliest_presentation_time = prev_earliest_cts;
 
-			/*if more subsegments requested than fragments available, make a single sidx*/
-			if ((s32) count < subsegments_per_sidx)
-				subsegments_per_sidx = 0;
-
-			if (daisy_chain_sidx && (subsegments_per_sidx<2))
-				subsegments_per_sidx = 2;
+			/*if more subsegments requested than fragments available, make a single sidx
+				for daisy chaining use strict as we don't count the chained entry in subsegments_per_sidx
+			*/
+			if (daisy_chain_sidx) {
+				if ((s32) count < subsegments_per_sidx)
+					subsegments_per_sidx = 0;
+				if (subsegments_per_sidx<2)
+					subsegments_per_sidx = 2;
+			} else {
+				if ((s32) count <= subsegments_per_sidx)
+					subsegments_per_sidx = 0;
+			}
 
 			/*single SIDX, each fragment is a subsegment and we reference all subsegments*/
 			if (!subsegments_per_sidx) {