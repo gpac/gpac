9efb5977c5b7a45e52acd83533076c18bc8f294a
Aurelien David <aurelien.david@telecom-paristech.fr>
2024-01-30 14:07:04 +0100

    fix buffer overflow on mp3 bitrate index
    ossfuzz issue 66196

--- src/media_tools/av_parsers.c
+++ src/media_tools/av_parsers.c
@@ -4520,6 +4520,10 @@ u32 gf_mp3_bit_rate(u32 hdr)
 		GF_LOG(GF_LOG_ERROR, GF_LOG_CODING, ("[MPEG-1/2 Audio] layer index not valid\n"));
 		return 0;
 	}
+	if (bitRateIndex>14) {
+		GF_LOG(GF_LOG_ERROR, GF_LOG_CODING, ("[MPEG-1/2 Audio] Bitrate index not valid\n"));
+		return 0;
+	}
 	return bitrate_table[lidx][bitRateIndex];
 }
 
