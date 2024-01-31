fe46265eda4375086374ed41a4bb7f3906ced4ca
jeanlf <jeanlf@gpac.io>
2024-01-31 18:36:02 +0100

    fixed possible use after free in broken ogg streams

--- src/filters/dmx_ogg.c
+++ src/filters/dmx_ogg.c
@@ -429,15 +429,16 @@ static void oggdmx_check_dur(GF_Filter *filter, GF_OGGDmxCtx *ctx)
 		if (ogg_page_bos(&oggpage)) {
 			ogg_stream_init(&os, ogg_page_serialno(&oggpage));
 			if (ogg_stream_pagein(&os, &oggpage) >= 0 ) {
-				ogg_stream_packetpeek(&os, &oggpacket);
-				if (ogg_stream_pagein(&os, &oggpage) >= 0 ) {
-					ogg_stream_packetpeek(&os, &oggpacket);
-					oggdmx_get_stream_info(&oggpacket, &info);
-				}
-				if (!has_stream) {
-					has_stream = GF_TRUE;
-					ogg_stream_init(&the_os, ogg_page_serialno(&oggpage));
-					the_info = info;
+				if (ogg_stream_packetpeek(&os, &oggpacket)>0) {
+					if (ogg_stream_pagein(&os, &oggpage) >= 0 ) {
+						ogg_stream_packetpeek(&os, &oggpacket);
+						oggdmx_get_stream_info(&oggpacket, &info);
+					}
+					if (!has_stream) {
+						has_stream = GF_TRUE;
+						ogg_stream_init(&the_os, ogg_page_serialno(&oggpage));
+						the_info = info;
+					}
 				}
 			}
 			ogg_stream_clear(&os);
