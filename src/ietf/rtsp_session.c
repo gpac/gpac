2e23cb56ff9aa1f3dbd7e0d528c7e17dd2136cbd
Aurelien David <aurelien.david@telecom-paristech.fr>
2024-02-16 09:53:26 +0100

    check text size in rtsp unpack
    ossfuzz issue 66742

--- src/ietf/rtsp_session.c
+++ src/ietf/rtsp_session.c
@@ -148,6 +148,7 @@ found:
 		if (test[i]=='[') is_ipv6 = GF_TRUE;
 		else if (test[i]==']') is_ipv6 = GF_FALSE;
 		if ( (test[i] == '/') || (!is_ipv6 && (test[i] == ':')) ) break;
+		if (i>=GF_ARRAY_LENGTH(text)) break;
 		text[i] = test[i];
 		i += 1;
 	}
