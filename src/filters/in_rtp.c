c29f928f4e110ec8b6520e664d2e5ddbebd06a11
Aurelien David <aurelien.david@telecom-paristech.fr>
2024-03-12 09:35:48 +0100

    completes b5dc2164
    ossfuzz issue 67354

--- src/filters/in_rtp.c
+++ src/filters/in_rtp.c
@@ -105,7 +105,7 @@ found:
 	if (retest && strstr(retest, "/")) {
 		retest += 1;
 		i = 0;
-		while (i<strlen(retest)) {
+		while (i<strlen(retest) && i<GF_ARRAY_LENGTH(text)) {
 			if (retest[i] == '/') break;
 			text[i] = retest[i];
 			i += 1;
@@ -116,7 +116,7 @@ found:
 	is_ipv6 = GF_FALSE;
 	len = (u32)strlen(test);
 	i = 0;
-	while (i<len) {
+	while (i<len && i<GF_ARRAY_LENGTH(text)) {
 		if (test[i] == '[') is_ipv6 = GF_TRUE;
 		else if (test[i] == ']') is_ipv6 = GF_FALSE;
 		if ((test[i] == '/') || (!is_ipv6 && (test[i] == ':'))) break;
