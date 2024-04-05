2995a6d20498cbd9d8c373d29d6513c7a6ef8c60
jeanlf <jeanlf@gpac.io>
2024-04-05 15:11:04 +0200

    added crypt byte offset option for cenc

--- include/gpac/crypt_tools.h
+++ include/gpac/crypt_tools.h
@@ -192,6 +192,8 @@ typedef struct
 
 	/*! CENS/CBCS pattern */
 	u32 crypt_byte_block, skip_byte_block;
+	/*! forces a minimum clear range for subsamples (ignored otherwise) - the final offset is at least the slice header size*/
+	u32 crypt_byte_offset;
 
 	/* ! for avc1 ctr CENC edition 1 */
 	Bool allow_encrypted_slice_header;
