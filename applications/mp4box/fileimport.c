gpac/gpac

commit 32c1dc0d83d21d551fc4c72983240ee3495e4a19

allow concatenation of tracks with different decoder configs in -cat - see #549

applications/mp4box/fileimport.c
@@ -2879,6 +2879,8 @@ GF_Err cat_isomedia_file(GF_ISOFile *dest, char *fileName, u32 import_flags, GF_
 			if (gf_isom_get_sample_description_count(orig, i+1) != gf_isom_get_sample_description_count(dest, dst_tk)) dst_tk = 0;
 			/*if not forcing cat, check the media codec config is the same*/
 			if (!gf_isom_is_same_sample_description(orig, i+1, 0, dest, dst_tk, 0)) {
+				//we will need to merge the same descriptions
+				if (!force_cat) dst_tk_sample_entry = dst_tk;
 				dst_tk = 0;
 			}
 			/*we force the same visual resolution*/

commit 5e27e1e1fed8d964d51b5373940cd768f0005d26

fixed bug introduced in 32c1dc0d

applications/mp4box/fileimport.c
@@ -2880,7 +2880,7 @@ GF_Err cat_isomedia_file(GF_ISOFile *dest, char *fileName, u32 import_flags, GF_
 			/*if not forcing cat, check the media codec config is the same*/
 			if (!gf_isom_is_same_sample_description(orig, i+1, 0, dest, dst_tk, 0)) {
 				//we will need to merge the same descriptions
-				if (!force_cat) dst_tk_sample_entry = dst_tk;
+				if (!force_cat && !dst_tk_sample_entry) dst_tk_sample_entry = dst_tk;
 				dst_tk = 0;
 			}
 			/*we force the same visual resolution*/