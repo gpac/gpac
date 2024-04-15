gpac/gpac

commit c08973cc893d51eba5abf885c018395975ba81f0

fix rfav1:temporal_delim, see #2681

src/media_tools/av_parsers.c
@@ -2620,9 +2620,9 @@ static void av1_populate_state_from_obu(GF_BitStream *bs, u64 pos, u64 obu_lengt
 GF_Err aom_av1_parse_temporal_unit_from_section5(GF_BitStream *bs, AV1State *state)
 {
 	if (!state) return GF_BAD_PARAM;
-	state->obu_type = -1;
+	Bool first_obu = GF_TRUE;
 
-	while (state->obu_type != OBU_TEMPORAL_DELIMITER) {
+	while (1) {
 		GF_Err e;
 		if (!gf_bs_available(bs))
 			return state->unframed ? GF_BUFFER_TOO_SMALL : GF_OK;
@@ -2638,8 +2638,18 @@ GF_Err aom_av1_parse_temporal_unit_from_section5(GF_BitStream *bs, AV1State *sta
 			return GF_NON_COMPLIANT_BITSTREAM;
 		}
 
+		if (state->obu_type == OBU_TEMPORAL_DELIMITER) {
+			if (!first_obu) {
+				// seek back
+				gf_bs_seek(bs, pos);
+				break;
+			}
+		}
+
 		GF_LOG(GF_LOG_DEBUG, GF_LOG_CODING, ("[AV1] Section5 OBU detected (size "LLU")\n", obu_size));
 		av1_populate_state_from_obu(bs, pos, obu_size, state->obu_type, state);
+
+		first_obu = GF_FALSE;
 	}
 
 	return GF_OK;

commit 05ebaaf712cfe11a77b3290143426cbce441e4ee

fixed potential bug in OBU parsing introduced by c08973cc

include/gpac/internal/media_dev.h
@@ -993,6 +993,7 @@ typedef struct
 
 	//set to one if a temporal delim is found when calling aom_av1_parse_temporal_unit_from_section5
 	u8 has_temporal_delim;
+	u8 has_frame_data;
 } AV1State;
 
 GF_Err aom_av1_parse_temporal_unit_from_section5(GF_BitStream *bs, AV1State *state);

src/media_tools/av_parsers.c
@@ -2622,7 +2622,8 @@ GF_Err aom_av1_parse_temporal_unit_from_section5(GF_BitStream *bs, AV1State *sta
 {
 	if (!state) return GF_BAD_PARAM;
 	state->has_temporal_delim = 0;
-	Bool first_obu = GF_TRUE;
+	//if some OBUs are written, we had a TL before but had to stop parsing due to not enough space
+	Bool first_obu = state->has_frame_data ? GF_FALSE : GF_TRUE;
 
 	while (1) {
 		GF_Err e;
@@ -2647,6 +2648,9 @@ GF_Err aom_av1_parse_temporal_unit_from_section5(GF_BitStream *bs, AV1State *sta
 				break;
 			}
 			state->has_temporal_delim = 1;
+			state->has_frame_data = 0;
+		} else {
+			state->has_frame_data = 1;
 		}
 		first_obu = GF_FALSE;
 
@@ -4038,6 +4042,7 @@ void gf_av1_reset_state(AV1State *state, Bool is_destroy)
 	l2 = state->frame_state.header_obus;
 	memset(&state->frame_state, 0, sizeof(AV1StateFrame));
 	state->frame_state.is_first_frame = GF_TRUE;
+	state->has_frame_data = 0;
 
 	if (is_destroy) {
 		gf_list_del(l1);