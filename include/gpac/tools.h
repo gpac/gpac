gpac/gpac

commit 1efdd3face8631f16a1153a27d5d617136c66a02

refactor: add gf_strict_atoi function to convert a str into int

include/gpac/tools.h
@@ -379,6 +379,16 @@ Compares two timestamps
  */
 Bool gf_timestamp_equal(u64 value1, u64 timescale1, u64 value2, u64 timescale2);
 
+/*!
+\brief strict convert str into integer
+
+Validate and parse str into integer
+\param str text to convert to integer
+\param integer to fill
+\return GF_TRUE if str represents an integer without any leading space nor extra chars
+ */
+Bool gf_strict_atoi(const char* str, int* ans);
+
 /*! @} */
 
 /*!

src/media_tools/route_dmx.c
@@ -1160,9 +1160,7 @@ static GF_Err gf_route_service_setup_stsid(GF_ROUTEDmx *routedmx, GF_ROUTEServic
 		while ((att = gf_list_enum(rs->attributes, &j))) {
 			if (!stricmp(att->name, "dIpAddr")) dst_ip = att->value;
 			else if (!stricmp(att->name, "dPort")) {
-				char* end_ptr;
-				dst_port = strtol(att->value, &end_ptr, 10); 
-				if(isspace(*att->value) || end_ptr == att->value || *end_ptr != '\0') {
+				if(! gf_strict_atoi(att->value, &dst_port)) {
 					GF_LOG(GF_LOG_ERROR, GF_LOG_ROUTE, ("[ROUTE] Service %d wrong dPort value (%s), it should be numeric \n", s->service_id, att->value));
 					return GF_CORRUPTED_DATA;
 				} else if(dst_port >= 65536 || dst_port < 0) {
@@ -1204,9 +1202,7 @@ static GF_Err gf_route_service_setup_stsid(GF_ROUTEDmx *routedmx, GF_ROUTEServic
 			k=0;
 			while ((att = gf_list_enum(ls->attributes, &k))) {
 				if (!strcmp(att->name, "tsi")) {
-					char* end_ptr;
-					tsi = strtol(att->value, &end_ptr, 10); 
-					if(isspace(*att->value) || end_ptr == att->value || *end_ptr != '\0') {
+					if(! gf_strict_atoi(att->value, &tsi)) {
 						GF_LOG(GF_LOG_ERROR, GF_LOG_ROUTE, ("[ROUTE] Service %d wrong TSI value (%s), it should be numeric \n", s->service_id, att->value));
 						return GF_CORRUPTED_DATA;
 					}
@@ -1262,9 +1258,7 @@ static GF_Err gf_route_service_setup_stsid(GF_ROUTEDmx *routedmx, GF_ROUTEServic
 							while ((att = gf_list_enum(fdt->attributes, &n))) {
 								if (!strcmp(att->name, "Content-Location")) rf->filename = gf_strdup(att->value);
 								else if (!strcmp(att->name, "TOI")) {
-									char * end_ptr;
-									rf->toi = strtol(att->value, &end_ptr, 10); 
-									if(isspace(*att->value) || end_ptr == att->value || *end_ptr != '\0') {
+									if(! gf_strict_atoi(att->value, &rf->toi)) {
 										GF_LOG(GF_LOG_ERROR, GF_LOG_ROUTE, ("[ROUTE] Service %d wrong TOI value (%s), it should be numeric \n", s->service_id, att->value));
 										gf_free(rf->filename);
 										gf_free(rf);
@@ -1299,9 +1293,7 @@ static GF_Err gf_route_service_setup_stsid(GF_ROUTEDmx *routedmx, GF_ROUTEServic
 							while ((att = gf_list_enum(fdt->attributes, &n))) {
 								if (!strcmp(att->name, "Content-Location")) rf->filename = gf_strdup(att->value);
 								else if (!strcmp(att->name, "TOI")) {
-									char * end_ptr;
-									rf->toi = strtol(att->value, &end_ptr, 10); 
-									if(isspace(*att->value) || end_ptr == att->value || *end_ptr != '\0') {
+									if(! gf_strict_atoi(att->value, &rf->toi)) {
 										GF_LOG(GF_LOG_ERROR, GF_LOG_ROUTE, ("[ROUTE] Service %d wrong TOI value (%s), it should be numeric \n", s->service_id, att->value));
 										gf_free(rf->filename);
 										gf_free(rf);

src/utils/error.c
@@ -2169,3 +2169,9 @@ Bool gf_parse_frac(const char *value, GF_Fraction *frac)
 	frac->den = (u32) r.den;
 	return res;
 }
+
+Bool gf_strict_atoi(const char* str, int* ans) {
+    char * end_ptr;
+    *ans = strtol(str, &end_ptr, 10); 
+    return !isspace(*str) && end_ptr != str && *end_ptr == '\0';
+}

commit 97dd6ba14d8620cfdf94552041d3539eb4e8e70d

fixed bug introduced in 1efdd3fa

include/gpac/tools.h
@@ -397,7 +397,18 @@ Validate and parse str into integer
 \param ans integer to fill
 \return GF_TRUE if str represents an integer without any leading space nor extra chars
  */
-Bool gf_strict_atoi(const char* str, int* ans);
+Bool gf_strict_atoi(const char* str, s32* ans);
+
+/*!
+\brief strict convert str into unsigned integer
+
+Validate and parse str into integer
+\param str text to convert to integer
+\param ans unsigned integer to fill
+\return GF_TRUE if str represents an unsigned integer without any leading space nor extra chars
+*/
+Bool gf_strict_atoui(const char* str, u32* ans);
+
 
 /*! @} */
 

src/media_tools/route_dmx.c
@@ -2,7 +2,7 @@
  *			GPAC - Multimedia Framework C SDK
  *
  *			Authors: Jean Le Feuvre
- *			Copyright (c) Telecom ParisTech 2018-2023
+ *			Copyright (c) Telecom ParisTech 2018-2024
  *					All rights reserved
  *
  *  This file is part of GPAC / Media Tools ROUTE (ATSC3, DVB-I) demux sub-project
@@ -1189,10 +1189,10 @@ static GF_Err gf_route_service_setup_stsid(GF_ROUTEDmx *routedmx, GF_ROUTEServic
 		while ((att = gf_list_enum(rs->attributes, &j))) {
 			if (!stricmp(att->name, "dIpAddr")) dst_ip = att->value;
 			else if (!stricmp(att->name, "dPort")) {
-				if(! gf_strict_atoi(att->value, &dst_port)) {
+				if(! gf_strict_atoui(att->value, &dst_port)) {
 					GF_LOG(GF_LOG_ERROR, GF_LOG_ROUTE, ("[ROUTE] Service %d wrong dPort value (%s), it should be numeric \n", s->service_id, att->value));
 					return GF_CORRUPTED_DATA;
-				} else if(dst_port >= 65536 || dst_port < 0) {
+				} else if(dst_port >= 65536) {
 					GF_LOG(GF_LOG_ERROR, GF_LOG_ROUTE, ("[ROUTE] Service %d wrong dPort value (%s), it should belong to the interval [0, 65535] \n", s->service_id, att->value));
 					return GF_CORRUPTED_DATA;
 				}
@@ -1231,7 +1231,7 @@ static GF_Err gf_route_service_setup_stsid(GF_ROUTEDmx *routedmx, GF_ROUTEServic
 			k=0;
 			while ((att = gf_list_enum(ls->attributes, &k))) {
 				if (!strcmp(att->name, "tsi")) {
-					if(! gf_strict_atoi(att->value, &tsi)) {
+					if(! gf_strict_atoui(att->value, &tsi)) {
 						GF_LOG(GF_LOG_ERROR, GF_LOG_ROUTE, ("[ROUTE] Service %d wrong TSI value (%s), it should be numeric \n", s->service_id, att->value));
 						return GF_CORRUPTED_DATA;
 					}
@@ -1287,7 +1287,7 @@ static GF_Err gf_route_service_setup_stsid(GF_ROUTEDmx *routedmx, GF_ROUTEServic
 							while ((att = gf_list_enum(fdt->attributes, &n))) {
 								if (!strcmp(att->name, "Content-Location")) rf->filename = gf_strdup(att->value);
 								else if (!strcmp(att->name, "TOI")) {
-									if(! gf_strict_atoi(att->value, &rf->toi)) {
+									if(! gf_strict_atoui(att->value, &rf->toi)) {
 										GF_LOG(GF_LOG_ERROR, GF_LOG_ROUTE, ("[ROUTE] Service %d wrong TOI value (%s), it should be numeric \n", s->service_id, att->value));
 										gf_free(rf->filename);
 										gf_free(rf);
@@ -1322,7 +1322,7 @@ static GF_Err gf_route_service_setup_stsid(GF_ROUTEDmx *routedmx, GF_ROUTEServic
 							while ((att = gf_list_enum(fdt->attributes, &n))) {
 								if (!strcmp(att->name, "Content-Location")) rf->filename = gf_strdup(att->value);
 								else if (!strcmp(att->name, "TOI")) {
-									if(! gf_strict_atoi(att->value, &rf->toi)) {
+									if(! gf_strict_atoui(att->value, &rf->toi)) {
 										GF_LOG(GF_LOG_ERROR, GF_LOG_ROUTE, ("[ROUTE] Service %d wrong TOI value (%s), it should be numeric \n", s->service_id, att->value));
 										gf_free(rf->filename);
 										gf_free(rf);

src/utils/error.c
@@ -2254,10 +2254,16 @@ Bool gf_parse_frac(const char *value, GF_Fraction *frac)
 	return res;
 }
 
-Bool gf_strict_atoi(const char* str, int* ans) {
+Bool gf_strict_atoi(const char* str, s32* ans) {
     char* end_ptr = NULL;
     *ans = strtol(str, &end_ptr, 10);
     return !isspace(*str) && end_ptr != str && *end_ptr == '\0';
+}
+
+Bool gf_strict_atoui(const char* str, u32* ans) {
+	char* end_ptr = NULL;
+	*ans = strtoul(str, &end_ptr, 10);
+	return !isspace(*str) && end_ptr != str && *end_ptr == '\0';
 }
 
 const char* gf_strmemstr(const char *data, u32 data_size, const char *pat)