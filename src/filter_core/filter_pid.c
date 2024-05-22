gpac/gpac

commit 6cd46593c1e029d46ad5b2f5d02731e4d64e8f0b

added ipid:// scheme for avmix playlist sources

src/filter_core/filter_pid.c
@@ -1858,7 +1858,7 @@ static Bool filter_pid_check_fragment(GF_FilterPid *src_pid, char *frag_name, Bo
 	return is_equal;
 }
 
-static Bool filter_source_id_match(GF_FilterPid *src_pid, const char *id, GF_Filter *dst_filter, Bool *pid_excluded, Bool *needs_clone)
+Bool filter_source_id_match(GF_FilterPid *src_pid, const char *src_filter_id, GF_Filter *dst_filter, Bool *pid_excluded, Bool *needs_clone, const char *ext_source_ids)
 {
 	const char *source_ids;
 	char *resolved_source_ids = NULL;
@@ -1867,15 +1867,15 @@ static Bool filter_source_id_match(GF_FilterPid *src_pid, const char *id, GF_Fil
 	Bool has_default_match;
 	Bool is_pid_excluded;
 	*pid_excluded = GF_FALSE;
-	if (!dst_filter->source_ids)
+	if (!ext_source_ids || (dst_filter && !dst_filter->source_ids))
 		return GF_TRUE;
-	if (!id)
+	if (dst_filter && !src_filter_id)
 		return GF_FALSE;
 
 sourceid_reassign:
-	source_ids = resolved_source_ids ? resolved_source_ids : dst_filter->source_ids;
+	source_ids = resolved_source_ids ? resolved_source_ids : (dst_filter ? dst_filter->source_ids : ext_source_ids);
 	if (!first_pass) {
-		gf_assert(dst_filter->dynamic_source_ids);
+		gf_assert(dst_filter && dst_filter->dynamic_source_ids);
 		source_ids = dst_filter->dynamic_source_ids;
 	}
 	has_default_match = GF_FALSE;
@@ -1912,7 +1912,11 @@ sourceid_reassign:
 		if (source_ids[0]=='*') { }
 		// id does not match
 		else {
-			Bool res = strncmp(id, source_ids, sublen) ? GF_FALSE : GF_TRUE;
+			Bool res;
+			if (src_filter_id)
+				res = strncmp(src_filter_id, source_ids, sublen) ? GF_FALSE : GF_TRUE;
+			else
+				res = sublen ? GF_FALSE : GF_TRUE;
 			if (use_neg) res = !res;
 			if (!res) {
 				source_ids += len+1;
@@ -1945,7 +1949,7 @@ sourceid_reassign:
 
 			if (! filter_pid_check_fragment(src_pid, frag_name, &local_pid_excluded, &needs_resolve, &prop_not_found, prop_dump_buffer)) {
 				if (needs_resolve) {
-					if (first_pass) {
+					if (first_pass && dst_filter) {
 						char *sid = resolved_source_ids ? resolved_source_ids : dst_filter->source_ids;
 						char *frag_sep = strchr(frag_name, dst_filter->session->sep_name);
 						gf_assert(frag_sep);
@@ -1974,6 +1978,8 @@ sourceid_reassign:
 				//remember we succeed because PID has no matching property
 				if (!prop_not_found)
 					all_frags_not_found = GF_FALSE;
+				else if (ext_source_ids)
+					all_matched = GF_FALSE;
 			}
 
 			if (!next_frag) break;
@@ -2004,14 +2010,14 @@ sourceid_reassign:
 
 	if (!result) {
 		if (resolved_source_ids) gf_free(resolved_source_ids);
-		if (dst_filter->dynamic_source_ids && first_pass) {
+		if (dst_filter && dst_filter->dynamic_source_ids && first_pass) {
 			first_pass = GF_FALSE;
 			goto sourceid_reassign;
 		}
 		*pid_excluded = is_pid_excluded;
 		return GF_FALSE;
 	}
-	if (resolved_source_ids) {
+	if (resolved_source_ids && dst_filter) {
 		if (!dst_filter->dynamic_source_ids) {
 			dst_filter->dynamic_source_ids = dst_filter->source_ids;
 			dst_filter->source_ids = resolved_source_ids;
@@ -4292,7 +4298,7 @@ void gf_filter_pid_set_args(GF_Filter *filter, GF_FilterPid *pid)
 	pid->request_property_map = req_map_bck;
 }
 
-static const char *gf_filter_last_id_in_chain(GF_Filter *filter, Bool ignore_first)
+const char *gf_filter_last_id_in_chain(GF_Filter *filter, Bool ignore_first)
 {
 	u32 i;
 	const char *id;
@@ -4966,7 +4972,7 @@ single_retry:
 		if (filter_id) {
 			if (filter_dst->source_ids) {
 				Bool pid_excluded=GF_FALSE;
-				if (!filter_source_id_match(pid, filter_id, filter_dst, &pid_excluded, &needs_clone)) {
+				if (!filter_source_id_match(pid, filter_id, filter_dst, &pid_excluded, &needs_clone, NULL)) {
 					Bool not_ours=GF_TRUE;
 					//if filter is a dynamic one with an ID set, fetch ID from previous filter in chain
 					//this is need for cases such as "-i source filterFoo @ -o live.mpd":
@@ -4976,7 +4982,7 @@ single_retry:
 					//which is the filter ID of filterFoo
 					if (filter->dynamic_filter && filter->id) {
 						const char *src_filter_id = gf_filter_last_id_in_chain(filter, GF_TRUE);
-						if (filter_source_id_match(pid, src_filter_id, filter_dst, &pid_excluded, &needs_clone)) {
+						if (filter_source_id_match(pid, src_filter_id, filter_dst, &pid_excluded, &needs_clone, NULL)) {
 							not_ours = GF_FALSE;
 						}
 					}
@@ -5016,7 +5022,7 @@ single_retry:
 				GF_LOG(GF_LOG_DEBUG, GF_LOG_FILTER, ("PID %s does not match filter %s source ID\n", pid->name, filter_dst->name));
 				continue;
 			}
-			if (!filter_source_id_match(pid, "*", filter_dst, &pid_excluded, &needs_clone)) {
+			if (!filter_source_id_match(pid, "*", filter_dst, &pid_excluded, &needs_clone, NULL)) {
 				if (pid_excluded && !num_pass) filter_found_but_pid_excluded = GF_TRUE;
 				GF_LOG(GF_LOG_DEBUG, GF_LOG_FILTER, ("PID %s is excluded by filter %s source ID\n", pid->name, filter_dst->name));
 				continue;

share/doc/man/gpac-filters.1
@@ -6760,7 +6760,7 @@ repair (enum, default: simple): repair mode for corrupted files
 .br
 * strict: incomplete mdat boxes will be lost as well as preceding moof boxes
 .br
-* full: HTTP-based repair, not yet implemented
+* full: HTTP-based repair of all lost packets
 .br
 
 .br
@@ -10428,6 +10428,34 @@ Properties for sourceURL objects
 .P
 .B
 Notes
+.br
+The special URL scheme ipid:// can be used to locate an input pid by link directives.
+.br
+Example
+.br
+in=ipid://#foo=bar
+.br
+
+.br
+This will use pids having property foo with value bar, regardless of source filter ID.
+.br
+
+.br
+Example
+.br
+in=ipid://TEST#foo=bar
+.br
+
+.br
+This will use pids having property foo with value bar coming from filter with ID TEST.
+.br
+
+.br
+When using the ipid:// scheme, filter chains cannot be specified (in accepts a single argument) and port is ignored.
+.br
+The syntax for link directive is the same as in gpac. However, if a listed property is not found on the input pid, the matching will fail.
+.br
+
 .br
 When launching a child process, the input filter is created first and the child process launched afterwards.
 .br

share/scripts/jsf/avmix/help.js
@@ -185,6 +185,16 @@ When reloading the playlist:
 - raw (true): indicate if input port is decoded AV (true) or compressed AV (false) when using a dedicated gpac process, ignored otherwise
 
 ### Notes
+The special URL scheme \`ipid://\` can be used to locate an input pid by link directives.
+EX in=ipid://#foo=bar
+This will use pids having property \`foo\` with value \`bar\`, regardless of source filter ID.
+
+EX in=ipid://TEST#foo=bar
+This will use pids having property \`foo\` with value \`bar\` coming from filter with ID \`TEST\`.
+
+When using the \`ipid://\` scheme, filter chains cannot be specified (in accepts a single argument) and \`port\` is ignored.
+The syntax for link directive is the same as in gpac. However, if a listed property is not found on the input pid, the matching will fail.
+
 When launching a child process, the input filter is created first and the child process launched afterwards.
 
 Warning: When launching a child process directly (e.g. \`in="ffmpeg ..."\`), any relative URL used in \`in\` must be relative to the current working directory.

share/scripts/jsf/avmix/init.js
@@ -2100,10 +2100,21 @@ function update_display_list()
 	root_scene.scenes.forEach(elm => do_traverse_all(elm, update_scene) );
 }
 
+let nolive_timeout_start = 0;
 function process_video()
 {
 	if (!video_inputs_ready) {
 		if (!video_time && !live_forced_play) {
+			let waitfor = filter.lwait;
+			if (!filter.live) waitfor *= 10;
+
+			if (!nolive_timeout_start) nolive_timeout_start = current_utc_clock;
+			else if (current_utc_clock - nolive_timeout_start > waitfor) {
+				print(GF_LOG_ERROR, 'No video input after ' + waitfor + ' ms in non-live mode, aborting');
+				do_terminate();
+				do_audio = false;
+				return;
+			}
 			print(GF_LOG_DEBUG, 'video not init');
 			do_audio = false;
 			return;
@@ -3255,6 +3266,13 @@ function get_source_by_pid(pid)
 	let res = null;
 	sources.forEach( elem => { 
 		elem.fsrc.forEach( (f, index) => {
+			if (typeof f.__dummy_tmp != 'undefined') {
+				if (pid.match_source(f._tmp_arg)) {
+					pid.skipped = false;
+					res = elem;
+				}
+				return;
+			}
 			if (!pid.is_filter_in_parents(f)) return;
 
 			if ((f.media_type=="all") 
@@ -3268,7 +3286,7 @@ function get_source_by_pid(pid)
 				pid.skipped = true;
 			}
 
-		} );
+		} );		
 	} );
 	return res;
 }
@@ -3399,13 +3417,12 @@ function apply_links(links, target, fchain)
 		return;
 	} 
 	let f_src = fchain[link_idx];
-
 	if (target) {
 		target.set_source(f_src, link_arg);
 	} else {
 		filter.set_source_restricted(f_src, link_arg);
 	}
-
+	f_src.require_source_id = true;
 	});
 	return broken_links;
 }
@@ -3425,16 +3442,19 @@ function open_source(s)
 
 	let f, args, i;
 	let port="";
-	let opts="";
 	let append_filter = null;
 	let local_filter = null;
-	if (typeof src.port == 'string') port = src.port;
-	if (typeof src.opts == 'string') opts = src.opts;
-	let use_raw = (typeof src.raw == 'boolean') ? src.raw : true;
+
+	if (src.in.indexOf('ipid://')<0) {
+		if (typeof src.port == 'string') port = src.port;
+	}
 
 	src.local_pipe = null;
 	src.process_id = null;
 	if (port.length) {
+		let use_raw = (typeof src.raw == 'boolean') ? src.raw : true;
+		let opts="";
+		if (typeof src.opts == 'string') opts = src.opts;
 		let do_cat_url = true;
 		let src_url = "";
 		let rfopts = "reframer";
@@ -3559,8 +3579,21 @@ function open_source(s)
 		try { 
 			if (prev_f) {
 				f = filter.add_filter(arg);
-			} else {
-				//relative to playlist
+			}
+			//ipid load 
+			else if (arg.startsWith('ipid://')) {
+				f = {};
+				f.__dummy_tmp = true;
+				f._tmp_arg = arg;
+				f.remove = function(){};
+				links.length = 0;
+					if (i+1<args.length) {
+					print(GF_LOG_WARNING, 'Cannot use filter chain with ipid:// source scheme, chain must be specified at prompt or when creating graph - ignoring');
+					args.length = 0;
+				}
+			}
+			//relative to playlist
+			else {
 				f = filter.add_source(arg, filter.pl);
 				links.length = 0;
 			}
@@ -3599,7 +3632,7 @@ function open_source(s)
 		}
 		prev_f = null;	
 	}
-	if (prev_f) {
+	if (prev_f && (typeof f.__dummy_tmp == 'undefined')) {
 		filter.set_source_restricted(prev_f);
 	}
 

src/filter_core/filter_session.h
@@ -1294,6 +1294,10 @@ GF_PropertyValue gf_filter_parse_prop_solve_env_var(GF_FilterSession *fs, GF_Fil
 //check if item can be added to a reservoir queue, returns GF_TRUE if not added
 Bool gf_fq_res_add(GF_FilterQueue *fq, void *item);
 
+Bool filter_source_id_match(GF_FilterPid *src_pid, const char *id, GF_Filter *dst_filter, Bool *pid_excluded, Bool *needs_clone, const char *source_ids);
+const char *gf_filter_last_id_in_chain(GF_Filter *filter, Bool ignore_first);
+
+
 #endif //_GF_FILTER_SESSION_H_
 
 

src/filters/jsfilter.c
@@ -2916,6 +2916,31 @@ static JSValue jsf_pid_forward(JSContext *ctx, JSValueConst this_val, int argc, 
     return JS_UNDEFINED;
 }
 
+static JSValue jsf_pid_match_source(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+{
+	JSValue ret = JS_FALSE;
+	GF_JSPidCtx *pctx = JS_GetOpaque(this_val, jsf_pid_class_id);
+    if (!pctx || !argc) return GF_JS_EXCEPTION(ctx);
+    if (pctx->pid == pctx->pid->pid) return GF_JS_EXCEPTION(ctx);
+    const char *src_id = JS_ToCString(ctx, argv[0]);
+    if (!src_id)
+		return ret;
+	const char *sid = src_id;
+	if (!strncmp(src_id, "ipid://", 7)) sid+=7;
+
+
+	Bool pid_excluded, needs_clone;
+	if (filter_source_id_match(pctx->pid, pctx->pid->pid->filter->id, NULL, &pid_excluded, &needs_clone, sid)) {
+		ret = JS_TRUE;
+	} else {
+		const char *src_filter_id = gf_filter_last_id_in_chain(pctx->pid->pid->filter, GF_TRUE);
+		if (src_filter_id && filter_source_id_match(pctx->pid, src_filter_id, NULL, &pid_excluded, &needs_clone, sid)) {
+			ret = JS_TRUE;
+		}
+	}
+	JS_FreeCString(ctx, src_id);
+    return ret;
+}
 
 static const JSCFunctionListEntry jsf_pid_funcs[] = {
     JS_CGETSET_MAGIC_DEF("name", jsf_pid_get_prop, jsf_pid_set_prop, JSF_PID_NAME),
@@ -2973,6 +2998,7 @@ static const JSCFunctionListEntry jsf_pid_funcs[] = {
     JS_CFUNC_DEF("forward", 0, jsf_pid_forward),
     JS_CFUNC_DEF("negotiate_prop", 0, jsf_pid_negotiate_prop),
     JS_CFUNC_DEF("ignore_blocking", 0, jsf_pid_ignore_blocking),
+    JS_CFUNC_DEF("match_source", 0, jsf_pid_match_source),
 };
 
 enum

src/filters/reframer.c
@@ -257,9 +257,13 @@ GF_Err reframer_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remo
 
 	if (is_remove) {
 		if (st) {
-			if (st->opid)
+			if (st->opid) {
 				gf_filter_pid_remove(st->opid);
+				gf_filter_pid_set_udta(st->opid, NULL);
+			}
 			gf_list_del_item(ctx->streams, st);
+			if (st->ipid)
+				gf_filter_pid_set_udta(st->ipid, NULL);
 			reframer_reset_stream(ctx, st, GF_TRUE);
 		}
 		return GF_OK;

commit 986b171d602acd5e996f625bd6d7686ebd1cf72b

fixed bug introduced in 6cd46593

src/filter_core/filter_pid.c
@@ -1867,11 +1867,13 @@ Bool filter_source_id_match(GF_FilterPid *src_pid, const char *src_filter_id, GF
 	Bool has_default_match;
 	Bool is_pid_excluded;
 	*pid_excluded = GF_FALSE;
-	if (!ext_source_ids || (dst_filter && !dst_filter->source_ids))
-		return GF_TRUE;
-	if (dst_filter && !src_filter_id)
-		return GF_FALSE;
-
+	if (dst_filter) {
+		if (!dst_filter->source_ids)
+			return GF_TRUE;
+		if (!src_filter_id)
+			return GF_FALSE;
+	}
+	
 sourceid_reassign:
 	source_ids = resolved_source_ids ? resolved_source_ids : (dst_filter ? dst_filter->source_ids : ext_source_ids);
 	if (!first_pass) {