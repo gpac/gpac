c6b31cc9c864bbc9e41221a2934c1daedb52c6c2
jeanlf <jeanlf@gpac.io>
2024-06-25 13:56:42 +0200

    added dnxhd mapping

--- include/gpac/constants.h
+++ include/gpac/constants.h
@@ -636,6 +636,7 @@ typedef enum
 	GF_CODECID_MSPEG4_V3 = GF_4CC('D','I','V','3'),
 
 	GF_CODECID_ALAC = GF_4CC('A','L','A','C'),
+	GF_CODECID_DNXHD = GF_4CC('D','N','x','H'),
 
 	//fake codec IDs for RTP
 	GF_CODECID_FAKE_MP2T = GF_4CC('M','P','2','T')
