gpac/gpac

commit 39762b44500c8f5e6537ea210746c995d73d5a87

Improved multicast abr low latency - sender: llow-latency mode allows in-segment carousel of init seg and manifests - demuxer: improved MABR config update and allow out of order file dispatch events - repair: perform simple repair on first segment when missing first range (tune-in) - cache: dispatch empty packets when updates to reception blob happen non-contiguously with last sent range

src/utils/downloader.c
@@ -3167,8 +3167,12 @@ GF_Err gf_dm_sess_setup_from_url(GF_DownloadSession *sess, const char *url, Bool
 				sess->total_size = gf_cache_get_content_length(sess->cache_entry);
                 sess->bytes_done = sess->total_size;
                 sess->status = GF_NETIO_DATA_TRANSFERED;
-                SET_LAST_ERR(GF_OK)
-            }
+                if (!sess->cache_entry) {
+					SET_LAST_ERR(GF_URL_ERROR)
+				} else {
+					SET_LAST_ERR(GF_OK)
+				}
+			}
 
 			sess->total_time_since_req = gf_cache_get_downtime(sess->cache_entry);
 			if (sess->total_time_since_req)
@@ -5049,7 +5053,7 @@ static void gf_dm_sess_estimate_chunk_rate(GF_DownloadSession *sess, u32 nb_byte
 	}
 }
 
-const u8 *gf_cache_get_content(const DownloadedCacheEntry entry, u32 *size, u32 *max_valid_size);
+const u8 *gf_cache_get_content(const DownloadedCacheEntry entry, u32 *size, u32 *max_valid_size, Bool *was_modified);
 void gf_cache_release_content(const DownloadedCacheEntry entry);
 
 GF_EXPORT
@@ -5115,6 +5119,7 @@ GF_Err gf_dm_sess_fetch_data(GF_DownloadSession *sess, char *buffer, u32 buffer_
 			e = GF_OK;
 		}
     } else if (sess->local_cache_only) {
+		Bool was_modified;
         u32 to_copy, full_cache_size, max_valid_size=0, cache_done;
         const u8 *ptr;
         e = GF_OK;
@@ -5122,10 +5127,14 @@ GF_Err gf_dm_sess_fetch_data(GF_DownloadSession *sess, char *buffer, u32 buffer_
         //always refresh total size
         sess->total_size = gf_cache_get_content_length(sess->cache_entry);
 
-        ptr = gf_cache_get_content(sess->cache_entry, &full_cache_size, &max_valid_size);
+        ptr = gf_cache_get_content(sess->cache_entry, &full_cache_size, &max_valid_size, &was_modified);
         if (!ptr) return GF_OUT_OF_MEM;
 
 		cache_done = gf_cache_is_done(sess->cache_entry);
+		//something went wrong, we cannot have less valid bytes than what we had at previous call(s)
+		if (max_valid_size < sess->bytes_done)
+			cache_done = 2;
+
 		if ((sess->bytes_done >= full_cache_size)|| (cache_done==2)) {
             *read_size = 0;
 			gf_cache_release_content(sess->cache_entry);
@@ -5139,7 +5148,7 @@ GF_Err gf_dm_sess_fetch_data(GF_DownloadSession *sess, char *buffer, u32 buffer_
                 SET_LAST_ERR( GF_OK)
                 return GF_EOS;
             }
-            return GF_IP_NETWORK_EMPTY;
+            return was_modified ? GF_OK : GF_IP_NETWORK_EMPTY;
         }
         //only copy valid bytes for http
         to_copy = max_valid_size - sess->bytes_done;

include/gpac/route.h
@@ -115,6 +115,17 @@ typedef struct
 } GF_LCTFragInfo;
 
 
+/*! Type of partial event*/
+typedef enum
+{
+	/* object is done receiving*/
+	GF_LCTO_PARTIAL_NONE=0,
+	/* object data being notfied is the begining of the payload*/
+	GF_LCTO_PARTIAL_BEGIN,
+	/* object data being notfied is the complete reception buffer (for low latency mode), POTENTIALLY with holes in it*/
+	GF_LCTO_PARTIAL_ANY,
+} GF_LCTObjectPartial;
+
 /*! Structure used to communicate file objects properties to the user*/
 typedef struct
 {
@@ -134,14 +145,22 @@ typedef struct
 	u32 download_ms;
 	/*! flag set if file content has been modified - not set for GF_ROUTE_EVT_DYN_SEG (always true)*/
 	Bool updated;
-    
-    /*! number of fragments, only set for GF_ROUTE_EVT_DYN_SEG*/
-    u32 nb_frags;
-    /*! fragment info, only set for GF_ROUTE_EVT_DYN_SEG*/
-    GF_LCTFragInfo *frags;
+	/*! flag set if first segment has been received for the given TSI - not set for init segments*/
+	Bool first_toi_received;
+
+	/*! number of fragments, only set for GF_ROUTE_EVT_DYN_SEG*/
+	u32 nb_frags;
+	/*! fragment info, set for all file events - this info is shared with the LCT object being reassembled and should not be modified concurrently from route demux
+	Any reallocation of the fragment info SHALL be done using  \ref gf_route_dmx_patch_frag_info
+	*/
+	GF_LCTFragInfo *frags;
 	
 	/*! offset of late received data, only for GF_ROUTE_EVT_LATE_DATA*/
 	u32 late_fragment_offset;
+
+	/*partial state, only used for GF_ROUTE_EVT_DYN_SEG_FRAG*/
+	GF_LCTObjectPartial partial;
+
 	/*! user data set to current object after callback, and passed back on next callbacks on same object
 	 Only used for GF_ROUTE_EVT_FILE, GF_ROUTE_EVT_DYN_SEG, GF_ROUTE_EVT_DYN_SEG_FRAG and GF_ROUTE_EVT_FILE_DELETE
 	 */
@@ -223,17 +242,28 @@ GF_Err gf_route_dmx_process(GF_ROUTEDmx *routedmx);
 \param timeout_us maximum delay in microseconds to wait before considering the object is done when ROUTE/LCT order is not used. A value of 0 implies any out-of-order packet triggers a download completion  (default value is 1 ms).
 \return error code if any
  */
-GF_Err gf_route_set_reorder(GF_ROUTEDmx *routedmx, Bool reorder_needed, u32 timeout_us);
+GF_Err gf_route_dmx_set_reorder(GF_ROUTEDmx *routedmx, Bool reorder_needed, u32 timeout_us);
+
+/*! Progressive dispatch mode for LCT objects*/
+typedef enum
+{
+	/*! notification is only sent once the entire object is received*/
+	GF_ROUTE_DISPATCH_FULL = 0,
+	/*! notifications are sent whenever the first byte-range starting at 0 changes, in which case the partial field is set to GF_LCTO_PARTIAL_BEGIN*/
+	GF_ROUTE_DISPATCH_PROGRESSIVE,
+	/*! notifications are sent whenever a new packet is received, in which case the partial field is set to GF_LCTO_PARTIAL_ANY*/
+	GF_ROUTE_DISPATCH_OUT_OF_ORDER,
+} GF_RouteProgressiveDispatch;
 
 /*! Allow segments to be sent while being downloaded.
  
 \note Files with a static TOI association are always sent once completely received, other files using TOI templating may be sent while being received if enabled. The data sent is always contiguous data since the beginning of the file in that case.
  
 \param routedmx the ROUTE demultiplexer
-\param allow_progressive if TRUE,  fragments of segments will be sent during download
+\param dispatch_mode set dispatch mode
 \return error code if any
  */
-GF_Err gf_route_set_allow_progressive_dispatch(GF_ROUTEDmx *routedmx, Bool allow_progressive);
+GF_Err gf_route_set_dispatch_mode(GF_ROUTEDmx *routedmx, GF_RouteProgressiveDispatch dispatch_mode);
 
 /*! Sets the service ID to tune into for ATSC 3.0
 \param routedmx the ROUTE demultiplexer
@@ -343,7 +373,7 @@ void *gf_route_dmx_get_service_udta(GF_ROUTEDmx *routedmx, u32 service_id);
 \param br_end end offset of byte range being patched
 \return error if any
  */
-GF_Err gf_routedmx_patch_frag_info(GF_ROUTEDmx *routedmx, u32 service_id, GF_ROUTEEventFileInfo *finfo, u32 br_start, u32 br_end);
+GF_Err gf_route_dmx_patch_frag_info(GF_ROUTEDmx *routedmx, u32 service_id, GF_ROUTEEventFileInfo *finfo, u32 br_start, u32 br_end);
 
 /*! Set active status of a representation
 \param routedmx the ROUTE demultiplexer
@@ -354,7 +384,7 @@ GF_Err gf_routedmx_patch_frag_info(GF_ROUTEDmx *routedmx, u32 service_id, GF_ROU
 \param is_selected representation status
 \return error if any
  */
-GF_Err gf_routedmx_mark_active_quality(GF_ROUTEDmx *routedmx, u32 service_id, const char *period_id, s32 as_id, const char *rep_id, Bool is_selected);
+GF_Err gf_route_dmx_mark_active_quality(GF_ROUTEDmx *routedmx, u32 service_id, const char *period_id, s32 as_id, const char *rep_id, Bool is_selected);
 
 
 /*! @} */

include/gpac/tools.h
@@ -1150,6 +1150,8 @@ typedef struct __gf_blob
     /*! blob mutex for multi-thread access */
     struct __tag_mutex *mx;
 #endif
+    /*! last blob modification time (write access) in microsec , 0 if unknown*/
+    u64 last_modification_time;
 	/*! function used to query if a range of a blob in transfer is valid. If NULL, any range is invalid until transfer is done
 	when set this function overrides the blob flags for gf_blob_query_range
 	size is updated to the maximum number of consecutive bytes starting from the goven offset */

share/doc/man/gpac-filters.1
@@ -9080,7 +9080,7 @@ The FLUTE session always uses a symbol length of .I mtu minus 44 bytes.
 .SH Low latency mode
 .LP
 .br
-When using low-latency mode, the input media segments are not re-assembled in a single packet but are instead sent as they are received.
+When using low-latency mode (-llmode)(), the input media segments are not re-assembled in a single packet but are instead sent as they are received.
 .br
 In order for the real-time scheduling of data chunks to work, each fragment of the segment should have a CTS and timestamp describing its timing.
 .br
@@ -9091,6 +9091,10 @@ If this fails, the filter will trigger warnings and send as fast as possible.
 Note: The LCT objects are sent with no length (TOL header) assigned until the final segment size is known, potentially leading to a final 0-size LCT fragment signaling only the final size.
 .br
 
+.br
+In this mode, init segments and manifests are sent at the frequency given by property ROUTECarousel of the source PID if set or by (-carousel)[] option.
+.br
+Indicating ROUTECarousel=0 will disable mid-segment repeating of manifests and init segments.
 .br
 .SH Examples
 .LP
@@ -9825,6 +9829,14 @@ following segmentation as hinted by the graph.
 .br
 .SH Options (expert):
 .LP
+.br
+mode (enum, default: 23001-18): mode to operate in
+.br
+* 23001-18: extract SCTE-35 markers as emib/emeb boxes for Event Tracks
+.br
+* passthrough: pass-through mode adding cue start property on splice points
+.br
+
 .br
 segdur (frac, default: 1/1):   segmentation duration in seconds. 0/0 flushes immediately for each input packet (beware of the bitrate overhead)
 .br

share/doc/man/gpac.1
@@ -5239,6 +5239,11 @@ MS-MPEG4 V3
 .br
 Apple Lossless Audio
 .br
+.TP
+.B dnx
+.br
+AViD DNxHD
+.br
 .SH Stream types
 .LP
 .br

src/filters/dmx_dash.c
@@ -792,11 +792,19 @@ const char *dashdmx_io_get_mime(GF_DASHFileIO *dashio, GF_DASHFileIOSession sess
 }
 const char *dashdmx_io_get_header_value(GF_DASHFileIO *dashio, GF_DASHFileIOSession session, const char *header_name)
 {
+	const char *hdr;
 #ifdef GPAC_USE_DOWNLOADER
-	return gf_dm_sess_get_header((GF_DownloadSession *)session, header_name);
-#else
-	return NULL;
+	hdr = gf_dm_sess_get_header((GF_DownloadSession *)session, header_name);
+	if (hdr) return hdr;
 #endif
+	GF_DASHDmxCtx *ctx = (GF_DASHDmxCtx *)dashio->udta;
+	const GF_PropertyValue *p = gf_filter_pid_get_property_str(ctx->mpd_pid, header_name);
+	if (p) return p->value.string;
+	GF_PropertyEntry *pe=NULL;
+	p = gf_filter_pid_get_info_str(ctx->mpd_pid, header_name, &pe);
+	gf_filter_release_property(pe);
+	if (p) return p->value.string;
+	return NULL;
 }
 u64 dashdmx_io_get_utc_start_time(GF_DASHFileIO *dashio, GF_DASHFileIOSession session)
 {

src/filters/in_http.c
@@ -509,18 +509,18 @@ static GF_Err httpin_process(GF_Filter *filter)
 		}
 		gf_dm_sess_get_stats(ctx->sess, NULL, NULL, &total_size, &bytes_done, &bytes_per_sec, &net_status);
 
-		//wait until we have some data to declare the pid
-        if ((e!= GF_EOS) && !nb_read) {
-            gf_filter_ask_rt_reschedule(filter, 1000);
-            return GF_OK;
-        }
-
 		if (!ctx->pid || ctx->do_reconfigure) {
 			u32 idx;
 			GF_Err cfg_e;
-			const char *hname, *hval;
-			const char *cached = gf_dm_sess_get_cache_name(ctx->sess);
+			const char *hname, *hval, *cached;
 
+			//wait until we have some data to declare the pid
+			if ((e!= GF_EOS) && !nb_read) {
+				gf_filter_ask_rt_reschedule(filter, 1000);
+				return GF_OK;
+			}
+
+			cached = gf_dm_sess_get_cache_name(ctx->sess);
 			ctx->do_reconfigure = GF_FALSE;
 
 			if ((e==GF_EOS) && cached) {
@@ -578,9 +578,10 @@ static GF_Err httpin_process(GF_Filter *filter)
 			gf_filter_pid_set_info(ctx->pid, GF_PROP_PID_DOWN_SIZE, &PROP_LONGUINT(ctx->file_size ? ctx->file_size : bytes_done) );
 		}
 	}
-
 	byte_offset = ctx->nb_read;
 
+	//nb_read may be 0 and error = GF_OK to signal data has been patched somewhere on the reception buffer but not in a contiguous area since last fetch
+	//we send empty packets in this case for filters using the underlying cache object directly (eg isobmf demux)
 	ctx->nb_read += nb_read;
 	if (ctx->file_size && (ctx->nb_read==ctx->file_size)) {
 		if (net_status!=GF_NETIO_DATA_EXCHANGE)

src/filters/in_route.c
@@ -434,6 +434,10 @@ void routein_on_event(void *udta, GF_ROUTEEventType evt, u32 evt_param, GF_ROUTE
 		routein_queue_repair(ctx, evt, evt_param, finfo);
 	} else {
 		routein_on_event_file(ctx, evt, evt_param, finfo, GF_FALSE, GF_FALSE);
+
+        if (ctx->llmode && (evt==GF_ROUTE_EVT_DYN_SEG_FRAG) && (finfo->partial==GF_LCTO_PARTIAL_ANY) && ctx->repair) {
+			routein_queue_repair(ctx, evt, evt_param, finfo);
+		}
 	}
 }
 
@@ -473,12 +477,15 @@ static Bool routein_local_cache_probe(void *par, char *url, Bool is_destroy)
 	return GF_TRUE;
 }
 
-static void routein_set_eos(GF_Filter *filter)
+static void routein_set_eos(GF_Filter *filter, ROUTEInCtx *ctx)
 {
 	u32 i, nb_out = gf_filter_get_opid_count(filter);
 	for (i=0; i<nb_out; i++) {
 		GF_FilterPid *opid = gf_filter_get_opid(filter, i);
 		if (opid) gf_filter_pid_set_eos(opid);
+	}
+	if (ctx->opid) {
+		gf_filter_pid_set_info_str(ctx->opid, "x-mcast-over", &PROP_STRING("yes") );
 	}
 }
 
@@ -500,7 +507,7 @@ static GF_Err routein_process(GF_Filter *filter)
 					u32 diff = gf_sys_clock() - ctx->last_timeout;
 					if (diff > ctx->timeout) {
 						GF_LOG(GF_LOG_INFO, GF_LOG_ROUTE, ("[%s] No data for %d ms, aborting\n", ctx->log_name, diff));
-						routein_set_eos(filter);
+						routein_set_eos(filter, ctx);
 						return GF_EOS;
 					}
 				}
@@ -510,10 +517,13 @@ static GF_Err routein_process(GF_Filter *filter)
 		} else if (!e) {
 			ctx->last_timeout = 0;
 			if (ctx->evt_interrupt) break;
+			//uncomment these to slow down demuxer (usefull when debugging low latency mode)
+//			gf_filter_ask_rt_reschedule(filter, 10000);
+//			break;
 		} else if (e==GF_EOS) {
 			e = routein_do_repair(ctx);
 			if (e == GF_EOS)
-				routein_set_eos(filter);
+				routein_set_eos(filter, ctx);
 			return e;
 		} else {
 			break;
@@ -524,7 +534,7 @@ static GF_Err routein_process(GF_Filter *filter)
 	 	if (diff>ctx->timeout) {
 			GF_LOG(GF_LOG_ERROR, GF_LOG_ROUTE, ("[%s] No data for %d ms, aborting\n", ctx->log_name, diff));
 			gf_filter_setup_failure(filter, GF_SERVICE_ERROR);
-			routein_set_eos(filter);
+			routein_set_eos(filter, ctx);
 			return GF_EOS;
 		}
 	}
@@ -626,10 +636,11 @@ static GF_Err routein_initialize(GF_Filter *filter)
 		sep[0] = ':';
 	}
 	if (!ctx->route_dmx) return GF_SERVICE_ERROR;
-	
-	gf_route_set_allow_progressive_dispatch(ctx->route_dmx, !ctx->fullseg);
 
-	gf_route_set_reorder(ctx->route_dmx, ctx->reorder, ctx->rtimeout);
+	gf_route_set_dispatch_mode(ctx->route_dmx, ctx->llmode ? GF_ROUTE_DISPATCH_OUT_OF_ORDER :
+		(ctx->fullseg ? GF_ROUTE_DISPATCH_FULL : GF_ROUTE_DISPATCH_PROGRESSIVE)
+	);
+	gf_route_dmx_set_reorder(ctx->route_dmx, ctx->reorder, ctx->rtimeout);
 
 	if (ctx->tsidbg) {
 		gf_route_dmx_debug_tsi(ctx->route_dmx, ctx->tsidbg);
@@ -679,7 +690,7 @@ static Bool routein_process_event(GF_Filter *filter, const GF_FilterEvent *evt)
 	} else if (evt->base.type==GF_FEVT_STOP) {
 		ctx->nb_playing--;
 	} else if (evt->base.type==GF_FEVT_DASH_QUALITY_SELECT) {
-		gf_routedmx_mark_active_quality(ctx->route_dmx, evt->dash_select.service_id, evt->dash_select.period_id, evt->dash_select.as_id, evt->dash_select.rep_id, (evt->dash_select.select_type==GF_QUALITY_SELECTED) ? GF_TRUE : GF_FALSE);
+		gf_route_dmx_mark_active_quality(ctx->route_dmx, evt->dash_select.service_id, evt->dash_select.period_id, evt->dash_select.as_id, evt->dash_select.rep_id, (evt->dash_select.select_type==GF_QUALITY_SELECTED) ? GF_TRUE : GF_FALSE);
 	}
 	return GF_TRUE;
 }

commit c48c47fc6edff0d524cb2200cf57c0ebd1b38eab

fixed bug introduced in 39762b44

src/utils/downloader.c
@@ -5095,10 +5095,10 @@ GF_Err gf_dm_sess_fetch_data(GF_DownloadSession *sess, char *buffer, u32 buffer_
 		gf_dm_connect(sess);
 		if (sess->last_error)
 			return sess->last_error;
-		e = GF_OK;
+		e = GF_IP_NETWORK_EMPTY;
 	} else if (sess->status < GF_NETIO_DATA_EXCHANGE) {
 		sess->do_requests(sess);
-		e = sess->last_error;
+		e = sess->last_error ? sess->last_error : GF_IP_NETWORK_EMPTY;
 	}
 	/*we're running but we had data previously*/
 	else if (sess->init_data) {