gpac/gpac

commit 2bff79fbd41534f3e390d224a351a4bfbf80bb70

always use task for filter remove

src/filter_core/filter.c
@@ -3806,7 +3806,8 @@ static void gf_filter_remove_local(GF_Filter *filter, GF_FSTask *task)
 GF_EXPORT
 void gf_filter_remove(GF_Filter *filter)
 {
-	gf_filter_remove_local(filter, NULL);
+	//always post a task for remove, this allows users to do remove() followed by add filter() without triggering stops
+	gf_fs_post_task(filter->session, gf_filter_remove_reschedule, filter, NULL, "filter_remove", NULL);
 }
 
 #if 0

commit 171ebfd5e45008344b8489aa682bbfd838208e71

fixed crash introduced in 2bff79fb

src/filter_core/filter.c
@@ -3959,6 +3959,7 @@ static void gf_filter_remove_local(GF_FSTask *task)
 GF_EXPORT
 void gf_filter_remove(GF_Filter *filter)
 {
+	if (!filter) return;
 	safe_int_inc(&filter->session->remove_tasks);
 	//always post a task for remove, this allows users to do remove() followed by add filter() without triggering stops
 	gf_fs_post_task(filter->session, gf_filter_remove_local, filter, NULL, "filter_remove", NULL);