gpac/gpac

commit f10c406251fde1eaa4888ecf22345811186c4084

track unused arguments in mp4box

applications/mp4box/mp4box.c
@@ -3510,10 +3510,12 @@ u32 mp4box_parse_args(int argc, char **argv)
 	u32 i;
 	/*parse our args*/
 	for (i = 1; i < (u32)argc; i++) {
+		u32 arg_idx = i;
 		char *arg = argv[i];
 		/*input file(s)*/
 		if ((arg[0] != '-') || !stricmp(arg, "--")) {
 			char *arg_val = arg;
+			gf_sys_mark_arg_used(i, GF_TRUE);
 			if (!stricmp(arg, "--")) {
 				if (i+1==(u32)argc) {
 					M4_LOG(GF_LOG_ERROR, ("Missing arg for `--` - please check usage\n"));
@@ -3522,6 +3524,7 @@ u32 mp4box_parse_args(int argc, char **argv)
 				has_next_arg = GF_TRUE;
 				arg_val = argv[i + 1];
 				i++;
+				arg_idx=i;
 			}
 			if (argc < 3) {
 				M4_LOG(GF_LOG_ERROR, ("Error - only one input file found as argument, please check usage\n"));
@@ -3570,10 +3573,12 @@ u32 mp4box_parse_args(int argc, char **argv)
 			return 2;
 		}
 		else if (!strncmp(arg, "-p=", 3)) {
+			gf_sys_mark_arg_used(i, GF_TRUE);
 			continue;
 		}
 #ifndef GPAC_MEMORY_TRACKING
 		else if (!strcmp(arg, "-mem-track") || !strcmp(arg, "-mem-track-stack")) {
+			gf_sys_mark_arg_used(i, GF_TRUE);
 			continue;
 		}
 #endif
@@ -3582,6 +3587,10 @@ u32 mp4box_parse_args(int argc, char **argv)
 		else if (mp4box_parse_single_arg(argc, argv, arg, &i)) {
 			if (arg_parse_res)
 				return mp4box_cleanup(arg_parse_res);
+
+			gf_sys_mark_arg_used(arg_idx, GF_TRUE);
+			if (i>arg_idx)
+				gf_sys_mark_arg_used(i, GF_TRUE);
 		}
 		//not a MP4Box arg
 		else {
@@ -3589,8 +3598,13 @@ u32 mp4box_parse_args(int argc, char **argv)
 			if (res==0) {
 				PrintHelp(arg, GF_FALSE, GF_TRUE);
 				return 2;
-			} else if (res==2) {
-				i++;
+			} else {
+				if (strncmp(arg, "--", 2)) {
+					gf_sys_mark_arg_used(arg_idx, GF_TRUE);
+				}
+				if (res==2) {
+					i++;
+				}
 			}
 		}
 		//live scene encoder does not use the unified parsing and should be moved as a scene encoder filter
@@ -5982,6 +5996,18 @@ static u32 mp4box_cleanup(u32 ret_code) {
 		gf_free(dash_inputs);
 		dash_inputs = NULL;
 	}
+	if (!ret_code) {
+		u32 i, found=0, count = gf_sys_get_argc();
+		for (i=1; i<count;i++) {
+			if (gf_sys_is_arg_used(i)) continue;
+			if (!found) {
+				GF_LOG(GF_LOG_ERROR, GF_LOG_APP, ("\nWarning: the following arguments have been set but not used:\n"));
+				found=1;
+			}
+			GF_LOG(GF_LOG_ERROR, GF_LOG_APP, ("%s\n", gf_sys_get_arg(i)));
+		}
+	}
+
 	if (logfile) gf_fclose(logfile);
 	gf_sys_close();
 
@@ -6050,6 +6076,12 @@ int mp4box_main(int argc, char **argv)
 	}
 
 	helpout = stdout;
+
+	e = gf_sys_set_args(argc, (const char **) argv);
+	if (e) {
+		M4_LOG(GF_LOG_ERROR, ("Error assigning libgpac arguments: %s\n", gf_error_to_string(e) ));
+		return mp4box_cleanup(1);
+	}
 
 	i = mp4box_parse_args(argc, argv);
 	if (i) {
@@ -6136,12 +6168,6 @@ int mp4box_main(int argc, char **argv)
 		file = gf_isom_open(inName, GF_ISOM_OPEN_KEEP_FRAGMENTS, NULL);
 		if (file) gf_isom_close(file);
 		return mp4box_cleanup(0);
-	}
-
-	e = gf_sys_set_args(argc, (const char **) argv);
-	if (e) {
-		M4_LOG(GF_LOG_ERROR, ("Error assigning libgpac arguments: %s\n", gf_error_to_string(e) ));
-		return mp4box_cleanup(1);
 	}
 
 	if (raw_cat)

applications/mp4box/filedump.c
@@ -3461,7 +3461,6 @@ static void DumpStsdInfo(GF_ISOFile *file, u32 trackNum, Bool full_dump, Bool du
 #endif
 			);
 			gf_odf_vvc_cfg_del(vvccfg);
-			fprintf(stderr, "\n");
 		}
 #if !defined(GPAC_DISABLE_AV_PARSERS)
 		if (vvc_state) gf_free(vvc_state);
@@ -3860,6 +3859,7 @@ void DumpTrackInfo(GF_ISOFile *file, GF_ISOTrackID trackID, Bool full_dump, Bool
 	for (i=0; i<count; i++) {
 		DumpStsdInfo(file, trackNum, full_dump, dump_m4sys, mtype, i+1, &is_od_track);
 	}
+	fprintf(stderr, "\n");
 
 	switch (gf_isom_has_sync_points(file, trackNum)) {
 	case 0:

applications/mp4box/fileimport.c
@@ -829,8 +829,8 @@ GF_Err import_file(GF_ISOFile *dest, char *inName, u32 import_flags, GF_Fraction
 
 #define GOTO_EXIT(_msg) if (e) { fail_msg = _msg; goto exit; }
 
-#define CHECK_FAKEIMPORT(_opt) if (fake_import) { M4_LOG(GF_LOG_ERROR, ("Option %s not available for self-reference import\n", _opt)); e = GF_BAD_PARAM; goto exit; }
-#define CHECK_FAKEIMPORT_2(_opt) if (fake_import==1) { M4_LOG(GF_LOG_ERROR, ("Option %s not available for self-reference import\n", _opt)); e = GF_BAD_PARAM; goto exit; }
+#define CHECK_FAKEIMPORT(_opt) if (fake_import) { M4_LOG(GF_LOG_ERROR, ("Option %s not available for cat or self-reference import\n", _opt)); e = GF_BAD_PARAM; goto exit; }
+#define CHECK_FAKEIMPORT_2(_opt) if (fake_import==1) { M4_LOG(GF_LOG_ERROR, ("Option %s not available for cat or self-reference import\n", _opt)); e = GF_BAD_PARAM; goto exit; }
 
 
 	handler_name = NULL;

src/filter_core/filter.c
@@ -1441,6 +1441,7 @@ static const char *gf_filter_load_arg_config(GF_Filter *filter, const char *sec_
 				flen++;
 				arg += flen;
 			}
+			gf_sys_mark_arg_used(i, GF_TRUE);
 
 			if (sep) {
 				len = (u32) (sep - (arg));

src/filter_core/filter_session.c
@@ -3091,6 +3091,7 @@ void gf_fs_print_unused_args(GF_FilterSession *fsess, const char *ignore_args)
 	u32 idx = 0;
 	const char *argname;
 	u32 argtype;
+	Bool first=GF_TRUE;
 
 	while (1) {
 		Bool found = GF_FALSE;
@@ -3115,7 +3116,11 @@ void gf_fs_print_unused_args(GF_FilterSession *fsess, const char *ignore_args)
 		}
 		if (found) continue;
 
-		GF_LOG(GF_LOG_ERROR, GF_LOG_APP, ("Arg %s set but not used\n", argname));
+		if (first) {
+			GF_LOG(GF_LOG_ERROR, GF_LOG_APP, ("\nWarning: the following arguments have been set but not used:\n"));
+			first=GF_FALSE;
+		}
+		GF_LOG(GF_LOG_ERROR, GF_LOG_APP, ("%s\n", argname));
 	}
 }
 

commit c7d9ad765a2d40a0757a30e1958bedf6a9355024

gixed mp4box log bug introduced in f10c4062

applications/mp4box/mp4box.c
@@ -6079,6 +6079,18 @@ int mp4box_main(int argc, char **argv)
 
 	helpout = stdout;
 
+	GF_LOG_Level level = verbose ? GF_LOG_DEBUG : GF_LOG_INFO;
+	gf_log_set_tool_level(GF_LOG_CONTAINER, level);
+	gf_log_set_tool_level(GF_LOG_SCENE, level);
+	gf_log_set_tool_level(GF_LOG_PARSER, level);
+	gf_log_set_tool_level(GF_LOG_MEDIA, level);
+	gf_log_set_tool_level(GF_LOG_CODING, level);
+	gf_log_set_tool_level(GF_LOG_DASH, level);
+#ifdef GPAC_MEMORY_TRACKING
+	if (mem_track)
+		gf_log_set_tool_level(GF_LOG_MEMORY, level);
+#endif
+
 	e = gf_sys_set_args(argc, (const char **) argv);
 	if (e) {
 		M4_LOG(GF_LOG_ERROR, ("Error assigning libgpac arguments: %s\n", gf_error_to_string(e) ));
@@ -6149,18 +6161,6 @@ int mp4box_main(int argc, char **argv)
 			return mp4box_cleanup(1);
 		}
 	}
-
-	GF_LOG_Level level = verbose ? GF_LOG_DEBUG : GF_LOG_INFO;
-	gf_log_set_tool_level(GF_LOG_CONTAINER, level);
-	gf_log_set_tool_level(GF_LOG_SCENE, level);
-	gf_log_set_tool_level(GF_LOG_PARSER, level);
-	gf_log_set_tool_level(GF_LOG_MEDIA, level);
-	gf_log_set_tool_level(GF_LOG_CODING, level);
-	gf_log_set_tool_level(GF_LOG_DASH, level);
-#ifdef GPAC_MEMORY_TRACKING
-	if (mem_track)
-		gf_log_set_tool_level(GF_LOG_MEMORY, level);
-#endif
 
 	if (fuzz_chk) {
 		file = gf_isom_open(inName, GF_ISOM_OPEN_READ_DUMP, NULL);