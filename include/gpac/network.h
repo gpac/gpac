gpac/gpac

commit fc3dda250ab8ab25058ac90c7781c344b9dbce6f

partial revert of a649ee66 - fixes #2972

include/gpac/network.h
@@ -365,9 +365,7 @@ enum
 	/*!Reuses port.*/
 	GF_SOCK_REUSE_PORT = 1,
 	/*!Forces IPV6 if available.*/
-	GF_SOCK_FORCE_IPV6 = 1<<1,
-	/*!Does not perfom the actual bind, only keeps address and port.*/
-	GF_SOCK_FAKE_BIND = 1<<2
+	GF_SOCK_FORCE_IPV6 = 1<<1
 };
 
 /*!

src/filters/out_sock.c
@@ -212,7 +212,7 @@ static GF_Err sockout_initialize(GF_Filter *filter)
 		|| (sock_type == GF_SOCK_TYPE_UDP_UN)
 #endif
 	) {
-		e = gf_sk_bind(ctx->socket, ctx->ifce, port, url, port, GF_SOCK_REUSE_PORT | GF_SOCK_FAKE_BIND);
+		e = gf_sk_bind(ctx->socket, ctx->ifce, port, url, port, GF_SOCK_REUSE_PORT);
 		ctx->listen = GF_FALSE;
 	} else if (ctx->listen) {
 		e = gf_sk_bind(ctx->socket, NULL, port, url, 0, GF_SOCK_REUSE_PORT);

src/ietf/rtp.c
@@ -253,7 +253,7 @@ GF_Err gf_rtp_initialize(GF_RTPChannel *ch, u32 UDPBufferSize, Bool IsSource, u3
 			//else bind and set remote destination
 			else {
 				if (!ch->net_info.port_first) ch->net_info.port_first = ch->net_info.client_port_first;
-				e = gf_sk_bind(ch->rtp, local_ip,ch->net_info.port_first, ch->net_info.destination, ch->net_info.client_port_first, GF_SOCK_REUSE_PORT | GF_SOCK_FAKE_BIND);
+				e = gf_sk_bind(ch->rtp, local_ip,ch->net_info.port_first, ch->net_info.destination, ch->net_info.client_port_first, GF_SOCK_REUSE_PORT);
 				if (e) return e;
 			}
 		} else {
@@ -290,7 +290,7 @@ GF_Err gf_rtp_initialize(GF_RTPChannel *ch, u32 UDPBufferSize, Bool IsSource, u3
 				e = gf_sk_bind(ch->rtcp, local_ip, ch->net_info.client_port_last, ch->net_info.source, port, GF_SOCK_REUSE_PORT);
 				if (e) return e;
 			} else {
-				e = gf_sk_bind(ch->rtcp, local_ip, ch->net_info.port_last, ch->net_info.destination, ch->net_info.client_port_last, GF_SOCK_REUSE_PORT | GF_SOCK_FAKE_BIND);
+				e = gf_sk_bind(ch->rtcp, local_ip, ch->net_info.port_last, ch->net_info.destination, ch->net_info.client_port_last, GF_SOCK_REUSE_PORT);
 				if (e) return e;
 			}
 		} else {

src/utils/os_net.c
@@ -2578,14 +2578,13 @@ GF_Err gf_sk_bind(GF_Socket *sock, const char *ifce_ip_or_name, u16 port, const 
 		if (peer_name && peer_port)
 			sock->flags |= GF_SOCK_HAS_PEER;
 
-		if (! (options & GF_SOCK_FAKE_BIND) ) {
-			ret = bind(sock->socket, aip->ai_addr, (int) aip->ai_addrlen);
-			if (ret == SOCKET_ERROR) {
-				GF_LOG(GF_LOG_WARNING, GF_LOG_NETWORK, ("[socket] bind failed: %s\n", gf_errno_str(LASTSOCKERROR) ));
-				sock_close(sock);
-				continue;
-			}
+		ret = bind(sock->socket, aip->ai_addr, (int) aip->ai_addrlen);
+		if (ret == SOCKET_ERROR) {
+			GF_LOG(GF_LOG_WARNING, GF_LOG_NETWORK, ("[socket] bind failed: %s\n", gf_errno_str(LASTSOCKERROR) ));
+			sock_close(sock);
+			continue;
 		}
+
 		if (aip->ai_family==PF_INET6) sock->flags |= GF_SOCK_IS_IPV6;
 		else sock->flags &= ~GF_SOCK_IS_IPV6;
 
@@ -2649,13 +2648,11 @@ GF_Err gf_sk_bind(GF_Socket *sock, const char *ifce_ip_or_name, u16 port, const 
 #endif
 	}
 
-	if (! (options & GF_SOCK_FAKE_BIND) ) {
-		/*bind the socket*/
-		ret = bind(sock->socket, (struct sockaddr *) &LocalAdd, (int) addrlen);
-		if (ret == SOCKET_ERROR) {
-			GF_LOG(GF_LOG_ERROR, GF_LOG_NETWORK, ("[socket] cannot bind socket: %s\n", gf_errno_str(LASTSOCKERROR) ));
-			ret = GF_IP_CONNECTION_FAILURE;
-		}
+	/*bind the socket*/
+	ret = bind(sock->socket, (struct sockaddr *) &LocalAdd, (int) addrlen);
+	if (ret == SOCKET_ERROR) {
+		GF_LOG(GF_LOG_ERROR, GF_LOG_NETWORK, ("[socket] cannot bind socket: %s\n", gf_errno_str(LASTSOCKERROR) ));
+		ret = GF_IP_CONNECTION_FAILURE;
 	}
 
 	if (peer_name && peer_port) {

commit c5f08cda6a17dd484721a7e1b20d4c5e10b63d46

fixed bug introduced by fc3dda25

include/gpac/network.h
@@ -365,7 +365,9 @@ enum
 	/*!Reuses port.*/
 	GF_SOCK_REUSE_PORT = 1,
 	/*!Forces IPV6 if available.*/
-	GF_SOCK_FORCE_IPV6 = 1<<1
+	GF_SOCK_FORCE_IPV6 = 1<<1,
+	/*! Indicates the socket will be used to send , only used in test modes*/
+	GF_SOCK_IS_SENDER = 1<<2
 };
 
 /*!

src/filters/dasher.c
@@ -4695,7 +4695,7 @@ static void dasher_purge_segments(GF_DasherCtx *ctx, u64 *period_dur)
 				if (sctx->frags && (ctx->llhls>1)) {
 					u32 k;
 					for (k=0; k<sctx->nb_frags; k++) {
-						char szTmp[10];
+						char szTmp[100];
 						sprintf(szTmp, ".%u", k+1);
 						char *frag_url = gf_strdup(sctx->filepath);
 						gf_dynstrcat(&frag_url, szTmp, NULL);

src/filters/out_sock.c
@@ -212,7 +212,7 @@ static GF_Err sockout_initialize(GF_Filter *filter)
 		|| (sock_type == GF_SOCK_TYPE_UDP_UN)
 #endif
 	) {
-		e = gf_sk_bind(ctx->socket, ctx->ifce, port, url, port, GF_SOCK_REUSE_PORT);
+		e = gf_sk_bind(ctx->socket, ctx->ifce, port, url, port, GF_SOCK_REUSE_PORT|GF_SOCK_IS_SENDER);
 		ctx->listen = GF_FALSE;
 	} else if (ctx->listen) {
 		e = gf_sk_bind(ctx->socket, NULL, port, url, 0, GF_SOCK_REUSE_PORT);

src/ietf/rtp.c
@@ -253,7 +253,7 @@ GF_Err gf_rtp_initialize(GF_RTPChannel *ch, u32 UDPBufferSize, Bool IsSource, u3
 			//else bind and set remote destination
 			else {
 				if (!ch->net_info.port_first) ch->net_info.port_first = ch->net_info.client_port_first;
-				e = gf_sk_bind(ch->rtp, local_ip,ch->net_info.port_first, ch->net_info.destination, ch->net_info.client_port_first, GF_SOCK_REUSE_PORT);
+				e = gf_sk_bind(ch->rtp, local_ip,ch->net_info.port_first, ch->net_info.destination, ch->net_info.client_port_first, GF_SOCK_REUSE_PORT|GF_SOCK_IS_SENDER);
 				if (e) return e;
 			}
 		} else {
@@ -290,7 +290,7 @@ GF_Err gf_rtp_initialize(GF_RTPChannel *ch, u32 UDPBufferSize, Bool IsSource, u3
 				e = gf_sk_bind(ch->rtcp, local_ip, ch->net_info.client_port_last, ch->net_info.source, port, GF_SOCK_REUSE_PORT);
 				if (e) return e;
 			} else {
-				e = gf_sk_bind(ch->rtcp, local_ip, ch->net_info.port_last, ch->net_info.destination, ch->net_info.client_port_last, GF_SOCK_REUSE_PORT);
+				e = gf_sk_bind(ch->rtcp, local_ip, ch->net_info.port_last, ch->net_info.destination, ch->net_info.client_port_last, GF_SOCK_REUSE_PORT|GF_SOCK_IS_SENDER);
 				if (e) return e;
 			}
 		} else {

src/ietf/rtp_streamer.c
@@ -132,7 +132,7 @@ static GF_Err rtp_stream_init_channel(GF_RTPStreamer *rtp, u32 path_mtu, const c
 	if (!rtp->channel) return GF_OUT_OF_MEM;
 	rtp->channel->TimeScale = rtp->packetizer->sl_config.timestampResolution;
 
-	gf_rtp_set_ports(rtp->channel, 0);
+	//gf_rtp_set_ports(rtp->channel, 0);
 	memset(&tr, 0, sizeof(GF_RTSPTransport));
 
 	tr.IsUnicast = gf_sk_is_multicast_address(dest) ? GF_FALSE : GF_TRUE;

src/utils/os_net.c
@@ -2575,16 +2575,24 @@ GF_Err gf_sk_bind(GF_Socket *sock, const char *ifce_ip_or_name, u16 port, const 
 		if (sock->flags & GF_SOCK_NON_BLOCKING)
 			gf_sk_set_block_mode(sock, GF_TRUE);
 
-		if (peer_name && peer_port)
+		if (peer_name && peer_port) {
 			sock->flags |= GF_SOCK_HAS_PEER;
-
-		ret = bind(sock->socket, aip->ai_addr, (int) aip->ai_addrlen);
-		if (ret == SOCKET_ERROR) {
-			GF_LOG(GF_LOG_WARNING, GF_LOG_NETWORK, ("[socket] bind failed: %s\n", gf_errno_str(LASTSOCKERROR) ));
-			sock_close(sock);
-			continue;
 		}
 
+#ifdef GPAC_CONFIG_LINUX
+		//weird bug in linux  (at least on our VMs) when reusing UDP port and binding,
+		//poll/select for read fails in the other program reusing the port
+		if (peer_name && !strcmp(peer_name, "127.0.0.1") && (options & GF_SOCK_IS_SENDER)) {
+		} else
+#endif
+		{
+			ret = bind(sock->socket, aip->ai_addr, (int) aip->ai_addrlen);
+			if (ret == SOCKET_ERROR) {
+				GF_LOG(GF_LOG_WARNING, GF_LOG_NETWORK, ("[socket] bind failed: %s\n", gf_errno_str(LASTSOCKERROR) ));
+				sock_close(sock);
+				continue;
+			}
+		}
 		if (aip->ai_family==PF_INET6) sock->flags |= GF_SOCK_IS_IPV6;
 		else sock->flags &= ~GF_SOCK_IS_IPV6;
 
@@ -2649,10 +2657,17 @@ GF_Err gf_sk_bind(GF_Socket *sock, const char *ifce_ip_or_name, u16 port, const 
 	}
 
 	/*bind the socket*/
-	ret = bind(sock->socket, (struct sockaddr *) &LocalAdd, (int) addrlen);
-	if (ret == SOCKET_ERROR) {
-		GF_LOG(GF_LOG_ERROR, GF_LOG_NETWORK, ("[socket] cannot bind socket: %s\n", gf_errno_str(LASTSOCKERROR) ));
-		ret = GF_IP_CONNECTION_FAILURE;
+#ifdef GPAC_CONFIG_LINUX
+	//see above comment
+	if (peer_name && !strcmp(peer_name, "127.0.0.1") && (options & GF_SOCK_IS_SENDER) ) {
+	} else
+#endif
+	{
+		ret = bind(sock->socket, (struct sockaddr *) &LocalAdd, (int) addrlen);
+		if (ret == SOCKET_ERROR) {
+			GF_LOG(GF_LOG_ERROR, GF_LOG_NETWORK, ("[socket] cannot bind socket: %s\n", gf_errno_str(LASTSOCKERROR) ));
+			ret = GF_IP_CONNECTION_FAILURE;
+		}
 	}
 
 	if (peer_name && peer_port) {