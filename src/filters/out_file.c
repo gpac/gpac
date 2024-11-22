223f7a89e7cf2dc372e08cb5b4d6b5bda725424e
jeanlf <jeanlf@gpac.io>
2024-11-22 09:48:30 +0100

    fixed ow reassign from auto to yes in fout

--- src/filters/out_file.c
+++ src/filters/out_file.c
@@ -203,7 +203,7 @@ static GF_Err fileout_open_close(GF_FileOutCtx *ctx, const char *filename, const
 				if (!res || (szRes[0] == 'n') || (szRes[0] == 'N')) {
 					return ctx->error = GF_IO_ERR;
 				}
-				if ((szRes[0] == 'a') || (szRes[0] == 'A')) ctx->ow = GF_TRUE;
+				if ((szRes[0] == 'a') || (szRes[0] == 'A')) ctx->ow = FOUT_OW_YES;
 			} else {
 				return ctx->error = GF_IO_ERR;
 			}
