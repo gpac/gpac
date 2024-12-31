a0c7599b74cd65e1c31bdad3cb0e467639f71b3a
DenizUgur <DenizUgur@users.noreply.github.com>
2024-12-31 15:33:03 -0800

    ability to filter prefix seis

--- src/filters/reframe_nalu.c
+++ src/filters/reframe_nalu.c
@@ -71,6 +71,7 @@ typedef struct
 	//filter args
 	GF_Fraction fps;
 	Double index;
+	GF_PropUIntList seis;
 	Bool explicit, force_sync, nosei, importer, subsamples, nosvc, novpsext, deps, seirw, audelim, analyze, notime, refs;
 	u32 nal_length;
 	u32 strict_poc;
@@ -2503,7 +2504,7 @@ void naludmx_add_subsample(GF_NALUDmxCtx *ctx, u32 subs_size, u8 subs_priority, 
 	ctx->subs_mapped_bytes += subs_size + ctx->nal_length;
 }
 
-static void naludmx_push_prefix(GF_NALUDmxCtx *ctx, u8 *data, u32 size, Bool avc_sei_rewrite)
+static Bool naludmx_push_prefix(GF_NALUDmxCtx *ctx, u8 *data, u32 size, Bool reformat)
 {
 	if (ctx->sei_buffer_alloc < ctx->sei_buffer_size + size + ctx->nal_length) {
 		ctx->sei_buffer_alloc = ctx->sei_buffer_size + size + ctx->nal_length;
@@ -2515,15 +2516,27 @@ static void naludmx_push_prefix(GF_NALUDmxCtx *ctx, u8 *data, u32 size, Bool avc
 	gf_bs_write_int(ctx->bs_w, size, 8*ctx->nal_length);
 	memcpy(ctx->sei_buffer + ctx->sei_buffer_size + ctx->nal_length, data, size);
 
-	if (avc_sei_rewrite) {
-		u32 rw_sei_size = gf_avc_reformat_sei(ctx->sei_buffer + ctx->sei_buffer_size + ctx->nal_length, size, ctx->seirw, ctx->avc_state);
-		if (rw_sei_size < size) {
-			gf_bs_seek(ctx->bs_w, 0);
-			gf_bs_write_int(ctx->bs_w, rw_sei_size, 8*ctx->nal_length);
-			size = rw_sei_size;
-		}
+	u32 rw_sei_size = size;
+	if (!reformat)
+		goto finish;
+
+	if (ctx->codecid==GF_CODECID_HEVC) {
+		rw_sei_size = gf_hevc_reformat_sei(ctx->sei_buffer + ctx->sei_buffer_size + ctx->nal_length, size, ctx->seirw, ctx->hevc_state, ctx->seis);
+	} else if (ctx->codecid==GF_CODECID_VVC) {
+		rw_sei_size = gf_vvc_reformat_sei(ctx->sei_buffer + ctx->sei_buffer_size + ctx->nal_length, size, ctx->seirw, ctx->vvc_state, ctx->seis);
+	} else {
+		rw_sei_size = gf_avc_reformat_sei(ctx->sei_buffer + ctx->sei_buffer_size + ctx->nal_length, size, ctx->seirw, ctx->avc_state, ctx->seis);
 	}
+
+	if (rw_sei_size < size) {
+		gf_bs_seek(ctx->bs_w, 0);
+		gf_bs_write_int(ctx->bs_w, rw_sei_size, 8*ctx->nal_length);
+		size = rw_sei_size;
+	}
+
+finish:
 	ctx->sei_buffer_size += size + ctx->nal_length;
+	return rw_sei_size > 0;
 }
 
 static s32 naludmx_parse_nal_hevc(GF_NALUDmxCtx *ctx, char *data, u32 size, Bool *skip_nal, u32 *is_slice, Bool *is_islice)
@@ -2589,11 +2602,12 @@ static s32 naludmx_parse_nal_hevc(GF_NALUDmxCtx *ctx, char *data, u32 size, Bool
 		if (ctx->hevc_state->has_3d_ref_disp_info) {
 			naludmx_queue_param_set(ctx, data, size, GF_HEVC_NALU_SEI_PREFIX, 0, temporal_id, layer_id);
 		}
-		if (!ctx->nosei) {
-			ctx->nb_sei++;
-			naludmx_push_prefix(ctx, data, size, GF_FALSE);
-		} else {
+		Bool still_has_sei = naludmx_push_prefix(ctx, data, size, GF_TRUE);
+		if (ctx->nosei || !still_has_sei) {
 			ctx->nb_nalus--;
+			ctx->sei_buffer_size = 0;
+		} else {
+			ctx->nb_sei++;
 		}
 		*skip_nal = GF_TRUE;
 		break;
@@ -2793,12 +2807,12 @@ static s32 naludmx_parse_nal_vvc(GF_NALUDmxCtx *ctx, char *data, u32 size, Bool 
 		break;
 	case GF_VVC_NALU_SEI_PREFIX:
 		gf_vvc_parse_sei(data, size, ctx->vvc_state);
-		if (!ctx->nosei) {
-			ctx->nb_sei++;
-
-			naludmx_push_prefix(ctx, data, size, GF_FALSE);
-		} else {
+		Bool still_has_sei = naludmx_push_prefix(ctx, data, size, GF_TRUE);
+		if (ctx->nosei || !still_has_sei) {
 			ctx->nb_nalus--;
+			ctx->sei_buffer_size = 0;
+		} else {
+			ctx->nb_sei++;
 		}
 		*skip_nal = GF_TRUE;
 		break;
@@ -2944,15 +2958,13 @@ static s32 naludmx_parse_nal_avc(GF_NALUDmxCtx *ctx, char *data, u32 size, u32 n
 
 	case GF_AVC_NALU_SEI:
 		if (ctx->avc_state->sps_active_idx != -1) {
-			naludmx_push_prefix(ctx, data, size, GF_TRUE);
-
-			*skip_nal = GF_TRUE;
-
-			if (ctx->nosei) {
+			Bool still_has_sei = naludmx_push_prefix(ctx, data, size, GF_TRUE);
+			if (ctx->nosei || !still_has_sei) {
 				ctx->sei_buffer_size = 0;
 			} else {
 				ctx->nb_sei++;
 			}
+			*skip_nal = GF_TRUE;
 		}
 		return 0;
 
@@ -4403,6 +4415,7 @@ static const GF_FilterArgs NALUDmxArgs[] =
 		"- off: disable GOP buffering\n"
 		"- on: enable GOP buffering, assuming no error in POC\n"
 		"- error: enable GOP buffering and try to detect lost frames", GF_PROP_UINT, "off", "off|on|error", GF_FS_ARG_HINT_ADVANCED},
+	{ OFFS(seis), "list of message types (4,137,144,...) to forward, other messages are dropped", GF_PROP_UINT_LIST, NULL, NULL, GF_FS_ARG_HINT_ADVANCED|GF_FS_ARG_UPDATE},
 	{ OFFS(nosei), "remove all sei messages", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(nosvc), "remove all SVC/MVC/LHVC data", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(novpsext), "remove all VPS extensions", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
