215f64bafbaf1a53cdf561d532da8cf083a9b19b
jeanlf <jeanlf@gpac.io>
2025-01-10 16:51:22 +0100

    updated project files

---
+++ src/utils/downloader_ngtcp2.c
@@ -0,0 +1,30 @@
+/*
+ *					GPAC Multimedia Framework
+ *
+ *			Authors: Jean Le Feuvre
+ *			Copyright (c) Telecom Paris 2025-2025
+ *					All rights reserved
+ *
+ *  This file is part of GPAC / downloader sub-project
+ *
+ *  GPAC is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  GPAC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "downloader.h"
+
+#if !defined(GPAC_DISABLE_NETWORK) && defined(GPAC_HAS_HTTP3)
+
+#endif // !defined(GPAC_DISABLE_NETWORK) && defined(GPAC_HAS_HTTP3)
