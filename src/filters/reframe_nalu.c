8a517dda24bb4550222357f1478942d0f460ffe2
DenizUgur <DenizUgur@users.noreply.github.com>
2025-01-16 16:43:13 -0800

    minor cleanup

--- src/filters/reframe_nalu.c
+++ src/filters/reframe_nalu.c
@@ -2509,6 +2509,7 @@ static void naludmx_push_prefix(GF_NALUDmxCtx *ctx, u8 *data, u32 size, Bool avc
 		ctx->sei_buffer_alloc = ctx->sei_buffer_size + size + ctx->nal_length;
 		ctx->sei_buffer = gf_realloc(ctx->sei_buffer, ctx->sei_buffer_alloc);
 	}
+
 	if (!ctx->bs_w) ctx->bs_w = gf_bs_new(ctx->sei_buffer + ctx->sei_buffer_size, ctx->nal_length + size, GF_BITSTREAM_WRITE);
 	else gf_bs_reassign_buffer(ctx->bs_w, ctx->sei_buffer + ctx->sei_buffer_size, ctx->nal_length + size);
 	gf_bs_write_int(ctx->bs_w, size, 8*ctx->nal_length);
