1e42b6c56671b45fb534e6237e907c035daaeb37
jeanlf <jeanlf@gpac.io>
2025-04-07 10:58:54 +0200

    added real-time regulation option for tssplit and tsgendts

--- src/filters/tssplit.c
+++ src/filters/tssplit.c
@@ -39,7 +39,7 @@ typedef struct
 	u32 pmt_pid;
 	u8 pat_pck[192];
 	u32 pat_pck_size;
-	u64 last_pcr_plus_one;
+	u64 last_pcr_plus_one, init_clock, init_pcr;
 	u32 pcr_id;
 	Bool start_sent;
 
@@ -60,6 +60,7 @@ typedef struct
 	u32 nb_pack;
 	Bool gendts;
 	Bool kpad;
+	Bool rt;
 
 	//internal
 	GF_Filter *filter;
@@ -74,6 +75,8 @@ typedef struct
 	GF_M2TSSplit_SPTS *out;
 
 	u64 filesize;
+	u64 process_clock;
+	u32 resched_next;
 	GF_Fraction64 duration;
 	Bool initial_play_done;
 } GF_M2TSSplitCtx;
@@ -98,7 +101,22 @@ void m2tssplit_send_packet(GF_M2TSSplitCtx *ctx, GF_M2TSSplit_SPTS *stream, u8 *
 				memcpy(buffer, data, size);
 			return;
 		}
-		if (pcr_plus_one) stream->last_pcr_plus_one = pcr_plus_one;
+		if (pcr_plus_one) {
+			if (!stream->last_pcr_plus_one && ctx->rt) {
+				stream->init_clock = gf_sys_clock_high_res();
+				stream->init_pcr = pcr_plus_one;
+			}
+			stream->last_pcr_plus_one = pcr_plus_one;
+			if (ctx->rt) {
+				u64 pck_time = gf_timestamp_rescale(pcr_plus_one - stream->init_pcr, 27000000, 1000000);
+				pck_time += stream->init_clock;
+				u32 next_time = 0;
+				if (pck_time > ctx->process_clock)
+					next_time = (u32)(pck_time - ctx->process_clock);
+				if (!ctx->resched_next || (ctx->resched_next > next_time))
+					ctx->resched_next = next_time;
+			}
+		}
 		if (stream->init_pck) {
 			gf_filter_pck_set_framing(stream->init_pck, !stream->start_sent, GF_FALSE);
 			stream->start_sent = GF_TRUE;
@@ -371,9 +389,16 @@ GF_Err m2tssplit_process(GF_Filter *filter)
 	}
 	data = gf_filter_pck_get_data(pck, &data_size);
 	if (data) {
+		if (ctx->rt) {
+			ctx->process_clock = gf_sys_clock_high_res();
+		}
 		gf_m2ts_process_data(ctx->dmx, (u8 *)data, data_size);
 	}
 	gf_filter_pid_drop_packet(ctx->ipid);
+	if (ctx->resched_next) {
+		gf_filter_ask_rt_reschedule(filter, ctx->resched_next);
+		ctx->resched_next = 0;
+	}
 	return GF_OK;
 }
 
@@ -652,6 +677,9 @@ GF_Err m2tssplit_initialize(GF_Filter *filter)
 	ctx->bsw = gf_bs_new(ctx->tsbuf, 192, GF_BITSTREAM_WRITE);
 	if (ctx->nb_pack<=1)
 		ctx->nb_pack = 0;
+
+	if (ctx->rt)
+		ctx->gendts = GF_TRUE;
 	return GF_OK;
 }
 
@@ -685,6 +713,7 @@ static const GF_FilterArgs M2TSSplitArgs[] =
 	{ OFFS(nb_pack), "pack N packets before sending", GF_PROP_UINT, "10", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(gendts), "generate timestamps on output packets based on PCR", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(kpad), "keep padding (null) TS packets", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
+	{ OFFS(rt), "enable real-time regulation", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{0}
 };
 
@@ -727,6 +756,7 @@ static const GF_FilterArgs M2TSGenDTSArgs[] =
 {
 	{ OFFS(nb_pack), "pack N packets before sending", GF_PROP_UINT, "10", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{ OFFS(kpad), "keep padding (null) TS packets", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
+	{ OFFS(rt), "enable real-time regulation", GF_PROP_BOOL, "false", NULL, GF_FS_ARG_HINT_ADVANCED},
 	{0}
 };
 
