66d2a074261a05b080c296b4cee9b169248bee87
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2025-05-18 08:20:49 -0400

    revision: don't print change when no previous version exists

--- check_revision.sh
+++ check_revision.sh
@@ -18,7 +18,9 @@ rm gtmp
 echo "#define GPAC_GIT_REVISION	\"$revision\"" > htmp
 
 if ! diff htmp ./include/gpac/revision.h > /dev/null ; then
-echo "revision has changed"
+if [ ! -f ./include/gpac/revision.h ]; then
+echo "Revision has changed"
+fi
 rm ./include/gpac/revision.h
 mv htmp ./include/gpac/revision.h
 else
@@ -27,9 +29,7 @@ fi
 
 else
 #otherwise, check id -DEV is in the version name. If not consider this a release
-
 if [ ! -e ".include/gpac/revision.h" ]; then
-
 if test "$version" != *"-DEV"* ; then
 echo "#define GPAC_GIT_REVISION \"release\"" > ./include/gpac/revision.h
 else
