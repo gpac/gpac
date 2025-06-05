gpac/gpac

commit fa445a025398d74731021b362873d42c8ddee8b2

show MABR status in vout

src/filters/in_http.c
@@ -534,6 +534,14 @@ static GF_Err httpin_process(GF_Filter *filter)
 		}
 		gf_dm_sess_get_stats(ctx->sess, NULL, NULL, &total_size, &bytes_done, &bytes_per_sec, &net_status);
 
+		//special case for no data
+		if ((e==GF_EOS) && !nb_read && !ctx->nb_read && !total_size) {
+			GF_LOG(GF_LOG_WARNING, GF_LOG_HTTP, ("[HTTPIn] No data in stream\n"));
+			httpin_notify_error(filter, ctx, GF_EOS);
+			ctx->is_end = GF_TRUE;
+			return GF_EOS;
+		}
+
 		if (!ctx->pid || ctx->do_reconfigure) {
 			u32 idx;
 			GF_Err cfg_e;
@@ -586,6 +594,17 @@ static GF_Err httpin_process(GF_Filter *filter)
 
 			/*in test mode don't expose http headers (they contain date/version/etc)*/
 			if (! gf_sys_is_test_mode()) {
+				//remove old headers
+				idx=0;
+				while (1) {
+					u32 p4cc;
+					const char *pname;
+					const GF_PropertyValue *p = gf_filter_pid_enum_properties(ctx->pid, &idx, &p4cc, &pname);
+					if (!p) break;
+					if (p4cc) continue;
+					gf_filter_pid_set_property_str(ctx->pid, pname, NULL);
+					idx--;
+				}
 				idx = 0;
 				while (gf_dm_sess_enum_headers(ctx->sess, &idx, &hname, &hval) == GF_OK) {
 					gf_filter_pid_set_property_dyn(ctx->pid, (char *) hname, & PROP_STRING(hval));

share/scripts/vout.js
@@ -499,8 +499,21 @@ function update_play()
 	let last_ts_f = vout.last_ts_drop;
 	if (!last_ts_f && aout)
 		last_ts_f = aout.last_ts_drop;
+
 	if (!last_ts_f) {
-		vout.update('oltxt', null);
+		let str = 'No input connected';
+		if (use_libcaca) {
+			vout.update('oltxt', str);
+			return;
+		}
+		text.fontsize = 20;
+		text.set_text(str);
+		let mx = new evg.Matrix2D();
+		mx.translate(- str.length * text.fontsize/3, -10);
+		ol_canvas.matrix = mx;
+		ol_canvas.path = text;
+		ol_canvas.fill(brush);
+		vout.update('oldata', ol_buffer);
 		return;
 	}
 
@@ -601,6 +614,22 @@ function update_play()
 		ol_canvas.fill(brush);
 		brush.set_color('white');
 	}
+
+	let src = vout ? vout.ipid_source(0) : aout.ipid_source(0);
+	let hdr = src.ipid_props(0, 'X-From-MABR');
+	if (hdr) {
+		text.fontsize = 16;
+		let msg = 'MABR ' + ((hdr=='yes') ? 'on' : ((hdr=='no') ? 'off' : hdr));
+		text.set_text(msg);
+		let mx = new evg.Matrix2D();
+		mx.translate(ol_width/2-10*msg.length, 5-ol_height/2);
+		ol_canvas.matrix = mx;
+		ol_canvas.path = text;
+		brush.set_color((hdr=='yes') ? 'green' : 'red');
+		ol_canvas.fill(brush);
+		brush.set_color('white');
+	}
+
 	//we could lock vout to avoid any tearing ...
 	vout.update('oldata', ol_buffer);
 }
@@ -1086,7 +1115,7 @@ function process_keyboard(evt)
 		}
 		overlay_type=OL_HELP;
 		toggle_overlay();
-		//show player player
+		//show player
 		if (!ol_visible && audio_only) {
 			overlay_type=OL_PLAY;
 			toggle_overlay();
@@ -1100,7 +1129,7 @@ function process_keyboard(evt)
 		}
 		overlay_type=OL_STATS;
 		toggle_overlay();
-		//show player player
+		//show player
 		if (!ol_visible && audio_only) {
 			overlay_type=OL_PLAY;
 			toggle_overlay();

src/filters/isoffin_read.c
@@ -382,11 +382,15 @@ static GF_Err isoffin_reconfigure(GF_Filter *filter, ISOMReader *read, const cha
 	}
 
 	isor_check_producer_ref_time(read);
+	prop = gf_filter_pid_get_property_str(read->pid, "X-From-MABR");
+
 
 	for (i=0; i<count; i++) {
 		ISOMChannel *ch = gf_list_get(read->channels, i);
 		ch->last_state = GF_OK;
 		ch->eos_sent = 0;
+		if (ch->pid)
+			gf_filter_pid_set_property_str(ch->pid, "X-From-MABR", prop);
 
 		//old code from master, currently no longer used
 		//in filters we don't use extractors for the time being, we only do implicit reconstruction at the decoder side

commit ad77e1ca0002f22dcdf198c31bcc4bcf4c99eb1b

fixed wrong httpin error notif introduced in fa445a02

src/filters/in_http.c
@@ -534,8 +534,8 @@ static GF_Err httpin_process(GF_Filter *filter)
 		}
 		gf_dm_sess_get_stats(ctx->sess, NULL, NULL, &total_size, &bytes_done, &bytes_per_sec, &net_status);
 
-		//special case for no data
-		if ((e==GF_EOS) && !nb_read && !ctx->nb_read && !total_size) {
+		//special case for no data when first source - be silent after source switch giving 0 bytes
+		if (!ctx->initial_ack_done && (e==GF_EOS) && !nb_read && !ctx->nb_read && !total_size) {
 			GF_LOG(GF_LOG_WARNING, GF_LOG_HTTP, ("[HTTPIn] No data in stream\n"));
 			httpin_notify_error(filter, ctx, GF_EOS);
 			ctx->is_end = GF_TRUE;