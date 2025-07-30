4e9091c4568e4574eeaf8714256d20a916735603
DenizUgur <DenizUgur@users.noreply.github.com>
2025-07-29 18:24:39 -0700

    ffmpeg v8 compatibility

--- src/filters/ff_dec.c
+++ src/filters/ff_dec.c
@@ -540,8 +540,13 @@ restart:
 
 	gf_filter_pck_set_seek_flag(dst_pck, GF_FALSE);
 
+#if (LIBAVFORMAT_VERSION_MAJOR < 62)
 	if (frame->interlaced_frame)
 		gf_filter_pck_set_interlaced(dst_pck, frame->top_field_first ? 2 : 1);
+#else
+	if (frame->flags & AV_FRAME_FLAG_INTERLACED)
+		gf_filter_pck_set_interlaced(dst_pck, frame->flags & AV_FRAME_FLAG_TOP_FIELD_FIRST ? 2 : 1);
+#endif
 
 	gf_filter_pck_send(dst_pck);
 
