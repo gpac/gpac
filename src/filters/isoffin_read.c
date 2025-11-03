gpac/gpac

commit 47c54a4ebe518d4c907dd780240dea828ea00ed8

base64 encode of init segments - cf #3330
- HLS and DASH generation
- HLS and DASH playback
- added generic function to load a gmem url as raw pid with single packet

src/filters/isoffin_read.c
@@ -1456,6 +1456,8 @@ static GF_Err isoffin_process(GF_Filter *filter)
 				const GF_PropertyValue *p = gf_filter_pid_get_info_str(read->pid, "aborted", &pe);
 				if (p && p->value.boolean) read->was_aborted = GF_TRUE;
 				gf_filter_release_property(pe);
+				//leave mem-load mode - needed for DASH/HLS where init segment is in base64 but not the rest
+				read->mem_load_mode = 0;
 			} else {
 				in_is_flush = GF_TRUE;
 			}

include/gpac/filters.h
@@ -1309,6 +1309,7 @@ enum
 	GF_PROP_PID_DASH_MULTI_PID = GF_4CC('D','M','S','D'),
 	GF_PROP_PID_DASH_MULTI_PID_IDX = GF_4CC('D','M','S','I'),
 	GF_PROP_PID_DASH_MULTI_TRACK = GF_4CC('D','M','T','K'),
+	GF_PROP_PID_DASH_INIT_BASE64 = GF_4CC('I','B','6','4'),
 	GF_PROP_PID_ROLE = GF_4CC('R','O','L','E'),
 	GF_PROP_PID_PERIOD_DESC = GF_4CC('P','D','E','S'),
 	GF_PROP_PID_AS_COND_DESC = GF_4CC('A','C','D','S'),
@@ -1908,6 +1909,8 @@ typedef struct
 	FILTER_EVENT_BASE
 	/*! URL of segment this info is for, or NULL if single file*/
 	const char *seg_url;
+	/*! base64 of segment payload (for init) or NULL*/
+	const char *base64_version;
 	/*! media start range in segment file*/
 	u64 media_range_start;
 	/*! media end range in segment file*/
@@ -3713,6 +3716,14 @@ void gf_filter_pid_remove(GF_FilterPid *PID);
 \return error code if any
 */
 GF_Err gf_filter_pid_raw_new(GF_Filter *filter, const char *url, const char *local_file, const char *mime_type, const char *fext, const u8 *probe_data, u32 probe_size, Bool trust_mime, GF_FilterPid **out_pid);
+
+/*! Creates an output PID for a gmem block, send packet as FILE and set created pid to EOS
+\param filter the target filter
+\param url gmem URL of the data block
+\param out_pid the output PID to create or update. If no referer PID, a new PID will be created otherwise the PID will be updated
+\return error code if any
+*/
+GF_Err gf_filter_pid_raw_gmem(GF_Filter *filter, const char *url, GF_FilterPid **out_pid);
 
 /*! Sets a new property on an output PID for built-in property names.
 Setting a new property will trigger a PID reconfigure at the consumption point of the next dispatched packet.

include/gpac/mpd.h
@@ -719,6 +719,8 @@ typedef struct {
 
 	Bool sub_forced;
 	const char *hls_forced;
+
+	const char *init_base64;
 } GF_MPD_Representation;
 
 /*! AdaptationSet*/

share/doc/man/gpac-filters.1
@@ -5969,6 +5969,8 @@ The segmenter adds the following properties to the output PIDs:
 .br
 * SegSync: indicates that fragments/segments must be completely flushed before sending back size events
 .br
+* InitBase64: inidcates that the base64-encoded init segment must be set in the init segment size event
+.br
 
 .br
 .SH Options (expert):
@@ -6331,6 +6333,8 @@ inband_event (bool, default: false): insert a default inband event stream in the
 ttml_agg (bool, default: false): force aggregation of TTML samples of a DASH segment into a single sample
 .br
 evte_agg (bool, default: false): force aggregation of Event Track samples of a DASH segment into a single sample
+.br
+base64 (bool, default: false): embed init segments in manifests as base64
 .br
 
 .br

share/doc/man/gpac.1
@@ -3902,6 +3902,11 @@ Name of mux source(s), set by dasher to direct its outputs
 DASH mode to be used by multiplexer if any, set by dasher. 0 is no DASH, 1 is regular DASH, 2 is VoD
 .br
 .TP
+.B InitBase64 (IB64,bool,D )
+.br
+Inidcate that multiplexer should send the base64 encoded version of the init segment
+.br
+.TP
 .B SegSync (DFSS,bool,D )
 .br
 Indicate segment must be completely flushed before sending segment/fragment size events

src/filter_core/filter.c
@@ -4794,6 +4794,31 @@ GF_Err gf_filter_pid_raw_new(GF_Filter *filter, const char *url, const char *loc
 }
 
 GF_EXPORT
+GF_Err gf_filter_pid_raw_gmem(GF_Filter *filter, const char *url, GF_FilterPid **out_pid)
+{
+	u8 *mem_address;
+	u32 mem_size;
+	if (!filter || !url || !out_pid) return GF_BAD_PARAM;
+
+	GF_Err e = gf_blob_get(url, &mem_address, &mem_size, NULL);
+	if (e) return e;
+
+	GF_FilterPacket *opck;
+	e = gf_filter_pid_raw_new(filter, NULL, NULL, NULL, NULL, mem_address, mem_size, GF_FALSE, out_pid);
+	if (e) {
+		gf_blob_release(url);
+		return e;
+	}
+	gf_filter_pid_set_property(*out_pid, GF_PROP_PID_URL, &PROP_STRING("NULL"));
+	opck = gf_filter_pck_new_shared(*out_pid, mem_address, mem_size, NULL);
+	gf_filter_pck_set_sap(opck, GF_FILTER_SAP_1);
+	gf_filter_pck_send(opck);
+	gf_filter_pid_set_eos(*out_pid);
+	gf_blob_release(url);
+	return GF_OK;
+}
+
+GF_EXPORT
 const char *gf_filter_probe_data(GF_Filter *filter, u8 *data, u32 size, GF_FilterProbeScore *pscore)
 {
 	u32 i, count;

src/filter_core/filter_pid.c
@@ -7787,8 +7787,12 @@ static Bool evt_get_refstr(GF_FilterEvent *evt, u32 r_idx, GF_RefString **ref_st
 		return GF_TRUE;
 	}
 	if (evt->base.type == GF_FEVT_SEGMENT_SIZE) {
-		if (r_idx) return GF_FALSE;
-		*ref_str = TO_REFSTRING(evt->seg_size.seg_url);
+		if (!r_idx)
+			*ref_str = TO_REFSTRING(evt->seg_size.seg_url);
+		else if (r_idx==1)
+			*ref_str = TO_REFSTRING(evt->seg_size.base64_version);
+		else
+			return GF_FALSE;
 		return GF_TRUE;
 	}
 	if (evt->base.type == GF_FEVT_DASH_QUALITY_SELECT) {
@@ -7852,9 +7856,15 @@ static GF_FilterEvent *init_evt(GF_FilterEvent *evt)
 			url_addr_src = (char **) &evt->seek.source_switch;
 			url_addr_dst = (char **) &an_evt->seek.source_switch;
 		} else if (evt->base.type==GF_FEVT_SEGMENT_SIZE) {
-			if (i) break;
-			url_addr_src = (char **) &evt->seg_size.seg_url;
-			url_addr_dst = (char **) &an_evt->seg_size.seg_url;
+			if (!i) {
+				url_addr_src = (char **) &evt->seg_size.seg_url;
+				url_addr_dst = (char **) &an_evt->seg_size.seg_url;
+			} else if (i==1) {
+				url_addr_src = (char **) &evt->seg_size.base64_version;
+				url_addr_dst = (char **) &an_evt->seg_size.base64_version;
+			} else {
+				break;
+			}
 		} else if (evt->base.type==GF_FEVT_DASH_QUALITY_SELECT) {
 			if (!i) {
 				url_addr_src = (char **) &evt->dash_select.period_id;

src/filter_core/filter_props.c
@@ -1709,6 +1709,7 @@ GF_BuiltInProperty GF_BuiltInProps [] =
 	"- negative value: LL-HLS compatability mode", GF_PROP_SINT, GF_PROP_FLAG_GSF_REM),
 	DEC_PROP_F( GF_PROP_PID_MUX_SRC, "MuxSrc", "Name of mux source(s), set by dasher to direct its outputs", GF_PROP_STRING, GF_PROP_FLAG_GSF_REM),
 	DEC_PROP_F( GF_PROP_PID_DASH_MODE, "DashMode", "DASH mode to be used by multiplexer if any, set by dasher. 0 is no DASH, 1 is regular DASH, 2 is VoD", GF_PROP_UINT, GF_PROP_FLAG_GSF_REM),
+	DEC_PROP_F( GF_PROP_PID_DASH_INIT_BASE64, "InitBase64", "Inidcate that multiplexer should send the base64 encoded version of the init segment", GF_PROP_BOOL, GF_PROP_FLAG_GSF_REM),
 	DEC_PROP_F( GF_PROP_PID_FORCE_SEG_SYNC, "SegSync", "Indicate segment must be completely flushed before sending segment/fragment size events", GF_PROP_BOOL, GF_PROP_FLAG_GSF_REM),
 	DEC_PROP_F( GF_PROP_PID_DASH_DUR, "DashDur", "DASH target segment duration in seconds", GF_PROP_FRACTION, GF_PROP_FLAG_GSF_REM),
 	DEC_PROP_F( GF_PROP_PID_DASH_FDUR, "FragDur", "DASH target fragment duration in seconds", GF_PROP_FRACTION, GF_PROP_FLAG_GSF_REM),

commit 5dbf094e35d314306da5bf1de20c41918ba89270

Fixed bug introduced in 47c54a4e

src/filters/isoffin_read.c
@@ -461,9 +461,15 @@ GF_Err isoffin_configure_pid(GF_Filter *filter, GF_FilterPid *pid, Bool is_remov
 	if (!prop || !prop->value.string) {
 		if (!read->mem_load_mode)
 			read->mem_load_mode = 1;
+
 		if (!read->pid) read->pid = pid;
 		read->input_loaded = GF_FALSE;
 		return GF_OK;
+	}
+	//we started with a base64 embedding of init segment, but now have an associated file, leave mem-load mode
+	else if (read->mem_load_mode) {
+		read->mem_load_mode = 0;
+		gf_isom_reset_data_offset(read->mov, NULL);
 	}
 
 	if (read->pid && prop->value.string) {
@@ -1460,8 +1466,6 @@ static GF_Err isoffin_process(GF_Filter *filter)
 				const GF_PropertyValue *p = gf_filter_pid_get_info_str(read->pid, "aborted", &pe);
 				if (p && p->value.boolean) read->was_aborted = GF_TRUE;
 				gf_filter_release_property(pe);
-				//leave mem-load mode - needed for DASH/HLS where init segment is in base64 but not the rest
-				read->mem_load_mode = 0;
 			} else {
 				in_is_flush = GF_TRUE;
 			}