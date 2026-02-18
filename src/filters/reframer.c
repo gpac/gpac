b515818621971fd87833b571f52c652808083411
Jean Le Feuvre <jeanlf@gpac.io>
2026-02-18 19:27:22 +0100

    Fixed possible flush error in split by size - cf #3460

--- src/filters/reframer.c
+++ src/filters/reframer.c
@@ -1632,6 +1632,8 @@ static void check_gop_split(GF_ReframerCtx *ctx)
 			RTStream *st = gf_list_get(ctx->streams, i);
 			if (st->range_start_computed==2) continue;
 			if (st->reinsert_single_pck) continue;
+			//do not check for timestamp if in EOS, implicitly flush the range
+			if (st->in_eos) continue;
 			pck = gf_list_last(st->pck_queue);
 			if (!pck) continue;
 			ts = gf_filter_pck_get_dts(pck);
