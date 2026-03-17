54b52add849a11742e2ea36db98295aad85b89e0
Aurelien David <aurelien.david@telecom-paristech.fr>
2026-03-17 13:39:14 +0100

    fuzz: enforce null terminating URI in iKMS_box_read()
    fixes 3494

--- src/isomedia/box_code_drm.c
+++ src/isomedia/box_code_drm.c
@@ -279,9 +279,12 @@ GF_Err iKMS_box_read(GF_Box *s, GF_BitStream *bs)
 	GF_ISMAKMSBox *ptr = (GF_ISMAKMSBox *)s;
 
 	len = (u32) (ptr->size);
-	ptr->URI = (char*) gf_malloc(sizeof(char)*len);
-	if (!ptr->URI) return GF_OUT_OF_MEM;
-	gf_bs_read_data(bs, ptr->URI, len);
+	if (len) {
+		ptr->URI = (char*) gf_malloc(sizeof(char)*len);
+		if (!ptr->URI) return GF_OUT_OF_MEM;
+		gf_bs_read_data(bs, ptr->URI, len);
+		ptr->URI[len-1] = 0;
+	}
 	return GF_OK;
 }
 
