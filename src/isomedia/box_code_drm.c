92a480321584a14f02fef0f44b51ca09e645e696
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2026-07-07 10:10:06 -0400

    senc v>0: add skeletton

--- src/isomedia/box_code_drm.c
+++ src/isomedia/box_code_drm.c
@@ -1526,6 +1526,9 @@ GF_Err senc_box_write(GF_Box *s, GF_BitStream *bs)
 	u32 sample_count, nb_crypt_samples;
 	GF_SampleEncryptionBox *ptr = (GF_SampleEncryptionBox *) s;
 
+	// TEMP: for test purpose only
+	ptr->version = gf_igetenv("GPAC_CENC_SENC_VER");
+
 	sample_count = gf_list_count(ptr->samp_aux_info);
 	//temp patch until we cleanup the spec...
 	nb_crypt_samples = 0;
@@ -1546,15 +1549,56 @@ GF_Err senc_box_write(GF_Box *s, GF_BitStream *bs)
 
 	gf_bs_write_u32(bs, nb_crypt_samples);
 
-	e = store_senc_info(ptr, bs);
-	if (e) return e;
+	if (ptr->version == 0) {
+		e = store_senc_info(ptr, bs);
+		if (e) return e;
 
-	for (i = 0; i < sample_count; i++) {
-		GF_CENCSampleAuxInfo *sai = (GF_CENCSampleAuxInfo *)gf_list_get(ptr->samp_aux_info, i);
-		if (sai->isNotProtected || !sai->cenc_data_size)
-			continue;
-		gf_bs_write_data(bs, sai->cenc_data, sai->cenc_data_size);
+		for (i = 0; i < sample_count; i++) {
+			GF_CENCSampleAuxInfo *sai = (GF_CENCSampleAuxInfo *)gf_list_get(ptr->samp_aux_info, i);
+			if (sai->isNotProtected || !sai->cenc_data_size)
+				continue;
+			gf_bs_write_data(bs, sai->cenc_data, sai->cenc_data_size);
+		}
+	} else {
+		/*
+unsigned int(32) sample_count;
+{
+if (version==0) {
+	unsigned int(Per_Sample_IV_Size*8) InitializationVector;
+	if (UseSubSampleEncryption) {
+		unsigned int(16) subsample_count;
+		{
+			unsigned int(16) BytesOfClearData;
+			unsigned int(32) BytesOfProtectedData;
+		} [subsample_count ]
 	}
+} else if ((version==1) && isProtected){
+	unsigned int(16) multi_IV_count;
+	for (i=1; i <= multi _IV_count; i++) {
+		unsigned int(16) multi_subindex_IV;
+		unsigned int(Per_Sample_IV_Size*8) IV;
+	}
+	unsigned int(32) subsample_count;
+	{
+		unsigned int(16) multi_subindex;
+		unsigned int(16) BytesOfClearData;
+		unsigned int(32) BytesOfProtectedData;
+	} [subsample_count]
+} else if ((version==2) && isProtected) {
+	unsigned int(Per_Sample_IV_Size*8) InitializationVector;
+	if (UseSubSampleEncryption) {
+		unsigned int(16) subsample_count;
+		{
+			unsigned int(16) BytesOfClearData;
+			unsigned int(32) BytesOfProtectedData;
+		} [subsample_count ]
+	}
+}
+}[ sample_count ]
+*/
+		assert(0); //not implemented yet
+	}
+
 	return GF_OK;
 }
 
