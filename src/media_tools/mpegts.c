gpac/gpac

commit ed03cbf3b5c6a1706d62dfbc5993d1a9e11c4031

old unmaintained code deprecation
- ipmpx removed
- dsmcc removed
- dvb_mpe removed
- html5 media and mse for svg removed
- deprecetad modules directory

src/Makefile
@@ -60,19 +60,7 @@ endif
 
 
 ## libgpac objects gathering: src/odf
-LIBGPAC_ODF=odf/desc_private.o odf/descriptors.o odf/odf_code.o odf/odf_codec.o odf/odf_command.o odf/slc.o
-ifeq ($(MINIMAL_OD),no)
-#LIBGPAC_ODF+=odf/qos.o odf/ipmpx_code.o odf/oci_codec.o
-
-ifeq ($(DISABLE_OD_DUMP),no)
-#LIBGPAC_ODF+=odf/ipmpx_dump.o
-endif
-
-ifeq ($(DISABLE_OD_PARSE),no)
-#LIBGPAC_ODF+=odf/ipmpx_parse.o
-endif
-
-endif
+LIBGPAC_ODF=odf/desc_private.o odf/descriptors.o odf/odf_code.o odf/odf_codec.o odf/odf_command.o odf/slc.o odf/qos.o odf/oci_codec.o
 
 ifeq ($(DISABLE_OD_DUMP),no)
 LIBGPAC_ODF+=odf/odf_dump.o
@@ -117,7 +105,7 @@ ifeq ($(DISABLE_M2TS_MUX),no)
 LIBGPAC_MEDIATOOLS+=media_tools/m2ts_mux.o
 endif
 ifeq ($(DISABLE_DVBX),no)
-LIBGPAC_MEDIATOOLS+=media_tools/ait.o media_tools/dsmcc.o media_tools/dvb_mpe.o media_tools/reedsolomon.o
+LIBGPAC_MEDIATOOLS+=media_tools/reedsolomon.o
 endif
 ifeq ($(DISABLE_AVILIB),no)
 LIBGPAC_MEDIATOOLS+=media_tools/avilib.o

src/media_tools/mpegts.c
@@ -42,14 +42,6 @@
 #include <unistd.h>
 #endif
 
-#ifdef GPAC_ENABLE_MPE
-#include <gpac/dvb_mpe.h>
-#endif
-
-#ifdef GPAC_ENABLE_DSMCC
-#include <gpac/ait.h>
-#endif
-
 #define DEBUG_TS_PACKET 0
 
 GF_EXPORT
@@ -280,12 +272,6 @@ static void gf_m2ts_es_del(GF_M2TS_ES *es, GF_M2TS_Demuxer *ts)
 	if (es->flags & GF_M2TS_ES_IS_SECTION) {
 		GF_M2TS_SECTION_ES *ses = (GF_M2TS_SECTION_ES *)es;
 		if (ses->sec) gf_m2ts_section_filter_del(ses->sec);
-
-#ifdef GPAC_ENABLE_MPE
-		if (es->flags & GF_M2TS_ES_IS_MPE)
-			gf_dvb_mpe_section_del(es);
-#endif
-
 	} else if (es->pid!=es->program->pmt_pid) {
 		GF_M2TS_PES *pes = (GF_M2TS_PES *)es;
 
@@ -344,35 +330,12 @@ static void gf_m2ts_section_complete(GF_M2TS_Demuxer *ts, GF_M2TS_SectionFilter 
 
 	if (!sec->process_section) {
 		if ((ts->on_event && (sec->section[0]==GF_M2TS_TABLE_ID_AIT)) ) {
-#ifdef GPAC_ENABLE_DSMCC
-			GF_M2TS_SL_PCK pck;
-			pck.data_len = sec->length;
-			pck.data = sec->section;
-			pck.stream = (GF_M2TS_ES *)ses;
-			//ts->on_event(ts, GF_M2TS_EVT_AIT_FOUND, &pck);
-			on_ait_section(ts, GF_M2TS_EVT_AIT_FOUND, &pck);
-#endif
 		} else if ((ts->on_event && (sec->section[0]==GF_M2TS_TABLE_ID_DSM_CC_ENCAPSULATED_DATA	|| sec->section[0]==GF_M2TS_TABLE_ID_DSM_CC_UN_MESSAGE ||
 		                             sec->section[0]==GF_M2TS_TABLE_ID_DSM_CC_DOWNLOAD_DATA_MESSAGE || sec->section[0]==GF_M2TS_TABLE_ID_DSM_CC_STREAM_DESCRIPTION || sec->section[0]==GF_M2TS_TABLE_ID_DSM_CC_PRIVATE)) ) {
 
-#ifdef GPAC_ENABLE_DSMCC
-			GF_M2TS_SL_PCK pck;
-			pck.data_len = sec->length;
-			pck.data = sec->section;
-			pck.stream = (GF_M2TS_ES *)ses;
-			on_dsmcc_section(ts,GF_M2TS_EVT_DSMCC_FOUND,&pck);
-			//ts->on_event(ts, GF_M2TS_EVT_DSMCC_FOUND, &pck);
-#endif
 		}
-#ifdef GPAC_ENABLE_MPE
-		else if (ts->on_mpe_event && ((ses && (ses->flags & GF_M2TS_EVT_DVB_MPE)) || (sec->section[0]==GF_M2TS_TABLE_ID_INT)) ) {
-			GF_M2TS_SL_PCK pck;
-			pck.data_len = sec->length;
-			pck.data = sec->section;
-			pck.stream = (GF_M2TS_ES *)ses;
-			ts->on_mpe_event(ts, GF_M2TS_EVT_DVB_MPE, &pck);
+		else if (((ses && (ses->flags & GF_M2TS_EVT_DVB_MPE)) || (sec->section[0]==GF_M2TS_TABLE_ID_INT)) ) {
 		}
-#endif
 		else if ((ts->on_event && (sec->section[0]==GF_M2TS_TABLE_ID_SCTE35_SPLICE_INFO)) ) {
 			GF_M2TS_SL_PCK pck;
 			pck.data_len = sec->length;
@@ -1536,13 +1499,6 @@ static void gf_m2ts_process_pmt(GF_M2TS_Demuxer *ts, GF_M2TS_SECTION_ES *pmt, GF
 		case GF_M2TS_MPE_SECTIONS:
 			if (! ts->prefix_present) {
 				GF_LOG(GF_LOG_INFO, GF_LOG_CONTAINER, ("stream type MPE found : pid = %d \n", pid));
-#ifdef GPAC_ENABLE_MPE
-				es = gf_dvb_mpe_section_new();
-				if (es->flags & GF_M2TS_ES_IS_SECTION) {
-					/* NULL means: trigger the call to on_event with DVB_GENERAL type and the raw section as payload */
-					((GF_M2TS_SECTION_ES*)es)->sec = gf_m2ts_section_filter_new(NULL, GF_TRUE);
-				}
-#endif
 				break;
 			}
 
@@ -3348,41 +3304,8 @@ GF_M2TS_Demuxer *gf_m2ts_demux_new()
 	ts->eit = gf_m2ts_section_filter_new(NULL, GF_TRUE);
 	ts->tdt_tot = gf_m2ts_section_filter_new(gf_m2ts_process_tdt_tot, GF_TRUE);
 
-#ifdef GPAC_ENABLE_MPE
-	gf_dvb_mpe_init(ts);
-#endif
-
 	ts->nb_prog_pmt_received = 0;
-	ts->ChannelAppList = gf_list_new();
 	return ts;
-}
-
-GF_EXPORT
-void gf_m2ts_demux_dmscc_init(GF_M2TS_Demuxer *ts) {
-
-	char temp_dir[GF_MAX_PATH];
-	u32 length;
-	GF_Err e;
-
-	ts->dsmcc_controler = gf_list_new();
-	ts->process_dmscc = GF_TRUE;
-
-	gf_strcpy(temp_dir, gf_get_default_cache_directory() );
-	length = (u32) strlen(temp_dir);
-	if(temp_dir[length-1] == GF_PATH_SEPARATOR) {
-		temp_dir[length-1] = 0;
-	}
-
-	ts->dsmcc_root_dir = (char*)gf_calloc(strlen(temp_dir)+strlen("CarouselData")+2,1);
-	if (!ts->dsmcc_root_dir)
-		e = GF_OUT_OF_MEM;
-	else {
-		sprintf(ts->dsmcc_root_dir,"%s%cCarouselData",temp_dir,GF_PATH_SEPARATOR);
-		e = gf_mkdir(ts->dsmcc_root_dir);
-	}
-	if (e) {
-		GF_LOG(GF_LOG_INFO, GF_LOG_CONTAINER, ("[Process DSMCC] Error during the creation of the directory %s \n",ts->dsmcc_root_dir));
-	}
 }
 
 GF_EXPORT
@@ -3430,47 +3353,8 @@ void gf_m2ts_demux_del(GF_M2TS_Demuxer *ts)
 	gf_m2ts_reset_sdt(ts);
 	if (ts->tdt_tot)
 		gf_list_del(ts->SDTs);
-
-#ifdef GPAC_ENABLE_MPE
-	gf_dvb_mpe_shutdown(ts);
-#endif
-
-	if (ts->dsmcc_controler) {
-		if (gf_list_count(ts->dsmcc_controler)) {
-#ifdef GPAC_ENABLE_DSMCC
-			GF_M2TS_DSMCC_OVERLORD* dsmcc_overlord = (GF_M2TS_DSMCC_OVERLORD*)gf_list_get(ts->dsmcc_controler,0);
-			gf_dir_cleanup(dsmcc_overlord->root_dir);
-			gf_rmdir(dsmcc_overlord->root_dir);
-			gf_m2ts_delete_dsmcc_overlord(dsmcc_overlord);
-			if(ts->dsmcc_root_dir) {
-				gf_free(ts->dsmcc_root_dir);
-			}
-#endif
-		}
-		gf_list_del(ts->dsmcc_controler);
-	}
-
-	while(gf_list_count(ts->ChannelAppList)) {
-#ifdef GPAC_ENABLE_DSMCC
-		GF_M2TS_CHANNEL_APPLICATION_INFO* ChanAppInfo = (GF_M2TS_CHANNEL_APPLICATION_INFO*)gf_list_get(ts->ChannelAppList,0);
-		gf_m2ts_delete_channel_application_info(ChanAppInfo);
-		gf_list_rem(ts->ChannelAppList,0);
-#endif
-	}
-	gf_list_del(ts->ChannelAppList);
-
-	if (ts->dsmcc_root_dir) gf_free(ts->dsmcc_root_dir);
 	gf_free(ts);
 }
-
-#if 0//unused
-void gf_m2ts_print_info(GF_M2TS_Demuxer *ts)
-{
-#ifdef GPAC_ENABLE_MPE
-	gf_dvb_mpe_print_info(ts);
-#endif
-}
-#endif
 
 //20 packets max
 #define M2TS_PROBE_SIZE	188*20

modules/deprecated/old_arch/audio_filter/Makefile
@@ -1,42 +1,0 @@
-include ../../config.mak
-
-vpath %.c $(SRC_PATH)/modules/audio_filter
-
-CFLAGS= $(OPTFLAGS) -I"$(SRC_PATH)/include"
-
-ifeq ($(DEBUGBUILD),yes)
-CFLAGS+=-g
-LDFLAGS+=-g
-endif
-
-ifeq ($(GPROFBUILD),yes)
-CFLAGS+=-pg
-LDFLAGS+=-pg
-endif
-
-#common obj
-OBJS= audio_filter.o
-
-SRCS := $(OBJS:.o=.c) 
-
-LIB=gm_audio_filter$(DYN_LIB_SUFFIX)
-
-all: $(LIB)
-
-$(LIB): $(OBJS)
-	$(CC) $(SHFLAGS) -o ../../bin/gcc/$@ $(OBJS) $(EXTRALIBS) -L../../bin/gcc -lgpac $(LDFLAGS)
-
-
-clean: 
-	rm -f $(OBJS) ../../bin/gcc/$(LIB)
-
-dep: depend
-
-depend:
-	rm -f .depend	
-	$(CC) -MM $(CFLAGS) $(SRCS) 1>.depend
-
-distclean: clean
-	rm -f Makefile.bak .depend
-
--include .depend

modules/deprecated/old_arch/avcap/Makefile
@@ -1,46 +1,0 @@
-include ../../config.mak
-
-vpath %.c $(SRC_PATH)/modules/avcap
-
-CFLAGS= $(CXXFLAGS) -I"$(SRC_PATH)/include" $(AVCAP_CFLAGS)
-
-ifeq ($(DEBUGBUILD),yes)
-CFLAGS+=-g
-LDFLAGS+=-g
-endif
-
-ifeq ($(GPROFBUILD),yes)
-CFLAGS+=-pg
-LDFLAGS+=-pg
-endif
-
-LINKLIBS= $(AVCAP_LDFLAGS) -L../../bin/gcc -lgpac
-
-
-#common objects
-OBJS=avcap.o
-
-SRCS := $(OBJS:.o=.c) 
-
-LIB=gm_avcap$(DYN_LIB_SUFFIX)
-
-
-all: $(LIB)
-
-
-$(LIB): $(OBJS)
-	$(CXX) -w $(SHFLAGS) -o ../../bin/gcc/$@ $(OBJS) $(LINKLIBS) $(LDFLAGS)
-
-clean: 
-	rm -f $(OBJS) ../../bin/gcc/$(LIB)
-
-dep: depend
-
-depend:
-	rm -f .depend	
-	$(CXX) -MM $(CFLAGS) $(SRCS) 1>.depend
-
-distclean: clean
-	rm -f Makefile.bak .depend
-
--include .depend

modules/deprecated/old_arch/ffmpeg_in/Makefile
@@ -1,88 +1,0 @@
-include ../../config.mak
-
-vpath %.c $(SRC_PATH)/modules/ffmpeg_in
-
-CFLAGS=-fPIC $(OPTFLAGS) -Wno-deprecated-declarations -I"$(SRC_PATH)/include" $(ffmpeg_cflags)
-
-ifeq ($(DEBUGBUILD),yes)
-CFLAGS+=-g
-LDFLAGS+=-g
-endif
-
-ifeq ($(GPROFBUILD),yes)
-CFLAGS+=-pg
-LDFLAGS+=-pg
-endif
-
-LOCAL_LIB=
-
-LINKFLAGS=$(ffmpeg_lflags)
-
-ifeq ($(STATICBUILD),yes)
-##include static modules and other deps for libgpac
-include ../../static.mak
-LINKFLAGS+=-lgpac_static
-LINKFLAGS+=$(EXTRALIBS)
-else
-LINKFLAGS+=-lgpac
-endif
-
-#common obj
-OBJS=ffmpeg_decode.o ffmpeg_demux.o ffmpeg_load.o
-
-
-#darwin needs bz2
-ifeq ($(CONFIG_DARWIN),yes)
-LINKFLAGS+=-lbz2
-endif
-
-#old ffmpeg lib
-ifeq ($(CONFIG_FFMPEG_OLD),yes)
-CFLAGS+=-DFFMPEG_OLD_HEADERS -I/usr/include
-else
-LINKFLAGS+=-lswscale
-endif
-
-#local ffmpeg lib
-ifeq ($(CONFIG_FFMPEG),local)
-LOCAL_LIB=-L../../extra_lib/lib/gcc
-CFLAGS+= -I"$(LOCAL_INC_PATH)"
-endif
-
-ifeq ($(CONFIG_ZLIB),local)
-LOCAL_LIB=-L../../extra_lib/lib/gcc
-endif
-
-
-SRCS := $(OBJS:.o=.c)
-
-LIB=gm_ffmpeg_in$(DYN_LIB_SUFFIX)
-ifeq ($(CONFIG_WIN32),yes)
-#LDFLAGS+=-export-symbols ffmpeg_in.def
-endif
-
-#need to add cross-libraries if not local
-ifeq ($(CROSS_COMPILING),yes)
-LINKFLAGS+=-L$(prefix)/$(libdir)
-endif
-
-
-all: $(LIB)
-
-
-$(LIB): $(OBJS)
-	$(CC) $(SHFLAGS) -o ../../bin/gcc/$@ $(OBJS) -L../../bin/gcc $(LOCAL_LIB) $(LDFLAGS) $(LINKFLAGS)
-
-clean:
-	rm -f $(OBJS) ../../bin/gcc/$(LIB)
-
-dep: depend
-
-depend:
-	rm -f .depend
-	$(CC) -MM $(CFLAGS) $(SRCS) 1>.depend
-
-distclean: clean
-	rm -f Makefile.bak .depend
-
--include .depend

modules/deprecated/old_arch/freenect/Makefile
@@ -1,54 +1,0 @@
-include ../../config.mak
-
-vpath %.c $(SRC_PATH)/modules/freenect
-
-CFLAGS= $(CXXFLAGS) -I"$(SRC_PATH)/include" -DGPAC_HAVE_CONFIG_H -I../.. $(FREENECT_CFLAGS)
-
-ifeq ($(DEBUGBUILD),yes)
-CFLAGS+=-g
-LDFLAGS+=-g
-endif
-
-ifeq ($(GPROFBUILD),yes)
-CFLAGS+=-pg
-LDFLAGS+=-pg
-endif
-
-
-LINKLIBS=
-
-ifeq ($(CONFIG_FREENECT),local)
-LINKLIBS+=-L../../extra_lib/lib/gcc
-CFLAGS+= -I"$(LOCAL_INC_PATH)"
-endif
-
-LINKLIBS+=-lfreenect -L../../bin/gcc -lgpac
-
-
-#common objects
-OBJS=freenect.o
-
-SRCS := $(OBJS:.o=.c) 
-
-LIB=gm_freenect$(DYN_LIB_SUFFIX)
-
-
-all: $(LIB)
-
-
-$(LIB): $(OBJS)
-	$(CXX) -w $(SHFLAGS) -o ../../bin/gcc/$@ $(OBJS) $(LINKLIBS) $(LDFLAGS)
-
-clean: 
-	rm -f $(OBJS) ../../bin/gcc/$(LIB)
-
-dep: depend
-
-depend:
-	rm -f .depend	
-	$(CXX) -MM $(CFLAGS) $(SRCS) 1>.depend
-
-distclean: clean
-	rm -f Makefile.bak .depend
-
--include .depend

modules/deprecated/old_arch/hyb_in/Makefile
@@ -1,46 +1,0 @@
-include ../../config.mak
-
-vpath %.c $(SRC_PATH)/modules/hyb_in
-
-CFLAGS= $(OPTFLAGS) -I"$(SRC_PATH)/include"
-
-ifeq ($(DEBUGBUILD),yes)
-CFLAGS+=-g
-LDFLAGS+=-g
-endif
-
-ifeq ($(GPROFBUILD),yes)
-CFLAGS+=-pg
-LDFLAGS+=-pg
-endif
-
-#common obj
-OBJS=hyb_in.o fm_fake_pull.o fm_fake_push.o
-
-SRCS := $(OBJS:.o=.c) 
-
-LIB=gm_hyb_in$(DYN_LIB_SUFFIX)
-
-
-all: $(LIB)
-
-
-$(LIB): $(OBJS)
-	$(CC) $(SHFLAGS) -o ../../bin/gcc/$@ $(OBJS) $(EXTRALIBS) -L../../bin/gcc -lgpac $(LDFLAGS)
-ifeq ($(STATICBUILD),yes)
-	$(CC) $(SHFLAGS) -o ../../bin/gcc/gm_hyb_in-static$(DYN_LIB_SUFFIX) $(OBJS) -L../../bin/gcc -lgpac_static $(LDFLAGS)
-endif
-
-clean: 
-	rm -f $(OBJS) ../../bin/gcc/$(LIB)
-
-dep: depend
-
-depend:
-	rm -f .depend	
-	$(CC) -MM $(CFLAGS) $(SRCS) 1>.depend
-
-distclean: clean
-	rm -f Makefile.bak .depend
-
--include .depend

modules/deprecated/old_arch/libplayer/Makefile
@@ -1,43 +1,0 @@
-include ../../config.mak
-
-vpath %.c $(SRC_PATH)/modules/libplayer
-
-CFLAGS= $(OPTFLAGS) -I"$(SRC_PATH)/include"
-
-ifeq ($(DEBUGBUILD),yes)
-CFLAGS+=-g
-LDFLAGS+=-g
-endif
-
-ifeq ($(GPROFBUILD),yes)
-CFLAGS+=-pg
-LDFLAGS+=-pg
-endif
-
-#common obj
-OBJS= libplayer.o
-
-SRCS := $(OBJS:.o=.c) 
-
-LIB=gm_libplayer$(DYN_LIB_SUFFIX)
-
-
-all: $(LIB)
-
-
-$(LIB): $(OBJS)
-	$(CC) $(SHFLAGS) -o ../../bin/gcc/$@ $(OBJS) $(EXTRALIBS) -L../../bin/gcc -lgpac -lplayer $(LDFLAGS)
-
-clean: 
-	rm -f $(OBJS) ../../bin/gcc/$(LIB)
-
-dep: depend
-
-depend:
-	rm -f .depend	
-	$(CC) -MM $(CFLAGS) $(SRCS) 1>.depend
-
-distclean: clean
-	rm -f Makefile.bak .depend
-
--include .depend

commit ed8a4a7334e5c36c465f049eb769966a0aa1d31b

fixed bug introduced by ed03cbf3

configure
@@ -238,7 +238,6 @@ push_feature "isoff-hds" "Adobe HDS in ISOBMFF support"
 push_feature "streaming" "RTP/RTSP/SDP support"
 push_feature "fonts" "Font and Text engine (used by compositor and EVG.js"
 push_feature "player" "deprecated - old name for compositor feature"
-push_feature "dvbx" "!DVB-specific tools (MPE, FEC, DSM-CC)"
 push_feature "vobsub" "VobSub support"
 push_feature "ttxt" "TTXT (3GPP / MPEG-4 Timed Text) support"
 push_feature "ttml" "TTML/EBU TTD support"
@@ -2523,11 +2522,6 @@ if test "$disable_streaming" = "yes" ; then
     echo "RTP/RTSP/SDP streaming disabled"
     echo "#define GPAC_DISABLE_STREAMING" >> $TMPH
 fi
-if test "$disable_dvbx" = "no" ; then
-    echo "DVB MPE and DSM-CC enabled"
-    echo "#define GPAC_ENABLE_MPE" >> $TMPH
-    echo "#define GPAC_ENABLE_DSMCC" >> $TMPH
-fi
 if test "$disable_vobsub" = "yes" ; then
     echo "VobSub disabled"
     echo "#define GPAC_DISABLE_VOBSUB" >> $TMPH
@@ -3112,7 +3106,6 @@ echo "DISABLE_SCENE_DUMP=$disable_scene_dump" >> config.mak
 echo "DISABLE_SCENE_ENCODE=$disable_scene_encode" >> config.mak
 echo "DISABLE_SCENEGRAPH=$disable_scenegraph" >> config.mak
 echo "DISABLE_CRYPTO=$disable_crypto" >> config.mak
-echo "DISABLE_DVBX=$disable_dvbx" >> config.mak
 echo "DISABLE_AVILIB=$disable_avi" >> config.mak
 echo "DISABLE_M2PS=$disable_m2ps" >> config.mak
 echo "DISABLE_OGG=$disable_ogg" >> config.mak

src/Makefile
@@ -60,7 +60,10 @@ endif
 
 
 ## libgpac objects gathering: src/odf
-LIBGPAC_ODF=odf/desc_private.o odf/descriptors.o odf/odf_code.o odf/odf_codec.o odf/odf_command.o odf/slc.o odf/qos.o odf/oci_codec.o
+LIBGPAC_ODF=odf/desc_private.o odf/descriptors.o odf/odf_code.o odf/odf_codec.o odf/odf_command.o odf/slc.o
+ifeq ($(MINIMAL_OD),no)
+LIBGPAC_ODF+=odf/qos.o odf/oci_codec.o
+endif
 
 ifeq ($(DISABLE_OD_DUMP),no)
 LIBGPAC_ODF+=odf/odf_dump.o

src/media_tools/mpegts.c
@@ -334,7 +334,7 @@ static void gf_m2ts_section_complete(GF_M2TS_Demuxer *ts, GF_M2TS_SectionFilter 
 		                             sec->section[0]==GF_M2TS_TABLE_ID_DSM_CC_DOWNLOAD_DATA_MESSAGE || sec->section[0]==GF_M2TS_TABLE_ID_DSM_CC_STREAM_DESCRIPTION || sec->section[0]==GF_M2TS_TABLE_ID_DSM_CC_PRIVATE)) ) {
 
 		}
-		else if (((ses && (ses->flags & GF_M2TS_EVT_DVB_MPE)) || (sec->section[0]==GF_M2TS_TABLE_ID_INT)) ) {
+		else if (sec->section[0]==GF_M2TS_TABLE_ID_INT) {
 		}
 		else if ((ts->on_event && (sec->section[0]==GF_M2TS_TABLE_ID_SCTE35_SPLICE_INFO)) ) {
 			GF_M2TS_SL_PCK pck;