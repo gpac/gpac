04bf5fed4993b6ba64c6d0d7ce927c2e8eb44726
Aurelien David <aurelien.david@telecom-paristech.fr>
2026-07-28 12:03:00 +0200

    fuzz: more nodes mem fixes

--- src/filters/reframe_av1.c
+++ src/filters/reframe_av1.c
@@ -1399,6 +1399,7 @@ static void av1dmx_finalize(GF_Filter *filter)
 	GF_AV1DmxCtx *ctx = gf_filter_get_udta(filter);
 	if (ctx->bs) gf_bs_del(ctx->bs);
 	if (ctx->indexes) gf_free(ctx->indexes);
+	if (ctx->src_pck) gf_filter_pck_unref(ctx->src_pck);
 
 	gf_av1_reset_state(&ctx->state, GF_TRUE);
 	if (ctx->state.config) gf_odf_av1_cfg_del(ctx->state.config);
