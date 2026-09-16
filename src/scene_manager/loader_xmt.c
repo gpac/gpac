e914e91a3b98ce5ae56bbd60ff3248fe29faa5d1
Ilias Aberkane <iliasaberkane2908@gmail.com>
2026-09-16 20:16:31 +0200

    fix stack buffer overflow in xmt_resolve_od_links od-link URL rewrite
    The sprintf rewriting OD-link URL fragments into a fixed 5000-byte stack
    buffer could be overflowed by a crafted XMT scene supplying an over-long
    URL fragment (observed write of 9031 bytes into the 5000-byte buffer).
    Allocate the rewritten URL dynamically instead, which also removes the
    fixed stack buffer entirely.
    
    Fixes gpac/gpac#3902

--- src/scene_manager/loader_xmt.c
+++ src/scene_manager/loader_xmt.c
@@ -445,7 +445,6 @@ static void xmt_resolve_od_links(GF_XMTParser *parser)
 	u32 i, j;
 	XMT_ESDLink *esdl, *esdl2;
 	XMT_ODLink *l;
-	char szURL[5000];
 
 	/*fix ESD IDs*/
 	i=0;
@@ -599,9 +598,14 @@ static void xmt_resolve_od_links(GF_XMTParser *parser)
 					seg = NULL;
 					if (url->url) seg = strstr(url->url, "#");
 					if (seg) {
-						sprintf(szURL, "od:%d#%s", l->od->objectDescriptorID, seg+1);
-						gf_free(url->url);
-						url->url = gf_strdup(szURL);
+						/* "od:" + u16 id + "#" + fragment + \0, fragment may be arbitrarily long */
+						u32 len = (u32) strlen(seg+1) + 16;
+						char *new_url = (char *) gf_malloc(len);
+						if (new_url) {
+							snprintf(new_url, len, "od:%d#%s", l->od->objectDescriptorID, seg+1);
+							gf_free(url->url);
+							url->url = new_url;
+						}
 					} else {
 						if (url->url) gf_free(url->url);
 						url->url = NULL;
